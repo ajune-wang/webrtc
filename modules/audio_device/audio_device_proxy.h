527ce0cdfc9c7dacd135bb970af9444b3f7cc8c1
henrika <henrika@webrtc.org>
2017-10-06 14:09:01 +0200

    ADM Proxy [EXPERIMENTAL]
    nit
    
    
    more work
    
    
    Added factory class
    
    
    Added AudioDeviceProxy class
    
    
    nit
    
    
    Added ADM proxy
    
    
    now works
    
    
    Added adm_test
    
    Bug:
    Change-Id: Icdadb9c58af2ca0718c393bd35cee0f6ef683c09

---
+++ modules/audio_device/audio_device_proxy.h
@@ -0,0 +1,28 @@
+/*
+ *  Copyright (c) 2017 The WebRTC project authors. All Rights Reserved.
+ *
+ *  Use of this source code is governed by a BSD-style license
+ *  that can be found in the LICENSE file in the root of the source
+ *  tree. An additional intellectual property rights grant can be found
+ *  in the file PATENTS.  All contributing project authors may
+ *  be found in the AUTHORS file in the root of the source tree.
+ */
+
+#ifndef MODULES_AUDIO_DEVICE_AUDIO_DEVICE_PROXY_H_
+#define MODULES_AUDIO_DEVICE_AUDIO_DEVICE_PROXY_H_
+
+#include "modules/audio_device/include/audio_device.h"
+#include "rtc_base/scoped_ref_ptr.h"
+
+namespace webrtc {
+
+class AudioDeviceProxy {
+ public:
+  static rtc::scoped_refptr<AudioDeviceModule> Create(
+      const int32_t id,
+      const AudioDeviceModule::AudioLayer audio_layer);
+};
+
+}  // namespace webrtc
+
+#endif  // MODULES_AUDIO_DEVICE_AUDIO_DEVICE_PROXY_H_

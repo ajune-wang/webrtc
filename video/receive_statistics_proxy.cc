af5dac05ab32de89a0cb43fcdc263e52cb9a60c9
Ilya Nikolaevskiy <ilnik@webrtc.org>
2017-10-10 09:46:20 +0200

    Fix potential crash bug in debug builds
    The call to |interframe_delay_max_moving_.Add()| below depends on |now|
    non decreasing in consequtive calls. However, if two threads are
    competing for the lock it may happen that current thread calculates |now|
    before the other thread, yet it will get the lock later. This will result
    in decreasing local time in consequtive calls and trigger a DCHECK.
    
    Bug:none
    Change-Id: I3376d88d4448c2c105e9227a445b11cd6ba8d341

--- video/receive_statistics_proxy.cc
+++ video/receive_statistics_proxy.cc
@@ -628,9 +628,9 @@ void ReceiveStatisticsProxy::DataCountersUpdated(
 
 void ReceiveStatisticsProxy::OnDecodedFrame(rtc::Optional<uint8_t> qp,
                                             VideoContentType content_type) {
-  uint64_t now = clock_->TimeInMilliseconds();
-
   rtc::CritScope lock(&crit_);
+
+  uint64_t now = clock_->TimeInMilliseconds();
 
   ContentSpecificStats* content_specific_stats =
       &content_specific_stats_[content_type];

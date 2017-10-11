075e90fd0ac8d865bde48bc9c511cd35fc04849c
Ilya Nikolaevskiy <ilnik@webrtc.org>
2017-10-11 17:32:18 +0200

    Fix potential crash in debug build due to clock drift
    If clock sudennly jumps into the past because of some caching
    optimizations, it will trigger DCHECK in the movingMaxCounter, as it
    strictly requires monotonically increasing timestamps as an input.
    
    This fix allows MovingMaxCounter to tolerate small jumps to the past
    and adjust input time automatically.
    
    Bug:none
    Change-Id: I66c1939d334cb18dea5a6738025cebfa48a82182

--- rtc_base/moving_max_counter.h
+++ rtc_base/moving_max_counter.h
@@ -23,6 +23,10 @@
 
 namespace rtc {
 
+namespace {
+const int64_t KMaxAllowedTimeAdjustmentMs = 10;
+}
+
 // Implements moving max: can add samples to it and calculate maximum over some
 // fixed moving window.
 //
@@ -44,6 +48,10 @@ class MovingMaxCounter {
   void Reset();
 
  private:
+  // Adjusts input time in case of a clock drift to make it increasing.
+  // Adjusted value is returned. It will be adjusted by at most
+  // KMaxAllowedTimeAdjustmentMs
+  int64_t AdjustTime(int64_t time_ms);
   // Throws out obsolete samples.
   void RollWindow(int64_t new_time_ms);
   const int64_t window_length_ms_;
@@ -66,6 +74,7 @@ MovingMaxCounter<T>::MovingMaxCounter(int64_t window_length_ms)
 
 template <class T>
 void MovingMaxCounter<T>::Add(const T& sample, int64_t current_time_ms) {
+  current_time_ms = AdjustTime(current_time_ms);
   RollWindow(current_time_ms);
   // Remove samples that will never be maximum in any window: newly added sample
   // will always be in all windows the previous samples are. Thus, smaller or
@@ -84,6 +93,7 @@ void MovingMaxCounter<T>::Add(const T& sample, int64_t current_time_ms) {
 
 template <class T>
 rtc::Optional<T> MovingMaxCounter<T>::Max(int64_t current_time_ms) {
+  current_time_ms = AdjustTime(current_time_ms);
   RollWindow(current_time_ms);
   rtc::Optional<T> res;
   if (!samples_.empty()) {
@@ -95,6 +105,15 @@ rtc::Optional<T> MovingMaxCounter<T>::Max(int64_t current_time_ms) {
 template <class T>
 void MovingMaxCounter<T>::Reset() {
   samples_.clear();
+}
+
+template <class T>
+int64_t MovingMaxCounter<T>::AdjustTime(int64_t time_ms) {
+  if (time_ms < last_call_time_ms_) {
+    return std::min(last_call_time_ms_, time_ms + KMaxAllowedTimeAdjustmentMs);
+  } else {
+    return time_ms;
+  }
 }
 
 template <class T>

5eb643a14ed56d9abb202ca97a003b379b9c61a4
Emircan Uysaler <emircan@webrtc.org>
2017-10-17 03:05:32 -0700

    Add StereoCodecAdapter classes
    This CL is the step 1 for adding alpha channel support over the wire in webrtc.
    - Add the footprint for adapter classes that wraps actual codecs.
    - This CL does not add a webrtc::VideoFrame container that can carry alpha to 
    make the CL shorter for an easier review. Therefore, it exercises a code path
    for when we receive no alpha input, just regular I420 frames.
    - Unittest sends a video frame for encode/decode through these adapters and 
    checks the output PSNR.
    - See https://webrtc-review.googlesource.com/c/src/+/7800 for the experimental 
    CL that gives an idea about how it will come together.
    Design Doc: https://goo.gl/sFeSUT
    
    Bug:webrtc:7671
    Change-Id: I9d3be13647a0a958feceb8d7a9aa93852fc6a1fa

---
+++ modules/video_coding/codecs/stereo/test/stereo_adapter_unittest.cc
@@ -0,0 +1,73 @@
+/*
+ *  Copyright (c) 2017 The WebRTC project authors. All Rights Reserved.
+ *
+ *  Use of this source code is governed by a BSD-style license
+ *  that can be found in the LICENSE file in the root of the source
+ *  tree. An additional intellectual property rights grant can be found
+ *  in the file PATENTS.  All contributing project authors may
+ *  be found in the AUTHORS file in the root of the source tree.
+ */
+
+#include "common_video/libyuv/include/webrtc_libyuv.h"
+#include "media/engine/internaldecoderfactory.h"
+#include "media/engine/internalencoderfactory.h"
+#include "modules/video_coding/codecs/stereo/include/stereo_decoder_adapter.h"
+#include "modules/video_coding/codecs/stereo/include/stereo_encoder_adapter.h"
+#include "modules/video_coding/codecs/test/video_codec_test.h"
+
+namespace webrtc {
+
+class TestStereoAdapter : public VideoCodecTest {
+ public:
+  TestStereoAdapter()
+      : internal_decoder_factory_(new cricket::InternalDecoderFactory()),
+        internal_encoder_factory_(new cricket::InternalEncoderFactory()) {}
+
+ protected:
+  VideoDecoder* CreateDecoder() override {
+    return new StereoDecoderAdapter(new cricket::InternalDecoderFactory());
+  }
+
+  VideoEncoder* CreateEncoder() override {
+    return new StereoEncoderAdapter(new cricket::InternalEncoderFactory());
+  }
+
+  VideoCodec codec_settings() override {
+    VideoCodec codec_settings;
+    codec_settings.codecType = webrtc::kVideoCodecVP9;
+    codec_settings.VP9()->numberOfTemporalLayers = 1;
+    codec_settings.VP9()->numberOfSpatialLayers = 1;
+    return codec_settings;
+  }
+
+ private:
+  const std::unique_ptr<cricket::WebRtcVideoDecoderFactory>
+      internal_decoder_factory_;
+  const std::unique_ptr<cricket::WebRtcVideoEncoderFactory>
+      internal_encoder_factory_;
+};
+
+TEST_F(TestStereoAdapter, ConstructAndDestructEncoder) {
+  EXPECT_EQ(WEBRTC_VIDEO_CODEC_OK, encoder_->Release());
+}
+
+TEST_F(TestStereoAdapter, ConstructAndDestructDecoder) {
+  EXPECT_EQ(WEBRTC_VIDEO_CODEC_OK, decoder_->Release());
+}
+
+TEST_F(TestStereoAdapter, EncodeDecodeI420Frame) {
+  EXPECT_EQ(WEBRTC_VIDEO_CODEC_OK,
+            encoder_->Encode(*input_frame_, nullptr, nullptr));
+  EncodedImage encoded_frame;
+  CodecSpecificInfo codec_specific_info;
+  ASSERT_TRUE(WaitForEncodedFrame(&encoded_frame, &codec_specific_info));
+  EXPECT_EQ(WEBRTC_VIDEO_CODEC_OK,
+            decoder_->Decode(encoded_frame, false, nullptr));
+  std::unique_ptr<VideoFrame> decoded_frame;
+  rtc::Optional<uint8_t> decoded_qp;
+  ASSERT_TRUE(WaitForDecodedFrame(&decoded_frame, &decoded_qp));
+  ASSERT_TRUE(decoded_frame);
+  EXPECT_GT(I420PSNR(input_frame_.get(), decoded_frame.get()), 36);
+}
+
+}  // namespace webrtc

9ddc3a6fa0e66513ff9431b316afa676fef301e3
Åsa Persson <asapersson@webrtc.org>
2017-10-18 17:48:52 +0200

    Change forced software encoder fallback for VP8 to be only based on resolution and not bitrate.
    Switches from VP8 HW to VP8 SW for resolutions <= max_pixels. 
    
    |<- min_pixels  VP8 SW  max_pixels ->|  VP8 HW  |
    
    Bug: webrtc:6634
    Change-Id: Ib324df2b8418659c29d999259c0ed47448310696

--- modules/video_coding/codecs/test/videoprocessor_integrationtest_mediacodec.cc
+++ modules/video_coding/codecs/test/videoprocessor_integrationtest_mediacodec.cc
@@ -12,7 +12,6 @@
 
 #include <vector>
 
-#include "test/field_trial.h"
 #include "test/testsupport/fileutils.h"
 
 namespace webrtc {
@@ -55,38 +54,6 @@ TEST_F(VideoProcessorIntegrationTestMediaCodec, ForemanCif500kbpsVp8) {
   AddRateControlThresholds(20, 95, 22, 11, 10, 0, 1, &rc_thresholds);
 
   QualityThresholds quality_thresholds(30.0, 14.0, 0.86, 0.39);
-
-  ProcessFramesAndMaybeVerify(rate_profile, &rc_thresholds, &quality_thresholds,
-                              nullptr, kNoVisualizationParams);
-}
-
-TEST_F(VideoProcessorIntegrationTestMediaCodec,
-       Foreman240p100kbpsVp8WithForcedSwFallback) {
-  ScopedFieldTrials override_field_trials(
-      "WebRTC-VP8-Forced-Fallback-Encoder/Enabled-150,175,10000,1/");
-
-  config_.filename = "foreman_320x240";
-  config_.input_filename = ResourcePath(config_.filename, "yuv");
-  config_.sw_fallback_encoder = true;
-  SetCodecSettings(&config_, kVideoCodecVP8, 1, false, false, false, false,
-                   false, 320, 240);
-
-  RateProfile rate_profile;
-  SetRateProfile(&rate_profile, 0, 100, 10, 0);    // Start below |low_kbps|.
-  SetRateProfile(&rate_profile, 1, 100, 10, 80);   // Fallback in this bucket.
-  SetRateProfile(&rate_profile, 2, 200, 10, 200);  // Switch back here.
-  rate_profile.frame_index_rate_update[3] = kForemanNumFrames + 1;
-  rate_profile.num_frames = kForemanNumFrames;
-
-  // The thresholds below may have to be tweaked to let even poor MediaCodec
-  // implementations pass. If this test fails on the bots, disable it and
-  // ping brandtr@.
-  std::vector<RateControlThresholds> rc_thresholds;
-  AddRateControlThresholds(0, 50, 75, 70, 10, 0, 1, &rc_thresholds);
-  AddRateControlThresholds(0, 50, 25, 12, 60, 0, 1, &rc_thresholds);
-  AddRateControlThresholds(0, 65, 15, 5, 5, 0, 1, &rc_thresholds);
-
-  QualityThresholds quality_thresholds(33.0, 30.0, 0.90, 0.85);
 
   ProcessFramesAndMaybeVerify(rate_profile, &rc_thresholds, &quality_thresholds,
                               nullptr, kNoVisualizationParams);

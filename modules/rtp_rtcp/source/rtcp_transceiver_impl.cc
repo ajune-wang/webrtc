0f89975917470258d19cd1e326df6223476873bd
Danil Chapovalov <danilchap@webrtc.org>
2017-10-24 23:16:03 +0200

    Change RtcpPacket::PacketReadyCallback to rtc::FunctionView from interface
    Bug:webrtc:5565
    Change-Id: I2df5d7a0554b938888581f1c73dbdb8b85c387cc

--- modules/rtp_rtcp/source/rtcp_transceiver_impl.cc
+++ modules/rtp_rtcp/source/rtcp_transceiver_impl.cc
@@ -17,52 +17,12 @@
 #include "modules/rtp_rtcp/include/receive_statistics.h"
 #include "modules/rtp_rtcp/include/rtp_rtcp_defines.h"
 #include "modules/rtp_rtcp/source/rtcp_packet.h"
+#include "modules/rtp_rtcp/source/rtcp_packet/compound_packet.h"
 #include "modules/rtp_rtcp/source/rtcp_packet/receiver_report.h"
 #include "modules/rtp_rtcp/source/rtcp_packet/report_block.h"
 #include "rtc_base/checks.h"
 
 namespace webrtc {
-namespace {
-
-// Helper to put several RTCP packets into lower layer datagram composing
-// Compound or Reduced-Size RTCP packet, as defined by RFC 5506 section 2.
-class PacketSender : public rtcp::RtcpPacket::PacketReadyCallback {
- public:
-  PacketSender(Transport* transport, size_t max_packet_size)
-      : transport_(transport), max_packet_size_(max_packet_size) {
-    RTC_CHECK_LE(max_packet_size, IP_PACKET_SIZE);
-  }
-  ~PacketSender() override {
-    RTC_DCHECK_EQ(index_, 0) << "Unsent rtcp packet.";
-  }
-
-  // Appends a packet to pending compound packet.
-  // Sends rtcp compound packet if buffer was already full and resets buffer.
-  void AppendPacket(const rtcp::RtcpPacket& packet) {
-    packet.Create(buffer_, &index_, max_packet_size_, this);
-  }
-
-  // Sends pending rtcp compound packet.
-  void Send() {
-    if (index_ > 0) {
-      OnPacketReady(buffer_, index_);
-      index_ = 0;
-    }
-  }
-
- private:
-  // Implements RtcpPacket::PacketReadyCallback
-  void OnPacketReady(uint8_t* data, size_t length) override {
-    transport_->SendRtcp(data, length);
-  }
-
-  Transport* const transport_;
-  const size_t max_packet_size_;
-  size_t index_ = 0;
-  uint8_t buffer_[IP_PACKET_SIZE];
-};
-
-}  // namespace
 
 RtcpTransceiverImpl::RtcpTransceiverImpl(const RtcpTransceiverConfig& config)
     : config_(config) {
@@ -72,7 +32,7 @@ RtcpTransceiverImpl::RtcpTransceiverImpl(const RtcpTransceiverConfig& config)
 RtcpTransceiverImpl::~RtcpTransceiverImpl() = default;
 
 void RtcpTransceiverImpl::SendCompoundPacket() {
-  PacketSender sender(config_.outgoing_transport, config_.max_packet_size);
+  rtcp::CompoundPacket compound;
 
   rtcp::ReceiverReport rr;
   rr.SetSenderSsrc(config_.feedback_ssrc);
@@ -86,11 +46,14 @@ void RtcpTransceiverImpl::SendCompoundPacket() {
     // when RtcpTransceiver handles incoming sender reports.
     rr.SetReportBlocks(std::move(report_blocks));
   }
-  sender.AppendPacket(rr);
+  compound.Append(&rr);
   // TODO(danilchap): Append SDES to conform to the requirements on minimal
   // compound RTCP packet.
 
-  sender.Send();
+  size_t max_packet_size = config_.max_packet_size;
+  compound.Build(max_packet_size, [&](rtc::ArrayView<const uint8_t> packet) {
+    config_.outgoing_transport->SendRtcp(packet.data(), packet.size());
+  });
 }
 
 }  // namespace webrtc

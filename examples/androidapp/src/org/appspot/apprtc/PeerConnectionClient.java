1d722ea8c4a87e13e79d184e9347fd02f95e13f5
henrika <henrika@webrtc.org>
2017-10-30 13:11:42 +0100

    Add SetAudioPlayout and SetAudioRecording methods to the PeerConnection API
    (this CL is based on the work by Taylor and Steve in https://webrtc-review.googlesource.com/c/src/+/10201)
    
    This SetAudioPlayout method lets applications disable audio playout while
    still processing incoming audio data and generating statistics on the
    received audio.
    
    This may be useful if the application wants to set up media flows as
    soon as possible, but isn't ready to play audio yet. Currently, native
    applications don't have any API point to control this, unless they
    completely implement their own AudioDeviceModule.
    
    The SetAudioRecording works in a similar fashion but for the recorded
    audio. One difference is that calling SetAudioRecording(false) does not
    keep any audio processing alive.
    
    Bug: webrtc:7313
    Change-Id: I0aa075f6bfef9818f1080f85a8ff7842fb0750aa

--- examples/androidapp/src/org/appspot/apprtc/PeerConnectionClient.java
+++ examples/androidapp/src/org/appspot/apprtc/PeerConnectionClient.java
@@ -622,6 +622,9 @@ public class PeerConnectionClient {
 
     peerConnection = factory.createPeerConnection(rtcConfig, pcConstraints, pcObserver);
 
+    // peerConnection.setAudioPlayout(false);
+    // peerConnection.setAudioRecording(false);
+
     if (dataChannelEnabled) {
       DataChannel.Init init = new DataChannel.Init();
       init.ordered = peerConnectionParameters.dataChannelParameters.ordered;
@@ -1344,6 +1347,12 @@ public class PeerConnectionClient {
               // and send local ICE candidates.
               Log.d(TAG, "Remote SDP set succesfully");
               drainCandidates();
+
+              // Log.d(TAG, "Enabling playout...");
+              // peerConnection.setAudioPlayout(true);
+
+              // Log.d(TAG, "Enabling recording...");
+              // peerConnection.setAudioRecording(true);
             }
           } else {
             // For answering peer connection we set remote SDP and then

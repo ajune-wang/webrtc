b9c22c5d556120ab9e882a4436eb88beda489491
Sergey Silkin <ssilkin@webrtc.org>
2017-11-09 18:01:13 +0100

    Get back spatial layers.
    SetupScreenshareOrSVC() is called only if screen sharing is enabled.
    This doesn't allow to setup regular SVC. I removed that condition.
    
    Bug:none
    Change-Id: I6320f9210889288f20f7e8e1deea8ad23d006e0a

--- video/video_quality_test.cc
+++ video/video_quality_test.cc
@@ -2070,8 +2070,7 @@ void VideoQualityTest::RunWithRenderers(const Params& params) {
           video_receive_configs_[selected_stream_id].sync_group = kSyncGroup;
       }
 
-      if (params_.screenshare.enabled)
-        SetupScreenshareOrSVC();
+      SetupScreenshareOrSVC();
 
       CreateFlexfecStreams();
       CreateVideoStreams();

975756cf4942bd1baaf0bc39eaa1f598fef8c02d
Sebastian Jansson <srte@webrtc.org>
2017-11-16 13:36:54 +0100

    Moved application limited region detector from pacer to congestion controller.
    This is part of an effort to refactor the congestion control to make it easier to implement other algorithms. The ALR detector is used primarily by the congestion control algorithm and doesn't have any essential relation to the pacer.
    
    A slight change in behavior is that packets are considered sent based on the OnPacketSent callback rather than when sent from PacedSender. This would mean that the AlrDetector considers RTP packets generated after the pacer as part of the application data.
    
    Bug: webrtc:8415
    Change-Id: I903d719d06962a8b49a8a5309d8e34882b68f9c8

--- modules/congestion_controller/probe_controller.h
+++ modules/congestion_controller/probe_controller.h
@@ -26,7 +26,7 @@ class Clock;
 // bitrate is adjusted by an application.
 class ProbeController {
  public:
-  ProbeController(PacedSender* pacer, const Clock* clock);
+  ProbeController(const Clock* clock, PacedSender* pacer);
 
   void SetBitrates(int64_t min_bitrate_bps,
                    int64_t start_bitrate_bps,
@@ -38,6 +38,7 @@ class ProbeController {
 
   void EnablePeriodicAlrProbing(bool enable);
 
+  void SetAlrStartTimeMs(rtc::Optional<int64_t> alr_start_time);
   void SetAlrEndedTimeMs(int64_t alr_end_time);
 
   void RequestProbe();
@@ -65,8 +66,8 @@ class ProbeController {
       RTC_EXCLUSIVE_LOCKS_REQUIRED(critsect_);
 
   rtc::CriticalSection critsect_;
-  PacedSender* const pacer_;
   const Clock* const clock_;
+  PacedSender* const pacer_;
   NetworkState network_state_ RTC_GUARDED_BY(critsect_);
   State state_ RTC_GUARDED_BY(critsect_);
   int64_t min_bitrate_to_probe_further_bps_ RTC_GUARDED_BY(critsect_);
@@ -75,6 +76,7 @@ class ProbeController {
   int64_t start_bitrate_bps_ RTC_GUARDED_BY(critsect_);
   int64_t max_bitrate_bps_ RTC_GUARDED_BY(critsect_);
   int64_t last_bwe_drop_probing_time_ms_ RTC_GUARDED_BY(critsect_);
+  rtc::Optional<int64_t> alr_start_time_ms_ RTC_GUARDED_BY(critsect_);
   rtc::Optional<int64_t> alr_end_time_ms_ RTC_GUARDED_BY(critsect_);
   bool enable_periodic_alr_probing_ RTC_GUARDED_BY(critsect_);
   int64_t time_of_last_large_drop_ms_ RTC_GUARDED_BY(critsect_);

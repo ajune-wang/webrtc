b7b506ea0582facd171d08dedbf3b652178a3f73
Steve Anton <steveanton@webrtc.org>
2017-12-01 09:23:42 -0800

    Modify SetLocalDescription helpers to work with RtpTransceivers
    This change allows EnableBundle and PushdownMediaDescription to
    work with RtpTransceivers, which means they can be reused in the
    Unified Plan version of SetLocalDescription.
    
    Bug: webrtc:8587
    Change-Id: I4d862556879c14cea06fdf9d5c7c29cc32e1057a

--- pc/peerconnection.h
+++ pc/peerconnection.h
@@ -380,6 +380,15 @@ class PeerConnection : public PeerConnectionInterface,
   RTCError ApplyRemoteDescription(
       std::unique_ptr<SessionDescriptionInterface> desc);
 
+  // Returns the media section in the given session description that is
+  // associated with the RtpTransceiver. Returns null if none found or this
+  // RtpTransceiver is not associated. Logic varies depending on the
+  // SdpSemantics specified in the configuration.
+  const cricket::ContentInfo* FindMediaSectionForTransceiver(
+      rtc::scoped_refptr<RtpTransceiverProxyWithInternal<RtpTransceiver>>
+          transceiver,
+      const SessionDescriptionInterface* sdesc) const;
+
   // Validates and takes ownership of the description, setting it as the current
   // or pending description (depending on the description's action) if it is
   // valid. Also updates ice role, candidates, creates and destroys channels.
@@ -644,7 +653,9 @@ class PeerConnection : public PeerConnectionInterface,
   bool EnableBundle(const cricket::ContentGroup& bundle);
 
   // Enables media channels to allow sending of media.
-  void EnableChannels();
+  // This enables media to flow on all configured audio/video channels and the
+  // RtpDataChannel.
+  void EnableMedia();
   // Returns the media index for a local ice candidate given the content name.
   // Returns false if the local session description does not have a media
   // content called  |content_name|.

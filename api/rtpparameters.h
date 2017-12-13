6d21a4547e53064d3567be7505133f6c08a03196
Seth Hampson <shampson@webrtc.org>
2017-12-13 10:07:53 -0800

    Updates to video config to allow changes in google3 tests, in order to not break anything.
    Bug:webrtc:8630
    Change-Id: I71bfd3f01344c80a83b728385b9231b47ee1fd5d

--- api/rtpparameters.h
+++ api/rtpparameters.h
@@ -72,6 +72,7 @@ enum class DegradationPreference {
   BALANCED,
 };
 
+const double kDefaultBitratePriority = 1.0;
 enum class PriorityType { VERY_LOW, LOW, MEDIUM, HIGH };
 
 struct RtcpFeedback {

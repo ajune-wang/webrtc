dc1a76262ca2f0af6c07117b569109d1f4008953
srte <srte@webrtc.org>
2017-12-14 21:01:00 +0100

    Moved Congestion control and PacedSender to task queues.
    This also prepares for the posibility to replace the congestion control
    algorithm with new implementations. Since many of the changes only
    make sense in the context of moving to task queues and would be
    tricky to implement in the previous lock based implementation.
    
    Bug: webrtc:8415
    Change-Id: Id3e26be530e4897a30c3093ee76641023ac70f9f

---
+++ network_control/network_types.cc
@@ -0,0 +1,70 @@
+/*
+ *  Copyright (c) 2017 The WebRTC project authors. All Rights Reserved.
+ *
+ *  Use of this source code is governed by a BSD-style license
+ *  that can be found in the LICENSE file in the root of the source
+ *  tree. An additional intellectual property rights grant can be found
+ *  in the file PATENTS.  All contributing project authors may
+ *  be found in the AUTHORS file in the root of the source tree.
+ */
+
+#include "network_control/include/network_types.h"
+
+namespace webrtc {
+namespace network {
+
+template struct signal::Message<CongestionWindow>;
+template struct signal::Message<Invalidation>;
+template struct signal::Message<NetworkAvailability>;
+template struct signal::Message<NetworkEstimate>;
+template struct signal::Message<NetworkRouteChange>;
+template struct signal::Message<PacerConfig>;
+template struct signal::Message<ProbeClusterConfig>;
+template struct signal::Message<ProcessInterval>;
+template struct signal::Message<RemoteBitrateReport>;
+template struct signal::Message<RoundTripTimeReport>;
+template struct signal::Message<SentPacket>;
+template struct signal::Message<StreamsConfig>;
+template struct signal::Message<TargetRateConstraints>;
+template struct signal::Message<TargetTransferRate>;
+template struct signal::Message<TransportLossReport>;
+template struct signal::Message<TransportPacketsFeedback>;
+
+::std::ostream& operator<<(::std::ostream& os,
+                           const ProbeClusterConfig& config) {
+  return os << "ProbeClusterConfig(...)";
+}
+
+::std::ostream& operator<<(::std::ostream& os, const PacerConfig& config) {
+  return os << "PacerConfig(...)";
+}
+
+NetworkPacketFeedback::NetworkPacketFeedback() {}
+
+NetworkPacketFeedback::NetworkPacketFeedback(
+    const NetworkPacketFeedback& other) {
+  sent_packet = other.sent_packet;
+  receive_time = other.receive_time;
+}
+
+NetworkPacketFeedback::~NetworkPacketFeedback() {}
+
+TransportPacketsFeedback::TransportPacketsFeedback() {}
+TransportPacketsFeedback::TransportPacketsFeedback(
+    const TransportPacketsFeedback& other)
+    : feedback_time(other.feedback_time),
+      packet_feedbacks(other.packet_feedbacks) {}
+
+TransportPacketsFeedback::~TransportPacketsFeedback() {}
+
+uint8_t NetworkEstimate::GetLossRatioUint8() {
+  int loss_ratio_255 = loss_rate_ratio * 255;
+  if (loss_ratio_255 < 0)
+    return 0;
+  else if (loss_ratio_255 > 255)
+    return 255;
+  else
+    return (uint8_t)loss_ratio_255;
+}
+}  // namespace network
+}  // namespace webrtc

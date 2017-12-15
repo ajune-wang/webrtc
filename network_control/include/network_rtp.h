9eb6ca4696e884abcd649e16e88ba982bfefee83
srte <srte@webrtc.org>
2017-12-15 17:47:51 +0100

    Moved Congestion control and PacedSender to task queues.
    This also prepares for the posibility to replace the congestion control
    algorithm with new implementations. Since many of the changes only
    make sense in the context of moving to task queues and would be
    tricky to implement in the previous lock based implementation.
    
    Bug: webrtc:8415
    Change-Id: Id3e26be530e4897a30c3093ee76641023ac70f9f

---
+++ network_control/include/network_rtp.h
@@ -0,0 +1,34 @@
+/*
+ *  Copyright (c) 2017 The WebRTC project authors. All Rights Reserved.
+ *
+ *  Use of this source code is governed by a BSD-style license
+ *  that can be found in the LICENSE file in the root of the source
+ *  tree. An additional intellectual property rights grant can be found
+ *  in the file PATENTS.  All contributing project authors may
+ *  be found in the AUTHORS file in the root of the source tree.
+ */
+
+#ifndef NETWORK_CONTROL_INCLUDE_NETWORK_RTP_H_
+#define NETWORK_CONTROL_INCLUDE_NETWORK_RTP_H_
+#include <stdint.h>
+#include <vector>
+#include "modules/rtp_rtcp/include/rtp_rtcp_defines.h"
+#include "network_control/include/network_types.h"
+
+namespace webrtc {
+namespace network {
+
+NetworkPacketFeedback NetworkPacketFeedbackFromRtpPacketFeedback(
+    const webrtc::PacketFeedback&);
+
+TransportPacketsFeedback TransportPacketsFeedbackFromRtpFeedbackVector(
+    const std::vector<PacketFeedback>&,
+    int64_t creation_time_ms);
+
+TransportPacketsFeedback TransportPacketsFeedbackFromRtpFeedbackVector(
+    const std::vector<PacketFeedback>&);
+
+}  // namespace network
+}  // namespace webrtc
+
+#endif  // NETWORK_CONTROL_INCLUDE_NETWORK_RTP_H_

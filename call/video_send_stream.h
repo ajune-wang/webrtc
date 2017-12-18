7a5ca66ee9b79ed303fd76c3526b4715dac71368
Patrik Höglund <phoglund@webrtc.org>
2017-12-18 12:27:52 +0100

    Move videosinkinterface.h to common_video to solve a circular dep.
    I updated some dependency enforcement rules to allow examples and pc
    to depend on common_video. I reckoned depending on common_video is
    not controversial when they already dependend on media/base, which
    is a lower-level abstraction.
    
    Bug:webrtc:6828
    Change-Id: I77dbeb10187b4e70dda1d873a29994fa76070758

--- call/video_send_stream.h
+++ call/video_send_stream.h
@@ -23,7 +23,7 @@
 #include "call/video_config.h"
 #include "common_types.h"  // NOLINT(build/include)
 #include "common_video/include/frame_callback.h"
-#include "media/base/videosinkinterface.h"
+#include "common_video/include/videosinkinterface.h"
 #include "media/base/videosourceinterface.h"
 #include "modules/rtp_rtcp/include/rtp_rtcp_defines.h"
 #include "rtc_base/platform_file.h"

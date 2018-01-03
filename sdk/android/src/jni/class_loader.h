02d17842651869cf6ba88ab03752967beabfc2c0
Magnus Jedvert <magjed@webrtc.org>
2018-01-03 12:48:02 +0100

    Android: Move JNI helper functions to rtc_base
    This CL moves the common JNI helper files to rtc_base so that it can be
    re-used for the audio code. The goal is to use the JNI generation script
    for the audio code. When the Android audio code is moved out from core
    WebRTC, we should move out the JNI helper files as well.
    
    Bug: webrtc:8689
    Change-Id: I0beffabebc8c79c7d04b5c92e3b938292d41d6f5

--- sdk/android/src/jni/class_loader.h
+++ sdk/android/src/jni/class_loader.h
@@ -1,5 +1,5 @@
 /*
- *  Copyright 2017 The WebRTC project authors. All Rights Reserved.
+ *  Copyright 2018 The WebRTC Project Authors. All rights reserved.
  *
  *  Use of this source code is governed by a BSD-style license
  *  that can be found in the LICENSE file in the root of the source
@@ -8,35 +8,11 @@
  *  be found in the AUTHORS file in the root of the source tree.
  */
 
-// Android's FindClass() is tricky because the app-specific ClassLoader is not
-// consulted when there is no app-specific frame on the stack (i.e. when called
-// from a thread created from native C++ code). These helper functions provide a
-// workaround for this.
-// http://developer.android.com/training/articles/perf-jni.html#faq_FindClass
+// TODO(magjed): Update clients to include new path and and remove this file.
 
 #ifndef SDK_ANDROID_SRC_JNI_CLASS_LOADER_H_
 #define SDK_ANDROID_SRC_JNI_CLASS_LOADER_H_
 
-#include <jni.h>
-
-#include "sdk/android/src/jni/scoped_java_ref.h"
-
-namespace webrtc {
-namespace jni {
-
-// This method should be called from JNI_OnLoad and before any calls to
-// FindClass.
-void InitClassLoader(JNIEnv* env);
-
-// This function is identical to JNIEnv::FindClass except that it works from any
-// thread. This function loads and returns a local reference to the class with
-// the given name. The name argument is a fully-qualified class name. For
-// example, the fully-qualified class name for the java.lang.String class is:
-// "java/lang/String". This function will be used from the JNI generated code
-// and should rarely be used manually.
-ScopedJavaLocalRef<jclass> GetClass(JNIEnv* env, const char* name);
-
-}  // namespace jni
-}  // namespace webrtc
+#include "rtc_base/jni/class_loader.h"
 
 #endif  // SDK_ANDROID_SRC_JNI_CLASS_LOADER_H_

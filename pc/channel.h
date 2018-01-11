e8fea395f8d43745623da9075dd7d147eb38e3fa
Niels Möller <nisse@webrtc.org>
2018-01-11 12:41:49 +0100

    Delete unused signal VoiceChannel::SignalAudioMonitor.
    Bug: None
    Change-Id: I8353f7c2cf4dbb81dad7fb21ed7e934662b2ad4f

--- pc/channel.h
+++ pc/channel.h
@@ -508,7 +508,6 @@ class VoiceChannel : public BaseChannel {
   void StartAudioMonitor(int cms);
   void StopAudioMonitor();
   bool IsAudioMonitorRunning() const;
-  sigslot::signal2<VoiceChannel*, const AudioInfo&> SignalAudioMonitor;
 
   int GetInputLevel_w();
   int GetOutputLevel_w();
@@ -538,7 +537,6 @@ class VoiceChannel : public BaseChannel {
       const std::vector<ConnectionInfo>& infos) override;
   void OnMediaMonitorUpdate(VoiceMediaChannel* media_channel,
                             const VoiceMediaInfo& info);
-  void OnAudioMonitorUpdate(AudioMonitor* monitor, const AudioInfo& info);
 
   static const int kEarlyMediaTimeout = 1000;
   MediaEngineInterface* media_engine_;

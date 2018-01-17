aaff4e722de130097dee6d26d20d7f05a5403bca
Qiang Chen <qiangchen@chromium.org>
2018-01-16 16:11:35 -0800

    Wrap Alpha and YUV frame into one EncodedImage for transmission
    Bug:
    Change-Id: Ibe746a46cb41fd92b399a7069e1d89f02f292af7

--- modules/video_coding/codecs/stereo/include/stereo_encoder_adapter.h
+++ modules/video_coding/codecs/stereo/include/stereo_encoder_adapter.h
@@ -18,7 +18,9 @@
 #include "api/video_codecs/sdp_video_format.h"
 #include "api/video_codecs/video_encoder.h"
 #include "api/video_codecs/video_encoder_factory.h"
+#include "modules/video_coding/codecs/stereo/include/multiplex_encoded_image_packer.h"
 #include "modules/video_coding/include/video_codec_interface.h"
+#include "system_wrappers/include/clock.h"
 
 namespace webrtc {
 
@@ -65,12 +67,14 @@ class StereoEncoderAdapter : public VideoEncoder {
   std::vector<std::unique_ptr<AdapterEncodedImageCallback>> adapter_callbacks_;
   EncodedImageCallback* encoded_complete_callback_;
 
-  // Holds the encoded image info.
-  struct ImageStereoInfo;
-  std::map<uint32_t /* timestamp */, ImageStereoInfo> image_stereo_info_;
+  std::map<uint32_t /* timestamp */, MultiplexImage> stashed_images_;
 
   uint16_t picture_index_ = 0;
   std::vector<uint8_t> stereo_dummy_planes_;
+
+  Clock* clock_;
+  int key_frame_interval_;
+  int64_t last_key_frame_ms_;
 };
 
 }  // namespace webrtc

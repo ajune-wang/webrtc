e2783723629f1de908a7ccba9aabac82c4195048
Sebastian Jansson <srte@webrtc.org>
2018-01-26 10:20:42 +0100

    Moved congestion controller to task queue.
    This is the first part of a split of a larger CL, see:
    https://webrtc-review.googlesource.com/c/src/+/39788/8
    For further explanations.
    
    Bug: webrtc:8415
    Change-Id: I770189c04cc31b313bd4e57821acff55fbcb1ad3

---
+++ modules/congestion_controller/pacer_controller.h
@@ -0,0 +1,49 @@
+/*
+ *  Copyright (c) 2018 The WebRTC project authors. All Rights Reserved.
+ *
+ *  Use of this source code is governed by a BSD-style license
+ *  that can be found in the LICENSE file in the root of the source
+ *  tree. An additional intellectual property rights grant can be found
+ *  in the file PATENTS.  All contributing project authors may
+ *  be found in the AUTHORS file in the root of the source tree.
+ */
+
+#ifndef MODULES_CONGESTION_CONTROLLER_PACER_CONTROLLER_H_
+#define MODULES_CONGESTION_CONTROLLER_PACER_CONTROLLER_H_
+
+#include <atomic>
+#include <memory>
+
+#include "modules/pacing/paced_sender.h"
+#include "network_control/include/network_types.h"
+#include "rtc_base/criticalsection.h"
+#include "rtc_base/task_queue.h"
+
+namespace webrtc {
+class Clock;
+
+// Wrapper class to control pacer using task queues
+class PacerController {
+ public:
+  explicit PacerController(PacedSender* pacer);
+  ~PacerController();
+  bool GetPacerConfigured();
+  void OnCongestionWindow(CongestionWindow);
+  void OnNetworkAvailability(NetworkAvailability);
+  void OnNetworkRouteChange(NetworkRouteChange);
+  void OnOutstandingData(OutstandingData);
+  void OnPacerConfig(PacerConfig);
+  void OnProbeClusterConfig(ProbeClusterConfig);
+
+ private:
+  void SetPacerState(bool paused);
+  PacedSender* const pacer_;
+
+  rtc::Optional<PacerConfig> current_pacer_config_;
+  bool pacer_paused_ = false;
+  bool network_available_ = true;
+  std::atomic<bool> pacer_configured_;
+  RTC_DISALLOW_IMPLICIT_CONSTRUCTORS(PacerController);
+};
+}  // namespace webrtc
+#endif  // MODULES_CONGESTION_CONTROLLER_PACER_CONTROLLER_H_

512197759503a60179925120cd53e942f5bd3661
Sebastian Jansson <srte@webrtc.org>
2018-01-26 19:25:40 +0100

    Moved congestion controller to task queue.
    The goal of this work is to make it easier to experiment with the
    bandwidth estimation implementation. For this reason network control
    functionality is moved from SendSideCongestionController(SSCC),
    PacedSender and BitrateController to the newly created
    GoogCcNetworkController which implements the newly created
    NetworkControllerInterface. This allows the implementation to be
    replaced at runtime in the future.
    
    This is the first part of a split of a larger CL, see:
    https://webrtc-review.googlesource.com/c/src/+/39788/8
    For further explanations.
    
    Bug: webrtc:8415
    Change-Id: I770189c04cc31b313bd4e57821acff55fbcb1ad3

---
+++ modules/congestion_controller/goog_cc_network_control.h
@@ -0,0 +1,100 @@
+/*
+ *  Copyright (c) 2018 The WebRTC project authors. All Rights Reserved.
+ *
+ *  Use of this source code is governed by a BSD-style license
+ *  that can be found in the LICENSE file in the root of the source
+ *  tree. An additional intellectual property rights grant can be found
+ *  in the file PATENTS.  All contributing project authors may
+ *  be found in the AUTHORS file in the root of the source tree.
+ */
+
+#ifndef MODULES_CONGESTION_CONTROLLER_GOOG_CC_NETWORK_CONTROL_H_
+#define MODULES_CONGESTION_CONTROLLER_GOOG_CC_NETWORK_CONTROL_H_
+
+#include <stdint.h>
+#include <deque>
+#include <memory>
+#include <vector>
+
+#include "modules/bitrate_controller/send_side_bandwidth_estimation.h"
+#include "modules/congestion_controller/acknowledged_bitrate_estimator.h"
+#include "modules/congestion_controller/alr_detector.h"
+#include "modules/congestion_controller/delay_based_bwe.h"
+#include "modules/congestion_controller/probe_controller.h"
+
+#include "api/optional.h"
+#include "logging/rtc_event_log/rtc_event_log.h"
+#include "network_control/include/network_control.h"
+#include "rtc_base/constructormagic.h"
+
+namespace webrtc {
+
+class GoogCcNetworkController : public NetworkControllerInterface {
+ public:
+  GoogCcNetworkController(RtcEventLog* event_log,
+                          NetworkControllerObserver* observer);
+  ~GoogCcNetworkController() override;
+  TimeDelta GetProcessInterval() override;
+
+  // NetworkControllerInterface
+  void OnNetworkAvailability(NetworkAvailability) override;
+  void OnNetworkRouteChange(NetworkRouteChange) override;
+  void OnProcessInterval(ProcessInterval) override;
+  void OnRemoteBitrateReport(RemoteBitrateReport) override;
+  void OnRoundTripTimeReport(RoundTripTimeReport) override;
+  void OnSentPacket(SentPacket) override;
+  void OnStreamsConfig(StreamsConfig) override;
+  void OnTargetRateConstraints(TargetRateConstraints) override;
+  void OnTransportLossReport(TransportLossReport) override;
+  void OnTransportPacketsFeedback(TransportPacketsFeedback) override;
+
+ private:
+  void MaybeUpdateCongestionWindow();
+
+  void MaybeTriggerOnNetworkChanged(Timestamp at_time);
+  void OnNetworkEstimate(NetworkEstimate);
+  void UpdatePacingRates(Timestamp at_time);
+  bool HasNetworkParametersToReportChanged(uint32_t bitrate_bps,
+                                           uint8_t fraction_loss,
+                                           int64_t rtt);
+  bool GetNetworkParameters(int32_t* estimated_bitrate_bps,
+                            uint8_t* fraction_loss,
+                            int64_t* rtt_ms,
+                            Timestamp at_time);
+
+  RtcEventLog* const event_log_;
+  NetworkControllerObserver* observer_;
+
+  const std::unique_ptr<ProbeController> probe_controller_;
+
+  std::unique_ptr<SendSideBandwidthEstimation> bandwidth_estimation_;
+  std::unique_ptr<AlrDetector> alr_detector_;
+  std::unique_ptr<DelayBasedBwe> delay_based_bwe_;
+  std::unique_ptr<AcknowledgedBitrateEstimator> acknowledged_bitrate_estimator_;
+
+  std::deque<int64_t> feedback_rtts_;
+  rtc::Optional<int64_t> min_feedback_rtt_ms_;
+
+  rtc::Optional<NetworkEstimate> last_estimate_;
+  rtc::Optional<TargetTransferRate> last_target_rate_;
+
+  int32_t last_estimated_bitrate_bps_ = 0;
+  uint8_t last_estimated_fraction_loss_ = 0;
+  int64_t last_estimated_rtt_ms_ = 0;
+
+  uint32_t last_reported_target_bitrate_bps_ = 0;
+  uint8_t last_reported_fraction_loss_ = 0;
+  int64_t last_reported_rtt_ms_ = 0;
+
+  StreamsConfig streams_config_;
+
+  bool in_cwnd_experiment_;
+  int64_t accepted_queue_ms_;
+  bool previously_in_alr = false;
+
+  RTC_DISALLOW_IMPLICIT_CONSTRUCTORS(GoogCcNetworkController);
+};
+
+}  // namespace webrtc
+
+#endif  // MODULES_CONGESTION_CONTROLLER_GOOG_CC_NETWORK_CONTROL_H_

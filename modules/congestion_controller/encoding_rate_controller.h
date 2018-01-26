d4c1a6aff4f6cda24a14a8275b7aa6f0b2950fcb
Sebastian Jansson <srte@webrtc.org>
2018-01-26 18:46:31 +0100

    Moved congestion controller to task queue.
    The goal of this work is to make it easier to experiment with the
    bandwidth estimation implementation. For this reason network control
    functionality is moved from SendSideCongestionController(SSCC),
    PacedSender and BitrateController to the newly created
    GoogCcNetworkController which implements the newly created
    NetworkControllerInterface. This allows the implementation to be
    replaced at runtime in the future.
    
    This is the first part of a split of a larger CL, see:
    https://webrtc-review.googlesource.com/c/src/+/39788/8
    For further explanations.
    
    Bug: webrtc:8415
    Change-Id: I770189c04cc31b313bd4e57821acff55fbcb1ad3

---
+++ modules/congestion_controller/encoding_rate_controller.h
@@ -0,0 +1,65 @@
+/*
+ *  Copyright (c) 2018 The WebRTC project authors. All Rights Reserved.
+ *
+ *  Use of this source code is governed by a BSD-style license
+ *  that can be found in the LICENSE file in the root of the source
+ *  tree. An additional intellectual property rights grant can be found
+ *  in the file PATENTS.  All contributing project authors may
+ *  be found in the AUTHORS file in the root of the source tree.
+ */
+
+#ifndef MODULES_CONGESTION_CONTROLLER_ENCODING_RATE_CONTROLLER_H_
+#define MODULES_CONGESTION_CONTROLLER_ENCODING_RATE_CONTROLLER_H_
+#include <memory>
+
+#include "modules/congestion_controller/include/send_side_congestion_controller.h"
+#include "network_control/include/network_types.h"
+#include "rtc_base/criticalsection.h"
+#include "rtc_base/rate_limiter.h"
+#include "rtc_base/task_queue.h"
+
+namespace webrtc {
+class Clock;
+
+// This class converts network messages to legacy API
+class EncodingRateController {
+ public:
+  explicit EncodingRateController(const Clock* clock);
+  ~EncodingRateController();
+  void RegisterNetworkObserver(
+      SendSideCongestionController::Observer* observer);
+  void DeRegisterNetworkObserver(
+      SendSideCongestionController::Observer* observer);
+  RateLimiter* GetRetransmissionRateLimiter();
+  void OnNetworkAvailability(NetworkAvailability msg);
+  void OnTargetTransferRate(TargetTransferRate msg);
+  void OnPacerQueueUpdate(PacerQueueUpdate msg);
+
+ private:
+  void OnNetworkInvalidation();
+
+  bool GetNetworkParameters(int32_t* estimated_bitrate_bps,
+                            uint8_t* fraction_loss,
+                            int64_t* rtt_ms);
+  bool IsSendQueueFull() const;
+  bool HasNetworkParametersToReportChanged(int64_t bitrate_bps,
+                                           uint8_t fraction_loss,
+                                           int64_t rtt);
+  rtc::CriticalSection observer_lock_;
+  SendSideCongestionController::Observer* observer_
+      RTC_GUARDED_BY(observer_lock_) = nullptr;
+  const std::unique_ptr<RateLimiter> retransmission_rate_limiter_;
+
+  rtc::Optional<TargetTransferRate> current_target_rate_msg_;
+
+  bool network_available_ = true;
+  int64_t last_reported_target_bitrate_bps_ = 0;
+  uint8_t last_reported_fraction_loss_ = 0;
+  int64_t last_reported_rtt_ms_ = 0;
+  bool pacer_pushback_experiment_ = false;
+  int64_t pacer_expected_queue_ms_ = 0;
+  float encoding_rate_ratio_ = 1.0;
+  RTC_DISALLOW_IMPLICIT_CONSTRUCTORS(EncodingRateController);
+};
+}  // namespace webrtc
+#endif  // MODULES_CONGESTION_CONTROLLER_ENCODING_RATE_CONTROLLER_H_
--- modules/congestion_controller/transport_feedback_adapter.cc
+++ modules/congestion_controller/transport_feedback_adapter.cc
@@ -82,6 +82,12 @@ void TransportFeedbackAdapter::OnSentPacket(uint16_t sequence_number,
   send_time_history_.OnSentPacket(sequence_number, send_time_ms);
 }
 
+rtc::Optional<PacketFeedback> TransportFeedbackAdapter::GetPacket(
+    uint16_t sequence_number) const {
+  rtc::CritScope cs(&lock_);
+  return send_time_history_.GetPacket(sequence_number);
+}
+
 void TransportFeedbackAdapter::SetNetworkIds(uint16_t local_id,
                                              uint16_t remote_id) {
   rtc::CritScope cs(&lock_);

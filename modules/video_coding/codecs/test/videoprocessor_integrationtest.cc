f8c723ea2478d4f96cc95550d432104a292e25c9
Rasmus Brandt <brandtr@webrtc.org>
2018-02-05 09:52:11 +0100

    Delete dead code for video quality calculation.
    Previously, the only user of this code was the
    VideoProcessorIntegrationTest. We have now changed that
    test to directly calculate image quality metrics using libyuv,
    similar to how the full stack tests and browser tests work.
    
    Bug: webrtc:8448
    Change-Id: Ia7a607d7ddc37741fba76d56aa7297851ffa1c6b

--- modules/video_coding/codecs/test/videoprocessor_integrationtest.cc
+++ modules/video_coding/codecs/test/videoprocessor_integrationtest.cc
@@ -40,7 +40,6 @@
 #include "system_wrappers/include/sleep.h"
 #include "test/statistics.h"
 #include "test/testsupport/fileutils.h"
-#include "test/testsupport/metrics/video_metrics.h"
 
 namespace webrtc {
 namespace test {

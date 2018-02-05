26acbd014bbc211eaf954bdacdf38ca6dcd79e99
Jonas Oreland <jonaso@webrtc.org>
2018-02-05 09:11:28 +0100

    Handle lifetime short than 2 minutes for TURN allocations
    This patch modifies behaviour when TurnPort gets a lifetime
    back from server that is shorter than 2 minutes.
    
    Before the patch such lifetime resulted in TurnPort not scheduling any
    refresh, leading to timeout on the turn allocation.
    
    After then patch lifetime shorter then 2 minutes leads to refresh
    after half stipulated lifetime.
    
    BUG=webrtc:8826
    
    Change-Id: I80561100f2307bd9a6a91af0924bb2814102ddd3

--- p2p/base/turnport.cc
+++ p2p/base/turnport.cc
@@ -976,15 +976,28 @@ void TurnPort::DispatchPacket(const char* data, size_t size,
   }
 }
 
-bool TurnPort::ScheduleRefresh(int lifetime) {
-  // Lifetime is in seconds; we schedule a refresh for one minute less.
+bool TurnPort::ScheduleRefresh(uint32_t lifetime) {
+  // Lifetime is in seconds, delay is in milliseconds.
+  int delay = 1 * 60 * 1000;
+
   if (lifetime < 2 * 60) {
-    LOG_J(LS_WARNING, this) << "Received response with lifetime that was "
-                            << "too short, lifetime=" << lifetime;
-    return false;
+    // The RFC does not mention a lower limit on lifetime.
+    // So if server sends a value less than 2 minutes, we schedule a refresh
+    // for half lifetime.
+    delay = (lifetime * 1000) / 2;
+    LOG_J(LS_WARNING, this) << "Received response with short lifetime="
+                            << lifetime << " seconds.";
+  } else if (lifetime > 60*60) {
+    // Make 1 hour largest delay.
+    LOG_J(LS_WARNING, this) << "Received response with long lifetime="
+                            << lifetime << " seconds.";
+    delay = 60*60 * 1000;
+  } else {
+    // Normal case,
+    // we schedule a refresh for one minute less than requested lifetime.
+    delay = (lifetime - 60) * 1000;
   }
 
-  int delay = (lifetime - 60) * 1000;
   SendRequest(new TurnRefreshRequest(this), delay);
   LOG_J(LS_INFO, this) << "Scheduled refresh in " << delay << "ms.";
   return true;

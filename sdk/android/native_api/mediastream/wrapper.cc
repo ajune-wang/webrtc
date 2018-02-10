a7a3b919564d5e79fc1be8dda46dbafbe9b6a801
Jonathan Yu <yujo@chromium.org>
2018-02-09 18:04:16 -0800

    Add wrapper for VideoSink to Android native_api.
    Bug: webrtc:8769
    Change-Id: If944b2a52a86666bebf094ec0e3c74c076d6c3d2

---
+++ sdk/android/native_api/mediastream/wrapper.cc
@@ -0,0 +1,25 @@
+/*
+ *  Copyright 2018 The WebRTC project authors. All Rights Reserved.
+ *
+ *  Use of this source code is governed by a BSD-style license
+ *  that can be found in the LICENSE file in the root of the source
+ *  tree. An additional intellectual property rights grant can be found
+ *  in the file PATENTS.  All contributing project authors may
+ *  be found in the AUTHORS file in the root of the source tree.
+ */
+
+#include "sdk/android/native_api/mediastream/wrapper.h"
+
+#include "rtc_base/ptr_util.h"
+#include "sdk/android/src/jni/videosink.h"
+
+namespace webrtc {
+
+std::unique_ptr<rtc::VideoSinkInterface<VideoFrame>> JavaToNativeVideoSink(
+    JNIEnv* jni,
+    jobject video_sink) {
+  return rtc::MakeUnique<jni::VideoSinkWrapper>(
+      jni, JavaParamRef<jobject>(video_sink));
+}
+
+}  // namespace webrtc

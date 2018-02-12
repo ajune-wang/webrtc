5f0260743183b62e1a1aead6ea3b7f7a3db89d7b
Sebastian Jansson <srte@webrtc.org>
2018-02-12 20:37:17 +0100

    Added congestion control functionality to pacer.
    This adds the ability to the pacer to apply a congestion window by
    tracking sent data. This makes it more reliable when the congestion
    window is small enough to be filled at a high rate as there are less
    thread context switches that might affect the timing and performance.
    
    Outstanding data is not reduced by the pacer as it has no information
    about acknowledged packet feedback. This is by design as the pacer would
    also need to keep track of on which connection packets were sent or
    received, requiring a larger, more complex, change to the pacer.
    
    Bug: webrtc:8415
    Change-Id: I4ecd303e835552ced042cd21186da910288a8258

--- modules/pacing/paced_sender.h
+++ modules/pacing/paced_sender.h
@@ -48,6 +48,7 @@ class PacedSender : public Pacer {
    protected:
     virtual ~PacketSender() {}
   };
+  static constexpr int32_t kNoCongestionWindow = -1;
 
   // Expected max pacer delay in ms. If ExpectedQueueTimeMs() is higher than
   // this value, the packet producers should wait (eg drop frames rather than
@@ -73,6 +74,9 @@ class PacedSender : public Pacer {
 
   // Resume sending packets.
   void Resume();
+
+  void SetCongestionWindow(int64_t congestion_window_bytes);
+  void UpdateOutstandingData(int64_t outstanding_bytes);
 
   // Enable bitrate probing. Enabled by default, mostly here to simplify
   // testing. Must be called before any packets are being sent to have an
@@ -133,6 +137,9 @@ class PacedSender : public Pacer {
   size_t SendPadding(size_t padding_needed, const PacedPacketInfo& cluster_info)
       RTC_EXCLUSIVE_LOCKS_REQUIRED(critsect_);
 
+  void OnBytesSent(size_t bytes_sent) RTC_EXCLUSIVE_LOCKS_REQUIRED(critsect_);
+  bool Congested() const RTC_EXCLUSIVE_LOCKS_REQUIRED(critsect_);
+
   const Clock* const clock_;
   PacketSender* const packet_sender_;
 
@@ -161,6 +168,10 @@ class PacedSender : public Pacer {
   const std::unique_ptr<PacketQueueInterface> packets_
       RTC_PT_GUARDED_BY(critsect_);
   uint64_t packet_counter_ RTC_GUARDED_BY(critsect_);
+
+  int64_t congestion_window_bytes_ RTC_GUARDED_BY(critsect_) =
+      kNoCongestionWindow;
+  int64_t outstanding_bytes_ RTC_GUARDED_BY(critsect_) = 0;
 
   int64_t queue_time_limit RTC_GUARDED_BY(critsect_);
   bool account_for_audio_ RTC_GUARDED_BY(critsect_);

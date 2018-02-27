f060e42c2d4930d39221c48539aa458963f90491
Alessio Bazzica <alessiob@webrtc.org>
2018-02-27 10:02:05 +0100

    === PRIVATE CL! === === DO NOT LAND ===
    AGC2 RNN based VAD
    
    # Compile unit tests
    $ ninja -C out/Default -j1000 common_audio:common_audio_unittests  common_audio/rnn_vad:rnn_vad_tool
    
    # Run unit tests
    $ cd out/Default
    $ ./common_audio_unittests --gtest_filter=RnnVad.*
    
    Change-Id: I98ded85ed4e0afc2176308f1ea929cf750477c29

---
+++ common_audio/rnn_vad/lp_residual_unittest.cc
@@ -0,0 +1,36 @@
+/*
+ *  Copyright (c) 2018 The WebRTC project authors. All Rights Reserved.
+ *
+ *  Use of this source code is governed by a BSD-style license
+ *  that can be found in the LICENSE file in the root of the source
+ *  tree. An additional intellectual property rights grant can be found
+ *  in the file PATENTS.  All contributing project authors may
+ *  be found in the AUTHORS file in the root of the source tree.
+ */
+
+#include "common_audio/rnn_vad/lp_residual.h"
+
+#include "test/gtest.h"
+
+namespace webrtc {
+namespace test {
+
+using rnn_vad::ComputeInverseFilterCoefficients;
+using rnn_vad::ComputeLpResidual;
+
+namespace {
+
+// TODO(alessiob): Remove if unused.
+
+}  // namespace
+
+TEST(RnnVad, ComputeInverseFilterCoefficientsBitExactness) {
+  // TODO(alessiob): Implement.
+}
+
+TEST(RnnVad, ComputeLpResidualBitExactness) {
+  // TODO(alessiob): Implement.
+}
+
+}  // namespace test
+}  // namespace webrtc
---
+++ common_audio/rnn_vad/pitch_search_unittest.cc
@@ -0,0 +1,31 @@
+/*
+ *  Copyright (c) 2018 The WebRTC project authors. All Rights Reserved.
+ *
+ *  Use of this source code is governed by a BSD-style license
+ *  that can be found in the LICENSE file in the root of the source
+ *  tree. An additional intellectual property rights grant can be found
+ *  in the file PATENTS.  All contributing project authors may
+ *  be found in the AUTHORS file in the root of the source tree.
+ */
+
+#include "common_audio/rnn_vad/pitch_search.h"
+
+#include "test/gtest.h"
+
+namespace webrtc {
+namespace test {
+
+using rnn_vad::CoarsePitchSearch;
+
+namespace {
+
+// TODO(alessiob): Remove if unused.
+
+}  // namespace
+
+TEST(RnnVad, CoarsePitchSearchBitExactness) {
+  // TODO(alessiob): Implement.
+}
+
+}  // namespace test
+}  // namespace webrtc

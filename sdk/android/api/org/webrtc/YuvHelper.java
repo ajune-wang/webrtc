f14f16ad06ff4d0a409984f60817a945f38284a3
Qiang Chen <qiangchen@chromium.org>
2018-02-27 15:41:24 -0800

    Add JavaI420ABuffer
    This CL is part of the project supporting alpha channel in webrtc.
    Specifically this CL implements JavaI420ABuffer, which is essentially
    an Android implementation of the new I420ABufferInterface [1]. So the
    Android app can pass video frame with alpha channel to WebRTC using
    container JavaI420ABuffer.
    
    [1] https://cs.chromium.org/chromium/src/third_party/webrtc/api/video/video_frame_buffer.h?q=I420ABufferInterface&l=115&dr=CSs
    
    Bug:webrtc:7671
    Change-Id: I0520da0679aece8eab7cdd8a8a150dec34b4a18e

--- sdk/android/api/org/webrtc/YuvHelper.java
+++ sdk/android/api/org/webrtc/YuvHelper.java
@@ -119,6 +119,14 @@ public class YuvHelper {
         dstStrideU, dstV, dstStrideV, srcWidth, srcHeight, rotationMode);
   }
 
+  public static ByteBuffer cropAndScalePlane(ByteBuffer src, int srcStride, int cropX, int cropY,
+      int cropWidth, int cropHeight, int dstStride, int scaleWidth, int scaleHeight) {
+    ByteBuffer dst = JniCommon.nativeAllocateByteBuffer(dstStride * scaleHeight);
+    nativeCropAndScalePlane(src, srcStride, cropX, cropY, cropWidth, cropHeight, dst, dstStride,
+        scaleWidth, scaleHeight);
+    return dst;
+  }
+
   private static native void nativeI420Copy(ByteBuffer srcY, int srcStrideY, ByteBuffer srcU,
       int srcStrideU, ByteBuffer srcV, int srcStrideV, ByteBuffer dstY, int dstStrideY,
       ByteBuffer dstU, int dstStrideU, ByteBuffer dstV, int dstStrideV, int width, int height);
@@ -129,4 +137,7 @@ public class YuvHelper {
       int srcStrideU, ByteBuffer srcV, int srcStrideV, ByteBuffer dstY, int dstStrideY,
       ByteBuffer dstU, int dstStrideU, ByteBuffer dstV, int dstStrideV, int srcWidth, int srcHeight,
       int rotationMode);
+  private static native void nativeCropAndScalePlane(ByteBuffer src, int srcStride, int cropX,
+      int cropY, int cropWidth, int cropHeight, ByteBuffer dst, int dstStride, int scaleWidth,
+      int scaleHeight);
 }

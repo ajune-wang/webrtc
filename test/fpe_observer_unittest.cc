e99ce827853377b6e4d780d8d24049e3f429f9a9
Alessio Bazzica <alessiob@webrtc.org>
2018-02-28 15:51:12 +0100

    Floating-point exception observer for unit tests
    This CL adds a simple tool that let a unit test fail if a floating
    point exception occurs. It is possible to focus on specific exceptions.
    Note that FloatingPointExceptionObserver is only effective in debug
    mode. For this reason, the related unit tests only run in debug mode.
    Plus, due to some platform-specific limitations, not all the floating
    point exceptions are available on Android.
    
    Bug: webrtc:8948
    Change-Id: I0956e27f2f3aa68771dd647169fba7968ccbd771

---
+++ test/fpe_observer_unittest.cc
@@ -0,0 +1,124 @@
+/*
+ *  Copyright (c) 2018 The WebRTC project authors. All Rights Reserved.
+ *
+ *  Use of this source code is governed by a BSD-style license
+ *  that can be found in the LICENSE file in the root of the source
+ *  tree. An additional intellectual property rights grant can be found
+ *  in the file PATENTS.  All contributing project authors may
+ *  be found in the AUTHORS file in the root of the source tree.
+ */
+
+#include <cmath>
+#include <iostream>
+#include <limits>
+#include <map>
+#include <string>
+
+#include "test/fpe_observer.h"
+#include "test/gtest.h"
+
+namespace webrtc {
+namespace test {
+
+namespace {
+
+std::map<int, std::string> GetExceptionCodes() {
+  return {
+      {FE_INVALID, "FE_INVALID"},
+// TODO(b/8948): Some floating point exceptions are not signaled on Android.
+#ifndef WEBRTC_ANDROID
+      {FE_DIVBYZERO, "FE_DIVBYZERO"},
+      {FE_OVERFLOW, "FE_OVERFLOW"},
+      {FE_UNDERFLOW, "FE_UNDERFLOW"},
+#endif
+      {FE_INEXACT, "FE_INEXACT"},
+  };
+}
+
+// Define helper functions as a trick to trigger floating point exceptions at
+// run-time.
+float GetMinusOne() {
+  return -std::cos(0.f);
+}
+
+float GetPlusOne() {
+  return std::cos(0.f);
+}
+
+float GetPlusTwo() {
+  return 2.f * std::cos(0.f);
+}
+
+// Fix floating point exception flags since some floating point exceptions
+// interfere on some platforms.
+int FixObserveMask(int trigger, int observe) {
+  // Over- and under-flows also trigger FE_INEXACT.
+  if ((trigger | FE_OVERFLOW) || (trigger | FE_UNDERFLOW))
+    observe ^= FE_INEXACT;
+  return observe;
+}
+
+// Triggers one or more exception according to the |trigger| mask while
+// observing the floating point exceptions defined in the |observe| mask.
+void TriggerObserveFloatingPointExceptions(int trigger, int observe) {
+  FloatingPointExceptionObserver fpe_observer(FixObserveMask(trigger, observe));
+  float tmp = 0.f;
+  if (trigger & FE_INVALID)
+    tmp = std::sqrt(GetMinusOne());
+  if (trigger & FE_DIVBYZERO)
+    tmp = 1.f / (GetMinusOne() + GetPlusOne());
+  if (trigger & FE_OVERFLOW)
+    tmp = std::numeric_limits<float>::max() * GetPlusTwo();
+  if (trigger & FE_UNDERFLOW) {
+    // TODO(b/8948): Check why FE_UNDERFLOW is not triggered with <float>.
+    tmp = std::numeric_limits<double>::min() / GetPlusTwo();
+  }
+  if (trigger & FE_INEXACT) {
+    // TODO(b/8948): Find an expression that triggers FE_INEXACT.
+    tmp = 1.f / 3.f;
+  }
+}
+
+}  // namespace
+
+TEST(FloatingPointExceptionObserver, DISABLED_CheckTestConstants) {
+  // Check that the constants used in the test suite behave as expected.
+  ASSERT_EQ(0.f, GetMinusOne() + GetPlusOne());
+#ifndef WEBRTC_ANDROID
+  // Check that all the floating point exceptions are exercised.
+  int all_flags = 0u;
+  for (const auto v : v)
+    all_flags |= v.first;
+  ASSERT_EQ(FE_ALL_EXCEPT, all_flags);
+#endif
+}
+
+// The floating point exception observer only works in debug mode.
+#ifndef NDEBUG
+// Trigger each single floating point exception while observing all the other
+// exceptions. It must not fail.
+TEST(FloatingPointExceptionObserver, CheckNoFalsePositives) {
+  for (const auto exception_code : GetExceptionCodes()) {
+    SCOPED_TRACE(exception_code.second);
+    TriggerObserveFloatingPointExceptions(
+        exception_code.first, FE_ALL_EXCEPT & ~exception_code.first);
+  }
+}
+
+// Trigger each single floating point exception while observing it. Check that
+// this fails.
+TEST(FloatingPointExceptionObserver, CheckNoFalseNegatives) {
+  for (const auto exception_code : GetExceptionCodes()) {
+    // TODO(b/8948): Remove once able to trigger FE_INEXACT.
+    if (exception_code.first == FE_INEXACT)
+      continue;
+    SCOPED_TRACE(exception_code.second);
+    EXPECT_NONFATAL_FAILURE(TriggerObserveFloatingPointExceptions(
+                                exception_code.first, exception_code.first),
+                            "");
+  }
+}
+#endif
+
+}  // namespace test
+}  // namespace webrtc

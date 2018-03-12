db274b434dd47d69f7acc9c0aecaa4acc1991173
Sergey Silkin <ssilkin@webrtc.org>
2018-03-12 14:00:45 +0100

    Adding layering configurator and rate allocator for VP9 SVC.
    The configurator decides number of spatial layers, their resolution
    and bitrate thresholds based on given input resolution and maximum
    number of spatial layers.
    
    The allocator distributes available bitrate across spatial and
    temporal layers. If there is no enough bitrate to provide acceptable
    quality for all spatial layers allocator disables enhancement layers
    one by one until the condition is met.
    
    VP9 SVC related unit tests have been updated. Input resolution and
    bitrate in these tests have been increased to the level enough to
    provide desirable number of spatial layers.
    
    Bug: webrtc:8518
    Change-Id: I9df790920227c7f7dd4d42a50a856c22f0f4389b

--- modules/video_coding/codecs/test/video_codec_unittest.h
+++ modules/video_coding/codecs/test/video_codec_unittest.h
@@ -22,6 +22,7 @@
 #include "rtc_base/criticalsection.h"
 #include "rtc_base/event.h"
 #include "rtc_base/thread_annotations.h"
+#include "test/frame_generator.h"
 #include "test/gtest.h"
 
 namespace webrtc {
@@ -29,7 +30,8 @@ namespace webrtc {
 class VideoCodecUnitTest : public ::testing::Test {
  public:
   VideoCodecUnitTest()
-      : encode_complete_callback_(this),
+      : last_input_frame_timestamp_(0),
+        encode_complete_callback_(this),
         decode_complete_callback_(this),
         encoded_frame_event_(false /* manual reset */,
                              false /* initially signaled */),
@@ -74,9 +76,12 @@ class VideoCodecUnitTest : public ::testing::Test {
 
   virtual std::unique_ptr<VideoEncoder> CreateEncoder() = 0;
   virtual std::unique_ptr<VideoDecoder> CreateDecoder() = 0;
-  virtual VideoCodec codec_settings() = 0;
 
   void SetUp() override;
+
+  virtual void ModifyCodecSettings(VideoCodec* codec_settings);
+
+  VideoFrame* NextInputFrame();
 
   // Helper method for waiting a single encoded frame.
   bool WaitForEncodedFrame(EncodedImage* frame,
@@ -94,16 +99,16 @@ class VideoCodecUnitTest : public ::testing::Test {
   bool WaitForDecodedFrame(std::unique_ptr<VideoFrame>* frame,
                            rtc::Optional<uint8_t>* qp);
 
-  // Populated by InitCodecs().
   VideoCodec codec_settings_;
-
-  std::unique_ptr<VideoFrame> input_frame_;
 
   std::unique_ptr<VideoEncoder> encoder_;
   std::unique_ptr<VideoDecoder> decoder_;
 
  private:
   void InitCodecs();
+
+  std::unique_ptr<test::FrameGenerator> input_frame_generator_;
+  size_t last_input_frame_timestamp_;
 
   FakeEncodeCompleteCallback encode_complete_callback_;
   FakeDecodeCompleteCallback decode_complete_callback_;

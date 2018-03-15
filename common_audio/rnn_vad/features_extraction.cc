50ecec007e3c4821850ec91bbba231a8f00614dd
Alessio Bazzica <alessiob@webrtc.org>
2018-03-15 18:37:46 +0100

    === PRIVATE CL! === === DO NOT LAND ===
    AGC2 RNN based VAD
    
    # Compile unit tests
    $ ninja -C out/Default -j1000 common_audio:common_audio_unittests  common_audio/rnn_vad:rnn_vad_tool
    
    # Run unit tests
    $ cd out/Default
    $ ./common_audio_unittests --gtest_filter=RnnVad.*
    
    Change-Id: I98ded85ed4e0afc2176308f1ea929cf750477c29

---
+++ common_audio/rnn_vad/features_extraction.cc
@@ -0,0 +1,169 @@
+/*
+ *  Copyright (c) 2018 The WebRTC project authors. All Rights Reserved.
+ *
+ *  Use of this source code is governed by a BSD-style license
+ *  that can be found in the LICENSE file in the root of the source
+ *  tree. An additional intellectual property rights grant can be found
+ *  in the file PATENTS.  All contributing project authors may
+ *  be found in the AUTHORS file in the root of the source tree.
+ */
+
+#include "common_audio/rnn_vad/features_extraction.h"
+
+#include <algorithm>
+#include <complex>
+#include <iostream>
+
+#include "common_audio/rnn_vad/common.h"
+#include "common_audio/rnn_vad/lp_residual.h"
+#include "rtc_base/logging.h"
+
+namespace webrtc {
+namespace rnn_vad {
+namespace {
+
+constexpr size_t kHalfFftSize =
+    256;  // Next power of 2 of |kHalf20msFrameSize|.
+static_assert(kHalfFftSize / 2 < kHalf20msFrameSize &&
+                  kHalf20msFrameSize <= kHalfFftSize,
+              "Invalid FFT size.");
+
+// Bi-quad high-pass filter config (generated in Python).
+// B, A = scipy.signal.iirfilter(2, 30/12000, btype='highpass')
+// def PlotFilter(b, a):
+//   w, h = signal.freqz(b, a, 1000)
+//   fig = plt.figure()
+//   ax = fig.add_subplot(111)
+//   ax.plot(w, 20 * np.log10(abs(h)))
+//   ax.set_xscale('log')
+//   ax.set_title('frequency response')
+//   ax.set_xlabel('Frequency [radians / second]')
+//   ax.set_ylabel('Amplitude [dB]')
+//   # ax.axis((10, 1000, -100, 10))
+//   ax.grid(which='both', axis='both')
+//   plt.show()
+const BiQuadFilter::Config kHpfConfig24k(-1.98889291,
+                                         0.98895425,
+                                         0.99446179,
+                                         -1.98892358,
+                                         0.99446179);
+
+// Copies even samples from |src| and pushes them into |dst|.
+void PushDecimated2xSamples(
+    SequenceBuffer<float, kHalfBufSize, kHalf10msFrameSize>* dst,
+    rtc::ArrayView<const float, k10msFrameSize> src) {
+  RTC_DCHECK(dst);
+  // Copy even samples from |src|.
+  std::array<float, kHalf10msFrameSize> samples_decimated;
+  for (size_t i = 0; i < samples_decimated.size(); ++i)
+    samples_decimated[i] = src[2 * i];
+  // Push decimated samples into |dst|.
+  dst->Push({samples_decimated.data(), samples_decimated.size()});
+}
+
+}  // namespace
+
+std::array<size_t, kNumOpusBands> ComputeOpusBandsIndexes(
+    const size_t sample_rate,
+    const size_t frame_size) {
+  std::array<size_t, kNumOpusBands> indexes;
+  for (size_t i = 0; i < kNumOpusBands; ++i)
+    indexes[i] = kOpusBandsFrequencies[i] * frame_size / sample_rate;
+  return indexes;
+}
+
+void ComputeBandEnergies(rtc::ArrayView<const std::complex<float>> fft_coeffs,
+                         const float scaling,
+                         rtc::ArrayView<const size_t> bands_indexes,
+                         rtc::ArrayView<float> band_energies) {
+  RTC_DCHECK_EQ(bands_indexes.size(), band_energies.size());
+  RTC_DCHECK_LT(0.f, scaling);
+  // Init.
+  for (auto& v : band_energies)
+    v = 0.f;
+  RTC_DCHECK_EQ(0.f, band_energies[0]);
+  size_t i;
+  const size_t max_freq_bin_index = fft_coeffs.size() - 1;
+  for (i = 0; i < band_energies.size() - 1; ++i) {
+    RTC_DCHECK_EQ(0.f, band_energies[i + 1]);
+    RTC_DCHECK_GT(bands_indexes[i + 1], bands_indexes[i]);
+    const size_t first_freq_bin = bands_indexes[i];
+    const size_t last_freq_bin =
+        std::min(max_freq_bin_index,
+                 first_freq_bin + bands_indexes[i + 1] - bands_indexes[i] - 1);
+    if (first_freq_bin >= last_freq_bin)
+      break;
+    const size_t band_size = last_freq_bin - first_freq_bin + 1;
+    // Compute the band energy using a triangular band with peak response at the
+    // band boundary.
+    for (size_t j = first_freq_bin; j <= last_freq_bin; ++j) {
+      const float w = static_cast<float>(j - first_freq_bin) / band_size;
+      const float freq_bin_energy = std::norm(fft_coeffs[j] * scaling);
+      band_energies[i] += (1.f - w) * freq_bin_energy;
+      band_energies[i + 1] += w * freq_bin_energy;
+    }
+  }
+  // Fix the first and the last bands (they only got half contribution).
+  band_energies[0] *= 2.f;
+  band_energies[band_energies.size() - 1] *= 2.f;
+  // band_energies[i] *= 2.f;
+}
+
+RnnVadFeaturesExtractor::RnnVadFeaturesExtractor()
+    : full_band_buf_(0.f),
+      half_band_buf_(0.f),
+      fft_(kHalf20msFrameSize),
+      bands_indexes_(ComputeOpusBandsIndexes(kHalfSampleRate, kHalfFftSize)),
+      is_silence_(true) {
+  RTC_DCHECK_EQ(kHalfFftSize / 2 + 1, fft_.num_fft_points())
+      << "Unexpected FFT size.";
+  Reset();
+}
+
+RnnVadFeaturesExtractor::~RnnVadFeaturesExtractor() = default;
+
+void RnnVadFeaturesExtractor::Reset() {
+  feature_vector_.fill(0.f);
+}
+
+rtc::ArrayView<const float, kFeatureVectorSize>
+RnnVadFeaturesExtractor::GetOutput() const {
+  return {feature_vector_.data(), kFeatureVectorSize};
+}
+
+void RnnVadFeaturesExtractor::ComputeFeatures(
+    rtc::ArrayView<const float, k10msFrameSize> samples) {
+  // High-pass filter.
+  // TODO(alessiob): This is originally done at 48 kHz, not 24 kHz.
+  std::array<float, k10msFrameSize> samples_filtered;
+  hpf_.ProcessFrame(samples, {samples_filtered});
+  // Feed buffers.
+  full_band_buf_.Push(samples);
+  PushDecimated2xSamples(&half_band_buf_, samples);
+  // Compute features.
+  AnalyzePitch();
+  fft_.ForwardFft(half_band_buf_.GetBufferView(kHalfPitchMaxPeriod));
+  ComputeBandEnergies(fft_.GetFftOutputBufferView(), 1.f / kHalfFftSize,
+                      {bands_indexes_}, {band_energies_});
+  is_silence_ = false;
+}
+
+void RnnVadFeaturesExtractor::AnalyzePitch() {
+  // Extract the LP residual (full-band).
+  std::array<float, kBufSize> lp_residual;
+  {
+    auto buf_view = full_band_buf_.GetBufferView();
+    float lpc_coeffs[kNumLpcCoefficients];
+    rtc::ArrayView<float, kNumLpcCoefficients> lpc_coeffs_view(
+        lpc_coeffs, kNumLpcCoefficients);
+    ComputeInverseFilterCoefficients(lpc_coeffs_view, buf_view);
+    ComputeLpResidual({lp_residual.data(), lp_residual.size()}, lpc_coeffs_view,
+                      buf_view);
+  }
+  // Search pitch on the LP-residual.
+  pitch_info_ =
+      PitchSearch({lp_residual.data(), lp_residual.size()}, pitch_info_);
+}
+
+}  // namespace rnn_vad
+}  // namespace webrtc
---
+++ common_audio/rnn_vad/test_utils.h
@@ -0,0 +1,95 @@
+/*
+ *  Copyright (c) 2018 The WebRTC project authors. All Rights Reserved.
+ *
+ *  Use of this source code is governed by a BSD-style license
+ *  that can be found in the LICENSE file in the root of the source
+ *  tree. An additional intellectual property rights grant can be found
+ *  in the file PATENTS.  All contributing project authors may
+ *  be found in the AUTHORS file in the root of the source tree.
+ */
+
+#ifndef COMMON_AUDIO_RNN_VAD_TEST_UTILS_H_
+#define COMMON_AUDIO_RNN_VAD_TEST_UTILS_H_
+
+#include <algorithm>
+#include <cfenv>
+#include <fstream>
+#include <string>
+#include <vector>
+
+#include "api/array_view.h"
+#include "rtc_base/checks.h"
+#include "test/testsupport/fileutils.h"
+
+namespace webrtc {
+namespace test {
+
+// Parameters for bit exactness tests.
+constexpr size_t kSampleRate48k = 48000;
+constexpr size_t k48k10msFrameSize = 480;
+constexpr size_t k48k20msFrameSize = 960;
+constexpr size_t k48k20msFftSize = 1024;
+constexpr size_t k48k20msNumFftCoeffs = k48k20msFftSize / 2 + 1;
+
+constexpr float kExpectNearTolerance = 3e-6f;
+
+// Element-wise comparison for rtc::ArrayView<float> pairs.
+void ExpectNear(rtc::ArrayView<const float> a,
+                rtc::ArrayView<const float> b,
+                const float tolerance = kExpectNearTolerance);
+
+// Reader for binary files consisting of an arbitrary long sequence of elements
+// having type T. It is possible to read and cast to another type D at once.
+template <typename T, typename D = T>
+class BinaryFileReader {
+ public:
+  explicit BinaryFileReader(const std::string& file_path,
+                            const size_t chunk_size = 1)
+      : is_(file_path, std::ios::binary | std::ios::ate),
+        data_length_(is_.tellg() / sizeof(T)),
+        chunk_size_(chunk_size) {
+    RTC_CHECK_LT(0, chunk_size_);
+    RTC_CHECK(is_);
+    is_.seekg(0, is_.beg);
+    buf_.resize(chunk_size_);
+  }
+  // Disable copy (and move) semantics.
+  BinaryFileReader(const BinaryFileReader&) = delete;
+  BinaryFileReader& operator=(const BinaryFileReader&) = delete;
+  ~BinaryFileReader() = default;
+  size_t data_length() const { return data_length_; }
+  bool ReadValue(D* dst) {
+    if (std::is_same<T, D>::value) {
+      is_.read(reinterpret_cast<char*>(dst), sizeof(T));
+    } else {
+      T v;
+      is_.read(reinterpret_cast<char*>(&v), sizeof(T));
+      *dst = static_cast<D>(v);
+    }
+    return is_.gcount() == sizeof(T);
+  }
+  bool ReadChunk(rtc::ArrayView<D> dst) {
+    RTC_DCHECK_EQ(chunk_size_, dst.size());
+    const std::streamsize bytes_to_read = chunk_size_ * sizeof(T);
+    if (std::is_same<T, D>::value) {
+      is_.read(reinterpret_cast<char*>(dst.data()), bytes_to_read);
+    } else {
+      is_.read(reinterpret_cast<char*>(buf_.data()), bytes_to_read);
+      std::transform(buf_.begin(), buf_.end(), dst.begin(),
+                     [](const T& v) -> D { return static_cast<D>(v); });
+    }
+    return is_.gcount() == bytes_to_read;
+  }
+  void SeekForward(size_t items) { is_.seekg(items * sizeof(T), is_.cur); }
+
+ private:
+  std::ifstream is_;
+  const size_t data_length_;
+  const size_t chunk_size_;
+  std::vector<T> buf_;
+};
+
+}  // namespace test
+}  // namespace webrtc
+
+#endif  // COMMON_AUDIO_RNN_VAD_TEST_UTILS_H_

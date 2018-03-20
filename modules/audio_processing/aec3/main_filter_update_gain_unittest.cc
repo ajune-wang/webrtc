f5201cf2a471cffb81e7db63252e403850189a42
Per Åhgren <peah@webrtc.org>
2018-03-20 21:52:41 +0100

    Robustification of the echo suppression behavior during headset usage.
    This CL robustifies the echo removal behavior when headsets are used.
    In particular it:
    -Introduces a secondary, more refined alignment when no alignment can
    be found using the delay estimator.
    -Changes decision logic for when to use the linear filter output.
    -Changes the decision logic for when to be transparent.
    -Changes the way that the transparent mode works.
    -Makes the nonlinear mode less aggressive.
    -Removes the detector for non-audible echoes.
    -Makes the attenuation when there are signals with strong narrowband
    characteristics more mild in scenarios with low render.
    
    Furthermore the CL:
    -Removes the input of external echo leakage information.
    
    
    Bug: webrtc:9047,chromium:824111
    Change-Id: Ied1fe0c0a35d3c31b47606ed2db319a73644d406

--- modules/audio_processing/aec3/main_filter_update_gain_unittest.cc
+++ modules/audio_processing/aec3/main_filter_update_gain_unittest.cc
@@ -69,6 +69,7 @@ void RunFilterUpdateTest(int num_blocks_to_process,
   AecState aec_state(config);
   RenderSignalAnalyzer render_signal_analyzer(config);
   rtc::Optional<DelayEstimate> delay_estimate;
+  rtc::Optional<int> echo_remover_delay;
   std::array<float, kFftLength> s_scratch;
   std::array<float, kBlockSize> s;
   FftData S;
@@ -114,7 +115,7 @@ void RunFilterUpdateTest(int num_blocks_to_process,
     render_delay_buffer->PrepareCaptureProcessing();
 
     render_signal_analyzer.Update(*render_delay_buffer->GetRenderBuffer(),
-                                  aec_state.FilterDelay());
+                                  aec_state.FilterDelayBlocks());
 
     // Apply the main filter.
     main_filter.Filter(*render_delay_buffer->GetRenderBuffer(), &S);
@@ -161,10 +162,10 @@ void RunFilterUpdateTest(int num_blocks_to_process,
     // Update the delay.
     aec_state.HandleEchoPathChange(EchoPathVariability(
         false, EchoPathVariability::DelayAdjustment::kNone, false));
-    aec_state.Update(delay_estimate, main_filter.FilterFrequencyResponse(),
-                     main_filter.FilterImpulseResponse(), true,
-                     *render_delay_buffer->GetRenderBuffer(), E2_main, Y2, s,
-                     false);
+    aec_state.Update(delay_estimate, echo_remover_delay,
+                     main_filter.FilterFrequencyResponse(),
+                     main_filter.FilterImpulseResponse(), true, false,
+                     *render_delay_buffer->GetRenderBuffer(), E2_main, Y2, s);
   }
 
   std::copy(e_main.begin(), e_main.end(), e_last_block->begin());

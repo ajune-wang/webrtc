4b1b04da270fb85a128a96f26410e92ab0c7fa64
Sebastian Jansson <srte@webrtc.org>
2018-03-26 14:30:29 +0200

    Added SetClockOffset on FakeNetworkPipe.
    Added functionality on the FakeNetworkPipe to introduce arbitrary
    clock offsets. This offset is added to the reported receive time of
    all packets. This prepares for a later CL using this to test correction
    of receive time stamps.
    
    Bug: webrtc:9054
    Change-Id: I811b3aa8359bc917f59443088d8a418368242db9

--- test/direct_transport.h
+++ test/direct_transport.h
@@ -57,6 +57,8 @@ class DirectTransport : public Transport {
 
   ~DirectTransport() override;
 
+  void SetClockOffset(int64_t offset_ms);
+
   void SetConfig(const FakeNetworkPipe::Config& config);
 
   RTC_DEPRECATED void StopSending();

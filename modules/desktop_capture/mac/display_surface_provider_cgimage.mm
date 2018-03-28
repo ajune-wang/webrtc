1170e2d8b0904fb1883feee7a529647c11d3804c
Julien Isorce <julien.isorce@chromium.org>
2018-03-28 15:56:40 +0100

    Add interface DisplaySurface and the DisplaySurfaceCGImage implementation
    The underlying capture logic is left exactly the same, this CL only adds
    some abstractions.
    
    Also add interface DisplaySurfaceProvider and the
    DisplaySurfaceProviderCGImage implementation.
    
    Both interfaces are useful to make ScreenCapturerMac work with different
    kind of DisplaySurface.
    
    Bug: webrtc:8652
    Change-Id: I37743ba385fea5e1b40df3b094bfc321b8d796ae

---
+++ modules/desktop_capture/mac/display_surface_provider_cgimage.mm
@@ -0,0 +1,27 @@
+/*
+ *  Copyright (c) 2018 The WebRTC project authors. All Rights Reserved.
+ *
+ *  Use of this source code is governed by a BSD-style license
+ *  that can be found in the LICENSE file in the root of the source
+ *  tree. An additional intellectual property rights grant can be found
+ *  in the file PATENTS.  All contributing project authors may
+ *  be found in the AUTHORS file in the root of the source tree.
+ */
+
+#include "modules/desktop_capture/mac/display_surface_provider_cgimage.h"
+
+namespace webrtc {
+
+DisplaySurfaceProviderCGImage::DisplaySurfaceProviderCGImage() = default;
+
+DisplaySurfaceProviderCGImage::~DisplaySurfaceProviderCGImage() = default;
+
+rtc::scoped_refptr<DisplaySurface> DisplaySurfaceProviderCGImage::GetSurfaceForDisplay(
+    CGDirectDisplayID display_id) {
+  return DisplaySurfaceCGImage::CreateFromCurrentDisplayState(display_id);
+}
+
+void DisplaySurfaceProviderCGImage::SetSurfaceForDisplay(
+    CGDirectDisplayID display_id, rtc::scoped_refptr<DisplaySurface> surface) {}
+
+}  // namespace webrtc

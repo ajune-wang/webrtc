0a95e046d3b8e01d26582c010a354357d38da0d5
Qingsi Wang <qingsi@google.com>
2018-03-29 10:32:31 -0700

    Refactor the port allocation in BasicPortAllocator using a scheduler based mechanism.
    This set of changes is to replace the legacy AllocationSequence, which
    allocates port by phases, by a scheduler that posts each port
    allocation as a task to the network thread with a predefined pace.
    
    Bug: webrtc:9056
    Change-Id: If1213b8463ea8ca79b9b978838b3b5139a7eeeb9

--- p2p/client/basicportallocator_unittest.cc
+++ p2p/client/basicportallocator_unittest.cc
@@ -1126,9 +1126,9 @@ TEST_F(BasicPortAllocatorTest, TestGetAllPortsWithOneSecondStepDelay) {
   allocator_->set_step_delay(kDefaultStepDelay);
   EXPECT_TRUE(CreateSession(ICE_CANDIDATE_COMPONENT_RTP));
   session_->StartGettingPorts();
-  ASSERT_EQ_SIMULATED_WAIT(2U, candidates_.size(), 1000, fake_clock);
+  ASSERT_EQ_SIMULATED_WAIT(2U, candidates_.size(), 1500, fake_clock);
   EXPECT_EQ(2U, ports_.size());
-  ASSERT_EQ_SIMULATED_WAIT(6U, candidates_.size(), 2000, fake_clock);
+  ASSERT_EQ_SIMULATED_WAIT(6U, candidates_.size(), 2500, fake_clock);
   EXPECT_EQ(3U, ports_.size());
   EXPECT_TRUE(HasCandidate(candidates_, "relay", "udp", kRelayUdpIntAddr));
   EXPECT_TRUE(HasCandidate(candidates_, "relay", "udp", kRelayUdpExtAddr));
@@ -1748,7 +1748,7 @@ TEST_F(BasicPortAllocatorTest,
   TestEachInterfaceHasItsOwnTurnPorts();
 }
 
-// Testing DNS resolve for the TURN server, this will test AllocationSequence
+// Testing DNS resolve for the TURN server, this will test AllocationScheduler
 // handling the unresolved address signal from TurnPort.
 // TODO(pthatcher): Make this test work with SIMULATED_WAIT. It
 // appears that it doesn't currently because of the DNS look up not
@@ -1777,8 +1777,9 @@ TEST_F(BasicPortAllocatorTestWithRealClock,
 
   EXPECT_TRUE(CreateSession(ICE_CANDIDATE_COMPONENT_RTP));
   session_->StartGettingPorts();
-
+  RTC_LOG(INFO) << "now before expect = " << rtc::TimeMillis();
   EXPECT_EQ_WAIT(2U, ports_.size(), kDefaultAllocationTimeout);
+  RTC_LOG(INFO) << "now = " << rtc::TimeMillis();
 }
 
 // Test that when PORTALLOCATOR_ENABLE_SHARED_SOCKET is enabled only one port
@@ -2023,7 +2024,7 @@ TEST_F(BasicPortAllocatorTest, TestStopGettingPorts) {
   allocator_->set_step_delay(kDefaultStepDelay);
   EXPECT_TRUE(CreateSession(ICE_CANDIDATE_COMPONENT_RTP));
   session_->StartGettingPorts();
-  ASSERT_EQ_SIMULATED_WAIT(2U, candidates_.size(), 1000, fake_clock);
+  ASSERT_EQ_SIMULATED_WAIT(2U, candidates_.size(), 1500, fake_clock);
   EXPECT_EQ(2U, ports_.size());
   session_->StopGettingPorts();
   EXPECT_TRUE_SIMULATED_WAIT(candidate_allocation_done_, 1000, fake_clock);
@@ -2045,7 +2046,7 @@ TEST_F(BasicPortAllocatorTest, TestClearGettingPorts) {
   allocator_->set_step_delay(kDefaultStepDelay);
   EXPECT_TRUE(CreateSession(ICE_CANDIDATE_COMPONENT_RTP));
   session_->StartGettingPorts();
-  ASSERT_EQ_SIMULATED_WAIT(2U, candidates_.size(), 1000, fake_clock);
+  ASSERT_EQ_SIMULATED_WAIT(2U, candidates_.size(), 1500, fake_clock);
   EXPECT_EQ(2U, ports_.size());
   session_->ClearGettingPorts();
   EXPECT_TRUE_SIMULATED_WAIT(candidate_allocation_done_, 1000, fake_clock);

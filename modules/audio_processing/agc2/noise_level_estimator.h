de16806bb38b8cdb02c55489d2dd1e1597bd6757
Alex Loiko <aleloi@webrtc.org>
2018-04-03 15:32:07 +0200

    Noise level estimation for AGC2.
    Bug: webrtc:7494
    Change-Id: I442bdbbeb3796eb2518e96000aec9dc5a039ae6d

--- modules/audio_processing/agc2/noise_level_estimator.h
+++ modules/audio_processing/agc2/noise_level_estimator.h
@@ -11,19 +11,29 @@
 #ifndef MODULES_AUDIO_PROCESSING_AGC2_NOISE_LEVEL_ESTIMATOR_H_
 #define MODULES_AUDIO_PROCESSING_AGC2_NOISE_LEVEL_ESTIMATOR_H_
 
+#include "modules/audio_processing/agc2/signal_classifier.h"
 #include "modules/audio_processing/include/audio_frame_view.h"
 #include "rtc_base/constructormagic.h"
 
 namespace webrtc {
+class ApmDataDumper;
 
 class NoiseLevelEstimator {
  public:
-  NoiseLevelEstimator() {}
-
-  // Returns the estimated noise level in dBFS.
-  float Analyze(AudioFrameView<const float> frame);
+  NoiseLevelEstimator(ApmDataDumper* data_dumper);
+  ~NoiseLevelEstimator();
+  float Analyze(const AudioFrameView<const float>& frame);
 
  private:
+  void Initialize(int sample_rate_hz);
+
+  int sample_rate_hz_ = 48000;
+  float min_noise_energy_ = 0.f;
+  bool first_update_;
+  float noise_energy_;
+  int noise_energy_hold_counter_;
+  SignalClassifier signal_classifier_;
+
   RTC_DISALLOW_COPY_AND_ASSIGN(NoiseLevelEstimator);
 };
 

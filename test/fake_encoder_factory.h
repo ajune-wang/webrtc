53b17c5b29c095f32b4e4ac8cef5d60794d8ea4f
Niels Möller <nisse@webrtc.org>
2018-04-10 15:03:16 +0200

    Move creating encoder to VideoStreamEncoder.
    This used to be in WebRtcVideoChannel::WebRtcVideoSendStream.
    One implication is that encoder is not created until the first
    frame arrives, and some of the tests needed updates to emit a
    frame or two.
    
    Bug: webrtc:8830
    Change-Id: I78169b2bb4dfa4197b4b4229af9fd69d0f747835

---
+++ test/fake_encoder_factory.h
@@ -0,0 +1,53 @@
+/*
+ *  Copyright (c) 2018 The WebRTC project authors. All Rights Reserved.
+ *
+ *  Use of this source code is governed by a BSD-style license
+ *  that can be found in the LICENSE file in the root of the source
+ *  tree. An additional intellectual property rights grant can be found
+ *  in the file PATENTS.  All contributing project authors may
+ *  be found in the AUTHORS file in the root of the source tree.
+ */
+
+#ifndef TEST_FAKE_ENCODER_FACTORY_H_
+#define TEST_FAKE_ENCODER_FACTORY_H_
+
+#include <memory>
+#include <vector>
+
+#include "api/video_codecs/video_encoder_factory.h"
+#include "rtc_base/ptr_util.h"
+#include "system_wrappers/include/clock.h"
+#include "test/fake_encoder.h"
+
+namespace webrtc {
+namespace test {
+
+// An encoder factory producing FakeEncoder.
+class FakeEncoderFactory : public VideoEncoderFactory {
+ public:
+  explicit FakeEncoderFactory(Clock* clock) : clock_(clock) {}
+
+  // Unused by tests.
+  std::vector<SdpVideoFormat> GetSupportedFormats() const override {
+    RTC_NOTREACHED();
+  }
+
+  CodecInfo QueryVideoEncoder(const SdpVideoFormat& format) const override {
+    CodecInfo codec_info;
+    codec_info.is_hardware_accelerated = false;
+    codec_info.has_internal_source = false;
+    return codec_info;
+  }
+
+  std::unique_ptr<VideoEncoder> CreateVideoEncoder(
+      const SdpVideoFormat& format) override {
+    return rtc::MakeUnique<FakeEncoder>(clock_);
+  }
+
+  Clock* const clock_;
+};
+
+}  // namespace test
+}  // namespace webrtc
+
+#endif  // TEST_FAKE_ENCODER_FACTORY_H_

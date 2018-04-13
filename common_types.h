58629e4c1f25313f1d8debf8d068817bc23a0d31
Jonas Olsson <jonasolsson@webrtc.org>
2018-04-13 16:13:29 +0200

    Remove our stream << overloads from non-test build targets.
    Most are removed entirely, but RtcErrorType, RtpTransceiverDirection, IPAddress and
    SocketAddress are kept behind gtest's #ifdef UNIT_TEST.
    
    Bug: webrtc:8982
    Change-Id: I36db19891e7d25aeacb08b9a08aa2b4004765e70

--- common_types.h
+++ common_types.h
@@ -13,7 +13,6 @@
 
 #include <stddef.h>
 #include <string.h>
-#include <ostream>
 #include <string>
 #include <vector>
 
@@ -268,16 +267,6 @@ struct CodecInst {
   }
 
   bool operator!=(const CodecInst& other) const { return !(*this == other); }
-
-  friend std::ostream& operator<<(std::ostream& os, const CodecInst& ci) {
-    os << "{pltype: " << ci.pltype;
-    os << ", plname: " << ci.plname;
-    os << ", plfreq: " << ci.plfreq;
-    os << ", pacsize: " << ci.pacsize;
-    os << ", channels: " << ci.channels;
-    os << ", rate: " << ci.rate << "}";
-    return os;
-  }
 };
 
 // RTP

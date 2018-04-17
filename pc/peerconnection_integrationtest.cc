b1c3292b24d5d69980e166657da7f2d65a0f4fbf
Benjamin Wright <benwright@webrtc.org>
2018-04-17 16:26:27 -0700

    This changeset adds dependency injection support for SSL trusted root certificates in the OpenSSLAdapter.
    This extends the API surface so that
    custom certificates can be provided by an API user in both the standalone and
    factory creation paths for the OpenSSLAdapter. Prior to this change the SSL
    roots were hardcoded in a header file and directly included into
    openssladapter.cc. This forces the 100 kilobytes of certificates to always be
    compiled into the library. This is undesirable in certain linking cases where
    these certificates can be shared from another binary that already has an
    equivalent set of trusted roots hard coded into the binary.
    
    Support for removing the hard coded SSL roots has also been added through a new
    build flag. By default the hard coded SSL roots will be included and will be
    used if no other trusted root certificates are provided.
    
    The main goal of this CL is to reduce total binary size requirements of WebRTC
    by about 100kb in certain applications where adding these certificates is
    redundant.
    
    Change-Id: Ifd36d92b5cb32d1b3098a61ddfc244d76df8f30f
    
    Bug: 526260
    Change-Id: Ifd36d92b5cb32d1b3098a61ddfc244d76df8f30f

--- pc/peerconnection_integrationtest.cc
+++ pc/peerconnection_integrationtest.cc
@@ -52,6 +52,7 @@
 #include "rtc_base/fakenetwork.h"
 #include "rtc_base/firewallsocketserver.h"
 #include "rtc_base/gunit.h"
+#include "rtc_base/testcertificateverifier.h"
 #include "rtc_base/virtualsocketserver.h"
 #include "test/gmock.h"
 
@@ -3880,6 +3881,59 @@ TEST_P(PeerConnectionIntegrationTest, TurnCustomizerUsedForTurnConnections) {
 
   EXPECT_GT(customizer2->allow_channel_data_cnt_, 0u);
   EXPECT_GT(customizer2->modify_cnt_, 0u);
+
+  // Need to free the clients here since they're using things we created on
+  // the stack.
+  delete SetCallerPcWrapperAndReturnCurrent(nullptr);
+  delete SetCalleePcWrapperAndReturnCurrent(nullptr);
+}
+
+// Verify that a SSLCertificateVerifier passed in through RTCConfiguration
+// is actually used by the underlying SSL implementation to determine whether
+// a certificate presented by the TURN server is accepted by the client.
+// Note that openssladapter_unittest.cc contains more detailed, lower-level
+// tests.
+TEST_P(PeerConnectionIntegrationTest,
+       SSLCertificateVerifierUsedForTurnConnections) {
+  static const rtc::SocketAddress turn_server_internal_address{"88.88.88.0",
+                                                               3478};
+  static const rtc::SocketAddress turn_server_external_address{"88.88.88.1", 0};
+
+  // Enable TCP-TLS for the fake turn server.
+  cricket::TestTurnServer turn_server(
+      network_thread(), turn_server_internal_address,
+      turn_server_external_address, cricket::PROTO_TLS, true, "88.88.88.0");
+
+  webrtc::PeerConnectionInterface::IceServer ice_server;
+  ice_server.urls.push_back("turns:88.88.88.0:3478?transport=tcp");
+  ice_server.username = "test";
+  ice_server.password = "test";
+
+  auto cert_verifier = rtc::MakeUnique<rtc::TestCertificateVerifier>();
+  PeerConnectionInterface::RTCConfiguration client_1_config;
+  client_1_config.servers.push_back(ice_server);
+  client_1_config.type = webrtc::PeerConnectionInterface::kRelay;
+  client_1_config.tls_cert_verifier = cert_verifier.get();
+
+  PeerConnectionInterface::RTCConfiguration client_2_config;
+  client_2_config.servers.push_back(ice_server);
+  client_2_config.type = webrtc::PeerConnectionInterface::kRelay;
+  client_2_config.tls_cert_verifier = cert_verifier.get();
+
+  ASSERT_TRUE(
+      CreatePeerConnectionWrappersWithConfig(client_1_config, client_2_config));
+  ConnectFakeSignaling();
+
+  // Set "offer to receive audio/video" without adding any tracks, so we just
+  // set up ICE/DTLS with no media.
+  PeerConnectionInterface::RTCOfferAnswerOptions options;
+  options.offer_to_receive_audio = 1;
+  options.offer_to_receive_video = 1;
+  caller()->SetOfferAnswerOptions(options);
+  caller()->CreateAndSetAndSignalOffer();
+  ASSERT_TRUE_WAIT(DtlsConnected(), kDefaultTimeout);
+
+  EXPECT_GT(cert_verifier->call_count_, 0u);
 
   // Need to free the clients here since they're using things we created on
   // the stack.

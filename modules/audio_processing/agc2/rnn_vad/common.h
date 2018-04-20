8b354fd31f194201da12772ac0e7751405efd50e
Alessio Bazzica <alessiob@webrtc.org>
2018-04-20 10:56:37 +0200

    AGC2 RNN VAD: Pitch Search
    Functions to estimate pitch period and gain.
    
    Bug: webrtc:9076
    Change-Id: Icfe9430dcae11bdb96165c5bfe6e2b1d3bf848ab

--- modules/audio_processing/agc2/rnn_vad/common.h
+++ modules/audio_processing/agc2/rnn_vad/common.h
@@ -15,7 +15,34 @@ namespace webrtc {
 namespace rnn_vad {
 
 constexpr size_t kSampleRate24kHz = 24000;
-constexpr size_t kFrameSize10ms24kHz = 240;
+constexpr size_t kFrameSize10ms24kHz = kSampleRate24kHz / 100;
+constexpr size_t kFrameSize20ms24kHz = kFrameSize10ms24kHz * 2;
+
+// Pitch analysis params.
+constexpr size_t kPitchMinPeriod24kHz = kSampleRate24kHz / 800;   // 0.00125 s.
+constexpr size_t kPitchMaxPeriod24kHz = kSampleRate24kHz / 62.5;  // 0.016 s.
+constexpr size_t kBufSize24kHz = kPitchMaxPeriod24kHz + kFrameSize20ms24kHz;
+static_assert((kBufSize24kHz & 1) == 0, "The buffer size must be even.");
+
+// Define a higher minimum pitch period for the initial search. This is used to
+// avoid searching for very short periods, for which a refinement step is
+// responsible.
+constexpr size_t kPitchMinPeriod24kHzPitchSearch = 3 * kPitchMinPeriod24kHz;
+static_assert(kPitchMinPeriod24kHz < kPitchMinPeriod24kHzPitchSearch, "");
+static_assert(kPitchMinPeriod24kHzPitchSearch < kPitchMaxPeriod24kHz, "");
+
+// 12 kHz analysis.
+constexpr size_t kSampleRate12kHz = 12000;
+constexpr size_t kFrameSize10ms12kHz = kSampleRate12kHz / 100;
+constexpr size_t kFrameSize20ms12kHz = kFrameSize10ms12kHz * 2;
+constexpr size_t kBufSize12kHz = kBufSize24kHz / 2;
+constexpr size_t kPitchMinPeriod12kHzPitchSearch =
+    kPitchMinPeriod24kHzPitchSearch / 2;
+constexpr size_t kPitchMaxPeriod12kHz = kPitchMaxPeriod24kHz / 2;
+
+// 48 kHz constants.
+constexpr size_t kPitchMinPeriod48kHz = kPitchMinPeriod24kHz * 2;
+constexpr size_t kPitchMaxPeriod48kHz = kPitchMaxPeriod24kHz * 2;
 
 }  // namespace rnn_vad
 }  // namespace webrtc

55ba55b23cfb7899166bd47d911d047ead182fd2
Niels Möller <nisse@webrtc.org>
2018-04-24 13:56:42 +0200

    Don't clear resilienceOn automatically, if provided by the caller.
    Bug: webrtc:8830
    Change-Id: Ida4407c3a4a351f93778c5427237474984a0b1cb

--- modules/video_coding/video_codec_initializer.cc
+++ modules/video_coding/video_codec_initializer.cc
@@ -183,7 +183,8 @@ VideoCodec VideoCodecInitializer::VideoEncoderConfigToVideoCodec(
       RTC_DCHECK_LE(video_codec.VP8()->numberOfTemporalLayers,
                     kMaxTemporalStreams);
 
-      if (nack_enabled && video_codec.VP8()->numberOfTemporalLayers == 1) {
+      if (!config.encoder_specific_settings &&
+          nack_enabled && video_codec.VP8()->numberOfTemporalLayers == 1) {
         RTC_LOG(LS_INFO)
             << "No temporal layers and nack enabled -> resilience off";
         video_codec.VP8()->resilienceOn = false;
@@ -250,7 +251,8 @@ VideoCodec VideoCodecInitializer::VideoEncoderConfigToVideoCodec(
                       kMaxTemporalStreams);
       }
 
-      if (nack_enabled && video_codec.VP9()->numberOfTemporalLayers == 1 &&
+      if (!config.encoder_specific_settings &&
+          nack_enabled && video_codec.VP9()->numberOfTemporalLayers == 1 &&
           video_codec.VP9()->numberOfSpatialLayers == 1) {
         RTC_LOG(LS_INFO) << "No temporal or spatial layers and nack enabled -> "
                          << "resilience off";

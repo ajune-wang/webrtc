55811412215ad5b661dc811f77ae4d13ecfecdb6
Paulina Hensman <phensman@webrtc.org>
2018-04-27 17:06:50 +0200

    WIP: Injectable logging
    Allows passing a Loggable to PCFactory.initializationOptions, which
    is then injected to Logging.java and logging.h. Future log messages
    in both Java and native will then be passed to this Loggable.
    
    Change-Id: I2ff693380639448301a78a93dc11d3a0106f0967

---
+++ sdk/android/src/jni/pc/jni_logsink.cc
@@ -0,0 +1,30 @@
+/*
+ *  Copyright 2018 The WebRTC Project Authors. All rights reserved.
+ *
+ *  Use of this source code is governed by a BSD-style license
+ *  that can be found in the LICENSE file in the root of the source
+ *  tree. An additional intellectual property rights grant can be found
+ *  in the file PATENTS.  All contributing project authors may
+ *  be found in the AUTHORS file in the root of the source tree.
+ */
+#include "sdk/android/src/jni/pc/jni_logsink.h"
+
+#include "sdk/android/generated_logging_jni/jni/JNILogging_jni.h"
+
+namespace webrtc {
+namespace jni {
+
+JNILogSink::JNILogSink(JNIEnv* env, const JavaRef<jobject>& j_logging)
+    : env_(env), j_logging_(j_logging) {}
+JNILogSink::~JNILogSink() = default;
+
+void JNILogSink::OnLogMessage(const std::string& msg,
+                              rtc::LoggingSeverity severity,
+                              const char* tag) {
+  Java_JNILogging_logToInjectable(
+      env_, j_logging_, NativeToJavaString(env_, msg),
+      NativeToJavaInteger(env_, severity), NativeToJavaString(env_, tag));
+}
+
+}  // namespace jni
+}  // namespace webrtc

909dde89751addd48d41b972da9f30a52a287e45
Qingsi Wang <qingsi@google.com>
2018-05-25 19:40:32 -0700

    Enable any address ports by default.
    Ports not bound to any specific network interface are allocated by
    default. These any address ports are pruned after allocation,
    conditional on the allocation results of normal ports that are bound to
    the enumerated interfaces.
    
    Bug: webrtc:9313
    Change-Id: I3ce12eeab0cf3547224e5f8c188d061fc530e145

--- p2p/base/stunserver.h
+++ p2p/base/stunserver.h
@@ -54,7 +54,7 @@ class StunServer : public sigslot::has_slots<> {
        const rtc::SocketAddress& addr);
 
   // A helper method to compose a STUN binding response.
-  void GetStunBindReqponse(StunMessage* request,
+  void GetStunBindResponse(StunMessage* request,
                            const rtc::SocketAddress& remote_addr,
                            StunMessage* response) const;
 

d08b168f9b5eaa2301ac8a5c818849eef8f7f83a
Jerome Jiang <jianj@google.com>
2018-05-25 17:33:27 -0700

    Initialize maximum consecutive drops to INT_MAX in vp9 wrapper.
    Bug: None
    Change-Id: Id9f5b127c224660f3016973261045b4231a617c1

--- modules/video_coding/codecs/vp9/vp9_impl.cc
+++ modules/video_coding/codecs/vp9/vp9_impl.cc
@@ -510,6 +510,7 @@ int VP9EncoderImpl::InitAndSetControlSettings(const VideoCodec* inst) {
         svc_drop_frame.framedrop_thresh[i] =
             (i == 0) ? config_->rc_dropframe_thresh : 0;
       }
+      svc_drop_frame.max_consec_drop = INT_MAX;
       vpx_codec_control(encoder_, VP9E_SET_SVC_FRAME_DROP_LAYER,
                         &svc_drop_frame);
     }

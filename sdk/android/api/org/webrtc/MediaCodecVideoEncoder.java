5e80d6bc0af45b65a3574ab1e952fb0b48f8fb85
Niels Möller <nisse@webrtc.org>
2018-06-01 15:02:32 +0200

    Delete enum RtpVideoCodecTypes, replaced with VideoCodecType.
    Bug: webrtc:8995
    Change-Id: I0b44aa26f2f6a81aec7ca1281b8513d8e03228b8

--- sdk/android/api/org/webrtc/MediaCodecVideoEncoder.java
+++ sdk/android/api/org/webrtc/MediaCodecVideoEncoder.java
@@ -47,6 +47,7 @@ public class MediaCodecVideoEncoder {
 
   // Tracks webrtc::VideoCodecType.
   public enum VideoCodecType {
+    VIDEO_CODEC_UNKNOWN,
     VIDEO_CODEC_VP8,
     VIDEO_CODEC_VP9,
     VIDEO_CODEC_H264;

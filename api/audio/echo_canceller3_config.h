3a33debab913eef87b8bf7fa00d14bd7af0b13c2
Jesús de Vicente Peña <devicentepena@webrtc.org>
2018-06-12 14:27:12 +0200

    AEC3: Changes to how the reverberation decay is applied.
    In this work we introduce some changes on how the reverberation model for AEC3 is applied. Currently, the exponential modelling of the tails is applied over the linear echo estimates. That might result  in an overestimation of the reverberation tails under certain conditions. In this work, the reverberation model is instead applied over an estimate of the energies at the tails of the linear estimate.
    
    Additionally, the stationary estimator is changed so it does not disable the aec immediately after a burst of activity.
    
    Bug: webrtc:9384
    Change-Id: Ia486694ed326cfe231fc688877c0b9b6e2c450ff

--- api/audio/echo_canceller3_config.h
+++ api/audio/echo_canceller3_config.h
@@ -66,6 +66,7 @@ struct EchoCanceller3Config {
     float mf = 1.f;
     float hf = 1.f;
     float default_len = 0.f;
+    bool reverb_based_on_render = true;
     bool echo_can_saturate = true;
     bool bounded_erl = false;
   } ep_strength;

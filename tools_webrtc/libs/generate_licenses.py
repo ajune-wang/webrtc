2d88ebeb48c2dbb4f5dd9bc0652c3ac63c7fa3d9
Artem Titov <titovartem@webrtc.org>
2018-06-21 16:07:32 +0200

    Move portaudio third party dependency to proper location
    Bug: webrtc:8366
    Change-Id: I2a6464c5097840219825fe08dde9630f2231451a

--- tools_webrtc/libs/generate_licenses.py
+++ tools_webrtc/libs/generate_licenses.py
@@ -47,6 +47,7 @@ LIB_TO_LICENSES_DICT = {
     'usrsctp': ['third_party/usrsctp/LICENSE'],
     'webrtc': ['LICENSE', 'LICENSE_THIRD_PARTY'],
     'zlib': ['third_party/zlib/LICENSE'],
+    'portaudio': ['modules/third_party/portaudio/LICENSE'],
 
     # Compile time dependencies, no license needed:
     'yasm': [],

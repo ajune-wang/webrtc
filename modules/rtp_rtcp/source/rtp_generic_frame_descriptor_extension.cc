3f9cd083f72125b61ac705563f8bd8134c3cb938
Danil Chapovalov <danilchap@webrtc.org>
2018-06-25 16:14:25 +0200

    Enlist Generic frame descritpor header extension
    Bug: webrtc:9361
    Change-Id: Iabee824381be3776e17e95f32507058607fc0bc8

--- modules/rtp_rtcp/source/rtp_generic_frame_descriptor_extension.cc
+++ modules/rtp_rtcp/source/rtp_generic_frame_descriptor_extension.cc
@@ -44,6 +44,8 @@ constexpr uint8_t kFlageXtendedOffset = 0x02;
 //      +---------------+
 //      |      ...      |
 //      +-+-+-+-+-+-+-+-+
+constexpr RTPExtensionType RtpGenericFrameDescriptorExtension::kId;
+constexpr const char RtpGenericFrameDescriptorExtension::kUri[];
 
 bool RtpGenericFrameDescriptorExtension::Parse(
     rtc::ArrayView<const uint8_t> data,

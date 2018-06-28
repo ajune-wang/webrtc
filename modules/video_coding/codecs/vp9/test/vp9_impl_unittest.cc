53e89f88937d23f03c2eeb7c0e1e155c696dc7a7
“Michael <mhoro@webrtc.org>
2018-06-28 08:32:52 -0500

    Unit test for case where the number of active and configured spatial layers differ.
    Bug:None
    Change-Id: I5cf292a12d73777ca0fd5771eb1a4756626f640c

--- modules/video_coding/codecs/vp9/test/vp9_impl_unittest.cc
+++ modules/video_coding/codecs/vp9/test/vp9_impl_unittest.cc
@@ -267,6 +267,41 @@ TEST_F(TestVp9Impl, EnableDisableSpatialLayers) {
   }
 }
 
+TEST_F(TestVp9Impl, InsufficientBwForConfiguredNumSpatialLayers) {
+  // Configure encoder to produce N spatial layers. Allocate zero bit rate
+  // To the Nth spatial layer and encode several frames.
+  const size_t num_spatial_layers = 3;
+  const size_t num_frames_to_encode = 5;
+
+  ConfigureSvc(num_spatial_layers);
+  codec_settings_.VP9()->frameDroppingOn = true;
+
+  EXPECT_EQ(WEBRTC_VIDEO_CODEC_OK,
+            encoder_->InitEncode(&codec_settings_, 1 /* number of cores */,
+                                 0 /* max payload size (unused) */));
+
+  VideoBitrateAllocation bitrate_allocation;
+  // Allocate high bit rate to N-1 lower spatial layers to avoid frame dropping
+  // due to rate control. Top-most layer has 0 bit rate.
+  for (size_t sl_idx = 0; sl_idx < num_spatial_layers - 1; ++sl_idx) {
+    bitrate_allocation.SetBitrate(
+        sl_idx, 0,
+        codec_settings_.spatialLayers[sl_idx].targetBitrate * 1000 * 2);
+  }
+  EXPECT_EQ(WEBRTC_VIDEO_CODEC_OK,
+            encoder_->SetRateAllocation(bitrate_allocation,
+                                        codec_settings_.maxFramerate));
+
+  for (size_t frame_num = 0; frame_num < num_frames_to_encode; ++frame_num) {
+    SetWaitForEncodedFramesThreshold(num_spatial_layers - 1);
+    EXPECT_EQ(WEBRTC_VIDEO_CODEC_OK,
+              encoder_->Encode(*NextInputFrame(), nullptr, nullptr));
+    std::vector<EncodedImage> encoded_frame;
+    std::vector<CodecSpecificInfo> codec_specific_info;
+    ASSERT_TRUE(WaitForEncodedFrames(&encoded_frame, &codec_specific_info));
+  }
+}
+
 TEST_F(TestVp9Impl, EndOfPicture) {
   const size_t num_spatial_layers = 2;
   ConfigureSvc(num_spatial_layers);

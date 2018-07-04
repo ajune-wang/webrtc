8d7a6f4784a84b994b25552c87cfa0cc5f2a04a7
Jonas Olsson <jonasolsson@webrtc.org>
2018-07-04 13:50:45 +0200

    Reimplement rtc::ToString and rtc::FromString without streams.
    Bug: webrtc:8982
    Change-Id: I3977435b035fdebef449732301d6e77fc899e7ba

--- rtc_base/string_to_number.cc
+++ rtc_base/string_to_number.cc
@@ -48,5 +48,47 @@ absl::optional<unsigned_type> ParseUnsigned(const char* str, int base) {
   return absl::nullopt;
 }
 
+template <>
+absl::optional<float> ParseFloatingPoint(const char* str) {
+  RTC_DCHECK(str);
+  if (*str == '\0')
+    return absl::nullopt;
+  char* end = nullptr;
+  errno = 0;
+  const float value = std::strtof(str, &end);
+  if (end && *end == '\0' && errno == 0) {
+    return value;
+  }
+  return absl::nullopt;
+}
+
+template <>
+absl::optional<double> ParseFloatingPoint(const char* str) {
+  RTC_DCHECK(str);
+  if (*str == '\0')
+    return absl::nullopt;
+  char* end = nullptr;
+  errno = 0;
+  const double value = std::strtod(str, &end);
+  if (end && *end == '\0' && errno == 0) {
+    return value;
+  }
+  return absl::nullopt;
+}
+
+template <>
+absl::optional<long double> ParseFloatingPoint(const char* str) {
+  RTC_DCHECK(str);
+  if (*str == '\0')
+    return absl::nullopt;
+  char* end = nullptr;
+  errno = 0;
+  const long double value = std::strtold(str, &end);
+  if (end && *end == '\0' && errno == 0) {
+    return value;
+  }
+  return absl::nullopt;
+}
+
 }  // namespace string_to_number_internal
 }  // namespace rtc

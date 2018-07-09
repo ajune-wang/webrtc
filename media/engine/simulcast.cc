80d31616e99d18462d3e0bd9a548d1c40488549f
Ilya Nikolaevskiy <ilnik@webrtc.org>
2018-07-09 12:19:25 +0200

    Enable simulcast screenshare by default and remove experiment code
    Bug:chromium:690537
    Change-Id: I8b713a9c4d9d5d1a5cf13dff607cc25806aceed2

--- media/engine/simulcast.cc
+++ media/engine/simulcast.cc
@@ -27,9 +27,6 @@ namespace {
 constexpr int kScreenshareDefaultTl0BitrateKbps = 200;
 constexpr int kScreenshareDefaultTl1BitrateKbps = 1000;
 
-static const char* kSimulcastScreenshareFieldTrialName =
-    "WebRTC-SimulcastScreenshare";
-
 }  // namespace
 
 struct SimulcastFormat {
@@ -192,7 +189,6 @@ std::vector<webrtc::VideoStream> GetSimulcastConfig(
   if (is_screenshare) {
     return GetScreenshareLayers(max_layers, width, height, bitrate_priority,
                                 max_qp, max_framerate,
-                                ScreenshareSimulcastFieldTrialEnabled(),
                                 temporal_layers_supported);
   } else {
     return GetNormalSimulcastLayers(max_layers, width, height, bitrate_priority,
@@ -293,10 +289,8 @@ std::vector<webrtc::VideoStream> GetScreenshareLayers(
     double bitrate_priority,
     int max_qp,
     int max_framerate,
-    bool screenshare_simulcast_enabled,
     bool temporal_layers_supported) {
-  auto max_screenshare_layers =
-      screenshare_simulcast_enabled ? kMaxScreenshareSimulcastLayers : 1;
+  auto max_screenshare_layers = kMaxScreenshareSimulcastLayers;
   size_t num_simulcast_layers =
       std::min<int>(max_layers, max_screenshare_layers);
 
@@ -342,10 +336,6 @@ std::vector<webrtc::VideoStream> GetScreenshareLayers(
   // just set it for the first simulcast layer.
   layers[0].bitrate_priority = bitrate_priority;
   return layers;
-}
-
-bool ScreenshareSimulcastFieldTrialEnabled() {
-  return webrtc::field_trial::IsEnabled(kSimulcastScreenshareFieldTrialName);
 }
 
 }  // namespace cricket

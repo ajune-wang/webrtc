f8dbf8b161a8585c20431184adf42274fc7f2121
Jiawei Ou <ouj@fb.com>
2018-07-10 16:07:17 -0700

    Exposing video bitrate allocator into API
    In order to have public video bitrate allocator factory, the video bitrate allocator has be part of
    the api.
    
    Bug: webrtc:9513
    Change-Id: Ia2e5ab9eb988c710c1ac492afccc470a92544aa2

--- api/video/video_bitrate_allocator.h
+++ api/video/video_bitrate_allocator.h
@@ -8,8 +8,8 @@
  *  be found in the AUTHORS file in the root of the source tree.
  */
 
-#ifndef COMMON_VIDEO_INCLUDE_VIDEO_BITRATE_ALLOCATOR_H_
-#define COMMON_VIDEO_INCLUDE_VIDEO_BITRATE_ALLOCATOR_H_
+#ifndef API_VIDEO_VIDEO_BITRATE_ALLOCATOR_H_
+#define API_VIDEO_VIDEO_BITRATE_ALLOCATOR_H_
 
 #include "api/video/video_bitrate_allocation.h"
 
@@ -35,4 +35,4 @@ class VideoBitrateAllocationObserver {
 
 }  // namespace webrtc
 
-#endif  // COMMON_VIDEO_INCLUDE_VIDEO_BITRATE_ALLOCATOR_H_
+#endif  // API_VIDEO_VIDEO_BITRATE_ALLOCATOR_H_
--- video/video_stream_encoder.h
+++ video/video_stream_encoder.h
@@ -17,6 +17,7 @@
 #include <string>
 #include <vector>
 
+#include "api/video/video_bitrate_allocator.h"
 #include "api/video/video_rotation.h"
 #include "api/video/video_sink_interface.h"
 #include "api/video/video_stream_encoder_interface.h"
@@ -24,7 +25,6 @@
 #include "call/call.h"
 #include "call/video_send_stream.h"
 #include "common_types.h"  // NOLINT(build/include)
-#include "common_video/include/video_bitrate_allocator.h"
 #include "modules/video_coding/include/video_coding_defines.h"
 #include "modules/video_coding/utility/quality_scaler.h"
 #include "modules/video_coding/video_coding_impl.h"

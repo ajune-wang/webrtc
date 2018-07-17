27cb6d602fc35cc51856d533e0f2fd7133c261ea
Alex Loiko <aleloi@webrtc.org>
2018-07-17 11:48:21 +0200

    Reset level estimator when analog gain changes.
    In AgcManagerDirect::UpdateGain(), Agc::GetRmsErrorDb() is
    called. Depending on the result of that call, the analog gain may be
    changed. After an analog gain change, the Agc should be reset, because
    it's memory contains now invalid loudness levels.
    
    The Agc in modules/audio_processing/agc/agc.cc resets itself at every
    successful Agc::GetRmsErrorDb call. The AdaptiveModeLevelEstimatorAgc
    does not. This change makes sure all Agcs are reset from
    AgcManagerDirect.
    
    It will cause some Agcs to be reset twice. This is fine, because
    Agc::Reset() is cheap and idempotent.
    
    Bug: webrtc:7494
    Change-Id: Iee7495d699cbdb9d69b2ae0cb07034c6e2761e22

--- modules/audio_processing/agc/agc_manager_direct.cc
+++ modules/audio_processing/agc/agc_manager_direct.cc
@@ -115,7 +115,7 @@ AgcManagerDirect::AgcManagerDirect(GainControl* gctrl,
                                    int clipped_level_min,
                                    bool use_agc2_level_estimation,
                                    bool use_agc2_digital_adaptive)
-    : AgcManagerDirect(new Agc(),
+    : AgcManagerDirect(use_agc2_level_estimation ? nullptr : new Agc(),
                        gctrl,
                        volume_callbacks,
                        startup_min_level,
@@ -437,6 +437,8 @@ void AgcManagerDirect::UpdateGain() {
     // level_ was updated by SetLevel; log the new value.
     RTC_HISTOGRAM_COUNTS_LINEAR("WebRTC.Audio.AgcSetLevel", level_, 1,
                                 kMaxMicLevel, 50);
+    // Reset the AGC since the level has changed.
+    agc_->Reset();
   }
 }
 

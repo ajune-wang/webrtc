682213d65c6ee8468d78f8eb247c82801a8a5bf6
Benjamin Wright <benwright@webrtc.org>
2018-07-17 16:45:17 -0700

    Added API to Objective-C PeerConnectionFactoryOptions to enable GCM Ciphers.
    This changeset adds the ability for API users to enable or disable GCM Cipher
    suites from objective-c.
    
    Bug: chromium:713701
    Change-Id: I0ac7b60f55dd56bebbcfb315a542ef4843099802

--- sdk/objc/Framework/Classes/PeerConnection/RTCPeerConnectionFactoryOptions.mm
+++ sdk/objc/Framework/Classes/PeerConnection/RTCPeerConnectionFactoryOptions.mm
@@ -35,6 +35,7 @@ void setNetworkBit(webrtc::PeerConnectionFactoryInterface::Options* options,
 @synthesize ignoreWiFiNetworkAdapter = _ignoreWiFiNetworkAdapter;
 @synthesize ignoreEthernetNetworkAdapter = _ignoreEthernetNetworkAdapter;
 @synthesize enableAes128Sha1_32CryptoCipher = _enableAes128Sha1_32CryptoCipher;
+@synthesize enableGcmCipherSuites = _enableGcmCipherSuites;
 
 - (instancetype)init {
   return [super init];
@@ -52,6 +53,7 @@ void setNetworkBit(webrtc::PeerConnectionFactoryInterface::Options* options,
   setNetworkBit(&options, rtc::ADAPTER_TYPE_ETHERNET, self.ignoreEthernetNetworkAdapter);
 
   options.crypto_options.enable_aes128_sha1_32_crypto_cipher = self.enableAes128Sha1_32CryptoCipher;
+  options.crypto_options.enable_gcm_cipher_suites = self.enableGcmCipherSuites;
 
   return options;
 }

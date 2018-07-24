fcfe32314c8a0ab91614dc2162b272f604727087
Artem Titov <titovartem@webrtc.org>
2018-07-24 14:55:01 +0200

    Move fft4g to proper third_party directory
    Bug: webrtc:8366
    Change-Id: I98d3ae56a1d14b3ecacd85a4b3d234e215c8bc58

--- tools_webrtc/libs/generate_licenses.py
+++ tools_webrtc/libs/generate_licenses.py
@@ -49,6 +49,7 @@ LIB_TO_LICENSES_DICT = {
     'zlib': ['third_party/zlib/LICENSE'],
     'base64': ['rtc_base/third_party/base64/LICENSE'],
     'portaudio': ['modules/third_party/portaudio/LICENSE'],
+    'fft4g': ['common_audio/third_party/fft4g/LICENSE'],
 
     # Compile time dependencies, no license needed:
     'yasm': [],

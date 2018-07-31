6f07f4abe37c5935cf7dca2407d39414342abcfb
Mirko Bonadei <mbonadei@webrtc.org>
2018-07-31 14:20:41 +0200

    Never invoke rtc::LogMessage::SetLogToStderr outside of main.
    Bug: None
    Change-Id: Iae86fb14d7ca40af6d78d0f0cd81c5a39f65068d

--- video/video_quality_test.cc
+++ video/video_quality_test.cc
@@ -774,7 +774,6 @@ VideoQualityTest::CreateReceiveTransport() {
 }
 
 void VideoQualityTest::RunWithAnalyzer(const Params& params) {
-  rtc::LogMessage::SetLogToStderr(params.logging.logs);
   num_video_streams_ = params.call.dual_video ? 2 : 1;
   std::unique_ptr<test::LayerFilteringTransport> send_transport;
   std::unique_ptr<test::DirectTransport> recv_transport;
@@ -969,7 +968,6 @@ void VideoQualityTest::SetupAudio(Transport* transport) {
 }
 
 void VideoQualityTest::RunWithRenderers(const Params& params) {
-  rtc::LogMessage::SetLogToStderr(params.logging.logs);
   num_video_streams_ = params.call.dual_video ? 2 : 1;
   std::unique_ptr<test::LayerFilteringTransport> send_transport;
   std::unique_ptr<test::DirectTransport> recv_transport;

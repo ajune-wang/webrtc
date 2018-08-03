a9febed2b7cc81ed058228c4d9c3f58594e686f2
Bjorn Terelius <terelius@webrtc.org>
2018-08-03 16:23:55 +0200

    Optimize execution time of RTPSender::UpdateDelayStatistics
    Bug:webrtc:9439
    Change-Id: I908e9ced10031c614678a89657d089cb9a66b9ce

--- modules/rtp_rtcp/source/rtp_sender.h
+++ modules/rtp_rtcp/source/rtp_sender.h
@@ -296,6 +296,8 @@ class RTPSender {
   // Statistics
   rtc::CriticalSection statistics_crit_;
   SendDelayMap send_delays_ RTC_GUARDED_BY(statistics_crit_);
+  int max_delay_ms_ RTC_GUARDED_BY(statistics_crit_);
+  int64_t sum_delays_ms_ RTC_GUARDED_BY(statistics_crit_);
   FrameCounts frame_counts_ RTC_GUARDED_BY(statistics_crit_);
   StreamDataCounters rtp_stats_ RTC_GUARDED_BY(statistics_crit_);
   StreamDataCounters rtx_rtp_stats_ RTC_GUARDED_BY(statistics_crit_);

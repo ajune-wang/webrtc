940bc0c889fa2b3f655aaf086d464c24600b7f0a
Sebastian Jansson <srte@webrtc.org>
2018-08-07 18:18:57 +0200

    Adds functionality to add delay spikes in SimulatedNetwork.
    Bug: webrtc:9467
    Change-Id: Ifddafa65a9e18a3131fc0415764599740fab2db4

--- call/fake_network_pipe.h
+++ call/fake_network_pipe.h
@@ -96,6 +96,7 @@ class SimulatedNetwork : public NetworkSimulationInterface {
 
   // Sets a new configuration. This won't affect packets already in the pipe.
   void SetConfig(const Config& config);
+  void PauseTransmissionUntil(int64_t until_us);
 
   // NetworkSimulationInterface
   bool EnqueuePacket(PacketInFlightInfo packet) override;
@@ -121,6 +122,8 @@ class SimulatedNetwork : public NetworkSimulationInterface {
 
   // Link configuration.
   Config config_ RTC_GUARDED_BY(config_lock_);
+  absl::optional<int64_t> pause_transmission_until_us_
+      RTC_GUARDED_BY(config_lock_);
 
   // Are we currently dropping a burst of packets?
   bool bursting_;

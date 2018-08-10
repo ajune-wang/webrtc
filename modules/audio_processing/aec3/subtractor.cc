fdb67b2f913891f5b6c791c397b113c57815ee1d
Per Åhgren <peah@webrtc.org>
2018-08-10 20:14:55 +0200

    AEC3: Allow the main and shadow filters to have different lengths
    This CL changes the AEC3 code to allow the main and shadow filters
    to have different lengths.
    
    Bug: webrtc:9614,chromium:873100
    Change-Id: I3ec2861d496986610d5a73db5771bbe9b8bf7dcd

--- modules/audio_processing/aec3/subtractor.cc
+++ modules/audio_processing/aec3/subtractor.cc
@@ -115,12 +115,6 @@ Subtractor::Subtractor(const EchoCanceller3Config& config,
       G_shadow_(config_.filter.shadow_initial,
                 config.filter.config_change_duration_blocks) {
   RTC_DCHECK(data_dumper_);
-  // Currently, the rest of AEC3 requires the main and shadow filter lengths to
-  // be identical.
-  RTC_DCHECK_EQ(config_.filter.main.length_blocks,
-                config_.filter.shadow.length_blocks);
-  RTC_DCHECK_EQ(config_.filter.main_initial.length_blocks,
-                config_.filter.shadow_initial.length_blocks);
 }
 
 Subtractor::~Subtractor() = default;
@@ -209,11 +203,28 @@ void Subtractor::Process(const RenderBuffer& render_buffer,
   E_shadow.Spectrum(optimization_, output->E2_shadow);
   E_main.Spectrum(optimization_, output->E2_main);
 
+  // Compute the render powers.
+  std::array<float, kFftLengthBy2Plus1> X2_main;
+  std::array<float, kFftLengthBy2Plus1> X2_shadow_data;
+  std::array<float, kFftLengthBy2Plus1>& X2_shadow =
+      main_filter_.SizePartitions() == shadow_filter_.SizePartitions()
+          ? X2_main
+          : X2_shadow_data;
+  if (main_filter_.SizePartitions() == shadow_filter_.SizePartitions()) {
+    render_buffer.SpectralSum(main_filter_.SizePartitions(), &X2_main);
+  } else if (main_filter_.SizePartitions() > shadow_filter_.SizePartitions()) {
+    render_buffer.SpectralSums(shadow_filter_.SizePartitions(),
+                               main_filter_.SizePartitions(), &X2_shadow,
+                               &X2_main);
+  } else {
+    render_buffer.SpectralSums(main_filter_.SizePartitions(),
+                               shadow_filter_.SizePartitions(), &X2_main,
+                               &X2_shadow);
+  }
+
   // Update the main filter.
-  std::array<float, kFftLengthBy2Plus1> X2;
-  render_buffer.SpectralSum(main_filter_.SizePartitions(), &X2);
   if (!main_filter_adjusted) {
-    G_main_.Compute(X2, render_signal_analyzer, *output, main_filter_,
+    G_main_.Compute(X2_main, render_signal_analyzer, *output, main_filter_,
                     aec_state.SaturatedCapture() || main_saturation, &G);
   } else {
     G.re.fill(0.f);
@@ -227,10 +238,7 @@ void Subtractor::Process(const RenderBuffer& render_buffer,
   poor_shadow_filter_counter_ =
       output->e2_main < output->e2_shadow ? poor_shadow_filter_counter_ + 1 : 0;
   if (poor_shadow_filter_counter_ < 10 || !enable_shadow_filter_jumpstart_) {
-    if (shadow_filter_.SizePartitions() != main_filter_.SizePartitions()) {
-      render_buffer.SpectralSum(shadow_filter_.SizePartitions(), &X2);
-    }
-    G_shadow_.Compute(X2, render_signal_analyzer, E_shadow,
+    G_shadow_.Compute(X2_shadow, render_signal_analyzer, E_shadow,
                       shadow_filter_.SizePartitions(),
                       aec_state.SaturatedCapture() || shadow_saturation, &G);
     shadow_filter_.Adapt(render_buffer, G);

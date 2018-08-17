55baa21bd5cfbbc875612aa049d70d0b58cd898b
Anders Carlsson <andersc@webrtc.org>
2018-08-17 12:59:08 +0200

    Obj-C SDK Cleanup
    This CL separates the files under sdk/objc into logical directories, replacing
    the previous file layout under Framework/.
    
    A long term goal is to have some system set up to generate the files under
    sdk/objc/sdk (the PeerConnection API wrappers) from the C++ code. In the shorter
    term the goal is to abstract out shared concepts from these classes in order to
    make them as uniform as possible.
    
    The separation into base/, components/, and helpers/ are to differentiate between
    the base layer's common protocols, various utilities and the actual platform
    specific components.
    
    The old directory layout that resembled a framework's internal layout is not
    necessary, since it is generated by the framework target when building it.
    
    Bug: webrtc:9627
    Change-Id: Ib084fd83f050ae980649ca99e841f4fb0580bd8f

---
+++ sdk/objc/legacy_headers/WebRTC/RTCVideoCodec.h
@@ -0,0 +1,18 @@
+/*
+ *  Copyright 2017 The WebRTC project authors. All Rights Reserved.
+ *
+ *  Use of this source code is governed by a BSD-style license
+ *  that can be found in the LICENSE file in the root of the source
+ *  tree. An additional intellectual property rights grant can be found
+ *  in the file PATENTS.  All contributing project authors may
+ *  be found in the AUTHORS file in the root of the source tree.
+ */
+
+#include "base/RTCCodecSpecificInfo.h"
+#include "base/RTCEncodedImage.h"
+#include "base/RTCRtpFragmentationHeader.h"
+#include "base/RTCVideoCodecInfo.h"
+#include "base/RTCVideoDecoder.h"
+#include "base/RTCVideoEncoder.h"
+#include "base/RTCVideoEncoderQpThresholds.h"
+#include "base/RTCVideoEncoderSettings.h"

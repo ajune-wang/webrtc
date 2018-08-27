8935f3d35392789f3891933941fbebcf1d1c49ed
Mirko Bonadei <mbonadei@webrtc.org>
2018-08-27 16:24:34 +0200

    Remove redundant assertion.
    Bug: None
    Change-Id: I23830c5fe91af2e6ae736176c75b6d64270111d5

--- tools_webrtc/gtest_parallel_wrapper_test.py
+++ tools_webrtc/gtest_parallel_wrapper_test.py
@@ -70,8 +70,6 @@ class GtestParallelWrapperTest(unittest.TestCase):
                             '--test_artifacts_dir=' + exp_artifacts_dir])
       self.assertEqual(result.gtest_parallel_args, exp)
       self.assertEqual(result.output_dir, output_dir)
-      self.assertRegexpMatches(result.test_artifacts_dir,
-                               output_dir + '.test_artifacts')
 
   def testNoDirsSpecified(self):
     result = script.ParseArgs(['exec'])

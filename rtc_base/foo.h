fe631a77ee4990b03b4eb232a1fbf7d9ce8fbd15
Mirko Bonadei <mbonadei@webrtc.org>
2018-09-04 16:02:05 +0200

    Example of usage of RTC_EXPORT(COMPONENT_NAME).
    ** DO NOT LAND **
    
    Bug: None
    Change-Id: I032ef284aaf8ed3610ee148da3df4148ec5d133a

---
+++ rtc_base/foo.h
@@ -0,0 +1,19 @@
+/*
+ *  Copyright (c) 2018 The WebRTC project authors. All Rights Reserved.
+ *
+ *  Use of this source code is governed by a BSD-style license
+ *  that can be found in the LICENSE file in the root of the source
+ *  tree. An additional intellectual property rights grant can be found
+ *  in the file PATENTS.  All contributing project authors may
+ *  be found in the AUTHORS file in the root of the source tree.
+ */
+
+#ifndef RTC_BASE_FOO_H_
+#define RTC_BASE_FOO_H_
+
+#include "rtc_base/rtc_export.h"
+
+// int RTC_EXPORT(FOO) One();
+int One();
+
+#endif  // RTC_BASE_FOO_H_

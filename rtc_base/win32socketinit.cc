4b9bf90703b35b16ec42b11e898f3ce9a0ce1428
Mirko Bonadei <mbonadei@webrtc.org>
2018-09-13 10:14:56 +0200

    Fix no_{global_constructors,exit_time_destructors} in rtc_base_generic.
    Bug: webrtc:9693
    Change-Id: I4aae12ff43671ef2713a6fc4592e20759dc6b495

--- rtc_base/win32socketinit.cc
+++ rtc_base/win32socketinit.cc
@@ -10,17 +10,10 @@
 
 #include "rtc_base/win32socketinit.h"
 
+#include "rtc_base/checks.h"
 #include "rtc_base/win32.h"
 
 namespace rtc {
-
-// Please don't remove this function.
-void EnsureWinsockInit() {
-  // The default implementation uses a global initializer, so WSAStartup
-  // happens at module load time.  Thus we don't need to do anything here.
-  // The hook is provided so that a client that statically links with
-  // libjingle can override it, to provide its own initialization.
-}
 
 #if defined(WEBRTC_WIN)
 class WinsockInitializer {
@@ -39,7 +32,15 @@ class WinsockInitializer {
  private:
   int err_;
 };
-WinsockInitializer g_winsockinit;
 #endif
+
+// If Winsock is not already initialized, applications that use WebRTC
+// will have to call this function.
+void EnsureWinsockInit() {
+  // winsock_init is intentionally leaked in order to avoid an exit time
+  // destructor.
+  static WinsockInitializer* winsock_init = new WinsockInitializer();
+  RTC_CHECK_EQ(winsock_init->error(), 0);
+}
 
 }  // namespace rtc

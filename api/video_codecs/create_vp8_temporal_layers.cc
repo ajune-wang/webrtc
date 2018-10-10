4b316b8e0915ba47d5e2f6ca5dd4a8038db80e56
Erik Språng <sprang@webrtc.org>
2018-10-10 09:56:53 +0200

    Graduate vp8 temporal layers interface to the api target.
    Rename it Vp8TemporalLayers since it is codec specific.
    
    Bug: webrtc:9012
    Change-Id: I18187538b8142cdd7538f1a4ed1bada09d040f1f

--- api/video_codecs/create_vp8_temporal_layers.cc
+++ api/video_codecs/create_vp8_temporal_layers.cc
@@ -1,4 +1,5 @@
-/* Copyright (c) 2017 The WebRTC project authors. All Rights Reserved.
+/*
+ *  Copyright (c) 2018 The WebRTC project authors. All Rights Reserved.
  *
  *  Use of this source code is governed by a BSD-style license
  *  that can be found in the LICENSE file in the root of the source
@@ -7,26 +8,16 @@
  *  be found in the AUTHORS file in the root of the source tree.
  */
 
-#include "modules/video_coding/codecs/vp8/include/vp8_temporal_layers.h"
-
+#include "api/video_codecs/create_vp8_temporal_layers.h"
 #include "absl/memory/memory.h"
+#include "api/video_codecs/vp8_temporal_layers.h"
 #include "modules/video_coding/codecs/vp8/default_temporal_layers.h"
 #include "modules/video_coding/codecs/vp8/screenshare_layers.h"
 #include "system_wrappers/include/clock.h"
 
 namespace webrtc {
 
-bool TemporalLayers::FrameConfig::operator==(const FrameConfig& o) const {
-  return drop_frame == o.drop_frame &&
-         last_buffer_flags == o.last_buffer_flags &&
-         golden_buffer_flags == o.golden_buffer_flags &&
-         arf_buffer_flags == o.arf_buffer_flags && layer_sync == o.layer_sync &&
-         freeze_entropy == o.freeze_entropy &&
-         encoder_layer_id == o.encoder_layer_id &&
-         packetizer_temporal_idx == o.packetizer_temporal_idx;
-}
-
-std::unique_ptr<TemporalLayers> TemporalLayers::CreateTemporalLayers(
+std::unique_ptr<Vp8TemporalLayers> CreateTemporalLayers(
     TemporalLayersType type,
     int num_temporal_layers) {
   switch (type) {

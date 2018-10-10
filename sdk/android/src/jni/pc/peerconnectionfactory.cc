d6447bb6611cbf3aa028dbd7b9fcac797b42b5ff
Jiawei Ou <ouj@fb.com>
2018-10-10 00:24:38 -0700

    Use video bitrate allocator factory to create bitrate allocator
    Use the factory instead of using the builtin code path in `VideoCodecInitializer`.
    
    Bug: webrtc:9513
    Change-Id: Ia299ae1044a3ff4c91e208200938cba540bdcea6

--- sdk/android/src/jni/pc/peerconnectionfactory.cc
+++ sdk/android/src/jni/pc/peerconnectionfactory.cc
@@ -13,6 +13,7 @@
 #include <memory>
 #include <utility>
 
+#include "api/video/builtin_video_bitrate_allocator_factory.h"
 #include "api/video_codecs/video_decoder_factory.h"
 #include "api/video_codecs/video_encoder_factory.h"
 #include "media/base/mediaengine.h"
@@ -250,13 +251,17 @@ jlong CreatePeerConnectionFactoryForJava(
   std::unique_ptr<RtcEventLogFactoryInterface> rtc_event_log_factory(
       CreateRtcEventLogFactory());
 
+  std::unique_ptr<VideoBitrateAllocatorFactory>
+      video_bitrate_allocator_factory = CreateVideoBitrateAllocatorFactory();
+
   std::unique_ptr<cricket::MediaEngineInterface> media_engine(CreateMediaEngine(
       audio_device_module, audio_encoder_factory, audio_decoder_factory,
       std::unique_ptr<VideoEncoderFactory>(
           CreateVideoEncoderFactory(jni, jencoder_factory)),
       std::unique_ptr<VideoDecoderFactory>(
           CreateVideoDecoderFactory(jni, jdecoder_factory)),
-      audio_mixer, audio_processor));
+      std::move(video_bitrate_allocator_factory), audio_mixer,
+      audio_processor));
 
   rtc::scoped_refptr<PeerConnectionFactoryInterface> factory(
       CreateModularPeerConnectionFactory(

0c204bfcf7ce964fa03bb8f70b0a709c56229a64
Sebastian Jansson <srte@webrtc.org>
2018-10-11 14:24:02 +0200

    Adds IP overhead info to PacketInfo.
    This prepares for an upcoming CL removing the SocketAdress members.
    
    Bug: webrtc:9586
    Change-Id: Iacb03a106f1b143bd2d401a621abb99847a634ed

--- rtc_base/asyncpacketsocket.cc
+++ rtc_base/asyncpacketsocket.cc
@@ -9,6 +9,7 @@
  */
 
 #include "rtc_base/asyncpacketsocket.h"
+#include "rtc_base/nethelper.h"
 
 namespace rtc {
 
@@ -33,6 +34,8 @@ void CopySocketInformationToPacketInfo(size_t packet_size_bytes,
                                        bool is_connectionless,
                                        rtc::PacketInfo* info) {
   info->packet_size_bytes = packet_size_bytes;
+  info->ip_overhead_bytes =
+      cricket::GetIpOverhead(socket_from.GetLocalAddress().family());
   info->local_socket_address = socket_from.GetLocalAddress();
   if (!is_connectionless) {
     info->remote_socket_address = socket_from.GetRemoteAddress();

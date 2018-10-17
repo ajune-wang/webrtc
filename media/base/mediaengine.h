7c575aeb86fcd58adb9e49d237db7b62d9cd23ae
Sebastian Jansson <srte@webrtc.org>
2018-10-17 15:22:41 +0200

    Exposing audio and video engines directly.
    The audio and video engine is exposed directly rather via redundant
    wrapping functions. This reduces the amount of boiler plate code.
    
    Bug: webrtc:9883
    Change-Id: I203a945ee6079397e24a378966a569cd5626ac4a

--- media/base/mediaengine.h
+++ media/base/mediaengine.h
@@ -107,34 +107,10 @@ class MediaEngineInterface {
   // Initialization
   // Starts the engine.
   virtual bool Init() = 0;
-  // TODO(solenberg): Remove once VoE API refactoring is done.
-  virtual rtc::scoped_refptr<webrtc::AudioState> GetAudioState() const = 0;
-
-  // MediaChannel creation
-  // Creates a voice media channel. Returns NULL on failure.
-  virtual VoiceMediaChannel* CreateChannel(webrtc::Call* call,
-                                           const MediaConfig& config,
-                                           const AudioOptions& options) = 0;
-  // Creates a video media channel, paired with the specified voice channel.
-  // Returns NULL on failure.
-  virtual VideoMediaChannel* CreateVideoChannel(
-      webrtc::Call* call,
-      const MediaConfig& config,
-      const VideoOptions& options) = 0;
-
-  virtual const std::vector<AudioCodec>& audio_send_codecs() = 0;
-  virtual const std::vector<AudioCodec>& audio_recv_codecs() = 0;
-  virtual RtpCapabilities GetAudioCapabilities() = 0;
-  virtual std::vector<VideoCodec> video_codecs() = 0;
-  virtual RtpCapabilities GetVideoCapabilities() = 0;
-
-  // Starts AEC dump using existing file, a maximum file size in bytes can be
-  // specified. Logging is stopped just before the size limit is exceeded.
-  // If max_size_bytes is set to a value <= 0, no limit will be used.
-  virtual bool StartAecDump(rtc::PlatformFile file, int64_t max_size_bytes) = 0;
-
-  // Stops recording AEC dump.
-  virtual void StopAecDump() = 0;
+  virtual AudioEngineInterface& voice() = 0;
+  virtual VideoEngineInterface& video() = 0;
+  virtual const AudioEngineInterface& voice() const = 0;
+  virtual const VideoEngineInterface& video() const = 0;
 };
 
 // CompositeMediaEngine constructs a MediaEngine from separate
@@ -147,29 +123,10 @@ class CompositeMediaEngine : public MediaEngineInterface {
 
   bool Init() override;
 
-  rtc::scoped_refptr<webrtc::AudioState> GetAudioState() const override;
-  VoiceMediaChannel* CreateChannel(webrtc::Call* call,
-                                   const MediaConfig& config,
-                                   const AudioOptions& options) override;
-  VideoMediaChannel* CreateVideoChannel(webrtc::Call* call,
-                                        const MediaConfig& config,
-                                        const VideoOptions& options) override;
-
-  const std::vector<AudioCodec>& audio_send_codecs() override;
-  const std::vector<AudioCodec>& audio_recv_codecs() override;
-  RtpCapabilities GetAudioCapabilities() override;
-  std::vector<VideoCodec> video_codecs() override;
-  RtpCapabilities GetVideoCapabilities() override;
-
-  bool StartAecDump(rtc::PlatformFile file, int64_t max_size_bytes) override;
-
-  void StopAecDump() override;
-
- protected:
-  AudioEngineInterface& voice();
-  VideoEngineInterface& video();
-  const AudioEngineInterface& voice() const;
-  const VideoEngineInterface& video() const;
+  AudioEngineInterface& voice() override;
+  VideoEngineInterface& video() override;
+  const AudioEngineInterface& voice() const override;
+  const VideoEngineInterface& video() const override;
 
  private:
   std::pair<std::unique_ptr<AudioEngineInterface>,

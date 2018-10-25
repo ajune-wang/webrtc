3b9dc60413c1484a5535af34f8e3b864687af7c4
Piotr (Peter) Slatala <psla@webrtc.org>
2018-10-25 15:26:23 -0700

    Add MediaTransportSettings struct for configuring media transport.
    The struct is more generic and easier to extend than parameters to the
    Factory. In addition, the list of parameters to the factory might grow,
    making invocations awkward if not difficult to read.
    
    Bug: webrtc:9719
    Change-Id: I4b98e26f1f4c0d5ea840f9c28e7ed7abee072b74

--- api/media_transport_interface.h
+++ api/media_transport_interface.h
@@ -18,9 +18,11 @@
 #define API_MEDIA_TRANSPORT_INTERFACE_H_
 
 #include <memory>
+#include <string>
 #include <utility>
 #include <vector>
 
+#include "absl/types/optional.h"
 #include "api/array_view.h"
 #include "api/rtcerror.h"
 #include "api/video/encoded_image.h"
@@ -32,6 +34,16 @@ class Thread;
 }  // namespace rtc
 
 namespace webrtc {
+
+// A collection of settings for creation of media transport.
+struct MediaTransportSettings {
+  // Group calls are not currently supported, in 1:1 call one side must set
+  //   is_caller = true and another is_caller = false.
+  bool is_caller;
+
+  // If pre_shared_key is known, this setting should be set.
+  absl::optional<std::string> pre_shared_key;
+};
 
 // Represents encoded audio frame in any encoding (type of encoding is opaque).
 // To avoid copying of encoded data use move semantics when passing by value.
@@ -241,10 +253,21 @@ class MediaTransportFactory {
   // - Does not take ownership of packet_transport or network_thread.
   // - Does not support group calls, in 1:1 call one side must set
   //   is_caller = true and another is_caller = false.
+  // TODO(psla) This constructor will be removed and replaced with the one
+  // below.
   virtual RTCErrorOr<std::unique_ptr<MediaTransportInterface>>
   CreateMediaTransport(rtc::PacketTransportInternal* packet_transport,
                        rtc::Thread* network_thread,
                        bool is_caller) = 0;
+
+  // Creates media transport.
+  // - Does not take ownership of packet_transport or network_thread.
+  virtual RTCErrorOr<std::unique_ptr<MediaTransportInterface>>
+  CreateMediaTransport(rtc::PacketTransportInternal* packet_transport,
+                       rtc::Thread* network_thread,
+                       const MediaTransportSettings settings) {
+    return nullptr;
+  }
 };
 
 }  // namespace webrtc

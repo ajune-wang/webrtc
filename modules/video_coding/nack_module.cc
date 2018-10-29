fc53f05a52a60d76f0b5a42d5a657ac05a602918
Ying Wang <yinwa@webrtc.org>
2018-10-29 17:02:10 +0100

    Bugfix: FlexFEC causes retransmit bitrate increase.
    When FlexFEC enabled, sometimes media packet will be recovered by FEC before the actual media packet's arrival. In current implementation this will be considered as packet out of order and nack will be sent, thus cause large increase in retransmit bitrate.
    This fix will avoid sending nack for recovered media packets.
    
    Bug: None
    Change-Id: I008ef4e33668bce6d2cb9ff52b4b5c8e3f349965

--- modules/video_coding/nack_module.cc
+++ modules/video_coding/nack_module.cc
@@ -16,6 +16,7 @@
 #include "modules/utility/include/process_thread.h"
 #include "rtc_base/checks.h"
 #include "rtc_base/logging.h"
+#include "system_wrappers/include/field_trial.h"
 
 namespace webrtc {
 
@@ -28,14 +29,30 @@ const int kProcessFrequency = 50;
 const int kProcessIntervalMs = 1000 / kProcessFrequency;
 const int kMaxReorderedPackets = 128;
 const int kNumReorderingBuckets = 10;
+const int kSendNackDelayMs = 0;
+
+bool GetSendNackDelayMs() {
+  int delay_ms = strtoul(
+      webrtc::field_trial::FindFullName("WebRTC-SendNackDelayMs").c_str(),
+      nullptr, 10);
+  if (delay_ms > 0 && delay_ms <= kSendNackDelayMs) {
+    RTC_LOG(LS_INFO) << "SendNackDelay is set to " << delay_ms;
+    return delay_ms;
+  }
+  return kSendNackDelayMs;
+}
+
 }  // namespace
 
 NackModule::NackInfo::NackInfo()
     : seq_num(0), send_at_seq_num(0), sent_at_time(-1), retries(0) {}
 
-NackModule::NackInfo::NackInfo(uint16_t seq_num, uint16_t send_at_seq_num)
+NackModule::NackInfo::NackInfo(uint16_t seq_num,
+                               uint16_t send_at_seq_num,
+                               int64_t created_at_time)
     : seq_num(seq_num),
       send_at_seq_num(send_at_seq_num),
+      created_at_time(-1),
       sent_at_time(-1),
       retries(0) {}
 
@@ -49,7 +66,8 @@ NackModule::NackModule(Clock* clock,
       initialized_(false),
       rtt_ms_(kDefaultRttMs),
       newest_seq_num_(0),
-      next_process_time_ms_(-1) {
+      next_process_time_ms_(-1),
+      send_nack_delay_ms_(GetSendNackDelayMs()) {
   RTC_DCHECK(clock_);
   RTC_DCHECK(nack_sender_);
   RTC_DCHECK(keyframe_request_sender_);
@@ -108,11 +126,32 @@ int NackModule::OnReceivedPacket(uint16_t seq_num, bool is_keyframe) {
   return 0;
 }
 
+int NackModule::OnRecoveredPacket(uint16_t seq_num, bool is_keyframe) {
+  rtc::CritScope lock(&crit_);
+  recovered_list_.insert(seq_num);
+  // And remove old ones so we don't accumulate recovered packets.
+  auto it = recovered_list_.lower_bound(seq_num - kMaxPacketAge);
+  if (it != recovered_list_.begin())
+    recovered_list_.erase(recovered_list_.begin(), it);
+
+  // Keep track of new keyframes.
+  if (is_keyframe)
+    keyframe_list_.insert(seq_num);
+
+  // And remove old ones so we don't accumulate keyframes.
+  it = keyframe_list_.lower_bound(seq_num - kMaxPacketAge);
+  if (it != keyframe_list_.begin())
+    keyframe_list_.erase(keyframe_list_.begin(), it);
+  return 0;
+}
+
 void NackModule::ClearUpTo(uint16_t seq_num) {
   rtc::CritScope lock(&crit_);
   nack_list_.erase(nack_list_.begin(), nack_list_.lower_bound(seq_num));
   keyframe_list_.erase(keyframe_list_.begin(),
                        keyframe_list_.lower_bound(seq_num));
+  recovered_list_.erase(recovered_list_.begin(),
+                        recovered_list_.lower_bound(seq_num));
 }
 
 void NackModule::UpdateRtt(int64_t rtt_ms) {
@@ -124,6 +163,7 @@ void NackModule::Clear() {
   rtc::CritScope lock(&crit_);
   nack_list_.clear();
   keyframe_list_.clear();
+  recovered_list_.clear();
 }
 
 int64_t NackModule::TimeUntilNextProcess() {
@@ -200,7 +240,8 @@ void NackModule::AddPacketsToNack(uint16_t seq_num_start,
   }
 
   for (uint16_t seq_num = seq_num_start; seq_num != seq_num_end; ++seq_num) {
-    NackInfo nack_info(seq_num, seq_num + WaitNumberOfPackets(0.5));
+    NackInfo nack_info(seq_num, seq_num + WaitNumberOfPackets(0.5),
+                       clock_->TimeInMilliseconds());
     RTC_DCHECK(nack_list_.find(seq_num) == nack_list_.end());
     nack_list_[seq_num] = nack_info;
   }
@@ -213,8 +254,14 @@ std::vector<uint16_t> NackModule::GetNackBatch(NackFilterOptions options) {
   std::vector<uint16_t> nack_batch;
   auto it = nack_list_.begin();
   while (it != nack_list_.end()) {
+    // Do not send nack for FEC recovered packets
+    if (recovered_list_.find(it->second.seq_num) != recovered_list_.end()) {
+      ++it;
+      continue;
+    }
     if (consider_seq_num && it->second.sent_at_time == -1 &&
-        AheadOrAt(newest_seq_num_, it->second.send_at_seq_num)) {
+        AheadOrAt(newest_seq_num_, it->second.send_at_seq_num) &&
+        (now_ms - it->second.created_at_time) > send_nack_delay_ms_) {
       nack_batch.emplace_back(it->second.seq_num);
       ++it->second.retries;
       it->second.sent_at_time = now_ms;
@@ -228,7 +275,8 @@ std::vector<uint16_t> NackModule::GetNackBatch(NackFilterOptions options) {
       continue;
     }
 
-    if (consider_timestamp && it->second.sent_at_time + rtt_ms_ <= now_ms) {
+    if (consider_timestamp && it->second.sent_at_time + rtt_ms_ <= now_ms &&
+        (now_ms - it->second.created_at_time) > send_nack_delay_ms_) {
       nack_batch.emplace_back(it->second.seq_num);
       ++it->second.retries;
       it->second.sent_at_time = now_ms;

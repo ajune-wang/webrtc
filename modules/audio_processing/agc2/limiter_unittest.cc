eda3176f7892aa057e3614d1e0a09bf4cddfd8d4
Alessio Bazzica <alessiob@webrtc.org>
2018-10-30 17:39:03 +0100

    unit tests moved fixed gain controller removed FrameCombiner using limiter instead of fixed digital fixed gc replaced with gain applier + limiter in agc2
    Change-Id: I4d7daeae917257ac019a645b74deba6642f77322

--- modules/audio_processing/agc2/limiter_unittest.cc
+++ modules/audio_processing/agc2/limiter_unittest.cc
@@ -10,14 +10,41 @@
 
 #include "modules/audio_processing/agc2/limiter.h"
 
+#include "absl/memory/memory.h"
 #include "common_audio/include/audio_util.h"
 #include "modules/audio_processing/agc2/agc2_common.h"
 #include "modules/audio_processing/agc2/agc2_testing_common.h"
 #include "modules/audio_processing/agc2/vector_float_frame.h"
 #include "modules/audio_processing/logging/apm_data_dumper.h"
 #include "rtc_base/gunit.h"
+#include "system_wrappers/include/metrics.h"
 
 namespace webrtc {
+namespace {
+
+float RunLimiterWithConstantInput(Limiter* limiter,
+                                  float input_level,
+                                  size_t num_frames,
+                                  int sample_rate) {
+  // Give time to the level estimator to converge.
+  for (size_t i = 0; i < num_frames; ++i) {
+    VectorFloatFrame vectors_with_float_frame(
+        1, rtc::CheckedDivExact(sample_rate, 100), input_level);
+    limiter->Process(vectors_with_float_frame.float_frame_view());
+  }
+
+  // Process the last frame with constant input level.
+  VectorFloatFrame vectors_with_float_frame_last(
+      1, rtc::CheckedDivExact(sample_rate, 100), input_level);
+  limiter->Process(vectors_with_float_frame_last.float_frame_view());
+
+  // Return the last sample from the last processed frame.
+  const auto channel =
+      vectors_with_float_frame_last.float_frame_view().channel(0);
+  return channel[channel.size() - 1];
+}
+
+}  // namespace
 
 TEST(Limiter, LimiterShouldConstructAndRun) {
   const int sample_rate_hz = 48000;
@@ -55,6 +82,33 @@ TEST(Limiter, OutputVolumeAboveThreshold) {
   for (const auto& sample : channel) {
     EXPECT_LT(0.9f * kMaxAbsFloatS16Value, sample);
   }
+}
+
+TEST(Limiter, RegionHistogramIsUpdated) {
+  constexpr size_t kSampleRateHz = 8000;
+  constexpr float kInputLevel = 1000.f;
+  constexpr size_t kNumFrames = 5;
+
+  metrics::Reset();
+
+  ApmDataDumper apm_data_dumper(0);
+  auto limiter =
+      absl::make_unique<Limiter>(kSampleRateHz, &apm_data_dumper, "Test");
+
+  static_cast<void>(RunLimiterWithConstantInput(limiter.get(), kInputLevel,
+                                                kNumFrames, kSampleRateHz));
+
+  // Destroying Limiter should cause the last limiter region to be logged.
+  limiter.reset();
+
+  EXPECT_EQ(1, metrics::NumSamples(
+                   "WebRTC.Audio.Test.FixedDigitalGainCurveRegion.Identity"));
+  EXPECT_EQ(0, metrics::NumSamples(
+                   "WebRTC.Audio.Test.FixedDigitalGainCurveRegion.Knee"));
+  EXPECT_EQ(0, metrics::NumSamples(
+                   "WebRTC.Audio.Test.FixedDigitalGainCurveRegion.Limiter"));
+  EXPECT_EQ(0, metrics::NumSamples(
+                   "WebRTC.Audio.Test.FixedDigitalGainCurveRegion.Saturation"));
 }
 
 }  // namespace webrtc

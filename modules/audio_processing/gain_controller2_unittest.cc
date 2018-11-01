a7279d19e6fd3131ec50ae4c5b3df93906bd1681
Alessio Bazzica <alessiob@webrtc.org>
2018-11-01 14:46:59 +0100

    Fix AGC2 fixed-adaptive gain controllers order.
    This CL refactors AGC2 and fixes the order with which the fixed
    and the adaptive digital gain controllers are applied - i.e., fixed
    first, then adaptive and finally limiter.
    
    FixedGainController has been removed since we need to split the
    processing done by the gain applier and the limiter.
    Also, GainApplier and Limiter are easy enough to be used without
    a wrapper and a wrapper would need 2 separated calls in the right
    order - i.e., error prone.
    
    FrameCombiner in audio mixer has been adapted and now only uses the
    limiter (which is what is needed since no gain is applied).
    
    The unit tests for FixedGainController have been moved to
    gain_controller2_unittests. They have been re-adapted and
    ChangeFixedGainShouldBeFastAndTimeInvariant has been re-tuned.
    
    Change-Id: I4d7daeae917257ac019a645b74deba6642f77322

--- modules/audio_processing/gain_controller2_unittest.cc
+++ modules/audio_processing/gain_controller2_unittest.cc
@@ -9,8 +9,11 @@
  */
 
 #include <algorithm>
+#include <tuple>
 
+#include "absl/memory/memory.h"
 #include "api/array_view.h"
+#include "modules/audio_processing/agc2/agc2_testing_common.h"
 #include "modules/audio_processing/audio_buffer.h"
 #include "modules/audio_processing/gain_controller2.h"
 #include "rtc_base/checks.h"
@@ -21,15 +24,48 @@ namespace test {
 
 namespace {
 
-constexpr size_t kFrameSizeMs = 10u;
-constexpr size_t kStereo = 2u;
-
 void SetAudioBufferSamples(float value, AudioBuffer* ab) {
   // Sets all the samples in |ab| to |value|.
   for (size_t k = 0; k < ab->num_channels(); ++k) {
     std::fill(ab->channels_f()[k], ab->channels_f()[k] + ab->num_frames(),
               value);
   }
+}
+
+float RunAgc2WithConstantInput(GainController2* agc2,
+                               float input_level,
+                               size_t num_frames,
+                               int sample_rate) {
+  const int num_samples = rtc::CheckedDivExact(sample_rate, 100);
+  AudioBuffer ab(num_samples, 1, num_samples, 1, num_samples);
+
+  // Give time to the level estimator to converge.
+  for (size_t i = 0; i < num_frames + 1; ++i) {
+    SetAudioBufferSamples(input_level, &ab);
+    agc2->Process(&ab);
+  }
+
+  // Return the last sample from the last processed frame.
+  return ab.channels_f()[0][num_samples - 1];
+}
+
+AudioProcessing::Config::GainController2 CreateAgc2FixedDigitalModeConfig(
+    float fixed_gain_db) {
+  AudioProcessing::Config::GainController2 config;
+  config.adaptive_digital_mode = false;
+  config.fixed_gain_db = fixed_gain_db;
+  // TODO(alessiob): Check why ASSERT_TRUE() below does not compile.
+  EXPECT_TRUE(GainController2::Validate(config));
+  return config;
+}
+
+std::unique_ptr<GainController2> CreateAgc2FixedDigitalMode(
+    float fixed_gain_db,
+    size_t sample_rate_hz) {
+  auto agc2 = absl::make_unique<GainController2>();
+  agc2->ApplyConfig(CreateAgc2FixedDigitalModeConfig(fixed_gain_db));
+  agc2->Initialize(sample_rate_hz);
+  return agc2;
 }
 
 }  // namespace
@@ -69,23 +105,107 @@ TEST(GainController2, ToString) {
             GainController2::ToString(config));
 }
 
-TEST(GainController2, Usage) {
-  // Tests GainController2::Process() on an AudioBuffer instance.
-  std::unique_ptr<GainController2> gain_controller2(new GainController2());
-  gain_controller2->Initialize(AudioProcessing::kSampleRate48kHz);
-  const size_t num_frames = rtc::CheckedDivExact<size_t>(
-      kFrameSizeMs * AudioProcessing::kSampleRate48kHz, 1000);
-  AudioBuffer ab(num_frames, kStereo, num_frames, kStereo, num_frames);
-  constexpr float sample_value = 1000.f;
-  SetAudioBufferSamples(sample_value, &ab);
-  AudioProcessing::Config::GainController2 config;
+TEST(GainController2FixedDigital, GainShouldChangeOnSetGain) {
+  constexpr float kInputLevel = 1000.f;
+  constexpr size_t kNumFrames = 5;
+  constexpr size_t kSampleRateHz = 8000;
+  constexpr float kGain0Db = 0.f;
+  constexpr float kGain20Db = 20.f;
 
-  // Check that samples are amplified when the fixed gain is greater than 0 dB.
-  config.fixed_gain_db = 5.f;
-  gain_controller2->ApplyConfig(config);
-  gain_controller2->Process(&ab);
-  EXPECT_LT(sample_value, ab.channels_f()[0][0]);
+  auto agc2_fixed = CreateAgc2FixedDigitalMode(kGain0Db, kSampleRateHz);
+
+  // Signal level is unchanged with 0 db gain.
+  EXPECT_FLOAT_EQ(RunAgc2WithConstantInput(agc2_fixed.get(), kInputLevel,
+                                           kNumFrames, kSampleRateHz),
+                  kInputLevel);
+
+  // +20 db should increase signal by a factor of 10.
+  agc2_fixed->ApplyConfig(CreateAgc2FixedDigitalModeConfig(kGain20Db));
+  EXPECT_FLOAT_EQ(RunAgc2WithConstantInput(agc2_fixed.get(), kInputLevel,
+                                           kNumFrames, kSampleRateHz),
+                  kInputLevel * 10);
 }
+
+TEST(GainController2FixedDigital, ChangeFixedGainShouldBeFastAndTimeInvariant) {
+  // Number of frames required for the fixed gain controller to adapt on the
+  // input signal when the gain changes.
+  constexpr size_t kNumFrames = 5;
+
+  constexpr float kInputLevel = 1000.f;
+  constexpr size_t kSampleRateHz = 8000;
+  constexpr float kGainDbLow = 0.f;
+  constexpr float kGainDbHigh = 25.f;
+  static_assert(kGainDbLow < kGainDbHigh, "");
+
+  auto agc2_fixed = CreateAgc2FixedDigitalMode(kGainDbLow, kSampleRateHz);
+
+  // Start with a lower gain.
+  const float output_level_pre = RunAgc2WithConstantInput(
+      agc2_fixed.get(), kInputLevel, kNumFrames, kSampleRateHz);
+
+  // Increase gain.
+  agc2_fixed->ApplyConfig(CreateAgc2FixedDigitalModeConfig(kGainDbHigh));
+  static_cast<void>(RunAgc2WithConstantInput(agc2_fixed.get(), kInputLevel,
+                                             kNumFrames, kSampleRateHz));
+
+  // Back to the lower gain.
+  agc2_fixed->ApplyConfig(CreateAgc2FixedDigitalModeConfig(kGainDbLow));
+  const float output_level_post = RunAgc2WithConstantInput(
+      agc2_fixed.get(), kInputLevel, kNumFrames, kSampleRateHz);
+
+  EXPECT_EQ(output_level_pre, output_level_post);
+}
+
+class FixedDigitalTest : public testing::Test,
+                         public testing::WithParamInterface<
+                             std::tuple<float, float, size_t, bool>> {};
+
+TEST_P(FixedDigitalTest, CheckSaturationBehaviorWithLimiter) {
+  const float kInputLevel = 32767.f;
+  const size_t kNumFrames = 5;
+
+  const auto params = GetParam();
+  const size_t gain_db_min = std::get<0>(params);
+  const size_t gain_db_max = std::get<1>(params);
+  const size_t sample_rate = std::get<2>(params);
+  const bool saturation_expected = std::get<3>(params);
+
+  const auto gains_db = test::LinSpace(gain_db_min, gain_db_max, 10);
+  for (const auto gain_db : gains_db) {
+    SCOPED_TRACE(std::to_string(gain_db));
+    auto agc2_fixed = CreateAgc2FixedDigitalMode(gain_db, sample_rate);
+    const float processed_sample = RunAgc2WithConstantInput(
+        agc2_fixed.get(), kInputLevel, kNumFrames, sample_rate);
+    if (saturation_expected) {
+      EXPECT_FLOAT_EQ(processed_sample, 32767.f);
+    } else {
+      EXPECT_LT(processed_sample, 32767.f);
+    }
+  }
+}
+
+static_assert(test::kLimiterMaxInputLevelDbFs < 10, "");
+INSTANTIATE_TEST_CASE_P(
+    GainController2,
+    FixedDigitalTest,
+    ::testing::Values(
+        // When gain < |test::kLimiterMaxInputLevelDbFs|, the limiter will not
+        // saturate the signal (at any sample rate).
+        std::make_tuple(0.1,
+                        test::kLimiterMaxInputLevelDbFs - 0.01,
+                        8000,
+                        false),
+        std::make_tuple(0.1,
+                        test::kLimiterMaxInputLevelDbFs - 0.01,
+                        48000,
+                        false),
+        // When gain > |test::kLimiterMaxInputLevelDbFs|, the limiter will
+        // saturate the signal (at any sample rate).
+        std::make_tuple(test::kLimiterMaxInputLevelDbFs + 0.01, 10, 8000, true),
+        std::make_tuple(test::kLimiterMaxInputLevelDbFs + 0.01,
+                        10,
+                        48000,
+                        true)));
 
 }  // namespace test
 }  // namespace webrtc

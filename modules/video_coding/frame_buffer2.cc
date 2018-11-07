4c48366a1ddaf9caa2ae5d823ec732d8c84ed47e
Ilya Nikolaevskiy <ilnik@webrtc.org>
2018-11-07 14:32:28 +0100

    Clear FrameBuffer if there were no frames received for 10 minutes
    This is a workaround for the case when there are no video frames in a
    call for a very long time, such that RTP timestamps wraparound and
    FrameBuffer can't figure out if the frame is older or newer.
    
    Bug:webrtc:9974
    Change-Id: Ie1eaa4938813dbbd637ddcbe7ff118ead2bfa4a9

--- modules/video_coding/frame_buffer2.cc
+++ modules/video_coding/frame_buffer2.cc
@@ -114,6 +114,9 @@ FrameBuffer::ReturnReason FrameBuffer::NextFrame(
         if (keyframe_required && !frame->is_keyframe())
           continue;
 
+        // TODO(https://bugs.webrtc.org/9974): consider removing this check
+        // as it may make a stream undecodable after a very long delay between
+        // frames.
         if (last_decoded_frame_timestamp_ &&
             AheadOf(*last_decoded_frame_timestamp_, frame->Timestamp())) {
           continue;
@@ -243,6 +246,11 @@ void FrameBuffer::Stop() {
   rtc::CritScope lock(&crit_);
   stopped_ = true;
   new_continuous_frame_event_.Set();
+}
+
+void FrameBuffer::Clear() {
+  rtc::CritScope lock(&crit_);
+  ClearFramesAndHistory();
 }
 
 void FrameBuffer::UpdateRtt(int64_t rtt_ms) {

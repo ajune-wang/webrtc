e67580953694867d209c19ebc74c3785ba7a7191
Johannes Kron <kron@webrtc.org>
2018-11-07 17:18:51 +0100

    Add RTP header extension for HDR metadata
    Bug: webrtc:8651
    Change-Id: I1c956eaac1532ac0d3820084edb4054a4cc9c68d

--- api/rtp_headers.h
+++ api/rtp_headers.h
@@ -16,6 +16,7 @@
 #include <string.h>
 
 #include "api/array_view.h"
+#include "api/video/hdr_metadata.h"
 #include "api/video/video_content_type.h"
 #include "api/video/video_frame_marking.h"
 #include "api/video/video_rotation.h"
@@ -126,6 +127,9 @@ struct RTPHeaderExtension {
   // For identifying the media section used to interpret this RTP packet. See
   // https://tools.ietf.org/html/draft-ietf-mmusic-sdp-bundle-negotiation-38
   Mid mid;
+
+  bool has_hdr_metadata;
+  HdrMetadata hdr_metadata;
 };
 
 struct RTPHeader {

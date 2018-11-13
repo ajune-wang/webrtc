b7e23194f1a7a6fa697afebb273126c724b32284
Ivo Creusen <ivoc@webrtc.org>
2018-11-13 15:30:06 +0100

    Clean up Opus wideband mode experiment.
    This experiment is obsolete now, since the code has been adopted by opus as the default behavior.
    
    Bug: webrtc:8522
    Change-Id: I91b271edd79ede537b22737df05401b684bbf651

--- modules/audio_coding/codecs/opus/audio_encoder_opus.h
+++ modules/audio_coding/codecs/opus/audio_encoder_opus.h
@@ -63,13 +63,6 @@ class AudioEncoderOpusImpl final : public AudioEncoder {
   static absl::optional<int> GetNewComplexity(
       const AudioEncoderOpusConfig& config);
 
-  // Returns OPUS_AUTO if the the current bitrate is above wideband threshold.
-  // Returns empty if it is below, but bandwidth coincides with the desired one.
-  // Otherwise returns the desired bandwidth.
-  static absl::optional<int> GetNewBandwidth(
-      const AudioEncoderOpusConfig& config,
-      OpusEncInst* inst);
-
   using AudioNetworkAdaptorCreator =
       std::function<std::unique_ptr<AudioNetworkAdaptor>(const std::string&,
                                                          RtcEventLog*)>;
@@ -178,8 +171,6 @@ class AudioEncoderOpusImpl final : public AudioEncoder {
   AudioEncoderOpusConfig config_;
   const int payload_type_;
   const bool send_side_bwe_with_overhead_;
-  const bool adjust_bandwidth_;
-  bool bitrate_changed_;
   float packet_loss_rate_;
   const float min_packet_loss_rate_;
   const std::unique_ptr<NewPacketLossRateOptimizer> new_packet_loss_optimizer_;

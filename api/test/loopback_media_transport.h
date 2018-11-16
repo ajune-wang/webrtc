2904abd1453607f985e4b8d52678d7ee67f6d4fb
Piotr (Peter) Slatala <psla@webrtc.org>
2018-11-16 13:14:34 -0800

    Remove legacy SetTargetTransferRateObserver
    Bug: webrtc:9719
    Change-Id: I04e892ce0f2af5c48040dd92ff0701209104fe65

--- api/test/loopback_media_transport.h
+++ api/test/loopback_media_transport.h
@@ -52,11 +52,6 @@ class WrapperMediaTransport : public MediaTransportInterface {
     wrapped_->SetReceiveVideoSink(sink);
   }
 
-  void SetTargetTransferRateObserver(
-      webrtc::TargetTransferRateObserver* observer) override {
-    wrapped_->SetTargetTransferRateObserver(observer);
-  }
-
   void SetMediaTransportStateCallback(
       MediaTransportStateCallback* callback) override {
     wrapped_->SetMediaTransportStateCallback(callback);
@@ -165,9 +160,6 @@ class MediaTransportPair {
     }
 
     void SetReceiveVideoSink(MediaTransportVideoSinkInterface* sink) override {}
-
-    void SetTargetTransferRateObserver(
-        webrtc::TargetTransferRateObserver* observer) override {}
 
     void SetMediaTransportStateCallback(
         MediaTransportStateCallback* callback) override {

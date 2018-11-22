3704f55aa1a23db1011b497e2adcd1bf7bee4d13
Alex Loiko <aleloi@webrtc.org>
2018-11-22 14:44:43 +0100

    Opus multistream.
    DO NOT SUBMIT.
    
    This SHOULD be a backwards-compatible change. It makes WebRTC use the
    Opus multistream decoder for all Opus packets. Single-stream packets
    are a special case of multistream ones (set stream=1). According to my
    understanding of the Opus docs, this should decode the single-stream
    packets to the same output as before.
    
    Uploaded just for running the trybots.
    
    Change-Id: I271de8e387d738254d6aa53af7fcf8644a53edb5

--- modules/audio_coding/codecs/opus/opus_interface.c
+++ modules/audio_coding/codecs/opus/opus_interface.c
@@ -274,8 +274,17 @@ int16_t WebRtcOpus_DecoderCreate(OpusDecInst** inst, size_t channels) {
       return -1;
     }
 
+    unsigned char mapping[255];
+    for (int i=0; i < 255; ++i) {
+      mapping[i] = 0;
+    }
     /* Create new memory, always at 48000 Hz. */
-    state->decoder = opus_decoder_create(48000, (int)channels, &error);
+    state->decoder = opus_multistream_decoder_create(
+        48000, (int)channels,
+        /* streams = */ 1,
+        /* coupled streams = */ channels == 2 ? 1 : 0,
+        mapping,
+        &error);
     if (error == OPUS_OK && state->decoder != NULL) {
       /* Creation of memory all ok. */
       state->channels = channels;
@@ -287,7 +296,7 @@ int16_t WebRtcOpus_DecoderCreate(OpusDecInst** inst, size_t channels) {
 
     /* If memory allocation was unsuccessful, free the entire state. */
     if (state->decoder) {
-      opus_decoder_destroy(state->decoder);
+      opus_multistream_decoder_destroy(state->decoder);
     }
     free(state);
   }
@@ -296,7 +305,7 @@ int16_t WebRtcOpus_DecoderCreate(OpusDecInst** inst, size_t channels) {
 
 int16_t WebRtcOpus_DecoderFree(OpusDecInst* inst) {
   if (inst) {
-    opus_decoder_destroy(inst->decoder);
+    opus_multistream_decoder_destroy(inst->decoder);
     free(inst);
     return 0;
   } else {
@@ -309,7 +318,7 @@ size_t WebRtcOpus_DecoderChannels(OpusDecInst* inst) {
 }
 
 void WebRtcOpus_DecoderInit(OpusDecInst* inst) {
-  opus_decoder_ctl(inst->decoder, OPUS_RESET_STATE);
+  opus_multistream_decoder_ctl(inst->decoder, OPUS_RESET_STATE);
   inst->in_dtx_mode = 0;
 }
 
@@ -338,8 +347,9 @@ static int16_t DetermineAudioType(OpusDecInst* inst, size_t encoded_bytes) {
 static int DecodeNative(OpusDecInst* inst, const uint8_t* encoded,
                         size_t encoded_bytes, int frame_size,
                         int16_t* decoded, int16_t* audio_type, int decode_fec) {
-  int res = opus_decode(inst->decoder, encoded, (opus_int32)encoded_bytes,
-                        (opus_int16*)decoded, frame_size, decode_fec);
+  int res = opus_multistream_decode(
+      inst->decoder, encoded, (opus_int32)encoded_bytes,
+      (opus_int16*)decoded, frame_size, decode_fec);
 
   if (res <= 0)
     return -1;

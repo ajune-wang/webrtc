01b0af1a0921d4a0b547756157ecad0270d77780
Niels Möller <nisse@webrtc.org>
2018-11-23 15:45:39 +0100

    Move RtcpStatistics from common_types.h to a new header file
    New location is modules/rtp_rtcp/include/rtcp_statistics.h.
    
    Bug: webrtc:5876
    Change-Id: I85f55b58658588228ed77175226b3479352fd5de

---
+++ modules/rtp_rtcp/include/rtcp_statistics.h
@@ -0,0 +1,42 @@
+/*
+ *  Copyright (c) 2018 The WebRTC project authors. All Rights Reserved.
+ *
+ *  Use of this source code is governed by a BSD-style license
+ *  that can be found in the LICENSE file in the root of the source
+ *  tree. An additional intellectual property rights grant can be found
+ *  in the file PATENTS.  All contributing project authors may
+ *  be found in the AUTHORS file in the root of the source tree.
+ */
+
+#ifndef MODULES_RTP_RTCP_INCLUDE_RTCP_STATISTICS_H_
+#define MODULES_RTP_RTCP_INCLUDE_RTCP_STATISTICS_H_
+
+#include <stdint.h>
+
+namespace webrtc {
+
+// Statistics for an RTCP channel
+struct RtcpStatistics {
+  RtcpStatistics()
+      : fraction_lost(0),
+        packets_lost(0),
+        extended_highest_sequence_number(0),
+        jitter(0) {}
+
+  uint8_t fraction_lost;
+  int32_t packets_lost;  // Defined as a 24 bit signed integer in RTCP
+  uint32_t extended_highest_sequence_number;
+  uint32_t jitter;
+};
+
+class RtcpStatisticsCallback {
+ public:
+  virtual ~RtcpStatisticsCallback() {}
+
+  virtual void StatisticsUpdated(const RtcpStatistics& statistics,
+                                 uint32_t ssrc) = 0;
+  virtual void CNameChanged(const char* cname, uint32_t ssrc) = 0;
+};
+
+}  // namespace webrtc
+#endif  // MODULES_RTP_RTCP_INCLUDE_RTCP_STATISTICS_H_

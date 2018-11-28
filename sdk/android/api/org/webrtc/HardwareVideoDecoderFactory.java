4d5ecab9bb51fa5af53c50523906d863abd657b4
Yura Yaroshevich <yura.yaroshevich@gmail.com>
2018-11-28 13:00:46 +0300

    Added support of user-defined blacklist of video codec implementations.
    Ability to provide user defined predicate to disable particular
    codec in particular circumstances was added. This could help
    addressing mysterious crashes on specific Android devices.
    
    Bug: webrtc:10029
    Change-Id: I7ad81f4b1351aa68f036c0ee3b6d32fbf0f697ed

--- sdk/android/api/org/webrtc/HardwareVideoDecoderFactory.java
+++ sdk/android/api/org/webrtc/HardwareVideoDecoderFactory.java
@@ -10,10 +10,29 @@
 
 package org.webrtc;
 
+import android.media.MediaCodecInfo;
+import java.util.Arrays;
 import javax.annotation.Nullable;
 
 /** Factory for Android hardware VideoDecoders. */
 public class HardwareVideoDecoderFactory extends MediaCodecVideoDecoderFactory {
+  private final static Predicate<MediaCodecInfo> defaultAllowedPredicate =
+      new Predicate<MediaCodecInfo>() {
+        private String[] prefixBlacklist =
+            Arrays.copyOf(MediaCodecUtils.SOFTWARE_IMPLEMENTATION_PREFIXES,
+                MediaCodecUtils.SOFTWARE_IMPLEMENTATION_PREFIXES.length);
+        @Override
+        public boolean test(MediaCodecInfo arg) {
+          final String name = arg.getName();
+          for (String prefix : prefixBlacklist) {
+            if (name.startsWith(prefix)) {
+              return true;
+            }
+          }
+          return false;
+        }
+      }.negate();
+
   /** Creates a HardwareVideoDecoderFactory that does not use surface textures. */
   @Deprecated // Not removed yet to avoid breaking callers.
   public HardwareVideoDecoderFactory() {
@@ -27,7 +46,20 @@ public class HardwareVideoDecoderFactory extends MediaCodecVideoDecoderFactory {
    *                      this disables texture support.
    */
   public HardwareVideoDecoderFactory(@Nullable EglBase.Context sharedContext) {
-    super(sharedContext, /* prefixWhitelist= */ new String[] {""},
-        /* prefixBlacklist= */ MediaCodecUtils.SOFTWARE_IMPLEMENTATION_PREFIXES);
+    super(sharedContext, defaultAllowedPredicate);
+  }
+
+  /**
+   * Creates a HardwareVideoDecoderFactory that supports surface texture rendering.
+   *
+   * @param sharedContext The textures generated will be accessible from this context. May be null,
+   *                      this disables texture support.
+   * @param codecAllowedPredicate predicate to filter codecs.
+   */
+  public HardwareVideoDecoderFactory(@Nullable EglBase.Context sharedContext,
+      @Nullable Predicate<MediaCodecInfo> codecAllowedPredicate) {
+    super(sharedContext,
+        (codecAllowedPredicate == null ? defaultAllowedPredicate
+                                       : codecAllowedPredicate.and(defaultAllowedPredicate)));
   }
 }

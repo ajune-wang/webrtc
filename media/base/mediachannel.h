0abae173f4c766b18a2367c51c445b9151838e73
Sergey Silkin <ssilkin@webrtc.org>
2018-12-18 17:55:28 +0100

    Expose video freeze metrics in getStats.
    Bug: webrtc:10145
    Change-Id: I4c76d5651102e73b1592ffed561e6224f2badeb6

--- media/base/mediachannel.h
+++ media/base/mediachannel.h
@@ -532,6 +532,8 @@ struct VideoReceiverInfo : public MediaReceiverInfo {
   uint32_t frames_rendered = 0;
   absl::optional<uint64_t> qp_sum;
   int64_t interframe_delay_max_ms = -1;
+  uint32_t num_freezes;
+  absl::optional<uint32_t> mean_freeze_duration_ms;
 
   webrtc::VideoContentType content_type = webrtc::VideoContentType::UNSPECIFIED;
 

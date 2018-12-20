12709badaff18e0e064c71f539d2391e1c0f87c0
Harald Alvestrand <hta@webrtc.org>
2018-12-20 11:05:04 +0100

    Add an observer API for DTLSTransport events.
    This wires up the "state change" event and defines an observer
    class that can be used by clients.
    
    Bug: chromium:907849
    Change-Id: I3cba2dc051a56280fb958f139f29cbb0022a39c6

--- api/dtlstransportinterface.h
+++ api/dtlstransportinterface.h
@@ -11,18 +11,50 @@
 #ifndef API_DTLSTRANSPORTINTERFACE_H_
 #define API_DTLSTRANSPORTINTERFACE_H_
 
+#include "api/rtcerror.h"
 #include "rtc_base/refcount.h"
 
 namespace webrtc {
 
+// States of a DTLS transport, corresponding to the JS API specification.
+// http://w3c.github.io/webrtc-pc/#dom-rtcdtlstransportstate
+enum class DtlsTransportState {
+  kNew,         // Has not started negotiating yet.
+  kConnecting,  // In the process of negotiating a secure connection.
+  kConnected,   // Completed negotiation and verified fingerprints.
+  kClosed,      // Intentionally closed.
+  kFailed  // Failure due to an error or not verifying a remote fingerprint.
+};
+
+class DtlsTransportObserverInterface {
+ public:
+  // This callback carries information about the state of the transport.
+  // Called (with state_changed=false) as soon as possible after
+  // the observer is set.
+  // Called subsequently (with state_changed=true) for every change in state
+  // of the DTLSTransport.
+  virtual void OnStateChange(DtlsTransportState state,
+                             bool state_changed,
+                             void* remoteCertificates) = 0;
+  // This callback is called when an error occurs, causing the transport
+  // to go to the kFailed state.
+  virtual void OnError(RTCError error) = 0;
+
+ protected:
+  virtual ~DtlsTransportObserverInterface() = default;
+};
+
 // A DTLS transport, as represented to the outside world.
 // Its role is to report state changes and errors, and make sure information
 // about remote certificates is available.
+// By design, the API has no accessors; all information is carried back
+// through the observer.
+// When the observer is registered, an immediate callback is dispatched
+// with state_changed = false, so that the current state is observable.
 class DtlsTransportInterface : public rtc::RefCountInterface {
  public:
-  // TODO(hta): Need a notifier interface to transmit state changes and
-  // error events. The generic NotifierInterface of mediasteraminterface.h
-  // may be suitable, or may be copyable.
+  virtual void RegisterObserver(DtlsTransportObserverInterface* observer) = 0;
+  virtual void UnregisterObserver() = 0;
 };
 
 }  // namespace webrtc
--- pc/dtlstransport.cc
+++ pc/dtlstransport.cc
@@ -14,10 +14,74 @@
 
 namespace webrtc {
 
+namespace {
+
+DtlsTransportState TranslateState(cricket::DtlsTransportState internal_state) {
+  switch (internal_state) {
+    case cricket::DTLS_TRANSPORT_NEW:
+      return DtlsTransportState::kNew;
+      break;
+    case cricket::DTLS_TRANSPORT_CONNECTING:
+      return DtlsTransportState::kConnecting;
+      break;
+    case cricket::DTLS_TRANSPORT_CONNECTED:
+      return DtlsTransportState::kConnected;
+      break;
+    case cricket::DTLS_TRANSPORT_CLOSED:
+      return DtlsTransportState::kClosed;
+      break;
+    case cricket::DTLS_TRANSPORT_FAILED:
+      return DtlsTransportState::kFailed;
+      break;
+  }
+}
+
+}  // namespace
+
 DtlsTransport::DtlsTransport(
     std::unique_ptr<cricket::DtlsTransportInternal> internal)
-    : internal_dtls_transport_(std::move(internal)) {
+    : thread_owning_internal_(rtc::Thread::Current()),
+      internal_dtls_transport_(std::move(internal)) {
   RTC_DCHECK(internal_dtls_transport_.get());
+  internal_dtls_transport_->SignalDtlsState.connect(
+      this, &DtlsTransport::OnInternalDtlsState);
+}
+
+void DtlsTransport::OnInternalDtlsState(
+    cricket::DtlsTransportInternal* transport,
+    cricket::DtlsTransportState state) {
+  RTC_DCHECK(transport == internal());
+  DispatchStateChange(true);
+}
+
+void DtlsTransport::RegisterObserver(DtlsTransportObserverInterface* observer) {
+  RTC_DCHECK(observer);
+  observer_ = observer;
+  if (!thread_owning_internal_->IsCurrent()) {
+    invoker_.AsyncInvoke<void>(
+        RTC_FROM_HERE, thread_owning_internal_,
+        Bind(&DtlsTransport::DispatchStateChange, this, false));
+    return;
+  }
+  // need to dispatch to signalling thread to safely access internal
+  DispatchStateChange(false);
+}
+
+void DtlsTransport::UnregisterObserver() {
+  observer_ = nullptr;
+}
+
+void DtlsTransport::DispatchStateChange(bool real_change) {
+  RTC_DCHECK(thread_owning_internal_->IsCurrent());
+  if (observer_) {
+    if (internal()) {
+      observer_->OnStateChange(TranslateState(internal()->dtls_state()),
+                               real_change, nullptr);
+    } else {
+      observer_->OnStateChange(DtlsTransportState::kClosed, real_change,
+                               nullptr);
+    }
+  }
 }
 
 }  // namespace webrtc

89057760bc303794a16dbb22cf0f760611df953f
Anders Carlsson <andersc@webrtc.org>
2018-12-20 14:47:50 +0100

    Remove legacy video codec factories.
    Removes the deprecated video codec factories and the related flag and
    helper classes.
    
    Bug: webrtc:7925
    Change-Id: I0a6d1666ece9ad074fefc79b626ba241765e1b98

--- pc/peerconnectionfactory.cc
+++ pc/peerconnectionfactory.cc
@@ -34,8 +34,6 @@
 // TODO(zhihuang): This wouldn't be necessary if the interface and
 // implementation of the media engine were in separate build targets.
 #include "media/engine/webrtcmediaengine.h"             // nogncheck
-#include "media/engine/webrtcvideodecoderfactory.h"     // nogncheck
-#include "media/engine/webrtcvideoencoderfactory.h"     // nogncheck
 #include "modules/audio_device/include/audio_device.h"  // nogncheck
 #include "modules/congestion_controller/bbr/bbr_factory.h"
 #include "p2p/base/basicpacketsocketfactory.h"

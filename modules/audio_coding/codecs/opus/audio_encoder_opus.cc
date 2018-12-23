76b87d6085fc8210229bc9b6bc86fba0c1e93774
Anton Sukhanov <sukhanov@webrtc.org>
2018-12-22 17:43:31 -0800

    Fix / refactor ANA overhead
    * NOTE: I am still working on tests, but I think it's good for initial review *
    
    Details of the problem are described in webrtc:10150.
    
    In this change:
    - Move ANA overhead estimation from webrtcvoiceengine to audio_send_channel
    - Use currently reported overhead (and reconfigure bitrate allocator on overhead changes)
    - Do not add overhead to config minimum/maximum bitrate. Instead adjust overhead when we reconfigure bitrate allocator.
    - Leave current SendSideBweWithOverhead calculations as is, just moved to audio_send_channel.
    - Add 2 additional trial options for SendSideBweWithOverhead
      * SendSideBweWithOverheadMin
      * SendSideBweWithOverheadMinMax
    
      (see changes in audio_send_stream with details of new trial options)
    
    - Introduce min frame size in encoder API to calcu
    - Fix max frame size for OPUS
    
    I can move encoder/OPUS changes in the separate CL if you think it makes sense.
    
    Bug: webrtc:10150
    
    Change-Id: I4df2ccbfdf90d4dcda400c82d268ffb3ddadbad2

--- modules/audio_coding/codecs/opus/audio_encoder_opus.cc
+++ modules/audio_coding/codecs/opus/audio_encoder_opus.cc
@@ -501,7 +501,15 @@ size_t AudioEncoderOpusImpl::Num10MsFramesInNextPacket() const {
 }
 
 size_t AudioEncoderOpusImpl::Max10MsFramesInAPacket() const {
-  return Num10msFramesPerPacket();
+  return *std::max_element(supported_frame_lengths_ms().begin(),
+                           supported_frame_lengths_ms().end()) /
+         10;
+}
+
+size_t AudioEncoderOpusImpl::Min10MsFramesInAPacket() const {
+  return *std::min_element(supported_frame_lengths_ms().begin(),
+                           supported_frame_lengths_ms().end()) /
+         10;
 }
 
 int AudioEncoderOpusImpl::GetTargetBitrate() const {
@@ -622,14 +630,13 @@ void AudioEncoderOpusImpl::OnReceivedUplinkBandwidth(
     }
     const int overhead_bps = static_cast<int>(
         *overhead_bytes_per_packet_ * 8 * 100 / Num10MsFramesInNextPacket());
-    SetTargetBitrate(
-        std::min(AudioEncoderOpusConfig::kMaxBitrateBps,
-                 std::max(AudioEncoderOpusConfig::kMinBitrateBps,
-                          target_audio_bitrate_bps - overhead_bps)));
+
+    SetTargetBitrate(target_audio_bitrate_bps - overhead_bps);
   } else {
     SetTargetBitrate(target_audio_bitrate_bps);
   }
 }
+
 void AudioEncoderOpusImpl::OnReceivedUplinkBandwidth(
     int target_audio_bitrate_bps,
     absl::optional<int64_t> bwe_period_ms) {
@@ -829,9 +836,9 @@ void AudioEncoderOpusImpl::SetProjectedPacketLossRate(float fraction) {
   }
 }
 
-void AudioEncoderOpusImpl::SetTargetBitrate(int bits_per_second) {
+void AudioEncoderOpusImpl::SetTargetBitrate(int target_encoder_bitrate) {
   config_.bitrate_bps = rtc::SafeClamp<int>(
-      bits_per_second, AudioEncoderOpusConfig::kMinBitrateBps,
+      target_encoder_bitrate, AudioEncoderOpusConfig::kMinBitrateBps,
       AudioEncoderOpusConfig::kMaxBitrateBps);
   RTC_DCHECK(config_.IsOk());
   RTC_CHECK_EQ(0, WebRtcOpus_SetBitRate(inst_, GetBitrateBps(config_)));

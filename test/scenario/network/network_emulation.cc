80fde4e08f6d61da6534973bde4aa79af1cdaf61
Artem Titov <titovartem@google.com>
2019-01-07 15:02:23 +0100

    Extract NetworkNode abstraction.
    Extract NetworkNode abstraction and introduce cleaner structure for emulation based on abstract NetworkBehaviorInterface.
    
    Bug: webrtc:10138
    Change-Id: I89cdae2f3792da34ce169f14592c53515c8ba3ac

--- test/scenario/network/network_emulation.cc
+++ test/scenario/network/network_emulation.cc
@@ -12,7 +12,11 @@
 
 #include <memory>
 
+#include "absl/memory/memory.h"
+#include "rtc_base/logging.h"
+
 namespace webrtc {
+namespace test {
 
 EmulatedIpPacket::EmulatedIpPacket(const rtc::SocketAddress& from,
                                    const rtc::SocketAddress& to,
@@ -29,4 +33,141 @@ EmulatedIpPacket::~EmulatedIpPacket() = default;
 
 EmulatedIpPacket::EmulatedIpPacket(EmulatedIpPacket&&) = default;
 
+NetworkNode::NetworkNode() = default;
+NetworkNode::~NetworkNode() = default;
+
+void NetworkNode::SetReceiver(uint64_t dest_endpoint_id,
+                              EmulatedNetworkReceiverInterface* receiver) {
+  rtc::CritScope crit(&lock_);
+  RTC_CHECK(routing_
+                .insert(std::pair<uint64_t, EmulatedNetworkReceiverInterface*>(
+                    dest_endpoint_id, receiver))
+                .second)
+      << "Such routing already exists";
+}
+
+void NetworkNode::RemoveReceiver(uint64_t dest_endpoint_id) {
+  rtc::CritScope crit(&lock_);
+  routing_.erase(dest_endpoint_id);
+}
+
+void NetworkNode::CreateRoute(uint64_t receiver_id,
+                              std::vector<NetworkNode*> nodes,
+                              EmulatedNetworkReceiverInterface* receiver) {
+  RTC_CHECK(!nodes.empty());
+  for (size_t i = 0; i + 1 < nodes.size(); ++i)
+    nodes[i]->SetReceiver(receiver_id, nodes[i + 1]);
+  nodes.back()->SetReceiver(receiver_id, receiver);
+}
+
+void NetworkNode::ClearRoute(uint64_t receiver_id,
+                             std::vector<NetworkNode*> nodes) {
+  for (NetworkNode* node : nodes)
+    node->RemoveReceiver(receiver_id);
+}
+
+EmulatedNetworkNode::EmulatedNetworkNode(
+    std::unique_ptr<NetworkBehaviorInterface> network_behavior,
+    size_t packet_overhead)
+    : network_behavior_(std::move(network_behavior)),
+      packet_overhead_(packet_overhead) {}
+
+EmulatedNetworkNode::~EmulatedNetworkNode() = default;
+
+EmulatedNetworkNode::StoredPacket::StoredPacket(uint64_t id,
+                                                EmulatedIpPacket packet,
+                                                bool removed)
+    : id(id), packet(std::move(packet)), removed(removed) {}
+EmulatedNetworkNode::StoredPacket::~StoredPacket() = default;
+
+void EmulatedNetworkNode::OnPacketReceived(EmulatedIpPacket packet) {
+  rtc::CritScope crit(&lock_);
+  if (routing_.find(packet.dest_endpoint_id) == routing_.end())
+    return;
+  uint64_t packet_id = next_packet_id_++;
+  bool sent = network_behavior_->EnqueuePacket(PacketInFlightInfo(
+      packet.size() + packet_overhead_, packet.arrival_time.us(), packet_id));
+  if (sent) {
+    packets_.emplace_back(
+        absl::make_unique<StoredPacket>(packet_id, std::move(packet), false));
+  }
+}
+
+void EmulatedNetworkNode::Process(Timestamp cur_time) {
+  std::vector<PacketDeliveryInfo> delivery_infos;
+  {
+    rtc::CritScope crit(&lock_);
+    absl::optional<int64_t> delivery_us =
+        network_behavior_->NextDeliveryTimeUs();
+    if (delivery_us && *delivery_us > cur_time.us())
+      return;
+
+    delivery_infos =
+        network_behavior_->DequeueDeliverablePackets(cur_time.us());
+  }
+  for (PacketDeliveryInfo& delivery_info : delivery_infos) {
+    StoredPacket* packet = nullptr;
+    EmulatedNetworkReceiverInterface* receiver = nullptr;
+    {
+      rtc::CritScope crit(&lock_);
+      for (std::unique_ptr<StoredPacket>& stored_packet : packets_) {
+        if (stored_packet->id == delivery_info.packet_id) {
+          packet = stored_packet.get();
+          break;
+        }
+      }
+      RTC_CHECK(packet);
+      RTC_DCHECK(!packet->removed);
+      receiver = routing_[packet->packet.dest_endpoint_id];
+      packet->removed = true;
+    }
+    RTC_CHECK(receiver);
+    // We don't want to keep the lock here. Otherwise we would get a deadlock if
+    // the receiver tries to push a new packet.
+    packet->packet.arrival_time = Timestamp::us(delivery_info.receive_time_us);
+    receiver->OnPacketReceived(std::move(packet->packet));
+    {
+      rtc::CritScope crit(&lock_);
+      while (!packets_.empty() && packets_.front()->removed) {
+        packets_.pop_front();
+      }
+    }
+  }
+}
+
+TransparentNetworkNode::TransparentNetworkNode() = default;
+TransparentNetworkNode::~TransparentNetworkNode() = default;
+
+void TransparentNetworkNode::OnPacketReceived(EmulatedIpPacket packet) {
+  rtc::CritScope crit(&lock_);
+  if (routing_.find(packet.dest_endpoint_id) == routing_.end()) {
+    RTC_LOG(INFO) << "PACKET DROPPED: No route for endpoint "
+                  << packet.dest_endpoint_id;
+  }
+  packets_.emplace_back(std::move(packet));
+}
+
+void TransparentNetworkNode::Process(Timestamp cur_time) {
+  std::vector<EmulatedIpPacket> to_send;
+  {
+    rtc::CritScope crit(&lock_);
+    while (!packets_.empty()) {
+      to_send.push_back(std::move(packets_.front()));
+      packets_.pop_front();
+    }
+  }
+  for (auto& packet : to_send) {
+    EmulatedNetworkReceiverInterface* receiver = nullptr;
+    {
+      rtc::CritScope crit(&lock_);
+      receiver = routing_[packet.dest_endpoint_id];
+    }
+    RTC_CHECK(receiver);
+    // We don't want to keep the lock here. Otherwise we would get a deadlock if
+    // the receiver tries to push a new packet.
+    receiver->OnPacketReceived(std::move(packet));
+  }
+}
+
+}  // namespace test
 }  // namespace webrtc

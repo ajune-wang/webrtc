0352644abdc4b21ef32d94c1c0a70cc87025037f
Artem Titov <titovartem@google.com>
2019-01-09 13:01:30 +0100

    Introduce EncodedImageIdInjector.
    EncodedImageIdInjector is responsible for injection of frame id into
    encoded image before it will be sent to the transport layer. It will
    help to track video frame from capturing on 1st peer side to rendering
    on 2nd peer side and will make it possible to calculate video quality
    stats between these frames.
    
    This CL also introduces two different implementations for injector:
      1. DefaultEncodedImageIdInjector will prepend all encoded images with
         extra data and then will restore them on another side. This injector
         can work even if peers are running on different devices.
      2. SingleProcessEncodedImageIdInjector can work only when all peers
         are running in the same process, but won't used any extra data
         to propagate frame id between peers, so it won't affect any
         transport level metrics and bitrate estimator.
    
    Bug: webrtc:10138
    Change-Id: I77defc8e8c95cb244a695a9732980a47bd7a2e9b

---
+++ test/pc/e2e/analyzer/video/default_encoded_image_id_injector.cc
@@ -0,0 +1,117 @@
+/*
+ *  Copyright (c) 2018 The WebRTC project authors. All Rights Reserved.
+ *
+ *  Use of this source code is governed by a BSD-style license
+ *  that can be found in the LICENSE file in the root of the source
+ *  tree. An additional intellectual property rights grant can be found
+ *  in the file PATENTS.  All contributing project authors may
+ *  be found in the AUTHORS file in the root of the source tree.
+ */
+
+#include "test/pc/e2e/analyzer/video/default_encoded_image_id_injector.h"
+
+#include <cstddef>
+
+#include "absl/memory/memory.h"
+#include "api/video/encoded_image.h"
+#include "rtc_base/checks.h"
+
+namespace webrtc {
+namespace test {
+namespace {
+// The amount on which encoded image buffer will be expanded to inject frame id.
+// This is 2 bytes for uint16_t frame id itself and 2 bytes for original length
+// of the buffer.
+constexpr int kEncodedImageBufferExpansion = 4;
+constexpr size_t kInitialBufferSize = 2 * 1024;
+constexpr size_t kBuffersPoolSize = 256;
+}  // namespace
+
+DefaultEncodedImageIdInjector::DefaultEncodedImageIdInjector(int bufs_count) {
+  for (int i = 0; i < bufs_count; i++) {
+    bufs_pool_.push_back(std::vector<uint8_t>(kInitialBufferSize));
+  }
+  cur_buffer_ = 0;
+}
+DefaultEncodedImageIdInjector::~DefaultEncodedImageIdInjector() = default;
+
+EncodedImage DefaultEncodedImageIdInjector::InjectId(
+    uint16_t id,
+    const EncodedImage& source) {
+  EncodedImage out = source;
+  std::vector<uint8_t>* buffer = NextBuffer();
+  if (buffer->capacity() < source._length + kEncodedImageBufferExpansion) {
+    buffer->resize(source._length + kEncodedImageBufferExpansion);
+  }
+  out.set_buffer(buffer->data(), buffer->size());
+  out._length = source._length + kEncodedImageBufferExpansion;
+  memcpy(&out._buffer[kEncodedImageBufferExpansion], source._buffer,
+         source._length);
+  out._buffer[0] = id & 0x00ff;
+  out._buffer[1] = (id & 0xff00) >> 8;
+  out._buffer[2] = source._length & 0x00ff;
+  out._buffer[3] = (source._length & 0xff00) >> 8;
+  return out;
+}
+
+std::pair<uint16_t, EncodedImage> DefaultEncodedImageIdInjector::ExtractId(
+    const EncodedImage& source) {
+  EncodedImage out = source;
+  std::vector<uint8_t>* buffer = NextBuffer();
+  if (buffer->capacity() < source.capacity() - kEncodedImageBufferExpansion) {
+    buffer->resize(source.capacity() - kEncodedImageBufferExpansion);
+  }
+  out.set_buffer(buffer->data(), buffer->size());
+
+  size_t source_pos = 0;
+  size_t out_pos = 0;
+  absl::optional<uint16_t> id = absl::nullopt;
+  while (source_pos < source._length) {
+    uint16_t next_id =
+        source._buffer[source_pos] + (source._buffer[source_pos + 1] << 8);
+    RTC_CHECK(!id || id.value() == next_id)
+        << "Different frames encoded into single encoded image: " << id.value()
+        << " vs " << next_id;
+    id = next_id;
+    uint16_t length =
+        source._buffer[source_pos + 2] + (source._buffer[source_pos + 3] << 8);
+    memcpy(&out._buffer[out_pos],
+           &source._buffer[source_pos + kEncodedImageBufferExpansion], length);
+    source_pos += length + kEncodedImageBufferExpansion;
+    out_pos += length;
+  }
+  out._length = out_pos;
+
+  return std::pair<uint16_t, EncodedImage>(id.value(), out);
+}
+
+std::vector<uint8_t>* DefaultEncodedImageIdInjector::NextBuffer() {
+  cur_buffer_ = (cur_buffer_ + 1) % bufs_pool_.size();
+  return &bufs_pool_.at(cur_buffer_);
+}
+
+DefaultQualityAnalyzingVideoContext::DefaultQualityAnalyzingVideoContext() =
+    default;
+DefaultQualityAnalyzingVideoContext::~DefaultQualityAnalyzingVideoContext() =
+    default;
+
+EncodedImageIdInjector* DefaultQualityAnalyzingVideoContext::GetIdInjector(
+    int coding_entity_id,
+    CodingType type) {
+  rtc::CritScope crit(&lock_);
+  auto it = injectors_.find(std::pair<int, CodingType>(coding_entity_id, type));
+  if (it == injectors_.end()) {
+    auto injector =
+        absl::make_unique<DefaultEncodedImageIdInjector>(kBuffersPoolSize);
+    EncodedImageIdInjector* out = injector.get();
+    injectors_.insert(std::pair<std::pair<int, CodingType>,
+                                std::unique_ptr<EncodedImageIdInjector>>(
+        std::pair<int, CodingType>(coding_entity_id, type),
+        std::move(injector)));
+    return out;
+  }
+  return it->second.get();
+}
+
+}  // namespace test
+}  // namespace webrtc

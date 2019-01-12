b23af6a0cda22154884d5f12ad487a530afa413f
Artem Titov <titovartem@google.com>
2019-01-12 12:35:12 +0100

    Introduce EndpointNode.
    EnpointNode represents a network interface on the machine. It will
    deliver data via emulated network nodes. Client can read or write
    data to/from the EndpointNode with FakeNetworkSocket.
    
    Further abstractions that will provide factory interfaces for
    creating sockets will be introduced in next CLs.
    
    Bug: webrtc:10138
    Change-Id: I691307235d823914614cd61c34de0f188cac410d

---
+++ test/scenario/repeated_activity.cc
@@ -0,0 +1,43 @@
+/*
+ *  Copyright 2018 The WebRTC project authors. All Rights Reserved.
+ *
+ *  Use of this source code is governed by a BSD-style license
+ *  that can be found in the LICENSE file in the root of the source
+ *  tree. An additional intellectual property rights grant can be found
+ *  in the file PATENTS.  All contributing project authors may
+ *  be found in the AUTHORS file in the root of the source tree.
+ */
+
+#include "test/scenario/repeated_activity.h"
+
+namespace webrtc {
+namespace test {
+
+RepeatedActivity::RepeatedActivity(TimeDelta interval,
+                                   std::function<void(TimeDelta)> function)
+    : interval_(interval), function_(function) {}
+RepeatedActivity::~RepeatedActivity() = default;
+
+void RepeatedActivity::Stop() {
+  interval_ = TimeDelta::PlusInfinity();
+}
+
+void RepeatedActivity::Poll(Timestamp time) {
+  RTC_DCHECK(last_update_.IsFinite());
+  if (time >= last_update_ + interval_) {
+    function_(time - last_update_);
+    last_update_ = time;
+  }
+}
+
+void RepeatedActivity::SetStartTime(Timestamp time) {
+  last_update_ = time;
+}
+
+Timestamp RepeatedActivity::NextTime() {
+  RTC_DCHECK(last_update_.IsFinite());
+  return last_update_ + interval_;
+}
+
+}  // namespace test
+}  // namespace webrtc

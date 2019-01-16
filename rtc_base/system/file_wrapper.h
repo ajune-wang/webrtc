8df47169adbd5d67e3aad90d498f64967638c07a
Niels Möller <nisse@webrtc.org>
2019-01-16 14:33:14 +0100

    Trim down FileWrapper class to be merely a wrapper owning a FILE*
    Bug: webrtc:6463
    Change-Id: If71e2f3a75dc1863bc805ab71de1e2d33294f805

--- rtc_base/system/file_wrapper.h
+++ rtc_base/system/file_wrapper.h
@@ -23,14 +23,16 @@ namespace webrtc {
 // TODO(tommi): Rename to rtc::File and move to base.
 class FileWrapper final {
  public:
-  static const size_t kMaxFileNameSize = 1024;
-
-  // Factory methods.
-  // TODO(tommi): Remove Create().
-  static FileWrapper* Create();
+  // Opens a file in read or write mode, decided by the read_only parameter.
   static FileWrapper Open(const char* file_name_utf8, bool read_only);
 
-  FileWrapper(FILE* file, size_t max_size);
+  FileWrapper();
+
+  // Initializes the wrapper from an existing FILE*.  The wrapper
+  // takes ownership of |handle|.
+  bool OpenFromFileHandle(FILE* handle);
+
+  explicit FileWrapper(FILE* file);
   ~FileWrapper();
 
   // Support for move semantics.
@@ -38,42 +40,21 @@ class FileWrapper final {
   FileWrapper& operator=(FileWrapper&& other);
 
   // Returns true if a file has been opened.
+  // TODO(nisse): Support implicit conversion to bool?
   bool is_open() const { return file_ != nullptr; }
 
-  // Opens a file in read or write mode, decided by the read_only parameter.
-  bool OpenFile(const char* file_name_utf8, bool read_only);
-
-  // Initializes the wrapper from an existing handle.  The wrapper
-  // takes ownership of |handle| and closes it in CloseFile().
-  bool OpenFromFileHandle(FILE* handle);
-
-  void CloseFile();
-
-  // Limits the file size to |bytes|. Writing will fail after the cap
-  // is hit. Pass zero to use an unlimited size.
-  // TODO(tommi): Could we move this out into a separate class?
-  void SetMaxFileSize(size_t bytes);
+  void Close();
 
   // Flush any pending writes.  Note: Flushing when closing, is not required.
-  int Flush();
+  bool Flush();
 
   // Rewinds the file to the start.
-  int Rewind();
+  bool Rewind();
   int Read(void* buf, size_t length);
   bool Write(const void* buf, size_t length);
 
  private:
-  FileWrapper();
-
-  void CloseFileImpl();
-  int FlushImpl();
-
-  // TODO(tommi): Remove the lock.
-  rtc::CriticalSection lock_;
-
   FILE* file_ = nullptr;
-  size_t position_ = 0;
-  size_t max_size_in_bytes_ = 0;
 
   // Copying is not supported.
   FileWrapper(const FileWrapper&) = delete;

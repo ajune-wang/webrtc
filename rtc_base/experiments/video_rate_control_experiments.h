7aeace753d43f050b8b7dda2f70416f7661ed8d4
Erik Språng <sprang@webrtc.org>
2019-01-18 17:20:35 +0100

    Move congestion window field trial parsing to new class.
    This cl is part of work to move several experiments into a joint
    experiment group. Most of them vill be ralted to video, hence the name.
    
    Bug: webrtc:10223
    Change-Id: I8767c43abb6aa910ab51710eeb908e0f9df1e296

---
+++ rtc_base/experiments/video_rate_control_experiments.h
@@ -0,0 +1,45 @@
+/*
+ *  Copyright (c) 2019 The WebRTC project authors. All Rights Reserved.
+ *
+ *  Use of this source code is governed by a BSD-style license
+ *  that can be found in the LICENSE file in the root of the source
+ *  tree. An additional intellectual property rights grant can be found
+ *  in the file PATENTS.  All contributing project authors may
+ *  be found in the AUTHORS file in the root of the source tree.
+ */
+
+#ifndef RTC_BASE_EXPERIMENTS_VIDEO_RATE_CONTROL_EXPERIMENTS_H_
+#define RTC_BASE_EXPERIMENTS_VIDEO_RATE_CONTROL_EXPERIMENTS_H_
+
+#include "absl/types/optional.h"
+#include "api/transport/webrtc_key_value_config.h"
+#include "rtc_base/experiments/field_trial_parser.h"
+
+namespace webrtc {
+
+class VideoRateControlExperiments final {
+ public:
+  ~VideoRateControlExperiments();
+  VideoRateControlExperiments(VideoRateControlExperiments&&);
+
+  static VideoRateControlExperiments ParseFromFieldTrials();
+  static VideoRateControlExperiments ParseFromKeyValueConfig(
+      const WebRtcKeyValueConfig* const key_value_config);
+
+  // When CongestionWindowPushback is enabled, the pacer is oblivious to
+  // the congestion window. The relation between outstanding data and
+  // the congestion window affects encoder allocations directly.
+  absl::optional<int64_t> GetCongestionWindowParameter() const;
+  absl::optional<uint32_t> GetCongestionWindowPushbackParameter() const;
+
+ private:
+  VideoRateControlExperiments(
+      const WebRtcKeyValueConfig* const key_value_config);
+
+  FieldTrialOptional<int> congestion_window_;
+  FieldTrialOptional<int> congestion_window_pushback_;
+};
+
+}  // namespace webrtc
+
+#endif  // RTC_BASE_EXPERIMENTS_VIDEO_RATE_CONTROL_EXPERIMENTS_H_

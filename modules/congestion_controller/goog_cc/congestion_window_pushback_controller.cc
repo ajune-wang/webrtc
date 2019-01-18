da273e28fc925e850394e9a45f58814cdfaec108
Erik Språng <sprang@webrtc.org>
2019-01-18 17:36:22 +0100

    Move congestion window field trial parsing to new class.
    This cl is part of work to move several experiments into a joint
    experiment group. Most of them vill be ralted to video, hence the name.
    
    Bug: webrtc:10223
    Change-Id: I8767c43abb6aa910ab51710eeb908e0f9df1e296

--- modules/congestion_controller/goog_cc/congestion_window_pushback_controller.cc
+++ modules/congestion_controller/goog_cc/congestion_window_pushback_controller.cc
@@ -15,33 +15,12 @@
 
 #include "modules/congestion_controller/goog_cc/congestion_window_pushback_controller.h"
 #include "rtc_base/checks.h"
+#include "rtc_base/experiments/video_rate_control_experiments.h"
 
 namespace webrtc {
 
 namespace {
-
-// When CongestionWindowPushback is enabled, the pacer is oblivious to
-// the congestion window. The relation between outstanding data and
-// the congestion window affects encoder allocations directly.
-// This experiment is build on top of congestion window experiment.
-const char kCongestionPushbackExperiment[] = "WebRTC-CongestionWindowPushback";
 const uint32_t kDefaultMinPushbackTargetBitrateBps = 30000;
-
-bool ReadCongestionWindowPushbackExperimentParameter(
-    const WebRtcKeyValueConfig* key_value_config,
-    uint32_t* min_pushback_target_bitrate_bps) {
-  RTC_DCHECK(min_pushback_target_bitrate_bps);
-  std::string experiment_string =
-      key_value_config->Lookup(kCongestionPushbackExperiment);
-  int parsed_values = sscanf(experiment_string.c_str(), "Enabled-%" PRIu32,
-                             min_pushback_target_bitrate_bps);
-  if (parsed_values == 1) {
-    RTC_CHECK_GE(*min_pushback_target_bitrate_bps, 0)
-        << "Min pushback target bitrate must be greater than or equal to 0.";
-    return true;
-  }
-  return false;
-}
 
 }  // namespace
 
@@ -49,12 +28,11 @@ CongestionWindowPushbackController::CongestionWindowPushbackController(
     const WebRtcKeyValueConfig* key_value_config)
     : add_pacing_(
           key_value_config->Lookup("WebRTC-AddPacingToCongestionWindowPushback")
-              .find("Enabled") == 0) {
-  if (!ReadCongestionWindowPushbackExperimentParameter(
-          key_value_config, &min_pushback_target_bitrate_bps_)) {
-    min_pushback_target_bitrate_bps_ = kDefaultMinPushbackTargetBitrateBps;
-  }
-}
+              .find("Enabled") == 0),
+      min_pushback_target_bitrate_bps_(
+          VideoRateControlExperiments::ParseFromKeyValueConfig(key_value_config)
+              .GetCongestionWindowPushbackParameter()
+              .value_or(kDefaultMinPushbackTargetBitrateBps)) {}
 
 CongestionWindowPushbackController::CongestionWindowPushbackController(
     const WebRtcKeyValueConfig* key_value_config,

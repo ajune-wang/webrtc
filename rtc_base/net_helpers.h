1eb3accf435015a074db59f6c02dcdc438f4177a
Karl Wiberg <kwiberg@webrtc.org>
2019-01-23 00:19:58 +0100

    Break a bunch of threading files out of the monolithic rtc_base build target
    Bug: webrtc:9987
    Change-Id: I9fd82e026c112f3443ad3aafc0398b6bd82fe9de

--- rtc_base/net_helpers.h
+++ rtc_base/net_helpers.h
@@ -19,37 +19,7 @@
 
 #include <vector>
 
-#include "rtc_base/async_resolver_interface.h"
-#include "rtc_base/ip_address.h"
-#include "rtc_base/signal_thread.h"
-#include "rtc_base/socket_address.h"
-
 namespace rtc {
-
-// AsyncResolver will perform async DNS resolution, signaling the result on
-// the SignalDone from AsyncResolverInterface when the operation completes.
-class AsyncResolver : public SignalThread, public AsyncResolverInterface {
- public:
-  AsyncResolver();
-  ~AsyncResolver() override;
-
-  void Start(const SocketAddress& addr) override;
-  bool GetResolvedAddress(int family, SocketAddress* addr) const override;
-  int GetError() const override;
-  void Destroy(bool wait) override;
-
-  const std::vector<IPAddress>& addresses() const { return addresses_; }
-  void set_error(int error) { error_ = error; }
-
- protected:
-  void DoWork() override;
-  void OnWorkDone() override;
-
- private:
-  SocketAddress addr_;
-  std::vector<IPAddress> addresses_;
-  int error_;
-};
 
 // rtc namespaced wrappers for inet_ntop and inet_pton so we can avoid
 // the windows-native versions of these.

bde90f93f39b1a45f043a45404270132f7deadc7
Danil Chapovalov <danilchap@webrtc.org>
2019-01-22 15:36:46 +0100

    Publish task queue test suite.
    The set of tests is a copy of rtc_base/task_queue_unittests excluding tests
    that verify rtc::NewClosure rather than task queue implementation itself.
    
    Bug: webrtc:10191
    Change-Id: I94e962ad63ff6510c43a97ef0cd4da7d08f25538

---
+++ api/task_queue/task_queue_test.h
@@ -0,0 +1,37 @@
+/*
+ *  Copyright 2019 The WebRTC Project Authors. All rights reserved.
+ *
+ *  Use of this source code is governed by a BSD-style license
+ *  that can be found in the LICENSE file in the root of the source
+ *  tree. An additional intellectual property rights grant can be found
+ *  in the file PATENTS.  All contributing project authors may
+ *  be found in the AUTHORS file in the root of the source tree.
+ */
+#ifndef API_TASK_QUEUE_TASK_QUEUE_TEST_H_
+#define API_TASK_QUEUE_TASK_QUEUE_TEST_H_
+
+#include "api/task_queue/task_queue_factory.h"
+#include "test/gtest.h"
+
+namespace webrtc {
+
+// Suite of tests to verify TaskQueue implementation with.
+// Example usage:
+//
+// namespace {
+//
+// using ::webrtc::TaskQueueTest;
+//
+// webrtc::TaskQueueFactory* Factory() {
+//   static std::unique_ptr<webrtc::TaskQueueFactory> factory =
+//       CreateMineTaskQueueFactory();
+//   return factory.get();
+// }
+// INSTANTIATE_TEST_SUITE_P(Mine, TaskQueueTest, ::testing::Values(Factory()));
+//
+// }  // namespace
+class TaskQueueTest : public ::testing::TestWithParam<TaskQueueFactory*> {};
+
+}  // namespace webrtc
+
+#endif  // API_TASK_QUEUE_TASK_QUEUE_TEST_H_

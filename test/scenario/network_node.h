f1f5886997473295c8a2e0b160047629a28ce941
Artem Titov <titovartem@google.com>
2019-01-22 15:45:09 +0100

    Introduce EndpointNode.
    EnpointNode represents a network interface on the machine. It will
    deliver data via emulated network nodes. Client can read or write
    data to/from the EndpointNode with FakeNetworkSocket.
    
    Further abstractions that will provide factory interfaces for
    creating sockets will be introduced in next CLs.
    
    Bug: webrtc:10138
    Change-Id: I691307235d823914614cd61c34de0f188cac410d

--- test/scenario/network_node.h
+++ test/scenario/network_node.h
@@ -46,27 +46,34 @@ class ActionReceiver : public EmulatedNetworkReceiverInterface {
 
 // SimulationNode is a EmulatedNetworkNode that expose an interface for changing
 // run time behavior of the underlying simulation.
-class SimulationNode : public EmulatedNetworkNode {
+class SimulationNode {
  public:
   void UpdateConfig(std::function<void(NetworkNodeConfig*)> modifier);
   void PauseTransmissionUntil(Timestamp until);
   ColumnPrinter ConfigPrinter() const;
+  EmulatedNetworkNode* node() const;
 
  private:
   friend class Scenario;
 
   SimulationNode(NetworkNodeConfig config,
-                 std::unique_ptr<NetworkBehaviorInterface> behavior,
+                 EmulatedNetworkNode* node,
                  SimulatedNetwork* simulation);
-  static std::unique_ptr<SimulationNode> Create(NetworkNodeConfig config);
+  static SimulatedNetwork::Config CreateSimulationConfig(
+      NetworkNodeConfig config);
 
   SimulatedNetwork* const simulated_network_;
   NetworkNodeConfig config_;
+  EmulatedNetworkNode* const node_;
 };
 
-class NetworkNodeTransport : public Transport {
+class NetworkNodeTransport : public Transport, public sigslot::has_slots<> {
  public:
-  NetworkNodeTransport(const Clock* sender_clock, Call* sender_call);
+  NetworkNodeTransport(
+      const Clock* sender_clock,
+      Call* sender_call,
+      rtc::SocketFactory* socket_factory,
+      std::function<void(rtc::CopyOnWriteBuffer, int64_t timestamp)> receiver);
   ~NetworkNodeTransport() override;
 
   bool SendRtp(const uint8_t* packet,
@@ -74,48 +81,41 @@ class NetworkNodeTransport : public Transport {
                const PacketOptions& options) override;
   bool SendRtcp(const uint8_t* packet, size_t length) override;
 
-  void Connect(EmulatedNetworkNode* send_node,
-               uint64_t receiver_id,
-               DataSize packet_overhead);
+  rtc::SocketAddress local_address() const;
+  void OnPacketReceived(rtc::AsyncSocket* socket);
+
+  // Creates and binds local socket to specified IP address
+  void Bind(EndpointNode* local_endpoint, EndpointNode* remote_endpoint);
+
+  // Connected created local socket to specified remote address.
+  void Connect(rtc::SocketAddress remote_addr, DataSize packet_overhead);
 
   DataSize packet_overhead() {
-    rtc::CritScope crit(&crit_sect_);
+    rtc::CritScope crit(&lock_);
     return packet_overhead_;
   }
 
  private:
-  rtc::CriticalSection crit_sect_;
+  rtc::CriticalSection lock_;
+
+  rtc::SocketFactory* const socket_factory_;
+  std::function<void(rtc::CopyOnWriteBuffer, int64_t timestamp)> receiver_;
   const Clock* const sender_clock_;
   Call* const sender_call_;
-  EmulatedNetworkNode* send_net_ RTC_GUARDED_BY(crit_sect_) = nullptr;
-  uint64_t receiver_id_ RTC_GUARDED_BY(crit_sect_) = 0;
-  DataSize packet_overhead_ RTC_GUARDED_BY(crit_sect_) = DataSize::Zero();
+
+  // Sockets to send outgoing packets and receive incoming. Current operational
+  // one is the last one. The rest are kept to receive the data, that can still
+  // be in the network.
+  std::vector<rtc::AsyncSocket*> sockets_ RTC_GUARDED_BY(lock_);
+  // Transport keep connection between sockets, that are binded to endpoints.
+  // If current socket is used for connection from endpoint A to B, we
+  // should reuse it for connection from B to A. Otherwise we should create
+  // new socket.
+  absl::optional<EndpointNode*> current_local_endpoint_ = absl::nullopt;
+  absl::optional<EndpointNode*> current_remote_endpoint_ = absl::nullopt;
+  DataSize packet_overhead_ RTC_GUARDED_BY(lock_) = DataSize::Zero();
 };
 
-// CrossTrafficSource is created by a Scenario and generates cross traffic. It
-// provides methods to access and print internal state.
-class CrossTrafficSource {
- public:
-  DataRate TrafficRate() const;
-  ColumnPrinter StatsPrinter();
-  ~CrossTrafficSource();
-
- private:
-  friend class Scenario;
-  CrossTrafficSource(EmulatedNetworkReceiverInterface* target,
-                     uint64_t receiver_id,
-                     CrossTrafficConfig config);
-  void Process(Timestamp at_time, TimeDelta delta);
-
-  EmulatedNetworkReceiverInterface* const target_;
-  const uint64_t receiver_id_;
-  CrossTrafficConfig config_;
-  webrtc::Random random_;
-
-  TimeDelta time_since_update_ = TimeDelta::Zero();
-  double intensity_ = 0;
-  DataSize pending_size_ = DataSize::Zero();
-};
 }  // namespace test
 }  // namespace webrtc
 #endif  // TEST_SCENARIO_NETWORK_NODE_H_

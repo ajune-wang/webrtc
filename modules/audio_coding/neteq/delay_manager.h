faed2c2f20f9f3f5bd3958f61225884a0e75e41b
Jakob Ivarsson <jakobi@webrtc.org>
2019-01-25 15:26:59 +0100

    Prevent updating state in the delay manager if the packet was reordered.
    Currently, if the last packet was reordered (e.g. due to retransmission) then the next packet's inter-arrival time will be estimated to 0 due to the jump in sequence numbers. This change prevents that by not resetting the stopwatch on reordered packets.
    
    This will also better estimate inter-arrival times when we have multiple reordered packets in a burst. Currently we would only measure the iat of the first reordered packet and not the ones coming after it.
    
    Change-Id: Ic938a37cbddf1cb9c30b610218f56794568d3d01

--- modules/audio_coding/neteq/delay_manager.h
+++ modules/audio_coding/neteq/delay_manager.h
@@ -36,6 +36,7 @@ class DelayManager {
   // PeakDetector object to the DelayManager.
   DelayManager(size_t max_packets_in_buffer,
                int base_min_target_delay_ms,
+               bool handle_rtx,
                DelayPeakDetector* peak_detector,
                const TickTimer* tick_timer);
 
@@ -170,6 +171,7 @@ class DelayManager {
   int last_pack_cng_or_dtmf_;
   const bool frame_length_change_experiment_;
   const absl::optional<int> forced_limit_probability_;
+  const bool handle_rtx_;
 
   RTC_DISALLOW_COPY_AND_ASSIGN(DelayManager);
 };

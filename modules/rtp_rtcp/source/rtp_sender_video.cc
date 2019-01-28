7a90bab79c4bfd2fc6037c2055dd8a314b247c68
Niels Möller <nisse@webrtc.org>
2019-01-28 11:51:24 +0100

    Add is_fec property to RtpPacketToSend
    Use instead of checking the packet's payload type.
    
    Bug: webrtc:7135
    Change-Id: I272922a7879ef3e5e1344ce49044688572b9d942

--- modules/rtp_rtcp/source/rtp_sender_video.cc
+++ modules/rtp_rtcp/source/rtp_sender_video.cc
@@ -227,6 +227,7 @@ void RTPSenderVideo::SendVideoPacketAsRedMaybeWithUlpfec(
     // Only protect while creating RED and FEC packets, not when sending.
     rtc::CritScope cs(&crit_);
     red_packet->SetPayloadType(red_payload_type_);
+    red_packet->set_is_fec(true);
     if (ulpfec_enabled()) {
       if (protect_media_packet) {
         ulpfec_generator_.AddRtpPacketAndGenerateFec(
@@ -261,6 +262,7 @@ void RTPSenderVideo::SendVideoPacketAsRedMaybeWithUlpfec(
         new RtpPacketToSend(*media_packet));
     RTC_CHECK(rtp_packet->Parse(fec_packet->data(), fec_packet->length()));
     rtp_packet->set_capture_time_ms(media_packet->capture_time_ms());
+    rtp_packet->set_is_fec(true);
     uint16_t fec_sequence_number = rtp_packet->SequenceNumber();
     if (rtp_sender_->SendToNetwork(std::move(rtp_packet), fec_storage,
                                    RtpPacketSender::kLowPriority)) {
@@ -321,13 +323,6 @@ void RTPSenderVideo::SetUlpfecConfig(int red_payload_type,
   // Reset FEC parameters.
   delta_fec_params_ = FecProtectionParams{0, 1, kFecMaskRandom};
   key_fec_params_ = FecProtectionParams{0, 1, kFecMaskRandom};
-}
-
-void RTPSenderVideo::GetUlpfecConfig(int* red_payload_type,
-                                     int* ulpfec_payload_type) const {
-  rtc::CritScope cs(&crit_);
-  *red_payload_type = red_payload_type_;
-  *ulpfec_payload_type = ulpfec_payload_type_;
 }
 
 size_t RTPSenderVideo::CalculateFecPacketOverhead() const {

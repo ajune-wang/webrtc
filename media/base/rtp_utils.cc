ac4b643d3d326c680d1ee467f8483dbecfde50fb
Piotr (Peter) Slatala <psla@webrtc.org>
2019-01-30 14:24:27 -0800

    Fix RTP transport accepting invalid RTCP headers.
    Currently, the RtpTransport checks that the packet is either RTP or
    RTCP. However, the RTCP check does not verify that the packet is a valid RTP,
    and therefore invalid RTCP packets were allowed in the RtpTransport::OnReadPacket.
    
    This change makes sure that the test for RTCP header (IsRtcpPacket) checks that it has the valid RTP version (2).
    
    So far if the packet had the second byte that looked like
    RTCP, it would ignore the first byte.
    
    
    Bug: None
    Change-Id: I5d07d497b9ef609c74b6e507c5f3e19e4bf10194

--- media/base/rtp_utils.cc
+++ media/base/rtp_utils.cc
@@ -290,6 +290,12 @@ bool IsRtcpPacket(const char* data, size_t len) {
   if (len < 2) {
     return false;
   }
+
+  // RTCP must be a valid RTP packet.
+  if ((static_cast<uint8_t>(data[0]) >> 6) != kRtpVersion) {
+    return false;
+  }
+
   char pt = data[1] & 0x7F;
   return (63 < pt) && (pt < 96);
 }

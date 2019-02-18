a7388ca6d02cbdd3ad2b7c6e8c82bd9deba46d93
Mirko Bonadei <mbonadei@webrtc.org>
2019-02-18 14:28:43 +0100

    Don't block the signaling thread during the call.
    Since WebRTC stats are collected on the signaling thread, this CL moves
    the wait from the signaling thread to the main thread.
    
    Bug: webrtc:10138
    Change-Id: I0e554fe82e3a4afe66b45e53032b06d533f54a39

--- test/pc/e2e/peer_connection_quality_test.cc
+++ test/pc/e2e/peer_connection_quality_test.cc
@@ -11,6 +11,7 @@
  */
 #include "test/pc/e2e/peer_connection_quality_test.h"
 
+#include <algorithm>
 #include <set>
 #include <utility>
 
@@ -189,8 +190,19 @@ void PeerConnectionE2EQualityTest::Run(
   video_quality_analyzer_injection_helper_->Start(video_analyzer_threads);
   signaling_thread->Invoke<void>(
       RTC_FROM_HERE,
-      rtc::Bind(&PeerConnectionE2EQualityTest::RunOnSignalingThread, this,
-                run_params));
+      rtc::Bind(&PeerConnectionE2EQualityTest::SetupCallOnSignalingThread,
+                this));
+
+  // TODO(bugs.webrtc.org/10138): Implement stats collection and send stats
+  // reports to analyzers every 1 second.
+  rtc::Event done;
+  done.Wait(static_cast<int>(run_params.run_duration.ms()));
+
+  signaling_thread->Invoke<void>(
+      RTC_FROM_HERE,
+      rtc::Bind(&PeerConnectionE2EQualityTest::TearDownCallOnSignalingThread,
+                this));
+
   video_quality_analyzer_injection_helper_->Stop();
 
   // Ensuring that TestPeers have been destroyed in order to correctly close
@@ -296,15 +308,14 @@ void PeerConnectionE2EQualityTest::SetupVideoSink(
   output_video_sinks_.push_back(std::move(video_sink));
 }
 
-void PeerConnectionE2EQualityTest::RunOnSignalingThread(RunParams run_params) {
+void PeerConnectionE2EQualityTest::SetupCallOnSignalingThread() {
   alice_video_sources_ = AddMedia(alice_.get());
   bob_video_sources_ = AddMedia(bob_.get());
 
   SetupCall();
+}
 
-  rtc::Event done;
-  done.Wait(static_cast<int>(run_params.run_duration.ms()));
-
+void PeerConnectionE2EQualityTest::TearDownCallOnSignalingThread() {
   TearDownCall();
 }
 

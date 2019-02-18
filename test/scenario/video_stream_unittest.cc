1cc75874e0090fbabc8caf8b0a4f440693c5be52
Sebastian Jansson <srte@webrtc.org>
2019-02-18 19:29:16 +0100

    Adds resource path support for video files in scenario tests.
    Bug: webrtc:9510
    Change-Id: Id41a32325cc5b16b119e62fba483cec88f52975b

--- test/scenario/video_stream_unittest.cc
+++ test/scenario/video_stream_unittest.cc
@@ -15,9 +15,59 @@
 namespace webrtc {
 namespace test {
 namespace {
+using Capture = VideoStreamConfig::Source::Capture;
+using ContentType = VideoStreamConfig::Encoder::ContentType;
 using Codec = VideoStreamConfig::Encoder::Codec;
 using CodecImpl = VideoStreamConfig::Encoder::Implementation;
 }  // namespace
+
+TEST(VideoStreamTest, ReceivesFramesFromFileBasedStreams) {
+  TimeDelta kRunTime = TimeDelta::ms(500);
+  std::vector<int> kFrameRates = {5, 15};
+  std::deque<std::atomic<int>> frame_counts(2);
+  frame_counts[0] = 0;
+  frame_counts[1] = 0;
+  {
+    Scenario s;
+    auto route = s.CreateRoutes(s.CreateClient("caller", CallClientConfig()),
+                                {s.CreateSimulationNode(NetworkNodeConfig())},
+                                s.CreateClient("callee", CallClientConfig()),
+                                {s.CreateSimulationNode(NetworkNodeConfig())});
+
+    s.CreateVideoStream(route->forward(), [&](VideoStreamConfig* c) {
+      c->analyzer.frame_quality_handler = [&](const VideoFrameQualityInfo&) {
+        frame_counts[0]++;
+      };
+      c->source.capture = Capture::kVideoFile;
+      c->source.video_file.name = "foreman_cif";
+      c->source.video_file.width = 352;
+      c->source.video_file.height = 288;
+      c->source.framerate = kFrameRates[0];
+      c->encoder.implementation = CodecImpl::kSoftware;
+      c->encoder.codec = Codec::kVideoCodecVP8;
+    });
+    s.CreateVideoStream(route->forward(), [&](VideoStreamConfig* c) {
+      c->analyzer.frame_quality_handler = [&](const VideoFrameQualityInfo&) {
+        frame_counts[1]++;
+      };
+      c->source.capture = Capture::kImageSlides;
+      c->source.slides.images.crop.width = 640;
+      c->source.slides.images.crop.height = 480;
+      c->encoder.content_type = ContentType::kScreen;
+      c->source.framerate = kFrameRates[1];
+      c->encoder.implementation = CodecImpl::kSoftware;
+      c->encoder.codec = Codec::kVideoCodecVP9;
+    });
+    s.RunFor(kRunTime);
+  }
+  std::vector<int> expected_counts;
+  for (auto fps : kFrameRates)
+    expected_counts.push_back(
+        static_cast<int>(kRunTime.seconds<double>() * fps));
+
+  EXPECT_GE(frame_counts[0], expected_counts[0] - 1);
+  EXPECT_GE(frame_counts[1], expected_counts[1] - 1);
+}
 
 TEST(VideoStreamTest, RecievesVp8SimulcastFrames) {
   TimeDelta kRunTime = TimeDelta::ms(500);

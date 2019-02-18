191d81c6f535a5a55ba386c81ac2098872dc83f4
Sebastian Jansson <srte@webrtc.org>
2019-02-18 19:29:59 +0100

    Adds support for VP8 simulcast to scenario tests.
    Bug: webrtc:9510
    Change-Id: Ice98e7bd98a1a8e4fd3b1a1c7c053a65de3f56e3

---
+++ test/scenario/video_stream_unittest.cc
@@ -0,0 +1,50 @@
+/*
+ *  Copyright 2019 The WebRTC project authors. All Rights Reserved.
+ *
+ *  Use of this source code is governed by a BSD-style license
+ *  that can be found in the LICENSE file in the root of the source
+ *  tree. An additional intellectual property rights grant can be found
+ *  in the file PATENTS.  All contributing project authors may
+ *  be found in the AUTHORS file in the root of the source tree.
+ */
+#include <atomic>
+
+#include "test/gtest.h"
+#include "test/scenario/scenario.h"
+
+namespace webrtc {
+namespace test {
+namespace {
+using Codec = VideoStreamConfig::Encoder::Codec;
+using CodecImpl = VideoStreamConfig::Encoder::Implementation;
+}  // namespace
+
+TEST(VideoStreamTest, RecievesVp8SimulcastFrames) {
+  TimeDelta kRunTime = TimeDelta::ms(500);
+  int kFrameRate = 30;
+
+  std::atomic<int> frame_count(0);
+  {
+    Scenario s;
+    auto route = s.CreateRoutes(s.CreateClient("caller", CallClientConfig()),
+                                {s.CreateSimulationNode(NetworkNodeConfig())},
+                                s.CreateClient("callee", CallClientConfig()),
+                                {s.CreateSimulationNode(NetworkNodeConfig())});
+    s.CreateVideoStream(route->forward(), [&](VideoStreamConfig* c) {
+      c->analyzer.frame_quality_handler = [&](const VideoFrameQualityInfo&) {
+        frame_count++;
+      };
+      c->source.framerate = kFrameRate;
+      c->encoder.implementation = CodecImpl::kSoftware;
+      c->encoder.codec = Codec::kVideoCodecVP8;
+    });
+    s.RunFor(TimeDelta::seconds(1));
+  }
+
+  const int kExpectedCount =
+      static_cast<int>(kRunTime.seconds<double>() * kFrameRate);
+
+  EXPECT_GE(frame_count, kExpectedCount - 1);
+}
+}  // namespace test
+}  // namespace webrtc

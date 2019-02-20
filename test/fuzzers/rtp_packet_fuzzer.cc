998fc7eb97471927a5bdecb35498b9c3b49cc98d
Johannes Kron <kron@webrtc.org>
2019-02-20 14:41:45 +0100

    Update RTP packet and header fuzzers to support additional extensions
    Bug: webrtc:10262
    Change-Id: I0a089329edc43dc004c616933ae8606a41546865

--- test/fuzzers/rtp_packet_fuzzer.cc
+++ test/fuzzers/rtp_packet_fuzzer.cc
@@ -17,32 +17,32 @@
 #include "modules/rtp_rtcp/source/rtp_packet_received.h"
 
 namespace webrtc {
-
-// We decide which header extensions to register by reading two bytes
+// We decide which header extensions to register by reading three bytes
 // from the beginning of |data| and interpreting it as a bitmask over
-// the RTPExtensionType enum. This assert ensures two bytes are enough.
-static_assert(kRtpExtensionNumberOfExtensions <= 16,
+// the RTPExtensionType enum. This assert ensures three bytes are enough.
+static_assert(kRtpExtensionNumberOfExtensions <= 24,
               "Insufficient bits read to configure all header extensions. Add "
               "an extra byte and update the switches.");
 
 void FuzzOneInput(const uint8_t* data, size_t size) {
-  if (size <= 2)
+  if (size <= 3)
     return;
 
-  // Don't use the configuration bytes as part of the packet.
-  std::bitset<16> extensionMask(*reinterpret_cast<const uint16_t*>(data));
-  data += 2;
-  size -= 2;
+  // Don't use the configuration byte as part of the packet.
+  std::bitset<24> extensionMask(*reinterpret_cast<const uint32_t*>(data));
+  data += 3;
+  size -= 3;
 
   RtpPacketReceived::ExtensionManager extensions;
-  // Skip i = 0 since it maps to ExtensionNone and extension id = 0 is invalid.
-  for (int i = 1; i < kRtpExtensionNumberOfExtensions; i++) {
+  // Start at 1 since 0 is an invalid id.
+  int local_id = 1;
+  for (int i = 0; i < kRtpExtensionNumberOfExtensions; i++) {
     RTPExtensionType extension_type = static_cast<RTPExtensionType>(i);
     if (extensionMask[i] && extension_type != kRtpExtensionNone) {
       // Extensions are registered with an ID, which you signal to the
       // peer so they know what to expect. This code only cares about
-      // parsing so the value of the ID isn't relevant; we use i.
-      extensions.RegisterByType(i, extension_type);
+      // parsing so the value of the ID isn't relevant.
+      extensions.RegisterByType(local_id++, extension_type);
     }
   }
 

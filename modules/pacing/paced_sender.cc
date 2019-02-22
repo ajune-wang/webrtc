9576f9606cc9e4a227f443a723bca7e87b22b5a3
Evan Shrubsole <eshr@google.com>
2019-02-22 13:41:54 +0100

    Log pacer values to verbose log
    The metrics of interest are the pacer rate, both normally and during a
    queue drain, as well as padding rate.
    
    Bug: None
    Change-Id: I9c36219f63ce61b46f20d42678e0d97cb2a1873c

--- modules/pacing/paced_sender.cc
+++ modules/pacing/paced_sender.cc
@@ -185,6 +185,11 @@ void PacedSender::SetPacingRates(uint32_t pacing_rate_bps,
   RTC_DCHECK(pacing_rate_bps > 0);
   pacing_bitrate_kbps_ = pacing_rate_bps / 1000;
   padding_budget_.set_target_rate_kbps(padding_rate_bps / 1000);
+
+  RTC_LOG(LS_VERBOSE) << "bwe:pacer_updated "
+                      << clock_->CurrentNtpInMilliseconds()
+                      << " pacing_kbps=" << pacing_bitrate_kbps_
+                      << " padding_budget_kbps=" << padding_rate_bps / 1000;
 }
 
 void PacedSender::InsertPacket(RtpPacketSender::Priority priority,
@@ -327,8 +332,12 @@ void PacedSender::Process() {
             1, queue_time_limit - packets_.AverageQueueTimeMs());
         int min_bitrate_needed_kbps =
             static_cast<int>(queue_size_bytes * 8 / avg_time_left_ms);
-        if (min_bitrate_needed_kbps > target_bitrate_kbps)
+        if (min_bitrate_needed_kbps > target_bitrate_kbps) {
           target_bitrate_kbps = min_bitrate_needed_kbps;
+          RTC_LOG(LS_VERBOSE)
+              << "bwe:large_pacing_queue " << clock_->CurrentNtpInMilliseconds()
+              << " pacing_rate_kbps=" << target_bitrate_kbps;
+        }
       }
     }
 

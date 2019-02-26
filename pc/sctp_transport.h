a78031f93a79bc15dd067b006b4bdff52b83dd24
Harald Alvestrand <hta@webrtc.org>
2019-02-26 12:14:52 +0100

    Add SCTP transport to the public API.
    This involves inserting an extra layer between jsep_transport_controller
    and the cricket::SctpTransportInternal layer. The objects at this layer
    are reference counted.
    
    Bug: chromium:818643
    Change-Id: Ibed57c4a538de981cee63e0f7f1f319f029cab39

---
+++ pc/sctp_transport.h
@@ -0,0 +1,77 @@
+/*
+ *  Copyright 2018 The WebRTC project authors. All Rights Reserved.
+ *
+ *  Use of this source code is governed by a BSD-style license
+ *  that can be found in the LICENSE file in the root of the source
+ *  tree. An additional intellectual property rights grant can be found
+ *  in the file PATENTS.  All contributing project authors may
+ *  be found in the AUTHORS file in the root of the source tree.
+ */
+
+#ifndef PC_SCTP_TRANSPORT_H_
+#define PC_SCTP_TRANSPORT_H_
+
+#include <memory>
+
+#include "api/ice_transport_interface.h"
+#include "api/scoped_refptr.h"
+#include "api/sctp_transport_interface.h"
+#include "media/sctp/sctp_transport.h"
+#include "pc/dtls_transport.h"
+#include "rtc_base/async_invoker.h"
+
+namespace webrtc {
+
+class IceTransportWithPointer;
+
+// This implementation wraps a cricket::SctpTransport, and takes
+// ownership of it.
+class SctpTransport : public SctpTransportInterface,
+                      public sigslot::has_slots<> {
+ public:
+  // This object must be constructed and updated on a consistent thread,
+  // the same thread as the one the cricket::SctpTransportInternal object
+  // lives on.
+  // The Information() function can be called from a different thread,
+  // such as the signalling thread.
+  explicit SctpTransport(
+      std::unique_ptr<cricket::SctpTransportInternal> internal);
+
+  rtc::scoped_refptr<DtlsTransportInterface> dtls_transport() override;
+  SctpTransportInformation Information() override;
+  void RegisterObserver(SctpTransportObserverInterface* observer) override;
+  void UnregisterObserver() override;
+
+  void Clear();
+  void SetDtlsTransport(rtc::scoped_refptr<DtlsTransport>);
+
+  cricket::SctpTransportInternal* internal() {
+    rtc::CritScope scope(&lock_);
+    return internal_sctp_transport_.get();
+  }
+
+  const cricket::SctpTransportInternal* internal() const {
+    rtc::CritScope scope(&lock_);
+    return internal_sctp_transport_.get();
+  }
+
+ protected:
+  ~SctpTransport();
+
+ private:
+  void UpdateInformation(SctpTransportState state);
+  void OnInternalReadyToSendData();
+  void OnInternalClosingProcedureStartedRemotely(int sid);
+  void OnInternalClosingProcedureComplete(int sid);
+
+  SctpTransportObserverInterface* observer_ = nullptr;
+  rtc::Thread* owner_thread_;
+  rtc::CriticalSection lock_;
+  SctpTransportInformation info_ RTC_GUARDED_BY(lock_);
+  std::unique_ptr<cricket::SctpTransportInternal> internal_sctp_transport_
+      RTC_GUARDED_BY(lock_);
+  rtc::scoped_refptr<DtlsTransport> dtls_transport_ RTC_GUARDED_BY(lock_);
+};
+
+}  // namespace webrtc
+#endif  // PC_SCTP_TRANSPORT_H_
---
+++ pc/sctp_transport_unittest.cc
@@ -0,0 +1,177 @@
+/*
+ *  Copyright 2019 The WebRTC project authors. All Rights Reserved.
+ *
+ *  Use of this source code is governed by a BSD-style license
+ *  that can be found in the LICENSE file in the root of the source
+ *  tree. An additional intellectual property rights grant can be found
+ *  in the file PATENTS.  All contributing project authors may
+ *  be found in the AUTHORS file in the root of the source tree.
+ */
+
+#include "pc/sctp_transport.h"
+
+#include <utility>
+#include <vector>
+
+#include "absl/memory/memory.h"
+#include "p2p/base/fake_dtls_transport.h"
+#include "pc/dtls_transport.h"
+#include "pc/test/fake_sctp_transport.h"
+#include "rtc_base/gunit.h"
+#include "test/gmock.h"
+#include "test/gtest.h"
+
+constexpr int kDefaultTimeout = 1000;  // milliseconds
+
+using cricket::FakeDtlsTransport;
+using ::testing::ElementsAre;
+
+namespace webrtc {
+
+namespace {
+
+// Fake / mock object written specially for this test.
+class TestCricketSctpTransport : public cricket::SctpTransportInternal {
+ public:
+  void SetDtlsTransport(rtc::PacketTransportInternal* transport) override {}
+  bool Start(int local_port, int remote_port) override { return true; }
+  bool OpenStream(int sid) override { return true; }
+  bool ResetStream(int sid) override { return true; }
+  bool SendData(const cricket::SendDataParams& params,
+                const rtc::CopyOnWriteBuffer& payload,
+                cricket::SendDataResult* result = nullptr) override {
+    return true;
+  }
+  bool ReadyToSendData() override { return true; }
+  void set_debug_name_for_testing(const char* debug_name) override {}
+  // Methods exposed for testing
+  void SendSignalReadyToSendData() { SignalReadyToSendData(); }
+
+  void SendSignalClosingProcedureStartedRemotely() {
+    SignalClosingProcedureStartedRemotely(1);
+  }
+
+  void SendSignalClosingProcedureComplete() {
+    SignalClosingProcedureComplete(1);
+  }
+
+ private:
+};
+
+}  // namespace
+
+class TestSctpTransportObserver : public SctpTransportObserverInterface {
+ public:
+  void OnStateChange(SctpTransportInformation info) override {
+    state_change_called_ = true;
+    states_.push_back(info.state());
+  }
+
+  SctpTransportState state() {
+    if (states_.size() > 0) {
+      return states_[states_.size() - 1];
+    } else {
+      return SctpTransportState::kNew;
+    }
+  }
+
+  bool state_change_called_ = false;
+  std::vector<SctpTransportState> states_;
+};
+
+class SctpTransportTest : public testing::Test {
+ public:
+  SctpTransport* transport() { return transport_.get(); }
+  SctpTransportObserverInterface* observer() { return &observer_; }
+
+  void CreateTransport() {
+    auto cricket_sctp_transport =
+        absl::WrapUnique(new TestCricketSctpTransport());
+    transport_ = new rtc::RefCountedObject<SctpTransport>(
+        std::move(cricket_sctp_transport));
+  }
+
+  void AddDtlsTransport() {
+    std::unique_ptr<cricket::DtlsTransportInternal> cricket_transport =
+        absl::make_unique<FakeDtlsTransport>(
+            "audio", cricket::ICE_CANDIDATE_COMPONENT_RTP);
+    dtls_transport_ =
+        new rtc::RefCountedObject<DtlsTransport>(std::move(cricket_transport));
+    transport_->SetDtlsTransport(dtls_transport_);
+  }
+
+  void CompleteSctpHandshake() {
+    CricketSctpTransport()->SendSignalReadyToSendData();
+  }
+
+  TestCricketSctpTransport* CricketSctpTransport() {
+    return static_cast<TestCricketSctpTransport*>(transport_->internal());
+  }
+
+  rtc::scoped_refptr<SctpTransport> transport_;
+  rtc::scoped_refptr<DtlsTransport> dtls_transport_;
+  TestSctpTransportObserver observer_;
+};
+
+TEST_F(SctpTransportTest, CreateClearDelete) {
+  std::unique_ptr<cricket::SctpTransportInternal> fake_cricket_sctp_transport =
+      absl::WrapUnique(new FakeSctpTransport());
+  rtc::scoped_refptr<SctpTransport> sctp_transport =
+      new rtc::RefCountedObject<SctpTransport>(
+          std::move(fake_cricket_sctp_transport));
+  ASSERT_TRUE(sctp_transport->internal());
+  ASSERT_EQ(SctpTransportState::kNew, sctp_transport->Information().state());
+  sctp_transport->Clear();
+  ASSERT_FALSE(sctp_transport->internal());
+  ASSERT_EQ(SctpTransportState::kClosed, sctp_transport->Information().state());
+}
+
+TEST_F(SctpTransportTest, EventsObservedWhenConnecting) {
+  CreateTransport();
+  transport()->RegisterObserver(observer());
+  AddDtlsTransport();
+  CompleteSctpHandshake();
+  ASSERT_TRUE_WAIT(observer_.state_change_called_, kDefaultTimeout);
+  EXPECT_THAT(observer_.states_, ElementsAre(SctpTransportState::kConnecting,
+                                             SctpTransportState::kConnected));
+}
+
+TEST_F(SctpTransportTest, CloseWhenClearing) {
+  CreateTransport();
+  transport()->RegisterObserver(observer());
+  AddDtlsTransport();
+  CompleteSctpHandshake();
+  ASSERT_TRUE_WAIT(observer_.state() == SctpTransportState::kConnected,
+                   kDefaultTimeout);
+  transport()->Clear();
+  ASSERT_TRUE_WAIT(observer_.state() == SctpTransportState::kClosed,
+                   kDefaultTimeout);
+}
+
+TEST_F(SctpTransportTest, RemoteDisconnectCloses) {
+  CreateTransport();
+  transport()->RegisterObserver(observer());
+  AddDtlsTransport();
+  CompleteSctpHandshake();
+  CricketSctpTransport()->SendSignalClosingProcedureStartedRemotely();
+  ASSERT_TRUE_WAIT(observer_.state() == SctpTransportState::kClosed,
+                   kDefaultTimeout);
+  EXPECT_THAT(observer_.states_, ElementsAre(SctpTransportState::kConnecting,
+                                             SctpTransportState::kConnected,
+                                             SctpTransportState::kClosed));
+}
+
+TEST_F(SctpTransportTest, ClosignProcedureCloses) {
+  CreateTransport();
+  transport()->RegisterObserver(observer());
+  AddDtlsTransport();
+  CompleteSctpHandshake();
+  CricketSctpTransport()->SendSignalClosingProcedureComplete();
+  ASSERT_TRUE_WAIT(observer_.state() == SctpTransportState::kClosed,
+                   kDefaultTimeout);
+  EXPECT_THAT(observer_.states_, ElementsAre(SctpTransportState::kConnecting,
+                                             SctpTransportState::kConnected,
+                                             SctpTransportState::kClosed));
+}
+
+}  // namespace webrtc

10c60a421b31a7bde5a65ab63363fb557076c4d3
Sebastian Jansson <srte@webrtc.org>
2019-03-04 19:15:38 +0100

    Add log based GoogCC simulation to visualizer.
    This CL adds  a mode to simulate roughly what GoogCC could have been
    doing during the recording of an rtc event log by using the logged
    events as input to GoogCC and visualizing the resulting target rate.
    
    This is similar to the existing simulated_sendside_bwe mode, but uses
    the new NetworkControllerInterface to ensure more reliable GoogCC
    simulation.
    
    Bug: None
    Change-Id: I57894aa666151efc8405407d928b5257fb9b7d61

--- logging/rtc_event_log/logged_events.h
+++ logging/rtc_event_log/logged_events.h
@@ -491,6 +491,7 @@ struct LoggedVideoSendConfig {
 };
 
 struct LoggedRouteChangeEvent {
+  int64_t log_time_ms() const { return log_time.ms(); }
   uint32_t route_id;
   Timestamp log_time = Timestamp::MinusInfinity();
   uint16_t send_overhead;
@@ -506,6 +507,7 @@ struct LoggedPacketInfo {
                    Timestamp capture_time);
   LoggedPacketInfo(const LoggedPacketInfo&);
   ~LoggedPacketInfo();
+  int64_t log_time_ms() const { return log_packet_time.ms(); }
   uint32_t ssrc;
   uint16_t stream_seq_no;
   uint16_t size;

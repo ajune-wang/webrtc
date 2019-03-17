ebe4dc55c923255828d39d7eba50734991400773
Karl Wiberg <kwiberg@webrtc.org>
2019-03-18 00:15:31 +0100

    rtc::Event::Wait(kForever): Print stack trace when we deadlock
    After being stuck "forever" (3 seconds) waiting for an event to
    trigger, log the stack trace of the current thread to aid debugging of
    the deadlock.
    
    Bug: webrtc:10308
    Change-Id: I04852f191027294d7e7a7f5e63de4c6c7fdd6326

---
+++ rtc_base/system/warn_current_thread_is_deadlocked.h
@@ -0,0 +1,26 @@
+/*
+ *  Copyright 2019 The WebRTC Project Authors. All rights reserved.
+ *
+ *  Use of this source code is governed by a BSD-style license
+ *  that can be found in the LICENSE file in the root of the source
+ *  tree. An additional intellectual property rights grant can be found
+ *  in the file PATENTS.  All contributing project authors may
+ *  be found in the AUTHORS file in the root of the source tree.
+ */
+
+#ifndef RTC_BASE_SYSTEM_WARN_CURRENT_THREAD_IS_DEADLOCKED_H_
+#define RTC_BASE_SYSTEM_WARN_CURRENT_THREAD_IS_DEADLOCKED_H_
+
+#include "rtc_base/location.h"
+
+namespace webrtc {
+
+#if defined(WEBRTC_ANDROID) && !defined(WEBRTC_CHROMIUM_BUILD)
+void WarnThatTheCurrentThreadIsProbablyDeadlocked(rtc::Location called_from);
+#else
+inline void WarnThatTheCurrentThreadIsProbablyDeadlocked(rtc::Location) {}
+#endif
+
+}  // namespace webrtc
+
+#endif  // RTC_BASE_SYSTEM_WARN_CURRENT_THREAD_IS_DEADLOCKED_H_

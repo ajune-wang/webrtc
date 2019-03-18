6700aa0ea8ca92966a025cd578bc31260f3288ff
Yves Gerey <yvesg@webrtc.org>
2019-03-18 10:50:17 +0100

    Fix VideoFileWriterTest flakiness by using unique file path.
    Both reference and tested videos were created
    via a file whose path was fixed.
    So, when tests were launched in parallel, race conditions ensued.
    This CL creates an unique temporary filename for each video.
    
    Bug: webrtc:10156
    Change-Id: Ie3abf85abdfa95735cb86880bbd6a59393e609c1

--- rtc_tools/video_file_writer.cc
+++ rtc_tools/video_file_writer.cc
@@ -18,19 +18,19 @@
 #include "api/video/video_frame_buffer.h"
 #include "rtc_base/logging.h"
 
-namespace webrtc {
-namespace test {
+namespace {
 
-void WriteVideoToFile(const rtc::scoped_refptr<Video>& video,
+void WriteVideoToFile(const rtc::scoped_refptr<webrtc::test::Video>& video,
                       const std::string& file_name,
-                      int fps) {
+                      int fps,
+                      bool isY4m) {
+  RTC_CHECK(video);
   FILE* output_file = fopen(file_name.c_str(), "wb");
   if (output_file == nullptr) {
     RTC_LOG(LS_ERROR) << "Could not open file for writing: " << file_name;
     return;
   }
 
-  bool isY4m = absl::EndsWith(file_name, ".y4m");
   if (isY4m) {
     fprintf(output_file, "YUV4MPEG2 W%d H%d F%d:1 C420\n", video->width(),
             video->height(), fps);
@@ -40,7 +40,9 @@ void WriteVideoToFile(const rtc::scoped_refptr<Video>& video,
       std::string frame = "FRAME\n";
       fwrite(frame.c_str(), 1, 6, output_file);
     }
-    rtc::scoped_refptr<I420BufferInterface> buffer = video->GetFrame(i);
+    rtc::scoped_refptr<webrtc::I420BufferInterface> buffer = video->GetFrame(i);
+    RTC_CHECK(buffer) << "Frame: " << i
+                      << "\nWhile trying to create: " << file_name;
     const uint8_t* data_y = buffer->DataY();
     int stride = buffer->StrideY();
     for (int i = 0; i < video->height(); ++i) {
@@ -61,6 +63,30 @@ void WriteVideoToFile(const rtc::scoped_refptr<Video>& video,
     RTC_LOG(LS_ERROR) << "Error writing to file " << file_name;
   }
   fclose(output_file);
+}
+
+}  // Anonymous namespace
+
+namespace webrtc {
+namespace test {
+
+void WriteVideoToFile(const rtc::scoped_refptr<Video>& video,
+                      const std::string& file_name,
+                      int fps) {
+  ::WriteVideoToFile(video, file_name, fps,
+                     /*isY4m=*/absl::EndsWith(file_name, ".y4m"));
+}
+
+void WriteY4mVideoToFile(const rtc::scoped_refptr<Video>& video,
+                         const std::string& file_name,
+                         int fps) {
+  ::WriteVideoToFile(video, file_name, fps, /*isY4m=*/true);
+}
+
+void WriteYuvVideoToFile(const rtc::scoped_refptr<Video>& video,
+                         const std::string& file_name,
+                         int fps) {
+  ::WriteVideoToFile(video, file_name, fps, /*isY4m=*/false);
 }
 
 }  // namespace test

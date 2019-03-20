c82e63f7828fff28f2adc48cb2ccbc96e1dc1b70
Sebastian Jansson <srte@webrtc.org>
2019-03-20 15:58:16 +0100

    Adds simulated time controller
    This CL introduces the ￼TimeControllerInterface that provides timing
    related functionality. Most notably it provides a TaskQueueFactory
    and facilitates creation of ProcessThread.
    
    Two implementations of the interface are provided, RealTimeController
    and SimulatedTimeController.
    
    This prepares for an upcoming CL using these in Scenario tests.
    
    Bug: webrtc:10365
    Change-Id: Id956a29628d7e2f53ecaedadd643a9f697329d2f

---
+++ test/time_controller/real_time_controller.cc
@@ -0,0 +1,53 @@
+/*
+ *  Copyright 2019 The WebRTC project authors. All Rights Reserved.
+ *
+ *  Use of this source code is governed by a BSD-style license
+ *  that can be found in the LICENSE file in the root of the source
+ *  tree. An additional intellectual property rights grant can be found
+ *  in the file PATENTS.  All contributing project authors may
+ *  be found in the AUTHORS file in the root of the source tree.
+ */
+#include "test/time_controller/real_time_controller.h"
+
+#include "api/task_queue/global_task_queue_factory.h"
+#include "rtc_base/event.h"
+#include "rtc_base/task_utils/to_queued_task.h"
+#include "system_wrappers/include/sleep.h"
+
+namespace webrtc {
+
+Clock* RealTimeController::GetClock() {
+  return Clock::GetRealTimeClock();
+}
+
+TaskQueueFactory* RealTimeController::GetTaskQueueFactory() {
+  return &GlobalTaskQueueFactory();
+}
+
+std::unique_ptr<ProcessThread> RealTimeController::CreateProcessThread(
+    const char* thread_name) {
+  return ProcessThread::Create(thread_name);
+}
+
+void RealTimeController::Sleep(TimeDelta duration) {
+  SleepMs(duration.ms());
+}
+
+void RealTimeController::SendTask(TaskQueueBase* task_queue,
+                                  std::unique_ptr<QueuedTask> task) {
+  RTC_DCHECK(!task_queue->IsCurrent());
+  bool delete_task = true;
+  rtc::Event event;
+  task_queue->PostTask(
+      ToQueuedTask([&task, &delete_task]() { delete_task = task->Run(); },
+                   [&event]() { event.Set(); }));
+  event.Wait(rtc::Event::kForever);
+  if (!delete_task)
+    task.release();
+}
+
+void RealTimeController::ScopedInvoke(std::function<void()> closure) {
+  closure();
+}
+
+}  // namespace webrtc

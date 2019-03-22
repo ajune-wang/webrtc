2d9671979dc2ca6d79ffcc063748b838cffd7b10
Mirko Bonadei <mbonadei@webrtc.org>
2019-03-22 12:23:18 +0100

    Try to not assume /DUNICODE.
    Bug: None
    Change-Id: I1244c45bf7f306686e2d9ca465f5a35c94eb6925

--- modules/audio_device/win/audio_device_core_win.cc
+++ modules/audio_device/win/audio_device_core_win.cc
@@ -180,7 +180,7 @@ bool AudioDeviceWindowsCore::CoreAudioIsSupported() {
 
   HRESULT hr(S_OK);
   TCHAR buf[MAXERRORLENGTH];
-  TCHAR errorText[MAXERRORLENGTH];
+  WCHAR errorText[MAXERRORLENGTH];
 
   // 1) Check if Windows version is Vista SP1 or later.
   //
@@ -290,7 +290,7 @@ bool AudioDeviceWindowsCore::CoreAudioIsSupported() {
     }
 
     StringCchPrintf(buf, MAXERRORLENGTH, TEXT("Error details: "));
-    StringCchCat(buf, MAXERRORLENGTH, errorText);
+    StringCchCatW(buf, MAXERRORLENGTH, errorText);
     RTC_LOG(LS_VERBOSE) << buf;
   } else {
     MMDeviceIsAvailable = true;
@@ -4161,7 +4161,7 @@ Exit:
 
 void AudioDeviceWindowsCore::_TraceCOMError(HRESULT hr) const {
   TCHAR buf[MAXERRORLENGTH];
-  TCHAR errorText[MAXERRORLENGTH];
+  WCHAR errorText[MAXERRORLENGTH];
 
   const DWORD dwFlags =
       FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS;
@@ -4182,7 +4182,7 @@ void AudioDeviceWindowsCore::_TraceCOMError(HRESULT hr) const {
 
   RTC_LOG(LS_ERROR) << "Core Audio method failed (hr=" << hr << ")";
   StringCchPrintf(buf, MAXERRORLENGTH, TEXT("Error details: "));
-  StringCchCat(buf, MAXERRORLENGTH, errorText);
+  StringCchCatW(buf, MAXERRORLENGTH, errorText);
   RTC_LOG(LS_ERROR) << WideToUTF8(buf);
 }
 

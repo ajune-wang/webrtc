cdd15eb2558ce724e7557c275e11189cdbc99977
Daniel Lee <dklee@google.com>
2019-03-25 17:08:22 +0100

    Ensure that we have sensible values for min and max audio bitrate.
    Use (in order from lowest to highest precedence):
    -- fixed 32000bps
    -- fixed target bitrate from codec
    -- explicit values from the rtp encoding parameters
    -- Final precedence is given to field trial values from
       WebRTC-Audio-Allocation
    
    
    Change-Id: I7e289f209a927785572058b6fbfdf60fa14edf05

--- rtc_base/experiments/audio_allocation_settings.cc
+++ rtc_base/experiments/audio_allocation_settings.cc
@@ -23,8 +23,8 @@ AudioAllocationSettings::AudioAllocationSettings()
       allocate_audio_without_feedback_("Enabled"),
       force_no_audio_feedback_("Enabled"),
       send_side_bwe_with_overhead_("Enabled"),
-      default_min_bitrate_("min", DataRate::bps(kOpusMinBitrateBps)),
-      default_max_bitrate_("max", DataRate::bps(kOpusBitrateFbBps)),
+      min_bitrate_("min", DataRate::bps(kOpusMinBitrateBps)),
+      max_bitrate_("max", DataRate::bps(kOpusBitrateFbBps)),
       priority_bitrate_("prio", DataRate::Zero()) {
   ParseFieldTrial({&audio_send_side_bwe_},
                   field_trial::FindFullName("WebRTC-Audio-SendSideBwe"));
@@ -35,9 +35,8 @@ AudioAllocationSettings::AudioAllocationSettings()
 
   ParseFieldTrial({&send_side_bwe_with_overhead_},
                   field_trial::FindFullName("WebRTC-SendSideBwe-WithOverhead"));
-  ParseFieldTrial(
-      {&default_min_bitrate_, &default_max_bitrate_, &priority_bitrate_},
-      field_trial::FindFullName("WebRTC-Audio-Allocation"));
+  ParseFieldTrial({&min_bitrate_, &max_bitrate_, &priority_bitrate_},
+                  field_trial::FindFullName("WebRTC-Audio-Allocation"));
 
   // TODO(mflodman): Keep testing this and set proper values.
   // Note: This is an early experiment currently only supported by Opus.
@@ -100,25 +99,11 @@ bool AudioAllocationSettings::IncludeAudioInAllocationOnReconfigure(
   return true;
 }
 
-int AudioAllocationSettings::MinBitrateBps() const {
-  return default_min_bitrate_->bps() + min_overhead_bps_;
+absl::optional<DataRate> AudioAllocationSettings::MinBitrate() const {
+  return min_bitrate_.GetOptional();
 }
-
-int AudioAllocationSettings::MaxBitrateBps(
-    absl::optional<int> rtp_parameter_max_bitrate_bps) const {
-  // We assume that the max is a hard limit on the payload bitrate, so we add
-  // min_overhead_bps to it to ensure that, when overhead is deducted, the
-  // payload rate never goes beyond the limit.  Note: this also means that if a
-  // higher overhead is forced, we cannot reach the limit.
-  // TODO(minyue): Reconsider this when the signaling to BWE is done
-  // through a dedicated API.
-
-  // This means that when RtpParameters is reset, we may change the
-  // encoder's bit rate immediately (through ReconfigureAudioSendStream()),
-  // meanwhile change the cap to the output of BWE.
-  if (rtp_parameter_max_bitrate_bps)
-    return *rtp_parameter_max_bitrate_bps + min_overhead_bps_;
-  return default_max_bitrate_->bps() + min_overhead_bps_;
+absl::optional<DataRate> AudioAllocationSettings::MaxBitrate() const {
+  return max_bitrate_.GetOptional();
 }
 DataRate AudioAllocationSettings::DefaultPriorityBitrate() const {
   DataRate max_overhead = DataRate::Zero();

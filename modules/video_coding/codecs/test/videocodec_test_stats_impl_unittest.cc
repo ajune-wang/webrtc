b132bbe9af03d1cfbd59e34cba09f1332e6190b4
Rasmus Brandt <brandtr@webrtc.org>
2019-03-25 14:36:59 +0100

    Change VideoCodecTestStats API.
    - Add GetFrameStatistics API:
      This is useful for downstream test users that want to read frame-level stats.
    - Remove other APIs that are not used by downstream tests:
        * AddFrame
        * GetFrame
        * GetFrameWithTimestamp
        * SliceAndCalcAggregatedVideoStatistic
        * PrintFrameStatistics
        * Size
        * Clear
      The implementations, which are used by the fixture implementation, are kept.
    
    Bug: webrtc:10349
    Change-Id: Id2f6fa5a36b8341a5ccb365725f71ebe0c0f1570

--- modules/video_coding/codecs/test/videocodec_test_stats_impl_unittest.cc
+++ modules/video_coding/codecs/test/videocodec_test_stats_impl_unittest.cc
@@ -10,11 +10,15 @@
 
 #include "modules/video_coding/codecs/test/videocodec_test_stats_impl.h"
 
+#include <vector>
+
 #include "test/gtest.h"
 
 namespace webrtc {
 namespace test {
+
 using FrameStatistics = VideoCodecTestStatsImpl::FrameStatistics;
+
 namespace {
 const size_t kTimestamp = 12345;
 }  // namespace
@@ -53,6 +57,28 @@ TEST(StatsTest, AddFrameLayering) {
     EXPECT_EQ(kTimestamp, frame_stat->rtp_timestamp - i);
     EXPECT_EQ(1u, stats.Size(i));
   }
+}
+
+TEST(StatsTest, GetFrameStatistics) {
+  VideoCodecTestStatsImpl stats;
+
+  stats.AddFrame(FrameStatistics(0, kTimestamp, 0));
+  stats.AddFrame(FrameStatistics(0, kTimestamp, 1));
+  stats.AddFrame(FrameStatistics(1, kTimestamp + 3000, 0));
+  stats.AddFrame(FrameStatistics(1, kTimestamp + 3000, 1));
+
+  const auto get_pred = [](size_t frame_number, size_t spatial_idx) {
+    return [frame_number, spatial_idx](const FrameStatistics& s) {
+      return s.frame_number == frame_number && s.spatial_idx == spatial_idx;
+    };
+  };
+  const std::vector<FrameStatistics> frame_stats = stats.GetFrameStatistics();
+  const auto begin_it = frame_stats.cbegin();
+  const auto end_it = frame_stats.cend();
+  EXPECT_NE(std::find_if(begin_it, end_it, get_pred(0, 0)), end_it);
+  EXPECT_NE(std::find_if(begin_it, end_it, get_pred(0, 1)), end_it);
+  EXPECT_NE(std::find_if(begin_it, end_it, get_pred(1, 0)), end_it);
+  EXPECT_NE(std::find_if(begin_it, end_it, get_pred(1, 1)), end_it);
 }
 
 }  // namespace test

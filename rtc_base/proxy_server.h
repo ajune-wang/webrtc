2834df9420e9921face2a6e89e26fa2bdc9f905e
Niels Möller <nisse@webrtc.org>
2019-03-28 13:30:15 +0100

    Move FifoBuffer to its own file and build target
    Used only by test code and by pseudo_tcp.
    
    Bug: webrtc:6424
    Change-Id: I28903e74f7b69cbdd8c368f4444c8a233eb76868

--- rtc_base/proxy_server.h
+++ rtc_base/proxy_server.h
@@ -15,9 +15,9 @@
 #include <memory>
 #include "rtc_base/async_socket.h"
 #include "rtc_base/constructor_magic.h"
+#include "rtc_base/memory/fifo_buffer.h"
 #include "rtc_base/server_socket_adapters.h"
 #include "rtc_base/socket_address.h"
-#include "rtc_base/stream.h"
 
 namespace rtc {
 

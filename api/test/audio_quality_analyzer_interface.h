fc06b7651cbbb74d52760587e9366d30490d4af9
Mirko Bonadei <mbonadei@webrtc.org>
2019-03-29 10:53:30 +0100

    Implement DefaultAudioQualityAnalyzer.
    The DefaultAudioQualityAnalyzer will read stats reports (temporarily
    using the old PeerConnectionInterface::GetStats) and for each audio
    stream it will collect some NetEq related stats.
    
    When DefaultAudioQualityAnalyzer::Stop is invoked by the framework,
    it will report the following metrics:
    - expand_rate
    - accelerate_rate
    - preemptive_rate
    - speech_expand_rate
    - preferred_buffer_size_ms
    
    Bug: webrtc:10138
    Change-Id: Ie493456fcb9ed86455b12dabdab98a317387ef46

--- api/test/audio_quality_analyzer_interface.h
+++ api/test/audio_quality_analyzer_interface.h
@@ -13,6 +13,7 @@
 
 #include <string>
 
+#include "api/test/analyzer_helper.h"
 #include "api/test/stats_observer_interface.h"
 
 namespace webrtc {
@@ -23,10 +24,18 @@ class AudioQualityAnalyzerInterface : public StatsObserverInterface {
  public:
   ~AudioQualityAnalyzerInterface() override = default;
 
-  // Will be called by framework before test.
+  // Will be called by the framework before the test.
   // |test_case_name| is name of test case, that should be used to report all
   // audio metrics.
-  virtual void Start(std::string test_case_name) = 0;
+  // |analyzer_helper| is a pointer to a class that will allow track_id to
+  // stream_id matching. The caller is responsible for ensuring the
+  // AnalyzerHelper outlives the instance of the AudioQualityAnalyzerInterface.
+  virtual void Start(std::string test_case_name,
+                     AnalyzerHelper* analyzer_helper) = 0;
+
+  // Will be called by the framework at the end of the test. The analyzer
+  // has to finalize all its stats and it should report them.
+  virtual void Stop() = 0;
 };
 
 }  // namespace webrtc_pc_e2e

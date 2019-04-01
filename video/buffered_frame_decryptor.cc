92dad57af1320d50cdd8b5f22b7d7241cd1cc949
Benjamin Wright <benwright@webrtc.org>
2019-04-01 15:45:06 -0700

    Added some more debugging logs
    Bug: webrtc:webrtc:10416
    Change-Id: I4f599d604753d101cf14f4ca561921ce6b6ffa58

--- video/buffered_frame_decryptor.cc
+++ video/buffered_frame_decryptor.cc
@@ -108,6 +108,10 @@ BufferedFrameDecryptor::FrameDecision BufferedFrameDecryptor::DecryptFrame(
 }
 
 void BufferedFrameDecryptor::RetryStashedFrames() {
+  if (!stashed_frames_.empty()) {
+    RTC_LOG(LS_WARNING) << "Retrying stashed encrypted frames. Count: "
+                        << stashed_frames_.size();
+  }
   for (auto& frame : stashed_frames_) {
     if (DecryptFrame(frame.get()) == FrameDecision::kDecrypted) {
       decrypted_frame_callback_->OnDecryptedFrame(std::move(frame));

b472b543fb2c2ebf2d83c22f7b31e9a047911ee3
Elad Alon <eladalon@webrtc.org>
2019-04-01 14:14:17 +0200

    Remove deprecated code in RtpPayloadParams
    Bug: webrtc:10242
    Change-Id: I09d09438ed4e1e13496b06fd291d8e85c3555b07

--- call/rtp_payload_params.h
+++ call/rtp_payload_params.h
@@ -13,7 +13,6 @@
 
 #include <array>
 
-#include "absl/types/optional.h"
 #include "api/video_codecs/video_encoder.h"
 #include "call/rtp_config.h"
 #include "modules/rtp_rtcp/source/rtp_generic_frame_descriptor.h"
@@ -54,28 +53,11 @@ class RtpPayloadParams final {
                     bool is_keyframe,
                     RTPVideoHeader* rtp_video_header);
 
-  // TODO(bugs.webrtc.org/10242): Delete SetDependenciesVp8Deprecated() and move
-  // the logic in SetDependenciesVp8New() into Vp8ToGeneric() once all hardware
-  // wrappers have been updated.
-  void SetDependenciesVp8Deprecated(
-      const CodecSpecificInfoVP8& vp8_info,
-      int64_t shared_frame_id,
-      bool is_keyframe,
-      int spatial_index,
-      int temporal_index,
-      bool layer_sync,
-      RTPVideoHeader::GenericDescriptorInfo* generic);
-  void SetDependenciesVp8New(const CodecSpecificInfoVP8& vp8_info,
-                             int64_t shared_frame_id,
-                             bool is_keyframe,
-                             bool layer_sync,
-                             RTPVideoHeader::GenericDescriptorInfo* generic);
-
-  // TODO(bugs.webrtc.org/10242): Remove once all encoder-wrappers are updated.
-  // Holds the last shared frame id for a given (spatial, temporal) layer.
-  std::array<std::array<int64_t, RtpGenericFrameDescriptor::kMaxTemporalLayers>,
-             RtpGenericFrameDescriptor::kMaxSpatialLayers>
-      last_shared_frame_id_;
+  void SetDependenciesVp8(const CodecSpecificInfoVP8& vp8_info,
+                          int64_t shared_frame_id,
+                          bool is_keyframe,
+                          bool layer_sync,
+                          RTPVideoHeader::GenericDescriptorInfo* generic);
 
   // TODO(eladalon): When additional codecs are supported,
   // set kMaxCodecBuffersCount to the max() of these codecs' buffer count.
@@ -84,12 +66,6 @@ class RtpPayloadParams final {
 
   // Maps buffer IDs to the frame-ID stored in them.
   std::array<int64_t, kMaxCodecBuffersCount> buffer_id_to_frame_id_;
-
-  // Until we remove SetDependenciesVp8Deprecated(), we should make sure
-  // that, for a given object, we either always use
-  // SetDependenciesVp8Deprecated(), or always use SetDependenciesVp8New().
-  // TODO(bugs.webrtc.org/10242): Remove.
-  absl::optional<bool> new_version_used_;
 
   const uint32_t ssrc_;
   RtpPayloadState state_;

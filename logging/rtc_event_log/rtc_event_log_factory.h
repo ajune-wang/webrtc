b799f325364a71397e0052ef4deb5d80cec53560
Danil Chapovalov <danilchap@webrtc.org>
2019-04-11 11:18:40 +0200

    Add RtcEventLogFactory factory with explicit TaskQueueFactory
    remove RtcEventLog factory function that relies on GlobalTaskQueueFactory,
    move that default behaviour up to RtcEventLogFactory level.
    
    Bug: webrtc:10284
    Change-Id: I512d8a13e6a2f320000dd08e6355c0a7e9de8561

--- logging/rtc_event_log/rtc_event_log_factory.h
+++ logging/rtc_event_log/rtc_event_log_factory.h
@@ -13,6 +13,7 @@
 
 #include <memory>
 
+#include "api/task_queue/task_queue_factory.h"
 #include "logging/rtc_event_log/rtc_event_log.h"
 #include "logging/rtc_event_log/rtc_event_log_factory_interface.h"
 
@@ -20,13 +21,22 @@ namespace webrtc {
 
 class RtcEventLogFactory : public RtcEventLogFactoryInterface {
  public:
+  explicit RtcEventLogFactory(TaskQueueFactory* task_queue_factory);
   ~RtcEventLogFactory() override {}
 
   std::unique_ptr<RtcEventLog> CreateRtcEventLog(
       RtcEventLog::EncodingType encoding_type) override;
+
+ private:
+  TaskQueueFactory* const task_queue_factory_;
 };
 
+// TODO(bugs.webrtc.org/10284): Stop using and deprecate this version of the
+// RtcEventLogFactory factory.
 std::unique_ptr<RtcEventLogFactoryInterface> CreateRtcEventLogFactory();
+
+std::unique_ptr<RtcEventLogFactoryInterface> CreateRtcEventLogFactory(
+    TaskQueueFactory* task_queue_factory);
 }  // namespace webrtc
 
 #endif  // LOGGING_RTC_EVENT_LOG_RTC_EVENT_LOG_FACTORY_H_

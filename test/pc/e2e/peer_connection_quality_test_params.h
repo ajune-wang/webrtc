2b7779144f7ad94173ce7483b17805062fcab98f
Danil Chapovalov <danilchap@webrtc.org>
2019-04-16 19:53:08 +0200

    Create webrtc:pc_e2e::TestPeer avoiding factories that use GlobalTaskQueueFactory
    Bug: webrtc:10284
    Change-Id: I377da1021e8e84bb664cb2013b6bf17c724eb36b

--- test/pc/e2e/peer_connection_quality_test_params.h
+++ test/pc/e2e/peer_connection_quality_test_params.h
@@ -42,6 +42,7 @@ namespace webrtc_pc_e2e {
 // can override only some parts of media engine like video encoder/decoder
 // factories.
 struct PeerConnectionFactoryComponents {
+  std::unique_ptr<TaskQueueFactory> task_queue_factory;
   std::unique_ptr<CallFactoryInterface> call_factory;
   std::unique_ptr<RtcEventLogFactoryInterface> event_log_factory;
   std::unique_ptr<FecControllerFactoryInterface> fec_controller_factory;

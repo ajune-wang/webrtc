47869ea37c75e87377afbe51dada79768f9edfc5
Fredrik Hernqvist <fhernqvist@webrtc.org>
2019-05-08 17:43:32 +0200

    Add argname
    Change-Id: Iac1111b739458a1b0ce1cac5e59de06905c085d2

--- modules/audio_processing/audio_processing_impl_unittest.cc
+++ modules/audio_processing/audio_processing_impl_unittest.cc
@@ -240,11 +240,15 @@ TEST(AudioProcessingImplTest,
   MockEchoControl* echo_control_mock = echo_control_factory_ptr->GetNext();
 
   EXPECT_CALL(*echo_control_mock, AnalyzeCapture(NotNull())).Times(1);
-  EXPECT_CALL(*echo_control_mock, ProcessCapture(NotNull(), false)).Times(1);
+  EXPECT_CALL(*echo_control_mock,
+              ProcessCapture(NotNull(), false /* echo_path_change */))
+      .Times(1);
   apm->ProcessStream(&frame);
 
   EXPECT_CALL(*echo_control_mock, AnalyzeCapture(NotNull())).Times(1);
-  EXPECT_CALL(*echo_control_mock, ProcessCapture(NotNull(), true)).Times(1);
+  EXPECT_CALL(*echo_control_mock,
+              ProcessCapture(NotNull(), true /* echo_path_change */))
+      .Times(1);
   apm->SetRuntimeSetting(
       AudioProcessing::RuntimeSetting::CreateCapturePreGain(2.f));
   apm->ProcessStream(&frame);

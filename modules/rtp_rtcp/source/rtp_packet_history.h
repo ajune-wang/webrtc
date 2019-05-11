ba11f6391cc4d59029f9901271ba0207feb1c8f7
Erik Språng <sprang@webrtc.org>
2019-05-11 11:42:51 -0700

    Add field trial for selecting potentially useful packets as padding.
    Currently, the packet in the history that most closely matches the bit
    budget between two PacedSender::Process() calls is selected to be
    retransmitted. This usually means that the smallest packet in the
    history is selected over and over.
    
    With this new field trial, we ignore the size constraint (since you're
    sending padding, you obviously have some bandwidth to spare) and
    instead prefer packets that have the fewest transmission times first,
    and after that we prefer new packets over older ones. This way, in
    case of avilable bandwidth but small loss, these padding pakcets have
    a greater chance of actually being useful to the receiver.
    
    WIP, need to add tests.
    
    Change-Id: I15a69231f44bfbefcb9ab38dd7886b966e3af6fe

--- modules/rtp_rtcp/source/rtp_packet_history.h
+++ modules/rtp_rtcp/source/rtp_packet_history.h
@@ -13,8 +13,10 @@
 
 #include <map>
 #include <memory>
+#include <set>
 #include <vector>
 
+#include "api/transport/webrtc_key_value_config.h"
 #include "modules/rtp_rtcp/include/rtp_rtcp_defines.h"
 #include "rtc_base/constructor_magic.h"
 #include "rtc_base/critical_section.h"
@@ -59,6 +61,8 @@ class RtpPacketHistory {
   static constexpr int kPacketCullingDelayFactor = 3;
 
   explicit RtpPacketHistory(Clock* clock);
+  explicit RtpPacketHistory(Clock* clock,
+                            const WebRtcKeyValueConfig& field_trials);
   ~RtpPacketHistory();
 
   // Set/get storage mode. Note that setting the state will clear the history,
@@ -90,6 +94,12 @@ class RtpPacketHistory {
   std::unique_ptr<RtpPacketToSend> GetBestFittingPacket(
       size_t packet_size) const;
 
+  // Get the packet (if any) from the history, that is deemed most likely to
+  // the remote side. This is calculated from heuristics such as packet age
+  // and times retransmitted. Updated the send time of the packet, so is not
+  // a const method.
+  std::unique_ptr<RtpPacketToSend> GetPayloadPaddingPacket();
+
   // Cull packets that have been acknowledged as received by the remote end.
   void CullAcknowledgedPackets(rtc::ArrayView<const uint16_t> sequence_numbers);
 
@@ -120,6 +130,10 @@ class RtpPacketHistory {
 
     // True if the packet is currently in the pacer queue pending transmission.
     bool pending_transmission = false;
+
+    // Unique number per StoredPacket, incremented by one for each added
+    // packet. Used to sort on insert order.
+    uint64_t insert_order = 0;
   };
 
   using StoredPacketIterator = std::map<uint16_t, StoredPacket>::iterator;
@@ -147,6 +161,13 @@ class RtpPacketHistory {
   std::map<uint16_t, StoredPacket> packet_history_ RTC_GUARDED_BY(lock_);
   // Map from packet size to sequence number.
   std::map<size_t, uint16_t> packet_size_ RTC_GUARDED_BY(lock_);
+
+  // Total number of packets with StorageType::kAllowsRetransmission inserted.
+  uint64_t retransmittable_packets_inserted_ RTC_GUARDED_BY(lock_);
+  // Retransmittable objects from |packet_history_| ordered by
+  // "most likely to be useful", used in GetPayloadPaddingPacket().
+  std::set<StoredPacket*, std::function<bool(StoredPacket*, StoredPacket*)>>
+      padding_priority_ RTC_GUARDED_BY(lock_);
 
   // The earliest packet in the history. This might not be the lowest sequence
   // number, in case there is a wraparound.

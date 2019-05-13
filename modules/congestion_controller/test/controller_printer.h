b43ad90bf3b9578223a2341df7b7173a9d60d6d9
Sebastian Jansson <srte@webrtc.org>
2019-05-13 15:40:08 +0200

    Refactor of GoogCC debug printer.
    Simplifying the code to better fit with how it is used.
    
    Bug: webrtc:9883
    Change-Id: I2bd52f26b829413e516dee4f551cf36574275019

--- modules/congestion_controller/test/controller_printer.h
+++ modules/congestion_controller/test/controller_printer.h
@@ -12,32 +12,14 @@
 
 #include <memory>
 
-#include "api/transport/network_types.h"
 #include "api/units/timestamp.h"
-#include "test/logging/log_writer.h"
 
 namespace webrtc {
-class DebugStatePrinter {
- public:
-  virtual bool Attached() const = 0;
-  virtual void PrintHeaders(RtcEventLogOutput* out) = 0;
-  virtual void PrintValues(RtcEventLogOutput* out) = 0;
-  virtual NetworkControlUpdate GetState(Timestamp at_time) const = 0;
-  virtual ~DebugStatePrinter() = default;
-};
 
 class ControlStatePrinter {
  public:
-  ControlStatePrinter(std::unique_ptr<RtcEventLogOutput> output,
-                      std::unique_ptr<DebugStatePrinter> debug_printer);
-  ~ControlStatePrinter();
-  void PrintHeaders();
-  void PrintState(const Timestamp time, const NetworkControlUpdate state);
-  void PrintState(const Timestamp time);
-
- private:
-  std::unique_ptr<RtcEventLogOutput> output_;
-  std::unique_ptr<DebugStatePrinter> debug_printer_;
+  virtual ~ControlStatePrinter() = default;
+  virtual void PrintState(const Timestamp at_time) = 0;
 };
 }  // namespace webrtc
 

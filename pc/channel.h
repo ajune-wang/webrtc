a4456884174365a12071061027601688a851f1d7
Anton Sukhanov <sukhanov@webrtc.org>
2019-05-16 12:14:19 -0700

    Introduce MediaTransportConfig
    Currently we pass media_transport from PeerConnection to media layers. The goal of this change is to replace media_transport with struct MediaTransportCondif, which will enable adding different transports (i.e. we plan to add DatagramTransport) as well as other media-transport related settings without changing 100s of files.
    
    TODO: In the future we should consider also adding rtp_transport in the same config, but it will require a bit more work, so I did not include it in the same change.
    
    
    Bug: webrtc:9719
    Change-Id: Ie31e1faa3ed9e6beefe30a3da208130509ce00cd

--- pc/channel.h
+++ pc/channel.h
@@ -20,7 +20,7 @@
 
 #include "api/call/audio_sink.h"
 #include "api/jsep.h"
-#include "api/media_transport_interface.h"
+#include "api/media_transport_config.h"
 #include "api/rtp_receiver_interface.h"
 #include "api/video/video_sink_interface.h"
 #include "api/video/video_source_interface.h"
@@ -93,7 +93,7 @@ class BaseChannel : public ChannelInterface,
               rtc::UniqueRandomIdGenerator* ssrc_generator);
   virtual ~BaseChannel();
   virtual void Init_w(webrtc::RtpTransportInternal* rtp_transport,
-                      webrtc::MediaTransportInterface* media_transport);
+                      webrtc::MediaTransportConfig media_transport_config);
 
   // Deinit may be called multiple times and is simply ignored if it's already
   // done.
@@ -169,7 +169,7 @@ class BaseChannel : public ChannelInterface,
 
   // Returns media transport, can be null if media transport is not available.
   webrtc::MediaTransportInterface* media_transport() {
-    return media_transport_;
+    return media_transport_config_.media_transport;
   }
 
   // From RtpTransport - public for testing only
@@ -322,10 +322,8 @@ class BaseChannel : public ChannelInterface,
 
   webrtc::RtpTransportInternal* rtp_transport_ = nullptr;
 
-  // Optional media transport (experimental).
-  // If provided, audio and video will be sent through media_transport instead
-  // of RTP/RTCP. Currently media_transport can co-exist with rtp_transport.
-  webrtc::MediaTransportInterface* media_transport_ = nullptr;
+  // Optional media transport configuration (experimental).
+  webrtc::MediaTransportConfig media_transport_config_;
 
   std::vector<std::pair<rtc::Socket::Option, int> > socket_options_;
   std::vector<std::pair<rtc::Socket::Option, int> > rtcp_socket_options_;
@@ -380,7 +378,7 @@ class VoiceChannel : public BaseChannel,
     return cricket::MEDIA_TYPE_AUDIO;
   }
   void Init_w(webrtc::RtpTransportInternal* rtp_transport,
-              webrtc::MediaTransportInterface* media_transport) override;
+              webrtc::MediaTransportConfig media_transport_config) override;
 
  private:
   // overrides from BaseChannel
@@ -462,9 +460,8 @@ class RtpDataChannel : public BaseChannel {
               DtlsTransportInternal* rtcp_dtls_transport,
               rtc::PacketTransportInternal* rtp_packet_transport,
               rtc::PacketTransportInternal* rtcp_packet_transport);
-  void Init_w(
-      webrtc::RtpTransportInternal* rtp_transport,
-      webrtc::MediaTransportInterface* media_transport = nullptr) override;
+  void Init_w(webrtc::RtpTransportInternal* rtp_transport,
+              webrtc::MediaTransportConfig media_transport_config) override;
 
   virtual bool SendData(const SendDataParams& params,
                         const rtc::CopyOnWriteBuffer& payload,

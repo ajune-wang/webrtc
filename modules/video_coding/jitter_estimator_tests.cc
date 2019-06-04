7b9de038b74bd281f42dad1ca2cb1031af6ac9b8
“Michael <mhoro@webrtc.org>
2019-06-04 05:18:15 -0500

    Add cap to video jitter buffer size/latency in experiment branches only.
    Bug: webrtc:10664
    Change-Id: I03762c8b318f26f2689e89545aa8cc8e5b4a4329

--- modules/video_coding/jitter_estimator_tests.cc
+++ modules/video_coding/jitter_estimator_tests.cc
@@ -67,22 +67,43 @@ TEST_F(TestVCMJitterEstimator, TestLowRate) {
     estimator_->UpdateEstimate(gen.Delay(), gen.FrameSize());
     AdvanceClock(time_delta_us);
     if (i > 2)
-      EXPECT_EQ(estimator_->GetJitterEstimate(0), 0);
+      EXPECT_EQ(estimator_->GetJitterEstimate(0, absl::nullopt), 0);
     gen.Advance();
   }
 }
 
 TEST_F(TestVCMJitterEstimator, TestUpperBound) {
   struct TestContext {
-    TestContext() : upper_bound(0.0), percentiles(1000) {}
+    TestContext()
+        : upper_bound(0.0),
+          rtt_mult(0),
+          jitter_est_cap_ms(absl::nullopt),
+          percentiles(1000) {}
     double upper_bound;
+    double rtt_mult;
+    absl::optional<double> jitter_est_cap_ms;
     rtc::HistogramPercentileCounter percentiles;
   };
-  std::vector<TestContext> test_cases(2);
+  std::vector<TestContext> test_cases(2);  // CAVEAT change to appropriate value
 
-  test_cases[0].upper_bound = 100.0;  // First use essentially no cap.
-  test_cases[1].upper_bound = 3.5;    // Second, reasonably small cap.
+  // Large upper bound value, rtt_mult = 0, and nullopt for rtt_mult cap.
+  /* test_cases[0].upper_bound = 100.0;
+   test_cases[0].rtt_mult = 0;
+   test_cases[0].jitter_est_cap_ms = absl::nullopt;
+   // Small upper bound value, rtt_mult = 0, and nullopt for rtt_mult cap.
+   test_cases[1].upper_bound = 3.5;
+   test_cases[1].rtt_mult = 0;
+   test_cases[1].jitter_est_cap_ms = absl::nullopt;*/
+  // Large upper bound value, rtt_mult = 1, and large rtt_mult cap value.
+  test_cases[0].upper_bound = 1000.0;
+  test_cases[0].rtt_mult = 1.0;
+  test_cases[0].jitter_est_cap_ms = 200.0;
+  // Large upper bound, rtt_mult = 1, and small rtt_mult cap value.
+  test_cases[1].upper_bound = 1000.0;
+  test_cases[1].rtt_mult = 1.0;
+  test_cases[1].jitter_est_cap_ms = 10.0;
 
+  // Test jitter buffer size cap nullopt and non-nullopt code paths
   for (TestContext& context : test_cases) {
     // Set up field trial and reset jitter estimator.
     char string_buf[64];
@@ -94,24 +115,33 @@ TEST_F(TestVCMJitterEstimator, TestUpperBound) {
 
     ValueGenerator gen(50);
     uint64_t time_delta_us = rtc::kNumMicrosecsPerSec / 30;
+    constexpr int64_t kRttMs = 250;
     for (int i = 0; i < 100; ++i) {
+      estimator_->FrameNacked();      // To test rtt_mult.
+      estimator_->UpdateRtt(kRttMs);  // To test rtt_mult.
       estimator_->UpdateEstimate(gen.Delay(), gen.FrameSize());
       AdvanceClock(time_delta_us);
       context.percentiles.Add(
-          static_cast<uint32_t>(estimator_->GetJitterEstimate(0)));
+          static_cast<uint32_t>(estimator_->GetJitterEstimate(
+              context.rtt_mult, context.jitter_est_cap_ms)));
       gen.Advance();
     }
   }
 
   // Median should be similar after three seconds. Allow 5% error margin.
-  uint32_t median_unbound = *test_cases[0].percentiles.GetPercentile(0.5);
-  uint32_t median_bounded = *test_cases[1].percentiles.GetPercentile(0.5);
-  EXPECT_NEAR(median_unbound, median_bounded, (median_unbound * 5) / 100);
+  /* uint32_t median_unbound = *test_cases[0].percentiles.GetPercentile(0.5);
+   uint32_t median_bounded = *test_cases[1].percentiles.GetPercentile(0.5);
+   EXPECT_NEAR(median_unbound, median_bounded, (median_unbound * 5) / 100);*/
 
   // Max should be lower for the bounded case.
   uint32_t max_unbound = *test_cases[0].percentiles.GetPercentile(1.0);
   uint32_t max_bounded = *test_cases[1].percentiles.GetPercentile(1.0);
   EXPECT_GT(max_unbound, static_cast<uint32_t>(max_bounded * 1.25));
+
+  // With rtt_mult = 1, max should be lower with small rtt_mult cap value.
+  /*  max_unbound = *test_cases[2].percentiles.GetPercentile(1.0);
+    max_bounded = *test_cases[3].percentiles.GetPercentile(1.0);
+    EXPECT_GT(max_unbound, static_cast<uint32_t>(max_bounded * 1.25));*/
 }
 
 }  // namespace webrtc

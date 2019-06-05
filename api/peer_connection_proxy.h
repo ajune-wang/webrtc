8d231309002f35a33ed30f58a7c3b4f2fc3a359d
Mirko Bonadei <mbonadei@webrtc.org>
2019-06-05 10:25:52 +0200

    Add PROXY_METHOD for the new PeerConnectionInterface::StartRtcEventLog.
    This overload has been introduced in [1] but the PROXY_METHOD hasn't
    been updated.
    
    [1] - https://webrtc-review.googlesource.com/c/src/+/135944
    
    Bug: webrtc:10716
    Change-Id: Ic6dc2f72d33e22e76eae5e7deb9dababc88b4486

--- api/peer_connection_proxy.h
+++ api/peer_connection_proxy.h
@@ -131,6 +131,7 @@ PROXY_METHOD0(IceConnectionState, ice_connection_state)
 PROXY_METHOD0(IceConnectionState, standardized_ice_connection_state)
 PROXY_METHOD0(PeerConnectionState, peer_connection_state)
 PROXY_METHOD0(IceGatheringState, ice_gathering_state)
+PROXY_METHOD1(bool, StartRtcEventLog, std::unique_ptr<RtcEventLogOutput>)
 PROXY_METHOD2(bool,
               StartRtcEventLog,
               std::unique_ptr<RtcEventLogOutput>,

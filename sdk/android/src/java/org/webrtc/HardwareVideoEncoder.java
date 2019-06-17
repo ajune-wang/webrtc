749201f37f98ce36826f3a76eb70846ada4a9202
Sergey Silkin <ssilkin@webrtc.org>
2019-06-17 11:39:14 +0200

    Add resolution bitrate thresholds to EncoderInfo.
    Bug: none
    Change-Id: Ida45ea832041963b8b8475d69114b5c60a172fb7

--- sdk/android/src/java/org/webrtc/HardwareVideoEncoder.java
+++ sdk/android/src/java/org/webrtc/HardwareVideoEncoder.java
@@ -21,7 +21,9 @@ import android.support.annotation.Nullable;
 import android.view.Surface;
 import java.io.IOException;
 import java.nio.ByteBuffer;
+import java.util.Collections;
 import java.util.Deque;
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.BlockingDeque;
 import java.util.concurrent.LinkedBlockingDeque;
@@ -428,6 +430,11 @@ class HardwareVideoEncoder implements VideoEncoder {
       }
     }
     return ScalingSettings.OFF;
+  }
+
+  @Override
+  public List<ResolutionBitrateThresholds> getResolutionBitrateThresholds() {
+    return Collections.emptyList();
   }
 
   @Override

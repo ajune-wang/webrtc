d80ba5a36dca763105825b312743c77a94bce236
Erik Språng <sprang@webrtc.org>
2019-07-30 16:40:04 +0200

    Fix SendSideBweWithOverhead using new pacer path
    Bug: webrtc:10633
    Change-Id: If1148f39c463e11ad49a659913465f131cf9b526

--- modules/pacing/round_robin_packet_queue.cc
+++ modules/pacing/round_robin_packet_queue.cc
@@ -71,8 +71,13 @@ RoundRobinPacketQueue::Stream::Stream() : bytes(0), ssrc(0) {}
 RoundRobinPacketQueue::Stream::Stream(const Stream& stream) = default;
 RoundRobinPacketQueue::Stream::~Stream() {}
 
-RoundRobinPacketQueue::RoundRobinPacketQueue(int64_t start_time_us)
-    : time_last_updated_ms_(start_time_us / 1000) {}
+RoundRobinPacketQueue::RoundRobinPacketQueue(
+    int64_t start_time_us,
+    const WebRtcKeyValueConfig& field_trials)
+    : time_last_updated_ms_(start_time_us / 1000),
+      send_side_bwe_with_overhead_(
+          field_trials.Lookup("WebRTC-SendSideBwe-WithOverhead")
+              .find("Enabled") == 0) {}
 
 RoundRobinPacketQueue::~RoundRobinPacketQueue() {}
 
@@ -98,7 +103,9 @@ void RoundRobinPacketQueue::Push(int priority,
   uint32_t ssrc = packet->Ssrc();
   uint16_t sequence_number = packet->SequenceNumber();
   int64_t capture_time_ms = packet->capture_time_ms();
-  size_t size_bytes = packet->payload_size() + packet->padding_size();
+  size_t size_bytes = send_side_bwe_with_overhead_
+                          ? packet->size()
+                          : packet->payload_size() + packet->padding_size();
   auto type = packet->packet_type();
   RTC_DCHECK(type.has_value());
 

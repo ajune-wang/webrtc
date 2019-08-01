f823e886ec420aedba4ba586a1cf3110a68ea5d3
Sebastian Jansson <srte@webrtc.org>
2019-08-01 10:58:33 +0200

    Adds PeerConnection scenario test framework.
    Bug: webrtc:10839
    Change-Id: If67eeb680d016d66c69d8e761a88c240e4931a5d

---
+++ test/peer_scenario/tests/peer_scenario_quality_test.cc
@@ -0,0 +1,36 @@
+/*
+ *  Copyright (c) 2019 The WebRTC project authors. All Rights Reserved.
+ *
+ *  Use of this source code is governed by a BSD-style license
+ *  that can be found in the LICENSE file in the root of the source
+ *  tree. An additional intellectual property rights grant can be found
+ *  in the file PATENTS.  All contributing project authors may
+ *  be found in the AUTHORS file in the root of the source tree.
+ */
+
+#include "test/gtest.h"
+#include "test/peer_scenario/peer_scenario.h"
+
+namespace webrtc {
+namespace test {
+
+TEST(PeerScenarioQualityTest, PsnrIsCollected) {
+  VideoQualityAnalyzerConfig analyzer_config;
+  analyzer_config.thread = rtc::Thread::Current();
+  VideoQualityAnalyzer analyzer(analyzer_config);
+  PeerScenario s;
+  auto caller = s.CreateClient(PeerScenarioClient::Config());
+  auto callee = s.CreateClient(PeerScenarioClient::Config());
+  auto video =
+      caller->CreateVideo("VIDEO", PeerScenarioClient::VideoSendTrackConfig());
+  auto link_builder = s.net()->NodeBuilder().delay_ms(100).capacity_kbps(600);
+  s.AttachVideoQualityAnalyzer(&analyzer, video.track, callee);
+  s.SimpleConnection(caller, callee, {link_builder.Build().node},
+                     {link_builder.Build().node});
+  s.ProcessMessages(TimeDelta::seconds(1));
+  EXPECT_GT(analyzer.stats().render.count, 10);
+  EXPECT_GT(analyzer.stats().psnr_with_freeze.Mean(), 20);
+}
+
+}  // namespace test
+}  // namespace webrtc

a3e55e7d8d23df0558d0f3b6948d94abaaafc53a
Tommi <tommi@webrtc.org>
2019-08-06 17:56:52 +0200

    Remove MediaTransport from Call.
    There aren't any tests for this and the code isn't currently
    active except for the fact that it adds complexity to the Call
    class, synchronization into the active code path and makes future
    improvements to the class more complex or impossible.
    
    Bug: webrtc:9719
    Change-Id: Ia41af0b2186b8a36ca70a07858990b6af7f3a5c3

--- call/degraded_call.h
+++ call/degraded_call.h
@@ -128,7 +128,6 @@ class DegradedCall : public Call, private Transport, private PacketReceiver {
   Clock* const clock_;
   const std::unique_ptr<Call> call_;
 
-  void MediaTransportChange(MediaTransportInterface* media_transport) override;
   void SetClientBitratePreferences(
       const webrtc::BitrateSettings& preferences) override {}
   const absl::optional<BuiltInNetworkBehaviorConfig> send_config_;

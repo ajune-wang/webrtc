17156f8dc19863b3630f646cfcc84708d9fdb2b9
Tommi <tommi@webrtc.org>
2019-08-06 13:26:40 +0200

    Looking into threading of RtcpRttStats impl in CallStats
    Change-Id: I54394f0e8623d0b66ec687187892d770355ee367
    Bug: webrtc:10847

--- video/call_stats.cc
+++ video/call_stats.cc
@@ -171,20 +171,26 @@ void CallStats::DeregisterStatsObserver(CallStatsObserver* observer) {
 }
 
 int64_t CallStats::LastProcessedRtt() const {
+  // TODO(tommi): On what thread does this get called? If it can be made to be
+  // the same TQ (e.g. configuration_sequence_checker_ in Call), then that TQ
+  // could be made to be the main TQ of CallStats and no lock would be required
+  // here.
+  // TEST:
+  //  RTC_DCHECK_RUN_ON(&process_thread_checker_);
+
   rtc::CritScope cs(&avg_rtt_ms_lock_);
   return avg_rtt_ms_;
 }
 
 void CallStats::OnRttUpdate(int64_t rtt) {
-  int64_t now_ms = clock_->TimeInMilliseconds();
-  process_thread_->PostTask(ToQueuedTask([rtt, now_ms, this]() {
-    RTC_DCHECK_RUN_ON(&process_thread_checker_);
-    reports_.push_back(RttTime(rtt, now_ms));
-    if (time_of_first_rtt_ms_ == -1)
-      time_of_first_rtt_ms_ = now_ms;
+  RTC_DCHECK_RUN_ON(&process_thread_checker_);
 
-    process_thread_->WakeUp(this);
-  }));
+  int64_t now_ms = clock_->TimeInMilliseconds();
+  reports_.push_back(RttTime(rtt, now_ms));
+  if (time_of_first_rtt_ms_ == -1)
+    time_of_first_rtt_ms_ = now_ms;
+
+  process_thread_->WakeUp(this);
 }
 
 void CallStats::UpdateHistograms() {

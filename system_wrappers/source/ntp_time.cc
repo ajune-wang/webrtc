9a8b0078be0cc2bfa2eaba61e97a19b2d1ed09f0
Chen Xing <chxg@google.com>
2019-08-07 14:29:41 +0200

    Add helper functions to convert between regular Unix timestamps and Q-format NTP timestamps.
    This change adds the following helper functions to convert between "integer milliseconds"-style Unix timestamps/durations, and "UQ32.32 and Q32.32"-style NTP timestamps/durations:
    - UnixTimeMsToNtpTimeUQ32x32
    - NtpTimeUQ32x32ToUnixTimeMs
    - DurationMsToDurationQ32x32
    - DurationQ32x32ToDurationMs
    
    The Q-format NTP timestamps/durations are used by some RTP/RTCP packets.
    
    Bug: webrtc:10739
    Change-Id: I89123d2dba7370f26e239d722a4975bf5ac6e668

---
+++ system_wrappers/source/ntp_time.cc
@@ -0,0 +1,75 @@
+/*
+ *  Copyright (c) 2019 The WebRTC project authors. All Rights Reserved.
+ *
+ *  Use of this source code is governed by a BSD-style license
+ *  that can be found in the LICENSE file in the root of the source
+ *  tree. An additional intellectual property rights grant can be found
+ *  in the file PATENTS.  All contributing project authors may
+ *  be found in the AUTHORS file in the root of the source tree.
+ */
+
+#include "system_wrappers/include/ntp_time.h"
+
+#include "system_wrappers/include/clock.h"
+
+namespace webrtc {
+namespace {
+
+template <typename InputType, typename OutputType>
+OutputType ConvertFixedPointTime(InputType input,
+                                 InputType input_seconds_divisor,
+                                 OutputType output_seconds_multiplier,
+                                 OutputType output_seconds_offset) {
+  InputType input_seconds = input / input_seconds_divisor;
+  InputType input_fractional = input % input_seconds_divisor;
+
+  if (input_fractional < 0) {
+    input_seconds -= 1;
+    input_fractional += input_seconds_divisor;
+  }
+
+  OutputType output_seconds = static_cast<uint64_t>(input_seconds) +
+                              static_cast<uint64_t>(output_seconds_offset);
+
+  OutputType output_fractional;
+  {
+    uint64_t numerator = static_cast<uint64_t>(input_fractional) *
+                         static_cast<uint64_t>(output_seconds_multiplier);
+    uint64_t denominator = static_cast<uint64_t>(input_seconds_divisor);
+
+    uint64_t quotient = numerator / denominator;
+    uint64_t remainder = numerator % denominator;
+
+    if (remainder >= ((denominator + 1) / 2)) {
+      output_fractional = quotient + 1;
+    } else {
+      output_fractional = quotient;
+    }
+  }
+
+  return (output_seconds * output_seconds_multiplier) + output_fractional;
+}
+
+}  // namespace
+
+uint64_t UnixTimeMsToNtpTimeUQ32x32(int64_t unix_time_ms) {
+  return ConvertFixedPointTime<int64_t, uint64_t>(
+      unix_time_ms, 1000, NtpTime::kFractionsPerSecond, 2208988800);
+}
+
+int64_t NtpTimeUQ32x32ToUnixTimeMs(uint64_t ntp_time_uq32x32) {
+  return ConvertFixedPointTime<uint64_t, int64_t>(
+      ntp_time_uq32x32, NtpTime::kFractionsPerSecond, 1000, -2208988800);
+}
+
+int64_t DurationMsToDurationQ32x32(int64_t duration_ms) {
+  return ConvertFixedPointTime<int64_t, int64_t>(
+      duration_ms, 1000, NtpTime::kFractionsPerSecond, 0);
+}
+
+int64_t DurationQ32x32ToDurationMs(int64_t duration_q32x32) {
+  return ConvertFixedPointTime<int64_t, int64_t>(
+      duration_q32x32, NtpTime::kFractionsPerSecond, 1000, 0);
+}
+
+}  // namespace webrtc

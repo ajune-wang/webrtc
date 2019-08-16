b7eeabd53018c749b13c5fc7ba8a04c3491791f7
Ying Wang <yinwa@webrtc.org>
2019-08-16 14:46:37 +0200

    Allowing 40ms audio frame length. Currently 20ms, 60ms and 120ms frame length are supported. The motivation is to better adapt audio bit rate to network conditions with more frame length choices.
    This is continuation of https://webrtc-review.googlesource.com/c/src/+/146206, since crodbro is out of office, I created this commit for continuing the code review.
    
    Bug: webrtc:10820
    Change-Id: I0e35e91b524f63686bfdf767b7a95c51aeb24716

--- modules/audio_coding/audio_network_adaptor/controller_manager.cc
+++ modules/audio_coding/audio_network_adaptor/controller_manager.cc
@@ -120,13 +120,25 @@ std::unique_ptr<FrameLengthController> CreateFrameLengthController(
   RTC_CHECK(config.has_fl_decreasing_packet_loss_fraction());
   RTC_CHECK(config.has_fl_20ms_to_60ms_bandwidth_bps());
   RTC_CHECK(config.has_fl_60ms_to_20ms_bandwidth_bps());
+  RTC_CHECK(config.has_fl_20ms_to_40ms_bandwidth_bps());
+  RTC_CHECK(config.has_fl_40ms_to_20ms_bandwidth_bps());
+  RTC_CHECK(config.has_fl_40ms_to_60ms_bandwidth_bps());
+  RTC_CHECK(config.has_fl_60ms_to_40ms_bandwidth_bps());
 
   std::map<FrameLengthController::Config::FrameLengthChange, int>
       fl_changing_bandwidths_bps = {
           {FrameLengthController::Config::FrameLengthChange(20, 60),
            config.fl_20ms_to_60ms_bandwidth_bps()},
           {FrameLengthController::Config::FrameLengthChange(60, 20),
-           config.fl_60ms_to_20ms_bandwidth_bps()}};
+           config.fl_60ms_to_20ms_bandwidth_bps()},
+          {FrameLengthController::Config::FrameLengthChange(20, 40),
+           config.fl_20ms_to_40ms_bandwidth_bps()},
+          {FrameLengthController::Config::FrameLengthChange(40, 20),
+           config.fl_40ms_to_20ms_bandwidth_bps()},
+          {FrameLengthController::Config::FrameLengthChange(40, 60),
+           config.fl_40ms_to_60ms_bandwidth_bps()},
+          {FrameLengthController::Config::FrameLengthChange(60, 40),
+           config.fl_60ms_to_40ms_bandwidth_bps()}};
 
   if (config.has_fl_60ms_to_120ms_bandwidth_bps() &&
       config.has_fl_120ms_to_60ms_bandwidth_bps()) {

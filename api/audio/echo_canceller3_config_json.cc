c6b83f2c3acf2449e6fbeded8578bb8e4a44ecca
Gustaf Ullberg <gustaf@webrtc.org>
2019-09-09 13:04:55 +0200

    AEC3: Downmix multichannel signals before delay estimation
    Multichannel signals are downmixed to mono before decimation and
    delay estimation. This is useful when not all channels play
    audio content. The feature can be toggled in the AEC3 configuration.
    
    Bug: webrtc:10913
    Change-Id: I7d40edf7732bb51fec69e7f3ca063d821c5069c4

--- api/audio/echo_canceller3_config_json.cc
+++ api/audio/echo_canceller3_config_json.cc
@@ -171,6 +171,8 @@ void Aec3ConfigFromJsonString(absl::string_view json_string,
 
     ReadParam(section, "use_external_delay_estimator",
               &cfg.delay.use_external_delay_estimator);
+    ReadParam(section, "downmix_before_delay_estimation",
+              &cfg.delay.downmix_before_delay_estimation);
   }
 
   if (rtc::GetValueFromJsonObject(aec3_root, "filter", &section)) {
@@ -352,6 +354,10 @@ std::string Aec3ConfigToJsonString(const EchoCanceller3Config& config) {
   ost << "\"converged\": " << config.delay.delay_selection_thresholds.converged;
   ost << "}";
 
+  ost << "\"use_external_delay_estimator\": "
+      << (config.delay.use_external_delay_estimator ? "true" : "false");
+  ost << "\"downmix_before_delay_estimation\": "
+      << (config.delay.downmix_before_delay_estimation ? "true" : "false");
   ost << "},";
 
   ost << "\"filter\": {";

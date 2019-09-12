cf4e7fa5ea36eafcb44c4f129c7e8a45be7bdd59
Sam Zackrisson <saza@webrtc.org>
2019-09-12 14:35:45 +0200

    Add parameter to APM builder to force multichannel AEC
    Bug: webrtc:10913
    Change-Id: I38795bf8f312b959fcc816a056fba2c68d4e424d

--- modules/audio_processing/test/audio_processing_simulator.cc
+++ modules/audio_processing/test/audio_processing_simulator.cc
@@ -377,6 +377,11 @@ void AudioProcessingSimulator::CreateAudioProcessor() {
   if (settings_.use_ts) {
     config.Set<ExperimentalNs>(new ExperimentalNs(*settings_.use_ts));
   }
+  if (settings_.experimental_multichannel_echo_control) {
+    apm_config.pipeline.experimental_multichannel_echo_control =
+        *settings_.experimental_multichannel_echo_control;
+  }
+
   if (settings_.use_agc2) {
     apm_config.gain_controller2.enabled = *settings_.use_agc2;
     if (settings_.agc2_fixed_gain_db) {

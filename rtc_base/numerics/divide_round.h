ae4cc9fa5a193b03fce15761a1c77d56a860da29
Danil Chapovalov <danilchap@webrtc.org>
2019-09-18 14:01:27 +0200

    Introduce integer division helpers with non-default rounding
    There are multiple places in webrtc code where alternative than
    default rounding is desired. Typically this rounding is inlined.
    e.g. as (<x> + <y>/2) / <y> making code more clumpsy (<y> might be long expression)
    and unsafe for large values of <x>
    
    This change introduce small helpers to address both concerns.
    
    Bug: None
    Change-Id: Icd8dcee80a697b7c50ba0b2e50295087d2be8670

---
+++ rtc_base/numerics/divide_round.h
@@ -0,0 +1,47 @@
+/*
+ *  Copyright 2019 The WebRTC Project Authors. All rights reserved.
+ *
+ *  Use of this source code is governed by a BSD-style license
+ *  that can be found in the LICENSE file in the root of the source
+ *  tree. An additional intellectual property rights grant can be found
+ *  in the file PATENTS.  All contributing project authors may
+ *  be found in the AUTHORS file in the root of the source tree.
+ */
+
+#ifndef RTC_BASE_NUMERICS_DIVIDE_ROUND_H_
+#define RTC_BASE_NUMERICS_DIVIDE_ROUND_H_
+
+#include <type_traits>
+
+#include "rtc_base/checks.h"
+
+namespace webrtc {
+
+template <typename Dividend, typename Divisor>
+inline auto constexpr DivideRoundUp(Dividend dividend, Divisor divisor) {
+  static_assert(std::is_integral<Dividend>::value, "");
+  static_assert(std::is_integral<Divisor>::value, "");
+  RTC_DCHECK_GE(dividend, 0);
+  RTC_DCHECK_GT(divisor, 0);
+
+  auto q = dividend / divisor;  // Quotient.
+  auto r = dividend % divisor;  // Remainder.
+  return q + (r > 0 ? 1 : 0);
+}
+
+template <typename Dividend, typename Divisor>
+inline auto constexpr DivideRoundToNearest(Dividend dividend, Divisor divisor) {
+  static_assert(std::is_integral<Dividend>::value, "");
+  static_assert(std::is_integral<Divisor>::value, "");
+  RTC_DCHECK_GE(dividend, 0);
+  RTC_DCHECK_GT(divisor, 0);
+
+  auto half_of_divisor = (divisor - 1) / 2;
+  auto q = dividend / divisor;  // Quotient.
+  auto r = dividend % divisor;  // Remainder.
+  return q + (static_cast<Divisor>(r) > half_of_divisor ? 1 : 0);
+}
+
+}  // namespace webrtc
+
+#endif  // RTC_BASE_NUMERICS_DIVIDE_ROUND_H_

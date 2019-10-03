9326cda688501a78a339ffb4c780e8ff5be41414
Sergey Silkin <ssilkin@webrtc.org>
2019-10-03 17:47:25 +0200

    Store and insert in-band SPS/PPS before IDR.
    SPS/PPS packets could have marker bit set and their timestamp could be
    different from timestamp of the following IDR frame. To prevent
    interpreting such packet as a separate frame the following changes have
    been made to the H264 SPS/PPS tracker:
    - Unset frame begin/end flags if packet doesn't contain H264 VCL NALUs.
    - Store in-band SPS/PPS. This replaces out-of-band headers with the
    same IDs.
    - Insert in-band SPS/PPS before first packet of IDR frame if timestamp
    of SPS/PPS packet is different from timestamp of IDR packet and there
    are no gaps in RTP sequence number between SPS,PPS and IDR.
    
    Bug: chromium:999807
    
    Change-Id: I8b6de1868d49cf3947abfe05c8cc897c49403458

--- modules/video_coding/h264_sps_pps_tracker.cc
+++ modules/video_coding/h264_sps_pps_tracker.cc
@@ -26,6 +26,7 @@ namespace webrtc {
 namespace video_coding {
 
 namespace {
+constexpr size_t kNaluHeaderSize = 1;
 const uint8_t start_code_h264[] = {0, 0, 0, 1};
 }  // namespace
 
@@ -44,6 +45,24 @@ H264SpsPpsTracker::SpsInfo& H264SpsPpsTracker::SpsInfo::operator=(
     SpsInfo&& rhs) = default;
 H264SpsPpsTracker::SpsInfo::~SpsInfo() = default;
 
+static bool HasVclData(const VCMPacket& packet) {
+  const auto* h264_header =
+      absl::get_if<RTPVideoHeaderH264>(&packet.video_header.video_type_header);
+  if (h264_header->nalus_length == 0) {
+    return h264_header->nalu_type == H264::NaluType::kIdr ||
+           h264_header->nalu_type == H264::NaluType::kSlice;
+  }
+
+  for (size_t i = 0; i < h264_header->nalus_length; ++i) {
+    if (h264_header->nalus[i].type == H264::NaluType::kIdr ||
+        h264_header->nalus[i].type == H264::NaluType::kSlice) {
+      return true;
+    }
+  }
+
+  return false;
+}
+
 H264SpsPpsTracker::PacketAction H264SpsPpsTracker::CopyAndFixBitstream(
     VCMPacket* packet) {
   RTC_DCHECK(packet->codec() == kVideoCodecH264);
@@ -57,6 +76,8 @@ H264SpsPpsTracker::PacketAction H264SpsPpsTracker::CopyAndFixBitstream(
   bool append_sps_pps = false;
   auto sps = sps_data_.end();
   auto pps = pps_data_.end();
+
+  StoreInBandSpsPps(*packet);
 
   for (size_t i = 0; i < h264_header.nalus_length; ++i) {
     const NaluInfo& nalu = h264_header.nalus[i];
@@ -100,12 +121,23 @@ H264SpsPpsTracker::PacketAction H264SpsPpsTracker::CopyAndFixBitstream(
           packet->video_header.width = sps->second.width;
           packet->video_header.height = sps->second.height;
 
-          // If the SPS/PPS was supplied out of band then we will have saved
-          // the actual bitstream in |data|.
           if (sps->second.data && pps->second.data) {
             RTC_DCHECK_GT(sps->second.size, 0);
             RTC_DCHECK_GT(pps->second.size, 0);
-            append_sps_pps = true;
+            const bool out_of_band_headers =
+                !sps->second.rtp_seq_num.has_value() &&
+                !pps->second.rtp_seq_num.has_value();
+            // Insert out-of-band SPS/PPS or in-band SPS/PPS if its timestamp
+            // is different from IDR's one and there is no gap in sequence
+            // number.
+            // TODO(ssilkin): There could be other NALU(s), e.g. SEI, inbetween
+            // SPS/PPS and IDR. In this case SPS/PPS won't be inserted because
+            // there will be gap in sequence number.
+            if (out_of_band_headers ||
+                (!IsSameTimestamp(sps->second, pps->second, *packet) &&
+                 IsContinuousSeqNum(sps->second, pps->second, *packet))) {
+              append_sps_pps = true;
+            }
           }
         }
         break;
@@ -212,68 +244,156 @@ H264SpsPpsTracker::PacketAction H264SpsPpsTracker::CopyAndFixBitstream(
 
   packet->dataPtr = buffer;
   packet->sizeBytes = required_size;
+
+  // If packet does not contain VCL NAL unit(s) then reset frame begin/end flags
+  // to prevent this packet to be interpreted as a frame by the packet buffer.
+  if (packet->is_first_packet_in_frame() && packet->is_last_packet_in_frame() &&
+      !HasVclData(*packet)) {
+    packet->video_header.is_first_packet_in_frame = false;
+    packet->video_header.is_last_packet_in_frame = false;
+  }
+
   return kInsert;
 }
 
 void H264SpsPpsTracker::InsertSpsPpsNalus(const std::vector<uint8_t>& sps,
                                           const std::vector<uint8_t>& pps) {
-  constexpr size_t kNaluHeaderOffset = 1;
-  if (sps.size() < kNaluHeaderOffset) {
+  if (sps.size() < kNaluHeaderSize) {
     RTC_LOG(LS_WARNING) << "SPS size  " << sps.size() << " is smaller than "
-                        << kNaluHeaderOffset;
+                        << kNaluHeaderSize;
     return;
   }
   if ((sps[0] & 0x1f) != H264::NaluType::kSps) {
     RTC_LOG(LS_WARNING) << "SPS Nalu header missing";
     return;
   }
-  if (pps.size() < kNaluHeaderOffset) {
+  if (pps.size() < kNaluHeaderSize) {
     RTC_LOG(LS_WARNING) << "PPS size  " << pps.size() << " is smaller than "
-                        << kNaluHeaderOffset;
+                        << kNaluHeaderSize;
     return;
   }
   if ((pps[0] & 0x1f) != H264::NaluType::kPps) {
     RTC_LOG(LS_WARNING) << "SPS Nalu header missing";
     return;
   }
-  absl::optional<SpsParser::SpsState> parsed_sps = SpsParser::ParseSps(
-      sps.data() + kNaluHeaderOffset, sps.size() - kNaluHeaderOffset);
-  absl::optional<PpsParser::PpsState> parsed_pps = PpsParser::ParsePps(
-      pps.data() + kNaluHeaderOffset, pps.size() - kNaluHeaderOffset);
 
+  absl::optional<SpsParser::SpsState> parsed_sps = SpsParser::ParseSps(
+      sps.data() + kNaluHeaderSize, sps.size() - kNaluHeaderSize);
+  absl::optional<PpsParser::PpsState> parsed_pps = PpsParser::ParsePps(
+      pps.data() + kNaluHeaderSize, pps.size() - kNaluHeaderSize);
   if (!parsed_sps) {
     RTC_LOG(LS_WARNING) << "Failed to parse SPS.";
   }
-
   if (!parsed_pps) {
     RTC_LOG(LS_WARNING) << "Failed to parse PPS.";
   }
-
   if (!parsed_pps || !parsed_sps) {
     return;
   }
 
-  SpsInfo sps_info;
-  sps_info.size = sps.size();
-  sps_info.width = parsed_sps->width;
-  sps_info.height = parsed_sps->height;
-  uint8_t* sps_data = new uint8_t[sps_info.size];
-  memcpy(sps_data, sps.data(), sps_info.size);
-  sps_info.data.reset(sps_data);
-  sps_data_[parsed_sps->id] = std::move(sps_info);
+  StoreSps(sps.data(), sps.size(), parsed_sps->id, parsed_sps->width,
+           parsed_sps->height, absl::nullopt, absl::nullopt);
 
-  PpsInfo pps_info;
-  pps_info.size = pps.size();
-  pps_info.sps_id = parsed_pps->sps_id;
-  uint8_t* pps_data = new uint8_t[pps_info.size];
-  memcpy(pps_data, pps.data(), pps_info.size);
-  pps_info.data.reset(pps_data);
-  pps_data_[parsed_pps->id] = std::move(pps_info);
-
-  RTC_LOG(LS_INFO) << "Inserted SPS id " << parsed_sps->id << " and PPS id "
-                   << parsed_pps->id << " (referencing SPS "
-                   << parsed_pps->sps_id << ")";
+  StorePps(pps.data(), pps.size(), parsed_sps->id, parsed_pps->id,
+           absl::nullopt, absl::nullopt);
 }
 
+void H264SpsPpsTracker::StoreInBandSpsPps(const VCMPacket& packet) {
+  auto& h264_header =
+      absl::get<RTPVideoHeaderH264>(packet.video_header.video_type_header);
+
+  if (h264_header.packetization_type == kH264SingleNalu) {
+    RTC_DCHECK_EQ(h264_header.nalus_length, 1);
+    if (h264_header.nalus[0].type == H264::NaluType::kSps) {
+      StoreSps(packet.dataPtr, packet.sizeBytes, h264_header.nalus[0].sps_id,
+               packet.width(), packet.height(), packet.seqNum,
+               packet.timestamp);
+    } else if (h264_header.nalus[0].type == H264::NaluType::kPps) {
+      StorePps(packet.dataPtr, packet.sizeBytes, h264_header.nalus[0].sps_id,
+               h264_header.nalus[0].pps_id, packet.seqNum, packet.timestamp);
+    }
+  } else if (h264_header.packetization_type == kH264StapA) {
+    // Find SPS/PPS in packet buffer.
+    const uint8_t* data_cur = packet.dataPtr + kNaluHeaderSize;
+    const uint8_t* data_end = packet.dataPtr + packet.sizeBytes;
+    for (size_t nalu_num = 0; nalu_num < h264_header.nalus_length; ++nalu_num) {
+      const NaluInfo& nalu_info = h264_header.nalus[nalu_num];
+      const int nalu_size_bytes = data_cur[0] << 8 | data_cur[1];
+      data_cur += 2;
+      if (data_cur + nalu_size_bytes > data_end) {
+        RTC_LOG(LS_WARNING) << "Failed to find a NALU in packet.";
+        return;
+      }
+
+      RTC_DCHECK_EQ(nalu_info.type, (*data_cur) & 0x1f);
+
+      if (nalu_info.type == H264::NaluType::kSps) {
+        StoreSps(data_cur, nalu_size_bytes, nalu_info.sps_id, packet.width(),
+                 packet.height(), packet.seqNum, packet.timestamp);
+      } else if (nalu_info.type == H264::NaluType::kPps) {
+        StorePps(data_cur, nalu_size_bytes, nalu_info.sps_id, nalu_info.pps_id,
+                 packet.seqNum, packet.timestamp);
+      }
+
+      data_cur += nalu_size_bytes;
+    }
+  }
+}
+
+void H264SpsPpsTracker::StoreSps(const uint8_t* data,
+                                 size_t data_size_bytes,
+                                 uint32_t sps_id,
+                                 uint32_t width,
+                                 uint32_t height,
+                                 absl::optional<uint16_t> rtp_seq_num,
+                                 absl::optional<uint32_t> rtp_timestamp) {
+  SpsInfo sps_info;
+  sps_info.size = data_size_bytes;
+  sps_info.width = width;
+  sps_info.height = height;
+  sps_info.rtp_seq_num = rtp_seq_num;
+  sps_info.rtp_timestamp = rtp_timestamp;
+  uint8_t* sps_data = new uint8_t[sps_info.size];
+  memcpy(sps_data, data, sps_info.size);
+  sps_info.data.reset(sps_data);
+  sps_data_[sps_id] = std::move(sps_info);
+}
+
+void H264SpsPpsTracker::StorePps(const uint8_t* data,
+                                 size_t data_size_bytes,
+                                 uint32_t sps_id,
+                                 uint32_t pps_id,
+                                 absl::optional<uint16_t> rtp_seq_num,
+                                 absl::optional<uint32_t> rtp_timestamp) {
+  PpsInfo pps_info;
+  pps_info.size = data_size_bytes;
+  pps_info.sps_id = sps_id;
+  pps_info.rtp_seq_num = rtp_seq_num;
+  pps_info.rtp_timestamp = rtp_timestamp;
+  uint8_t* pps_data = new uint8_t[pps_info.size];
+  memcpy(pps_data, data, pps_info.size);
+  pps_info.data.reset(pps_data);
+  pps_data_[pps_id] = std::move(pps_info);
+}
+
+bool H264SpsPpsTracker::IsContinuousSeqNum(const SpsInfo& sps,
+                                           const PpsInfo& pps,
+                                           const VCMPacket& packet) {
+  RTC_DCHECK(sps.rtp_seq_num.has_value());
+  RTC_DCHECK(pps.rtp_seq_num.has_value());
+  const uint16_t seq_num = packet.seqNum;
+  return pps.rtp_seq_num == (uint16_t)(seq_num - 1) &&
+         (sps.rtp_seq_num == (uint16_t)(seq_num - 1) ||
+          sps.rtp_seq_num == (uint16_t)(seq_num - 2));
+}
+
+bool H264SpsPpsTracker::IsSameTimestamp(const SpsInfo& sps,
+                                        const PpsInfo& pps,
+                                        const VCMPacket& packet) {
+  RTC_DCHECK(sps.rtp_timestamp.has_value());
+  RTC_DCHECK(pps.rtp_timestamp.has_value());
+  return packet.timestamp == sps.rtp_timestamp &&
+         packet.timestamp == pps.rtp_timestamp;
+}
 }  // namespace video_coding
 }  // namespace webrtc

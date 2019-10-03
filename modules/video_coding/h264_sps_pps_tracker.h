e6a122ad5bdb3b2b867671ce87e2129ac691373a
Sergey Silkin <ssilkin@webrtc.org>
2019-10-03 17:47:25 +0200

    Store and insert in-band SPS/PPS before IDR.
    Change-Id: I8b6de1868d49cf3947abfe05c8cc897c49403458

--- modules/video_coding/h264_sps_pps_tracker.h
+++ modules/video_coding/h264_sps_pps_tracker.h
@@ -16,6 +16,8 @@
 #include <memory>
 #include <vector>
 
+#include "absl/types/optional.h"
+
 namespace webrtc {
 
 class VCMPacket;
@@ -43,6 +45,8 @@ class H264SpsPpsTracker {
 
     int sps_id = -1;
     size_t size = 0;
+    absl::optional<uint16_t> rtp_seq_num;
+    absl::optional<uint32_t> rtp_timestamp;
     std::unique_ptr<uint8_t[]> data;
   };
 
@@ -55,9 +59,31 @@ class H264SpsPpsTracker {
     size_t size = 0;
     int width = -1;
     int height = -1;
+    absl::optional<uint16_t> rtp_seq_num;
+    absl::optional<uint32_t> rtp_timestamp;
     std::unique_ptr<uint8_t[]> data;
   };
 
+  void StoreInBandSpsPps(const VCMPacket& packet);
+  void StoreSps(const uint8_t* data,
+                size_t data_size,
+                uint32_t sps_id,
+                uint32_t width,
+                uint32_t height,
+                absl::optional<uint16_t> rtp_seq_num,
+                absl::optional<uint32_t> rtp_timestamp);
+  void StorePps(const uint8_t* data,
+                size_t data_size,
+                uint32_t sps_id,
+                uint32_t pps_id,
+                absl::optional<uint16_t> rtp_seq_num,
+                absl::optional<uint32_t> rtp_timestamp);
+  bool IsContinuousSeqNum(const SpsInfo& sps,
+                          const PpsInfo& pps,
+                          const VCMPacket& packet);
+  bool IsSameTimestamp(const SpsInfo& sps,
+                       const PpsInfo& pps,
+                       const VCMPacket& packet);
   std::map<uint32_t, PpsInfo> pps_data_;
   std::map<uint32_t, SpsInfo> sps_data_;
 };

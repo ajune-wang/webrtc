2c7b75d8855510d2ec96b3914b533c2e80fe5fb2
Sergey Silkin <ssilkin@webrtc.org>
2019-10-04 13:10:22 +0200

    Store and insert in-band SPS/PPS before IDR.
    * If insert_inband_sps_pps_before_idr_ is set, store in-band SPS and
    PPS and insert them before the following IDR frame if timestamps of IDR
    and SPS/PPS are not the same and if there is gap in sequence number
    between all packets.
    
    * If reset_end_of_frame_flag_on_nonvlc_packet_ is set, reset
    is_last_packet_in_frame flag on packets which do not contain any VLC
    NAL units (nal_unit_type=5 or =1). This prevents such packets to be
    interpreted as a frame by the packet buffer.
    
    Bug: chromium:999807
    
    Change-Id: I8b6de1868d49cf3947abfe05c8cc897c49403458

--- modules/video_coding/h264_sps_pps_tracker.h
+++ modules/video_coding/h264_sps_pps_tracker.h
@@ -16,6 +16,8 @@
 #include <memory>
 #include <vector>
 
+#include "absl/types/optional.h"
+
 namespace webrtc {
 
 class VCMPacket;
@@ -42,8 +44,9 @@ class H264SpsPpsTracker {
     ~PpsInfo();
 
     int sps_id = -1;
-    size_t size = 0;
-    std::unique_ptr<uint8_t[]> data;
+    absl::optional<uint16_t> rtp_seq_num;
+    absl::optional<uint32_t> rtp_timestamp;
+    std::vector<uint8_t> data;
   };
 
   struct SpsInfo {
@@ -52,14 +55,38 @@ class H264SpsPpsTracker {
     SpsInfo& operator=(SpsInfo&& rhs);
     ~SpsInfo();
 
-    size_t size = 0;
     int width = -1;
     int height = -1;
-    std::unique_ptr<uint8_t[]> data;
+    absl::optional<uint16_t> rtp_seq_num;
+    absl::optional<uint32_t> rtp_timestamp;
+    std::vector<uint8_t> data;
   };
 
+  void StoreInBandSpsPps(const VCMPacket& packet);
+  void StoreSps(const uint8_t* data,
+                size_t data_size,
+                uint32_t sps_id,
+                uint32_t width,
+                uint32_t height,
+                absl::optional<uint16_t> rtp_seq_num,
+                absl::optional<uint32_t> rtp_timestamp);
+  void StorePps(const uint8_t* data,
+                size_t data_size,
+                uint32_t sps_id,
+                uint32_t pps_id,
+                absl::optional<uint16_t> rtp_seq_num,
+                absl::optional<uint32_t> rtp_timestamp);
+  bool IsContinuousSeqNum(const SpsInfo& sps,
+                          const PpsInfo& pps,
+                          const VCMPacket& packet);
+  bool IsSameTimestamp(const SpsInfo& sps,
+                       const PpsInfo& pps,
+                       const VCMPacket& packet);
   std::map<uint32_t, PpsInfo> pps_data_;
   std::map<uint32_t, SpsInfo> sps_data_;
+
+  const bool insert_inband_sps_pps_before_idr_;
+  const bool reset_end_of_frame_flag_on_nonvlc_packet_;
 };
 
 }  // namespace video_coding

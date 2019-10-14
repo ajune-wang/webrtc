0c161376d6f31637e4d8a1cef6cff0906b66e7b5
Sebastian Jansson <srte@webrtc.org>
2019-10-14 17:15:51 +0200

    Cleanup: Moving PacketFeedback from rtp_rtcp_defines.h
    It's only used by TransportFeedbackAdapter, moving it there.
    
    Bug: webrtc:9883
    Change-Id: I76ba224bb1bedbc6ef7d09c5d266269fe5a83627

--- modules/congestion_controller/rtp/transport_feedback_adapter.cc
+++ modules/congestion_controller/rtp/transport_feedback_adapter.cc
@@ -47,6 +47,78 @@ PacketResult NetworkPacketFeedbackFromRtpPacketFeedback(
 const int64_t kNoTimestamp = -1;
 const int64_t kSendTimeHistoryWindowMs = 60000;
 
+PacketFeedback::PacketFeedback(int64_t arrival_time_ms,
+                               uint16_t sequence_number)
+    : PacketFeedback(-1,
+                     arrival_time_ms,
+                     kNoSendTime,
+                     sequence_number,
+                     0,
+                     0,
+                     0,
+                     PacedPacketInfo()) {}
+
+PacketFeedback::PacketFeedback(int64_t arrival_time_ms,
+                               int64_t send_time_ms,
+                               uint16_t sequence_number,
+                               size_t payload_size,
+                               const PacedPacketInfo& pacing_info)
+    : PacketFeedback(-1,
+                     arrival_time_ms,
+                     send_time_ms,
+                     sequence_number,
+                     payload_size,
+                     0,
+                     0,
+                     pacing_info) {}
+
+PacketFeedback::PacketFeedback(int64_t creation_time_ms,
+                               uint16_t sequence_number,
+                               size_t payload_size,
+                               uint16_t local_net_id,
+                               uint16_t remote_net_id,
+                               const PacedPacketInfo& pacing_info)
+    : PacketFeedback(creation_time_ms,
+                     kNotReceived,
+                     kNoSendTime,
+                     sequence_number,
+                     payload_size,
+                     local_net_id,
+                     remote_net_id,
+                     pacing_info) {}
+
+PacketFeedback::PacketFeedback(int64_t creation_time_ms,
+                               int64_t arrival_time_ms,
+                               int64_t send_time_ms,
+                               uint16_t sequence_number,
+                               size_t payload_size,
+                               uint16_t local_net_id,
+                               uint16_t remote_net_id,
+                               const PacedPacketInfo& pacing_info)
+    : creation_time_ms(creation_time_ms),
+      arrival_time_ms(arrival_time_ms),
+      send_time_ms(send_time_ms),
+      sequence_number(sequence_number),
+      long_sequence_number(0),
+      payload_size(payload_size),
+      unacknowledged_data(0),
+      local_net_id(local_net_id),
+      remote_net_id(remote_net_id),
+      pacing_info(pacing_info),
+      ssrc(0),
+      rtp_sequence_number(0) {}
+
+PacketFeedback::PacketFeedback(const PacketFeedback&) = default;
+PacketFeedback& PacketFeedback::operator=(const PacketFeedback&) = default;
+PacketFeedback::~PacketFeedback() = default;
+
+bool PacketFeedback::operator==(const PacketFeedback& rhs) const {
+  return arrival_time_ms == rhs.arrival_time_ms &&
+         send_time_ms == rhs.send_time_ms &&
+         sequence_number == rhs.sequence_number &&
+         payload_size == rhs.payload_size && pacing_info == rhs.pacing_info;
+}
+
 TransportFeedbackAdapter::TransportFeedbackAdapter()
     : packet_age_limit_ms_(kSendTimeHistoryWindowMs),
       current_offset_ms_(kNoTimestamp),
--- modules/congestion_controller/rtp/transport_feedback_adapter_unittest.cc
+++ modules/congestion_controller/rtp/transport_feedback_adapter_unittest.cc
@@ -14,7 +14,6 @@
 #include <memory>
 #include <vector>
 
-#include "modules/congestion_controller/rtp/congestion_controller_unittests_helper.h"
 #include "modules/rtp_rtcp/include/rtp_rtcp_defines.h"
 #include "modules/rtp_rtcp/source/rtcp_packet/transport_feedback.h"
 #include "rtc_base/checks.h"
@@ -36,6 +35,33 @@ const PacedPacketInfo kPacingInfo1(1, 8, 4000);
 const PacedPacketInfo kPacingInfo2(2, 14, 7000);
 const PacedPacketInfo kPacingInfo3(3, 20, 10000);
 const PacedPacketInfo kPacingInfo4(4, 22, 10000);
+
+void ComparePacketFeedbackVectors(const std::vector<PacketFeedback>& truth,
+                                  const std::vector<PacketFeedback>& input) {
+  ASSERT_EQ(truth.size(), input.size());
+  size_t len = truth.size();
+  // truth contains the input data for the test, and input is what will be
+  // sent to the bandwidth estimator. truth.arrival_tims_ms is used to
+  // populate the transport feedback messages. As these times may be changed
+  // (because of resolution limits in the packets, and because of the time
+  // base adjustment performed by the TransportFeedbackAdapter at the first
+  // packet, the truth[x].arrival_time and input[x].arrival_time may not be
+  // equal. However, the difference must be the same for all x.
+  int64_t arrival_time_delta =
+      truth[0].arrival_time_ms - input[0].arrival_time_ms;
+  for (size_t i = 0; i < len; ++i) {
+    RTC_CHECK(truth[i].arrival_time_ms != PacketFeedback::kNotReceived);
+    if (input[i].arrival_time_ms != PacketFeedback::kNotReceived) {
+      EXPECT_EQ(truth[i].arrival_time_ms,
+                input[i].arrival_time_ms + arrival_time_delta);
+    }
+    EXPECT_EQ(truth[i].send_time_ms, input[i].send_time_ms);
+    EXPECT_EQ(truth[i].sequence_number, input[i].sequence_number);
+    EXPECT_EQ(truth[i].payload_size, input[i].payload_size);
+    EXPECT_EQ(truth[i].pacing_info, input[i].pacing_info);
+  }
+}
+
 }  // namespace
 
 namespace test {

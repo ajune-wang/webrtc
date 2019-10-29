19b1d8f22c46719a00e965130b9be02735a9b2c7
Minyue Li <minyue@webrtc.org>
2019-10-29 22:23:13 +0100

    Make Opus PLC always output 10ms audio.
    BUG: b/143582588
    Change-Id: I41ad5f4f91d9af3f595666a8f32b7ab5382605bd

--- modules/audio_coding/codecs/opus/opus_fec_test.cc
+++ modules/audio_coding/codecs/opus/opus_fec_test.cc
@@ -154,8 +154,15 @@ void OpusFecTest::DecodeABlock(bool lost_previous, bool lost_current) {
           WebRtcOpus_DecodeFec(opus_decoder_, &bit_stream_[0], encoded_bytes_,
                                &out_data_[0], &audio_type);
     } else {
-      value_1 =
-          WebRtcOpus_Decode(opus_decoder_, NULL, 0, &out_data_[0], &audio_type);
+      // Call decoder PLC.
+      constexpr int kPlcDurationMs = 10;
+      const int plc_samples = sampling_khz_ * kPlcDurationMs;
+      while (value_1 < static_cast<int>(block_length_sample_)) {
+        int ret = WebRtcOpus_Decode(opus_decoder_, NULL, 0, &out_data_[value_1],
+                                    &audio_type);
+        EXPECT_EQ(ret, plc_samples);
+        value_1 += ret;
+      }
     }
     EXPECT_EQ(static_cast<int>(block_length_sample_), value_1);
   }

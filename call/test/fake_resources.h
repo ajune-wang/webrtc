dc16a2839c24adebba729cec521f23d01b15c252
Henrik Boström <hbos@webrtc.org>
2019-11-18 13:42:18 +0100

    Prototype of new Resource Adaptation Strategy.
    Work-in-Progress!
    
    Bug: None
    Change-Id: I49204df1727248463d206f0b481feb045e519a75

---
+++ call/test/fake_resources.h
@@ -0,0 +1,46 @@
+/*
+ *  Copyright 2019 The WebRTC Project Authors. All rights reserved.
+ *
+ *  Use of this source code is governed by a BSD-style license
+ *  that can be found in the LICENSE file in the root of the source
+ *  tree. An additional intellectual property rights grant can be found
+ *  in the file PATENTS.  All contributing project authors may
+ *  be found in the AUTHORS file in the root of the source tree.
+ */
+
+#ifndef CALL_TEST_FAKE_RESOURCES_H_
+#define CALL_TEST_FAKE_RESOURCES_H_
+
+#include <string>
+
+#include "call/resource.h"
+
+namespace webrtc {
+namespace adaptation {
+
+// Fake resource representing CPU usage percent, with a setter.
+// - [0.8, inf) triggers ResourceUsageState::kOveruse.
+// - (0.6, 0.8) triggers ResourceUsageState::kStable.
+// - (-inf, 0.6] triggers ResourceUsageState::kUnderuse.
+// These numbers are arbitrary and don't necessarily represent how we want a
+// real CPU resource to be treated.
+class FakeCpuResource : public Resource {
+ public:
+  FakeCpuResource(double usage);
+  ~FakeCpuResource() override;
+
+  void set_usage(double usage);
+
+  std::string Name() const override;
+  std::string UsageUnitsOfMeasurement() const override;
+  double CurrentUsage() const override;
+  ResourceUsageState CurrentUsageState() const override;
+
+ private:
+  double usage_;
+};
+
+}  // namespace adaptation
+}  // namespace webrtc
+
+#endif  // CALL_TEST_FAKE_RESOURCES_H_

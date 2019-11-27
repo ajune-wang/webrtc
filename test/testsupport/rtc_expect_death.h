beb8969e52441f75c1d012f6e562160234d0f9e7
Artem Titov <titovartem@webrtc.org>
2019-11-27 17:12:01 +0100

    Add ability to disable detailed error message in RTC_CHECKs
    Bug: webrtc:11133
    Change-Id: I989654f1fb97b476a17956d69ee374406439ea8f

---
+++ test/testsupport/rtc_expect_death.h
@@ -0,0 +1,23 @@
+/*
+ *  Copyright (c) 2019 The WebRTC project authors. All Rights Reserved.
+ *
+ *  Use of this source code is governed by a BSD-style license
+ *  that can be found in the LICENSE file in the root of the source
+ *  tree. An additional intellectual property rights grant can be found
+ *  in the file PATENTS.  All contributing project authors may
+ *  be found in the AUTHORS file in the root of the source tree.
+ */
+
+#ifndef TEST_TESTSUPPORT_RTC_EXPECT_DEATH_H_
+#define TEST_TESTSUPPORT_RTC_EXPECT_DEATH_H_
+
+#include "test/gtest.h"
+
+#if defined(RTC_DISABLE_CHECK_MSG)
+// If RTC_CHECKs messages are disabled we can't validate failure message
+#define RTC_EXPECT_DEATH(statement, regex) EXPECT_DEATH(statement, "")
+#else
+#define RTC_EXPECT_DEATH(statement, regex) EXPECT_DEATH(statement, regex)
+#endif
+
+#endif  // TEST_TESTSUPPORT_RTC_EXPECT_DEATH_H_

1e3be2668c2590399d9baad4c75253d72fe91458
Henrik Boström <hbos@webrtc.org>
2019-12-03 12:40:57 +0100

    Prototype of new Resource Adaptation Strategy.
    Work-in-Progress!
    
    OFFICIAL PLAN on a napkin:
    - https://photos.app.goo.gl/ZtTT1EqoL91qqcpZ7
    - https://photos.app.goo.gl/q4BjPL1on6SFJFGf9
    - https://photos.app.goo.gl/VxrQHVF4a5wLSQ759
    
    Bug: None
    Change-Id: I49204df1727248463d206f0b481feb045e519a75

---
+++ call/resource_consumer_configuration.cc
@@ -0,0 +1,61 @@
+/*
+ *  Copyright 2019 The WebRTC Project Authors. All rights reserved.
+ *
+ *  Use of this source code is governed by a BSD-style license
+ *  that can be found in the LICENSE file in the root of the source
+ *  tree. An additional intellectual property rights grant can be found
+ *  in the file PATENTS.  All contributing project authors may
+ *  be found in the AUTHORS file in the root of the source tree.
+ */
+
+#include "call/resource_consumer_configuration.h"
+
+#include <utility>
+
+#include "rtc_base/checks.h"
+
+namespace webrtc {
+namespace adaptation {
+
+ResourceConsumerConfiguration::ResourceConsumerConfiguration(std::string name)
+    : name_(std::move(name)) {
+  RTC_DCHECK(!name_.empty());
+}
+
+ResourceConsumerConfiguration::~ResourceConsumerConfiguration() {}
+
+std::string ResourceConsumerConfiguration::name() const {
+  return name_;
+}
+
+const std::vector<ResourceConsumerConfiguration*>&
+ResourceConsumerConfiguration::neighbors() const {
+  return neighbors_;
+}
+
+void ResourceConsumerConfiguration::AddNeighbor(
+    ResourceConsumerConfiguration* neighbor) {
+  neighbors_.push_back(neighbor);
+}
+
+double ResourceConsumerConfiguration::ResourceImpactFactor(
+    const Resource& resource) const {
+  auto it = impact_factor_by_resource_.find(&resource);
+  if (it == impact_factor_by_resource_.end())
+    return 1.0;
+  return it->second;
+}
+
+void ResourceConsumerConfiguration::SetResourceImpactFactor(
+    const Resource* resource,
+    double factor) {
+  impact_factor_by_resource_.insert(std::make_pair(resource, factor));
+}
+
+double ResourceConsumerConfiguration::ApproximateImpact(
+    const Resource& resource) const {
+  return ApproximateCost() * ResourceImpactFactor(resource);
+}
+
+}  // namespace adaptation
+}  // namespace webrtc

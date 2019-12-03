9c0e331ea5e7c9cb12789e9e0b21e2b8c039647e
Sebastian Jansson <srte@webrtc.org>
2019-12-03 15:31:24 +0100

    Exposing more features in the network emulation manager API.
    Bug: webrtc:9883
    Change-Id: I2a687b46e3374db0dd08b0c02dfea1482e6fb89f

--- api/test/network_emulation_manager.cc
+++ api/test/network_emulation_manager.cc
@@ -7,55 +7,58 @@
  *  in the file PATENTS.  All contributing project authors may
  *  be found in the AUTHORS file in the root of the source tree.
  */
-#include "test/network/simulated_network_node.h"
-
-#include <memory>
 #include <utility>
 
+#include "api/test/network_emulation_manager.h"
+#include "call/simulated_network.h"
+
 namespace webrtc {
-namespace test {
 
-SimulatedNetworkNode::Builder::Builder() {}
-
-SimulatedNetworkNode::Builder::Builder(NetworkEmulationManager* net)
+NetworkEmulationManager::NetworkNodeBuilder::NetworkNodeBuilder(
+    NetworkEmulationManager* net)
     : net_(net) {}
 
-SimulatedNetworkNode::Builder& SimulatedNetworkNode::Builder::config(
-    SimulatedNetwork::Config config) {
+NetworkEmulationManager::NetworkNodeBuilder&
+NetworkEmulationManager::NetworkNodeBuilder::config(
+    SimulatedNetworkInterface::Config config) {
   config_ = config;
   return *this;
 }
 
-SimulatedNetworkNode::Builder& SimulatedNetworkNode::Builder::delay_ms(
-    int queue_delay_ms) {
+NetworkEmulationManager::NetworkNodeBuilder&
+NetworkEmulationManager::NetworkNodeBuilder::delay_ms(int queue_delay_ms) {
   config_.queue_delay_ms = queue_delay_ms;
   return *this;
 }
 
-SimulatedNetworkNode::Builder& SimulatedNetworkNode::Builder::capacity_kbps(
+NetworkEmulationManager::NetworkNodeBuilder&
+NetworkEmulationManager::NetworkNodeBuilder::capacity_kbps(
     int link_capacity_kbps) {
   config_.link_capacity_kbps = link_capacity_kbps;
   return *this;
 }
 
-SimulatedNetworkNode::Builder& SimulatedNetworkNode::Builder::capacity_Mbps(
+NetworkEmulationManager::NetworkNodeBuilder&
+NetworkEmulationManager::NetworkNodeBuilder::capacity_Mbps(
     int link_capacity_Mbps) {
   config_.link_capacity_kbps = link_capacity_Mbps * 1000;
   return *this;
 }
 
-SimulatedNetworkNode::Builder& SimulatedNetworkNode::Builder::loss(
-    double loss_rate) {
+NetworkEmulationManager::NetworkNodeBuilder&
+NetworkEmulationManager::NetworkNodeBuilder::loss(double loss_rate) {
   config_.loss_percent = std::round(loss_rate * 100);
   return *this;
 }
 
-SimulatedNetworkNode SimulatedNetworkNode::Builder::Build() const {
+NetworkEmulationManager::SimulatedNetworkNode
+NetworkEmulationManager::NetworkNodeBuilder::Build() const {
   RTC_DCHECK(net_);
   return Build(net_);
 }
 
-SimulatedNetworkNode SimulatedNetworkNode::Builder::Build(
+NetworkEmulationManager::SimulatedNetworkNode
+NetworkEmulationManager::NetworkNodeBuilder::Build(
     NetworkEmulationManager* net) const {
   SimulatedNetworkNode res;
   auto behavior = std::make_unique<SimulatedNetwork>(config_);
@@ -63,6 +66,4 @@ SimulatedNetworkNode SimulatedNetworkNode::Builder::Build(
   res.node = net->CreateEmulatedNode(std::move(behavior));
   return res;
 }
-
-}  // namespace test
 }  // namespace webrtc

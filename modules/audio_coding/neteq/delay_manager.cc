6fbe08c066b0cf1a54a8aa0a214a8588fac99645
Jakob Ivarsson <jakobi@webrtc.org>
2020-01-08 09:55:55 +0100

    Remove extra delay field trial.
    Bug: webrtc:10817
    Change-Id: I704a8ea0dc774f242f8d5d88b140f850cf23d518

--- modules/audio_coding/neteq/delay_manager.cc
+++ modules/audio_coding/neteq/delay_manager.cc
@@ -79,23 +79,6 @@ DelayHistogramConfig GetDelayHistogramConfig() {
   return config;
 }
 
-absl::optional<int> GetExtraDelayMs() {
-  constexpr char kExtraDelayFieldTrial[] = "WebRTC-Audio-NetEqExtraDelay";
-  if (!webrtc::field_trial::IsEnabled(kExtraDelayFieldTrial)) {
-    return absl::nullopt;
-  }
-
-  const auto field_trial_string =
-      webrtc::field_trial::FindFullName(kExtraDelayFieldTrial);
-  int extra_delay_ms = -1;
-  sscanf(field_trial_string.c_str(), "Enabled-%d", &extra_delay_ms);
-  if (extra_delay_ms >= 0) {
-    RTC_LOG(LS_INFO) << "NetEq extra delay in milliseconds: " << extra_delay_ms;
-    return extra_delay_ms;
-  }
-  return absl::nullopt;
-}
-
 }  // namespace
 
 namespace webrtc {
@@ -127,8 +110,7 @@ DelayManager::DelayManager(size_t max_packets_in_buffer,
       last_pack_cng_or_dtmf_(1),
       frame_length_change_experiment_(
           field_trial::IsEnabled("WebRTC-Audio-NetEqFramelengthExperiment")),
-      enable_rtx_handling_(enable_rtx_handling),
-      extra_delay_ms_(GetExtraDelayMs()) {
+      enable_rtx_handling_(enable_rtx_handling) {
   assert(peak_detector);  // Should never be NULL.
   RTC_CHECK(histogram_);
   RTC_DCHECK_GE(base_minimum_delay_ms_, 0);
@@ -341,10 +323,6 @@ int DelayManager::CalculateTargetLevel(int iat_packets, bool reordered) {
   target_level = std::max(target_level, 1);
   // Scale to Q8 and assign to member variable.
   target_level_ = target_level << 8;
-  if (extra_delay_ms_ && packet_len_ms_ > 0) {
-    int extra_delay = (extra_delay_ms_.value() << 8) / packet_len_ms_;
-    target_level_ += extra_delay;
-  }
   return target_level_;
 }
 

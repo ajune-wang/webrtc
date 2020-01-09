862a69f99f84f7c179b4d96a67ade3e2abb93008
Patrik Höglund <phoglund@webrtc.org>
2020-01-09 14:10:07 +0100

    Extract an interface from the perf results logger.
    I plan to introduce a second implementation of the perf logger that
    uses the new Histogram C++ API. I think I will add a flag that chooses
    between the two implementations so I can try it out (perhaps by
    setting up a second, limited run of webrtc_perf_tests on the perf
    bots that uses the new implementation).
    
    As a side effect, I disentangled the plottable counter printer from
    the perf result printer so it will work for both implementations.
    The only thing they had in common was that both wrote JSON anyway.
    
    See the bug for details on the new API.
    
    Bug: 1029452
    Change-Id: Icb21b25ced08ea73aeecd221e9d51f2adf3dab1b

--- test/testsupport/perf_test.cc
+++ test/testsupport/perf_test.cc
@@ -19,19 +19,24 @@
 #include <sstream>
 #include <vector>
 
+#include "absl/flags/flag.h"
 #include "rtc_base/checks.h"
 #include "rtc_base/critical_section.h"
+#include "test/testsupport/perf_test_graphjson_writer.h"
+#include "test/testsupport/perf_test_histogram_writer.h"
+
+ABSL_FLAG(bool,
+          write_histogram_proto_json,
+          false,
+          "Use the histogram C++ API, which will write Histogram proto JSON "
+          "instead of Chart JSON. Note, Histogram set JSON and Histogram "
+          "proto JSON are not quite the same thing. This flag only has effect "
+          "if --isolated_script_test_perf_output is specified.");
+
+namespace webrtc {
+namespace test {
 
 namespace {
-
-template <typename Container>
-void OutputListToStream(std::ostream* ostream, const Container& values) {
-  const char* sep = "";
-  for (const auto& v : values) {
-    (*ostream) << sep << v;
-    sep = ",";
-  }
-}
 
 struct PlottableCounter {
   std::string graph_name;
@@ -40,112 +45,22 @@ struct PlottableCounter {
   std::string units;
 };
 
-class PerfResultsLogger {
+class PlottableCounterPrinter {
  public:
-  PerfResultsLogger() : crit_(), output_(stdout), graphs_() {}
-  void ClearResults() {
-    rtc::CritScope lock(&crit_);
-    graphs_.clear();
-  }
   void SetOutput(FILE* output) {
     rtc::CritScope lock(&crit_);
     output_ = output;
   }
-  void LogResult(const std::string& graph_name,
-                 const std::string& trace_name,
-                 const webrtc::SamplesStatsCounter& counter,
-                 const std::string& units,
-                 const bool important,
-                 webrtc::test::ImproveDirection improve_direction) {
-    LogResultMeanAndError(
-        graph_name, trace_name, counter.IsEmpty() ? 0 : counter.GetAverage(),
-        counter.IsEmpty() ? 0 : counter.GetStandardDeviation(), units,
-        important, improve_direction);
 
+  void AddCounter(const std::string& graph_name,
+                  const std::string& trace_name,
+                  const SamplesStatsCounter& counter,
+                  const std::string& units) {
     rtc::CritScope lock(&crit_);
     plottable_counters_.push_back({graph_name, trace_name, counter, units});
   }
-  void LogResult(const std::string& graph_name,
-                 const std::string& trace_name,
-                 const double value,
-                 const std::string& units,
-                 const bool important,
-                 webrtc::test::ImproveDirection improve_direction) {
-    RTC_CHECK(std::isfinite(value))
-        << "Expected finite value for graph " << graph_name << ", trace name "
-        << trace_name << ", units " << units << ", got " << value;
 
-    std::ostringstream value_stream;
-    value_stream.precision(8);
-    value_stream << value;
-    LogResultsImpl(graph_name, trace_name, value_stream.str(), units, important,
-                   improve_direction);
-
-    std::ostringstream json_stream;
-    json_stream << '"' << trace_name << R"(":{)";
-    json_stream << R"("type":"scalar",)";
-    json_stream << R"("value":)" << value << ',';
-    json_stream << R"("units":")" << UnitWithDirection(units, improve_direction)
-                << R"("})";
-    rtc::CritScope lock(&crit_);
-    graphs_[graph_name].push_back(json_stream.str());
-  }
-  void LogResultMeanAndError(const std::string& graph_name,
-                             const std::string& trace_name,
-                             const double mean,
-                             const double error,
-                             const std::string& units,
-                             const bool important,
-                             webrtc::test::ImproveDirection improve_direction) {
-    RTC_CHECK(std::isfinite(mean));
-    RTC_CHECK(std::isfinite(error));
-
-    std::ostringstream value_stream;
-    value_stream.precision(8);
-    value_stream << '{' << mean << ',' << error << '}';
-    LogResultsImpl(graph_name, trace_name, value_stream.str(), units, important,
-                   improve_direction);
-
-    std::ostringstream json_stream;
-    json_stream << '"' << trace_name << R"(":{)";
-    json_stream << R"("type":"list_of_scalar_values",)";
-    json_stream << R"("values":[)" << mean << "],";
-    json_stream << R"("std":)" << error << ',';
-    json_stream << R"("units":")" << UnitWithDirection(units, improve_direction)
-                << R"("})";
-    rtc::CritScope lock(&crit_);
-    graphs_[graph_name].push_back(json_stream.str());
-  }
-  void LogResultList(const std::string& graph_name,
-                     const std::string& trace_name,
-                     const rtc::ArrayView<const double> values,
-                     const std::string& units,
-                     const bool important,
-                     webrtc::test::ImproveDirection improve_direction) {
-    for (double v : values) {
-      RTC_CHECK(std::isfinite(v));
-    }
-
-    std::ostringstream value_stream;
-    value_stream.precision(8);
-    value_stream << '[';
-    OutputListToStream(&value_stream, values);
-    value_stream << ']';
-    LogResultsImpl(graph_name, trace_name, value_stream.str(), units, important,
-                   improve_direction);
-
-    std::ostringstream json_stream;
-    json_stream << '"' << trace_name << R"(":{)";
-    json_stream << R"("type":"list_of_scalar_values",)";
-    json_stream << R"("values":)" << value_stream.str() << ',';
-    json_stream << R"("units":")" << UnitWithDirection(units, improve_direction)
-                << R"("})";
-    rtc::CritScope lock(&crit_);
-    graphs_[graph_name].push_back(json_stream.str());
-  }
-  std::string ToJSON() const;
-  void PrintPlottableCounters(
-      const std::vector<std::string>& desired_graphs_raw) const {
+  void Print(const std::vector<std::string>& desired_graphs_raw) const {
     std::set<std::string> desired_graphs(desired_graphs_raw.begin(),
                                          desired_graphs_raw.end());
     rtc::CritScope lock(&crit_);
@@ -181,87 +96,43 @@ class PerfResultsLogger {
   }
 
  private:
-  void LogResultsImpl(const std::string& graph_name,
-                      const std::string& trace,
-                      const std::string& values,
-                      const std::string& units,
-                      bool important,
-                      webrtc::test::ImproveDirection improve_direction) {
-    // <*>RESULT <graph_name>: <trace_name>= <value> <units>
-    // <*>RESULT <graph_name>: <trace_name>= {<mean>, <std deviation>} <units>
-    // <*>RESULT <graph_name>: <trace_name>= [<value>,value,value,...,] <units>
-    rtc::CritScope lock(&crit_);
-
-    if (important) {
-      fprintf(output_, "*");
-    }
-    fprintf(output_, "RESULT %s: %s= %s %s\n", graph_name.c_str(),
-            trace.c_str(), values.c_str(),
-            UnitWithDirection(units, improve_direction).c_str());
-  }
-
-  std::string UnitWithDirection(
-      const std::string& units,
-      webrtc::test::ImproveDirection improve_direction) {
-    switch (improve_direction) {
-      case webrtc::test::ImproveDirection::kNone:
-        return units;
-      case webrtc::test::ImproveDirection::kSmallerIsBetter:
-        return units + "_smallerIsBetter";
-      case webrtc::test::ImproveDirection::kBiggerIsBetter:
-        return units + "_biggerIsBetter";
-    }
-  }
-
   rtc::CriticalSection crit_;
-  FILE* output_ RTC_GUARDED_BY(&crit_);
-  std::map<std::string, std::vector<std::string>> graphs_
-      RTC_GUARDED_BY(&crit_);
   std::vector<PlottableCounter> plottable_counters_ RTC_GUARDED_BY(&crit_);
+  FILE* output_ RTC_GUARDED_BY(&crit_);
 };
 
-std::string PerfResultsLogger::ToJSON() const {
-  std::ostringstream json_stream;
-  json_stream << R"({"format_version":"1.0",)";
-  json_stream << R"("charts":{)";
-  rtc::CritScope lock(&crit_);
-  for (auto graphs_it = graphs_.begin(); graphs_it != graphs_.end();
-       ++graphs_it) {
-    if (graphs_it != graphs_.begin())
-      json_stream << ',';
-    json_stream << '"' << graphs_it->first << "\":";
-    json_stream << '{';
-    OutputListToStream(&json_stream, graphs_it->second);
-    json_stream << '}';
+PerfTestResultWriter& GetPerfWriter() {
+  if (absl::GetFlag(FLAGS_write_histogram_proto_json)) {
+    static PerfTestResultWriter* logger_ = CreateHistogramWriter();
+    return *logger_;
+  } else {
+    static PerfTestResultWriter* logger_ = CreateGraphJsonWriter();
+    return *logger_;
   }
-  json_stream << "}}";
-  return json_stream.str();
 }
 
-PerfResultsLogger& GetPerfResultsLogger() {
-  static PerfResultsLogger* const logger_ = new PerfResultsLogger();
-  return *logger_;
+PlottableCounterPrinter& GetPlottableCounterPrinter() {
+  static PlottableCounterPrinter* printer_ = new PlottableCounterPrinter();
+  return *printer_;
 }
 
 }  // namespace
 
-namespace webrtc {
-namespace test {
-
 void ClearPerfResults() {
-  GetPerfResultsLogger().ClearResults();
+  GetPerfWriter().ClearResults();
 }
 
 void SetPerfResultsOutput(FILE* output) {
-  GetPerfResultsLogger().SetOutput(output);
+  GetPerfWriter().SetOutput(output);
+  GetPlottableCounterPrinter().SetOutput(output);
 }
 
 std::string GetPerfResultsJSON() {
-  return GetPerfResultsLogger().ToJSON();
+  return GetPerfWriter().ToJSON();
 }
 
 void PrintPlottableResults(const std::vector<std::string>& desired_graphs) {
-  GetPerfResultsLogger().PrintPlottableCounters(desired_graphs);
+  GetPlottableCounterPrinter().Print(desired_graphs);
 }
 
 void WritePerfResults(const std::string& output_path) {
@@ -278,8 +149,8 @@ void PrintResult(const std::string& measurement,
                  const std::string& units,
                  bool important,
                  ImproveDirection improve_direction) {
-  GetPerfResultsLogger().LogResult(measurement + modifier, trace, value, units,
-                                   important, improve_direction);
+  GetPerfWriter().LogResult(measurement + modifier, trace, value, units,
+                            important, improve_direction);
 }
 
 void PrintResult(const std::string& measurement,
@@ -289,8 +160,13 @@ void PrintResult(const std::string& measurement,
                  const std::string& units,
                  const bool important,
                  ImproveDirection improve_direction) {
-  GetPerfResultsLogger().LogResult(measurement + modifier, trace, counter,
-                                   units, important, improve_direction);
+  PrintResultMeanAndError(
+      measurement, modifier, trace,
+      counter.IsEmpty() ? 0 : counter.GetAverage(),
+      counter.IsEmpty() ? 0 : counter.GetStandardDeviation(), units, important,
+      improve_direction);
+  GetPlottableCounterPrinter().AddCounter(measurement + modifier, trace,
+                                          counter, units);
 }
 
 void PrintResultMeanAndError(const std::string& measurement,
@@ -301,9 +177,9 @@ void PrintResultMeanAndError(const std::string& measurement,
                              const std::string& units,
                              bool important,
                              ImproveDirection improve_direction) {
-  GetPerfResultsLogger().LogResultMeanAndError(measurement + modifier, trace,
-                                               mean, error, units, important,
-                                               improve_direction);
+  GetPerfWriter().LogResultMeanAndError(measurement + modifier, trace, mean,
+                                        error, units, important,
+                                        improve_direction);
 }
 
 void PrintResultList(const std::string& measurement,
@@ -313,8 +189,8 @@ void PrintResultList(const std::string& measurement,
                      const std::string& units,
                      bool important,
                      ImproveDirection improve_direction) {
-  GetPerfResultsLogger().LogResultList(measurement + modifier, trace, values,
-                                       units, important, improve_direction);
+  GetPerfWriter().LogResultList(measurement + modifier, trace, values, units,
+                                important, improve_direction);
 }
 
 }  // namespace test

23a8b62c981214d76c320c54a5d3f46269a4a13c
Patrik Höglund <phoglund@webrtc.org>
2020-01-20 10:57:14 +0100

    Implement histogram perf results writer.
    This will be used by WebRTC tests. It converts results exactly the
    same as our downstream implementation (histogram_util).
    
    This implementation should be pretty feature complete, or at least
    enough to start testing the end-to-end flow. I will set up some
    experimental recipe code and see if this actually makes it into the
    dashboard.
    
    Note: needs some catapult changes to land first and be rolled
    into Chromium, and then WebRTC.
    
    Bug: chromium:1029452
    Change-Id: I939046929652fc27b8fcb18af54bde22886d9228

---
+++ test/testsupport/perf_test_histogram_writer_unittest.cc
@@ -0,0 +1,169 @@
+/*
+ *  Copyright (c) 2020 The WebRTC project authors. All Rights Reserved.
+ *
+ *  Use of this source code is governed by a BSD-style license
+ *  that can be found in the LICENSE file in the root of the source
+ *  tree. An additional intellectual property rights grant can be found
+ *  in the file PATENTS.  All contributing project authors may
+ *  be found in the AUTHORS file in the root of the source tree.
+ */
+
+#include "test/testsupport/perf_test_histogram_writer.h"
+
+#include <memory>
+#include <string>
+
+#include "test/gtest.h"
+
+namespace webrtc {
+namespace test {
+
+TEST(PerfHistogramWriterUnittest, WritesSimpleValue) {
+  std::unique_ptr<PerfTestResultWriter> writer =
+      std::unique_ptr<PerfTestResultWriter>(CreateHistogramWriter());
+
+  writer->LogResult("measurement", "user_story", 15e7, "Hz", false,
+                    ImproveDirection::kBiggerIsBetter);
+
+  EXPECT_EQ(R"({
+ "histograms": [
+  {
+   "name": "measurement",
+   "unit": {
+    "unit": "HERTZ",
+    "improvementDirection": "BIGGER_IS_BETTER"
+   },
+   "diagnostics": {
+    "diagnosticMap": {
+     "stories": {
+      "genericSet": {
+       "values": [
+        "\"user_story\""
+       ]
+      }
+     }
+    }
+   },
+   "sampleValues": [
+    1.5e+08
+   ],
+   "maxNumSampleValues": 10,
+   "running": {
+    "count": 1,
+    "max": 1.5e+08,
+    "meanlogs": 18.8261452,
+    "mean": 1.5e+08,
+    "min": 1.5e+08,
+    "sum": 1.5e+08
+   }
+  }
+ ]
+}
+)",
+            writer->ToJSON());
+}
+
+TEST(PerfHistogramWriterUnittest, IgnoresError) {
+  std::unique_ptr<PerfTestResultWriter> writer =
+      std::unique_ptr<PerfTestResultWriter>(CreateHistogramWriter());
+
+  writer->LogResultMeanAndError("-", "-", 17, 12345, "ms", false,
+                                ImproveDirection::kNone);
+
+  std::string json = writer->ToJSON();
+  EXPECT_NE(json.find("17"), std::string::npos)
+      << "Sample value should be somewhere in the file";
+  EXPECT_EQ(json.find("12345"), std::string::npos)
+      << "Error should be thrown away";
+}
+
+TEST(PerfHistogramWriterUnittest, WritesDecibelIntoMeasurementName) {
+  std::unique_ptr<PerfTestResultWriter> writer =
+      std::unique_ptr<PerfTestResultWriter>(CreateHistogramWriter());
+
+  writer->LogResult("measurement", "-", 0, "dB", false,
+                    ImproveDirection::kNone);
+
+  std::string json = writer->ToJSON();
+  EXPECT_NE(json.find(R"("unit": "UNITLESS")"), std::string::npos)
+      << "dB should map to unitless" << json;
+  EXPECT_NE(json.find(R"("name": "measurement_dB")"), std::string::npos)
+      << "measurement should be renamed" << json;
+}
+
+TEST(PerfHistogramWriterUnittest, WritesFpsIntoMeasurementName) {
+  std::unique_ptr<PerfTestResultWriter> writer =
+      std::unique_ptr<PerfTestResultWriter>(CreateHistogramWriter());
+
+  writer->LogResult("measurement", "-", 0, "fps", false,
+                    ImproveDirection::kNone);
+
+  std::string json = writer->ToJSON();
+  EXPECT_NE(json.find(R"("unit": "HERTZ")"), std::string::npos)
+      << "fps should map to hertz" << json;
+  EXPECT_NE(json.find(R"("name": "measurement_fps")"), std::string::npos)
+      << "measurement should be renamed" << json;
+}
+
+TEST(PerfHistogramWriterUnittest, WritesPercentIntoMeasurementName) {
+  std::unique_ptr<PerfTestResultWriter> writer =
+      std::unique_ptr<PerfTestResultWriter>(CreateHistogramWriter());
+
+  writer->LogResult("measurement", "-", 0, "%", false, ImproveDirection::kNone);
+
+  std::string json = writer->ToJSON();
+  EXPECT_NE(json.find(R"("unit": "UNITLESS")"), std::string::npos)
+      << "fps should map to unitless" << json;
+  EXPECT_NE(json.find(R"("name": "measurement_%")"), std::string::npos)
+      << "measurement should be renamed" << json;
+}
+
+TEST(PerfHistogramWriterUnittest, BitsPerSecondIsConvertedToBytes) {
+  std::unique_ptr<PerfTestResultWriter> writer =
+      std::unique_ptr<PerfTestResultWriter>(CreateHistogramWriter());
+
+  writer->LogResult("-", "-", 1024, "bps", false, ImproveDirection::kNone);
+
+  std::string json = writer->ToJSON();
+  EXPECT_NE(json.find("128"), std::string::npos)
+      << "1024 bits = 128 bytes" << json;
+  EXPECT_EQ(json.find("1024"), std::string::npos) << json;
+}
+
+TEST(PerfHistogramWriterUnittest, ParsesDirection) {
+  std::unique_ptr<PerfTestResultWriter> writer =
+      std::unique_ptr<PerfTestResultWriter>(CreateHistogramWriter());
+
+  writer->LogResult("measurement1", "-", 0, "bps", false,
+                    ImproveDirection::kBiggerIsBetter);
+  writer->LogResult("measurement2", "-", 0, "frames", false,
+                    ImproveDirection::kSmallerIsBetter);
+  writer->LogResult("measurement3", "-", 0, "sigma", false,
+                    ImproveDirection::kNone);
+
+  std::string json = writer->ToJSON();
+  EXPECT_NE(json.find(R"("name": "measurement1",
+   "unit": {
+    "unit": "BYTES_PER_SECOND",
+    "improvementDirection": "BIGGER_IS_BETTER")"),
+            std::string::npos)
+      << json;
+  EXPECT_NE(json.find(R"("name": "measurement2",
+   "unit": {
+    "unit": "COUNT",
+    "improvementDirection": "SMALLER_IS_BETTER")"),
+            std::string::npos)
+      << json;
+
+  // In the case of kNone the improvement direction isn't set in the wire
+  // format.
+  EXPECT_NE(json.find(R"("name": "measurement3",
+   "unit": {
+    "unit": "SIGMA"
+   })"),
+            std::string::npos)
+      << json;
+}
+
+}  // namespace test
+}  // namespace webrtc

a17211066af3aea352a64ebfdc12a61dca97be4f
Henrik Boström <hbos@webrtc.org>
2020-01-24 16:01:57 +0100

    [Overuse] Move GetCpuOveruseOptions() to adaption module.
    This removes the last remaining direct dependency on VideoStreamEncoder
    from OveruseFrameDetectorResourceAdaptationModule.
    
    Bug: webrtc:11222
    Change-Id: I9746f28a4df8e631e297669c10636bf17b39acec

--- video/video_stream_encoder.cc
+++ video/video_stream_encoder.cc
@@ -312,7 +312,7 @@ VideoStreamEncoder::VideoStreamEncoder(
           /*source=*/nullptr)),
       resource_adaptation_module_(
           std::make_unique<OveruseFrameDetectorResourceAdaptationModule>(
-              /*video_stream_encoder=*/this,
+              settings_.experiment_cpu_load_estimator,
               std::move(overuse_detector),
               encoder_stats_observer,
               /*adaptation_listener=*/this)),
@@ -1698,25 +1698,6 @@ bool VideoStreamEncoder::TryQualityRampup(int64_t now_ms) {
     }
   }
   return false;
-}
-
-// TODO(pbos): Lower these thresholds (to closer to 100%) when we handle
-// pipelining encoders better (multiple input frames before something comes
-// out). This should effectively turn off CPU adaptations for systems that
-// remotely cope with the load right now.
-CpuOveruseOptions VideoStreamEncoder::GetCpuOveruseOptions() const {
-  RTC_DCHECK_RUN_ON(&encoder_queue_);
-  CpuOveruseOptions options;
-  // Hardware accelerated encoders are assumed to be pipelined; give them
-  // additional overuse time.
-  if (encoder_->GetEncoderInfo().is_hardware_accelerated) {
-    options.low_encode_usage_threshold_percent = 150;
-    options.high_encode_usage_threshold_percent = 200;
-  }
-  if (settings_.experiment_cpu_load_estimator) {
-    options.filter_time_ms = 5 * rtc::kNumMillisecsPerSec;
-  }
-  return options;
 }
 
 bool VideoStreamEncoder::TriggerAdaptDown(

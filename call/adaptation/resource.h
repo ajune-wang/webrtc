3a09ff36bedcb061ed8ceccd1842dd1d70ce11d3
Henrik Boström <hbos@webrtc.org>
2020-02-06 10:37:57 +0100

    [Overuse] Implement Resource and ResourceUsageListener.
    The Resource interface (previously a skeleton not used outside of
    testing) is updated to inform listeners of changes to resource
    usage. Debugging methods are removed (Name, UsageUnitsOfMeasurements,
    CurrentUsage). The interface is implemented by
    OveruseFrameDetectorResourceAdaptationModule's inner classes
    EncodeUsageResource and QualityScalerResource.
    
    The new ResourceUsageListener interface is implemented by
    OveruseFrameDetectorResourceAdaptationModule. In order to avoid adding
    AdaptationObserverInterface::AdaptReason to the ResourceUsageListener
    interface, the module figures out if the reason is "kCpu" or "kQuality"
    by looking which Resource object triggered
    OnResourceUsageStateMeasured(). These resources no longer need an
    explicit reference to OveruseFrameDetectorResourceAdaptationModule and
    could potentially be used by a different module.
    
    In this CL, AdaptationObserverInterface::AdaptDown()'s return value is
    still needed by QualityScaler. This is mirrored in the return value of
    ResourceUsageListener::OnResourceUsageStateMeasured(). A TODO is added
    to remove it and a comment explains how the current implementation
    seems to break the contract of the method (as was the case prior to
    this CL).
    
    Follow-up work include:
    - Move EncodeUsageResource and QualityScalerResource to separate files.
    - Make resources injectable, allowing fake resources in testing and
      removing OnResourceOveruseForTesting() methods.
      (Investigate adding the necessary input signals to the Resource
      interface or relevant sub-interfaces so that the module does not need
      to know which Resource implementation is used.)
    - And more! See whiteboard :)
    
    Bug: webrtc:11222
    Change-Id: I0a46ace4a2e617874e3ee97e67e3a199fef420a2

--- call/adaptation/resource.h
+++ call/adaptation/resource.h
@@ -11,9 +11,13 @@
 #ifndef CALL_ADAPTATION_RESOURCE_H_
 #define CALL_ADAPTATION_RESOURCE_H_
 
-#include <string>
+#include <vector>
+
+#include "absl/types/optional.h"
 
 namespace webrtc {
+
+class Resource;
 
 enum class ResourceUsageState {
   // Action is needed to minimze the load on this resource.
@@ -25,32 +29,65 @@ enum class ResourceUsageState {
   kUnderuse,
 };
 
+class ResourceUsageListener {
+ public:
+  virtual ~ResourceUsageListener();
+
+  // Informs the listener of a new measurement of resource usage. This means
+  // that |resource.usage_state()| is now up-to-date.
+  //
+  // The listener may optionally return "whether or not adaptation happened as a
+  // result". Note that this is mirroring AdaptationObserverInterface's
+  // AdaptDown() method and current implementations may behave differently than
+  // this contract, see 2) below.
+  // TODO(https://crbug.com/webrtc/11222): Remove this return value or
+  // investigate alternative means to achieve what the implementation is
+  // currently doing. Reasons to abandon current approach:
+  // 1) Resource usage measurements and adaptation decisions need to be
+  //    separated in order to support injectable adaptation modules,
+  //    multi-stream aware adaptation and decision-making logic based on
+  //    multiple resources. If the resource depends on the the latest adaptation
+  //    perhaps it should listen to VideoSourceRestriction updates instead?
+  // 2) The implementation currently does not seem to do what the interfaces
+  //    says. Documentation for AdaptDown(): "Returns false if a downgrade was
+  //    requested but the request did not result in a new limiting resolution or
+  //    fps." The implementation: Return false if !has_input_video_ or if we use
+  //    balanced degradation preference and we DID adapt frame rate but the
+  //    difference between input frame rate and balanced settings' min fps is
+  //    less than the balanced settings' min fps diff - in all other cases,
+  //    return true whether or not adaptation happened.
+  // The true purpose of this return value seem to be that if its false the
+  // QualityScaler may check quality more often but by default we return true.
+  // This kind of logic should move out of
+  // overuse_frame_detector_resource_adaptation_module.cc.
+  virtual absl::optional<bool> OnResourceUsageStateMeasured(
+      const Resource& resource) = 0;
+};
+
 // A Resource is something which can be measured as "overused", "stable" or
-// "underused". For example, if we are overusing CPU we may need to lower the
-// resolution of one of the streams. In other words, one of the ResourceConumers
-// - representing an encoder - needs to be reconfigured with a different
-// ResourceConsumerConfiguration - representing a different encoder setting.
+// "underused". When the resource usage changes, listeners of the resource are
+// informed.
 //
-// This is an abstract class used by the ResourceAdaptationProcessor to make
-// decisions about which configurations to use. How a resource is measured or
-// what measurements map to different ResourceUsageState values is
-// implementation-specific.
+// Implementations of this interface are responsible for performing resource
+// usage measurements and invoking OnResourceUsageStateMeasured().
 class Resource {
  public:
+  Resource();
   virtual ~Resource();
 
-  // Informational, not formally part of the decision-making process.
-  virtual std::string Name() const = 0;
-  virtual std::string UsageUnitsOfMeasurement() const = 0;
-  // Valid ranges are implementation-specific.
-  virtual double CurrentUsage() const = 0;
+  void RegisterListener(ResourceUsageListener* listener);
 
-  // The current usage state of this resource. Used by the
-  // ResourceAdaptationProcessor to calculate the desired consumer
-  // configurations.
-  virtual ResourceUsageState CurrentUsageState() const = 0;
+  ResourceUsageState usage_state() const;
 
-  std::string ToString() const;
+ protected:
+  // Updates the usage state and informs all registered listeners.
+  // Returns false if any listeners' OnResourceUsageStateMeasured() returned
+  // false.
+  bool OnResourceUsageStateMeasured(ResourceUsageState usage_state);
+
+ private:
+  ResourceUsageState usage_state_;
+  std::vector<ResourceUsageListener*> listeners_;
 };
 
 }  // namespace webrtc

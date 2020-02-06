1bdb9184a6225f3554154b0dfcc8c60aed6a8c7e
Erik Språng <sprang@webrtc.org>
2020-02-06 15:01:55 +0100

    Moves RtpSequenceNumberMap from RtpSenderVideo to RtpSenderEgress.
    Bug: webrtc:11340
    Change-Id: Icd9032e3589324cb9ee7b699b38a35e733081e55

--- modules/rtp_rtcp/source/rtp_sender_video.cc
+++ modules/rtp_rtcp/source/rtp_sender_video.cc
@@ -40,7 +40,6 @@ namespace webrtc {
 
 namespace {
 constexpr size_t kRedForFecHeaderLength = 1;
-constexpr size_t kRtpSequenceNumberMapMaxEntries = 1 << 13;
 constexpr int64_t kMaxUnretransmittableFrameIntervalMs = 33 * 4;
 
 // This is experimental field trial to exclude transport sequence number from
@@ -248,7 +247,6 @@ RTPSenderVideo::RTPSenderVideo(Clock* clock,
                                PlayoutDelayOracle* playout_delay_oracle,
                                FrameEncryptorInterface* frame_encryptor,
                                bool require_frame_encryption,
-                               bool need_rtp_packet_infos,
                                bool enable_retransmit_all_layers,
                                const WebRtcKeyValueConfig& field_trials)
     : RTPSenderVideo([&] {
@@ -259,7 +257,6 @@ RTPSenderVideo::RTPSenderVideo(Clock* clock,
         config.playout_delay_oracle = playout_delay_oracle;
         config.frame_encryptor = frame_encryptor;
         config.require_frame_encryption = require_frame_encryption;
-        config.need_rtp_packet_infos = need_rtp_packet_infos;
         config.enable_retransmit_all_layers = enable_retransmit_all_layers;
         config.field_trials = &field_trials;
         return config;
@@ -275,10 +272,6 @@ RTPSenderVideo::RTPSenderVideo(const Config& config)
       last_rotation_(kVideoRotation_0),
       transmit_color_space_next_frame_(false),
       playout_delay_oracle_(config.playout_delay_oracle),
-      rtp_sequence_number_map_(config.need_rtp_packet_infos
-                                   ? std::make_unique<RtpSequenceNumberMap>(
-                                         kRtpSequenceNumberMapMaxEntries)
-                                   : nullptr),
       red_payload_type_(config.red_payload_type),
       ulpfec_payload_type_(config.ulpfec_payload_type),
       flexfec_sender_(config.flexfec_sender),
@@ -711,7 +704,6 @@ bool RTPSenderVideo::SendVideo(
   if (num_packets == 0)
     return false;
 
-  uint16_t first_sequence_number;
   bool first_frame = first_frame_sent_();
   std::vector<std::unique_ptr<RtpPacketToSend>> rtp_packets;
   for (size_t i = 0; i < num_packets; ++i) {
@@ -735,15 +727,13 @@ bool RTPSenderVideo::SendVideo(
       expected_payload_capacity = limits.max_payload_len;
     }
 
+    packet->set_first_packet_of_frame(i == 0);
+
     if (!packetizer->NextPacket(packet.get()))
       return false;
     RTC_DCHECK_LE(packet->payload_size(), expected_payload_capacity);
     if (!rtp_sender_->AssignSequenceNumber(packet.get()))
       return false;
-
-    if (rtp_sequence_number_map_ && i == 0) {
-      first_sequence_number = packet->SequenceNumber();
-    }
 
     if (i == 0) {
       playout_delay_oracle_->OnSentPacket(packet->SequenceNumber(),
@@ -788,13 +778,6 @@ bool RTPSenderVideo::SendVideo(
     }
   }
 
-  if (rtp_sequence_number_map_) {
-    const uint32_t timestamp = rtp_timestamp - rtp_sender_->TimestampOffset();
-    rtc::CritScope cs(&crit_);
-    rtp_sequence_number_map_->InsertFrame(first_sequence_number, num_packets,
-                                          timestamp);
-  }
-
   LogAndSendToNetwork(std::move(rtp_packets), unpacketized_payload_size);
 
   TRACE_EVENT_ASYNC_END1("webrtc", "Video", capture_time_ms, "timestamp",
@@ -816,37 +799,6 @@ uint32_t RTPSenderVideo::PacketizationOverheadBps() const {
   rtc::CritScope cs(&stats_crit_);
   return packetization_overhead_bitrate_.Rate(clock_->TimeInMilliseconds())
       .value_or(0);
-}
-
-std::vector<RtpSequenceNumberMap::Info> RTPSenderVideo::GetSentRtpPacketInfos(
-    rtc::ArrayView<const uint16_t> sequence_numbers) const {
-  RTC_DCHECK(!sequence_numbers.empty());
-
-  std::vector<RtpSequenceNumberMap::Info> results;
-  if (!rtp_sequence_number_map_) {
-    return results;
-  }
-  results.reserve(sequence_numbers.size());
-
-  {
-    rtc::CritScope cs(&crit_);
-    for (uint16_t sequence_number : sequence_numbers) {
-      const absl::optional<RtpSequenceNumberMap::Info> info =
-          rtp_sequence_number_map_->Get(sequence_number);
-      if (!info) {
-        // The empty vector will be returned. We can delay the clearing
-        // of the vector until after we exit the critical section.
-        break;
-      }
-      results.push_back(*info);
-    }
-  }
-
-  if (results.size() != sequence_numbers.size()) {
-    results.clear();  // Some sequence number was not found.
-  }
-
-  return results;
 }
 
 bool RTPSenderVideo::AllowRetransmission(

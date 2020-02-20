4f241adb6190a4fce65548cd840166aa175a57c5
Marina Ciocea <marinaciocea@webrtc.org>
2020-02-20 15:21:54 +0100

    [Prototype][Insertable streams] Expose frames from webrtc to blink to be transformed.
    Change-Id: I697f7f92ec5c3ac71e3a62327806d89a814a1d8e

---
+++ video/rtp_video_stream_receiver_delegate.h
@@ -0,0 +1,41 @@
+/*
+ *  Copyright (c) 2020 The WebRTC project authors. All Rights Reserved.
+ *
+ *  Use of this source code is governed by a BSD-style license
+ *  that can be found in the LICENSE file in the root of the source
+ *  tree. An additional intellectual property rights grant can be found
+ *  in the file PATENTS.  All contributing project authors may
+ *  be found in the AUTHORS file in the root of the source tree.
+ */
+
+#ifndef VIDEO_RTP_VIDEO_STREAM_RECEIVER_DELEGATE_H_
+#define VIDEO_RTP_VIDEO_STREAM_RECEIVER_DELEGATE_H_
+
+#include "api/frame_transformer_interface.h"
+#include "rtc_base/thread.h"
+#include "rtc_base/weak_ptr.h"
+
+namespace webrtc {
+
+class RtpVideoStreamReceiver;
+
+class RtpVideoStreamReceiverDelegate : public TransformedFrameCallback {
+ public:
+  RtpVideoStreamReceiverDelegate(rtc::WeakPtr<RtpVideoStreamReceiver> receiver,
+                                 rtc::Thread* network_thread);
+
+  // Implements TransformedFrameCallback.
+  void OnTransformedFrame(
+      std::unique_ptr<video_coding::EncodedFrame> frame) override;
+
+ protected:
+  ~RtpVideoStreamReceiverDelegate() override = default;
+
+ private:
+  rtc::WeakPtr<RtpVideoStreamReceiver> receiver_;
+  rtc::Thread* network_thread_;
+};
+
+}  // namespace webrtc
+
+#endif  // VIDEO_RTP_VIDEO_STREAM_RECEIVER_DELEGATE_H_

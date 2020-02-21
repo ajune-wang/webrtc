1d3ade06ea487799e78f9929cebeb37ceff29984
Marina Ciocea <marinaciocea@webrtc.org>
2020-02-21 10:53:41 +0100

    [Prototype][Insertable streams] Expose frames from webrtc to blink to be transformed.
    Bug: webrtc:11380
    Change-Id: I697f7f92ec5c3ac71e3a62327806d89a814a1d8e

--- api/rtp_receiver_interface.cc
+++ api/rtp_receiver_interface.cc
@@ -38,4 +38,7 @@ RtpReceiverInterface::dtls_transport() const {
   return nullptr;
 }
 
+void RtpReceiverInterface::InsertDepacketizerToDecoderFrameTransformer(
+    rtc::scoped_refptr<FrameTransformerInterface> frame_transformer) {}
+
 }  // namespace webrtc

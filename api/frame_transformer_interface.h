92b42a87053db0d8cdc248717da3494f5496bd80
Marina Ciocea <marinaciocea@webrtc.org>
2020-02-27 11:31:16 +0100

    Introduce frame transformer interfaces for Insertable Streams Web API.
    Define FrameTransformerInterface for transforming encoded frames, and
    TransformedFrameCallback for receiving transformed frames.
    
    The FrameTransformerInterface will be implemented on the browser side,
    and will be set in WebRTC sender and receiver in follow up CLs:
    - Sender: https://webrtc-review.googlesource.com/c/src/+/169127
    - Receiver: https://webrtc-review.googlesource.com/c/src/+/169129/1
    
    Insertable Streams Web API explainer:
    https://github.com/alvestrand/webrtc-media-streams/blob/master/explainer.md
    
    Design doc for WebRTC library changes:
    http://doc/1eiLkjNUkRy2FssCPLUp6eH08BZuXXoHfbbBP1ZN7EVk
    
    Bug: webrtc:11380
    Change-Id: Icf8ff159feb604f006e18157660f13d300a08b2b

---
+++ api/frame_transformer_interface.h
@@ -0,0 +1,52 @@
+/*
+ *  Copyright 2020 The WebRTC project authors. All Rights Reserved.
+ *
+ *  Use of this source code is governed by a BSD-style license
+ *  that can be found in the LICENSE file in the root of the source
+ *  tree. An additional intellectual property rights grant can be found
+ *  in the file PATENTS.  All contributing project authors may
+ *  be found in the AUTHORS file in the root of the source tree.
+ */
+
+#ifndef API_FRAME_TRANSFORMER_INTERFACE_H_
+#define API_FRAME_TRANSFORMER_INTERFACE_H_
+
+#include <memory>
+#include <vector>
+
+#include "api/video/encoded_frame.h"
+#include "rtc_base/ref_count.h"
+
+namespace webrtc {
+
+class TransformedFrameCallback;
+
+// Transformes encoded frames. The transformed frame is sent in a callback using
+// the TransformedFrameCallback interface (see below).
+class FrameTransformerInterface : public rtc::RefCountInterface {
+ public:
+  virtual void TransformFrame(std::unique_ptr<video_coding::EncodedFrame> frame,
+                              std::vector<uint8_t> additional_data,
+                              uint32_t ssrc) = 0;
+
+  virtual void RegisterTransformedFrameCallback(
+      rtc::scoped_refptr<TransformedFrameCallback>) = 0;
+  virtual void UnregisterTransformedFrameCallback() = 0;
+
+ protected:
+  ~FrameTransformerInterface() override = default;
+};
+
+// Objects implement this interface to be notified with the transformed frame.
+class TransformedFrameCallback : public rtc::RefCountInterface {
+ public:
+  virtual void OnTransformedFrame(
+      std::unique_ptr<video_coding::EncodedFrame> transformed_frame) = 0;
+
+ protected:
+  ~TransformedFrameCallback() override = default;
+};
+
+}  // namespace webrtc
+
+#endif  // API_FRAME_TRANSFORMER_INTERFACE_H_

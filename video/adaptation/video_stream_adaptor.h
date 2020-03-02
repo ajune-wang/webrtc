9cb912bc7816808692fe5d684867ebd48687b03e
Henrik Boström <hbos@webrtc.org>
2020-03-02 11:45:17 +0100

    [Overuse] Initial version of VideoStreamAdaptor (Restrictor moved).
    This CL simply moved the VideoSourceRestrictor from being an inner class
    of OveruseFrameDetectorResourceAdaptationModule to a new class,
    VideoStreamAdaptor.
    
    In follow-up CLs, the responsibility of determining what the next step
    for adapting up or down is to the VideoStreamAdaptor.
    
    The end-goal is that the VideoStreamAdaptor takes care of "can adapt?"
    and "do adapt!" type of logic so that a multi-stream aware adaptation
    module can decide which one to adapt, and the adaptor can take care of
    the nitty gritty details of executing that order.
    
    This is partially achieved in this CL, but the caller has to know what
    the next step is and when the end-goal is realized the module's decision
    making will be a lot simpler.
    
    Bug: webrtc:11393
    Change-Id: I2c12c1281eca854c62791abb65f0aca47a119726

---
+++ video/adaptation/video_stream_adaptor.h
@@ -0,0 +1,57 @@
+/*
+ *  Copyright 2020 The WebRTC Project Authors. All rights reserved.
+ *
+ *  Use of this source code is governed by a BSD-style license
+ *  that can be found in the LICENSE file in the root of the source
+ *  tree. An additional intellectual property rights grant can be found
+ *  in the file PATENTS.  All contributing project authors may
+ *  be found in the AUTHORS file in the root of the source tree.
+ */
+
+#ifndef VIDEO_ADAPTATION_VIDEO_STREAM_ADAPTOR_H_
+#define VIDEO_ADAPTATION_VIDEO_STREAM_ADAPTOR_H_
+
+#include <memory>
+
+#include "call/adaptation/video_source_restrictions.h"
+
+namespace webrtc {
+
+class VideoStreamAdaptor {
+ public:
+  static const int kMinFramerateFps;
+
+  static int GetLowerFrameRateThan(int fps);
+  static int GetHigherFrameRateThan(int fps);
+  static int GetLowerResolutionThan(int pixel_count);
+  static int GetHigherResolutionThan(int pixel_count);
+
+  VideoStreamAdaptor();
+  ~VideoStreamAdaptor();
+
+  VideoSourceRestrictions source_restrictions();
+  void ClearRestrictions();
+
+  // "Can adapt?" and "do adapt!" methods.
+  // TODO(https://crbug.com/webrtc/11393): Make the adaptor responsible for
+  // deciding what the next step are, i.e. taking on degradation preference
+  // logic. Then, these can be expressed either as CanAdaptUp() and DoAdaptUp()
+  // or as GetNextRestrictionsUp() and ApplyRestrictions().
+  bool CanDecreaseResolutionTo(int target_pixels, int min_pixels_per_frame);
+  void DecreaseResolutionTo(int target_pixels, int min_pixels_per_frame);
+  bool CanIncreaseResolutionTo(int target_pixels);
+  void IncreaseResolutionTo(int target_pixels);
+  bool CanDecreaseFrameRateTo(int max_frame_rate);
+  void DecreaseFrameRateTo(int max_frame_rate);
+  bool CanIncreaseFrameRateTo(int max_frame_rate);
+  void IncreaseFrameRateTo(int max_frame_rate);
+
+ private:
+  class VideoSourceRestrictor;
+
+  const std::unique_ptr<VideoSourceRestrictor> source_restrictor_;
+};
+
+}  // namespace webrtc
+
+#endif  // VIDEO_ADAPTATION_VIDEO_STREAM_ADAPTOR_H_

e14a32f3795f6eea9dcbae440542cc1492d434f2
Henrik Boström <hbos@webrtc.org>
2020-03-05 14:09:40 +0100

    [Overuse] Initial version of VideoStreamAdaptor (Restrictor moved).
    This CL simply moves the VideoSourceRestrictor from being an inner class
    of OveruseFrameDetectorResourceAdaptationModule to a new class,
    VideoStreamAdaptor.
    
    In follow-up CLs, the responsibility of determining what the next step
    for adapting up or down should also be moved to the VideoStreamAdaptor.
    
    The end-goal is that the VideoStreamAdaptor takes care of "can adapt?"
    and "do adapt!" type of logic so that a multi-stream aware adaptation
    module can decide which stream (adaptor) to adapt, and the adaptor can
    take care of the nitty gritty details of doing so.
    
    In this CL the "can?"/"do!" part is realized but not the logic for
    determining what the next step up or down is, and the class interface
    needs improvement.
    
    This CL also sets up the video/adaptation/ subdirectory and moves the
    AdaptationCounters class here. Other adaptation-related classes (e.g.
    the module and its resources) should move into this directory as well
    in the future.
    
    Bug: webrtc:11393
    Change-Id: I2c12c1281eca854c62791abb65f0aca47a119726

--- video/overuse_frame_detector_resource_adaptation_unittest.cc
+++ video/overuse_frame_detector_resource_adaptation_unittest.cc
@@ -12,41 +12,9 @@
 
 #include "test/gmock.h"
 #include "test/gtest.h"
+#include "video/adaptation/adaptation_counters.h"
 
 namespace webrtc {
-
-TEST(AdaptationCountersTest, Addition) {
-  AdaptationCounters a{0, 0};
-  AdaptationCounters b{1, 2};
-  AdaptationCounters total = a + b;
-  EXPECT_EQ(1, total.resolutions_adaptations);
-  EXPECT_EQ(2, total.resolutions_adaptations);
-}
-
-TEST(AdaptationCountersTest, Subtraction) {
-  AdaptationCounters a{0, 1};
-  AdaptationCounters b{2, 1};
-  AdaptationCounters diff = a - b;
-  EXPECT_EQ(-2, diff.resolutions_adaptations);
-  EXPECT_EQ(0, diff.fps_adaptations);
-}
-
-TEST(AdaptationCountersTest, Equality) {
-  AdaptationCounters a{1, 2};
-  AdaptationCounters b{2, 1};
-  EXPECT_EQ(a, a);
-  EXPECT_NE(a, b);
-}
-
-TEST(AdaptationCountersTest, SelfAdditionSubtraction) {
-  AdaptationCounters a{1, 0};
-  AdaptationCounters b{0, 1};
-
-  EXPECT_EQ(a, a + b - b);
-  EXPECT_EQ(a, b + a - b);
-  EXPECT_EQ(a, a - b + b);
-  EXPECT_EQ(a, b - b + a);
-}
 
 TEST(OveruseFrameDetectorResourceAdaptationModuleTest,
      FirstAdaptationDown_Fps) {

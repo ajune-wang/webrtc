dba5023ae30dd505f2724213847ca11fcdcbd8b4
Danil Chapovalov <danilchap@webrtc.org>
2020-03-11 17:31:19 +0100

    Pass callbacks for RtcpReceiver at construction
    Bug: webrtc:10680
    Change-Id: Ic242008e63a5a86ac30ab5f4041a30dbdb7fc72b

--- modules/rtp_rtcp/source/rtcp_receiver.h
+++ modules/rtp_rtcp/source/rtcp_receiver.h
@@ -113,11 +113,6 @@ class RTCPReceiver final {
   // Set new bandwidth and notify remote clients about it.
   void NotifyTmmbrUpdated();
 
-  void RegisterRtcpStatisticsCallback(RtcpStatisticsCallback* callback);
-  void RegisterRtcpCnameCallback(RtcpCnameCallback* callback);
-  RtcpStatisticsCallback* GetRtcpStatisticsCallback();
-  void SetReportBlockDataObserver(ReportBlockDataObserver* observer);
-
  private:
   struct PacketInformation;
   struct TmmbrInformation;
@@ -267,13 +262,14 @@ class RTCPReceiver final {
   // delivered RTP packet to the remote side.
   int64_t last_increased_sequence_number_ms_;
 
-  RtcpStatisticsCallback* stats_callback_ RTC_GUARDED_BY(feedbacks_lock_);
-  RtcpCnameCallback* cname_callback_ RTC_GUARDED_BY(feedbacks_lock_);
+  RtcpStatisticsCallback* const stats_callback_
+      RTC_PT_GUARDED_BY(feedbacks_lock_);
+  RtcpCnameCallback* const cname_callback_ RTC_PT_GUARDED_BY(feedbacks_lock_);
   // TODO(hbos): Remove RtcpStatisticsCallback in favor of
   // ReportBlockDataObserver; the ReportBlockData contains a superset of the
   // RtcpStatistics data.
-  ReportBlockDataObserver* report_block_data_observer_
-      RTC_GUARDED_BY(feedbacks_lock_);
+  ReportBlockDataObserver* const report_block_data_observer_
+      RTC_PT_GUARDED_BY(feedbacks_lock_);
 
   RtcpPacketTypeCounterObserver* const packet_type_counter_observer_;
   RtcpPacketTypeCounter packet_type_counter_;

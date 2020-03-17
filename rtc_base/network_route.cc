218900f20c2a3f242349361eb3cb0fea74726c01
Jonas Oreland <jonaso@webrtc.org>
2020-03-17 12:50:41 +0100

    Extend NetworkRoute with more info about local/remote endpoints
    This patch exends the NetworkRoute struct with more information
    about local/remote endpoints. It adds
    - adapter type
    - adapter id
    - relay
    
    (previously it was "only" network_id)
    
    BUG: webrtc:11434
    Change-Id: I9bcec385b40d707db385fef40b2c7a315dd35dd0

---
+++ rtc_base/network_route.cc
@@ -0,0 +1,28 @@
+/*
+ *  Copyright 2020 The WebRTC Project Authors. All rights reserved.
+ *
+ *  Use of this source code is governed by a BSD-style license
+ *  that can be found in the LICENSE file in the root of the source
+ *  tree. An additional intellectual property rights grant can be found
+ *  in the file PATENTS.  All contributing project authors may
+ *  be found in the AUTHORS file in the root of the source tree.
+ */
+
+#include "rtc_base/network_route.h"
+
+#include "rtc_base/strings/string_builder.h"
+
+namespace rtc {
+
+std::string NetworkRoute::ToString() const {
+  rtc::StringBuilder oss;
+  oss << "[ connected: " << connected << " local: [ " << local.adapter_id << "/"
+      << local.network_id << " " << AdapterTypeToString(local.adapter_type)
+      << " relay: " << local.relay << " ] remote: [ " << remote.adapter_id
+      << "/" << remote.network_id << " "
+      << AdapterTypeToString(remote.adapter_type) << " relay: " << remote.relay
+      << " ] packet_overhead_bytes: " << packet_overhead << " ]";
+  return oss.Release();
+}
+
+}  // namespace rtc
--- rtc_base/network_route.h
+++ rtc_base/network_route.h
@@ -13,21 +13,52 @@
 
 #include <stdint.h>
 
+#include <string>
+
+#include "rtc_base/network_constants.h"
+
 // TODO(honghaiz): Make a directory that describes the interfaces and structs
 // the media code can rely on and the network code can implement, and both can
 // depend on that, but not depend on each other. Then, move this file to that
 // directory.
 namespace rtc {
 
+struct RouteEndpoint {
+  AdapterType adapter_type = ADAPTER_TYPE_UNKNOWN;
+  uint16_t adapter_id = 0;
+  uint16_t network_id = 0;
+  bool relay = false;
+};
+
 struct NetworkRoute {
   bool connected = false;
-  uint16_t local_network_id = 0;
-  uint16_t remote_network_id = 0;
+  RouteEndpoint local;
+  RouteEndpoint remote;
   // Last packet id sent on the PREVIOUS route.
   int last_sent_packet_id = -1;
   // The overhead in bytes from IP layer and above.
+  // This is the maximum of any part of the route.
   int packet_overhead = 0;
+
+  // Downstream projects depend on the old representation,
+  // populate that until they have been migrated.
+  // TODO(jonaso): remove.
+  uint16_t local_network_id = 0;
+  uint16_t remote_network_id = 0;
+
+  std::string ToString() const;
 };
+
+inline bool operator==(const RouteEndpoint& a, const RouteEndpoint& b) {
+  return a.adapter_type == b.adapter_type && a.adapter_id == b.adapter_id &&
+         a.network_id == b.network_id && a.relay == b.relay;
+}
+
+inline bool operator==(const NetworkRoute& a, const NetworkRoute& b) {
+  return a.connected == b.connected && a.local == b.local &&
+         a.remote == b.remote && a.packet_overhead == b.packet_overhead;
+}
+
 }  // namespace rtc
 
 #endif  // RTC_BASE_NETWORK_ROUTE_H_

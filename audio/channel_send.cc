9293263385bf0e7e29d8277826bac65124f4318e
Philipp Hancke <philipp.hancke@googlemail.com>
2020-04-10 17:45:11 +0200

    audio_channel: dont add timestamp start offset twice
    Do not add the rtp timestamp start offset twice and pass
    it to the frame transformer
    
    Bug: webrtc:11501
    Change-Id: I7d10130404d93df7cee3b8f87a0b780801a51415

--- audio/channel_send.cc
+++ audio/channel_send.cc
@@ -389,11 +389,13 @@ int32_t ChannelSend::SendData(AudioFrameType frameType,
     // Asynchronously transform the payload before sending it. After the payload
     // is transformed, the delegate will call SendRtpAudio to send it.
     frame_transformer_delegate_->Transform(
-        frameType, payloadType, rtp_timestamp, payloadData, payloadSize,
-        absolute_capture_timestamp_ms, _rtpRtcpModule->SSRC());
+        frameType, payloadType,
+        rtp_timestamp + _rtpRtcpModule->StartTimestamp(), payloadData,
+        payloadSize, absolute_capture_timestamp_ms, _rtpRtcpModule->SSRC());
     return 0;
   }
-  return SendRtpAudio(frameType, payloadType, rtp_timestamp, payload,
+  return SendRtpAudio(frameType, payloadType,
+                      rtp_timestamp + _rtpRtcpModule->StartTimestamp(), payload,
                       absolute_capture_timestamp_ms);
 }
 
@@ -465,10 +467,9 @@ int32_t ChannelSend::SendRtpAudio(AudioFrameType frameType,
   // knowledge of the offset to a single place.
 
   // This call will trigger Transport::SendPacket() from the RTP/RTCP module.
-  if (!rtp_sender_audio_->SendAudio(
-          frameType, payloadType,
-          rtp_timestamp + _rtpRtcpModule->StartTimestamp(), payload.data(),
-          payload.size(), absolute_capture_timestamp_ms)) {
+  if (!rtp_sender_audio_->SendAudio(frameType, payloadType, rtp_timestamp,
+                                    payload.data(), payload.size(),
+                                    absolute_capture_timestamp_ms)) {
     RTC_DLOG(LS_ERROR)
         << "ChannelSend::SendData() failed to send data to RTP/RTCP module";
     return -1;

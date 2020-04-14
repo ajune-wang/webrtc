786a3e48a47ec2818bd8f71480234576ed315d47
Taylor Brandstetter <deadbeef@webrtc.org>
2020-04-14 16:10:46 -0700

    Remove deprecated SSLIdentity methods that return raw pointers.
    Bug: webrtc:11410
    Change-Id: I40e5549cb7c1082eebd870e0f133a3be0918dcaf

--- rtc_base/openssl_identity.h
+++ rtc_base/openssl_identity.h
@@ -72,15 +72,6 @@ class OpenSSLIdentity final : public SSLIdentity {
   static std::unique_ptr<SSLIdentity> CreateFromPEMChainStrings(
       const std::string& private_key,
       const std::string& certificate_chain);
-  // Deprecated versions
-  static OpenSSLIdentity* GenerateWithExpiration(const std::string& common_name,
-                                                 const KeyParams& key_params,
-                                                 time_t certificate_lifetime);
-  static OpenSSLIdentity* GenerateForTest(const SSLIdentityParams& params);
-  static SSLIdentity* FromPEMStrings(const std::string& private_key,
-                                     const std::string& certificate);
-  static SSLIdentity* FromPEMChainStrings(const std::string& private_key,
-                                          const std::string& certificate_chain);
   ~OpenSSLIdentity() override;
 
   const OpenSSLCertificate& certificate() const override;

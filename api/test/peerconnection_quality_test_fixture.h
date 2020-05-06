19db9d78c4cd7be9ca83b20a7bb6e6d16c910c19
Andrey Logvin <landrey@webrtc.org>
2020-05-06 12:33:52 +0000

    Remove pc level test framework redundant code.
    After the migration to passing frame video source implementation directly, part of the peer connection framework code became redundant. Removing screen_share_config and capturing_device_index from the VideoConfig is to be done in later reviews.
    
    Bug: webrtc:11534
    Change-Id: I7a8ea85d26d00fb5bfe7ec0d2facef9c44a0f749

--- api/test/peerconnection_quality_test_fixture.h
+++ api/test/peerconnection_quality_test_fixture.h
@@ -170,23 +170,11 @@ class PeerConnectionE2EQualityTestFixture {
     // Have to be unique among all specified configs for all peers in the call.
     // Will be auto generated if omitted.
     absl::optional<std::string> stream_label;
-    // You can specify one of |generator|, |input_file_name|,
-    // |screen_share_config| and |capturing_device_index|.
-    // If none of them are specified and config is added to the PeerConfigurer
-    // without specifying any video source, then |generator| will be set to
-    // VideoGeneratorType::kDefault.
-    // If video source implementation is specified,
-    // then it will be used as video source regardless of other
-    // options. Please consider this way unless you are using
-    // |capturing_device_index| option, since the possibility of using other
-    // three is about to be removed.
 
-    // If specified generator of this type will be used to produce input video.
-    absl::optional<VideoGeneratorType> generator;
-    // If specified this file will be used as input. Input video will be played
-    // in a circle.
-    absl::optional<std::string> input_file_name;
-    // If specified screen share video stream will be created as input.
+    // If specified will be used in order to define if
+    // VideoTrackInterface::ContentHint::kText should be set for current video
+    // track.
+    // TODO(landrey) use just use_text_content_hint field here.
     absl::optional<ScreenShareConfig> screen_share_config;
     // If specified this capturing device will be used to get input video. The
     // |capturing_device_index| is the index of required capturing device in OS

3feb11cb4e714bef9828c1e0e660fd7b9ce5f066
Markus Handell <handellm@webrtc.org>
2020-05-18 13:59:58 +0200

    PhysicalSocketServer: remove dynamic epoll_event handling.
    This change deletes dynamic handling of number of epoll
    events handled per call to epoll, with the assumption
    that PSS load is likely not dominated by the epoll syscalls.
    
    Bug:webrtc:11567
    Change-Id: I34fbe1fa8bf0a037bf849a4adac1a0a333c9dd86

--- rtc_base/physical_socket_server.h
+++ rtc_base/physical_socket_server.h
@@ -16,6 +16,7 @@
 #define WEBRTC_USE_EPOLL 1
 #endif
 
+#include <array>
 #include <memory>
 #include <set>
 #include <vector>
@@ -80,6 +81,9 @@ class RTC_EXPORT PhysicalSocketServer : public SocketServer {
   void Update(Dispatcher* dispatcher);
 
  private:
+  // The number of events to process with one call to "epoll_wait".
+  static constexpr size_t kNumEpollEvents = 128;
+
   typedef std::set<Dispatcher*> DispatcherSet;
 
   void AddRemovePendingDispatchers();
@@ -94,8 +98,8 @@ class RTC_EXPORT PhysicalSocketServer : public SocketServer {
   bool WaitEpoll(int cms);
   bool WaitPoll(int cms, Dispatcher* dispatcher);
 
+  std::array<struct epoll_event, kNumEpollEvents> epoll_events_;
   const int epoll_fd_ = INVALID_SOCKET;
-  std::vector<struct epoll_event> epoll_events_;
 #endif  // WEBRTC_USE_EPOLL
   DispatcherSet dispatchers_;
   DispatcherSet pending_add_dispatchers_;

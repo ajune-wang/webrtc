ea413f31c1227347ed7a6b77f496451b78866752
Markus Handell <handellm@webrtc.org>
2020-05-18 15:51:17 +0200

    PhysicalSocketServer: remove heap-based epoll_event handling.
    This change deletes heap-based handling of number of epoll
    events handled per call to epoll, with the assumption
    that PSS load is likely not dominated by the epoll syscalls.
    
    Bug:webrtc:11567
    Change-Id: I34fbe1fa8bf0a037bf849a4adac1a0a333c9dd86

--- rtc_base/physical_socket_server.h
+++ rtc_base/physical_socket_server.h
@@ -16,6 +16,7 @@
 #define WEBRTC_USE_EPOLL 1
 #endif
 
+#include <array>
 #include <memory>
 #include <set>
 #include <vector>
@@ -25,6 +26,7 @@
 #include "rtc_base/socket_server.h"
 #include "rtc_base/system/rtc_export.h"
 #include "rtc_base/thread_annotations.h"
+#include "rtc_base/thread_checker.h"
 
 #if defined(WEBRTC_POSIX)
 typedef int SOCKET;
@@ -81,6 +83,9 @@ class RTC_EXPORT PhysicalSocketServer : public SocketServer {
   void Update(Dispatcher* dispatcher);
 
  private:
+  // The number of events to process with one call to "epoll_wait".
+  static constexpr size_t kNumEpollEvents = 128;
+
   typedef std::set<Dispatcher*> DispatcherSet;
 
   void AddRemovePendingDispatchers() RTC_EXCLUSIVE_LOCKS_REQUIRED(crit_);
@@ -95,8 +100,9 @@ class RTC_EXPORT PhysicalSocketServer : public SocketServer {
   bool WaitEpoll(int cms);
   bool WaitPoll(int cms, Dispatcher* dispatcher);
 
+  std::array<struct epoll_event, kNumEpollEvents> epoll_events_
+      RTC_GUARDED_BY(wait_thread_checker_);
   const int epoll_fd_ = INVALID_SOCKET;
-  std::vector<struct epoll_event> epoll_events_;
 #endif  // WEBRTC_USE_EPOLL
   DispatcherSet dispatchers_ RTC_GUARDED_BY(crit_);
   DispatcherSet pending_add_dispatchers_ RTC_GUARDED_BY(crit_);
@@ -108,6 +114,7 @@ class RTC_EXPORT PhysicalSocketServer : public SocketServer {
   const WSAEVENT socket_ev_;
 #endif
   bool fWait_;
+  rtc::ThreadChecker wait_thread_checker_;
 };
 
 class PhysicalSocket : public AsyncSocket, public sigslot::has_slots<> {

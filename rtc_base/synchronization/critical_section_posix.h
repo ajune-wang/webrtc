8883751d3bcdc4ed123d7e79f4afce300abe1627
Danil Chapovalov <danilchap@webrtc.org>
2020-05-26 19:07:15 +0200

    Split CriticalSection implementation per platform
    Bug: None
    Change-Id: I0b0a782a2c20527c71569f222fe62a3a188a0f7b

---
+++ rtc_base/synchronization/critical_section_posix.h
@@ -0,0 +1,41 @@
+/*
+ *  Copyright 2020 The WebRTC Project Authors. All rights reserved.
+ *
+ *  Use of this source code is governed by a BSD-style license
+ *  that can be found in the LICENSE file in the root of the source
+ *  tree. An additional intellectual property rights grant can be found
+ *  in the file PATENTS.  All contributing project authors may
+ *  be found in the AUTHORS file in the root of the source tree.
+ */
+
+#ifndef RTC_BASE_SYNCHRONIZATION_CRITICAL_SECTION_POSIX_H_
+#define RTC_BASE_SYNCHRONIZATION_CRITICAL_SECTION_POSIX_H_
+
+#include <pthread.h>
+#include <time.h>
+
+namespace webrtc {
+namespace webrtc_critical_section_internal {
+
+class CriticalSectionImpl {
+ public:
+  CriticalSectionImpl();
+  ~CriticalSectionImpl() { pthread_mutex_destroy(&mutex_); }
+
+  void Enter() { pthread_mutex_lock(&mutex_); }
+  bool TryEnter() { return pthread_mutex_trylock(&mutex_) == 0; }
+  void Leave() { pthread_mutex_unlock(&mutex_); }
+
+ private:
+  pthread_mutex_t mutex_;
+};
+
+inline void Yield() {
+  constexpr struct timespec ts_null = {0};
+  nanosleep(&ts_null, nullptr);
+}
+
+}  // namespace webrtc_critical_section_internal
+}  // namespace webrtc
+
+#endif  // RTC_BASE_SYNCHRONIZATION_CRITICAL_SECTION_POSIX_H_

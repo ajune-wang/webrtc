3bfa532265d7f5e6a9ba937389601cdd044e4147
Danil Chapovalov <danilchap@webrtc.org>
2020-05-27 14:24:59 +0200

    Add av1 temporal scalability test.
    Bug: webrtc:11404
    Change-Id: Iaf2fcca0dd450f7b296bd0250a119b8e7dfef270

--- modules/video_coding/codecs/av1/libaom_av1_unittest.cc
+++ modules/video_coding/codecs/av1/libaom_av1_unittest.cc
@@ -22,6 +22,7 @@
 #include "modules/video_coding/codecs/av1/libaom_av1_decoder.h"
 #include "modules/video_coding/codecs/av1/libaom_av1_encoder.h"
 #include "modules/video_coding/codecs/av1/scalable_video_controller.h"
+#include "modules/video_coding/codecs/av1/scalable_video_controller_l1t2.h"
 #include "modules/video_coding/codecs/av1/scalable_video_controller_no_layering.h"
 #include "modules/video_coding/include/video_codec_interface.h"
 #include "modules/video_coding/include/video_error_codes.h"
@@ -271,6 +272,8 @@ INSTANTIATE_TEST_SUITE_P(
     Svc,
     LibaomAv1SvcTest,
     Values(SvcTestParam{std::make_unique<ScalableVideoControllerNoLayering>,
+                        /*num_frames_to_generate=*/4},
+           SvcTestParam{std::make_unique<ScalableVideoControllerL1T2>,
                         /*num_frames_to_generate=*/4}));
 
 }  // namespace

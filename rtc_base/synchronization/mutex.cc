2a3375d6154aaed5e36d5fe256e30c3645a0bba6
Markus Handell <handellm@webrtc.org>
2020-05-29 15:02:16 +0200

    Introduces rtc_base/synchronization/mutex.h.
    This change introduces a new non-reentrant mutex to WebRTC. It
    enables eventual migration to Abseil's mutex.
    
    The mutex types supportable by webrtc::Mutex are
    
    - std::mutex
    - absl::Mutex
    - CriticalSection (Windows only)
    - pthread_mutex (POSIX only)
    
    In addition to introducing the mutexes, the CL also changes
    PacketBuffer to use the new mutex instead of rtc::CriticalSecion.
    
    Additionally, google_benchmark benchmarks for the mutexes were added
    (test courtesy of danilchap@), and some results from a pthread/Abseil
    shootout were added showing Abseil has the advantage.
    
    Bug: webrtc:11567
    Change-Id: Iaec324ccb32ec3851bf6db3fd290f5ea5dee4c81

---
+++ rtc_base/synchronization/mutex.cc
@@ -0,0 +1,50 @@
+/*
+ *  Copyright 2020 The WebRTC Project Authors. All rights reserved.
+ *
+ *  Use of this source code is governed by a BSD-style license
+ *  that can be found in the LICENSE file in the root of the source
+ *  tree. An additional intellectual property rights grant can be found
+ *  in the file PATENTS.  All contributing project authors may
+ *  be found in the AUTHORS file in the root of the source tree.
+ */
+
+#include "rtc_base/synchronization/mutex.h"
+
+#include <time.h>
+
+#include "rtc_base/atomic_ops.h"
+#include "rtc_base/checks.h"
+#include "rtc_base/platform_thread_types.h"
+#include "rtc_base/system/unused.h"
+
+namespace webrtc {
+
+void GlobalMutex::Lock() {
+#if !defined(WEBRTC_WIN) && \
+    (!defined(WEBRTC_MAC) || RTC_USE_NATIVE_MUTEX_ON_MAC)
+  const struct timespec ts_null = {0};
+#endif
+
+  while (rtc::AtomicOps::CompareAndSwap(&mutex_locked_, 0, 1)) {
+#if defined(WEBRTC_WIN)
+    ::Sleep(0);
+#else
+    nanosleep(&ts_null, nullptr);
+#endif
+  }
+}
+
+void GlobalMutex::Unlock() {
+  int old_value = rtc::AtomicOps::CompareAndSwap(&mutex_locked_, 1, 0);
+  RTC_DCHECK_EQ(1, old_value) << "Unlock called without calling Lock first";
+}
+
+GlobalMutexLock::GlobalMutexLock(GlobalMutex* mu) : mu_(mu) {
+  mu_->Lock();
+}
+
+GlobalMutexLock::~GlobalMutexLock() {
+  mu_->Unlock();
+}
+
+}  // namespace webrtc

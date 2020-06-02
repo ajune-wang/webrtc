9d0dd6ab482b96b0c7a968260e62a50948717b42
Philipp Hancke <fippo@sip-communicator.org>
2020-06-02 13:30:11 +0200

    red: modify the encoder to send RFC 2198
    modifies the RED encoder to send the actual RFC 2198 format
    described in
      https://tools.ietf.org/html/rfc2198
    Decoding is handled in neteq, see red_payload_splitter.h
    
    BUG=tbd
    
    Change-Id: Ib3005882a3ceee49d2b05c43357f552432a984ac

--- modules/audio_coding/codecs/red/audio_encoder_copy_red.cc
+++ modules/audio_coding/codecs/red/audio_encoder_copy_red.cc
@@ -15,6 +15,7 @@
 #include <utility>
 #include <vector>
 
+#include "rtc_base/byte_order.h"
 #include "rtc_base/checks.h"
 
 namespace webrtc {
@@ -59,11 +60,50 @@ AudioEncoder::EncodedInfo AudioEncoderCopyRed::EncodeImpl(
     uint32_t rtp_timestamp,
     rtc::ArrayView<const int16_t> audio,
     rtc::Buffer* encoded) {
+  // Allocate room for RFC 2198 header.
+  rtc::Buffer header(secondary_info_.encoded_bytes > 0 ? 5 : 0);
+  encoded->AppendData(header);
+
   const size_t primary_offset = encoded->size();
+  size_t secondary_offset = 0;
+
+  if (secondary_info_.encoded_bytes > 0) {
+    encoded->AppendData(secondary_encoded_);
+    secondary_offset = secondary_info_.encoded_bytes;
+  }
   EncodedInfo info = speech_encoder_->Encode(rtp_timestamp, audio, encoded);
 
+  if (info.encoded_bytes == 0) {
+    encoded->Clear();
+    return info;
+  }
+
+  // Actually construct the RFC 2198 header.
+  if (secondary_info_.encoded_bytes > 0) {
+    // TODO(fippo): oddly rtp_timestamp - secondary_info_.encoded_timestamp is
+    // 1440, not 960. Therefore we compare the encoded timestamps.
+    const uint32_t timestamp_delta =
+        info.encoded_timestamp > secondary_info_.encoded_timestamp
+            ? info.encoded_timestamp - secondary_info_.encoded_timestamp
+            : secondary_info_.encoded_timestamp - info.encoded_timestamp;
+
+    encoded->data()[0] = secondary_info_.payload_type | 0x80;
+    rtc::SetBE16(static_cast<uint8_t*>(encoded->data()) + 1,
+                 (timestamp_delta << 2) | (secondary_info_.encoded_bytes >> 8));
+    encoded->data()[3] = secondary_info_.encoded_bytes & 0xff;
+  }
+  // TODO(fippo): with DTX we get a TS delta of up to 19200 (i.e. 400ms).
+  // We might not want to send very old RED in that case.
+
+  if (secondary_info_.encoded_bytes > 0) {
+    encoded->data()[4] = info.payload_type;
+  } else {
+    // encoded->data()[0] = info.payload_type;
+  }
+
   RTC_CHECK(info.redundant.empty()) << "Cannot use nested redundant encoders.";
-  RTC_DCHECK_EQ(encoded->size() - primary_offset, info.encoded_bytes);
+  RTC_DCHECK_EQ(encoded->size() - primary_offset - secondary_offset,
+                info.encoded_bytes);
 
   if (info.encoded_bytes > 0) {
     // |info| will be implicitly cast to an EncodedInfoLeaf struct, effectively
@@ -72,19 +112,22 @@ AudioEncoder::EncodedInfo AudioEncoderCopyRed::EncodeImpl(
     info.redundant.push_back(info);
     RTC_DCHECK_EQ(info.redundant.size(), 1);
     if (secondary_info_.encoded_bytes > 0) {
-      encoded->AppendData(secondary_encoded_);
       info.redundant.push_back(secondary_info_);
       RTC_DCHECK_EQ(info.redundant.size(), 2);
     }
     // Save primary to secondary.
-    secondary_encoded_.SetData(encoded->data() + primary_offset,
-                               info.encoded_bytes);
+    secondary_encoded_.SetData(
+        encoded->data() + primary_offset + secondary_info_.encoded_bytes,
+        info.encoded_bytes);
     secondary_info_ = info;
     RTC_DCHECK_EQ(info.speech, info.redundant[0].speech);
   }
+
   // Update main EncodedInfo.
-  info.payload_type = red_payload_type_;
-  info.encoded_bytes = 0;
+  if (header.size() > 0) {
+    info.payload_type = red_payload_type_;
+  }
+  info.encoded_bytes = primary_offset;
   for (std::vector<EncodedInfoLeaf>::const_iterator it = info.redundant.begin();
        it != info.redundant.end(); ++it) {
     info.encoded_bytes += it->encoded_bytes;

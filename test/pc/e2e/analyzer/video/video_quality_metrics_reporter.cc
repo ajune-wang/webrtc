2b56ba07f7353925d65a14403125702f0f9ca77d
Andrey Logvin <landrey@webrtc.org>
2020-06-11 10:08:15 +0000

    TMP: Migrate PC level test framework on new GetStats API
    Bug: None
    Change-Id: I607803db345acab0e510c21858e629262744c846

--- test/pc/e2e/analyzer/video/video_quality_metrics_reporter.cc
+++ test/pc/e2e/analyzer/video/video_quality_metrics_reporter.cc
@@ -10,6 +10,9 @@
 
 #include "test/pc/e2e/analyzer/video/video_quality_metrics_reporter.h"
 
+#include "api/stats/rtcstats_objects.h"
+#include "api/units/time_delta.h"
+
 namespace webrtc {
 namespace webrtc_pc_e2e {
 namespace {
@@ -20,6 +23,7 @@ constexpr int kBitsInByte = 8;
 
 void VideoQualityMetricsReporter::Start(absl::string_view test_case_name) {
   test_case_name_ = std::string(test_case_name);
+  start_time_ = Now();
 }
 
 // TODO(bugs.webrtc.org/10430): Migrate to the new GetStats as soon as
@@ -57,6 +61,84 @@ void VideoQualityMetricsReporter::OnStatsReports(
   }
 }
 
+void VideoQualityMetricsReporter::OnStatsReports(
+    const std::string& pc_label,
+    const rtc::scoped_refptr<const RTCStatsReport>& report) {
+  RTC_CHECK(start_time_)
+      << "Please invoke Start(...) method before calling OnStatsReports(...)";
+
+  auto transport_stats = report->GetStatsOfType<RTCTransportStats>();
+  if (transport_stats.size() == 0u ||
+      !transport_stats[0]->selected_candidate_pair_id.is_defined()) {
+    return;
+  }
+  std::string selected_ice_id =
+      transport_stats[0]->selected_candidate_pair_id.ValueToString();
+  // Use the selected ICE candidate pair ID to get the appropriate ICE stats.
+  const RTCIceCandidatePairStats ice_candidate_pair_stats =
+      report->Get(selected_ice_id)->cast_to<const RTCIceCandidatePairStats>();
+
+  auto outbound_rtp_stats = report->GetStatsOfType<RTCOutboundRTPStreamStats>();
+  StatsSample sample;
+  for (auto& s : outbound_rtp_stats) {
+    if (!s->media_type.is_defined()) {
+      continue;
+    }
+    if (!(*s->media_type == "video")) {
+      continue;
+    }
+    if (s->timestamp_us() > sample.sample_time_us) {
+      sample.sample_time_us = s->timestamp_us();
+    }
+    if (s->retransmitted_bytes_sent.is_defined()) {
+      sample.retransmitted_bytes_sent += *s->retransmitted_bytes_sent;
+    }
+    if (s->bytes_sent.is_defined()) {
+      sample.bytes_sent += *s->bytes_sent;
+    }
+    if (s->header_bytes_sent.is_defined()) {
+      sample.header_bytes_sent += *s->header_bytes_sent;
+    }
+    if (s->total_encoded_bytes_target.is_defined()) {
+      sample.total_encoded_bytes_target += *s->total_encoded_bytes_target;
+    }
+  }
+
+  rtc::CritScope crit(&video_bwe_stats_lock_);
+  VideoBweStats& video_bwe_stats = video_bwe_stats_[pc_label];
+  if (ice_candidate_pair_stats.available_outgoing_bitrate.is_defined()) {
+    video_bwe_stats.available_send_bandwidth_new.AddSample(
+        *ice_candidate_pair_stats.available_outgoing_bitrate);
+  }
+
+  StatsSample prev_sample = last_stats_sample_[pc_label];
+  if (prev_sample.sample_time_us == 0) {
+    prev_sample.sample_time_us = start_time_->us();
+  }
+  last_stats_sample_[pc_label] = sample;
+  std::printf("Debug log:\n");
+  prev_sample.DebugLog();
+  sample.DebugLog();
+
+  int64_t time_between_samples_us =
+      sample.sample_time_us - prev_sample.sample_time_us;
+  if (time_between_samples_us == 0) {
+    return;
+  }
+  std::printf("Time: %lu; Delta: %lu\n", sample.sample_time_us,
+              time_between_samples_us);
+  double time_between_samples_sec = time_between_samples_us / 1000000.0;
+  video_bwe_stats.retransmission_bitrate_new.AddSample(
+      static_cast<double>(sample.retransmitted_bytes_sent -
+                          prev_sample.retransmitted_bytes_sent) /
+      time_between_samples_sec);
+  video_bwe_stats.transmission_bitrate_new.AddSample(
+      static_cast<double>(sample.bytes_sent + sample.header_bytes_sent -
+                          prev_sample.bytes_sent -
+                          prev_sample.header_bytes_sent) /
+      time_between_samples_sec);
+}
+
 void VideoQualityMetricsReporter::StopAndReportResults() {
   rtc::CritScope video_bwe_crit(&video_bwe_stats_lock_);
   for (const auto& item : video_bwe_stats_) {
@@ -72,15 +154,22 @@ std::string VideoQualityMetricsReporter::GetTestCaseName(
 void VideoQualityMetricsReporter::ReportVideoBweResults(
     const std::string& test_case_name,
     const VideoBweStats& video_bwe_stats) {
+  //ReportResult("available_send_bandwidth", test_case_name,
+  //             video_bwe_stats.available_send_bandwidth / kBitsInByte,
+  //             "bytesPerSecond");
   ReportResult("available_send_bandwidth", test_case_name,
-               video_bwe_stats.available_send_bandwidth / kBitsInByte,
+               video_bwe_stats.available_send_bandwidth_new / kBitsInByte,
                "bytesPerSecond");
+  //ReportResult("transmission_bitrate", test_case_name,
+  //             video_bwe_stats.transmission_bitrate / kBitsInByte,
+  //             "bytesPerSecond");
   ReportResult("transmission_bitrate", test_case_name,
-               video_bwe_stats.transmission_bitrate / kBitsInByte,
-               "bytesPerSecond");
+               video_bwe_stats.transmission_bitrate_new, "bytesPerSecond");
+  //ReportResult("retransmission_bitrate", test_case_name,
+  //             video_bwe_stats.retransmission_bitrate / kBitsInByte,
+  //             "bytesPerSecond");
   ReportResult("retransmission_bitrate", test_case_name,
-               video_bwe_stats.retransmission_bitrate / kBitsInByte,
-               "bytesPerSecond");
+               video_bwe_stats.retransmission_bitrate_new, "bytesPerSecond");
 }
 
 void VideoQualityMetricsReporter::ReportResult(

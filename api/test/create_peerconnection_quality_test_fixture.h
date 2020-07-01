a28ceed7f0ce9f7c035d9f0cb8b936ef6d453491
Artem Titov <titovartem@webrtc.org>
2020-07-01 14:02:26 +0200

    Add TimeController to the CreatePeerConnectionE2EQualityTestFixture API
    Add TimeController to the CreatePeerConnectionE2EQualityTestFixture
    method as a first step to make PC level framework compatible with
    TimeController abstraction.
    
    Bug: webrtc:11743
    Change-Id: I69305abc880059bf9fe1d4f2e3b7c10cf35417db

--- api/test/create_peerconnection_quality_test_fixture.h
+++ api/test/create_peerconnection_quality_test_fixture.h
@@ -15,6 +15,7 @@
 
 #include "api/test/audio_quality_analyzer_interface.h"
 #include "api/test/peerconnection_quality_test_fixture.h"
+#include "api/test/time_controller.h"
 #include "api/test/video_quality_analyzer_interface.h"
 
 namespace webrtc {
@@ -29,6 +30,13 @@ namespace webrtc_pc_e2e {
 std::unique_ptr<PeerConnectionE2EQualityTestFixture>
 CreatePeerConnectionE2EQualityTestFixture(
     std::string test_case_name,
+    std::unique_ptr<AudioQualityAnalyzerInterface> audio_quality_analyzer,
+    std::unique_ptr<VideoQualityAnalyzerInterface> video_quality_analyzer);
+
+std::unique_ptr<PeerConnectionE2EQualityTestFixture>
+CreatePeerConnectionE2EQualityTestFixture(
+    std::string test_case_name,
+    TimeController* time_controller,
     std::unique_ptr<AudioQualityAnalyzerInterface> audio_quality_analyzer,
     std::unique_ptr<VideoQualityAnalyzerInterface> video_quality_analyzer);
 

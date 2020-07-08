ae911cdbbba50de8b601f0d5e4f7d6153597cc19
Mirko Bonadei <mbonadei@webrtc.org>
2020-07-08 09:42:30 +0000

    Revert "Implement packets_(sent | received) for RTCTransportStats"
    This reverts commit fb6f975401972635a644c0db06c135b4c0aaef4a.
    
    Reason for revert: Looks like this breaks chromium.webrtc.fyi:
    https://ci.chromium.org/p/chromium/builders/webrtc.fyi/WebRTC%20Chromium%20FYI%20Mac%20Tester/6000
    https://ci.chromium.org/p/chromium/builders/webrtc.fyi/WebRTC%20Chromium%20FYI%20Win10%20Tester/6209
    https://ci.chromium.org/p/chromium/builders/webrtc.fyi/WebRTC%20Chromium%20FYI%20Win7%20Tester/6177
    https://ci.chromium.org/p/chromium/builders/webrtc.fyi/WebRTC%20Chromium%20FYI%20Win8%20Tester/6299
    
    Original change's description:
    > Implement packets_(sent | received) for RTCTransportStats
    > 
    > Bug: webrtc:11756
    > Change-Id: Ic0caad6d4675969ef3ae886f50326e4a2e1cbfe7
    > Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/178741
    > Reviewed-by: Tommi <tommi@webrtc.org>
    > Reviewed-by: Henrik Boström <hbos@webrtc.org>
    > Commit-Queue: Artem Titov <titovartem@webrtc.org>
    > Cr-Commit-Position: refs/heads/master@{#31643}
    
    TBR=hbos@webrtc.org,tommi@webrtc.org,titovartem@webrtc.org
    
    Change-Id: Icbb0974ba29cbddb614f1f37f8a2de1a7c56b571
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: webrtc:11756

--- pc/rtc_stats_collector_unittest.cc
+++ pc/rtc_stats_collector_unittest.cc
@@ -2139,9 +2139,6 @@ TEST_F(RTCStatsCollectorTest, CollectRTCTransportStats) {
   rtp_connection_info.remote_candidate = *rtp_remote_candidate.get();
   rtp_connection_info.sent_total_bytes = 42;
   rtp_connection_info.recv_total_bytes = 1337;
-  rtp_connection_info.sent_total_packets = 3;
-  rtp_connection_info.sent_discarded_packets = 2;
-  rtp_connection_info.packets_received = 4;
   cricket::TransportChannelStats rtp_transport_channel_stats;
   rtp_transport_channel_stats.component = cricket::ICE_CANDIDATE_COMPONENT_RTP;
   rtp_transport_channel_stats.ice_transport_stats.connection_infos.push_back(
@@ -2159,9 +2156,7 @@ TEST_F(RTCStatsCollectorTest, CollectRTCTransportStats) {
           rtc::ToString(cricket::ICE_CANDIDATE_COMPONENT_RTP),
       report->timestamp_us());
   expected_rtp_transport.bytes_sent = 42;
-  expected_rtp_transport.packets_sent = 1;
   expected_rtp_transport.bytes_received = 1337;
-  expected_rtp_transport.packets_received = 4;
   expected_rtp_transport.dtls_state = RTCDtlsTransportState::kNew;
   expected_rtp_transport.selected_candidate_pair_changes = 1;
 
@@ -2176,9 +2171,6 @@ TEST_F(RTCStatsCollectorTest, CollectRTCTransportStats) {
   rtcp_connection_info.remote_candidate = *rtcp_remote_candidate.get();
   rtcp_connection_info.sent_total_bytes = 1337;
   rtcp_connection_info.recv_total_bytes = 42;
-  rtcp_connection_info.sent_total_packets = 3;
-  rtcp_connection_info.sent_discarded_packets = 2;
-  rtcp_connection_info.packets_received = 4;
   cricket::TransportChannelStats rtcp_transport_channel_stats;
   rtcp_transport_channel_stats.component =
       cricket::ICE_CANDIDATE_COMPONENT_RTCP;
@@ -2196,9 +2188,7 @@ TEST_F(RTCStatsCollectorTest, CollectRTCTransportStats) {
           rtc::ToString(cricket::ICE_CANDIDATE_COMPONENT_RTCP),
       report->timestamp_us());
   expected_rtcp_transport.bytes_sent = 1337;
-  expected_rtcp_transport.packets_sent = 1;
   expected_rtcp_transport.bytes_received = 42;
-  expected_rtcp_transport.packets_received = 4;
   expected_rtcp_transport.dtls_state = RTCDtlsTransportState::kConnecting;
   expected_rtcp_transport.selected_candidate_pair_changes = 0;
 
@@ -2292,9 +2282,6 @@ TEST_F(RTCStatsCollectorTest, CollectRTCTransportStatsWithCrypto) {
   rtp_connection_info.remote_candidate = *rtp_remote_candidate.get();
   rtp_connection_info.sent_total_bytes = 42;
   rtp_connection_info.recv_total_bytes = 1337;
-  rtp_connection_info.sent_total_packets = 3;
-  rtp_connection_info.sent_discarded_packets = 2;
-  rtp_connection_info.packets_received = 4;
   cricket::TransportChannelStats rtp_transport_channel_stats;
   rtp_transport_channel_stats.component = cricket::ICE_CANDIDATE_COMPONENT_RTP;
   rtp_transport_channel_stats.ice_transport_stats.connection_infos.push_back(
@@ -2317,9 +2304,7 @@ TEST_F(RTCStatsCollectorTest, CollectRTCTransportStatsWithCrypto) {
           rtc::ToString(cricket::ICE_CANDIDATE_COMPONENT_RTP),
       report->timestamp_us());
   expected_rtp_transport.bytes_sent = 42;
-  expected_rtp_transport.packets_sent = 1;
   expected_rtp_transport.bytes_received = 1337;
-  expected_rtp_transport.packets_received = 4;
   expected_rtp_transport.dtls_state = RTCDtlsTransportState::kConnected;
   expected_rtp_transport.selected_candidate_pair_changes = 1;
   // Crypto parameters

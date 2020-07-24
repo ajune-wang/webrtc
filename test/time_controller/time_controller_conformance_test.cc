d26151ce991bd69833bc8d93aa7a37c04a038748
Artem Titov <titovartem@google.com>
2020-07-24 13:15:21 +0200

    Add time controller conformance test and fix conformance bug
    Bug: webrtc:11799
    Change-Id: I13f79f3ab025c105e56dcb93da5b7631893850e2

---
+++ test/time_controller/time_controller_conformance_test.cc
@@ -0,0 +1,79 @@
+/*
+ *  Copyright 2020 The WebRTC project authors. All Rights Reserved.
+ *
+ *  Use of this source code is governed by a BSD-style license
+ *  that can be found in the LICENSE file in the root of the source
+ *  tree. An additional intellectual property rights grant can be found
+ *  in the file PATENTS.  All contributing project authors may
+ *  be found in the AUTHORS file in the root of the source tree.
+ */
+
+#include <memory>
+#include <vector>
+
+#include "api/test/time_controller.h"
+#include "api/units/time_delta.h"
+#include "rtc_base/location.h"
+#include "rtc_base/thread.h"
+#include "test/gmock.h"
+#include "test/gtest.h"
+#include "test/time_controller/real_time_controller.h"
+#include "test/time_controller/simulated_time_controller.h"
+
+namespace webrtc {
+namespace {
+
+using ::testing::ElementsAreArray;
+using ::testing::TestParamInfo;
+using ::testing::TestWithParam;
+using ::testing::Values;
+
+enum class TimeMode { kRealTime, kSimulated };
+
+std::unique_ptr<TimeController> CreateTimeController(TimeMode mode) {
+  switch (mode) {
+    case TimeMode::kRealTime:
+      return std::make_unique<RealTimeController>();
+    case TimeMode::kSimulated:
+      // Using an offset of 100000 to get nice fixed width and readable
+      // timestamps in typical test scenarios.
+      constexpr Timestamp kSimulatedStartTime = Timestamp::Seconds(100000);
+      return std::make_unique<GlobalSimulatedTimeController>(
+          kSimulatedStartTime);
+  }
+}
+
+std::string ParamsToString(const TestParamInfo<webrtc::TimeMode>& param) {
+  switch (param.param) {
+    case webrtc::TimeMode::kRealTime:
+      return "RealTime";
+    case webrtc::TimeMode::kSimulated:
+      return "SimulatedTime";
+    default:
+      RTC_CHECK(false) << "Time mode not supported";
+  }
+}
+
+class TimeControllerConformanceTest : public TestWithParam<webrtc::TimeMode> {};
+
+TEST_P(TimeControllerConformanceTest, ThreadTaskOrderTest) {
+  std::unique_ptr<TimeController> time_controller =
+      CreateTimeController(GetParam());
+  std::unique_ptr<rtc::Thread> thread = time_controller->CreateThread("thread");
+
+  // Tasks on thread have to be executed in order in which they were
+  // posted/invoked.
+  std::vector<int> execution_order;
+  thread->PostTask(RTC_FROM_HERE, [&]() { execution_order.push_back(1); });
+  thread->Invoke<void>(RTC_FROM_HERE, [&]() { execution_order.push_back(2); });
+  time_controller->AdvanceTime(TimeDelta::Millis(100));
+  EXPECT_THAT(execution_order, ElementsAreArray({1, 2}));
+}
+
+INSTANTIATE_TEST_SUITE_P(ConformanceTest,
+                         TimeControllerConformanceTest,
+                         Values(TimeMode::kRealTime, TimeMode::kSimulated),
+                         ParamsToString);
+
+}  // namespace
+}  // namespace webrtc

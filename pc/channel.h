127ac35f5496d53039f69e4615b5ddb4e4bd8f0a
Lennart Grahl <lennart.grahl@gmail.com>
2020-07-29 19:01:09 +0200

    Fix RTP header extension encryption
    Previously, RTP header extensions with encryption had been filtered
    if the encryption had been activated (not the other way around) which
    was likely an unintended logic inversion.
    
    In addition, it ensures that encrypted RTP header extensions are only
    negotiated if RTP header extension encryption is turned on. Formerly,
    which extensions had been negotiated depended on the order in which
    they were inserted, regardless of whether or not header encryption was
    actually enabled, leading to no extensions being sent on the wire.
    
    Further changes:
    
    - If RTP header encryption enabled, prefer encrypted extensions over
      non-encrypted extensions
    - Add most extensions to list of extensions supported for encryption
    - Discard encrypted extensions in a session description in case encryption
      is not supported for that extension
    
    Note that this depends on https://github.com/cisco/libsrtp/pull/491 to get
    into libwebrtc (cherry-pick or bump libsrtp version). Otherwise, two-byte
    header extensions will prevent any RTP packets being sent/received.
    
    Bug: webrtc:11713
    Change-Id: Ia0779453d342fa11e06996d9bc2d3c826f3466d3

--- pc/channel.h
+++ pc/channel.h
@@ -245,10 +245,11 @@ class BaseChannel : public ChannelInterface,
   virtual bool SetRemoteContent_w(const MediaContentDescription* content,
                                   webrtc::SdpType type,
                                   std::string* error_desc) = 0;
-  // Return a list of RTP header extensions with the non-encrypted extensions
-  // removed depending on the current crypto_options_ and only if both the
-  // non-encrypted and encrypted extension is present for the same URI.
-  RtpHeaderExtensions GetFilteredRtpHeaderExtensions(
+
+  // Returns a list of RTP header extensions where any extension URI is unique.
+  // Encrypted extensions will be either preferred or discarded, depending on
+  // the current crypto_options_.
+  RtpHeaderExtensions GetDeduplicatedRtpHeaderExtensions(
       const RtpHeaderExtensions& extensions);
 
   // From MessageHandler

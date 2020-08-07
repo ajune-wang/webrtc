a3243c35431047a745ba1d200b78cb47a93b388c
Jonas Oreland <jonaso@webrtc.org>
2020-08-07 09:04:20 +0200

    Add interface for os/firmware to affect ICE selection.
    This patch adds an interface for os/firmware to set a network
    preference NOT_PREFERRED / NEUTRAL that can be picked up by
    an IceController and used when selection ice candidate pair.
    
    The patch exposes this using an Android Intent based interface.
    
    BUG: webrtc:11825
    Change-Id: Ic12b6bf704fde7f9c912020dd7bc79ccae4613ab

--- sdk/android/instrumentationtests/src/org/webrtc/NetworkMonitorTest.java
+++ sdk/android/instrumentationtests/src/org/webrtc/NetworkMonitorTest.java
@@ -31,14 +31,16 @@ import android.support.test.annotation.UiThreadTest;
 import android.support.test.filters.MediumTest;
 import android.support.test.filters.SmallTest;
 import android.support.test.rule.UiThreadTestRule;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicReference;
 import org.chromium.base.test.BaseJUnit4ClassRunner;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.webrtc.NetworkMonitorAutoDetect.ConnectionType;
+import org.webrtc.NetworkChangeDetector.ConnectionType;
+import org.webrtc.NetworkChangeDetector.NetworkInformation;
 import org.webrtc.NetworkMonitorAutoDetect.ConnectivityManagerDelegate;
-import org.webrtc.NetworkMonitorAutoDetect.NetworkInformation;
 import org.webrtc.NetworkMonitorAutoDetect.NetworkState;
 
 /**
@@ -155,6 +157,10 @@ public class NetworkMonitorTest {
 
     @Override
     public void onNetworkDisconnect(long networkHandle) {}
+
+    @Override
+    public void onNetworkPreference(List<ConnectionType> types, @NetworkPreference int preference) {
+    }
   }
 
   private static final Object lock = new Object();
@@ -311,9 +317,34 @@ public class NetworkMonitorTest {
     Context context = ContextUtils.getApplicationContext();
     networkMonitor.startMonitoring(context);
     assertEquals(1, networkMonitor.getNumObservers());
-    assertNotNull(networkMonitor.getNetworkMonitorAutoDetect());
+    assertNotNull(networkMonitor.getNetworkChangeDetector());
     networkMonitor.stopMonitoring();
     assertEquals(0, networkMonitor.getNumObservers());
-    assertNull(networkMonitor.getNetworkMonitorAutoDetect());
+    assertNull(networkMonitor.getNetworkChangeDetector());
+  }
+
+  /**
+   * Tests setNetworkChangeDetectorFactory.
+   */
+  @Test
+  @SmallTest
+  public void testSetNetworkChangeDetectorFactory() {
+    NetworkMonitor networkMonitor = NetworkMonitor.getInstance();
+    Context context = ContextUtils.getApplicationContext();
+    final AtomicReference<NetworkChangeDetector> detector = new AtomicReference<>(null);
+    networkMonitor.setNetworkChangeDetectorFactory(new NetworkChangeDetectorFactory() {
+      @Override
+      public NetworkChangeDetector create(
+          NetworkChangeDetector.Observer observer, Context context) {
+        detector.set(new NetworkMonitorAutoDetect(observer, context));
+        return detector.get();
+      }
+    });
+    networkMonitor.startMonitoring(context);
+    assertEquals(1, networkMonitor.getNumObservers());
+    assertEquals(detector.get(), networkMonitor.getNetworkChangeDetector());
+    networkMonitor.stopMonitoring();
+    assertEquals(0, networkMonitor.getNumObservers());
+    assertNull(networkMonitor.getNetworkChangeDetector());
   }
 }

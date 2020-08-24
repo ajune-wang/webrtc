fd4759be0a53d3093e00473456ff724922c4d840
Danil Chapovalov <danilchap@webrtc.org>
2020-08-24 13:12:34 +0200

    Remove RTPFragmentationHeader in most places through webrtc
    (This draft change ignores backward combatibility)
    
    Bug: webrtc:6471
    Change-Id: I5cc586fb5dd31f43dc747139077ff7e45a3cab8f

--- sdk/objc/components/video_codec/RTCVideoEncoderH264.mm
+++ sdk/objc/components/video_codec/RTCVideoEncoderH264.mm
@@ -783,17 +783,6 @@ NSUInteger GetMaxSampleRate(const webrtc::H264::ProfileLevelId &profile_level_id
   }
 
   __block std::unique_ptr<rtc::Buffer> buffer = std::make_unique<rtc::Buffer>();
-  RTC_OBJC_TYPE(RTCRtpFragmentationHeader) * header;
-  {
-    std::unique_ptr<webrtc::RTPFragmentationHeader> header_cpp;
-    bool result =
-        H264CMSampleBufferToAnnexBBuffer(sampleBuffer, isKeyframe, buffer.get(), &header_cpp);
-    header = [[RTC_OBJC_TYPE(RTCRtpFragmentationHeader) alloc]
-        initWithNativeFragmentationHeader:header_cpp.get()];
-    if (!result) {
-      return;
-    }
-  }
 
   RTC_OBJC_TYPE(RTCEncodedImage) *frame = [[RTC_OBJC_TYPE(RTCEncodedImage) alloc] init];
   // This assumes ownership of `buffer` and is responsible for freeing it when done.
@@ -818,6 +807,8 @@ NSUInteger GetMaxSampleRate(const webrtc::H264::ProfileLevelId &profile_level_id
   _h264BitstreamParser.GetLastSliceQp(&qp);
   frame.qp = @(qp);
 
+  RTC_OBJC_TYPE(RTCRtpFragmentationHeader) *header =
+      [[RTC_OBJC_TYPE(RTCRtpFragmentationHeader) alloc] init];
   BOOL res = _callback(frame, codecSpecificInfo, header);
   if (!res) {
     RTC_LOG(LS_ERROR) << "Encode callback failed";

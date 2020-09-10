e2757d4e1820d9b469302a700d4c8e9fa8965f93
Lahiru Ginnaliya Gamathige <glahiru@webrtc.org>
2020-09-10 13:15:21 -0700

    Cancer Stick Castle, a sigslot replacement
    This is the initial implementation of the cancer stick castle library
    with basic unit testing.
    
    Bug: none
    Change-Id: Ibf28b4e20f74792fce2fe11d4780fd375a4ad3a3

---
+++ rtc_base/cancer_stick_castle_unittest.cc
@@ -0,0 +1,128 @@
+/*
+ *  Copyright 2020 The WebRTC Project Authors. All rights reserved.
+ *
+ *  Use of this source code is governed by a BSD-style license
+ *  that can be found in the LICENSE file in the root of the source
+ *  tree. An additional intellectual property rights grant can be found
+ *  in the file PATENTS.  All contributing project authors may
+ *  be found in the AUTHORS file in the root of the source tree.
+ */
+#include <string>
+
+#include "api/function_view.h"
+#include "rtc_base/bind.h"
+#include "rtc_base/cancer_stick_castle.h"
+#include "test/gtest.h"
+
+namespace rtc {
+TEST(CancerStickCastle, NoRecieverSingleMessageTest) {
+  webrtc::CancerStickCastle<std::string> c;
+
+  c.Send("message");
+}
+
+TEST(CancerStickCastle, MultipleParameterMessageTest) {
+  webrtc::CancerStickCastle<std::string, int> c;
+
+  c.Send("message0", 123);
+}
+
+TEST(CancerStickCastle, NoParameterMessageTest) {
+  webrtc::CancerStickCastle<> c;
+
+  c.Send();
+}
+
+TEST(CancerStickCastle, ReferenceTest) {
+  webrtc::CancerStickCastle<int&> c;
+  int index = 1;
+
+  c.AddReceiver([](int& index) { index++; });
+  c.Send(index);
+
+  EXPECT_EQ(index, 2);
+}
+
+TEST(CancerStickCastle, ConstReferenceTest) {
+  webrtc::CancerStickCastle<int&> c;
+  int i = 0;
+  int index = 1;
+
+  c.AddReceiver([&](const int& index) mutable { i = index; });
+  c.Send(index);
+
+  EXPECT_EQ(i, 1);
+}
+
+TEST(CancerStickCastle, FunctionPtrTest) {
+  webrtc::CancerStickCastle<int&> c;
+  auto increment = [](int& index) { index++; };
+  int index = 1;
+
+  c.AddReceiver(increment);
+  c.Send(index);
+
+  EXPECT_EQ(index, 2);
+}
+
+TEST(CancerStickCastle, PointerTest) {
+  webrtc::CancerStickCastle<int*> c;
+  int index = 1;
+
+  c.AddReceiver([](int* index) { (*index)++; });
+  c.Send(&index);
+
+  EXPECT_EQ(index, 2);
+}
+
+void PlusOne(int& a) {
+  a++;
+}
+
+TEST(CancerStickCastle, SmallNonTrivialTest) {
+  webrtc::CancerStickCastle<int&> c;
+  std::function<void(int&)> smallFunc = PlusOne;
+  int index = 1;
+
+  c.AddReceiver(smallFunc);
+  c.Send(index);
+
+  EXPECT_EQ(index, 2);
+}
+
+/* sizeof(LargeTrivial) = 20bytes which is greater than
+ * the size check (16bytes) of the CSC library */
+struct LargeTrivial {
+  int a, b, c, d, e;
+  void operator()() { a = 10; }
+};
+
+TEST(CancerStickCastle, LargeTrivial) {
+  webrtc::CancerStickCastle<> c;
+  LargeTrivial l = LargeTrivial();
+  LargeTrivial& r = l;
+
+  c.AddReceiver(r);
+  c.Send();
+}
+
+struct OnlyNonTriviallyConstructible {
+  int a;
+  OnlyNonTriviallyConstructible() = default;
+  OnlyNonTriviallyConstructible(OnlyNonTriviallyConstructible& m) {}
+  OnlyNonTriviallyConstructible(OnlyNonTriviallyConstructible&& m) {}
+  void operator()() { a = 10; }
+};
+
+TEST(CancerStickCastle, OnlyNonTriviallyMoveConstructible) {
+  webrtc::CancerStickCastle<> c;
+  OnlyNonTriviallyConstructible l = OnlyNonTriviallyConstructible();
+
+  c.AddReceiver(l);
+  c.Send();
+}
+// todo(glahiru): Add a test to test function pointers
+// todo(glahiru): Add a test case to catch some error for Karl's first fix
+// which used the following code in the Send
+// f.Call<void(ArgT...), ArgT...
+}  // namespace rtc

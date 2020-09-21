d7e92fc6843915491cfd1a0d71df5260c7093d8c
Alessio Bazzica <alessiob@webrtc.org>
2020-09-21 16:25:23 +0200

    AGC2 saturation protector: simplify impl
    By removing the private class SaturationProtector::PeakEnveloper.
    
    Bug: webrtc:7494
    Change-Id: I6cad2974397319737c8ac201d44311bf16275f28

--- modules/audio_processing/agc2/saturation_protector.cc
+++ modules/audio_processing/agc2/saturation_protector.cc
@@ -26,21 +26,41 @@ constexpr int kRingBuffCapacity = kPeakEnveloperBufferSize;
 
 }  // namespace
 
-SaturationProtector::PeakEnveloper::PeakEnveloper() = default;
+SaturationProtector::SaturationProtector(ApmDataDumper* apm_data_dumper)
+    : SaturationProtector(apm_data_dumper,
+                          GetInitialSaturationMarginDb(),
+                          GetExtraSaturationMarginOffsetDb()) {}
 
-void SaturationProtector::PeakEnveloper::Process(float frame_peak_dbfs) {
+SaturationProtector::SaturationProtector(ApmDataDumper* apm_data_dumper,
+                                         float initial_saturation_margin_db,
+                                         float extra_saturation_margin_db)
+    : apm_data_dumper_(apm_data_dumper),
+      initial_saturation_margin_db_(initial_saturation_margin_db),
+      extra_saturation_margin_db_(extra_saturation_margin_db) {
+  Reset();
+}
+
+void SaturationProtector::Reset() {
+  margin_db_ = initial_saturation_margin_db_;
+  peak_delay_buffer_.next = 0;
+  peak_delay_buffer_.size = 0;
+  max_peaks_dbfs_ = kMinLevelDbfs;
+  time_since_push_ms_ = 0;
+}
+
+void SaturationProtector::UpdateMargin(float speech_peak_dbfs,
+                                       float speech_level_dbfs) {
   RTC_DCHECK_EQ(peak_delay_buffer_.buffer.size(), kRingBuffCapacity);
   RTC_DCHECK_LT(peak_delay_buffer_.next, kRingBuffCapacity);
   RTC_DCHECK_LE(peak_delay_buffer_.size, kRingBuffCapacity);
 
   // Get the max peak over `kPeakEnveloperSuperFrameLengthMs` ms.
-  current_superframe_peak_dbfs_ =
-      std::max(current_superframe_peak_dbfs_, frame_peak_dbfs);
-  speech_time_in_estimate_ms_ += kFrameDurationMs;
-  if (speech_time_in_estimate_ms_ > kPeakEnveloperSuperFrameLengthMs) {
-    // Push `current_superframe_peak_dbfs_` back into the ring buffer.
-    peak_delay_buffer_.buffer[peak_delay_buffer_.next++] =
-        current_superframe_peak_dbfs_;
+  max_peaks_dbfs_ = std::max(max_peaks_dbfs_, speech_peak_dbfs);
+  time_since_push_ms_ += kFrameDurationMs;
+  if (time_since_push_ms_ >
+      static_cast<int>(kPeakEnveloperSuperFrameLengthMs)) {
+    // Push `max_peaks_dbfs_` back into the ring buffer.
+    peak_delay_buffer_.buffer[peak_delay_buffer_.next++] = max_peaks_dbfs_;
     if (peak_delay_buffer_.next == kRingBuffCapacity) {
       peak_delay_buffer_.next = 0;
     }
@@ -48,65 +68,44 @@ void SaturationProtector::PeakEnveloper::Process(float frame_peak_dbfs) {
       peak_delay_buffer_.size++;
     }
     // Reset.
-    speech_time_in_estimate_ms_ = 0;
-    current_superframe_peak_dbfs_ = kMinLevelDbfs;
+    max_peaks_dbfs_ = kMinLevelDbfs;
+    time_since_push_ms_ = 0;
   }
+
+  // Update margin by comparing the estimated speech level and the delayed max
+  // speech peak power.
+  const float difference_db = GetDelayedPeakDbfs() - speech_level_dbfs;
+  if (margin_db_ < difference_db) {
+    margin_db_ = margin_db_ * kSaturationProtectorAttackConstant +
+                 difference_db * (1.f - kSaturationProtectorAttackConstant);
+  } else {
+    margin_db_ = margin_db_ * kSaturationProtectorDecayConstant +
+                 difference_db * (1.f - kSaturationProtectorDecayConstant);
+  }
+
+  margin_db_ = rtc::SafeClamp<float>(margin_db_, kMinMarginDb, kMaxMarginDb);
 }
 
-float SaturationProtector::PeakEnveloper::Query() const {
-  RTC_DCHECK_LT(peak_delay_buffer_.next, kRingBuffCapacity);
+float SaturationProtector::GetDelayedPeakDbfs() const {
+  // TODO(alessiob): Check with aleloi@ why we use a delay and how to tune it.
   return peak_delay_buffer_.size == 0
-             ? current_superframe_peak_dbfs_
+             ? max_peaks_dbfs_
              : peak_delay_buffer_
                    .buffer[peak_delay_buffer_.size == kRingBuffCapacity
                                ? peak_delay_buffer_.next
                                : 0];
 }
 
-SaturationProtector::SaturationProtector(ApmDataDumper* apm_data_dumper)
-    : SaturationProtector(apm_data_dumper, GetExtraSaturationMarginOffsetDb()) {
-}
-
-SaturationProtector::SaturationProtector(ApmDataDumper* apm_data_dumper,
-                                         float extra_saturation_margin_db)
-    : apm_data_dumper_(apm_data_dumper),
-      last_margin_(GetInitialSaturationMarginDb()),
-      extra_saturation_margin_db_(extra_saturation_margin_db) {}
-
-void SaturationProtector::UpdateMargin(
-    const VadWithLevel::LevelAndProbability& vad_data,
-    float last_speech_level_estimate) {
-  peak_enveloper_.Process(vad_data.speech_peak_dbfs);
-  const float delayed_peak_dbfs = peak_enveloper_.Query();
-  const float difference_db = delayed_peak_dbfs - last_speech_level_estimate;
-
-  if (last_margin_ < difference_db) {
-    last_margin_ = last_margin_ * kSaturationProtectorAttackConstant +
-                   difference_db * (1.f - kSaturationProtectorAttackConstant);
-  } else {
-    last_margin_ = last_margin_ * kSaturationProtectorDecayConstant +
-                   difference_db * (1.f - kSaturationProtectorDecayConstant);
-  }
-
-  last_margin_ =
-      rtc::SafeClamp<float>(last_margin_, kMinMarginDb, kMaxMarginDb);
-}
-
-float SaturationProtector::LastMargin() const {
-  return last_margin_ + extra_saturation_margin_db_;
-}
-
-void SaturationProtector::Reset() {
-  peak_enveloper_ = PeakEnveloper();
+float SaturationProtector::GetMarginDb() const {
+  return margin_db_ + extra_saturation_margin_db_;
 }
 
 void SaturationProtector::DebugDumpEstimate() const {
   if (apm_data_dumper_) {
     apm_data_dumper_->DumpRaw(
         "agc2_adaptive_saturation_protector_delayed_peak_dbfs",
-        peak_enveloper_.Query());
-    apm_data_dumper_->DumpRaw("agc2_adaptive_saturation_margin_db",
-                              last_margin_);
+        GetDelayedPeakDbfs());
+    apm_data_dumper_->DumpRaw("agc2_adaptive_saturation_margin_db", margin_db_);
   }
 }
 

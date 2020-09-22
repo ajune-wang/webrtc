3b638d2980df97c99afe5d0e97651a74a10da5a3
Alessio Bazzica <alessiob@webrtc.org>
2020-09-22 11:35:02 +0200

    AGC2 saturation protector: extra margin added by level estimator
    In preparation for a coming refactoring CL, the (fixed) extra saturation
    margin is now applied into `AdaptiveModeLevelEstimator`.
    
    This CL also improves the unit tests by hard-coding its saturation
    params instead of reading them from a field trial.
    This reduces the chances of making the test flaky if a default value
    changes.
    
    Tested: Bit-exactness verified with audioproc_f
    
    Bug: webrtc:7494
    Change-Id: I6765def9887a2f4e55b04d929af754cfecbb1626

--- modules/audio_processing/agc2/saturation_protector.cc
+++ modules/audio_processing/agc2/saturation_protector.cc
@@ -54,16 +54,12 @@ absl::optional<float> SaturationProtector::RingBuffer::Front() const {
 }
 
 SaturationProtector::SaturationProtector(ApmDataDumper* apm_data_dumper)
-    : SaturationProtector(apm_data_dumper,
-                          GetInitialSaturationMarginDb(),
-                          GetExtraSaturationMarginOffsetDb()) {}
+    : SaturationProtector(apm_data_dumper, GetInitialSaturationMarginDb()) {}
 
 SaturationProtector::SaturationProtector(ApmDataDumper* apm_data_dumper,
-                                         float initial_saturation_margin_db,
-                                         float extra_saturation_margin_db)
+                                         float initial_saturation_margin_db)
     : apm_data_dumper_(apm_data_dumper),
-      initial_saturation_margin_db_(initial_saturation_margin_db),
-      extra_saturation_margin_db_(extra_saturation_margin_db) {
+      initial_saturation_margin_db_(initial_saturation_margin_db) {
   Reset();
 }
 
@@ -105,10 +101,6 @@ void SaturationProtector::UpdateMargin(float speech_peak_dbfs,
 
 float SaturationProtector::GetDelayedPeakDbfs() const {
   return peak_delay_buffer_.Front().value_or(max_peaks_dbfs_);
-}
-
-float SaturationProtector::GetMarginDb() const {
-  return margin_db_ + extra_saturation_margin_db_;
 }
 
 void SaturationProtector::DebugDumpEstimate() const {

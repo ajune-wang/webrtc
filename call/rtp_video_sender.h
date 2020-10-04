4aa8f995ad7c792eda0e0d730d05056198f41552
Erik Språng <sprang@webrtc.org>
2020-10-04 11:18:29 +0200

    Cleans up code related to legacy pre-pacing fec generation.
    Bug: webrtc:11340
    Change-Id: If3493db9fafdd3ad041f78999e304c8714be517f

--- call/rtp_video_sender.h
+++ call/rtp_video_sender.h
@@ -174,7 +174,6 @@ class RtpVideoSender : public RtpVideoSenderInterface,
   const bool account_for_packetization_overhead_;
   const bool use_early_loss_detection_;
   const bool has_packet_feedback_;
-  const bool use_deferred_fec_;
 
   // TODO(holmer): Remove mutex_ once RtpVideoSender runs on the
   // transport task queue.

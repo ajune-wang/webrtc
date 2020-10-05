d044da3793536c1e79e105e87d1adba0382715d7
Ivo Creusen <ivoc@webrtc.org>
2020-10-05 16:05:50 +0200

    Notify NetEqController during muted state.
    During muted state NetEq shortcircuits a large part of the internals to
    quickly return a buffer filled with zeros. It can be beneficial for the
    controller to be aware that it is in muted state.
    
    Bug: webrtc:11005
    Change-Id: I5fe24b4a3704d953cbd68b5a24bbb7ef58b30be0

--- modules/audio_coding/neteq/decision_logic.h
+++ modules/audio_coding/neteq/decision_logic.h
@@ -82,6 +82,8 @@ class DecisionLogic : public NetEqController {
                                     uint32_t main_timestamp,
                                     int fs_hz) override;
 
+  void MutedState() override {}
+
   void RegisterEmptyPacket() override { delay_manager_->RegisterEmptyPacket(); }
 
   bool SetMaximumDelay(int delay_ms) override {

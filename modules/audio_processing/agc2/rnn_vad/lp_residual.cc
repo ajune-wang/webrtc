8cee5b3355eb606dee14f9d89bf0de7fc1a05451
Alessio Bazzica <alessiob@webrtc.org>
2020-10-21 13:14:22 +0200

    RNN VAD: LP residual optimized (part 4)
    This CL removes the circular shift to compute the convolution in
    `ComputeLpResidual()`, which is now 4x faster.
    
    Bug: webrtc:10480
    Change-Id: Ia7767d9b57378c12c8ff31f58fea03905be5c5de

--- modules/audio_processing/agc2/rnn_vad/lp_residual.cc
+++ modules/audio_processing/agc2/rnn_vad/lp_residual.cc
@@ -117,19 +117,18 @@ void ComputeLpResidual(
     rtc::ArrayView<const float, kNumLpcCoefficients> lpc_coeffs,
     rtc::ArrayView<const float> x,
     rtc::ArrayView<float> y) {
-  RTC_DCHECK_LT(kNumLpcCoefficients, x.size());
+  RTC_DCHECK_GT(x.size(), kNumLpcCoefficients);
   RTC_DCHECK_EQ(x.size(), y.size());
-  std::array<float, kNumLpcCoefficients> input_chunk;
-  input_chunk.fill(0.f);
-  for (size_t i = 0; i < y.size(); ++i) {
-    const float sum = std::inner_product(input_chunk.begin(), input_chunk.end(),
-                                         lpc_coeffs.begin(), x[i]);
-    // Circular shift and add a new sample.
-    for (size_t j = kNumLpcCoefficients - 1; j > 0; --j)
-      input_chunk[j] = input_chunk[j - 1];
-    input_chunk[0] = x[i];
-    // Copy result.
-    y[i] = sum;
+  y[0] = x[0];
+  for (size_t i = 1; i < kNumLpcCoefficients; ++i) {
+    y[i] = std::inner_product(x.cbegin(), x.cbegin() + i,
+                              lpc_coeffs.crbegin() + kNumLpcCoefficients - i,
+                              x[i]);
+  }
+  const float* first = x.cbegin();
+  for (size_t i = kNumLpcCoefficients; i < y.size(); ++i, ++first) {
+    y[i] = std::inner_product(first, first + kNumLpcCoefficients,
+                              lpc_coeffs.crbegin(), x[i]);
   }
 }
 

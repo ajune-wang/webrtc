7e436e7d2f4ad4f3e90c9c7f2af0d963f926e5f7
Alessio Bazzica <alessiob@webrtc.org>
2020-11-03 21:15:22 +0100

    RNN VAD: pitch search optimizations (part 2)
    This CL brings a large improvement to the VAD by precomputing the
    energy for the sliding frame `y` in the pitch buffer. The realtime
    factor has improved by +30x.
    
    There is room for additional improvement (TODOs added), but that will
    be done in a follow up CL since the change won't be bit-exact and
    careful testing is needed.
    
    Bug: webrtc:10480
    Change-Id: I599a4dda2bde16dc6c2f42cf89e96afbd4630311

--- modules/audio_processing/agc2/rnn_vad/pitch_search.cc
+++ modules/audio_processing/agc2/rnn_vad/pitch_search.cc
@@ -19,36 +19,48 @@ namespace webrtc {
 namespace rnn_vad {
 
 PitchEstimator::PitchEstimator()
-    : pitch_buf_decimated_(kBufSize12kHz),
-      pitch_buf_decimated_view_(pitch_buf_decimated_.data(), kBufSize12kHz),
-      auto_corr_(kNumInvertedLags12kHz),
-      auto_corr_view_(auto_corr_.data(), kNumInvertedLags12kHz) {
-  RTC_DCHECK_EQ(kBufSize12kHz, pitch_buf_decimated_.size());
-  RTC_DCHECK_EQ(kNumInvertedLags12kHz, auto_corr_view_.size());
+    : y_energy_(kMaxPitch24kHz + 1, 0.f),
+      y_energy_view_(y_energy_.data(), kMaxPitch24kHz + 1),
+      pitch_buffer_decimated_(kBufSize12kHz),
+      pitch_buffer_decimated_view_(pitch_buffer_decimated_.data(),
+                                   kBufSize12kHz),
+      auto_correlation_(kNumInvertedLags12kHz),
+      auto_correlation_view_(auto_correlation_.data(), kNumInvertedLags12kHz) {
+  RTC_DCHECK_EQ(kBufSize12kHz, pitch_buffer_decimated_.size());
+  RTC_DCHECK_EQ(kNumInvertedLags12kHz, auto_correlation_view_.size());
 }
 
 PitchEstimator::~PitchEstimator() = default;
 
 PitchInfo PitchEstimator::Estimate(
-    rtc::ArrayView<const float, kBufSize24kHz> pitch_buf) {
+    rtc::ArrayView<const float, kBufSize24kHz> pitch_buffer) {
   // Perform the initial pitch search at 12 kHz.
-  Decimate2x(pitch_buf, pitch_buf_decimated_view_);
-  auto_corr_calculator_.ComputeOnPitchBuffer(pitch_buf_decimated_view_,
-                                             auto_corr_view_);
+  Decimate2x(pitch_buffer, pitch_buffer_decimated_view_);
+  auto_corr_calculator_.ComputeOnPitchBuffer(pitch_buffer_decimated_view_,
+                                             auto_correlation_view_);
   CandidatePitchPeriods pitch_candidates_inverted_lags =
-      FindBestPitchPeriods12kHz(auto_corr_view_, pitch_buf_decimated_view_);
+      FindBestPitchPeriods12kHz(auto_correlation_view_,
+                                pitch_buffer_decimated_view_);
+
   // Refine the pitch period estimation.
+
+  // Pre-compute frame energies at 24 kHz.
+  ComputeSlidingFrameSquareEnergies(pitch_buffer, y_energy_view_);
+  // ComputeSlidingFrameSquareEnergies(pitch_buffer, y_energy_view_ ,
+  // y_energy_.back());
+
   // The refinement is done using the pitch buffer that contains 24 kHz samples.
   // Therefore, adapt the inverted lags in |pitch_candidates_inv_lags| from 12
   // to 24 kHz.
   pitch_candidates_inverted_lags.best *= 2;
   pitch_candidates_inverted_lags.second_best *= 2;
-  const int pitch_inv_lag_48kHz =
-      RefinePitchPeriod48kHz(pitch_buf, pitch_candidates_inverted_lags);
+  const int pitch_inv_lag_48kHz = RefinePitchPeriod48kHz(
+      pitch_buffer, y_energy_view_, pitch_candidates_inverted_lags);
   // Look for stronger harmonics to find the final pitch period and its gain.
   RTC_DCHECK_LT(pitch_inv_lag_48kHz, kMaxPitch48kHz);
   last_pitch_48kHz_ = CheckLowerPitchPeriodsAndComputePitchGain(
-      pitch_buf, kMaxPitch48kHz - pitch_inv_lag_48kHz, last_pitch_48kHz_);
+      pitch_buffer, y_energy_view_, kMaxPitch48kHz - pitch_inv_lag_48kHz,
+      last_pitch_48kHz_);
   return last_pitch_48kHz_;
 }
 

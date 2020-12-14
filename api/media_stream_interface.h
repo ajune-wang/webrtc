93abdec3065c5fa9c37de0d93ca7cee12cbb2c56
Gustaf Ullberg <gustaf@webrtc.org>
2020-12-14 20:25:06 +0100

    Surface the number of encoded channels
    Two audio channels going into the AudioSource::Sink can either be
    down-mixed to mono or encoded as stereo. This change enables WebRTC
    users (such as Chromium) to query the number of audio channels actually
    encoded. That information can in turn be used to tailor the audio
    processing to the number of channels actually encoded.
    
    Bug: webrtc:8133
    Change-Id: I8e8a08292002919784c05a5aacb21707918809c8

--- api/media_stream_interface.h
+++ api/media_stream_interface.h
@@ -216,6 +216,11 @@ class AudioTrackSinkInterface {
                   number_of_frames);
   }
 
+  // Returns the number of channels that are processed by the sink. This can
+  // be less the number_of_channels if down-mixing occur. A value of -1 means
+  // an unknown number.
+  virtual int NumProcessedChannels() const { return -1; }
+
  protected:
   virtual ~AudioTrackSinkInterface() {}
 };

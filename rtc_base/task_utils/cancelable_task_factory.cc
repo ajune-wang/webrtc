0e13826a8183b9f75b30e3642e99036f967a2b76
Danil Chapovalov <danilchap@webrtc.org>
2021-01-15 20:06:48 +0100

    Introduce helper for cancellable TaskQueue tasks
    Bug: None
    Change-Id: I4566727197f486c463ad8f0bca182dd2e01a9d03

---
+++ rtc_base/task_utils/cancelable_task_factory.cc
@@ -0,0 +1,63 @@
+/*
+ *  Copyright 2020 The WebRTC Project Authors. All rights reserved.
+ *
+ *  Use of this source code is governed by a BSD-style license
+ *  that can be found in the LICENSE file in the root of the source
+ *  tree. An additional intellectual property rights grant can be found
+ *  in the file PATENTS.  All contributing project authors may
+ *  be found in the AUTHORS file in the root of the source tree.
+ */
+
+#include "rtc_base/task_utils/cancelable_task_factory.h"
+#include "rtc_base/checks.h"
+
+namespace webrtc {
+
+void CancelableTaskFactory::ControlBlock::AddRef() {
+  ref_count_.IncRef();
+}
+
+void CancelableTaskFactory::ControlBlock::Release() {
+  if (ref_count_.DecRef() == rtc::RefCountReleaseStatus::kDroppedLastRef) {
+    delete this;
+  }
+}
+
+void CancelableTaskFactory::ControlBlock::CancelAll() {
+  {
+    MutexLock lock(&mu_);
+    canceled_ = true;
+    if (num_running_ == 0) {
+      return;
+    }
+  }
+  // Some tasks were running, thus need to wait until they are done.
+  canceling_.Wait(rtc::Event::kForever);
+}
+
+bool CancelableTaskFactory::ControlBlock::StartTask() {
+  MutexLock lock(&mu_);
+  if (canceled_) {
+    return false;
+  }
+  ++num_running_;
+  return true;
+}
+
+void CancelableTaskFactory::ControlBlock::MarkTaskCompleted() {
+  {
+    MutexLock lock(&mu_);
+    RTC_DCHECK_GT(num_running_, 0);
+    --num_running_;
+    if (!canceled_ || num_running_ > 0) {
+      return;
+    }
+  }
+  // CancelAll was called while a task was running and this was the last
+  // task running. Unblock the CancelAll function.
+  canceling_.Set();
+}
+
+CancelableTaskFactory::CancelableTaskFactory() : controll_(new ControlBlock) {}
+
+}  // namespace webrtc

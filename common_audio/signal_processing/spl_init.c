84c111bb7e09ecb2d159b8e6c6c29fd6ed9b929c
Ivo Creusen <ivoc@webrtc.org>
2021-01-21 17:31:48 +0100

    Optimize the AbsMax operations by implementing it in terms of min/max.
    The AbsMax operation is used several times in the audio processing.
    By using a min/max search to implement this function, it can be done
    more efficiently. In the NetEq perf test, this gives an improvement of
    ~25% on non-Neon platforms. The improvements in the Neon version are
    expected to be lower.
    
    Bug: None
    Change-Id: I7e91f944e1caf876db66bc462109254e0b23f229

--- common_audio/signal_processing/spl_init.c
+++ common_audio/signal_processing/spl_init.c
@@ -18,8 +18,6 @@
 
 #if defined(WEBRTC_HAS_NEON)
 
-const MaxAbsValueW16 WebRtcSpl_MaxAbsValueW16 = WebRtcSpl_MaxAbsValueW16Neon;
-const MaxAbsValueW32 WebRtcSpl_MaxAbsValueW32 = WebRtcSpl_MaxAbsValueW32Neon;
 const MaxValueW16 WebRtcSpl_MaxValueW16 = WebRtcSpl_MaxValueW16Neon;
 const MaxValueW32 WebRtcSpl_MaxValueW32 = WebRtcSpl_MaxValueW32Neon;
 const MinValueW16 WebRtcSpl_MinValueW16 = WebRtcSpl_MinValueW16Neon;
@@ -32,13 +30,6 @@ const ScaleAndAddVectorsWithRound WebRtcSpl_ScaleAndAddVectorsWithRound =
 
 #elif defined(MIPS32_LE)
 
-const MaxAbsValueW16 WebRtcSpl_MaxAbsValueW16 = WebRtcSpl_MaxAbsValueW16_mips;
-const MaxAbsValueW32 WebRtcSpl_MaxAbsValueW32 =
-#ifdef MIPS_DSP_R1_LE
-    WebRtcSpl_MaxAbsValueW32_mips;
-#else
-    WebRtcSpl_MaxAbsValueW32C;
-#endif
 const MaxValueW16 WebRtcSpl_MaxValueW16 = WebRtcSpl_MaxValueW16_mips;
 const MaxValueW32 WebRtcSpl_MaxValueW32 = WebRtcSpl_MaxValueW32_mips;
 const MinValueW16 WebRtcSpl_MinValueW16 = WebRtcSpl_MinValueW16_mips;
@@ -55,8 +46,6 @@ const ScaleAndAddVectorsWithRound WebRtcSpl_ScaleAndAddVectorsWithRound =
 
 #else
 
-const MaxAbsValueW16 WebRtcSpl_MaxAbsValueW16 = WebRtcSpl_MaxAbsValueW16C;
-const MaxAbsValueW32 WebRtcSpl_MaxAbsValueW32 = WebRtcSpl_MaxAbsValueW32C;
 const MaxValueW16 WebRtcSpl_MaxValueW16 = WebRtcSpl_MaxValueW16C;
 const MaxValueW32 WebRtcSpl_MaxValueW32 = WebRtcSpl_MaxValueW32C;
 const MinValueW16 WebRtcSpl_MinValueW16 = WebRtcSpl_MinValueW16C;

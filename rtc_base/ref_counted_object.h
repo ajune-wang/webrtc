4d6a078b15540c16e534745951254cf6b5dcf3b5
Danil Chapovalov <danilchap@webrtc.org>
2021-01-21 20:35:34 +0100

    Introduce FinalRefCountedObject template class
    To add ref counting to any class while avoiding
    virtual functions for reference counting.
    This template can both slightly reduce binary size
    and slightly improve performance.
    
    Bug: webrtc:11308
    Change-Id: I90ac735f6c220ee2a1a991a71039acdb0ca86453

--- rtc_base/ref_counted_object.h
+++ rtc_base/ref_counted_object.h
@@ -59,6 +59,33 @@ class RefCountedObject : public T {
   RTC_DISALLOW_COPY_AND_ASSIGN(RefCountedObject);
 };
 
+template <class T>
+class FinalRefCountedObject final : public T {
+ public:
+  using T::T;
+  FinalRefCountedObject() = default;
+  FinalRefCountedObject(const FinalRefCountedObject&) = delete;
+  FinalRefCountedObject& operator=(const FinalRefCountedObject&) = delete;
+
+  void AddRef() const { ref_count_.IncRef(); }
+  void Release() const {
+    if (ref_count_.DecRef() == RefCountReleaseStatus::kDroppedLastRef) {
+      delete this;
+    }
+  }
+  bool HasOneRef() const { return ref_count_.HasOneRef(); }
+
+ private:
+  ~FinalRefCountedObject() = default;
+
+  // Some compilers (gcc and msvc) require default contructors for members of
+  // `FinalRefCountedObject` to be able to use inherited constructors.
+  class ZeroBasedRefCounter : public webrtc::webrtc_impl::RefCounter {
+   public:
+    ZeroBasedRefCounter() : RefCounter(0) {}
+  } mutable ref_count_;
+};
+
 }  // namespace rtc
 
 #endif  // RTC_BASE_REF_COUNTED_OBJECT_H_

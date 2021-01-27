d68b054b5be14357ea80eab5020f1b1c91da8dc8
Mirko Bonadei <mbonadei@webrtc.org>
2021-01-27 23:43:52 +0100

    Move some RTC_LOG to RTC_DLOG.
    Some locations in the WebRTC codebase RTC_LOG the value of the
    __FUNCTION__ macro which probably is useful in debug mode. Moving
    these instances to RTC_DLOG saves ~10 KiB on arm64.
    
    Bug: webrtc:11986
    Change-Id: I5d81cc459d2850657a712b9aed80c187edf49a3a

--- media/sctp/sctp_transport.cc
+++ media/sctp/sctp_transport.cc
@@ -350,7 +350,7 @@ class SctpTransport::UsrSctpWrapper {
   }
 
   static void UninitializeUsrSctp() {
-    RTC_LOG(LS_INFO) << __FUNCTION__;
+    RTC_DLOG(LS_INFO) << __FUNCTION__;
     // usrsctp_finish() may fail if it's called too soon after the transports
     // are
     // closed. Wait and try again until it succeeds for up to 3 seconds.

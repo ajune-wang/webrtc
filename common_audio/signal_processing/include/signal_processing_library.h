36754a07259dbe0ee5112ded44f7b98f45192a9e
Ivo Creusen <ivoc@webrtc.org>
2021-02-05 17:26:57 +0100

    Optimize the AbsMax operations by implementing it in terms of min/max.
    The AbsMax operation is used several times in the audio processing.
    By using a min/max search to implement this function, it can be done
    more efficiently. In the NetEq perf test, this gives an overall
    improvement of ~25% on non-Neon platforms. The improvements in the
    Neon version are expected to be lower.
    
    Bug: None
    Change-Id: I7e91f944e1caf876db66bc462109254e0b23f229

--- common_audio/signal_processing/include/signal_processing_library.h
+++ common_audio/signal_processing/include/signal_processing_library.h
@@ -133,15 +133,7 @@ void WebRtcSpl_ZerosArrayW32(int32_t* vector, size_t vector_length);
 //      - length : Number of samples in vector.
 //
 // Return value  : Maximum absolute value in vector.
-typedef int16_t (*MaxAbsValueW16)(const int16_t* vector, size_t length);
-extern const MaxAbsValueW16 WebRtcSpl_MaxAbsValueW16;
-int16_t WebRtcSpl_MaxAbsValueW16C(const int16_t* vector, size_t length);
-#if defined(WEBRTC_HAS_NEON)
-int16_t WebRtcSpl_MaxAbsValueW16Neon(const int16_t* vector, size_t length);
-#endif
-#if defined(MIPS32_LE)
-int16_t WebRtcSpl_MaxAbsValueW16_mips(const int16_t* vector, size_t length);
-#endif
+int16_t WebRtcSpl_MaxAbsValueW16(const int16_t* vector, size_t length);
 
 // Returns the largest absolute value in a signed 32-bit vector.
 //
@@ -150,15 +142,7 @@ int16_t WebRtcSpl_MaxAbsValueW16_mips(const int16_t* vector, size_t length);
 //      - length : Number of samples in vector.
 //
 // Return value  : Maximum absolute value in vector.
-typedef int32_t (*MaxAbsValueW32)(const int32_t* vector, size_t length);
-extern const MaxAbsValueW32 WebRtcSpl_MaxAbsValueW32;
-int32_t WebRtcSpl_MaxAbsValueW32C(const int32_t* vector, size_t length);
-#if defined(WEBRTC_HAS_NEON)
-int32_t WebRtcSpl_MaxAbsValueW32Neon(const int32_t* vector, size_t length);
-#endif
-#if defined(MIPS_DSP_R1_LE)
-int32_t WebRtcSpl_MaxAbsValueW32_mips(const int32_t* vector, size_t length);
-#endif
+int32_t WebRtcSpl_MaxAbsValueW32(const int32_t* vector, size_t length);
 
 // Returns the maximum value of a 16-bit vector.
 //
@@ -231,44 +215,51 @@ int32_t WebRtcSpl_MinValueW32_mips(const int32_t* vector, size_t length);
 // Returns both the minimum and maximum values of a 16-bit vector.
 //
 // Input:
-//      - vector : 16-bit input vector.
-//      - length : Number of samples in vector.
+//      - data   : 16-bit input array.
+//      - length : Number of samples in data.
 // Ouput:
-//      - max_val : Maximum sample value in |vector|.
-//      - min_val : Minimum sample value in |vector|.
-void WebRtcSpl_MinMaxW16(const int16_t* vector,
+//      - max_val : Maximum sample value in |data|.
+//      - min_val : Minimum sample value in |data|.
+void WebRtcSpl_MinMaxW16(const int16_t* data,
                          size_t length,
                          int16_t* min_val,
                          int16_t* max_val);
 #if defined(WEBRTC_HAS_NEON)
-void WebRtcSpl_MinMaxW16Neon(const int16_t* vector,
+void WebRtcSpl_MinMaxW16Neon(const int16_t* data,
                              size_t length,
                              int16_t* min_val,
                              int16_t* max_val);
 #endif
 
-// Returns the vector index to the largest absolute value of a 16-bit vector.
+// Returns both the minimum and maximum values of a 32-bit vector.
 //
 // Input:
-//      - vector : 16-bit input vector.
-//      - length : Number of samples in vector.
-//
-// Return value  : Index to the maximum absolute value in vector.
-//                 If there are multiple equal maxima, return the index of the
-//                 first. -32768 will always have precedence over 32767 (despite
-//                 -32768 presenting an int16 absolute value of 32767).
-size_t WebRtcSpl_MaxAbsIndexW16(const int16_t* vector, size_t length);
+//      - data   : 32-bit input array.
+//      - length : Number of samples in data.
+// Ouput:
+//      - max_val : Maximum sample value in |data|.
+//      - min_val : Minimum sample value in |data|.
+void WebRtcSpl_MinMaxW32(const int32_t* data,
+                         size_t length,
+                         int32_t* min_val,
+                         int32_t* max_val);
+#if defined(WEBRTC_HAS_NEON)
+void WebRtcSpl_MinMaxW32Neon(const int32_t* data,
+                             size_t length,
+                             int32_t* min_val,
+                             int32_t* max_val);
+#endif
 
 // Returns the element with the largest absolute value of a 16-bit vector. Note
 // that this function can return a negative value.
 //
 // Input:
-//      - vector : 16-bit input vector.
-//      - length : Number of samples in vector.
+//      - data   : 16-bit input array.
+//      - length : Number of samples in data.
 //
 // Return value  : The element with the largest absolute value. Note that this
 //                 may be a negative value.
-int16_t WebRtcSpl_MaxAbsElementW16(const int16_t* vector, size_t length);
+int16_t WebRtcSpl_MaxAbsElementW16(const int16_t* data, size_t length);
 
 // Returns the vector index to the maximum sample value of a 16-bit vector.
 //
--- common_audio/signal_processing/min_max_operations_neon.c
+++ common_audio/signal_processing/min_max_operations_neon.c
@@ -14,113 +14,6 @@
 #include "rtc_base/checks.h"
 #include "common_audio/signal_processing/include/signal_processing_library.h"
 
-// Maximum absolute value of word16 vector. C version for generic platforms.
-int16_t WebRtcSpl_MaxAbsValueW16Neon(const int16_t* vector, size_t length) {
-  int absolute = 0, maximum = 0;
-
-  RTC_DCHECK_GT(length, 0);
-
-  const int16_t* p_start = vector;
-  size_t rest = length & 7;
-  const int16_t* p_end = vector + length - rest;
-
-  int16x8_t v;
-  uint16x8_t max_qv;
-  max_qv = vdupq_n_u16(0);
-
-  while (p_start < p_end) {
-    v = vld1q_s16(p_start);
-    // Note vabs doesn't change the value of -32768.
-    v = vabsq_s16(v);
-    // Use u16 so we don't lose the value -32768.
-    max_qv = vmaxq_u16(max_qv, vreinterpretq_u16_s16(v));
-    p_start += 8;
-  }
-
-#ifdef WEBRTC_ARCH_ARM64
-  maximum = (int)vmaxvq_u16(max_qv);
-#else
-  uint16x4_t max_dv;
-  max_dv = vmax_u16(vget_low_u16(max_qv), vget_high_u16(max_qv));
-  max_dv = vpmax_u16(max_dv, max_dv);
-  max_dv = vpmax_u16(max_dv, max_dv);
-
-  maximum = (int)vget_lane_u16(max_dv, 0);
-#endif
-
-  p_end = vector + length;
-  while (p_start < p_end) {
-    absolute = abs((int)(*p_start));
-
-    if (absolute > maximum) {
-      maximum = absolute;
-    }
-    p_start++;
-  }
-
-  // Guard the case for abs(-32768).
-  if (maximum > WEBRTC_SPL_WORD16_MAX) {
-    maximum = WEBRTC_SPL_WORD16_MAX;
-  }
-
-  return (int16_t)maximum;
-}
-
-// Maximum absolute value of word32 vector. NEON intrinsics version for
-// ARM 32-bit/64-bit platforms.
-int32_t WebRtcSpl_MaxAbsValueW32Neon(const int32_t* vector, size_t length) {
-  // Use uint32_t for the local variables, to accommodate the return value
-  // of abs(0x80000000), which is 0x80000000.
-
-  uint32_t absolute = 0, maximum = 0;
-  size_t i = 0;
-  size_t residual = length & 0x7;
-
-  RTC_DCHECK_GT(length, 0);
-
-  const int32_t* p_start = vector;
-  uint32x4_t max32x4_0 = vdupq_n_u32(0);
-  uint32x4_t max32x4_1 = vdupq_n_u32(0);
-
-  // First part, unroll the loop 8 times.
-  for (i = 0; i < length - residual; i += 8) {
-    int32x4_t in32x4_0 = vld1q_s32(p_start);
-    p_start += 4;
-    int32x4_t in32x4_1 = vld1q_s32(p_start);
-    p_start += 4;
-    in32x4_0 = vabsq_s32(in32x4_0);
-    in32x4_1 = vabsq_s32(in32x4_1);
-    // vabs doesn't change the value of 0x80000000.
-    // Use u32 so we don't lose the value 0x80000000.
-    max32x4_0 = vmaxq_u32(max32x4_0, vreinterpretq_u32_s32(in32x4_0));
-    max32x4_1 = vmaxq_u32(max32x4_1, vreinterpretq_u32_s32(in32x4_1));
-  }
-
-  uint32x4_t max32x4 = vmaxq_u32(max32x4_0, max32x4_1);
-#if defined(WEBRTC_ARCH_ARM64)
-  maximum = vmaxvq_u32(max32x4);
-#else
-  uint32x2_t max32x2 = vmax_u32(vget_low_u32(max32x4), vget_high_u32(max32x4));
-  max32x2 = vpmax_u32(max32x2, max32x2);
-
-  maximum = vget_lane_u32(max32x2, 0);
-#endif
-
-  // Second part, do the remaining iterations (if any).
-  for (i = residual; i > 0; i--) {
-    absolute = abs((int)(*p_start));
-    if (absolute > maximum) {
-      maximum = absolute;
-    }
-    p_start++;
-  }
-
-  // Guard against the case for 0x80000000.
-  maximum = WEBRTC_SPL_MIN(maximum, WEBRTC_SPL_WORD32_MAX);
-
-  return (int32_t)maximum;
-}
-
 // Maximum value of word16 vector. NEON intrinsics version for
 // ARM 32-bit/64-bit platforms.
 int16_t WebRtcSpl_MaxValueW16Neon(const int16_t* vector, size_t length) {
@@ -282,7 +175,7 @@ int32_t WebRtcSpl_MinValueW32Neon(const int32_t* vector, size_t length) {
 }
 
 // Finds both the minimum and maximum elements in an array of 16-bit integers.
-void WebRtcSpl_MinMaxW16Neon(const int16_t* vector, size_t length,
+void WebRtcSpl_MinMaxW16Neon(const int16_t* data, size_t length,
                              int16_t* min_val, int16_t* max_val) {
   int16_t minimum = WEBRTC_SPL_WORD16_MAX;
   int16_t maximum = WEBRTC_SPL_WORD16_MIN;
@@ -291,16 +184,14 @@ void WebRtcSpl_MinMaxW16Neon(const int16_t* vector, size_t length,
 
   RTC_DCHECK_GT(length, 0);
 
-  const int16_t* p_start = vector;
   int16x8_t min16x8 = vdupq_n_s16(WEBRTC_SPL_WORD16_MAX);
   int16x8_t max16x8 = vdupq_n_s16(WEBRTC_SPL_WORD16_MIN);
 
   // First part, unroll the loop 8 times.
   for (i = 0; i < length - residual; i += 8) {
-    int16x8_t in16x8 = vld1q_s16(p_start);
+    int16x8_t in16x8 = vld1q_s16(&data[i]);
     min16x8 = vminq_s16(min16x8, in16x8);
     max16x8 = vmaxq_s16(max16x8, in16x8);
-    p_start += 8;
   }
 
 #if defined(WEBRTC_ARCH_ARM64)
@@ -321,12 +212,60 @@ void WebRtcSpl_MinMaxW16Neon(const int16_t* vector, size_t length,
 #endif
 
   // Second part, do the remaining iterations (if any).
-  for (i = residual; i > 0; i--) {
-    if (*p_start < minimum)
-      minimum = *p_start;
-    if (*p_start > maximum)
-      maximum = *p_start;
-    p_start++;
+  for (; i < length; ++i) {
+    const int16_t cur_val = data[i];
+    if (cur_val < minimum)
+      minimum = cur_val;
+    if (cur_val > maximum)
+      maximum = cur_val;
+  }
+  *min_val = minimum;
+  *max_val = maximum;
+}
+
+// Finds both the minimum and maximum elements in an array of 32-bit integers.
+void WebRtcSpl_MinMaxW32Neon(const int32_t* data, size_t length,
+                             int32_t* min_val, int32_t* max_val) {
+  int32_t minimum = WEBRTC_SPL_WORD32_MAX;
+  int32_t maximum = WEBRTC_SPL_WORD32_MIN;
+  size_t i = 0;
+  size_t residual = length & 0x3;
+
+  RTC_DCHECK_GT(length, 0);
+
+  const int32_t* p_start = data;
+  int32x4_t min32x4 = vdupq_n_s32(WEBRTC_SPL_WORD32_MAX);
+  int32x4_t max32x4 = vdupq_n_s32(WEBRTC_SPL_WORD32_MIN);
+
+  // First part, unroll the loop 4 times.
+  for (i = 0; i < length - residual; i += 4) {
+    int32x4_t in = vld1q_s32(&data[i]);
+    min32x4 = vminq_s32(min32x4, in);
+    max32x4 = vmaxq_s32(max32x4, in);
+  }
+
+#if defined(WEBRTC_ARCH_ARM64)
+  minimum = vminvq_s32(min32x4);
+  maximum = vmaxvq_s32(max32x4);
+#else
+  int32x2_t min32x2 = vmin_s32(vget_low_s32(min32x4), vget_high_s32(min32x4));
+  min32x2 = vpmin_s32(min32x2, min32x2);
+
+  minimum = vget_lane_s32(min32x2, 0);
+
+  int32x2_t max32x2 = vmax_s32(vget_low_s32(max32x4), vget_high_s32(max32x4));
+  max32x2 = vpmax_s32(max32x2, max32x2);
+
+  maximum = vget_lane_s32(max32x2, 0);
+#endif
+
+  // Second part, do the remaining iterations (if any).
+  for (; i < length; ++i) {
+    const int32_t cur_val = data[i];
+    if (cur_val < minimum)
+      minimum = cur_val;
+    if (cur_val > maximum)
+      maximum = cur_val;
   }
   *min_val = minimum;
   *max_val = maximum;

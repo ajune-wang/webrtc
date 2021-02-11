2f9b2be48f003d06fb41c16ac568c3ff13b3be9b
Danil Chapovalov <danilchap@webrtc.org>
2021-02-11 19:45:10 +0100

    Consolidate full svc structure in the one souce file
    Keeping structures in the same file makes it clearer which are missing
    and makes it easier to see if structures are consistent with one another.
    
    Bug: None
    Change-Id: I4e5e6971054dd28dd326c68369ee57b6df62725e

--- modules/video_coding/svc/scalability_structure_full_svc.cc
+++ modules/video_coding/svc/scalability_structure_full_svc.cc
@@ -21,21 +21,24 @@
 namespace webrtc {
 namespace {
 enum : int { kKey, kDelta };
+
+constexpr absl::string_view kFramePatternNames[] = {
+    "None", "DeltaT2A", "DeltaT1", "DeltaT2B", "DeltaT0"};
+
 }  // namespace
 
 constexpr int ScalabilityStructureFullSvc::kMaxNumSpatialLayers;
 constexpr int ScalabilityStructureFullSvc::kMaxNumTemporalLayers;
-constexpr absl::string_view ScalabilityStructureFullSvc::kFramePatternNames[];
 
 ScalabilityStructureFullSvc::ScalabilityStructureFullSvc(
-    int num_spatial_layers,
-    int num_temporal_layers)
-    : num_spatial_layers_(num_spatial_layers),
-      num_temporal_layers_(num_temporal_layers),
+    NumLayers num_layers,
+    ScalingFactor resolution_factor)
+    : num_layers_(num_layers),
+      resolution_factor_(resolution_factor),
       active_decode_targets_(
-          (uint32_t{1} << (num_spatial_layers * num_temporal_layers)) - 1) {
-  RTC_DCHECK_LE(num_spatial_layers, kMaxNumSpatialLayers);
-  RTC_DCHECK_LE(num_temporal_layers, kMaxNumTemporalLayers);
+          (uint32_t{1} << (num_layers_.spatial * num_layers_.temporal)) - 1) {
+  RTC_DCHECK_LE(num_layers_.spatial, kMaxNumSpatialLayers);
+  RTC_DCHECK_LE(num_layers_.temporal, kMaxNumTemporalLayers);
 }
 
 ScalabilityStructureFullSvc::~ScalabilityStructureFullSvc() = default;
@@ -43,22 +46,24 @@ ScalabilityStructureFullSvc::~ScalabilityStructureFullSvc() = default;
 ScalabilityStructureFullSvc::StreamLayersConfig
 ScalabilityStructureFullSvc::StreamConfig() const {
   StreamLayersConfig result;
-  result.num_spatial_layers = num_spatial_layers_;
-  result.num_temporal_layers = num_temporal_layers_;
-  result.scaling_factor_num[num_spatial_layers_ - 1] = 1;
-  result.scaling_factor_den[num_spatial_layers_ - 1] = 1;
-  for (int sid = num_spatial_layers_ - 1; sid > 0; --sid) {
-    result.scaling_factor_num[sid - 1] = 1;
-    result.scaling_factor_den[sid - 1] = 2 * result.scaling_factor_den[sid];
+  result.num_spatial_layers = num_layers_.spatial;
+  result.num_temporal_layers = num_layers_.temporal;
+  result.scaling_factor_num[num_layers_.spatial - 1] = 1;
+  result.scaling_factor_den[num_layers_.spatial - 1] = 1;
+  for (int sid = num_layers_.spatial - 1; sid > 0; --sid) {
+    result.scaling_factor_num[sid - 1] =
+        resolution_factor_.num * result.scaling_factor_num[sid];
+    result.scaling_factor_den[sid - 1] =
+        resolution_factor_.den * result.scaling_factor_den[sid];
   }
   return result;
 }
 
 bool ScalabilityStructureFullSvc::TemporalLayerIsActive(int tid) const {
-  if (tid >= num_temporal_layers_) {
+  if (tid >= num_layers_.temporal) {
     return false;
   }
-  for (int sid = 0; sid < num_spatial_layers_; ++sid) {
+  for (int sid = 0; sid < num_layers_.spatial; ++sid) {
     if (DecodeTargetIsActive(sid, tid)) {
       return true;
     }
@@ -128,7 +133,7 @@ ScalabilityStructureFullSvc::NextFrameConfig(bool restart) {
     last_pattern_ = kNone;
     return configs;
   }
-  configs.reserve(num_spatial_layers_);
+  configs.reserve(num_layers_.spatial);
 
   if (last_pattern_ == kNone || restart) {
     can_reference_t0_frame_for_spatial_id_.reset();
@@ -141,7 +146,7 @@ ScalabilityStructureFullSvc::NextFrameConfig(bool restart) {
     case kDeltaT0:
       // Disallow temporal references cross T0 on higher temporal layers.
       can_reference_t1_frame_for_spatial_id_.reset();
-      for (int sid = 0; sid < num_spatial_layers_; ++sid) {
+      for (int sid = 0; sid < num_layers_.spatial; ++sid) {
         if (!DecodeTargetIsActive(sid, /*tid=*/0)) {
           // Next frame from the spatial layer `sid` shouldn't depend on
           // potentially old previous frame from the spatial layer `sid`.
@@ -171,7 +176,7 @@ ScalabilityStructureFullSvc::NextFrameConfig(bool restart) {
       }
       break;
     case kDeltaT1:
-      for (int sid = 0; sid < num_spatial_layers_; ++sid) {
+      for (int sid = 0; sid < num_layers_.spatial; ++sid) {
         if (!DecodeTargetIsActive(sid, /*tid=*/1) ||
             !can_reference_t0_frame_for_spatial_id_[sid]) {
           continue;
@@ -186,7 +191,7 @@ ScalabilityStructureFullSvc::NextFrameConfig(bool restart) {
           config.Reference(*spatial_dependency_buffer_id);
         }
         // No frame reference top layer frame, so no need save it into a buffer.
-        if (num_temporal_layers_ > 2 || sid < num_spatial_layers_ - 1) {
+        if (num_layers_.temporal > 2 || sid < num_layers_.spatial - 1) {
           config.Update(BufferIndex(sid, /*tid=*/1));
         }
         spatial_dependency_buffer_id = BufferIndex(sid, /*tid=*/1);
@@ -194,7 +199,7 @@ ScalabilityStructureFullSvc::NextFrameConfig(bool restart) {
       break;
     case kDeltaT2A:
     case kDeltaT2B:
-      for (int sid = 0; sid < num_spatial_layers_; ++sid) {
+      for (int sid = 0; sid < num_layers_.spatial; ++sid) {
         if (!DecodeTargetIsActive(sid, /*tid=*/2) ||
             !can_reference_t0_frame_for_spatial_id_[sid]) {
           continue;
@@ -214,7 +219,7 @@ ScalabilityStructureFullSvc::NextFrameConfig(bool restart) {
           config.Reference(*spatial_dependency_buffer_id);
         }
         // No frame reference top layer frame, so no need save it into a buffer.
-        if (sid < num_spatial_layers_ - 1) {
+        if (sid < num_layers_.spatial - 1) {
           config.Update(BufferIndex(sid, /*tid=*/2));
         }
         spatial_dependency_buffer_id = BufferIndex(sid, /*tid=*/2);
@@ -227,11 +232,11 @@ ScalabilityStructureFullSvc::NextFrameConfig(bool restart) {
 
   if (configs.empty() && !restart) {
     RTC_LOG(LS_WARNING) << "Failed to generate configuration for L"
-                        << num_spatial_layers_ << "T" << num_temporal_layers_
+                        << num_layers_.spatial << "T" << num_layers_.temporal
                         << " with active decode targets "
                         << active_decode_targets_.to_string('-').substr(
                                active_decode_targets_.size() -
-                               num_spatial_layers_ * num_temporal_layers_)
+                               num_layers_.spatial * num_layers_.temporal)
                         << " and transition from "
                         << kFramePatternNames[last_pattern_] << " to "
                         << kFramePatternNames[current_pattern]
@@ -253,20 +258,20 @@ GenericFrameInfo ScalabilityStructureFullSvc::OnEncodeDone(
   frame_info.spatial_id = config.SpatialId();
   frame_info.temporal_id = config.TemporalId();
   frame_info.encoder_buffers = config.Buffers();
-  frame_info.decode_target_indications.reserve(num_spatial_layers_ *
-                                               num_temporal_layers_);
-  for (int sid = 0; sid < num_spatial_layers_; ++sid) {
-    for (int tid = 0; tid < num_temporal_layers_; ++tid) {
+  frame_info.decode_target_indications.reserve(num_layers_.spatial *
+                                               num_layers_.temporal);
+  for (int sid = 0; sid < num_layers_.spatial; ++sid) {
+    for (int tid = 0; tid < num_layers_.temporal; ++tid) {
       frame_info.decode_target_indications.push_back(Dti(sid, tid, config));
     }
   }
   if (config.TemporalId() == 0) {
-    frame_info.part_of_chain.resize(num_spatial_layers_);
-    for (int sid = 0; sid < num_spatial_layers_; ++sid) {
+    frame_info.part_of_chain.resize(num_layers_.spatial);
+    for (int sid = 0; sid < num_layers_.spatial; ++sid) {
       frame_info.part_of_chain[sid] = config.SpatialId() <= sid;
     }
   } else {
-    frame_info.part_of_chain.assign(num_spatial_layers_, false);
+    frame_info.part_of_chain.assign(num_layers_.spatial, false);
   }
   frame_info.active_decode_targets = active_decode_targets_;
   return frame_info;
@@ -274,15 +279,115 @@ GenericFrameInfo ScalabilityStructureFullSvc::OnEncodeDone(
 
 void ScalabilityStructureFullSvc::OnRatesUpdated(
     const VideoBitrateAllocation& bitrates) {
-  for (int sid = 0; sid < num_spatial_layers_; ++sid) {
+  for (int sid = 0; sid < num_layers_.spatial; ++sid) {
     // Enable/disable spatial layers independetely.
     bool active = true;
-    for (int tid = 0; tid < num_temporal_layers_; ++tid) {
+    for (int tid = 0; tid < num_layers_.temporal; ++tid) {
       // To enable temporal layer, require bitrates for lower temporal layers.
       active = active && bitrates.GetBitrate(sid, tid) > 0;
       SetDecodeTargetIsActive(sid, tid, active);
     }
   }
+}
+
+FrameDependencyStructure ScalabilityStructureL1T2::DependencyStructure() const {
+  FrameDependencyStructure structure;
+  structure.num_decode_targets = 2;
+  structure.num_chains = 1;
+  structure.decode_target_protected_by_chain = {0, 0};
+  structure.templates.resize(3);
+  structure.templates[0].T(0).Dtis("SS").ChainDiffs({0});
+  structure.templates[1].T(0).Dtis("SS").ChainDiffs({2}).FrameDiffs({2});
+  structure.templates[2].T(1).Dtis("-D").ChainDiffs({1}).FrameDiffs({1});
+  return structure;
+}
+
+FrameDependencyStructure ScalabilityStructureL1T3::DependencyStructure() const {
+  FrameDependencyStructure structure;
+  structure.num_decode_targets = 3;
+  structure.num_chains = 1;
+  structure.decode_target_protected_by_chain = {0, 0, 0};
+  structure.templates.resize(5);
+  structure.templates[0].T(0).Dtis("SSS").ChainDiffs({0});
+  structure.templates[1].T(0).Dtis("SSS").ChainDiffs({4}).FrameDiffs({4});
+  structure.templates[2].T(1).Dtis("-DS").ChainDiffs({2}).FrameDiffs({2});
+  structure.templates[3].T(2).Dtis("--D").ChainDiffs({1}).FrameDiffs({1});
+  structure.templates[4].T(2).Dtis("--D").ChainDiffs({3}).FrameDiffs({1});
+  return structure;
+}
+
+FrameDependencyStructure ScalabilityStructureL2T1::DependencyStructure() const {
+  FrameDependencyStructure structure;
+  structure.num_decode_targets = 2;
+  structure.num_chains = 2;
+  structure.decode_target_protected_by_chain = {0, 1};
+  structure.templates.resize(4);
+  structure.templates[0].S(0).Dtis("SR").ChainDiffs({2, 1}).FrameDiffs({2});
+  structure.templates[1].S(0).Dtis("SS").ChainDiffs({0, 0});
+  structure.templates[2].S(1).Dtis("-S").ChainDiffs({1, 1}).FrameDiffs({2, 1});
+  structure.templates[3].S(1).Dtis("-S").ChainDiffs({1, 1}).FrameDiffs({1});
+  return structure;
+}
+
+FrameDependencyStructure ScalabilityStructureL2T2::DependencyStructure() const {
+  FrameDependencyStructure structure;
+  structure.num_decode_targets = 4;
+  structure.num_chains = 2;
+  structure.decode_target_protected_by_chain = {0, 0, 1, 1};
+  structure.templates.resize(6);
+  auto& templates = structure.templates;
+  templates[0].S(0).T(0).Dtis("SSSS").ChainDiffs({0, 0});
+  templates[1].S(0).T(0).Dtis("SSRR").ChainDiffs({4, 3}).FrameDiffs({4});
+  templates[2].S(0).T(1).Dtis("-D-R").ChainDiffs({2, 1}).FrameDiffs({2});
+  templates[3].S(1).T(0).Dtis("--SS").ChainDiffs({1, 1}).FrameDiffs({1});
+  templates[4].S(1).T(0).Dtis("--SS").ChainDiffs({1, 1}).FrameDiffs({4, 1});
+  templates[5].S(1).T(1).Dtis("---D").ChainDiffs({3, 2}).FrameDiffs({2, 1});
+  return structure;
+}
+
+FrameDependencyStructure ScalabilityStructureL3T1::DependencyStructure() const {
+  FrameDependencyStructure structure;
+  structure.num_decode_targets = 3;
+  structure.num_chains = 3;
+  structure.decode_target_protected_by_chain = {0, 1, 2};
+  auto& templates = structure.templates;
+  templates.resize(6);
+  templates[0].S(0).Dtis("SRR").ChainDiffs({3, 2, 1}).FrameDiffs({3});
+  templates[1].S(0).Dtis("SSS").ChainDiffs({0, 0, 0});
+  templates[2].S(1).Dtis("-SR").ChainDiffs({1, 1, 1}).FrameDiffs({3, 1});
+  templates[3].S(1).Dtis("-SS").ChainDiffs({1, 1, 1}).FrameDiffs({1});
+  templates[4].S(2).Dtis("--S").ChainDiffs({2, 1, 1}).FrameDiffs({3, 1});
+  templates[5].S(2).Dtis("--S").ChainDiffs({2, 1, 1}).FrameDiffs({1});
+  return structure;
+}
+
+FrameDependencyStructure ScalabilityStructureL3T3::DependencyStructure() const {
+  FrameDependencyStructure structure;
+  structure.num_decode_targets = 9;
+  structure.num_chains = 3;
+  structure.decode_target_protected_by_chain = {0, 0, 0, 1, 1, 1, 2, 2, 2};
+  auto& t = structure.templates;
+  t.resize(15);
+  // Templates are shown in the order frames following them appear in the
+  // stream, but in `structure.templates` array templates are sorted by
+  // (`spatial_id`, `temporal_id`) since that is a dependency descriptor
+  // requirement. Indexes are written in hex for nicer alignment.
+  t[0x1].S(0).T(0).Dtis("SSSSSSSSS").ChainDiffs({0, 0, 0});
+  t[0x6].S(1).T(0).Dtis("---SSSSSS").ChainDiffs({1, 1, 1}).FrameDiffs({1});
+  t[0xB].S(2).T(0).Dtis("------SSS").ChainDiffs({2, 1, 1}).FrameDiffs({1});
+  t[0x3].S(0).T(2).Dtis("--D--R--R").ChainDiffs({3, 2, 1}).FrameDiffs({3});
+  t[0x8].S(1).T(2).Dtis("-----D--R").ChainDiffs({4, 3, 2}).FrameDiffs({3, 1});
+  t[0xD].S(2).T(2).Dtis("--------D").ChainDiffs({5, 4, 3}).FrameDiffs({3, 1});
+  t[0x2].S(0).T(1).Dtis("-DS-RR-RR").ChainDiffs({6, 5, 4}).FrameDiffs({6});
+  t[0x7].S(1).T(1).Dtis("----DS-RR").ChainDiffs({7, 6, 5}).FrameDiffs({6, 1});
+  t[0xC].S(2).T(1).Dtis("-------DS").ChainDiffs({8, 7, 6}).FrameDiffs({6, 1});
+  t[0x4].S(0).T(2).Dtis("--D--R--R").ChainDiffs({9, 8, 7}).FrameDiffs({3});
+  t[0x9].S(1).T(2).Dtis("-----D--R").ChainDiffs({10, 9, 8}).FrameDiffs({3, 1});
+  t[0xE].S(2).T(2).Dtis("--------D").ChainDiffs({11, 10, 9}).FrameDiffs({3, 1});
+  t[0x0].S(0).T(0).Dtis("SSSRRRRRR").ChainDiffs({12, 11, 10}).FrameDiffs({12});
+  t[0x5].S(1).T(0).Dtis("---SSSRRR").ChainDiffs({1, 1, 1}).FrameDiffs({12, 1});
+  t[0xA].S(2).T(0).Dtis("------SSS").ChainDiffs({2, 1, 1}).FrameDiffs({12, 1});
+  return structure;
 }
 
 }  // namespace webrtc

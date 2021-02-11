d39dc2b5777bfdef26747648ad6cb9b2066c7aa8
Harald Alvestrand <hta@webrtc.org>
2021-02-11 08:55:35 +0000

    Remove blanket disabling of TSAN for peer_connection_integrationtests
    Bug: webrtc:3608
    Change-Id: Iac45e52efcdfebc1bb85697a7606873255643e00

--- pc/peer_connection_integrationtest.cc
+++ pc/peer_connection_integrationtest.cc
@@ -8,10 +8,6 @@
  *  be found in the AUTHORS file in the root of the source tree.
  */
 
-// Disable for TSan v2, see
-// https://code.google.com/p/webrtc/issues/detail?id=1205 for details.
-#if !defined(THREAD_SANITIZER)
-
 #include <stdio.h>
 
 #include <algorithm>
@@ -6097,5 +6093,3 @@ TEST_F(PeerConnectionIntegrationTestUnifiedPlan,
 
 }  // namespace
 }  // namespace webrtc
-
-#endif  // if !defined(THREAD_SANITIZER)

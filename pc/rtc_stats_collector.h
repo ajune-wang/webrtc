5c74d5412376f29220f6dba227cd5a2335ce96c7
Tomas Gunnarsson <tommi@webrtc.org>
2021-03-28 18:06:48 +0200

    Remove Init_w, set transport in ctor
    Bug: webrtc:12601
    Change-Id: I85f34f3341a06da9a9efd936b1d36722b10ec487

--- pc/rtc_stats_collector.h
+++ pc/rtc_stats_collector.h
@@ -229,7 +229,6 @@ class RTCStatsCollector : public virtual rtc::RefCountInterface,
           transport_stats_by_name) const;
   // The results are stored in |transceiver_stats_infos_| and |call_stats_|.
   void PrepareTransceiverStatsInfosAndCallStats_s_w();
-  std::set<std::string> PrepareTransportNames_s() const;
 
   // Stats gathering on a particular thread.
   void ProducePartialResultsOnSignalingThread(int64_t timestamp_us);
@@ -273,7 +272,6 @@ class RTCStatsCollector : public virtual rtc::RefCountInterface,
   // passed as arguments to avoid copies. This is thread safe - when we
   // set/reset we know there are no pending stats requests in progress.
   std::vector<RtpTransceiverStatsInfo> transceiver_stats_infos_;
-  std::set<std::string> transport_names_;
 
   Call::Stats call_stats_;
 

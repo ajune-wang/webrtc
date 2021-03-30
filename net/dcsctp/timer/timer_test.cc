7736665c82ba53e8facbc9385f2a3e82aeb31b94
Victor Boivie <boivie@webrtc.org>
2021-03-30 22:54:41 +0200

    dcsctp: Add Timer and TimerManager
    Timer is a high-level timer (in contrast to the low-level `Timeout`
    class). Timers are started and can be stopped or restarted. When a timer
    expires, the provided callback will be triggered.
    
    Timers can be configured to do e.g. exponential backoff when they expire
    and how many times they should be automatically restarted.
    
    Bug: webrtc:12614
    Change-Id: Id5eddd58dd0af62184b10dd1f98e3e886e3f1d50

---
+++ net/dcsctp/timer/timer_test.cc
@@ -0,0 +1,347 @@
+#include "net/dcsctp/timer/timer.h"
+
+#include <memory>
+
+#include "absl/types/optional.h"
+#include "net/dcsctp/public/timeout.h"
+#include "net/dcsctp/timer/fake_timeout.h"
+#include "rtc_base/gunit.h"
+#include "test/gmock.h"
+
+namespace dcsctp {
+namespace {
+using ::testing::Return;
+
+class TimerTest : public testing::Test {
+ protected:
+  TimerTest()
+      : timeout_manager_([this]() { return now_; }),
+        manager_([this]() { return timeout_manager_.CreateTimeout(); }) {
+    ON_CALL(on_expired_, Call).WillByDefault(Return(absl::nullopt));
+  }
+
+  void AdvanceTimeAndRunTimers(int duration_ms) {
+    now_ += duration_ms;
+
+    for (uint64_t timeout_id : timeout_manager_.RunTimers()) {
+      manager_.HandleTimeout(timeout_id);
+    }
+  }
+
+  int64_t now_ = 0;
+  FakeTimeoutManager timeout_manager_;
+  TimerManager manager_;
+  testing::MockFunction<absl::optional<int>()> on_expired_;
+};
+
+TEST_F(TimerTest, TimerIsInitiallyStopped) {
+  std::unique_ptr<Timer> t1 = manager_.CreateTimer(
+      "t1", on_expired_.AsStdFunction(),
+      {.duration_ms = 5000,
+       .backoff_algorithm = TimerBackoffAlgorithm::kFixed});
+
+  EXPECT_FALSE(t1->is_running());
+}
+
+TEST_F(TimerTest, TimerExpiresAtGivenTime) {
+  std::unique_ptr<Timer> t1 = manager_.CreateTimer(
+      "t1", on_expired_.AsStdFunction(),
+      {.duration_ms = 5000,
+       .backoff_algorithm = TimerBackoffAlgorithm::kFixed});
+
+  EXPECT_CALL(on_expired_, Call).Times(0);
+  t1->Start();
+  EXPECT_TRUE(t1->is_running());
+
+  AdvanceTimeAndRunTimers(4000);
+
+  EXPECT_CALL(on_expired_, Call).Times(1);
+  AdvanceTimeAndRunTimers(1000);
+}
+
+TEST_F(TimerTest, TimerReschedulesAfterExpiredWithFixedBackoff) {
+  std::unique_ptr<Timer> t1 = manager_.CreateTimer(
+      "t1", on_expired_.AsStdFunction(),
+      {.duration_ms = 5000,
+       .backoff_algorithm = TimerBackoffAlgorithm::kFixed});
+
+  EXPECT_CALL(on_expired_, Call).Times(0);
+  t1->Start();
+  EXPECT_EQ(t1->expiration_count(), 0);
+
+  AdvanceTimeAndRunTimers(4000);
+
+  // Fire first time
+  EXPECT_CALL(on_expired_, Call).Times(1);
+  AdvanceTimeAndRunTimers(1000);
+  EXPECT_TRUE(t1->is_running());
+  EXPECT_EQ(t1->expiration_count(), 1);
+
+  EXPECT_CALL(on_expired_, Call).Times(0);
+  AdvanceTimeAndRunTimers(4000);
+
+  // Second time
+  EXPECT_CALL(on_expired_, Call).Times(1);
+  AdvanceTimeAndRunTimers(1000);
+  EXPECT_TRUE(t1->is_running());
+  EXPECT_EQ(t1->expiration_count(), 2);
+
+  EXPECT_CALL(on_expired_, Call).Times(0);
+  AdvanceTimeAndRunTimers(4000);
+
+  // Third time
+  EXPECT_CALL(on_expired_, Call).Times(1);
+  AdvanceTimeAndRunTimers(1000);
+  EXPECT_TRUE(t1->is_running());
+  EXPECT_EQ(t1->expiration_count(), 3);
+}
+
+TEST_F(TimerTest, TimerWithNoRestarts) {
+  std::unique_ptr<Timer> t1 = manager_.CreateTimer(
+      "t1", on_expired_.AsStdFunction(),
+      {.duration_ms = 5000,
+       .max_restarts = 0,
+       .backoff_algorithm = TimerBackoffAlgorithm::kFixed});
+
+  EXPECT_CALL(on_expired_, Call).Times(0);
+  t1->Start();
+  AdvanceTimeAndRunTimers(4000);
+
+  // Fire first time
+  EXPECT_CALL(on_expired_, Call).Times(1);
+  AdvanceTimeAndRunTimers(1000);
+
+  EXPECT_FALSE(t1->is_running());
+
+  // Second time - shouldn't fire
+  EXPECT_CALL(on_expired_, Call).Times(0);
+  AdvanceTimeAndRunTimers(5000);
+  EXPECT_FALSE(t1->is_running());
+}
+
+TEST_F(TimerTest, TimerWithOneRestart) {
+  std::unique_ptr<Timer> t1 = manager_.CreateTimer(
+      "t1", on_expired_.AsStdFunction(),
+      {.duration_ms = 5000,
+       .max_restarts = 1,
+       .backoff_algorithm = TimerBackoffAlgorithm::kFixed});
+
+  EXPECT_CALL(on_expired_, Call).Times(0);
+  t1->Start();
+  AdvanceTimeAndRunTimers(4000);
+
+  // Fire first time
+  EXPECT_CALL(on_expired_, Call).Times(1);
+  AdvanceTimeAndRunTimers(1000);
+  EXPECT_TRUE(t1->is_running());
+
+  EXPECT_CALL(on_expired_, Call).Times(0);
+  AdvanceTimeAndRunTimers(4000);
+
+  // Second time - max restart limit reached.
+  EXPECT_CALL(on_expired_, Call).Times(1);
+  AdvanceTimeAndRunTimers(1000);
+  EXPECT_FALSE(t1->is_running());
+
+  // Third time - should not fire.
+  EXPECT_CALL(on_expired_, Call).Times(0);
+  AdvanceTimeAndRunTimers(5000);
+  EXPECT_FALSE(t1->is_running());
+}
+
+TEST_F(TimerTest, TimerWithTwoRestart) {
+  std::unique_ptr<Timer> t1 = manager_.CreateTimer(
+      "t1", on_expired_.AsStdFunction(),
+      {.duration_ms = 5000,
+       .max_restarts = 2,
+       .backoff_algorithm = TimerBackoffAlgorithm::kFixed});
+
+  EXPECT_CALL(on_expired_, Call).Times(0);
+  t1->Start();
+  AdvanceTimeAndRunTimers(4000);
+
+  // Fire first time
+  EXPECT_CALL(on_expired_, Call).Times(1);
+  AdvanceTimeAndRunTimers(1000);
+  EXPECT_TRUE(t1->is_running());
+
+  EXPECT_CALL(on_expired_, Call).Times(0);
+  AdvanceTimeAndRunTimers(4000);
+
+  // Second time
+  EXPECT_CALL(on_expired_, Call).Times(1);
+  AdvanceTimeAndRunTimers(1000);
+  EXPECT_TRUE(t1->is_running());
+
+  EXPECT_CALL(on_expired_, Call).Times(0);
+  AdvanceTimeAndRunTimers(4000);
+
+  // Third time
+  EXPECT_CALL(on_expired_, Call).Times(1);
+  AdvanceTimeAndRunTimers(1000);
+  EXPECT_FALSE(t1->is_running());
+}
+
+TEST_F(TimerTest, TimerWithExponentialBackoff) {
+  std::unique_ptr<Timer> t1 = manager_.CreateTimer(
+      "t1", on_expired_.AsStdFunction(),
+      {.duration_ms = 5000,
+       .backoff_algorithm = TimerBackoffAlgorithm::kExponential});
+
+  t1->Start();
+
+  // Fire first time at 5 seconds
+  EXPECT_CALL(on_expired_, Call).Times(1);
+  AdvanceTimeAndRunTimers(5000);
+
+  // Second time at 5*2^1 = 10 seconds later.
+  EXPECT_CALL(on_expired_, Call).Times(0);
+  AdvanceTimeAndRunTimers(9000);
+  EXPECT_CALL(on_expired_, Call).Times(1);
+  AdvanceTimeAndRunTimers(1000);
+
+  // Third time at 5*2^2 = 20 seconds later.
+  EXPECT_CALL(on_expired_, Call).Times(0);
+  AdvanceTimeAndRunTimers(19000);
+  EXPECT_CALL(on_expired_, Call).Times(1);
+  AdvanceTimeAndRunTimers(1000);
+
+  // Fourth time at 5*2^3 = 40 seconds later.
+  EXPECT_CALL(on_expired_, Call).Times(0);
+  AdvanceTimeAndRunTimers(39000);
+  EXPECT_CALL(on_expired_, Call).Times(1);
+  AdvanceTimeAndRunTimers(1000);
+}
+
+TEST_F(TimerTest, StartTimerIsNoopIfAlreadyStarted) {
+  std::unique_ptr<Timer> t1 = manager_.CreateTimer(
+      "t1", on_expired_.AsStdFunction(),
+      {.duration_ms = 5000,
+       .backoff_algorithm = TimerBackoffAlgorithm::kExponential});
+
+  t1->Start();
+
+  AdvanceTimeAndRunTimers(3000);
+
+  // This will not restart the timer - it will still expire 2 seconds from now.
+  t1->Start();
+
+  EXPECT_CALL(on_expired_, Call).Times(1);
+  AdvanceTimeAndRunTimers(2000);
+}
+
+TEST_F(TimerTest, RestartTimerWillStopAndStart) {
+  std::unique_ptr<Timer> t1 = manager_.CreateTimer(
+      "t1", on_expired_.AsStdFunction(),
+      {.duration_ms = 5000,
+       .backoff_algorithm = TimerBackoffAlgorithm::kExponential});
+
+  t1->Start();
+
+  AdvanceTimeAndRunTimers(3000);
+
+  t1->Restart();
+
+  EXPECT_CALL(on_expired_, Call).Times(0);
+  AdvanceTimeAndRunTimers(2000);
+
+  EXPECT_CALL(on_expired_, Call).Times(1);
+  AdvanceTimeAndRunTimers(3000);
+}
+
+TEST_F(TimerTest, ExpirationCounterWillResetIfStopped) {
+  std::unique_ptr<Timer> t1 = manager_.CreateTimer(
+      "t1", on_expired_.AsStdFunction(),
+      {.duration_ms = 5000,
+       .backoff_algorithm = TimerBackoffAlgorithm::kExponential});
+
+  t1->Start();
+
+  // Fire first time at 5 seconds
+  EXPECT_CALL(on_expired_, Call).Times(1);
+  AdvanceTimeAndRunTimers(5000);
+  EXPECT_EQ(t1->expiration_count(), 1);
+
+  // Second time at 5*2^1 = 10 seconds later.
+  EXPECT_CALL(on_expired_, Call).Times(0);
+  AdvanceTimeAndRunTimers(9000);
+  EXPECT_CALL(on_expired_, Call).Times(1);
+  AdvanceTimeAndRunTimers(1000);
+  EXPECT_EQ(t1->expiration_count(), 2);
+
+  t1->Restart();
+  EXPECT_EQ(t1->expiration_count(), 0);
+
+  // Third time at 5*2^0 = 5 seconds later.
+  EXPECT_CALL(on_expired_, Call).Times(0);
+  AdvanceTimeAndRunTimers(4000);
+  EXPECT_CALL(on_expired_, Call).Times(1);
+  AdvanceTimeAndRunTimers(1000);
+  EXPECT_EQ(t1->expiration_count(), 1);
+}
+
+TEST_F(TimerTest, RestartTimerCanAlsoStartTime) {
+  std::unique_ptr<Timer> t1 = manager_.CreateTimer(
+      "t1", on_expired_.AsStdFunction(),
+      {.duration_ms = 5000,
+       .backoff_algorithm = TimerBackoffAlgorithm::kExponential});
+
+  t1->Restart();
+
+  EXPECT_CALL(on_expired_, Call).Times(0);
+  AdvanceTimeAndRunTimers(4000);
+  EXPECT_CALL(on_expired_, Call).Times(1);
+  AdvanceTimeAndRunTimers(1000);
+}
+
+TEST_F(TimerTest, StopTimerWillMakeItNotExpire) {
+  std::unique_ptr<Timer> t1 = manager_.CreateTimer(
+      "t1", on_expired_.AsStdFunction(),
+      {.duration_ms = 5000,
+       .backoff_algorithm = TimerBackoffAlgorithm::kExponential});
+
+  t1->Start();
+  EXPECT_TRUE(t1->is_running());
+
+  EXPECT_CALL(on_expired_, Call).Times(0);
+  AdvanceTimeAndRunTimers(4000);
+  t1->Stop();
+  EXPECT_FALSE(t1->is_running());
+
+  EXPECT_CALL(on_expired_, Call).Times(0);
+  AdvanceTimeAndRunTimers(1000);
+}
+
+TEST_F(TimerTest, ReturningNewDurationWhenExpired) {
+  std::unique_ptr<Timer> t1 = manager_.CreateTimer(
+      "t1", on_expired_.AsStdFunction(),
+      {.duration_ms = 5000,
+       .backoff_algorithm = TimerBackoffAlgorithm::kFixed});
+
+  EXPECT_CALL(on_expired_, Call).Times(0);
+  t1->Start();
+  EXPECT_EQ(t1->duration_ms(), 5000);
+
+  AdvanceTimeAndRunTimers(4000);
+
+  // Fire first time
+  EXPECT_CALL(on_expired_, Call).WillOnce(Return(2000));
+  AdvanceTimeAndRunTimers(1000);
+  EXPECT_EQ(t1->duration_ms(), 2000);
+
+  EXPECT_CALL(on_expired_, Call).Times(0);
+  AdvanceTimeAndRunTimers(1000);
+
+  // Second time
+  EXPECT_CALL(on_expired_, Call).WillOnce(Return(10000));
+  AdvanceTimeAndRunTimers(1000);
+  EXPECT_EQ(t1->duration_ms(), 10000);
+
+  EXPECT_CALL(on_expired_, Call).Times(0);
+  AdvanceTimeAndRunTimers(9000);
+  EXPECT_CALL(on_expired_, Call).Times(1);
+  AdvanceTimeAndRunTimers(1000);
+}
+
+}  // namespace
+}  // namespace dcsctp

6aef8cef603f4f07210a8f9b43e150f804a60101
Victor Boivie <boivie@webrtc.org>
2021-03-30 19:50:17 +0200

    dcsctp: Add parameters, error causes and chunks
    Quite a large commit, but mostly trivial. It adds all the (in dcSCTP)
    supported parameters, error causes and chunks as an object model, with
    serializers and deserializers. They are verified with packet captures
    where available, that have been captured with Wireshark against a
    reference implementation.
    
    This _could_ be split in parameter/ as one commit, error_cause/ in the
    following, and chunk/ as the third, but as each chunk/parameter is
    completely isolated from the other, reviewing it should be linear with
    the number of chunks/parameters and having them in more commits wouldn't
    change that, taken all those three commits into account.
    
    Bug: webrtc:12614
    Change-Id: Ie83c9a22cae6e3a39e35ef26fd532837a6387a08

---
+++ net/dcsctp/packet/chunk/data_common.h
@@ -0,0 +1,93 @@
+/*
+ *  Copyright (c) 2021 The WebRTC project authors. All Rights Reserved.
+ *
+ *  Use of this source code is governed by a BSD-style license
+ *  that can be found in the LICENSE file in the root of the source
+ *  tree. An additional intellectual property rights grant can be found
+ *  in the file PATENTS.  All contributing project authors may
+ *  be found in the AUTHORS file in the root of the source tree.
+ */
+#ifndef NET_DCSCTP_PACKET_CHUNK_DATA_COMMON_H_
+#define NET_DCSCTP_PACKET_CHUNK_DATA_COMMON_H_
+#include <stdint.h>
+
+#include <utility>
+#include <vector>
+
+#include "api/array_view.h"
+#include "net/dcsctp/packet/chunk/chunk.h"
+#include "net/dcsctp/packet/data.h"
+
+namespace dcsctp {
+
+// Data chunk options.
+// See https://tools.ietf.org/html/rfc4960#section-3.3.1
+struct DataChunkOptions {
+  bool is_end = false;
+  bool is_beginning = false;
+  bool is_unordered = false;
+  bool immediate_ack = false;  // From RFC7053
+};
+
+// Base class for DataChunk and IDataChunk
+class AnyDataChunk : public Chunk {
+ public:
+  uint32_t tsn() const { return tsn_; }
+
+  DataChunkOptions options() const {
+    return DataChunkOptions{
+        .is_end = data_.is_end,
+        .is_beginning = data_.is_beginning,
+        .is_unordered = data_.is_unordered,
+        .immediate_ack = immediate_ack_,
+    };
+  }
+  uint16_t stream_id() const { return data_.stream_id; }
+  uint16_t ssn() const { return data_.ssn; }
+  uint32_t message_id() const { return data_.message_id; }
+  uint32_t fsn() const { return data_.fsn; }
+  uint32_t ppid() const { return data_.ppid; }
+  rtc::ArrayView<const uint8_t> payload() const { return data_.payload; }
+
+  // Extracts the Data from the chunk, as a destructive action.
+  Data extract() && { return std::move(data_); }
+
+  AnyDataChunk(uint32_t tsn,
+               uint16_t stream_id,
+               uint16_t ssn,
+               uint32_t message_id,
+               uint32_t fsn,
+               uint32_t ppid,
+               std::vector<uint8_t> payload,
+               const DataChunkOptions& options)
+      : tsn_(tsn),
+        data_(stream_id,
+              ssn,
+              message_id,
+              fsn,
+              ppid,
+              std::move(payload),
+              options.is_beginning,
+              options.is_end,
+              options.is_unordered),
+        immediate_ack_(options.immediate_ack) {}
+
+  AnyDataChunk(uint32_t tsn, Data data, bool immediate_ack)
+      : tsn_(tsn), data_(std::move(data)), immediate_ack_(immediate_ack) {}
+
+ protected:
+  // Bits in `flags` header field.
+  static constexpr int kFlagsBitEnd = 0;
+  static constexpr int kFlagsBitBeginning = 1;
+  static constexpr int kFlagsBitUnordered = 2;
+  static constexpr int kFlagsBitImmediateAck = 3;
+
+ private:
+  uint32_t tsn_;
+  Data data_;
+  bool immediate_ack_;
+};
+
+}  // namespace dcsctp
+
+#endif  // NET_DCSCTP_PACKET_CHUNK_DATA_COMMON_H_

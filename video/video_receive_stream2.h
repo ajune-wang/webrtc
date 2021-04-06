054791d4f2c80488e86a43032693a03d7bb7405c
Markus Handell <handellm@webrtc.org>
2021-04-06 17:46:49 +0200

    VideoReceiveStream2: AV1 encoded sink support.
    This change adds support for emitting encoded frames
    for recording when the decoder can't easily read out
    encoded width and height as is the case for AV1 streams,
    in which case the information is buried in OBUs. Downstream
    project relies on resolution information being present for key
    frames. With the change, VideoReceiveStream2 infers the
    resolution from decoded frames, and supplies it in the
    RecordableEncodedFrames.
    
    Bug: chromium:1191972
    Change-Id: I07beda6526206c80a732976e8e19d3581489b8fe

--- video/video_receive_stream2.h
+++ video/video_receive_stream2.h
@@ -28,6 +28,7 @@
 #include "rtc_base/system/no_unique_address.h"
 #include "rtc_base/task_queue.h"
 #include "rtc_base/task_utils/pending_task_safety_flag.h"
+#include "rtc_base/thread_annotations.h"
 #include "system_wrappers/include/clock.h"
 #include "video/receive_statistics_proxy2.h"
 #include "video/rtp_streams_synchronizer2.h"
@@ -172,6 +173,8 @@ class VideoReceiveStream2 : public webrtc::VideoReceiveStream,
       RTC_RUN_ON(worker_sequence_checker_);
   bool IsReceivingKeyFrame(int64_t timestamp_ms) const
       RTC_RUN_ON(worker_sequence_checker_);
+  int DecodeAndMaybeDispatchEncodedFrame(std::unique_ptr<EncodedFrame> frame)
+      RTC_RUN_ON(decode_queue_);
 
   void UpdateHistograms();
 
@@ -254,6 +257,17 @@ class VideoReceiveStream2 : public webrtc::VideoReceiveStream,
   // Set to true while we're requesting keyframes but not yet received one.
   bool keyframe_generation_requested_ RTC_GUARDED_BY(worker_sequence_checker_) =
       false;
+  // Lock to avoid unnecessary per-frame idle wakeups in the code.
+  webrtc::Mutex pending_resolution_mutex_;
+  // Signal from decode queue to decoder completion callback to fill
+  // pending_resolution_
+  bool fill_pending_resolution_ RTC_GUARDED_BY(pending_resolution_mutex_) =
+      false;
+  RecordableEncodedFrame::EncodedResolution pending_resolution_
+      RTC_GUARDED_BY(pending_resolution_mutex_){0, 0};
+  // Buffered encoded frames held while waiting for decoded resolution.
+  std::vector<std::unique_ptr<EncodedFrame>> buffered_encoded_frames_
+      RTC_GUARDED_BY(decode_queue_);
 
   // Set by the field trial WebRTC-LowLatencyRenderer. The parameter |enabled|
   // determines if the low-latency renderer algorithm should be used for the

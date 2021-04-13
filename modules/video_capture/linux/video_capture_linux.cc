755ade9199c264889d1b083ad8f9e9014ade4e7a
Markus Handell <handellm@webrtc.org>
2021-04-13 16:45:17 +0200

    WIP refactor PlatformThread
    Change-Id: Id00a0464edf4fc9e552b6a1fbb5d2e1280e88811

--- modules/video_capture/linux/video_capture_linux.cc
+++ modules/video_capture/linux/video_capture_linux.cc
@@ -241,12 +241,15 @@ int32_t VideoCaptureModuleV4L2::StartCapture(
   }
 
   // start capture thread;
-  if (!_captureThread) {
+  if (_captureThread.empty()) {
     quit_ = false;
-    _captureThread.reset(new rtc::PlatformThread(
-        VideoCaptureModuleV4L2::CaptureThread, this, "CaptureThread",
-        rtc::ThreadAttributes().SetPriority(rtc::kHighPriority)));
-    _captureThread->Start();
+    _captureThread = rtc::PlatformThread::SpawnJoinable(
+        [this] {
+          while (CaptureProcess()) {
+          }
+        },
+        "CaptureThread",
+        rtc::ThreadAttributes().SetPriority(rtc::ThreadPriority::kHigh));
   }
 
   // Needed to start UVC camera - from the uvcview application
@@ -262,14 +265,13 @@ int32_t VideoCaptureModuleV4L2::StartCapture(
 }
 
 int32_t VideoCaptureModuleV4L2::StopCapture() {
-  if (_captureThread) {
+  if (!_captureThread.empty()) {
     {
       MutexLock lock(&capture_lock_);
       quit_ = true;
     }
-    // Make sure the capture thread stop stop using the critsect.
-    _captureThread->Stop();
-    _captureThread.reset();
+    // Make sure the capture thread stops using the mutex.
+    _captureThread.Clear();
   }
 
   MutexLock lock(&capture_lock_);
@@ -357,11 +359,6 @@ bool VideoCaptureModuleV4L2::CaptureStarted() {
   return _captureStarted;
 }
 
-void VideoCaptureModuleV4L2::CaptureThread(void* obj) {
-  VideoCaptureModuleV4L2* capture = static_cast<VideoCaptureModuleV4L2*>(obj);
-  while (capture->CaptureProcess()) {
-  }
-}
 bool VideoCaptureModuleV4L2::CaptureProcess() {
   int retVal = 0;
   fd_set rSet;

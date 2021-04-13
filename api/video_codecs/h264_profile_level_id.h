f6d0d0bda9e425b61d3ec810201de6b3d98c8e09
Johannes Kron <kron@webrtc.org>
2021-04-13 09:35:53 +0200

    Add codec comparison function to SdpVideoFormat
    SdpVideoFormat is used to configure video encoder and decoders.
    This CL adds support for comparing two SdpVideoFormat objects
    to determine if they specify the same video codec.
    
    This functionality previously only existed in media/base/codec.h
    which made the code sensitive to circular dependencies. Once
    downstream projects stop using cricket::IsSameCodec, this code
    can be removed.
    
    Bug: chromium:1187565
    Change-Id: I242069aa6af07917637384c80ee4820887defc7d

--- api/video_codecs/h264_profile_level_id.h
+++ api/video_codecs/h264_profile_level_id.h
@@ -1,5 +1,5 @@
 /*
- *  Copyright (c) 2017 The WebRTC project authors. All Rights Reserved.
+ *  Copyright (c) 2021 The WebRTC project authors. All Rights Reserved.
  *
  *  Use of this source code is governed by a BSD-style license
  *  that can be found in the LICENSE file in the root of the source
@@ -8,39 +8,80 @@
  *  be found in the AUTHORS file in the root of the source tree.
  */
 
-#ifndef MEDIA_BASE_H264_PROFILE_LEVEL_ID_H_
-#define MEDIA_BASE_H264_PROFILE_LEVEL_ID_H_
+#ifndef API_VIDEO_CODECS_H264_PROFILE_LEVEL_ID_H_
+#define API_VIDEO_CODECS_H264_PROFILE_LEVEL_ID_H_
 
-#include "api/video_codecs/h264_profile_level_id.h"
+#include <string>
+
+#include "absl/types/optional.h"
+#include "api/video_codecs/sdp_video_format.h"
+#include "rtc_base/system/rtc_export.h"
 
 namespace webrtc {
-namespace H264 {
 
-typedef H264Profile Profile;
-typedef H264Level Level;
-typedef H264ProfileLevelId ProfileLevelId;
+// TODO(kron): Change to enum class once downstream projects are updated.
+enum H264Profile {
+  kProfileConstrainedBaseline,
+  kProfileBaseline,
+  kProfileMain,
+  kProfileConstrainedHigh,
+  kProfileHigh,
+};
+
+// All values are equal to ten times the level number, except level 1b which is
+// special.
+// TODO(kron): Change to enum class once downstream projects are updated.
+enum H264Level {
+  kLevel1_b = 0,
+  kLevel1 = 10,
+  kLevel1_1 = 11,
+  kLevel1_2 = 12,
+  kLevel1_3 = 13,
+  kLevel2 = 20,
+  kLevel2_1 = 21,
+  kLevel2_2 = 22,
+  kLevel3 = 30,
+  kLevel3_1 = 31,
+  kLevel3_2 = 32,
+  kLevel4 = 40,
+  kLevel4_1 = 41,
+  kLevel4_2 = 42,
+  kLevel5 = 50,
+  kLevel5_1 = 51,
+  kLevel5_2 = 52
+};
+
+struct H264ProfileLevelId {
+  constexpr H264ProfileLevelId(H264Profile profile, H264Level level)
+      : profile(profile), level(level) {}
+  H264Profile profile;
+  H264Level level;
+};
 
 // Parse profile level id that is represented as a string of 3 hex bytes.
 // Nothing will be returned if the string is not a recognized H264
 // profile level id.
-constexpr auto ParseProfileLevelId = ParseH264ProfileLevelId;
+absl::optional<H264ProfileLevelId> ParseH264ProfileLevelId(const char* str);
 
 // Parse profile level id that is represented as a string of 3 hex bytes
 // contained in an SDP key-value map. A default profile level id will be
 // returned if the profile-level-id key is missing. Nothing will be returned if
 // the key is present but the string is invalid.
-RTC_EXPORT constexpr auto ParseSdpProfileLevelId =
-    ParseSdpForH264ProfileLevelId;
+RTC_EXPORT absl::optional<H264ProfileLevelId> ParseSdpForH264ProfileLevelId(
+    const SdpVideoFormat::Parameters& params);
 
 // Given that a decoder supports up to a given frame size (in pixels) at up to a
 // given number of frames per second, return the highest H.264 level where it
 // can guarantee that it will be able to support all valid encoded streams that
 // are within that level.
-RTC_EXPORT constexpr auto SupportedLevel = H264SupportedLevel;
+RTC_EXPORT absl::optional<H264Level> H264SupportedLevel(
+    int max_frame_pixel_count,
+    float max_fps);
 
 // Returns canonical string representation as three hex bytes of the profile
 // level id, or returns nothing for invalid profile level ids.
-RTC_EXPORT constexpr auto ProfileLevelIdToString = H264ProfileLevelIdToString;
+RTC_EXPORT absl::optional<std::string> H264ProfileLevelIdToString(
+    const H264ProfileLevelId& profile_level_id);
 
 // Generate codec parameters that will be used as answer in an SDP negotiation
 // based on local supported parameters and remote offered parameters. Both
@@ -58,13 +99,16 @@ RTC_EXPORT constexpr auto ProfileLevelIdToString = H264ProfileLevelIdToString;
 // and |remote_offered_params| must be equal before calling this function. The
 // parameters that are used when negotiating are the level part of
 // profile-level-id and level-asymmetry-allowed.
-constexpr auto GenerateProfileLevelIdForAnswer =
-    GenerateH264ProfileLevelIdForAnswer;
+void GenerateH264ProfileLevelIdForAnswer(
+    const SdpVideoFormat::Parameters& local_supported_params,
+    const SdpVideoFormat::Parameters& remote_offered_params,
+    SdpVideoFormat::Parameters* answer_params);
 
 // Returns true if the parameters have the same H264 profile, i.e. the same
-// H264::Profile (Baseline, High, etc).
-constexpr auto IsSameH264Profile = webrtc::IsSameH264Profile;
+// H264Profile (Baseline, High, etc).
+bool IsSameH264Profile(const SdpVideoFormat::Parameters& params1,
+                       const SdpVideoFormat::Parameters& params2);
 
-}  // namespace H264
 }  // namespace webrtc
-#endif  // MEDIA_BASE_H264_PROFILE_LEVEL_ID_H_
+
+#endif  // API_VIDEO_CODECS_H264_PROFILE_LEVEL_ID_H_

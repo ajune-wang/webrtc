f9cf38c0998556ca9f36dc8b78091586183d52c0
Johannes Kron <kron@webrtc.org>
2021-04-14 15:17:46 +0200

    Add codec comparison function to SdpVideoFormat
    SdpVideoFormat is used to configure video encoder and decoders.
    This CL adds support for comparing two SdpVideoFormat objects
    to determine if they specify the same video codec.
    
    This functionality previously only existed in media/base/codec.h
    which made the code sensitive to circular dependencies. Once
    downstream projects stop using cricket::IsSameCodec, this code
    can be removed.
    
    Bug: chromium:1187565
    Change-Id: I242069aa6af07917637384c80ee4820887defc7d

--- api/video_codecs/vp9_profile.cc
+++ api/video_codecs/vp9_profile.cc
@@ -1,5 +1,5 @@
 /*
- *  Copyright (c) 2018 The WebRTC project authors. All Rights Reserved.
+ *  Copyright (c) 2021 The WebRTC project authors. All Rights Reserved.
  *
  *  Use of this source code is governed by a BSD-style license
  *  that can be found in the LICENSE file in the root of the source
@@ -8,7 +8,7 @@
  *  be found in the AUTHORS file in the root of the source tree.
  */
 
-#include "media/base/vp9_profile.h"
+#include "api/video_codecs/vp9_profile.h"
 
 #include <map>
 #include <utility>

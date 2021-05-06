b50eb6892fa1885deb851f34d8a4e63669248a5f
Florent Castelli <orphis@webrtc.org>
2021-05-07 00:22:42 +0200

    datachannel: Make SendDataParams reliability fields optional<uint16_t>
    It doesn't make sense to use negative values or 0 to disable the
    feature, so we use an optional 16bit value, matching the specification.
    Values bigger than 16bit max are clamped down.
    
    Bug: webrtc:12730
    
    Change-Id: I6bd9cd92f7d0a70a78cf5a7c91dca52c28d08ba1

--- media/sctp/dcsctp_transport.cc
+++ media/sctp/dcsctp_transport.cc
@@ -221,11 +221,10 @@ bool DcSctpTransport::SendData(const cricket::SendDataParams& params,
 
   dcsctp::SendOptions send_options;
   send_options.unordered = dcsctp::IsUnordered(!params.ordered);
-  if (params.max_rtx_ms > 0)
-    send_options.lifetime = dcsctp::DurationMs(params.max_rtx_ms);
-  if (params.max_rtx_count > 0)
-    send_options.max_retransmissions =
-        static_cast<size_t>(params.max_rtx_count);
+  if (params.max_rtx_ms.has_value())
+    send_options.lifetime = dcsctp::DurationMs(*params.max_rtx_ms);
+  if (params.max_rtx_count.has_value())
+    send_options.max_retransmissions = *params.max_rtx_count;
 
   auto error = socket_->Send(std::move(message), send_options);
   switch (error) {

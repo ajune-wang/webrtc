4ca82416c0f0bc10de2907c8e88f26aa1bde3a95
Victor Boivie <boivie@webrtc.org>
2021-05-07 10:55:32 +0200

    dcsctp: Log socket name also in callbacks
    This makes it easier to understand which socket that experience an error
    or abort. Aborts are now also logged, which was missed previously.
    
    Bug: webrtc:12614
    Change-Id: Ie5e4357b3e5450106cc6cc28c1e9578ad53d073a

--- net/dcsctp/socket/mock_dcsctp_socket_callbacks.h
+++ net/dcsctp/socket/mock_dcsctp_socket_callbacks.h
@@ -51,8 +51,9 @@ inline int GetUniqueSeed() {
 
 class MockDcSctpSocketCallbacks : public DcSctpSocketCallbacks {
  public:
-  MockDcSctpSocketCallbacks()
-      : random_(internal::GetUniqueSeed()),
+  MockDcSctpSocketCallbacks(absl::string_view name = "")
+      : log_prefix_(name.empty() ? "" : std::string(name) + ": "),
+        random_(internal::GetUniqueSeed()),
         timeout_manager_([this]() { return now_; }) {
     ON_CALL(*this, SendPacket)
         .WillByDefault([this](rtc::ArrayView<const uint8_t> data) {
@@ -65,9 +66,16 @@ class MockDcSctpSocketCallbacks : public DcSctpSocketCallbacks {
         });
 
     ON_CALL(*this, OnError)
-        .WillByDefault([](ErrorKind error, absl::string_view message) {
+        .WillByDefault([this](ErrorKind error, absl::string_view message) {
           RTC_LOG(LS_WARNING)
-              << "Socket error: " << ToString(error) << "; " << message;
+              << log_prefix_ << "Socket error: " << ToString(error) << "; "
+              << message;
+        });
+    ON_CALL(*this, OnAborted)
+        .WillByDefault([this](ErrorKind error, absl::string_view message) {
+          RTC_LOG(LS_WARNING)
+              << log_prefix_ << "Socket abort: " << ToString(error) << "; "
+              << message;
         });
   }
   MOCK_METHOD(void,
@@ -139,6 +147,7 @@ class MockDcSctpSocketCallbacks : public DcSctpSocketCallbacks {
   }
 
  private:
+  const std::string log_prefix_;
   TimeMs now_ = TimeMs(0);
   webrtc::Random random_;
   FakeTimeoutManager timeout_manager_;

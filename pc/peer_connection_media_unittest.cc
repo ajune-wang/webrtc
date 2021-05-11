33de17afda7bdc82e1854eca3ba2b94b243ad27e
Philipp Hancke <philipp.hancke@googlemail.com>
2021-05-11 13:15:30 +0200

    demonstrate test does not work
    Change-Id: Ibca25a29e8ba796fb3073c1280ded69bb720a7e6

--- pc/peer_connection_media_unittest.cc
+++ pc/peer_connection_media_unittest.cc
@@ -859,6 +859,18 @@ TEST_P(PeerConnectionMediaTest,
 }
 
 TEST_P(PeerConnectionMediaTest,
+       CreateOfferWithVoiceActivityDetectionIncludesComfortNoiseCodecs) {
+  auto caller = CreatePeerConnectionWithAudioVideo();
+  AddComfortNoiseCodecsToSend(caller->media_engine());
+
+  RTCOfferAnswerOptions options;
+  options.voice_activity_detection = true;
+  auto offer = caller->CreateOffer(options);
+
+  EXPECT_TRUE(HasAnyComfortNoiseCodecs(offer->description()));
+}
+
+TEST_P(PeerConnectionMediaTest,
        CreateAnswerWithNoVoiceActivityDetectionIncludesNoComfortNoiseCodecs) {
   auto caller = CreatePeerConnectionWithAudioVideo();
   AddComfortNoiseCodecsToSend(caller->media_engine());

b62edc4dc6fffbff7f36384549fecfdbf99c7575
Danil Chapovalov <danilchap@webrtc.org>
2021-05-17 14:56:45 +0200

    Remove redundant VideoSendStream::rtcp_stats field
    its content is duplicated in the report_block_data member
    
    Bug: webrtc:10678
    Change-Id: I89421ae4ab5f727a233161924372105e222ed404

--- video/report_block_stats_unittest.cc
+++ video/report_block_stats_unittest.cc
@@ -13,65 +13,44 @@
 #include "test/gtest.h"
 
 namespace webrtc {
+namespace {
 
-class ReportBlockStatsTest : public ::testing::Test {
- protected:
-  ReportBlockStatsTest() {
-    // kSsrc1: report 1-3.
-    stats1_1_.packets_lost = 10;
-    stats1_1_.extended_highest_sequence_number = 24000;
-    stats1_2_.packets_lost = 15;
-    stats1_2_.extended_highest_sequence_number = 24100;
-    stats1_3_.packets_lost = 50;
-    stats1_3_.extended_highest_sequence_number = 24200;
-    // kSsrc2: report 1,2.
-    stats2_1_.packets_lost = 111;
-    stats2_1_.extended_highest_sequence_number = 8500;
-    stats2_2_.packets_lost = 136;
-    stats2_2_.extended_highest_sequence_number = 8800;
-  }
+constexpr uint32_t kSsrc1 = 123;
+constexpr uint32_t kSsrc2 = 234;
 
-  const uint32_t kSsrc1 = 123;
-  const uint32_t kSsrc2 = 234;
-  RtcpStatistics stats1_1_;
-  RtcpStatistics stats1_2_;
-  RtcpStatistics stats1_3_;
-  RtcpStatistics stats2_1_;
-  RtcpStatistics stats2_2_;
-};
-
-TEST_F(ReportBlockStatsTest, StoreAndGetFractionLost) {
+TEST(ReportBlockStatsTest, StoreAndGetFractionLost) {
   ReportBlockStats stats;
   EXPECT_EQ(-1, stats.FractionLostInPercent());
 
   // First report.
-  stats.Store(kSsrc1, stats1_1_);
+  stats.Store(kSsrc1, 10, 24'000);
   EXPECT_EQ(-1, stats.FractionLostInPercent());
   // fl: 100 * (15-10) / (24100-24000) = 5%
-  stats.Store(kSsrc1, stats1_2_);
+  stats.Store(kSsrc1, 15, 24'100);
   EXPECT_EQ(5, stats.FractionLostInPercent());
   // fl: 100 * (50-10) / (24200-24000) = 20%
-  stats.Store(kSsrc1, stats1_3_);
+  stats.Store(kSsrc1, 50, 24'200);
   EXPECT_EQ(20, stats.FractionLostInPercent());
 }
 
-TEST_F(ReportBlockStatsTest, StoreAndGetFractionLost_TwoSsrcs) {
+TEST(ReportBlockStatsTest, StoreAndGetFractionLost_TwoSsrcs) {
   ReportBlockStats stats;
   EXPECT_EQ(-1, stats.FractionLostInPercent());
 
   // First report.
-  stats.Store(kSsrc1, stats1_1_);
+  stats.Store(kSsrc1, 10, 24'000);
   EXPECT_EQ(-1, stats.FractionLostInPercent());
   // fl: 100 * (15-10) / (24100-24000) = 5%
-  stats.Store(kSsrc1, stats1_2_);
+  stats.Store(kSsrc1, 15, 24'100);
   EXPECT_EQ(5, stats.FractionLostInPercent());
 
   // First report, kSsrc2.
-  stats.Store(kSsrc2, stats2_1_);
+  stats.Store(kSsrc2, 111, 8'500);
   EXPECT_EQ(5, stats.FractionLostInPercent());
   // fl: 100 * ((15-10) + (136-111)) / ((24100-24000) + (8800-8500)) = 7%
-  stats.Store(kSsrc2, stats2_2_);
+  stats.Store(kSsrc2, 136, 8'800);
   EXPECT_EQ(7, stats.FractionLostInPercent());
 }
 
+}  // namespace
 }  // namespace webrtc

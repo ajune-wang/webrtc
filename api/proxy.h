3324ec7c58ede9ca9b2f0272a8510ed95ddf500f
Tommi <tommi@webrtc.org>
2021-05-18 22:06:20 +0200

    Fix race between enabled() and set_enabled() in VideoTrack.
    Along the way I introduced VideoSourceBaseGuarded, which is equivalent
    to VideoSourceBase except that it applies thread checks. I found that
    it's easy to use VideoSourceBase incorrectly and in fact there appear
    to be tests that do this.
    
    I made the source object const in VideoTrack, as it already was in
    AudioTrack, and that allowed for making the GetSource() accessors
    bypass the proxy thread hop and give the caller direct access.
    
    Bug: webrtc:12773, b/188139639, webrtc:12780
    Change-Id: I022175c4239a1306ef54059c131d81411d5124fe

--- api/proxy.h
+++ api/proxy.h
@@ -411,10 +411,6 @@ class ConstMethodCall : public QueuedTask {
 // always be the same.
 #define BYPASS_PROXY_CONSTMETHOD0(r, method)                                \
   r method() const override {                                               \
-    static_assert(                                                          \
-        std::is_same<r, rtc::Thread*>::value || !std::is_pointer<r>::value, \
-        "Type is a pointer");                                               \
-    static_assert(!std::is_reference<r>::value, "Type is a reference");     \
     return c_->method();                                                    \
   }
 

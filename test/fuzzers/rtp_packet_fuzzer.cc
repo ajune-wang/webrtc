104e842e86cfe4ff8240d72c96377cb36023ba29
Doudou Kisabaka <doudouk@google.com>
2021-05-24 12:29:27 +0200

    Implement the mixer-to-client per CSRC audio level extension (RFC 6465).
    This is loosely based on the similar implementation in gecko.
    
    Bug: webrtc:9965
    Change-Id: I5203a05e1c34ca6f97bd1b143790f95ff245e340

--- test/fuzzers/rtp_packet_fuzzer.cc
+++ test/fuzzers/rtp_packet_fuzzer.cc
@@ -9,6 +9,7 @@
  */
 
 #include <bitset>
+#include <vector>
 
 #include "absl/types/optional.h"
 #include "modules/rtp_rtcp/include/rtp_header_extension_map.h"
@@ -75,6 +76,10 @@ void FuzzOneInput(const uint8_t* data, size_t size) {
         bool voice_activity;
         uint8_t audio_level;
         packet.GetExtension<AudioLevel>(&voice_activity, &audio_level);
+        break;
+      case kRtpExtensionCsrcAudioLevel:
+        std::vector<uint8_t> audio_levels;
+        packet.GetExtension<CsrcAudioLevel>(&audio_levels);
         break;
       case kRtpExtensionAbsoluteSendTime:
         uint32_t sendtime;

e4927c3a085890b739091f1097c601b031a97dec
philipel <philipel@webrtc.org>
2021-05-24 14:12:22 +0200

    RtpFrameReferenceFinder return frames directly instead of via callback.
    Bug: webrtc:12579
    Change-Id: I41263f70a6f3dc60167e41f8b015a7d3b0dc3dd7

--- video/rtp_video_stream_receiver.h
+++ video/rtp_video_stream_receiver.h
@@ -68,7 +68,6 @@ class RtpVideoStreamReceiver : public LossNotificationSender,
                                public RecoveredPacketReceiver,
                                public RtpPacketSinkInterface,
                                public KeyFrameRequestSender,
-                               public OnCompleteFrameCallback,
                                public OnDecryptedFrameCallback,
                                public OnDecryptionStatusChangeCallback,
                                public RtpVideoFrameReceiver {
@@ -172,8 +171,7 @@ class RtpVideoStreamReceiver : public LossNotificationSender,
   // Don't use, still experimental.
   void RequestPacketRetransmit(const std::vector<uint16_t>& sequence_numbers);
 
-  // Implements OnCompleteFrameCallback.
-  void OnCompleteFrame(std::unique_ptr<EncodedFrame> frame) override;
+  void OnCompleteFrames(RtpFrameReferenceFinder::ReturnVector frames);
 
   // Implements OnDecryptedFrameCallback.
   void OnDecryptedFrame(std::unique_ptr<RtpFrameObject> frame) override;

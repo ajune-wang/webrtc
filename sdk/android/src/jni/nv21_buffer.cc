015e58d7604ea4c47ee351eb54394a5d756bbae2
Evan Shrubsole <eshr@google.com>
2021-05-24 18:09:35 +0200

    Deprecate webrtc::NV12ToI420Scaler
    Bug: webrtc:12792
    Change-Id: I481b56dc0a744f25abe3242083a225e2d2d74e67

--- sdk/android/src/jni/nv21_buffer.cc
+++ sdk/android/src/jni/nv21_buffer.cc
@@ -9,14 +9,14 @@
  */
 
 #include <jni.h>
+
 #include <vector>
 
-#include "third_party/libyuv/include/libyuv/convert.h"
-#include "third_party/libyuv/include/libyuv/scale.h"
-
-#include "common_video/libyuv/include/webrtc_libyuv.h"
 #include "rtc_base/checks.h"
 #include "sdk/android/generated_video_jni/NV21Buffer_jni.h"
+#include "third_party/libyuv/include/libyuv/convert.h"
+#include "third_party/libyuv/include/libyuv/planar_functions.h"
+#include "third_party/libyuv/include/libyuv/scale.h"
 
 namespace webrtc {
 namespace jni {
@@ -38,15 +38,20 @@ static void JNI_NV21Buffer_CropAndScale(JNIEnv* jni,
                                         const JavaParamRef<jobject>& j_dst_v,
                                         jint dst_stride_v) {
   const int src_stride_y = src_width;
-  const int src_stride_uv = src_width;
+  const int src_stride_vu = src_width;
   const int crop_chroma_x = crop_x / 2;
   const int crop_chroma_y = crop_y / 2;
+  const int crop_chroma_width = (crop_width + 1) / 2;
+  const int crop_chroma_height = (crop_height + 1) / 2;
+  const int tmp_stride_u = crop_chroma_width;
+  const int tmp_stride_v = crop_chroma_width;
+  const int tmp_size = crop_chroma_height * (tmp_stride_u + tmp_stride_v);
 
   jboolean was_copy;
   jbyte* src_bytes = jni->GetByteArrayElements(j_src.obj(), &was_copy);
   RTC_DCHECK(!was_copy);
   uint8_t const* src_y = reinterpret_cast<uint8_t const*>(src_bytes);
-  uint8_t const* src_uv = src_y + src_height * src_stride_y;
+  uint8_t const* src_vu = src_y + src_height * src_stride_y;
 
   uint8_t* dst_y =
       static_cast<uint8_t*>(jni->GetDirectBufferAddress(j_dst_y.obj()));
@@ -57,13 +62,20 @@ static void JNI_NV21Buffer_CropAndScale(JNIEnv* jni,
 
   // Crop using pointer arithmetic.
   src_y += crop_x + crop_y * src_stride_y;
-  src_uv += 2 * crop_chroma_x + crop_chroma_y * src_stride_uv;
+  src_vu += 2 * crop_chroma_x + crop_chroma_y * src_stride_vu;
 
-  NV12ToI420Scaler scaler;
-  // U- and V-planes are swapped because this is NV21 not NV12.
-  scaler.NV12ToI420Scale(src_y, src_stride_y, src_uv, src_stride_uv, crop_width,
-                         crop_height, dst_y, dst_stride_y, dst_v, dst_stride_v,
-                         dst_u, dst_stride_u, scale_width, scale_height);
+  std::vector<uint8_t> tmp_buffer(tmp_size);
+  uint8_t* tmp_u = tmp_buffer.data();
+  uint8_t* tmp_v = tmp_u + crop_chroma_height * tmp_stride_u;
+
+  // Swap U/V planes since chroma is VU.
+  libyuv::SplitUVPlane(src_vu, src_stride_vu, tmp_v, tmp_stride_v, tmp_u,
+                       tmp_stride_u, crop_chroma_width, crop_chroma_height);
+
+  libyuv::I420Scale(src_y, src_stride_y, tmp_u, tmp_stride_u, tmp_v,
+                    tmp_stride_v, crop_width, crop_height, dst_y, dst_stride_y,
+                    dst_u, dst_stride_u, dst_v, dst_stride_v, scale_width,
+                    scale_height, libyuv::kFilterBox);
 
   jni->ReleaseByteArrayElements(j_src.obj(), src_bytes, JNI_ABORT);
 }

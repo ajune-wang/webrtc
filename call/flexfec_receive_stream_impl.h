5297277744ce96bcdbe2a9a7ac307c7e55f4b8d7
Tommi <tommi@webrtc.org>
2021-05-31 13:41:55 +0200

    Split VideoReceiveStream2 init into worker / network steps.
    This is in preparation for actually doing this initialization
    differently in the Call class. This CL takes the registration
    steps that are inherently network thread associated and makes
    them separate from the ctor/dtor.
    
    Inject Call* instead of worker_thread(), which will simplify upcoming
    work that needs to access the network_thread() as well.
    
    This is related to:
    https://webrtc-review.googlesource.com/c/src/+/220608
    https://webrtc-review.googlesource.com/c/src/+/220609
    
    Bug: webrtc:11993
    Change-Id: I72769fd61de84967d9a645750c40d01660a2716b

--- call/flexfec_receive_stream_impl.h
+++ call/flexfec_receive_stream_impl.h
@@ -54,7 +54,7 @@ class FlexfecReceiveStreamImpl : public FlexfecReceiveStream {
   const Config& GetConfig() const override;
 
  private:
-  RTC_NO_UNIQUE_ADDRESS SequenceChecker network_thread_checker_;
+  RTC_NO_UNIQUE_ADDRESS SequenceChecker packet_sequence_checker_;
 
   // Config.
   const Config config_;
@@ -68,7 +68,7 @@ class FlexfecReceiveStreamImpl : public FlexfecReceiveStream {
   ProcessThread* const process_thread_;
 
   std::unique_ptr<RtpStreamReceiverInterface> rtp_stream_receiver_
-      RTC_GUARDED_BY(network_thread_checker_);
+      RTC_GUARDED_BY(packet_sequence_checker_);
 };
 
 }  // namespace webrtc

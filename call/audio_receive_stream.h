0dc3b3d4963e63252861410e0a53f0b65c11b718
Tommi <tommi@webrtc.org>
2021-06-06 18:43:20 +0200

    Remove AudioReceiveStream::Reconfigure() method.
    Instead, adding specific setters that are needed at runtime:
    * SetDepacketizerToDecoderFrameTransformer
    * SetDecoderMap
    * SetUseTransportCcAndNackHistory
    
    The whole config struct is big and much of the state it holds, needs to
    be considered const. For that reason the Reconfigure() method is too
    broad of an interface since it overwrites the whole config struct
    and doesn't actually handle all the potential config changes that might
    occur when the config changes.
    
    Bug: webrtc:11993
    Change-Id: Ia5311978f56b2e136781467e44f0d18039f0bb2d

--- call/audio_receive_stream.h
+++ call/audio_receive_stream.h
@@ -164,8 +164,13 @@ class AudioReceiveStream {
     rtc::scoped_refptr<webrtc::FrameTransformerInterface> frame_transformer;
   };
 
-  // Reconfigure the stream according to the Configuration.
-  virtual void Reconfigure(const Config& config) = 0;
+  // Methods that support reconfiguring the stream post initialization.
+  virtual void SetDepacketizerToDecoderFrameTransformer(
+      rtc::scoped_refptr<webrtc::FrameTransformerInterface>
+          frame_transformer) = 0;
+  virtual void SetDecoderMap(std::map<int, SdpAudioFormat> decoder_map) = 0;
+  virtual void SetUseTransportCcAndNackHistory(bool use_transport_cc,
+                                               int history_ms) = 0;
 
   // Starts stream activity.
   // When a stream is active, it can receive, process and deliver packets.

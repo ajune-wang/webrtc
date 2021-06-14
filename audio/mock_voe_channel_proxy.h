386e87d6474a1080280ac5c77ada0ba655583d29
Tommi <tommi@webrtc.org>
2021-06-14 09:47:08 +0200

    Don't recreate the audio receive stream when updating the local_ssrc.
    Bug: webrtc:11993
    Change-Id: Ic5d8a8a8b7c12fb1d906e0b3cbdf657fd9e8eafc

--- audio/mock_voe_channel_proxy.h
+++ audio/mock_voe_channel_proxy.h
@@ -104,6 +104,7 @@ class MockChannelReceive : public voe::ChannelReceiveInterface {
       SetFrameDecryptor,
       (rtc::scoped_refptr<webrtc::FrameDecryptorInterface> frame_decryptor),
       (override));
+  MOCK_METHOD(void, OnLocalSsrcChange, (uint32_t local_ssrc), (override));
 };
 
 class MockChannelSend : public voe::ChannelSendInterface {

681ae5bb50805070c9c5ebe2f2df811e4354cf28
Johannes Kron <kron@webrtc.org>
2021-06-16 12:40:12 +0200

    Handle scalability mode correctly in QueryCodecSupport
    All valid scalability modes should be supported by the builtin
    software decoder/encoder.
    
    Bug: chromium:1187565
    Change-Id: If66105d210d5055019f35dae2f80a18ad4a70cdd

--- media/engine/internal_encoder_factory_unittest.cc
+++ media/engine/internal_encoder_factory_unittest.cc
@@ -1,5 +1,5 @@
 /*
- *  Copyright (c) 2016 The WebRTC project authors. All Rights Reserved.
+ *  Copyright (c) 2021 The WebRTC project authors. All Rights Reserved.
  *
  *  Use of this source code is governed by a BSD-style license
  *  that can be found in the LICENSE file in the root of the source
@@ -8,13 +8,13 @@
  *  be found in the AUTHORS file in the root of the source tree.
  */
 
-#include "media/engine/internal_decoder_factory.h"
+#include "media/engine/internal_encoder_factory.h"
 
 #include "api/video_codecs/sdp_video_format.h"
-#include "api/video_codecs/video_decoder.h"
+#include "api/video_codecs/video_encoder.h"
 #include "api/video_codecs/vp9_profile.h"
 #include "media/base/media_constants.h"
-#include "modules/video_coding/codecs/av1/libaom_av1_decoder.h"
+#include "modules/video_coding/codecs/av1/libaom_av1_encoder.h"
 #include "test/gmock.h"
 #include "test/gtest.h"
 
@@ -25,11 +25,11 @@ constexpr bool kVp9Enabled = true;
 #else
 constexpr bool kVp9Enabled = false;
 #endif
-constexpr VideoDecoderFactory::CodecSupport kSupported = {true, false};
-constexpr VideoDecoderFactory::CodecSupport kUnsupported = {false, false};
+constexpr VideoEncoderFactory::CodecSupport kSupported = {true, false};
+constexpr VideoEncoderFactory::CodecSupport kUnsupported = {false, false};
 
-bool Equals(VideoDecoderFactory::CodecSupport a,
-            VideoDecoderFactory::CodecSupport b) {
+bool Equals(VideoEncoderFactory::CodecSupport a,
+            VideoEncoderFactory::CodecSupport b) {
   return a.is_supported == b.is_supported &&
          a.is_power_efficient == b.is_power_efficient;
 }
@@ -39,45 +39,45 @@ using ::testing::Contains;
 using ::testing::Field;
 using ::testing::Not;
 
-TEST(InternalDecoderFactory, TestVP8) {
-  InternalDecoderFactory factory;
-  std::unique_ptr<VideoDecoder> decoder =
-      factory.CreateVideoDecoder(SdpVideoFormat(cricket::kVp8CodecName));
-  EXPECT_TRUE(decoder);
+TEST(InternalEncoderFactory, TestVP8) {
+  InternalEncoderFactory factory;
+  std::unique_ptr<VideoEncoder> encoder =
+      factory.CreateVideoEncoder(SdpVideoFormat(cricket::kVp8CodecName));
+  EXPECT_TRUE(encoder);
 }
 
-TEST(InternalDecoderFactory, TestVP9Profile0) {
-  InternalDecoderFactory factory;
-  std::unique_ptr<VideoDecoder> decoder =
-      factory.CreateVideoDecoder(SdpVideoFormat(
+TEST(InternalEncoderFactory, TestVP9Profile0) {
+  InternalEncoderFactory factory;
+  std::unique_ptr<VideoEncoder> encoder =
+      factory.CreateVideoEncoder(SdpVideoFormat(
           cricket::kVp9CodecName,
           {{kVP9FmtpProfileId, VP9ProfileToString(VP9Profile::kProfile0)}}));
-  EXPECT_EQ(static_cast<bool>(decoder), kVp9Enabled);
+  EXPECT_EQ(static_cast<bool>(encoder), kVp9Enabled);
 }
 
-TEST(InternalDecoderFactory, TestVP9Profile1) {
-  InternalDecoderFactory factory;
-  std::unique_ptr<VideoDecoder> decoder =
-      factory.CreateVideoDecoder(SdpVideoFormat(
+TEST(InternalEncoderFactory, TestVP9Profile1) {
+  InternalEncoderFactory factory;
+  std::unique_ptr<VideoEncoder> encoder =
+      factory.CreateVideoEncoder(SdpVideoFormat(
           cricket::kVp9CodecName,
           {{kVP9FmtpProfileId, VP9ProfileToString(VP9Profile::kProfile1)}}));
-  EXPECT_EQ(static_cast<bool>(decoder), kVp9Enabled);
+  EXPECT_EQ(static_cast<bool>(encoder), kVp9Enabled);
 }
 
-TEST(InternalDecoderFactory, Av1) {
-  InternalDecoderFactory factory;
-  if (kIsLibaomAv1DecoderSupported) {
+TEST(InternalEncoderFactory, Av1) {
+  InternalEncoderFactory factory;
+  if (kIsLibaomAv1EncoderSupported) {
     EXPECT_THAT(factory.GetSupportedFormats(),
                 Contains(Field(&SdpVideoFormat::name, "AV1X")));
-    EXPECT_TRUE(factory.CreateVideoDecoder(SdpVideoFormat("AV1X")));
+    EXPECT_TRUE(factory.CreateVideoEncoder(SdpVideoFormat("AV1X")));
   } else {
     EXPECT_THAT(factory.GetSupportedFormats(),
                 Not(Contains(Field(&SdpVideoFormat::name, "AV1X"))));
   }
 }
 
-TEST(InternalDecoderFactory, QueryCodecSupportNoSvc) {
-  InternalDecoderFactory factory;
+TEST(InternalEncoderFactory, QueryCodecSupportNoSvc) {
+  InternalEncoderFactory factory;
   EXPECT_TRUE(
       Equals(factory.QueryCodecSupport(SdpVideoFormat("VP8"),
                                        /*scalability_mode=*/absl::nullopt),
@@ -89,17 +89,17 @@ TEST(InternalDecoderFactory, QueryCodecSupportNoSvc) {
   EXPECT_TRUE(Equals(
       factory.QueryCodecSupport(
           SdpVideoFormat("VP9", {{kVP9FmtpProfileId,
-                                  VP9ProfileToString(VP9Profile::kProfile1)}}),
+                                  VP9ProfileToString(VP9Profile::kProfile2)}}),
           /*scalability_mode=*/absl::nullopt),
       kVp9Enabled ? kSupported : kUnsupported));
   EXPECT_TRUE(
       Equals(factory.QueryCodecSupport(SdpVideoFormat("AV1X"),
                                        /*scalability_mode=*/absl::nullopt),
-             kIsLibaomAv1DecoderSupported ? kSupported : kUnsupported));
+             kIsLibaomAv1EncoderSupported ? kSupported : kUnsupported));
 }
 
-TEST(InternalDecoderFactory, QueryCodecSupportSvc) {
-  InternalDecoderFactory factory;
+TEST(InternalEncoderFactory, QueryCodecSupportSvc) {
+  InternalEncoderFactory factory;
   // VP8 and VP9 supported for singles spatial layers.
   EXPECT_TRUE(Equals(factory.QueryCodecSupport(SdpVideoFormat("VP8"), "L1T2"),
                      kSupported));
@@ -111,7 +111,7 @@ TEST(InternalDecoderFactory, QueryCodecSupportSvc) {
                      kVp9Enabled ? kSupported : kUnsupported));
 
   EXPECT_TRUE(Equals(factory.QueryCodecSupport(SdpVideoFormat("AV1X"), "L2T1"),
-                     kIsLibaomAv1DecoderSupported ? kSupported : kUnsupported));
+                     kIsLibaomAv1EncoderSupported ? kSupported : kUnsupported));
 
   // Invalid SVC config even though VP8 is supported.
   EXPECT_TRUE(Equals(factory.QueryCodecSupport(SdpVideoFormat("H264"), "L1T2"),

c714d95a64a7282b8d5a06d918fdab0c4ba263a1
philipel <philipel@webrtc.org>
2021-06-18 16:16:04 +0200

    Public RtpVideoFrameAssembler
    This class takes RtpPacketReceived and assembles them into RtpFrameObjects.
    
    Change-Id: Ia9785d069fecccc1d5b81efd257f33c8bd7a778b
    Bug: webrtc:7408, webrtc:12579

---
+++ api/video/rtp_video_frame_assembler.h
@@ -0,0 +1,59 @@
+/*
+ *  Copyright (c) 2021 The WebRTC project authors. All Rights Reserved.
+ *
+ *  Use of this source code is governed by a BSD-style license
+ *  that can be found in the LICENSE file in the root of the source
+ *  tree. An additional intellectual property rights grant can be found
+ *  in the file PATENTS.  All contributing project authors may
+ *  be found in the AUTHORS file in the root of the source tree.
+ */
+
+#ifndef API_VIDEO_RTP_VIDEO_FRAME_ASSEMBLER_H_
+#define API_VIDEO_RTP_VIDEO_FRAME_ASSEMBLER_H_
+
+#include <memory>
+
+#include "absl/container/inlined_vector.h"
+#include "absl/types/variant.h"
+#include "api/video/encoded_frame.h"
+#include "modules/rtp_rtcp/source/rtp_packet_received.h"
+
+namespace webrtc {
+// The RtpVideoFrameAssembler takes RtpPacketReceived and assembles them into
+// complete frames. A frame is considered complete when all packets of the frame
+// has been received, the bitstream data has successfully extracted, an ID has
+// been assigned, and all dependencies are known. Frame IDs are strictly
+// monotonic in decode order, dependencies are expressed as frame IDs.
+class RtpVideoFrameAssembler {
+ public:
+  using FrameVector = absl::InlinedVector<std::unique_ptr<EncodedFrame>, 3>;
+  enum PayloadFormat { kRaw, kH264, kVp8, kVp9, kAv1, kGeneric };
+
+  explicit RtpVideoFrameAssembler(PayloadFormat payload_format);
+  RtpVideoFrameAssembler(const RtpVideoFrameAssembler& other) = delete;
+  RtpVideoFrameAssembler& operator=(const RtpVideoFrameAssembler& other) =
+      delete;
+  ~RtpVideoFrameAssembler();
+
+  // Typically when a packet is inserted zero or one frame is completed. In the
+  // case of RTP packets being inserted out of order then sometime multiple
+  // frames could be completed from a single packet, hence the 'FrameVector'
+  // return type.
+  FrameVector InsertPacket(const RtpPacketReceived& packet);
+
+  // When the receiver is no longer interested in frames past a certain point
+  // (typically after the decoding of a frame) then `ClearTo` should be called
+  // to discard incomplete frames that are prior to the frame with `frame_id`
+  // in decode order. It is not critical that `ClearTo` is called immediately
+  // but it should be called regularly to avoid old packets conflicting with new
+  // packets after an RTP packet sequence number wraparound.
+  void ClearTo(int64_t frame_id);
+
+ private:
+  class Impl;
+  std::unique_ptr<Impl> impl_;
+};
+
+}  // namespace webrtc
+
+#endif  // API_VIDEO_RTP_VIDEO_FRAME_ASSEMBLER_H_

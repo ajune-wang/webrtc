f312b4e35aeeab581f0cdb27da461a0ed198b584
Sergey Silkin <ssilkin@webrtc.org>
2021-07-08 16:00:47 +0200

    Correctly set number of reference buffers in H264 encoder
    iNumRefFrame specifies total number of reference buffers to allocate.
    For N temporal layers we need at least (N - 1) buffers to store last
    encoded frames of all reference temporal layers.
    
    There is no API in OpenH254 encoder to specify exact set of references
    to be used to prediction of a given frame. Encoder can theoretically
    use all available references.
    
    Note that there is logic in OpenH264 which overrides iNumRefFrame to
    max(iNumRefFrame, N - 1): https://source.chromium.org/chromium/chromium/src/+/main:third_party/openh264/src/codec/encoder/core/src/au_set.cpp;drc=8e90a2775c5b9448324fe8fef11d177cb65f36cc;l=122.
    I.e., this change has no real effect. It only makes setup more clear.
    
    Bug: none
    Change-Id: If4b4970007e1cc55d8f052ea05213ab2e89a878f

--- modules/video_coding/codecs/h264/h264_encoder_impl.cc
+++ modules/video_coding/codecs/h264/h264_encoder_impl.cc
@@ -241,7 +241,7 @@ int32_t H264EncoderImpl::InitEncode(const VideoCodec* inst,
     configurations_[i].frame_dropping_on = codec_.H264()->frameDroppingOn;
     configurations_[i].key_frame_interval = codec_.H264()->keyFrameInterval;
     configurations_[i].num_temporal_layers =
-        codec_.simulcastStream[idx].numberOfTemporalLayers;
+        codec_.H264()->numberOfTemporalLayers;
 
     // Create downscaled image buffers.
     if (i > 0) {
@@ -578,7 +578,7 @@ SEncParamExt H264EncoderImpl::CreateEncoderParams(size_t i) const {
       encoder_params.iMaxBitrate;
   encoder_params.iTemporalLayerNum = configurations_[i].num_temporal_layers;
   if (encoder_params.iTemporalLayerNum > 1) {
-    encoder_params.iNumRefFrame = 1;
+    encoder_params.iNumRefFrame = encoder_params.iTemporalLayerNum - 1;
   }
   RTC_LOG(INFO) << "OpenH264 version is " << OPENH264_MAJOR << "."
                 << OPENH264_MINOR;

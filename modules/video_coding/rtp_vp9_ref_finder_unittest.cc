60fbbe6631b76bdc623cbeb7c3093e88645ce926
philipel <philipel@webrtc.org>
2021-07-19 15:44:33 +0200

    Always unwrap VP9 TL0PicIdx forward if the frame is newer.
    Bug: webrtc:12979
    Change-Id: Idcc14f8f61b04f9eb194b55ffa40fb95319a881c

--- modules/video_coding/rtp_vp9_ref_finder_unittest.cc
+++ modules/video_coding/rtp_vp9_ref_finder_unittest.cc
@@ -661,6 +661,22 @@ TEST_F(RtpVp9RefFinderTest, GofTl0Jump) {
   Insert(Frame().Pid(1).SidAndTid(0, 0).Tl0(0).Gof(&ss));
 }
 
+TEST_F(RtpVp9RefFinderTest, Tl0JumpAfterPacketLoss) {
+  GofInfoVP9 ss;
+  ss.SetGofInfoVP9(kTemporalStructureMode1);
+
+  Insert(
+      Frame().Pid(0).SidAndTid(0, 0).Tl0(2).SeqNum(0, 0).AsKeyFrame().Gof(&ss));
+
+  // After packet loss, new frame have "older" Tl0 but are newer in SeqNum
+  // order. These frames should not be considered continous with the frame
+  // before the loss.
+  Insert(Frame().Pid(2).SidAndTid(0, 0).Tl0(1).SeqNum(2, 2));
+  Insert(Frame().Pid(3).SidAndTid(0, 0).Tl0(2).SeqNum(3, 3));
+  Insert(Frame().Pid(4).SidAndTid(0, 0).Tl0(3).SeqNum(4, 4));
+  ASSERT_EQ(1UL, frames_.size());
+}
+
 TEST_F(RtpVp9RefFinderTest, GofTidTooHigh) {
   const int kMaxTemporalLayers = 5;
   GofInfoVP9 ss;

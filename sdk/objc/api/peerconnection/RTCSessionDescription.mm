3cde244a09d40ebd25d6c3cf6f62af5b1509da34
Byoungchan Lee <daniel.l@hpcnt.com>
2021-08-03 10:05:44 +0900

    Fix crash of ObjC SDK sLD / sRD with incorrect SDP.
    There are two problems with setLocalDescription / setRemoteDescription
    in ObjC SDK.
    First, RTCSessionDescription.nativeDescription returns a raw
    nullableSessionDescriptionInterface pointer, where sLD/sRD are calling
    Clone() method unconditionally, so it might crash.
    Second, unnecessary sLD/sRD calls Clone() of the raw pointer and
    does not delete it, so this pointer will leak.
    
    To solve these problems, I changed the return type of nativeDescription to
    std::unique_ptr and removed the call to Clone() method.
    
    Bug: webrtc:13022, webrtc:13035
    Change-Id: Icbb87dda62d3a11af47ec74621cf64b8a6c05228

--- sdk/objc/api/peerconnection/RTCSessionDescription.mm
+++ sdk/objc/api/peerconnection/RTCSessionDescription.mm
@@ -46,13 +46,12 @@
 
 #pragma mark - Private
 
-- (webrtc::SessionDescriptionInterface *)nativeDescription {
+- (std::unique_ptr<webrtc::SessionDescriptionInterface>)nativeDescription {
   webrtc::SdpParseError error;
 
-  webrtc::SessionDescriptionInterface *description =
-      webrtc::CreateSessionDescription([[self class] stdStringForType:_type],
-                                       _sdp.stdString,
-                                       &error);
+  std::unique_ptr<webrtc::SessionDescriptionInterface> description =
+      std::unique_ptr<webrtc::SessionDescriptionInterface>(webrtc::CreateSessionDescription(
+          [[self class] stdStringForType:_type], _sdp.stdString, &error));
 
   if (!description) {
     RTCLogError(@"Failed to create session description: %s\nline: %s",

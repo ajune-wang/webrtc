5d2ee33ad9341a98f3b9233d6d5ae108e49d3aec
Niels Möller <nisse@webrtc.org>
2021-08-11 10:50:49 +0200

    Delete AsyncSocketAdapter::Attach, make socket construction time const
    Bug: webrtc:6424
    Change-Id: I7001c4ac52ddd267dcd55f751f3f38976eab820f

--- rtc_base/async_socket.h
+++ rtc_base/async_socket.h
@@ -14,6 +14,8 @@
 #include <stddef.h>
 #include <stdint.h>
 
+#include <memory>
+
 #include "rtc_base/socket.h"
 #include "rtc_base/socket_address.h"
 #include "rtc_base/third_party/sigslot/sigslot.h"
@@ -45,13 +47,8 @@ class AsyncSocket : public Socket {
 
 class AsyncSocketAdapter : public AsyncSocket, public sigslot::has_slots<> {
  public:
-  // The adapted socket may explicitly be null, and later assigned using Attach.
-  // However, subclasses which support detached mode must override any methods
-  // that will be called during the detached period (usually GetState()), to
-  // avoid dereferencing a null pointer.
   explicit AsyncSocketAdapter(AsyncSocket* socket);
-  ~AsyncSocketAdapter() override;
-  void Attach(AsyncSocket* socket);
+
   SocketAddress GetLocalAddress() const override;
   SocketAddress GetRemoteAddress() const override;
   int Bind(const SocketAddress& addr) override;
@@ -78,7 +75,10 @@ class AsyncSocketAdapter : public AsyncSocket, public sigslot::has_slots<> {
   virtual void OnWriteEvent(AsyncSocket* socket);
   virtual void OnCloseEvent(AsyncSocket* socket, int err);
 
-  AsyncSocket* socket_;
+  AsyncSocket* GetSocket() const { return socket_.get(); }
+
+ private:
+  const std::unique_ptr<AsyncSocket> socket_;
 };
 
 }  // namespace rtc

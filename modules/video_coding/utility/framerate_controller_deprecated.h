ee5e82aedf667a3354e05ea5c85339d05bdbed82
Åsa Persson <asapersson@webrtc.org>
2021-08-15 14:39:32 +0200

    Move frame drop functionality in VideoAdapter into a separate class.
    This class will replace modules/video_coding/utility/framerate_controller.h
    
    Bug: webrtc:13031
    Change-Id: I8faa9c3c158b7c5ab0618e3504224c7e00f8e0b1

--- modules/video_coding/utility/framerate_controller_deprecated.h
+++ modules/video_coding/utility/framerate_controller_deprecated.h
@@ -8,8 +8,8 @@
  *  be found in the AUTHORS file in the root of the source tree.
  */
 
-#ifndef MODULES_VIDEO_CODING_UTILITY_FRAMERATE_CONTROLLER_H_
-#define MODULES_VIDEO_CODING_UTILITY_FRAMERATE_CONTROLLER_H_
+#ifndef MODULES_VIDEO_CODING_UTILITY_FRAMERATE_CONTROLLER_DEPRECATED_H_
+#define MODULES_VIDEO_CODING_UTILITY_FRAMERATE_CONTROLLER_DEPRECATED_H_
 
 #include <stdint.h>
 
@@ -18,9 +18,9 @@
 
 namespace webrtc {
 
-class FramerateController {
+class FramerateControllerDeprecated {
  public:
-  explicit FramerateController(float target_framerate_fps);
+  explicit FramerateControllerDeprecated(float target_framerate_fps);
 
   void SetTargetRate(float target_framerate_fps);
   float GetTargetRate();
@@ -43,4 +43,4 @@ class FramerateController {
 
 }  // namespace webrtc
 
-#endif  // MODULES_VIDEO_CODING_UTILITY_FRAMERATE_CONTROLLER_H_
+#endif  // MODULES_VIDEO_CODING_UTILITY_FRAMERATE_CONTROLLER_DEPRECATED_H_

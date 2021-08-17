d9a4a80b5e8b3370ea915faab5e4efa2218b0ff9
Sergey Silkin <ssilkin@webrtc.org>
2021-08-17 14:10:21 +0200

    Non-integer frame rates in Android HW encoder
    Bug: webrtc:10812
    Change-Id: I4443dcfea851114bd5fbb10f11ca8a51cda12da8

---
+++ sdk/android/tests/src/org/webrtc/FramerateBitrateAdjusterTest.java
@@ -0,0 +1,44 @@
+/*
+ *  Copyright 2021 The WebRTC Project Authors. All rights reserved.
+ *
+ *  Use of this source code is governed by a BSD-style license
+ *  that can be found in the LICENSE file in the root of the source
+ *  tree. An additional intellectual property rights grant can be found
+ *  in the file PATENTS.  All contributing project authors may
+ *  be found in the AUTHORS file in the root of the source tree.
+ */
+
+package org.webrtc;
+
+import static org.junit.Assert.assertEquals;
+
+import org.chromium.testing.local.LocalRobolectricTestRunner;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.annotation.Config;
+import org.webrtc.VideoEncoder.ScalingSettings;
+
+@RunWith(LocalRobolectricTestRunner.class)
+@Config(manifest = Config.NONE)
+public class FramerateBitrateAdjusterTest {
+  @Test
+  public void getAdjustedFramerate_alwaysReturnsDefault() {
+    FramerateBitrateAdjuster bitrateAdjuster = new FramerateBitrateAdjuster();
+    bitrateAdjuster.setTargets(1000, 15);
+    assertEquals(30, bitrateAdjuster.getAdjustedFramerateFps(), 0.0f);
+  }
+
+  @Test
+  public void getAdjustedBitrate_defaultFramerate_returnsTargetBitrate() {
+    FramerateBitrateAdjuster bitrateAdjuster = new FramerateBitrateAdjuster();
+    bitrateAdjuster.setTargets(1000, 30);
+    assertEquals(1000, bitrateAdjuster.getAdjustedBitrateBps());
+  }
+
+  @Test
+  public void getAdjustedBitrate_nonDefaultFramerate_returnsAdjustedBitrate() {
+    FramerateBitrateAdjuster bitrateAdjuster = new FramerateBitrateAdjuster();
+    bitrateAdjuster.setTargets(1000, 7.5);
+    assertEquals(4000, bitrateAdjuster.getAdjustedBitrateBps());
+  }
+}

300d8d92aa93462da155d018cd85afe2de5d6587
Jesús de Vicente Peña <devicentepena@webrtc.org>
2021-09-10 09:19:29 +0200

    Using a more conservative frequency response representative of the tails.
    Change-Id: Ic469f6226fe079c306cec6f941eeb70d6d9094f3

--- modules/audio_processing/aec3/reverb_frequency_response.h
+++ modules/audio_processing/aec3/reverb_frequency_response.h
@@ -44,6 +44,7 @@ class ReverbFrequencyResponse {
               int filter_delay_blocks,
               float linear_filter_quality);
 
+  const bool use_conservative_tail_frequency_response_;
   float average_decay_ = 0.f;
   std::array<float, kFftLengthBy2Plus1> tail_response_;
 };

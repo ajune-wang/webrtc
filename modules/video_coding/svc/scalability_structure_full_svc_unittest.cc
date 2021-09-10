817b9e8cde6bc1fbed40244e682bd2d4bf1cbfd1
Emil Lundmark <lndmrk@webrtc.org>
2021-09-10 12:36:31 +0200

    Allow full SVC to reference T0 frame only after it has been encoded
    The VP9 encoder may drop a frame internally which will not advance the
    frame pattern. Consider the following scenario where only spatial layer
    0 and temporal layer 0 is active:
    
    1. Key frame encoded
    2. Spatial layer 1 is activated
    3. Delta T0 dropped
    4. Delta T0 encoded
    
    No S1T0 frame is encoded in (1) since it's not active. When
    NextFrameConfig is called in (3) it will say that future frames may
    reference T0 on both S0 and S1, but it's then dropped.
    
    On step (4), the SVC controller essentially thinks it's encoding a new
    picture and will happily reference the T0 on what it thinks is the first
    delta frame. However, this is actually still the key frame and since
    there was no S1T0 frame produced it will reference an invalid buffer.
    
    To fix this, only say it's possible to reference a T0 frame after it has
    been successfully encoded.
    
    Bug: webrtc:11999, chromium:1178444

--- modules/video_coding/svc/scalability_structure_full_svc_unittest.cc
+++ modules/video_coding/svc/scalability_structure_full_svc_unittest.cc
@@ -21,6 +21,28 @@ namespace {
 using ::testing::IsEmpty;
 using ::testing::SizeIs;
 
+TEST(ScalabilityStructureL3T3Test, SkipT0FrameByEncoderKeepsReferencesValid) {
+  std::vector<GenericFrameInfo> frames;
+  ScalabilityStructureL3T3 structure;
+  ScalabilityStructureWrapper wrapper(structure);
+
+  // Only S0T0 decode target is enabled.
+  structure.OnRatesUpdated(EnableTemporalLayers(/*s0=*/1, /*s1=*/0));
+  // Encoder generates S0T0 key frame.
+  wrapper.GenerateFrames(/*num_temporal_units=*/1, frames);
+  EXPECT_THAT(frames, SizeIs(1));
+  // Spatial layers 1 is enabled.
+  structure.OnRatesUpdated(EnableTemporalLayers(/*s0=*/1, /*s1=*/1));
+  // Encoder tries to generate S0 delta frames but it gets dropped.
+  structure.NextFrameConfig(/*restart=*/false);
+  ASSERT_THAT(frames, SizeIs(1));
+  // Encoder successfully generates all S0 delta frames.
+  wrapper.GenerateFrames(/*num_temporal_units=*/1, frames);
+  EXPECT_THAT(frames, SizeIs(3));
+
+  EXPECT_TRUE(wrapper.FrameReferencesAreValid(frames));
+}
+
 TEST(ScalabilityStructureL3T3Test, SkipS1T1FrameKeepsStructureValid) {
   ScalabilityStructureL3T3 structure;
   ScalabilityStructureWrapper wrapper(structure);

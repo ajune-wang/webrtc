807fa62fc136a0f76d9fc62db5225d7cfffe911c
Fanny Linderborg <linderborg@webrtc.org>
2021-10-18 09:16:56 +0000

    Add config validation and tests for LossBasedBweV2
    Add tests:
    * Config string with bad syntax
    * Empty config string
    
    Add validation for non-negative `higher_log_bandwidth_bias_factor`-
    
    The mock is needed to be able to test with an empty string.
    
    Bug: webrtc:12707
    Change-Id: I3b13736336886129d5c29e8699f559c9b358ba31

--- modules/congestion_controller/goog_cc/loss_based_bwe_v2_test.cc
+++ modules/congestion_controller/goog_cc/loss_based_bwe_v2_test.cc
@@ -12,6 +12,7 @@
 
 #include <string>
 
+#include "api/test/mock_key_value_config.h"
 #include "api/transport/network_types.h"
 #include "api/units/data_rate.h"
 #include "api/units/data_size.h"
@@ -19,11 +20,15 @@
 #include "api/units/timestamp.h"
 #include "rtc_base/strings/string_builder.h"
 #include "test/explicit_key_value_config.h"
+#include "test/gmock.h"
 #include "test/gtest.h"
 
 namespace webrtc {
 
 namespace {
+
+using ::testing::Return;
+using ::webrtc::test::MockKeyValueConfig;
 
 constexpr TimeDelta kObservationDurationLowerBound = TimeDelta::Millis(200);
 
@@ -85,6 +90,46 @@ TEST(LossBasedBweV2Test, DisabledWhenGivenNonValidConfigurationValues) {
       Config(/*enabled=*/true, /*valid=*/false));
   LossBasedBweV2 loss_based_bandwidth_estimator(&key_value_config);
 
+  EXPECT_FALSE(loss_based_bandwidth_estimator.IsEnabled());
+}
+
+TEST(LossBasedBweV2Test, EnabledWhenGivenAnEnabledConfigurationWithBadSyntax) {
+  // Test that we don't break or disable on bad syntax.
+  test::ExplicitKeyValueConfig key_value_config(
+      "WebRTC-Bwe-LossBasedBweV2/"
+      // Last valid value is used. Value with colon.
+      "Enabled:false,Enabled:true,Enabled:fal:se,"
+      // Empty string.
+      ",,"
+      // Key and value is missing.
+      ",:,"
+      // Keyless value.
+      ":KeylessValue,"
+      // Unknown key.
+      "UnknownKey:Value,"
+      // Empty value.
+      "InstantUpperBoundLossOffset:,"
+      // List with empty element.
+      "CandidateFactors:10||5,"
+      // Boolean value is not a boolean.
+      "AckedRateCandidate:ValueThatCannotBeConvertedToABoolean,"
+      // Float with non-unit unit.
+      "InstantUpperBoundTemporalWeightFactor:0.8k,"
+      // `DataRate` without unit.
+      "InherentLossUpperBoundBwBalance:1,"
+      // `TimeDelta` without unit.
+      "ObservationDurationLowerBound:1,"
+      "/");
+  LossBasedBweV2 loss_based_bandwidth_estimator(&key_value_config);
+  EXPECT_TRUE(loss_based_bandwidth_estimator.IsEnabled());
+}
+
+TEST(LossBasedBweV2Test, DisabledWhenNotGivenAConfiguration) {
+  // Cannot use `ExplicitKeyValueConfig` here since it does not allow empty
+  // field trials.
+  MockKeyValueConfig key_value_config;
+  EXPECT_CALL(key_value_config, Lookup).WillRepeatedly(Return(""));
+  LossBasedBweV2 loss_based_bandwidth_estimator(&key_value_config);
   EXPECT_FALSE(loss_based_bandwidth_estimator.IsEnabled());
 }
 

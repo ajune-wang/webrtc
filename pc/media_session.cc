8380f82fbbd8c7d110728960e5ca2ae2fa86151a
Philipp Hancke <philipp.hancke@googlemail.com>
2021-10-19 13:21:22 +0200

    make setCodecPreferences retain position of rtx/red
    This does not change functionality but improves readability of the SDP
    
    BUG=webrtc:13287
    
    Change-Id: I9b03cd3131eaa932ffed3fb4e66cbf55faedcdd6

--- pc/media_session.cc
+++ pc/media_session.cc
@@ -1078,6 +1078,13 @@ static Codecs MatchCodecPreference(
   bool want_red = false;
 
   for (const auto& codec_preference : codec_preferences) {
+    if (IsRtxCodec(codec_preference)) {
+      want_rtx = true;
+    } else if (IsRedCodec(codec_preference)) {
+      want_red = true;
+    }
+  }
+  for (const auto& codec_preference : codec_preferences) {
     auto found_codec = absl::c_find_if(
         supported_codecs,
         [&codec_preference](const typename Codecs::value_type& codec) {
@@ -1096,33 +1103,28 @@ static Codecs MatchCodecPreference(
       if (FindMatchingCodec(supported_codecs, codecs, *found_codec,
                             &found_codec_with_correct_pt)) {
         filtered_codecs.push_back(found_codec_with_correct_pt);
-        kept_codecs_ids.insert(std::to_string(found_codec_with_correct_pt.id));
-      }
-    } else if (IsRtxCodec(codec_preference)) {
-      want_rtx = true;
-    } else if (IsRedCodec(codec_preference)) {
-      want_red = true;
-    }
-  }
-
-  if (want_rtx || want_red) {
-    for (const auto& codec : codecs) {
-      if (IsRtxCodec(codec)) {
-        const auto apt =
-            codec.params.find(cricket::kCodecParamAssociatedPayloadType);
-        if (apt != codec.params.end() &&
-            kept_codecs_ids.count(apt->second) > 0) {
-          filtered_codecs.push_back(codec);
-        }
-      } else if (IsRedCodec(codec)) {
-        const auto fmtp =
-            codec.params.find(cricket::kCodecParamNotInNameValueFormat);
-        if (fmtp != codec.params.end()) {
-          std::vector<std::string> redundant_payloads;
-          rtc::split(fmtp->second, '/', &redundant_payloads);
-          if (redundant_payloads.size() > 0 &&
-              kept_codecs_ids.count(redundant_payloads[0]) > 0) {
-            filtered_codecs.push_back(codec);
+        std::string id = rtc::ToString(found_codec_with_correct_pt.id);
+        // Search for the match
+        if (want_red || want_rtx) {
+          for (const auto& codec : codecs) {
+            if (IsRtxCodec(codec)) {
+              const auto apt =
+                  codec.params.find(cricket::kCodecParamAssociatedPayloadType);
+              if (apt != codec.params.end() && apt->second == id) {
+                filtered_codecs.push_back(codec);
+              }
+            } else if (IsRedCodec(codec)) {
+              const auto fmtp =
+                  codec.params.find(cricket::kCodecParamNotInNameValueFormat);
+              if (fmtp != codec.params.end()) {
+                std::vector<std::string> redundant_payloads;
+                rtc::split(fmtp->second, '/', &redundant_payloads);
+                if (redundant_payloads.size() > 0 &&
+                    redundant_payloads[0] == id) {
+                  filtered_codecs.push_back(codec);
+                }
+              }
+            }
           }
         }
       }

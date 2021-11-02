446a57bc158edae4abdbb5b32a2484bb2fd8eeac
Markus Handell <handellm@webrtc.org>
2021-11-02 18:24:00 +0100

    VideoStreamEncoder: Introduce frame cadence adapter.
    This change introduces a new FrameCadenceAdapter class which takes the
    role of being a VideoFrameSinkInterface<> instead of VideoStreamEncoder.
    The FrameCadenceAdapter will see its functionality grow in future CLs
    and eventually enable screenshare capture sources to have zero hertz as
    the minimum capture frequency.
    
    This CL moves logic related to UMA collection and constraints into the
    adapter.
    
    The adapter has two major modes. Future functionality is planned to be
    added under the WebRTC-ZeroHertzScreenshare field trial. Unit tests are
    added that verify passthrough operation when WebRTC-ZeroHertzScreenshare
    isn't specified or disabled.
    
    Just specifying the WebRTC-ZeroHertzScreenshare field trial isn't
    enough to activate the feature, but the caller has to additionally
    configure screen content type, minimum FPS 0, and maximum FPS > 0 for
    the new mode.
    
    go/rtc-0hz-present
    
    Bug: chromium:1255737
    Change-Id: I1799110ed40843152786ad80df10acfb83a608b1

---
+++ video/frame_cadence_adapter.cc
@@ -0,0 +1,161 @@
+/*
+ *  Copyright (c) 2021 The WebRTC project authors. All Rights Reserved.
+ *
+ *  Use of this source code is governed by a BSD-style license
+ *  that can be found in the LICENSE file in the root of the source
+ *  tree. An additional intellectual property rights grant can be found
+ *  in the file PATENTS.  All contributing project authors may
+ *  be found in the AUTHORS file in the root of the source tree.
+ */
+
+#include "video/frame_cadence_adapter.h"
+
+#include <memory>
+
+#include "api/sequence_checker.h"
+#include "rtc_base/logging.h"
+#include "rtc_base/race_checker.h"
+#include "rtc_base/synchronization/mutex.h"
+#include "system_wrappers/include/field_trial.h"
+#include "system_wrappers/include/metrics.h"
+
+namespace webrtc {
+namespace {
+
+class FrameCadenceAdapterImpl : public FrameCadenceAdapterInterface {
+ public:
+  FrameCadenceAdapterImpl();
+
+  // FrameCadenceAdapterInterface overrides.
+  void Initialize(Callback* callback) override;
+  void SetEnabledByContentType(bool enabled) override;
+
+  // VideoFrameSink overrides.
+  void OnFrame(const VideoFrame& frame) override;
+  void OnDiscardedFrame() override { callback_->OnDiscardedFrame(); }
+  void OnConstraintsChanged(
+      const VideoTrackSourceConstraints& constraints) override;
+
+ private:
+  // Called to report on constraint UMAs.
+  void MaybeReportFrameRateConstraintUmas()
+      RTC_RUN_ON(&incoming_frame_race_checker_) RTC_LOCKS_EXCLUDED(mutex_);
+
+  // True if we support frame entry for screenshare with a minimum frequency of
+  // 0 Hz.
+  const bool enabled_by_field_trial_;
+
+  // Set up during Initialize.
+  Callback* callback_ = nullptr;
+
+  // Lock protecting zero-hertz activation state. This is needed because the
+  // threading contexts of OnFrame, OnConstraintsChanged, and ConfigureEncoder
+  // are mutating it.
+  Mutex mutex_;
+
+  // The source's constraints.
+  absl::optional<VideoTrackSourceConstraints> source_constraints_
+      RTC_GUARDED_BY(mutex_);
+
+  // Whether operation is enabled by content type (screenshare).
+  bool enabled_by_callee_ RTC_GUARDED_BY(mutex_) = false;
+
+  // Race checker for incoming frames. This is the network thread in chromium,
+  // but may vary from test contexts.
+  rtc::RaceChecker incoming_frame_race_checker_;
+  bool has_reported_screenshare_frame_rate_umas_ RTC_GUARDED_BY(mutex_) = false;
+};
+
+FrameCadenceAdapterImpl::FrameCadenceAdapterImpl()
+    : enabled_by_field_trial_(
+          field_trial::IsEnabled("WebRTC-ZeroHertzScreenshare")) {}
+
+void FrameCadenceAdapterImpl::Initialize(Callback* callback) {
+  callback_ = callback;
+}
+
+void FrameCadenceAdapterImpl::SetEnabledByContentType(bool enabled) {
+  // This method is called on the worker thread.
+  MutexLock lock(&mutex_);
+  if (enabled && !enabled_by_callee_)
+    has_reported_screenshare_frame_rate_umas_ = false;
+  enabled_by_callee_ = enabled;
+}
+
+void FrameCadenceAdapterImpl::OnFrame(const VideoFrame& frame) {
+  // This method is called on the network thread under Chromium, or other
+  // various contexts in test.
+  RTC_DCHECK_RUNS_SERIALIZED(&incoming_frame_race_checker_);
+  callback_->OnFrame(frame);
+  MaybeReportFrameRateConstraintUmas();
+}
+
+void FrameCadenceAdapterImpl::OnConstraintsChanged(
+    const VideoTrackSourceConstraints& constraints) {
+  RTC_LOG(LS_INFO) << __func__ << " min_fps "
+                   << constraints.min_fps.value_or(-1) << " max_fps "
+                   << constraints.max_fps.value_or(-1);
+  MutexLock lock(&mutex_);
+  source_constraints_ = constraints;
+}
+
+// RTC_RUN_ON(&incoming_frame_race_checker_)
+void FrameCadenceAdapterImpl::MaybeReportFrameRateConstraintUmas() {
+  MutexLock lock(&mutex_);
+  if (has_reported_screenshare_frame_rate_umas_)
+    return;
+  has_reported_screenshare_frame_rate_umas_ = true;
+  if (!enabled_by_callee_)
+    return;
+  RTC_HISTOGRAM_BOOLEAN("WebRTC.Screenshare.FrameRateConstraints.Exists",
+                        source_constraints_.has_value());
+  if (!source_constraints_.has_value())
+    return;
+  RTC_HISTOGRAM_BOOLEAN("WebRTC.Screenshare.FrameRateConstraints.Min.Exists",
+                        source_constraints_->min_fps.has_value());
+  if (source_constraints_->min_fps.has_value()) {
+    RTC_HISTOGRAM_COUNTS_100(
+        "WebRTC.Screenshare.FrameRateConstraints.Min.Value",
+        source_constraints_->min_fps.value());
+  }
+  RTC_HISTOGRAM_BOOLEAN("WebRTC.Screenshare.FrameRateConstraints.Max.Exists",
+                        source_constraints_->max_fps.has_value());
+  if (source_constraints_->max_fps.has_value()) {
+    RTC_HISTOGRAM_COUNTS_100(
+        "WebRTC.Screenshare.FrameRateConstraints.Max.Value",
+        source_constraints_->max_fps.value());
+  }
+  if (!source_constraints_->min_fps.has_value()) {
+    if (source_constraints_->max_fps.has_value()) {
+      RTC_HISTOGRAM_COUNTS_100(
+          "WebRTC.Screenshare.FrameRateConstraints.MinUnset.Max",
+          source_constraints_->max_fps.value());
+    }
+  } else if (source_constraints_->max_fps.has_value()) {
+    if (source_constraints_->min_fps.value() <
+        source_constraints_->max_fps.value()) {
+      RTC_HISTOGRAM_COUNTS_100(
+          "WebRTC.Screenshare.FrameRateConstraints.MinLessThanMax.Min",
+          source_constraints_->min_fps.value());
+      RTC_HISTOGRAM_COUNTS_100(
+          "WebRTC.Screenshare.FrameRateConstraints.MinLessThanMax.Max",
+          source_constraints_->max_fps.value());
+    }
+    constexpr int kMaxBucketCount =
+        60 * /*max min_fps=*/60 + /*max max_fps=*/60 - 1;
+    RTC_HISTOGRAM_ENUMERATION_SPARSE(
+        "WebRTC.Screenshare.FrameRateConstraints.60MinPlusMaxMinusOne",
+        source_constraints_->min_fps.value() * 60 +
+            source_constraints_->max_fps.value() - 1,
+        /*boundary=*/kMaxBucketCount);
+  }
+}
+
+}  // namespace
+
+std::unique_ptr<FrameCadenceAdapterInterface>
+FrameCadenceAdapterInterface::Create() {
+  return std::make_unique<FrameCadenceAdapterImpl>();
+}
+
+}  // namespace webrtc

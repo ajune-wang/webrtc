a3c0c42a2c9d250bbabe11de61898cb32bcbf5ff
Evan Shrubsole <eshr@webrtc.org>
2021-11-05 10:45:22 +0100

    Set FrameBuffer's callback_queue in constructor
    Bug: webrtc:13343
    Change-Id: Id0f3f3e7120493f4eba90b4cd81017436bc39532

--- video/video_receive_stream.cc
+++ video/video_receive_stream.cc
@@ -217,8 +217,8 @@ VideoReceiveStream::VideoReceiveStream(
 
   timing_->set_render_delay(config_.render_delay_ms);
 
-  frame_buffer_.reset(
-      new video_coding::FrameBuffer(clock_, timing_.get(), &stats_proxy_));
+  frame_buffer_ = std::make_unique<video_coding::FrameBuffer>(
+      clock_, timing_.get(), &stats_proxy_, &decode_queue_);
 
   process_thread_->RegisterModule(&rtp_stream_sync_, RTC_FROM_HERE);
   // Register with RtpStreamReceiverController.
@@ -610,7 +610,7 @@ int64_t VideoReceiveStream::GetWaitMs() const {
 void VideoReceiveStream::StartNextDecode() {
   TRACE_EVENT0("webrtc", "VideoReceiveStream::StartNextDecode");
   frame_buffer_->NextFrame(
-      GetWaitMs(), keyframe_required_, &decode_queue_,
+      GetWaitMs(), keyframe_required_,
       /* encoded frame handler */
       [this](std::unique_ptr<EncodedFrame> frame, ReturnReason res) {
         RTC_DCHECK_EQ(frame == nullptr, res == ReturnReason::kTimeout);

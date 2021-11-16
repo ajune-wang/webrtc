47b3a48656bb1d1f3381691cf0c8fd572b51dbaa
Jan Grulich <grulja@gmail.com>
2021-11-16 10:33:57 +0100

    PipeWire capturer: split xdg-desktop-portal implementation
    We will need to use xdg-desktop-portal in order to bring similar support
    for webcams on Linux. This makes the xdg-desktop-portal implementation a
    standalone class, extended by support for the Camera portal so it can be
    used for webcam support using PipeWire.
    
    Change-Id: Ide5875124371c5b060d9707be47bd60b8ba8be10

--- modules/desktop_capture/linux/base_capturer_pipewire.h
+++ modules/desktop_capture/linux/base_capturer_pipewire.h
@@ -10,16 +10,15 @@
 
 #ifndef MODULES_DESKTOP_CAPTURE_LINUX_BASE_CAPTURER_PIPEWIRE_H_
 #define MODULES_DESKTOP_CAPTURE_LINUX_BASE_CAPTURER_PIPEWIRE_H_
-#include <gio/gio.h>
-#define typeof __typeof__
+
 #include <pipewire/pipewire.h>
 #include <spa/param/video/format-utils.h>
 #include <spa/utils/result.h>
 
-#include "absl/types/optional.h"
 #include "modules/desktop_capture/desktop_capture_options.h"
 #include "modules/desktop_capture/desktop_capturer.h"
 #include "modules/desktop_capture/linux/egl_dmabuf.h"
+#include "modules/desktop_capture/linux/xdg_desktop_portal.h"
 #include "rtc_base/constructor_magic.h"
 #include "rtc_base/synchronization/mutex.h"
 
@@ -27,22 +26,7 @@ namespace webrtc {
 
 class BaseCapturerPipeWire : public DesktopCapturer {
  public:
-  // Values are set based on source type property in
-  // xdg-desktop-portal/screencast
-  // https://github.com/flatpak/xdg-desktop-portal/blob/master/data/org.freedesktop.portal.ScreenCast.xml
-  enum class CaptureSourceType : uint32_t {
-    kScreen = 0b01,
-    kWindow = 0b10,
-    kAny = 0b11
-  };
-
-  enum class CursorMode : uint32_t {
-    kHidden = 0b01,
-    kEmbedded = 0b10,
-    kMetadata = 0b100
-  };
-
-  explicit BaseCapturerPipeWire(CaptureSourceType source_type);
+  explicit BaseCapturerPipeWire();
   ~BaseCapturerPipeWire() override;
 
   static std::unique_ptr<DesktopCapturer> CreateRawCapturer(
@@ -70,25 +54,7 @@ class BaseCapturerPipeWire : public DesktopCapturer {
 
   struct spa_video_info_raw spa_video_format_;
 
-  guint32 pw_stream_node_id_ = 0;
-  gint32 pw_fd_ = -1;
-
-  CaptureSourceType capture_source_type_ =
-      BaseCapturerPipeWire::CaptureSourceType::kScreen;
-
-  // <-- end of PipeWire types
-
-  GDBusConnection* connection_ = nullptr;
-  GDBusProxy* proxy_ = nullptr;
-  GCancellable *cancellable_ = nullptr;
-  gchar* portal_handle_ = nullptr;
-  gchar* session_handle_ = nullptr;
-  gchar* sources_handle_ = nullptr;
-  gchar* start_handle_ = nullptr;
-  guint session_request_signal_id_ = 0;
-  guint sources_request_signal_id_ = 0;
-  guint start_request_signal_id_ = 0;
-
+  bool capturer_failed_ = false;
   int64_t modifier_;
   DesktopSize video_size_;
   DesktopSize desktop_size_ = {};
@@ -98,13 +64,13 @@ class BaseCapturerPipeWire : public DesktopCapturer {
   std::unique_ptr<BasicDesktopFrame> current_frame_;
   Callback* callback_ = nullptr;
 
-  bool portal_init_failed_ = false;
-
   std::unique_ptr<EglDmaBuf> egl_dmabuf_;
+  std::unique_ptr<XdgDesktopPortal> xdg_desktop_portal_;
 
   void Init();
   void InitPortal();
   void InitPipeWireTypes();
+  void OnPortalResponse(bool result);
 
   pw_stream* CreateReceivingStream();
   void HandleBuffer(pw_buffer* buffer);
@@ -126,57 +92,6 @@ class BaseCapturerPipeWire : public DesktopCapturer {
 
   static void OnStreamProcess(void* data);
   static void OnNewBuffer(void* data, uint32_t id);
-
-  guint SetupRequestResponseSignal(const gchar* object_path,
-                                   GDBusSignalCallback callback);
-
-  static void OnProxyRequested(GObject* object,
-                               GAsyncResult* result,
-                               gpointer user_data);
-
-  static gchar* PrepareSignalHandle(GDBusConnection* connection,
-                                    const gchar* token);
-
-  void SessionRequest();
-  static void OnSessionRequested(GDBusProxy *proxy,
-                                 GAsyncResult* result,
-                                 gpointer user_data);
-  static void OnSessionRequestResponseSignal(GDBusConnection* connection,
-                                             const gchar* sender_name,
-                                             const gchar* object_path,
-                                             const gchar* interface_name,
-                                             const gchar* signal_name,
-                                             GVariant* parameters,
-                                             gpointer user_data);
-
-  void SourcesRequest();
-  static void OnSourcesRequested(GDBusProxy *proxy,
-                                 GAsyncResult* result,
-                                 gpointer user_data);
-  static void OnSourcesRequestResponseSignal(GDBusConnection* connection,
-                                             const gchar* sender_name,
-                                             const gchar* object_path,
-                                             const gchar* interface_name,
-                                             const gchar* signal_name,
-                                             GVariant* parameters,
-                                             gpointer user_data);
-
-  void StartRequest();
-  static void OnStartRequested(GDBusProxy *proxy,
-                               GAsyncResult* result,
-                               gpointer user_data);
-  static void OnStartRequestResponseSignal(GDBusConnection* connection,
-                                           const gchar* sender_name,
-                                           const gchar* object_path,
-                                           const gchar* interface_name,
-                                           const gchar* signal_name,
-                                           GVariant* parameters,
-                                           gpointer user_data);
-
-  void OpenPipeWireRemote();
-  static void OnOpenPipeWireRemoteRequested(GDBusProxy *proxy,
-                                            GAsyncResult* result,
-                                            gpointer user_data);
 
   RTC_DISALLOW_COPY_AND_ASSIGN(BaseCapturerPipeWire);
 };

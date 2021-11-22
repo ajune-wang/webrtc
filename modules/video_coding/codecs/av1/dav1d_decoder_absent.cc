b9934f1b1b1e6db31d5460f1fce00aae33bf3ac4
philipel <philipel@webrtc.org>
2021-11-22 14:10:31 +0100

    Reland "Add dav1d decoder to WebRTC."
    This reverts commit 8498b7e7f6b90fa036de2a6887d34256f0565b4f.
    
    Reason for revert: Updating CL to include conditional build flag.
    
    Original change's description:
    > Revert "Add dav1d decoder to WebRTC."
    >
    > This reverts commit 147858577d4db6d257d3cc248fe571a1bbf887e3.
    >
    > Reason for revert: High binary size increase
    >
    > Original change's description:
    > > Add dav1d decoder to WebRTC.
    > >
    > > Bug: none
    > > Change-Id: I7642f42e592dcf510679f881f118bc4dab93b31c
    > > Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/237504
    > > Commit-Queue: Philip Eliasson <philipel@webrtc.org>
    > > Reviewed-by: Mirko Bonadei <mbonadei@webrtc.org>
    > > Reviewed-by: Danil Chapovalov <danilchap@webrtc.org>
    > > Reviewed-by: Magnus Flodman <mflodman@webrtc.org>
    > > Reviewed-by: Sergey Silkin <ssilkin@webrtc.org>
    > > Reviewed-by: Ilya Nikolaevskiy <ilnik@webrtc.org>
    > > Cr-Commit-Position: refs/heads/main@{#35394}
    >
    > TBR=danilchap@webrtc.org,mbonadei@webrtc.org,ilnik@webrtc.org,philipel@webrtc.org,mflodman@webrtc.org,ssilkin@webrtc.org,webrtc-scoped@luci-project-accounts.iam.gserviceaccount.com
    >
    > Change-Id: I00a8acd6ea94ce523c2d5ba705333c9174678180
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Bug: none
    > Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/238560
    > Reviewed-by: Artem Titov <titovartem@webrtc.org>
    > Reviewed-by: Mirko Bonadei <mbonadei@webrtc.org>
    > Owners-Override: Mirko Bonadei <mbonadei@webrtc.org>
    > Owners-Override: Artem Titov <titovartem@webrtc.org>
    > Commit-Queue: Mirko Bonadei <mbonadei@webrtc.org>
    > Cr-Commit-Position: refs/heads/main@{#35395}
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: none
    Change-Id: Iff51848731646159e87e075c38af7cb6355f5b5b

---
+++ modules/video_coding/codecs/av1/dav1d_decoder_absent.cc
@@ -0,0 +1,24 @@
+/*
+ *  Copyright (c) 2021 The WebRTC project authors. All Rights Reserved.
+ *
+ *  Use of this source code is governed by a BSD-style license
+ *  that can be found in the LICENSE file in the root of the source
+ *  tree. An additional intellectual property rights grant can be found
+ *  in the file PATENTS.  All contributing project authors may
+ *  be found in the AUTHORS file in the root of the source tree.
+ */
+#include "modules/video_coding/codecs/av1/dav1d_decoder.h"
+
+#include <memory>
+
+#include "api/video_codecs/video_decoder.h"
+
+namespace webrtc {
+
+const bool kIsDav1dDecoderSupported = false;
+
+std::unique_ptr<VideoDecoder> CreateDav1dDecoder() {
+  return nullptr;
+}
+
+}  // namespace webrtc

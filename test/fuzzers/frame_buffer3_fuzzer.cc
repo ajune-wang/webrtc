790788283a03b45a1f707f166f2b0b7a000bd17a
philipel <philipel@webrtc.org>
2021-11-26 17:16:39 +0100

    FrameBuffer3 WIP
    Change-Id: Ic3bd540c4f69cec26fce53a40425f3bcd9afe085

---
+++ test/fuzzers/frame_buffer3_fuzzer.cc
@@ -0,0 +1,66 @@
+/*
+ *  Copyright (c) 2021 The WebRTC project authors. All Rights Reserved.
+ *
+ *  Use of this source code is governed by a BSD-style license
+ *  that can be found in the LICENSE file in the root of the source
+ *  tree. An additional intellectual property rights grant can be found
+ *  in the file PATENTS.  All contributing project authors may
+ *  be found in the AUTHORS file in the root of the source tree.
+ */
+
+#include "modules/video_coding/frame_buffer3.h"
+
+#include "api/video/encoded_frame.h"
+#include "test/fuzzers/fuzz_data_helper.h"
+
+namespace webrtc {
+namespace {
+class FuzzyFrameObject : public EncodedFrame {
+ public:
+  int64_t ReceivedTime() const override { return 0; }
+  int64_t RenderTime() const override { return 0; }
+};
+}  // namespace
+
+void FuzzOneInput(const uint8_t* data, size_t size) {
+  if (size > 10000) {
+    return;
+  }
+
+  FrameBuffer buffer(/*max_frame_slots=*/100, /*max_decode_history=*/1000);
+  test::FuzzDataHelper helper({data, size});
+
+  while (helper.BytesLeft()) {
+    int action = helper.ReadOrDefaultValue<uint8_t>(0) % 3;
+
+    switch (action) {
+      case 0: {
+        auto frame = std::make_unique<FuzzyFrameObject>();
+        frame->SetTimestamp(helper.ReadOrDefaultValue<uint32_t>(0));
+        frame->SetId(helper.ReadOrDefaultValue<int64_t>(0));
+        frame->is_last_spatial_layer = helper.ReadOrDefaultValue<bool>(false);
+
+        frame->num_references = helper.ReadOrDefaultValue<uint8_t>(0) %
+                                EncodedFrame::kMaxFrameReferences;
+
+        for (uint8_t i = 0; i < frame->num_references; ++i) {
+          frame->references[i] = helper.ReadOrDefaultValue<int64_t>(0);
+        }
+
+        buffer.InsertFrame(std::move(frame));
+        break;
+      }
+
+      case 1: {
+        buffer.ExtractNextTemporalUnit();
+        break;
+      }
+      case 2: {
+        buffer.DropNextTemporalUnit();
+        break;
+      }
+    }
+  }
+}
+
+}  // namespace webrtc

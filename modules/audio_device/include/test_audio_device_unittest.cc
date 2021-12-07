c5ccfeb3a741c28a41032af00f1c85bca293cb6f
Harald Alvestrand <hta@webrtc.org>
2021-12-07 15:49:08 +0000

    Change single-argument rtc::Buffer() to static create function.
    This makes it more obvious when we're creating uninitialized data
    that might be problematic later.
    
    Bug: webrtc:13474
    Change-Id: Iabacade4170d8fb12d4fd3cf80dad301f7a5586e

--- modules/audio_device/include/test_audio_device_unittest.cc
+++ modules/audio_device/include/test_audio_device_unittest.cc
@@ -153,7 +153,8 @@ TEST(WavFileReaderTest, RepeatedTrueWithSingleFrameFileReadTwice) {
   {
     std::unique_ptr<TestAudioDeviceModule::Capturer> reader =
         TestAudioDeviceModule::CreateWavFileReader(output_filename, true);
-    rtc::BufferT<int16_t> buffer(kExpectedSamples.size());
+    rtc::BufferT<int16_t> buffer =
+        rtc::BufferT<int16_t>::CreateWithCapacity(kExpectedSamples.size());
     EXPECT_TRUE(reader->Capture(&buffer));
     EXPECT_EQ(kExpectedSamples, buffer);
     EXPECT_TRUE(reader->Capture(&buffer));

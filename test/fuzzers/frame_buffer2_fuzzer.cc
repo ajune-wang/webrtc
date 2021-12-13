1c6611e1acff91cf6ae07e53218754378ea6110a
Evan Shrubsole <eshr@webrtc.org>
2021-12-13 10:51:06 +0100

    Implement FrameBuffer2 adapter for testing frame scheduler
    Bug: webrtc:13343
    Change-Id: I1aebdf1c107bbe9f96be881a0a9374a88d29703d

--- test/fuzzers/frame_buffer2_fuzzer.cc
+++ test/fuzzers/frame_buffer2_fuzzer.cc
@@ -69,9 +69,7 @@ void FuzzOneInput(const uint8_t* data, size_t size) {
       time_controller.GetTaskQueueFactory()->CreateTaskQueue(
           "time_tq", TaskQueueFactory::Priority::NORMAL));
   VCMTiming timing(time_controller.GetClock());
-  TimeDelta max_wait_time = TimeDelta::Millis(reader.GetNum<uint8_t>());
-  video_coding::FrameBuffer frame_buffer({max_wait_time, max_wait_time},
-                                         time_controller.GetClock(), &timing,
+  video_coding::FrameBuffer frame_buffer(time_controller.GetClock(), &timing,
                                          nullptr);
 
   bool next_frame_task_running = false;
@@ -93,10 +91,12 @@ void FuzzOneInput(const uint8_t* data, size_t size) {
       if (!next_frame_task_running) {
         next_frame_task_running = true;
         bool keyframe_required = reader.GetNum<uint8_t>() % 2;
+        int max_wait_time_ms = reader.GetNum<uint8_t>();
         task_queue.PostTask([&task_queue, &frame_buffer,
-                             &next_frame_task_running, keyframe_required] {
+                             &next_frame_task_running, keyframe_required,
+                             max_wait_time_ms] {
           frame_buffer.NextFrame(
-              keyframe_required, &task_queue,
+              max_wait_time_ms, keyframe_required, &task_queue,
               [&next_frame_task_running](std::unique_ptr<EncodedFrame> frame) {
                 next_frame_task_running = false;
               });

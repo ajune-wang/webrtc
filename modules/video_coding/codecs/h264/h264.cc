6989bb5e498458dda7fd34aca2ea15ebf750fa9a
Stefan Mitic <stefan.mitic@nutanix.com>
2021-12-17 06:39:59 -0800

    Added support for H264 YUV444 (I444) decoding.
    Added Nutanix Inc. to the AUTHORS file.
    
    PS#1 is a reland of "Added support for H264 YUV444 (I444) decoding." https://webrtc-review.googlesource.com/c/src/+/234540
    
    Bug: chromium:1251096
    Change-Id: I99a1b1e4d8b60192ff96f92334a430240875c66c

--- modules/video_coding/codecs/h264/h264.cc
+++ modules/video_coding/codecs/h264/h264.cc
@@ -80,18 +80,21 @@ std::vector<SdpVideoFormat> SupportedH264Codecs() {
   //
   // We support both packetization modes 0 (mandatory) and 1 (optional,
   // preferred).
-  return {CreateH264Format(H264Profile::kProfileBaseline, H264Level::kLevel3_1,
-                           "1"),
-          CreateH264Format(H264Profile::kProfileBaseline, H264Level::kLevel3_1,
-                           "0"),
-          CreateH264Format(H264Profile::kProfileConstrainedBaseline,
-                           H264Level::kLevel3_1, "1"),
-          CreateH264Format(H264Profile::kProfileConstrainedBaseline,
-                           H264Level::kLevel3_1, "0"),
-          CreateH264Format(H264Profile::kProfileMain,
-                           H264Level::kLevel3_1, "1"),
-          CreateH264Format(H264Profile::kProfileMain,
-                           H264Level::kLevel3_1, "0")};
+  return {
+      CreateH264Format(H264Profile::kProfileBaseline, H264Level::kLevel3_1,
+                       "1"),
+      CreateH264Format(H264Profile::kProfileBaseline, H264Level::kLevel3_1,
+                       "0"),
+      CreateH264Format(H264Profile::kProfileConstrainedBaseline,
+                       H264Level::kLevel3_1, "1"),
+      CreateH264Format(H264Profile::kProfileConstrainedBaseline,
+                       H264Level::kLevel3_1, "0"),
+      CreateH264Format(H264Profile::kProfileMain, H264Level::kLevel3_1, "1"),
+      CreateH264Format(H264Profile::kProfileMain, H264Level::kLevel3_1, "0"),
+      CreateH264Format(H264Profile::kProfilePredictiveHigh444,
+                       H264Level::kLevel3_1, "1"),
+      CreateH264Format(H264Profile::kProfilePredictiveHigh444,
+                       H264Level::kLevel3_1, "0")};
 }
 
 std::unique_ptr<H264Encoder> H264Encoder::Create(

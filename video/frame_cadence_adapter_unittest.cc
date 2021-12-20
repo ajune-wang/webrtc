d9e25fafed0e8c83a521cf6b0a9b6b28670f50f1
Markus Handell <handellm@webrtc.org>
2021-12-20 16:49:01 +0100

    ZeroHertzAdapterMode: handle key frame requests.
    Under zero-hertz mode, it may take up to a second between frames.
    This results in key frame requests getting serviced with that delay,
    which is undesired.
    
    This change introduces VideoSourceInterface::RequestRefreshFrame
    which results in a refresh frame being sent from complying sources.
    The method is used under zero-hertz mode from the VideoStreamEncoder
    when frames didn't arrive to it yet (with changes to the zero-hertz
    adapter).
    
    With this change, when the frame adapter has received at least one
    frame, it will conditionally repeat that frame in response to the
    key frame request.
    
    Bug: chromium:1255737
    Change-Id: I6f97813b3a938747357d45e5dda54f759129b44d

--- video/frame_cadence_adapter_unittest.cc
+++ video/frame_cadence_adapter_unittest.cc
@@ -352,6 +352,100 @@ TEST(FrameCadenceAdapterTest, StopsRepeatingFramesDelayed) {
   time_controller.AdvanceTime(TimeDelta::Seconds(1));
 }
 
+TEST(FrameCadenceAdapterTest, RequestsRefreshFrameOnKeyFrameRequestWhenNew) {
+  ZeroHertzFieldTrialEnabler enabler;
+  MockCallback callback;
+  GlobalSimulatedTimeController time_controller(Timestamp::Millis(0));
+  auto adapter = CreateAdapter(time_controller.GetClock());
+  adapter->Initialize(&callback);
+  adapter->SetZeroHertzModeEnabled(
+      FrameCadenceAdapterInterface::ZeroHertzModeParams{});
+  adapter->OnConstraintsChanged(VideoTrackSourceConstraints{0, 10});
+  time_controller.AdvanceTime(TimeDelta::Zero());
+  EXPECT_TRUE(adapter->ProcessKeyFrameRequest());
+}
+
+TEST(FrameCadenceAdapterTest, IgnoresKeyFrameRequestShortlyAfterFrame) {
+  ZeroHertzFieldTrialEnabler enabler;
+  MockCallback callback;
+  GlobalSimulatedTimeController time_controller(Timestamp::Millis(0));
+  auto adapter = CreateAdapter(time_controller.GetClock());
+  adapter->Initialize(&callback);
+  adapter->SetZeroHertzModeEnabled(
+      FrameCadenceAdapterInterface::ZeroHertzModeParams{});
+  adapter->OnConstraintsChanged(VideoTrackSourceConstraints{0, 10});
+  adapter->OnFrame(CreateFrame());
+  time_controller.AdvanceTime(TimeDelta::Zero());
+  EXPECT_FALSE(adapter->ProcessKeyFrameRequest());
+}
+
+TEST(FrameCadenceAdapterTest, IgnoresKeyFrameRequestWhileShortRepeating) {
+  ZeroHertzFieldTrialEnabler enabler;
+  MockCallback callback;
+  GlobalSimulatedTimeController time_controller(Timestamp::Millis(0));
+  auto adapter = CreateAdapter(time_controller.GetClock());
+  adapter->Initialize(&callback);
+  adapter->SetZeroHertzModeEnabled(
+      FrameCadenceAdapterInterface::ZeroHertzModeParams{
+          /*num_simulcast_layers=*/1});
+  adapter->OnConstraintsChanged(VideoTrackSourceConstraints{0, 10});
+  time_controller.AdvanceTime(TimeDelta::Zero());
+  adapter->UpdateLayerStatus(0, true);
+  adapter->OnFrame(CreateFrame());
+  time_controller.AdvanceTime(TimeDelta::Millis(200));
+  EXPECT_FALSE(adapter->ProcessKeyFrameRequest());
+
+  // Expect repeating as ususal.
+  EXPECT_CALL(callback, OnFrame).Times(8);
+  time_controller.AdvanceTime(TimeDelta::Millis(800));
+}
+
+TEST(FrameCadenceAdapterTest, IgnoresKeyFrameRequestJustBeforeIdleRepeating) {
+  ZeroHertzFieldTrialEnabler enabler;
+  MockCallback callback;
+  GlobalSimulatedTimeController time_controller(Timestamp::Millis(0));
+  auto adapter = CreateAdapter(time_controller.GetClock());
+  adapter->Initialize(&callback);
+  adapter->SetZeroHertzModeEnabled(
+      FrameCadenceAdapterInterface::ZeroHertzModeParams{});
+  adapter->OnConstraintsChanged(VideoTrackSourceConstraints{0, 10});
+  time_controller.AdvanceTime(TimeDelta::Zero());
+  adapter->OnFrame(CreateFrame());
+  time_controller.AdvanceTime(TimeDelta::Millis(1000));
+
+  // We process the key frame request 100 ms before the first idle repeat should
+  // happen. The repeat should happen at T=1100 ms as originally expected.
+  EXPECT_FALSE(adapter->ProcessKeyFrameRequest());
+  EXPECT_CALL(callback, OnFrame);
+  time_controller.AdvanceTime(TimeDelta::Millis(100));
+  EXPECT_CALL(callback, OnFrame);
+  time_controller.AdvanceTime(TimeDelta::Millis(1000));
+}
+
+TEST(FrameCadenceAdapterTest,
+     IgnoresKeyFrameRequestShortRepeatsBeforeIdleRepeat) {
+  ZeroHertzFieldTrialEnabler enabler;
+  MockCallback callback;
+  GlobalSimulatedTimeController time_controller(Timestamp::Millis(0));
+  auto adapter = CreateAdapter(time_controller.GetClock());
+  adapter->Initialize(&callback);
+  adapter->SetZeroHertzModeEnabled(
+      FrameCadenceAdapterInterface::ZeroHertzModeParams{});
+  adapter->OnConstraintsChanged(VideoTrackSourceConstraints{0, 10});
+  time_controller.AdvanceTime(TimeDelta::Zero());
+  adapter->OnFrame(CreateFrame());
+  time_controller.AdvanceTime(TimeDelta::Millis(200));
+
+  // We process the key frame request 900 ms before the first idle repeat should
+  // happen. We should get a short repeat in 100 ms and an idle repeat after
+  // that.
+  EXPECT_FALSE(adapter->ProcessKeyFrameRequest());
+  EXPECT_CALL(callback, OnFrame);
+  time_controller.AdvanceTime(TimeDelta::Millis(100));
+  EXPECT_CALL(callback, OnFrame);
+  time_controller.AdvanceTime(TimeDelta::Millis(1000));
+}
+
 class ZeroHertzLayerQualityConvergenceTest : public ::testing::Test {
  public:
   static constexpr TimeDelta kMinFrameDelay = TimeDelta::Millis(100);

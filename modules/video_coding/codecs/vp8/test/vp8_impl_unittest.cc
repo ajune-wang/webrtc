146e48dac516f8d19f46409f0721988147d883c9
Sergey Silkin <ssilkin@webrtc.org>
2021-12-22 12:06:43 +0100

    Disable frame dropping from codec settings.
    This was broken in https://webrtc-review.googlesource.com/c/src/+/99062/
    
    Bug: webrtc:9734
    Change-Id: Ibce41a732cb2e943354c87fbb05be0dd218acf27

--- modules/video_coding/codecs/vp8/test/vp8_impl_unittest.cc
+++ modules/video_coding/codecs/vp8/test/vp8_impl_unittest.cc
@@ -76,9 +76,6 @@ class TestVp8Impl : public VideoCodecUnitTest {
     webrtc::test::CodecSettings(kVideoCodecVP8, codec_settings);
     codec_settings->width = kWidth;
     codec_settings->height = kHeight;
-    codec_settings->VP8()->denoisingOn = true;
-    codec_settings->VP8()->frameDroppingOn = false;
-    codec_settings->VP8()->automaticResizeOn = false;
     codec_settings->VP8()->complexity = VideoCodecComplexity::kComplexityNormal;
   }
 
@@ -408,7 +405,6 @@ TEST_F(TestVp8Impl, EncoderWith2TemporalLayers) {
 }
 
 TEST_F(TestVp8Impl, ScalingDisabledIfAutomaticResizeOff) {
-  codec_settings_.VP8()->frameDroppingOn = true;
   codec_settings_.VP8()->automaticResizeOn = false;
   EXPECT_EQ(WEBRTC_VIDEO_CODEC_OK,
             encoder_->InitEncode(&codec_settings_, kSettings));
@@ -419,7 +415,6 @@ TEST_F(TestVp8Impl, ScalingDisabledIfAutomaticResizeOff) {
 }
 
 TEST_F(TestVp8Impl, ScalingEnabledIfAutomaticResizeOn) {
-  codec_settings_.VP8()->frameDroppingOn = true;
   codec_settings_.VP8()->automaticResizeOn = true;
   EXPECT_EQ(WEBRTC_VIDEO_CODEC_OK,
             encoder_->InitEncode(&codec_settings_, kSettings));
@@ -440,7 +435,7 @@ TEST_F(TestVp8Impl, DontDropKeyframes) {
 
   // Screensharing has the internal frame dropper off, and instead per frame
   // asks ScreenshareLayers to decide if it should be dropped or not.
-  codec_settings_.VP8()->frameDroppingOn = false;
+  // codec_settings_.VP8()->frameDroppingOn = false;
   codec_settings_.mode = VideoCodecMode::kScreensharing;
   // ScreenshareLayers triggers on 2 temporal layers and 1000kbps max bitrate.
   codec_settings_.VP8()->numberOfTemporalLayers = 2;

e18a625c20012245aaba3f025874abbea3039a9d
Austin Orion <auorion@microsoft.com>
2022-01-06 15:24:10 -0800

    Avoid container-overflow in WgcCaptureSession::GetFrame
    ASAN is throwing a container-overflow because we are accessing a region
    in the vector that is valid but does not have an element. We can avoid
    this by using a std::unique_ptr<uint8_t[]> instead.
    
    See the documentation for container-overflow for more details:
    https://github.com/google/sanitizers/wiki/AddressSanitizerContainerOverflow#:~:text=One%20kind%20of%20bugs%20that%20AddressSanitizer%20can%20find,outside%20of%20the%20current%20container%20bounds.%20Simplest%20example%3A
    
    Fixed: 13541
    Change-Id: Id11def90ef8c2cfec9c20f38384547ce6c37b980

--- modules/desktop_capture/win/wgc_capture_session.cc
+++ modules/desktop_capture/win/wgc_capture_session.cc
@@ -289,9 +289,8 @@ HRESULT WgcCaptureSession::GetFrame(
   // Make a copy of the data pointed to by `map_info.pData` so we are free to
   // unmap our texture.
   uint8_t* src_data = static_cast<uint8_t*>(map_info.pData);
-  std::vector<uint8_t> image_data;
-  image_data.reserve(image_height * row_data_length);
-  uint8_t* image_data_ptr = image_data.data();
+  auto image_data = std::make_unique<uint8_t[]>(image_height * row_data_length);
+  uint8_t* image_data_ptr = image_data.get();
   for (int i = 0; i < image_height; i++) {
     memcpy(image_data_ptr, src_data, row_data_length);
     image_data_ptr += row_data_length;

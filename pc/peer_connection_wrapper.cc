4bf07ca061040ffc5452673d87ca0481e489794b
Niels Möller <nisse@webrtc.org>
2022-01-26 16:04:15 +0100

    New PeerConnectionFactory::CreateVideoTrack with refcounted source
    Bug: None
    Change-Id: I04c794d8959583bb4cc5c3898f4175783ec49f16

--- pc/peer_connection_wrapper.cc
+++ pc/peer_connection_wrapper.cc
@@ -279,7 +279,7 @@ rtc::scoped_refptr<AudioTrackInterface> PeerConnectionWrapper::CreateAudioTrack(
 
 rtc::scoped_refptr<VideoTrackInterface> PeerConnectionWrapper::CreateVideoTrack(
     const std::string& label) {
-  return pc_factory()->CreateVideoTrack(label, FakeVideoTrackSource::Create());
+  return pc_factory()->CreateVideoTrack(FakeVideoTrackSource::Create(), label);
 }
 
 rtc::scoped_refptr<RtpSenderInterface> PeerConnectionWrapper::AddTrack(

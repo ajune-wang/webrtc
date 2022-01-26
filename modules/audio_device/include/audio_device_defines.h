814a5bc939741aaf64cf328284df2500a536780d
Olov Brändström <brandstrom@google.com>
2022-01-26 15:52:53 +0100

    Add timestamps to AudioDeviceBuffer::SetRecordedBuffer
    Add timestamps to the function AudioDeviceBuffer::SetRecordedBuffer. This will
    be used to store audio timestaps in future changes.
    
    This is a part of the A/V sync metric metric feature for mobile. The metric
    have already launched for web clients.
    
    Bug: webrtc:13609
    Change-Id: I0031843476ff1b573b262308fca52d587fae30b7

--- modules/audio_device/include/audio_device_defines.h
+++ modules/audio_device/include/audio_device_defines.h
@@ -33,6 +33,7 @@ static const int kAdmMaxPlayoutBufferSizeMs = 250;
 
 class AudioTransport {
  public:
+  // TODO(brandstrom) Depricate
   virtual int32_t RecordedDataIsAvailable(const void* audioSamples,
                                           size_t nSamples,
                                           size_t nBytesPerSample,
@@ -43,6 +44,19 @@ class AudioTransport {
                                           uint32_t currentMicLevel,
                                           bool keyPressed,
                                           uint32_t& newMicLevel) = 0;  // NOLINT
+
+  virtual int32_t RecordedDataIsAvailable(
+      const void* audioSamples,
+      size_t nSamples,
+      size_t nBytesPerSample,
+      size_t nChannels,
+      uint32_t samplesPerSec,
+      uint32_t totalDelayMS,
+      int32_t clockDrift,
+      uint32_t currentMicLevel,
+      bool keyPressed,
+      uint32_t& newMicLevel,
+      int64_t estimated_capture_time_ns) = 0;  // NOLINT
 
   // Implementation has to setup safe values for all specified out parameters.
   virtual int32_t NeedMorePlayData(size_t nSamples,

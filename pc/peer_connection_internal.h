c949b627a75eee0ecf3d8323a7e484b070e27494
Harald Alvestrand <hta@webrtc.org>
2022-01-28 11:10:47 +0000

    Refactor PeerConnectionInternal to break SdpOfferAnswer dependency
    This CL changes the SdpOfferAnswerHandler class to depend on a new class
    PerConnectionInternalMethods, which is implemented by PeerConnection.
    This means that SdpOfferAnswerHandler no longer depends on
    PeerConnectionInterface.
    
    This opens the way for refactoring PeerConnection so that
    PeerConnectionInternalMethods is a member object (encapsulation not
    inheritance), which will make it possible to break some of the
    dependency cycles that make the "peerconnection" target in the BUILD
    file so huge.
    
    Bug: webrtc:11995
    Change-Id: Ib8413a31c0148b8d8602764b7367dfd3068da58a

--- pc/peer_connection_internal.h
+++ pc/peer_connection_internal.h
@@ -19,14 +19,26 @@
 
 #include "api/peer_connection_interface.h"
 #include "call/call.h"
+#include "pc/data_channel_controller.h"
+#include "pc/jsep_transport_controller.h"
+#include "pc/peer_connection_message_handler.h"
 #include "pc/rtp_transceiver.h"
+#include "pc/rtp_transmission_manager.h"
 #include "pc/sctp_data_channel.h"
 
 namespace webrtc {
 
+class StatsCollector;
+
 // Internal interface for extra PeerConnection methods.
-class PeerConnectionInternal : public PeerConnectionInterface {
+class PeerConnectionInternalMethods {
  public:
+  virtual ~PeerConnectionInternalMethods() = default;
+
+  // NOTE - signaling_thread() is a member of PeerConnectionInterface,
+  // so we have to use a different name for this function as long as
+  // PeerConnection is a subclass of PeerConnectionInternalMethods.
+  virtual rtc::Thread* signaling_thread_internal() const = 0;
   virtual rtc::Thread* network_thread() const = 0;
   virtual rtc::Thread* worker_thread() const = 0;
 
@@ -80,7 +92,84 @@ class PeerConnectionInternal : public PeerConnectionInterface {
   // Get SSL role for an arbitrary m= section (handles bundling correctly).
   virtual bool GetSslRole(const std::string& content_name,
                           rtc::SSLRole* role) = 0;
+  // Return a pointer to the active configuration.
+  virtual const PeerConnectionInterface::RTCConfiguration* configuration()
+      const = 0;
+
+  // Report the UMA metric SdpFormatReceived for the given remote description.
+  virtual void ReportSdpFormatReceived(
+      const SessionDescriptionInterface& remote_description) = 0;
+
+  // Report the UMA metric BundleUsage for the given remote description.
+  virtual void ReportSdpBundleUsage(
+      const SessionDescriptionInterface& remote_description) = 0;
+
+  virtual PeerConnectionMessageHandler* message_handler() = 0;
+  virtual RtpTransmissionManager* rtp_manager() = 0;
+  virtual const RtpTransmissionManager* rtp_manager() const = 0;
+  virtual bool dtls_enabled() const = 0;
+  virtual const PeerConnectionFactoryInterface::Options* options() const = 0;
+
+  // Returns the CryptoOptions for this PeerConnection. This will always
+  // return the RTCConfiguration.crypto_options if set and will only default
+  // back to the PeerConnectionFactory settings if nothing was set.
+  virtual CryptoOptions GetCryptoOptions() = 0;
+  virtual cricket::ChannelManager* channel_manager() = 0;
+  virtual JsepTransportController* transport_controller() = 0;
+  virtual DataChannelController* data_channel_controller() = 0;
+  virtual cricket::PortAllocator* port_allocator() = 0;
+  virtual StatsCollector* stats() = 0;
+  // Returns the observer. Will crash on CHECK if the observer is removed.
+  virtual PeerConnectionObserver* Observer() const = 0;
+  virtual bool GetSctpSslRole(rtc::SSLRole* role) = 0;
+  virtual PeerConnectionInterface::IceConnectionState
+  ice_connection_state_internal() = 0;
+  virtual void SetIceConnectionState(
+      PeerConnectionInterface::IceConnectionState new_state) = 0;
+  virtual void NoteUsageEvent(UsageEvent event) = 0;
+  virtual bool IsClosed() const = 0;
+  // Returns true if the PeerConnection is configured to use Unified Plan
+  // semantics for creating offers/answers and setting local/remote
+  // descriptions. If this is true the RtpTransceiver API will also be available
+  // to the user. If this is false, Plan B semantics are assumed.
+  // TODO(bugs.webrtc.org/8530): Flip the default to be Unified Plan once
+  // sufficient time has passed.
+  virtual bool IsUnifiedPlan() const = 0;
+  virtual bool ValidateBundleSettings(
+      const cricket::SessionDescription* desc,
+      const std::map<std::string, const cricket::ContentGroup*>&
+          bundle_groups_by_mid) = 0;
+
+  virtual absl::optional<std::string> GetDataMid() const = 0;
+  // Internal implementation for AddTransceiver family of methods. If
+  // `fire_callback` is set, fires OnRenegotiationNeeded callback if successful.
+  virtual RTCErrorOr<rtc::scoped_refptr<RtpTransceiverInterface>>
+  AddTransceiver(cricket::MediaType media_type,
+                 rtc::scoped_refptr<MediaStreamTrackInterface> track,
+                 const RtpTransceiverInit& init,
+                 bool fire_callback = true) = 0;
+  // Asynchronously calls SctpTransport::Start() on the network thread for
+  // `sctp_mid()` if set. Called as part of setting the local description.
+  virtual void StartSctpTransport(int local_port,
+                                  int remote_port,
+                                  int max_message_size) = 0;
+
+  // Asynchronously adds a remote candidate on the network thread.
+  virtual void AddRemoteCandidate(const std::string& mid,
+                                  const cricket::Candidate& candidate) = 0;
+
+  virtual Call* call_ptr() = 0;
+  // Returns true if SRTP (either using DTLS-SRTP or SDES) is required by
+  // this session.
+  virtual bool SrtpRequired() const = 0;
+  virtual bool SetupDataChannelTransport_n(const std::string& mid) = 0;
+  virtual void TeardownDataChannelTransport_n() = 0;
+  virtual void SetSctpDataMid(const std::string& mid) = 0;
+  virtual void ResetSctpDataMid() = 0;
 };
+
+class PeerConnectionInternal : public PeerConnectionInterface,
+                               public PeerConnectionInternalMethods {};
 
 }  // namespace webrtc
 

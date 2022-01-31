4430976e161f35b6b05e2a802c9b7cc67db358a1
Tommi <tommi@webrtc.org>
2022-01-31 16:57:55 +0100

    Removing MessageHandler dependency from Connection.
    Bug: webrtc:11988
    Change-Id: Ic35bb5baeafbda7210012dceb0d6d5f5b3eb95c9

--- p2p/base/connection.cc
+++ p2p/base/connection.cc
@@ -833,15 +833,15 @@ void Connection::Prune() {
 
 void Connection::Destroy() {
   RTC_DCHECK_RUN_ON(network_thread_);
-  // TODO(deadbeef, nisse): This may leak if an application closes a
-  // PeerConnection and then quickly destroys the PeerConnectionFactory (along
-  // with the networking thread on which this message is posted). Also affects
-  // tests, with a workaround in
-  // AutoSocketServerThread::~AutoSocketServerThread.
-  RTC_LOG(LS_VERBOSE) << ToString() << ": Connection destroyed";
-  // TODO(bugs.webrtc.org/11988): Use PostTask.
-  port_->thread()->Post(RTC_FROM_HERE, this, MSG_DELETE);
+  RTC_DLOG(LS_VERBOSE) << ToString() << ": Connection destroyed";
+
+  SignalDestroyed(this);
+  SignalDestroyed.disconnect_all();
+
   LogCandidatePairConfig(webrtc::IceCandidatePairConfigType::kDestroyed);
+
+  network_thread_->PostTask(
+      webrtc::ToQueuedTask([me = std::unique_ptr<Connection>(this)]() {}));
 }
 
 void Connection::FailAndDestroy() {
@@ -1420,15 +1420,6 @@ void Connection::MaybeUpdatePeerReflexiveCandidate(
       remote_candidate_.generation() == new_candidate.generation()) {
     remote_candidate_ = new_candidate;
   }
-}
-
-void Connection::OnMessage(rtc::Message* pmsg) {
-  RTC_DCHECK_RUN_ON(network_thread_);
-  RTC_DCHECK(pmsg->message_id == MSG_DELETE);
-  RTC_LOG(LS_INFO) << "Connection deleted with number of pings sent: "
-                   << num_pings_sent_;
-  SignalDestroyed(this);
-  delete this;
 }
 
 int64_t Connection::last_received() const {

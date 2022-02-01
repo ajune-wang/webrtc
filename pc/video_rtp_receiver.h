b67b0948c276045e584a33d3f549c055dc1d74a8
Tommi <tommi@webrtc.org>
2022-02-02 00:43:46 +0100

    Move sender/receiver and channel destruction to the worker.
    Bug: none
    Change-Id: Ied61636c8ef09d782bf519524fff2a31e15219a8

--- pc/video_rtp_receiver.h
+++ pc/video_rtp_receiver.h
@@ -88,7 +88,7 @@ class VideoRtpReceiver : public RtpReceiverInternal {
 
   // RtpReceiverInternal implementation.
   void Stop() override;
-  void StopAndEndTrack() override;
+  void StopSource(bool end_track) override;
   void SetupMediaChannel(uint32_t ssrc) override;
   void SetupUnsignaledMediaChannel() override;
   uint32_t ssrc() const override;
@@ -110,8 +110,16 @@ class VideoRtpReceiver : public RtpReceiverInternal {
 
   std::vector<RtpSource> GetSources() const override;
 
+  // Combines SetMediaChannel, SetupMediaChannel and
+  // SetupUnsignaledMediaChannel.
+  void SetupMediaChannel(absl::optional<uint32_t> ssrc,
+                         cricket::MediaChannel* media_channel);
+
  private:
-  void RestartMediaChannel(absl::optional<uint32_t> ssrc);
+  void RestartMediaChannel(absl::optional<uint32_t> ssrc)
+      RTC_RUN_ON(&signaling_thread_checker_);
+  void RestartMediaChannel_w(absl::optional<uint32_t> ssrc)
+      RTC_RUN_ON(worker_thread_);
   void SetSink(rtc::VideoSinkInterface<VideoFrame>* sink)
       RTC_RUN_ON(worker_thread_);
   void SetMediaChannel_w(cricket::MediaChannel* media_channel)
@@ -160,7 +168,7 @@ class VideoRtpReceiver : public RtpReceiverInternal {
   // After that, `stopped_` will remain false until `Stop()` is called.
   // Note, for checking the state of the class on the worker thread,
   // check `media_channel_` instead, as that's the main worker thread state.
-  bool stopped_ RTC_GUARDED_BY(&signaling_thread_checker_) = true;
+  bool stopped_ RTC_GUARDED_BY(worker_thread_) = true;
   RtpReceiverObserverInterface* observer_
       RTC_GUARDED_BY(&signaling_thread_checker_) = nullptr;
   bool received_first_packet_ RTC_GUARDED_BY(&signaling_thread_checker_) =

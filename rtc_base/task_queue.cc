50f895a5a9a94b1f9444d37c012d8d9df4d778f1
Markus Handell <handellm@webrtc.org>
2022-02-02 10:53:05 +0100

    Proof-of-concept: avoid PostTask context switches if we can.
    Bug: None, yet
    Change-Id: I49dead84e8797fbcb3d5f46eec7f15158b368836

--- rtc_base/task_queue.cc
+++ rtc_base/task_queue.cc
@@ -12,6 +12,36 @@
 #include "api/task_queue/task_queue_base.h"
 
 namespace rtc {
+namespace {
+struct CurrentTaskQueueSetterExposer : public webrtc::TaskQueueBase {
+  using Setter = webrtc::TaskQueueBase::CurrentTaskQueueSetter;
+};
+
+}  // namespace
+
+TaskQueue::WrapperTask::WrapperTask(
+    std::unique_ptr<QueuedTask> task,
+    rtc::scoped_refptr<rtc::FinalRefCountedObject<TaskQueue::SharedState>>
+        shared_state)
+    : task_(std::move(task)), shared_state_(std::move(shared_state)) {}
+
+bool TaskQueue::WrapperTask::Run() {
+  webrtc::MutexLock lock(&shared_state_->task_mu);
+  if (!task_->Run())
+    (void)task_.release();
+  return true;
+}
+
+TaskQueue::RefDecrementingWrapperTask::RefDecrementingWrapperTask(
+    std::unique_ptr<QueuedTask> task,
+    rtc::scoped_refptr<rtc::FinalRefCountedObject<TaskQueue::SharedState>>
+        shared_state)
+    : WrapperTask(std::move(task), shared_state) {}
+
+TaskQueue::RefDecrementingWrapperTask::~RefDecrementingWrapperTask() {
+  webrtc::MutexLock lock(&shared_state_->mu);
+  shared_state_->value--;
+}
 
 TaskQueue::TaskQueue(
     std::unique_ptr<webrtc::TaskQueueBase, webrtc::TaskQueueDeleter> task_queue)
@@ -30,12 +60,43 @@ bool TaskQueue::IsCurrent() const {
 }
 
 void TaskQueue::PostTask(std::unique_ptr<webrtc::QueuedTask> task) {
-  return impl_->PostTask(std::move(task));
+  if (!fastpath_enabled_)
+    return impl_->PostTask(std::move(task));
+
+  shared_state_->mu.Lock();
+  if (shared_state_->value != 0) {
+    // Slow path: Queued tasks present. Post normally.
+    shared_state_->value++;
+    shared_state_->mu.Unlock();
+    impl_->PostTask(std::make_unique<RefDecrementingWrapperTask>(
+        std::move(task), shared_state_));
+    return;
+  }
+
+  // Fast path: no queued tasks.
+  shared_state_->value = 1;
+  shared_state_->task_mu.Lock();
+  shared_state_->mu.Unlock();
+  {
+    CurrentTaskQueueSetterExposer::Setter setter(impl_);
+    if (!task->Run())
+      (void)task.release();
+  }
+
+  shared_state_->mu.Lock();
+  shared_state_->task_mu.Unlock();
+  // Value might have increased in the unlocked region above.
+  shared_state_->value--;
+  shared_state_->mu.Unlock();
 }
 
 void TaskQueue::PostDelayedTask(std::unique_ptr<webrtc::QueuedTask> task,
                                 uint32_t milliseconds) {
-  return impl_->PostDelayedTask(std::move(task), milliseconds);
+  if (!fastpath_enabled_)
+    return impl_->PostDelayedTask(std::move(task), milliseconds);
+  return impl_->PostDelayedTask(
+      std::make_unique<WrapperTask>(std::move(task), shared_state_),
+      milliseconds);
 }
 
 }  // namespace rtc

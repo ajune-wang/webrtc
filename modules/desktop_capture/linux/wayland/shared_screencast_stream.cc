360bfa1d8cc472e38d56f23ddcb96574e5d51864
Jan Grulich <grulja@gmail.com>
2022-02-04 12:27:12 +0100

    PipeWire capturer: Import DMA-BUFs with correct render node
    With more GPUs available it might happen that it used different render
    node from the one we pick from the list. This would cause DMA-BUF to
    fail to import so we use Wayland client library to obtain wl_display in
    order to initialize EGLDisplay using same render node and have previous
    approach as a fallback. Also everyone else uses EGL_LINUX_DMA_BUF_EXT
    target for importing EGLImages from DMA-BUF file descriptors so use it
    as well to be sure we import buffers same way as they are produced.
    
    Bug: chromium:1290566
    Bug: webrtc:13429
    Change-Id: I32bbb0bdb28c08b6e7fcb3f94009f82a2041b6ee

--- modules/desktop_capture/linux/wayland/shared_screencast_stream.cc
+++ modules/desktop_capture/linux/wayland/shared_screencast_stream.cc
@@ -35,6 +35,7 @@
 using modules_desktop_capture_linux_wayland::InitializeStubs;
 using modules_desktop_capture_linux_wayland::kModuleDrm;
 using modules_desktop_capture_linux_wayland::kModulePipewire;
+using modules_desktop_capture_linux_wayland::kModuleWayland_client;
 using modules_desktop_capture_linux_wayland::StubPathMap;
 #endif  // defined(WEBRTC_DLOPEN_PIPEWIRE)
 
@@ -45,6 +46,7 @@ const int kBytesPerPixel = 4;
 #if defined(WEBRTC_DLOPEN_PIPEWIRE)
 const char kPipeWireLib[] = "libpipewire-0.3.so.0";
 const char kDrmLib[] = "libdrm.so.2";
+const char kWayland_clientLib[] = "libwayland-client.so";
 #endif
 
 #if !PW_CHECK_VERSION(0, 3, 29)
@@ -100,21 +102,28 @@ spa_pod* BuildFormat(spa_pod_builder* builder,
   spa_pod_builder_add(builder, SPA_FORMAT_VIDEO_format, SPA_POD_Id(format), 0);
 
   if (modifiers.size()) {
-    spa_pod_builder_prop(
-        builder, SPA_FORMAT_VIDEO_modifier,
-        SPA_POD_PROP_FLAG_MANDATORY | SPA_POD_PROP_FLAG_DONT_FIXATE);
-    spa_pod_builder_push_choice(builder, &frames[1], SPA_CHOICE_Enum, 0);
-    // modifiers from the array
-    for (int64_t val : modifiers) {
-      spa_pod_builder_long(builder, val);
-      // Add the first modifier twice as the very first value is the default
-      // option
-      if (first) {
+    if (modifiers.size() == 1 && modifiers[0] == DRM_FORMAT_MOD_INVALID) {
+      spa_pod_builder_prop(builder, SPA_FORMAT_VIDEO_modifier,
+                           SPA_POD_PROP_FLAG_MANDATORY);
+      spa_pod_builder_long(builder, modifiers[0]);
+    } else {
+      spa_pod_builder_prop(
+          builder, SPA_FORMAT_VIDEO_modifier,
+          SPA_POD_PROP_FLAG_MANDATORY | SPA_POD_PROP_FLAG_DONT_FIXATE);
+      spa_pod_builder_push_choice(builder, &frames[1], SPA_CHOICE_Enum, 0);
+
+      // modifiers from the array
+      for (int64_t val : modifiers) {
         spa_pod_builder_long(builder, val);
-        first = false;
+        // Add the first modifier twice as the very first value is the default
+        // option
+        if (first) {
+          spa_pod_builder_long(builder, val);
+          first = false;
+        }
       }
+      spa_pod_builder_pop(builder, &frames[1]);
     }
-    spa_pod_builder_pop(builder, &frames[1]);
   }
 
   spa_pod_builder_add(
@@ -431,6 +440,8 @@ bool SharedScreenCastStreamPrivate::StartScreenCastStream(
   // Check if the PipeWire and DRM libraries are available.
   paths[kModulePipewire].push_back(kPipeWireLib);
   paths[kModuleDrm].push_back(kDrmLib);
+  paths[kModuleWayland_client].push_back(kWayland_clientLib);
+
   if (!InitializeStubs(paths)) {
     RTC_LOG(LS_ERROR) << "Failed to load the PipeWire library and symbols.";
     return false;

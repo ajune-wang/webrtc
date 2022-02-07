dd5df27866d633d67e784744c5d011d3280ae6a1
Harald Alvestrand <hta@webrtc.org>
2022-02-07 13:31:13 +0000

    Break out webrtc_sdp from peerconnection target
    Bug: webrtc:13634
    Change-Id: Ia3a9a73350dbb4a847b03a66ae480e7c311750a9

---
+++ pc/jsep_session_description.h
@@ -0,0 +1,16 @@
+/*
+ *  Copyright 2012 The WebRTC project authors. All Rights Reserved.
+ *
+ *  Use of this source code is governed by a BSD-style license
+ *  that can be found in the LICENSE file in the root of the source
+ *  tree. An additional intellectual property rights grant can be found
+ *  in the file PATENTS.  All contributing project authors may
+ *  be found in the AUTHORS file in the root of the source tree.
+ */
+
+#ifndef PC_JSEP_SESSION_DESCRIPTION_H_
+#define PC_JSEP_SESSION_DESCRIPTION_H_
+
+#include "api/jsep_session_description.h"
+
+#endif  // PC_JSEP_SESSION_DESCRIPTION_H_

901c0ea75ecf1a8e087f27edde27e55828099f97
Sergey Silkin <ssilkin@webrtc.org>
2022-02-18 10:03:31 +0100

    Avoid frame dups in ping-poing mode
    YUV reader returned last and first frames twice in ping-pong mode before
    this change.
    
    Bug: none
    Change-Id: Ifa0686839bbea9ab8151d71835eca4ed2b31b6e4

--- test/testsupport/yuv_frame_reader.cc
+++ test/testsupport/yuv_frame_reader.cc
@@ -75,7 +75,8 @@ YuvFrameReaderImpl::YuvFrameReaderImpl(std::string input_filename,
       frame_size_bytes_(FrameSizeBytes(input_width, input_height)),
       repeat_mode_(repeat_mode),
       number_of_frames_(-1),
-      current_frame_index_(-1),
+      next_frame_index_(0),
+      prev_frame_index_(-1),
       dropper_(clip_fps.has_value() ? new DropperUtil(*clip_fps, target_fps)
                                     : nullptr),
       input_file_(nullptr) {}
@@ -104,7 +105,8 @@ bool YuvFrameReaderImpl::Init() {
   }
   number_of_frames_ =
       static_cast<int>(source_file_size / frame_length_in_bytes_);
-  current_frame_index_ = 0;
+  next_frame_index_ = 0;
+  prev_frame_index_ = -1;
   return true;
 }
 
@@ -118,27 +120,30 @@ rtc::scoped_refptr<I420Buffer> YuvFrameReaderImpl::ReadFrame() {
   rtc::scoped_refptr<I420Buffer> buffer;
 
   do {
-    if (current_frame_index_ >= number_of_frames_) {
+    if (next_frame_index_ == number_of_frames_) {
       switch (repeat_mode_) {
         case RepeatMode::kSingle:
           return nullptr;
         case RepeatMode::kRepeat:
-          fseek(input_file_, 0, SEEK_SET);
-          current_frame_index_ = 0;
+          next_frame_index_ = 0;
+          prev_frame_index_ = -1;
           break;
         case RepeatMode::kPingPong:
-          if (current_frame_index_ == number_of_frames_ * 2) {
-            fseek(input_file_, 0, SEEK_SET);
-            current_frame_index_ = 0;
-          } else {
-            int reverse_frame_index = current_frame_index_ - number_of_frames_;
-            int seek_frame_pos = (number_of_frames_ - reverse_frame_index - 1);
-            fseek(input_file_, seek_frame_pos * frame_size_bytes_, SEEK_SET);
-          }
+          next_frame_index_ = std::max(number_of_frames_ - 2, 0);
           break;
       }
+    } else if (next_frame_index_ == -1) {
+      RTC_DCHECK(repeat_mode_ == RepeatMode::kPingPong);
+      next_frame_index_ = std::min(number_of_frames_ - 1, 1);
     }
-    ++current_frame_index_;
+
+    if (next_frame_index_ == 0 || next_frame_index_ < prev_frame_index_) {
+      fseek(input_file_, next_frame_index_ * frame_size_bytes_, SEEK_SET);
+    }
+
+    int frame_index_incriment = prev_frame_index_ < next_frame_index_ ? 1 : -1;
+    prev_frame_index_ = next_frame_index_;
+    next_frame_index_ += frame_index_incriment;
 
     buffer = ReadI420Buffer(input_width_, input_height_, input_file_);
     if (!buffer && ferror(input_file_)) {

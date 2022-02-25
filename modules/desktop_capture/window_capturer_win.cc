e2521773f4d360eceaa91e3187a86e098137b846
Sunggook Chue <sunggch@microsoft.com>
2022-02-25 15:53:27 -0800

    getDisplayMedia shows black window on Youtube PiP in Windows.
    getDisplayMedia capture the view of the screens and windows
    in the capture dialog, but the issue is that captured view
    of the Youtube somehow is blank. It repros only in certain
    circumstances, for example, Canary channel.
    If user reinstall the Canary as fresh new, we observed that
    it doesn't repro.
    
    Cause:
    We aren't sure what's cause of this one yet.
    
    Solution:
    We decided to provide fallback WGC capturer when the main
    capturer (GDI) shows blank. WGC could show yellow outline
    in prior Win11 OS, but yellow outline looks better than
    blank.
    
    The blank detector and fallback capturer are what screen capturer
    already supported so the solution will follow similar
    pattern in the window capturer.
    
    Bug:13726
    Change-Id: I620c817d259d7bb5c295adab11c4444349ab1c6c

--- modules/desktop_capture/window_capturer_win.cc
+++ modules/desktop_capture/window_capturer_win.cc
@@ -12,12 +12,33 @@
 #include "modules/desktop_capture/desktop_capturer.h"
 #include "modules/desktop_capture/win/window_capturer_win_gdi.h"
 
+#if defined(RTC_ENABLE_WIN_WGC)
+#include "modules/desktop_capture/blank_detector_desktop_capturer_wrapper.h"
+#include "modules/desktop_capture/fallback_desktop_capturer_wrapper.h"
+#include "modules/desktop_capture/win/wgc_capturer_win.h"
+#include "rtc_base/win/windows_version.h"
+#endif  // defined(RTC_ENABLE_WIN_WGC)
+
 namespace webrtc {
 
 // static
 std::unique_ptr<DesktopCapturer> DesktopCapturer::CreateRawWindowCapturer(
     const DesktopCaptureOptions& options) {
-  return WindowCapturerWinGdi::CreateRawWindowCapturer(options);
+  std::unique_ptr<DesktopCapturer> capturer(
+      WindowCapturerWinGdi::CreateRawWindowCapturer(options));
+#if defined(RTC_ENABLE_WIN_WGC)
+  if (rtc::rtc_win::GetVersion() >= rtc::rtc_win::Version::VERSION_WIN10_RS5) {
+    // BlankDectector capturer will send an error when it detects a failed
+    // GDI rendering, then Fallback capturer will try to capture it again with
+    // WGC.
+    capturer.reset(new BlankDetectorDesktopCapturerWrapper(
+        std::move(capturer), RgbaColor(0, 0, 0, 0)));
+
+    capturer.reset(new FallbackDesktopCapturerWrapper(
+        std::move(capturer), WgcCapturerWin::CreateRawWindowCapturer(options)));
+  }
+#endif  // defined(RTC_ENABLE_WIN_WGC)
+  return capturer;
 }
 
 }  // namespace webrtc

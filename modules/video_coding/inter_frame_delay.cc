d4b447a5efb267ce78696af575ed6db1e365c291
Evan Shrubsole <eshr@webrtc.org>
2022-03-04 11:54:48 +0100

    Migrate VCMInterFrameDelay to use Time units
    Additionally,
    * Moved to its own GN target.
    * Added unittests.
    * Removed unused variable `_zeroWallClock`.
    * Renamed variables to match style guide.
    * Moved fields _dTS and _wrapArounds to variables.
    
    Change-Id: I7aa8b8dec55abab49ceabe838dabf2a7e13d685d
    Bug: webrtc:13756

--- modules/video_coding/inter_frame_delay.cc
+++ modules/video_coding/inter_frame_delay.cc
@@ -10,85 +10,86 @@
 
 #include "modules/video_coding/inter_frame_delay.h"
 
+#include "absl/types/optional.h"
+#include "api/units/frequency.h"
+#include "api/units/time_delta.h"
+
 namespace webrtc {
 
-VCMInterFrameDelay::VCMInterFrameDelay(int64_t currentWallClock) {
-  Reset(currentWallClock);
+namespace {
+constexpr Frequency k90kHz = Frequency::KiloHertz(90);
+}
+
+VCMInterFrameDelay::VCMInterFrameDelay() {
+  Reset();
 }
 
 // Resets the delay estimate.
-void VCMInterFrameDelay::Reset(int64_t currentWallClock) {
-  _zeroWallClock = currentWallClock;
-  _wrapArounds = 0;
-  _prevWallClock = 0;
-  _prevTimestamp = 0;
-  _dTS = 0;
+void VCMInterFrameDelay::Reset() {
+  prev_wall_clock_ = absl::nullopt;
+  prev_rtp_timestamp_ = 0;
 }
 
 // Calculates the delay of a frame with the given timestamp.
 // This method is called when the frame is complete.
-bool VCMInterFrameDelay::CalculateDelay(uint32_t timestamp,
-                                        int64_t* delay,
-                                        int64_t currentWallClock) {
-  if (_prevWallClock == 0) {
+absl::optional<TimeDelta> VCMInterFrameDelay::CalculateDelay(
+    uint32_t rtp_timestamp,
+    Timestamp now) {
+  if (!prev_wall_clock_) {
     // First set of data, initialization, wait for next frame.
-    _prevWallClock = currentWallClock;
-    _prevTimestamp = timestamp;
-    *delay = 0;
-    return true;
+    prev_wall_clock_ = now;
+    prev_rtp_timestamp_ = rtp_timestamp;
+    return TimeDelta::Zero();
   }
 
-  int32_t prevWrapArounds = _wrapArounds;
-  CheckForWrapArounds(timestamp);
-
   // This will be -1 for backward wrap arounds and +1 for forward wrap arounds.
-  int32_t wrapAroundsSincePrev = _wrapArounds - prevWrapArounds;
+  int32_t wrap_arounds_since_prev = CheckForWrapArounds(rtp_timestamp);
 
   // Account for reordering in jitter variance estimate in the future?
   // Note that this also captures incomplete frames which are grabbed for
   // decoding after a later frame has been complete, i.e. real packet losses.
-  if ((wrapAroundsSincePrev == 0 && timestamp < _prevTimestamp) ||
-      wrapAroundsSincePrev < 0) {
-    *delay = 0;
-    return false;
+  if ((wrap_arounds_since_prev == 0 && rtp_timestamp < prev_rtp_timestamp_) ||
+      wrap_arounds_since_prev < 0) {
+    return absl::nullopt;
   }
 
-  // Compute the compensated timestamp difference and convert it to ms and round
-  // it to closest integer.
-  _dTS = static_cast<int64_t>(
-      (timestamp + wrapAroundsSincePrev * (static_cast<int64_t>(1) << 32) -
-       _prevTimestamp) /
-          90.0 +
-      0.5);
+  // Compute the compensated timestamp difference.
+  int64_t d_rtp_ticks =
+      rtp_timestamp +
+      wrap_arounds_since_prev * (static_cast<int64_t>(1) << 32) -
+      prev_rtp_timestamp_;
+  TimeDelta dts = d_rtp_ticks / k90kHz;
+  TimeDelta dt = now - *prev_wall_clock_;
 
   // frameDelay is the difference of dT and dTS -- i.e. the difference of the
   // wall clock time difference and the timestamp difference between two
   // following frames.
-  *delay = static_cast<int64_t>(currentWallClock - _prevWallClock - _dTS);
+  TimeDelta delay = dt - dts;
 
-  _prevTimestamp = timestamp;
-  _prevWallClock = currentWallClock;
-
-  return true;
+  prev_rtp_timestamp_ = rtp_timestamp;
+  prev_wall_clock_ = now;
+  return delay;
 }
 
 // Investigates if the timestamp clock has overflowed since the last timestamp
 // and keeps track of the number of wrap arounds since reset.
-void VCMInterFrameDelay::CheckForWrapArounds(uint32_t timestamp) {
-  if (timestamp < _prevTimestamp) {
+int VCMInterFrameDelay::CheckForWrapArounds(uint32_t rtp_timestamp) const {
+  if (rtp_timestamp < prev_rtp_timestamp_) {
     // This difference will probably be less than -2^31 if we have had a wrap
     // around (e.g. timestamp = 1, _prevTimestamp = 2^32 - 1). Since it is cast
     // to a int32_t, it should be positive.
-    if (static_cast<int32_t>(timestamp - _prevTimestamp) > 0) {
+    if (static_cast<int32_t>(rtp_timestamp - prev_rtp_timestamp_) > 0) {
       // Forward wrap around.
-      _wrapArounds++;
+      return 1;
     }
     // This difference will probably be less than -2^31 if we have had a
     // backward wrap around. Since it is cast to a int32_t, it should be
     // positive.
-  } else if (static_cast<int32_t>(_prevTimestamp - timestamp) > 0) {
+  } else if (static_cast<int32_t>(prev_rtp_timestamp_ - rtp_timestamp) > 0) {
     // Backward wrap around.
-    _wrapArounds--;
+    return -1;
   }
+  return 0;
 }
+
 }  // namespace webrtc

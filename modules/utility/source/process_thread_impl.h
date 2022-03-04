f58c15f70ae12c6d38405659e4e94b97e1454d60
Jianhui Dai <jianhui.j.dai@intel.com>
2022-03-04 14:43:56 +0800

    Ensure FIFO order for delayed tasks in `ProcessThreadImpl`
    TaskQueue posts delayed task in milliseconds precision. If delayed tasks
    have the same wakeup time in queue, we should ensure they are waked up
    in FIFO order. E.g., call `PostDelayedTask(task-i, 0)` in a loop, we
    expect `task-i` is waked up as enqueue order.
    
    Co-Author: jiahe.zhang@intel.com
    Bug: webrtc:13761
    Change-Id: I3bc87c2d251f8dffee868a012e828fd42e783afc

--- modules/utility/source/process_thread_impl.h
+++ modules/utility/source/process_thread_impl.h
@@ -65,14 +65,18 @@ class ProcessThreadImpl : public ProcessThread {
     ModuleCallback& operator=(ModuleCallback&);
   };
   struct DelayedTask {
-    DelayedTask(int64_t run_at_ms, std::unique_ptr<QueuedTask> task)
-        : run_at_ms(run_at_ms), task(task.release()) {}
+    DelayedTask(int64_t run_at_ms,
+                uint64_t seq_id,
+                std::unique_ptr<QueuedTask> task)
+        : run_at_ms(run_at_ms), seq_id_(seq_id), task(task.release()) {}
     friend bool operator<(const DelayedTask& lhs, const DelayedTask& rhs) {
       // Earliest DelayedTask should be at the top of the priority queue.
-      return lhs.run_at_ms > rhs.run_at_ms;
+      return lhs.run_at_ms > rhs.run_at_ms ||
+             (lhs.run_at_ms == rhs.run_at_ms && lhs.seq_id_ > rhs.seq_id_);
     }
 
     int64_t run_at_ms;
+    uint64_t seq_id_;
     // DelayedTask owns the `task`, but some delayed tasks must be removed from
     // the std::priority_queue, but mustn't be deleted. std::priority_queue does
     // not give non-const access to the values, so storing unique_ptr would
@@ -101,7 +105,10 @@ class ProcessThreadImpl : public ProcessThread {
   // Set to true when calling Process, to allow reentrant calls to WakeUp.
   bool holds_mutex_ RTC_GUARDED_BY(this) = false;
   std::queue<QueuedTask*> queue_;
+  // `std::priority_queue` does not guarantee stable sort. For delayed tasks
+  // with the same wakeup time, use `seq_id_` to ensure FIFO ordering.
   std::priority_queue<DelayedTask> delayed_tasks_ RTC_GUARDED_BY(mutex_);
+  uint64_t seq_id_ RTC_GUARDED_BY(mutex_) = 0;
   // The `stop_` flag is modified only by the construction thread, protected by
   // `thread_checker_`. It is read also by the spawned `thread_`. The latter
   // thread must take `mutex_` before access, and for thread safety, the

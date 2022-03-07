1ca5a0e0d7b4a3a1b14c4bc6835a78c35e5e095a
Niels Möller <nisse@webrtc.org>
2022-03-07 16:41:32 +0100

    Update some internal Create functions to use rtc::scoped_refptr arguments
    Fixed the ...ProxyWithInternal::Create functions, VideoTrack::Create
    and AudioTrack::Create.
    
    Also extend rtc::make_ref_counted templates to work correctly for
    types that inherit both rtc::RefCountedInterface and
    rtc::RefCountedObject.
    
    Bug: webrtc:13464
    Change-Id: I6508fbede2f447f8c0c9b37556d37e7fc2ccb744

--- pc/proxy.h
+++ pc/proxy.h
@@ -286,15 +286,15 @@ class ConstMethodCall : public QueuedTask {
   void DestroyInternal() { delete c_; }                         \
   INTERNAL_CLASS* c_;
 
-#define BEGIN_PRIMARY_PROXY_MAP(class_name)                        \
-  PROXY_MAP_BOILERPLATE(class_name)                                \
-  PRIMARY_PROXY_MAP_BOILERPLATE(class_name)                        \
-  REFCOUNTED_PROXY_MAP_BOILERPLATE(class_name)                     \
- public:                                                           \
-  static rtc::scoped_refptr<class_name##ProxyWithInternal> Create( \
-      rtc::Thread* primary_thread, INTERNAL_CLASS* c) {            \
-    return rtc::make_ref_counted<class_name##ProxyWithInternal>(   \
-        primary_thread, c);                                        \
+#define BEGIN_PRIMARY_PROXY_MAP(class_name)                                \
+  PROXY_MAP_BOILERPLATE(class_name)                                        \
+  PRIMARY_PROXY_MAP_BOILERPLATE(class_name)                                \
+  REFCOUNTED_PROXY_MAP_BOILERPLATE(class_name)                             \
+ public:                                                                   \
+  static rtc::scoped_refptr<class_name##ProxyWithInternal> Create(         \
+      rtc::Thread* primary_thread, rtc::scoped_refptr<INTERNAL_CLASS> c) { \
+    return rtc::make_ref_counted<class_name##ProxyWithInternal>(           \
+        primary_thread, std::move(c));                                     \
   }
 
 #define BEGIN_PROXY_MAP(class_name)                                \
@@ -304,9 +304,9 @@ class ConstMethodCall : public QueuedTask {
  public:                                                           \
   static rtc::scoped_refptr<class_name##ProxyWithInternal> Create( \
       rtc::Thread* primary_thread, rtc::Thread* secondary_thread,  \
-      INTERNAL_CLASS* c) {                                         \
+      rtc::scoped_refptr<INTERNAL_CLASS> c) {                      \
     return rtc::make_ref_counted<class_name##ProxyWithInternal>(   \
-        primary_thread, secondary_thread, c);                      \
+        primary_thread, secondary_thread, std::move(c));           \
   }
 
 #define BEGIN_OWNED_PROXY_MAP(class_name)                                   \

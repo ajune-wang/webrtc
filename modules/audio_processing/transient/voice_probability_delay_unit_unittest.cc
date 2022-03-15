f71f1ea10603f9c1af8c702884bb11b00a57f109
Alessio Bazzica <alessiob@webrtc.org>
2022-03-15 22:13:33 +0100

    APM TS: Adding `VoiceProbabilityDelayUnit`
    Adding a delay unit to be used in the APM Transient Suppressor (TS)
    sub-module through which the observerd voice probabilities are
    temporally aligned to the audio processed by TS, which introduces
    algorithmic delay.
    
    Bug: webrtc:13663
    Change-Id: I2136c303914580851c742d8db89478a13b06dacb

---
+++ modules/audio_processing/transient/voice_probability_delay_unit_unittest.cc
@@ -0,0 +1,75 @@
+/*
+ *  Copyright (c) 2022 The WebRTC project authors. All Rights Reserved.
+ *
+ *  Use of this source code is governed by a BSD-style license
+ *  that can be found in the LICENSE file in the root of the source
+ *  tree. An additional intellectual property rights grant can be found
+ *  in the file PATENTS.  All contributing project authors may
+ *  be found in the AUTHORS file in the root of the source tree.
+ */
+
+#include "modules/audio_processing/transient/voice_probability_delay_unit.h"
+
+#include "test/gtest.h"
+
+namespace webrtc {
+namespace {
+
+// Checks that with zero delay, the observed value is immediately returned as
+// delayed value.
+TEST(VoiceProbabilityDelayUnit, NoDelay) {
+  VoiceProbabilityDelayUnit delay_unit(/*delay_num_samples=*/0,
+                                       /*sample_rate_hz=*/48000);
+  constexpr int kMax = 5;
+  for (int i = 0; i <= kMax; ++i) {
+    SCOPED_TRACE(i);
+    float voice_probability = static_cast<float>(i) / kMax;
+    EXPECT_EQ(voice_probability, delay_unit.Delay(voice_probability));
+  }
+}
+
+// Checks that with integer delays, an exact copy of a previously observed value
+// is returned.
+TEST(VoiceProbabilityDelayUnit, IntegerDelay) {
+  VoiceProbabilityDelayUnit delay_unit_10ms(/*delay_num_samples=*/480,
+                                            /*sample_rate_hz=*/48000);
+  delay_unit_10ms.Delay(0.125f);
+  EXPECT_EQ(0.125f, delay_unit_10ms.Delay(0.9f));
+
+  VoiceProbabilityDelayUnit delay_unit_20ms(/*delay_num_samples=*/960,
+                                            /*sample_rate_hz=*/48000);
+  delay_unit_20ms.Delay(0.125f);
+  delay_unit_20ms.Delay(0.8f);
+  EXPECT_EQ(0.125f, delay_unit_20ms.Delay(0.9f));
+}
+
+// Checks that with a fractional delay < 10 ms, interpolation is applied.
+TEST(VoiceProbabilityDelayUnit, FractionalDelayLessThan10ms) {
+  // Create delay unit with fractional delay of 6 ms.
+  VoiceProbabilityDelayUnit delay_unit(/*delay_num_samples=*/288,
+                                       /*sample_rate_hz=*/48000);
+  //  frame 0
+  // --------- frame 1
+  //          ---------
+  //    0000001111
+  delay_unit.Delay(1.0f);
+  EXPECT_FLOAT_EQ(0.68f, delay_unit.Delay(0.2f));
+}
+
+// Checks that with a fractional delay > 10 ms, interpolation is applied.
+TEST(VoiceProbabilityDelayUnit, FractionalDelayGreaterThan10ms) {
+  // Create delay unit with fractional delay of 14 ms.
+  VoiceProbabilityDelayUnit delay_unit(/*delay_num_samples=*/672,
+                                       /*sample_rate_hz=*/48000);
+  //  frame 0
+  // --------- frame 1
+  //          --------- frame 2
+  //                   ---------
+  //      0000111111
+  delay_unit.Delay(1.0f);
+  delay_unit.Delay(0.2f);
+  EXPECT_FLOAT_EQ(0.52f, delay_unit.Delay(1.0f));
+}
+
+}  // namespace
+}  // namespace webrtc

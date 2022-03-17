9ee064cb2e5ed9f8f8839195ff05d3f9130306d6
Jeremy Leconte <jleconte@webrtc.org>
2022-03-17 10:23:06 +0100

    Add a new script to upload perf tests.
    Change-Id: I806fa11f417ef37674bdaeb5126c71570e3697d7

--- tools_webrtc/perf/catapult_uploader.py
+++ tools_webrtc/perf/catapult_uploader.py
@@ -52,8 +52,8 @@ class UploaderOptions():
   commit_position: int
   build_page_url: str
   dashboard_url: str
-  input_results_file: str
-  output_json_file: str
+  input_results_file: str = None
+  output_json_file: str = None
   wait_timeout_sec: datetime.timedelta = datetime.timedelta(seconds=1200)
   wait_polling_period_sec: datetime.timedelta = datetime.timedelta(seconds=120)
 

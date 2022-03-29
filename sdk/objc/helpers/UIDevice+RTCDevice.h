1a02c74fd268beb0cc73c529f4ad4e1256333f4f
Yura Yaroshevich <yura.yaroshevich@gmail.com>
2022-03-29 11:07:46 +0200

    Added new apple devices.
    Added new apple devices to corresponding enumeration.
    Added H264 profile level information.
    Previous update was done as part of:
    https://webrtc-review.googlesource.com/c/src/+/202743
    Device machine names obtained from:
    https://gist.github.com/adamawolf/3048717
    
    Bug: None
    Change-Id: If5cf2773500f2dd73783ede637cd2694e114e353

--- sdk/objc/helpers/UIDevice+RTCDevice.h
+++ sdk/objc/helpers/UIDevice+RTCDevice.h
@@ -44,6 +44,11 @@ typedef NS_ENUM(NSInteger, RTCDeviceType) {
   RTCDeviceTypeIPhone12,
   RTCDeviceTypeIPhone12Pro,
   RTCDeviceTypeIPhone12ProMax,
+  RTCDeviceTypeIPhone13,
+  RTCDeviceTypeIPhone13Mini,
+  RTCDeviceTypeIPhone13Pro,
+  RTCDeviceTypeIPhone13ProMax,
+
   RTCDeviceTypeIPodTouch1G,
   RTCDeviceTypeIPodTouch2G,
   RTCDeviceTypeIPodTouch3G,
@@ -88,7 +93,11 @@ typedef NS_ENUM(NSInteger, RTCDeviceType) {
   RTCDeviceTypeIPadMini5Gen,
   RTCDeviceTypeIPadAir3Gen,
   RTCDeviceTypeIPad8,
+  RTCDeviceTypeIPad9,
+  RTCDeviceTypeIPadMini6,
   RTCDeviceTypeIPadAir4Gen,
+  RTCDeviceTypeIPadPro5Gen11Inch,
+  RTCDeviceTypeIPadPro5Gen12Inch,
   RTCDeviceTypeSimulatori386,
   RTCDeviceTypeSimulatorx86_64,
 };

e43923bf4c174718aa96df634e01dfa7e100ddb8
Danil Chapovalov <danilchap@webrtc.org>
2022-04-01 16:06:30 +0200

    in RtcpTransceiver support replying to RRTR per local ssrc
    to support clients (e.g. RTCPReceiver) that collect and report RTT per sender ssrc.
    
    Bug: webrtc:8239, webrtc:13853
    Change-Id: I907fb35277b0f23bbe9f2cd2ef979ce0fb1f9338

--- modules/rtp_rtcp/source/rtcp_transceiver_impl.h
+++ modules/rtp_rtcp/source/rtcp_transceiver_impl.h
@@ -128,13 +128,16 @@ class RtcpTransceiverImpl {
   void SchedulePeriodicCompoundPackets(TimeDelta delay);
   // Appends RTCP sender and receiver reports to the `sender`.
   // Both sender and receiver reports may have attached report blocks.
-  // Uses up to `config_.max_packet_size - reserved_bytes`
+  // Uses up to `config_.max_packet_size - reserved_bytes.per_packet`
   struct CompoundPacketInfo {
-    uint32_t sender_ssrc;
-    bool has_sender_report;
+    std::vector<uint32_t> sender_ssrcs;
+  };
+  struct ReservedBytes {
+    size_t per_packet = 0;
+    size_t per_sender = 0;
   };
   CompoundPacketInfo FillReports(Timestamp now,
-                                 size_t reserved_bytes,
+                                 ReservedBytes reserved_bytes,
                                  PacketSender& rtcp_sender);
 
   // Creates compound RTCP packet, as defined in

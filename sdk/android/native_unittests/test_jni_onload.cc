457fb75575030173f5b5fc0b6868c9a3754ac6a8
Björn Terelius <terelius@webrtc.org>
2022-04-08 16:59:59 +0200

    Use WebRTC's Java VM initialization in tests.
    WebRTC should not depend on chromium's //base.
    
    Bug:webrtc:13662
    Change-Id: Ie660aa0f2477cc747830bba611aa23ed5e732385

--- sdk/android/native_unittests/test_jni_onload.cc
+++ sdk/android/native_unittests/test_jni_onload.cc
@@ -9,15 +9,26 @@
  */
 
 #include <jni.h>
+
 #undef JNIEXPORT
 #define JNIEXPORT __attribute__((visibility("default")))
 
 #include "rtc_base/checks.h"
-#include "sdk/android/native_api/base/init.h"
-#include "sdk/android/native_api/jni/java_types.h"
+#include "sdk/android/src/jni/class_reference_holder.h"
+#include "sdk/android/src/jni/jni_helpers.h"
+#include "sdk/android/src/jni/jvm.h"
 
 // This is called by the VM when the shared library is first loaded.
-JNIEXPORT jint JNI_OnLoad(JavaVM* vm, void* reserved) {
-  webrtc::InitAndroid(vm);
-  return JNI_VERSION_1_4;
+extern "C" jint JNIEXPORT JNICALL JNI_OnLoad(JavaVM* jvm, void* reserved) {
+  jint ret = webrtc_jni::InitGlobalJniVariables(jvm);
+  RTC_DCHECK_GE(ret, 0);
+  if (ret < 0)
+    return -1;
+
+  webrtc_jni::LoadGlobalClassReferenceHolder();
+  return ret;
+}
+
+extern "C" void JNIEXPORT JNICALL JNI_OnUnLoad(JavaVM* jvm, void* reserved) {
+  webrtc_jni::FreeGlobalClassReferenceHolder();
 }

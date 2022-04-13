38821ef4c7ee2a727e3b59201484d001e6928c81
Victor Boivie <boivie@webrtc.org>
2022-04-13 16:16:43 +0200

    DO NOT SUBMIT: Add example code from bug 13969
    This is the attached sample code from https://crbug.com/webrtc/13969,
    with minimal changes and build files to make it build and run in the
    WebRTC tree.
    
    This is just to easily share the example code and to easily build it
    with the standard toolchain.
    
    I do not claim copyright to any of this code.
    
    Build instructions:
      gn gen --check out/Default --args='is_debug=true' && \
        ninja -C out/Default dcsctp_speed_test
    
    Note: For highest performance, run without debug enabled.
    
    Run instructions:
      In one terminal:
        ./out/Default/dcsctp_speed_test 127.0.0.1 a
      In another:
        ./out/Default/dcsctp_speed_test 127.0.0.1 o 2000000 1000000 200000
    
    Change-Id: I8ae35e4822b5334cb41b7f1152fc7f717bc780d6

---
+++ net/dcsctp/speed_test/createsessiondescriptionobserver.h
@@ -0,0 +1,14 @@
+#pragma once
+#include "api/create_peerconnection_factory.h"
+class Peerconnection;
+
+class CreateSessionDescriptionObserver
+    : public webrtc::CreateSessionDescriptionObserver {
+ private:
+  Peerconnection& parent;
+
+ public:
+  CreateSessionDescriptionObserver(Peerconnection& parent);
+  void OnSuccess(webrtc::SessionDescriptionInterface* desc) override;
+  void OnFailure(webrtc::RTCError error) override;
+};

ce9e0510e1b8191f1482d839094c466f49dd73c7
Harald Alvestrand <hta@webrtc.org>
2022-04-21 05:30:25 +0000

    Change stream.AddTrack/RemoveTrack to take a scoped_refptr argument
    This better reflects the ownership passing of AddTrack, and is more
    consistent for RemoveTrack.
    
    Bug: webrtc:13980
    Change-Id: Ide5baccf15fc687a4e092f8831ce8c0fea46604e

--- pc/media_stream.cc
+++ pc/media_stream.cc
@@ -36,22 +36,20 @@ rtc::scoped_refptr<MediaStream> MediaStream::Create(const std::string& id) {
 
 MediaStream::MediaStream(const std::string& id) : id_(id) {}
 
-bool MediaStream::AddTrack(AudioTrackInterface* track) {
-  return AddTrack<AudioTrackVector, AudioTrackInterface>(
-      &audio_tracks_, rtc::scoped_refptr<AudioTrackInterface>(track));
+bool MediaStream::AddTrack(rtc::scoped_refptr<AudioTrackInterface> track) {
+  return AddTrack<AudioTrackVector, AudioTrackInterface>(&audio_tracks_, track);
 }
 
-bool MediaStream::AddTrack(VideoTrackInterface* track) {
-  return AddTrack<VideoTrackVector, VideoTrackInterface>(
-      &video_tracks_, rtc::scoped_refptr<VideoTrackInterface>(track));
+bool MediaStream::AddTrack(rtc::scoped_refptr<VideoTrackInterface> track) {
+  return AddTrack<VideoTrackVector, VideoTrackInterface>(&video_tracks_, track);
 }
 
-bool MediaStream::RemoveTrack(AudioTrackInterface* track) {
-  return RemoveTrack<AudioTrackVector>(&audio_tracks_, track);
+bool MediaStream::RemoveTrack(rtc::scoped_refptr<AudioTrackInterface> track) {
+  return RemoveTrack<AudioTrackVector>(&audio_tracks_, track.get());
 }
 
-bool MediaStream::RemoveTrack(VideoTrackInterface* track) {
-  return RemoveTrack<VideoTrackVector>(&video_tracks_, track);
+bool MediaStream::RemoveTrack(rtc::scoped_refptr<VideoTrackInterface> track) {
+  return RemoveTrack<VideoTrackVector>(&video_tracks_, track.get());
 }
 
 rtc::scoped_refptr<AudioTrackInterface> MediaStream::FindAudioTrack(

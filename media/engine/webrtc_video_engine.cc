5e64f97058da4ba6d6645b4d9fdf6c0bb2249c5d
Philipp Hancke <phancke@nvidia.com>
2022-04-21 21:20:53 +0200

    stats: frame assembly time stats
    implements a total frame assembly time statistic that measures the
    cumulative time between the arrival of the first packet of a frame
    (the lowest reception time) and the time all packets of the frame have been
    received (i.e. the highest reception time)
    
    This is similar to totalProcessingDelay
      https://w3c.github.io/webrtc-stats/#dom-rtcinboundrtpstreamstats-totalprocessingdelay
    but does not include the amount of time spent decoding the frame.
    
    This statistic is useful for evaluating mechanisms like NACK and FEC
    and gives some insight into the behavior of the pacer sending the
    packets.
    Note that for frames with just a single packet the assembly time will be zero.
    
    BUG=webrtc:13986
    
    Change-Id: I8aa4dd454ac45b2fd47bd076b629e4cbd8a064e9

--- media/engine/webrtc_video_engine.cc
+++ media/engine/webrtc_video_engine.cc
@@ -3164,6 +3164,7 @@ WebRtcVideoChannel::WebRtcVideoReceiveStream::GetVideoReceiverInfo(
   info.frames_rendered = stats.frames_rendered;
   info.qp_sum = stats.qp_sum;
   info.total_decode_time_ms = stats.total_decode_time_ms;
+  info.total_assembly_time_ms = stats.total_assembly_time_ms;
   info.last_packet_received_timestamp_ms =
       stats.rtp_stats.last_packet_received_timestamp_ms;
   info.estimated_playout_ntp_timestamp_ms =

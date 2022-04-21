838c9879236712a1f6920d49c773a2262cbe6f39
Philipp Hancke <philipp.hancke@googlemail.com>
2022-04-21 13:08:33 +0200

    stats: add dtlsRole to transport
    BUG=webrtc:13978
    
    Change-Id: Ib158427d2df0307884381bdd46c411f60f56a371

--- pc/jsep_transport.cc
+++ pc/jsep_transport.cc
@@ -714,6 +714,7 @@ bool JsepTransport::GetTransportStats(DtlsTransportInternal* dtls_transport,
   dtls_transport->GetSrtpCryptoSuite(&substats.srtp_crypto_suite);
   dtls_transport->GetSslCipherSuite(&substats.ssl_cipher_suite);
   substats.dtls_state = dtls_transport->dtls_state();
+  dtls_transport->GetDtlsRole(&(*substats.dtls_role));
   if (!dtls_transport->ice_transport()->GetStats(
           &substats.ice_transport_stats)) {
     return false;

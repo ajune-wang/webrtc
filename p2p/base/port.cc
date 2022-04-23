a5c3e807c1423551101cb37297b24cf7f13e2f96
Tommi <tommi@webrtc.org>
2022-04-23 13:34:25 +0200

    Make Port::Close protected
    Bug: webrtc:13892
    Change-Id: I07edb7bb5e5d93b33542581b4b09def548de9e12

--- p2p/base/port.cc
+++ p2p/base/port.cc
@@ -186,22 +186,7 @@ void Port::Construct() {
 Port::~Port() {
   RTC_DCHECK_RUN_ON(thread_);
   CancelPendingTasks();
-
-  // Delete all of the remaining connections.  We copy the list up front
-  // because each deletion will cause it to be modified.
-
-  std::vector<Connection*> list;
-
-  AddressMap::iterator iter = connections_.begin();
-  while (iter != connections_.end()) {
-    list.push_back(iter->second);
-    ++iter;
-  }
-
-  for (uint32_t i = 0; i < list.size(); i++) {
-    list[i]->SignalDestroyed.disconnect(this);
-    delete list[i];
-  }
+  DestroyAllConnections();
 }
 
 const std::string& Port::Type() const {
@@ -352,9 +337,11 @@ void Port::AddOrReplaceConnection(Connection* conn) {
         << ": A new connection was created on an existing remote address. "
            "New remote candidate: "
         << conn->remote_candidate().ToSensitiveString();
-    ret.first->second->SignalDestroyed.disconnect(this);
-    ret.first->second->Destroy();
+    std::unique_ptr<Connection> old_conn(ret.first->second);
     ret.first->second = conn;
+    old_conn->SignalDestroyed.disconnect(this);
+    HandleConnectionDestroyed(old_conn.get());
+    old_conn->Shutdown();
   }
   conn->SignalDestroyed.connect(this, &Port::OnConnectionDestroyed);
 }
@@ -612,11 +599,20 @@ rtc::DiffServCodePoint Port::StunDscpValue() const {
   return rtc::DSCP_NO_CHANGE;
 }
 
+void Port::DestroyConnection(Connection* conn) {
+  RTC_DCHECK_RUN_ON(thread_);
+  conn->SignalDestroyed.disconnect(this);
+  OnConnectionDestroyed(conn);
+  conn->Shutdown();
+  delete conn;
+}
+
 void Port::DestroyAllConnections() {
   RTC_DCHECK_RUN_ON(thread_);
-  for (auto kv : connections_) {
-    kv.second->SignalDestroyed.disconnect(this);
-    kv.second->Destroy();
+  for (auto& [unused, connection] : connections_) {
+    connection->SignalDestroyed.disconnect(this);
+    connection->Shutdown();
+    delete connection;
   }
   connections_.clear();
 }

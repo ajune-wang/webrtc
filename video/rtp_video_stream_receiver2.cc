6225ccc852b0934739e05a44b8030faae7a0fe86
Tomas Gunnarsson <tommi@webrtc.org>
2022-04-30 20:23:23 +0200

    Look into moving use of receive_rtp_config_ over to network thread
    Bug: webrtc:11993
    Change-Id: I5c36458d61e398008b0c5d4943b4cf9248a1b0c9

--- video/rtp_video_stream_receiver2.cc
+++ video/rtp_video_stream_receiver2.cc
@@ -944,6 +944,8 @@ void RtpVideoStreamReceiver2::ManageFrame(
 
 // RTC_RUN_ON(packet_sequence_checker_)
 void RtpVideoStreamReceiver2::ReceivePacket(const RtpPacketReceived& packet) {
+  // TODO(tommi): We get here via Call::DeliverRtp (and other calls), so should
+  // be moved to the network thread.
   RTC_DCHECK_RUN_ON(&worker_task_checker_);
   if (packet.payload_size() == 0) {
     // Padding or keep-alive packet.

83c5b5c3ef1ba08e40a74c27aa863b8cefe202b6
Philipp Hancke <philipp.hancke@googlemail.com>
2022-05-05 14:57:52 +0200

    second try
    Bug: None
    Change-Id: I002d28333d2d678564c0d01cbc38786a372011af

--- pc/rtc_stats_integrationtest.cc
+++ pc/rtc_stats_integrationtest.cc
@@ -917,6 +917,9 @@ class RTCStatsReportVerifier {
       verifier.TestMemberIsNonNegative<double>(
           inbound_stream.total_processing_delay);
       verifier.TestMemberIsNonNegative<double>(
+          inbound_stream.total_assembly_time);
+      verifier.TestMemberIsDefined(inbound_stream.frames_assembled);
+      verifier.TestMemberIsNonNegative<double>(
           inbound_stream.total_inter_frame_delay);
       verifier.TestMemberIsNonNegative<double>(
           inbound_stream.total_squared_inter_frame_delay);
@@ -929,6 +932,8 @@ class RTCStatsReportVerifier {
       verifier.TestMemberIsUndefined(inbound_stream.frames_dropped);
       verifier.TestMemberIsUndefined(inbound_stream.total_decode_time);
       verifier.TestMemberIsUndefined(inbound_stream.total_processing_delay);
+      verifier.TestMemberIsUndefined(inbound_stream.total_assembly_time);
+      verifier.TestMemberIsUndefined(inbound_stream.frames_assembled);
       verifier.TestMemberIsUndefined(inbound_stream.total_inter_frame_delay);
       verifier.TestMemberIsUndefined(
           inbound_stream.total_squared_inter_frame_delay);

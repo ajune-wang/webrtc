4b6c9412d04f6853af50aa2caf07207ea00fc6b0
Tommi <tommi@webrtc.org>
2022-05-17 15:55:09 +0200

    Rename ReceiveStream to ReceiveStreamInterface
    Bug: webrtc:7484
    Change-Id: I41176a66b8399f6c8cf568630f2808eb95cf6247

--- call/receive_stream.h
+++ call/receive_stream.h
@@ -24,7 +24,7 @@ namespace webrtc {
 
 // Common base interface for MediaReceiveStream based classes and
 // FlexfecReceiveStream.
-class ReceiveStream {
+class ReceiveStreamInterface {
  public:
   // Receive-stream specific RTP settings.
   // TODO(tommi): This struct isn't needed at this level anymore. Move it closer
@@ -68,11 +68,11 @@ class ReceiveStream {
   virtual bool transport_cc() const = 0;
 
  protected:
-  virtual ~ReceiveStream() {}
+  virtual ~ReceiveStreamInterface() {}
 };
 
 // Either an audio or video receive stream.
-class MediaReceiveStream : public ReceiveStream {
+class MediaReceiveStream : public ReceiveStreamInterface {
  public:
   // Starts stream activity.
   // When a stream is active, it can receive, process and deliver packets.

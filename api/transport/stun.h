1ef21d9f9c909b664ebb54c8f12832d16ad890e4
Tommi <tommi@webrtc.org>
2022-06-01 10:11:44 +0200

    Update tests Add ctor for StunMessage
    Bug: none
    Change-Id: Ib013e58f28e7b2b4fcb3b3e1034da31dfc93e9d3

--- api/transport/stun.h
+++ api/transport/stun.h
@@ -145,6 +145,7 @@ class StunXorAddressAttribute;
 class StunMessage {
  public:
   StunMessage();
+  StunMessage(int type, absl::string_view transaction_id);
   virtual ~StunMessage();
 
   // The verification status of the message. This is checked on parsing,
@@ -169,7 +170,12 @@ class StunMessage {
   bool IsLegacy() const;
 
   void SetType(int type) { type_ = static_cast<uint16_t>(type); }
-  bool SetTransactionID(const std::string& str);
+  [[deprecated]] bool SetTransactionID(absl::string_view transaction_id) {
+    if (!IsValidTransactionId(transaction_id))
+      return false;
+    SetTransactionIdForTesting(transaction_id);
+    return true;
+  }
 
   // Get a list of all of the attribute types in the "comprehension required"
   // range that were not recognized.
@@ -249,7 +255,10 @@ class StunMessage {
 
   // Modify the stun magic cookie used for this STUN message.
   // This is used for testing.
-  void SetStunMagicCookie(uint32_t val);
+  [[deprecated]] void SetStunMagicCookie(uint32_t val);
+
+  // Change the internal transaction id. Used only for testing.
+  void SetTransactionIdForTesting(absl::string_view transaction_id);
 
   // Contruct a copy of `this`.
   std::unique_ptr<StunMessage> Clone() const;
@@ -292,7 +301,7 @@ class StunMessage {
  private:
   StunAttribute* CreateAttribute(int type, size_t length) /* const*/;
   const StunAttribute* GetAttribute(int type) const;
-  static bool IsValidTransactionId(const std::string& transaction_id);
+  static bool IsValidTransactionId(absl::string_view transaction_id);
   bool AddMessageIntegrityOfType(int mi_attr_type,
                                  size_t mi_attr_size,
                                  const char* key,
@@ -303,11 +312,11 @@ class StunMessage {
                                              size_t size,
                                              const std::string& password);
 
-  uint16_t type_;
-  uint16_t length_;
+  uint16_t type_ = 0;
+  uint16_t length_ = 0;
   std::string transaction_id_;
-  uint32_t reduced_transaction_id_;
-  uint32_t stun_magic_cookie_;
+  uint32_t reduced_transaction_id_ = 0;
+  uint32_t stun_magic_cookie_ = kStunMagicCookie;
   // The original buffer for messages created by Read().
   std::string buffer_;
   IntegrityStatus integrity_ = IntegrityStatus::kNotSet;
@@ -635,6 +644,9 @@ enum RelayAttributeType {
 
 // A "GTURN" STUN message.
 class RelayMessage : public StunMessage {
+ public:
+  using StunMessage::StunMessage;
+
  protected:
   StunAttributeValueType GetAttributeValueType(int type) const override;
   StunMessage* CreateNew() const override;
@@ -689,6 +701,9 @@ extern const char STUN_ERROR_REASON_ALLOCATION_MISMATCH[];
 extern const char STUN_ERROR_REASON_WRONG_CREDENTIALS[];
 extern const char STUN_ERROR_REASON_UNSUPPORTED_PROTOCOL[];
 class TurnMessage : public StunMessage {
+ public:
+  using StunMessage::StunMessage;
+
  protected:
   StunAttributeValueType GetAttributeValueType(int type) const override;
   StunMessage* CreateNew() const override;
@@ -747,6 +762,9 @@ extern const char STUN_ERROR_REASON_ROLE_CONFLICT[];
 
 // A RFC 5245 ICE STUN message.
 class IceMessage : public StunMessage {
+ public:
+  using StunMessage::StunMessage;
+
  protected:
   StunAttributeValueType GetAttributeValueType(int type) const override;
   StunMessage* CreateNew() const override;

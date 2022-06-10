77de2b64a3f7a62b5a13fde70751a7215a8ad8e6
Niels Möller <nisse@webrtc.org>
2022-06-10 11:09:55 +0200

    Move rtc::make_ref_counted to api/
    Bug: webrtc:12701
    Change-Id: If49095b101c1a1763c2a44a0284c0d670cce953f

--- modules/audio_device/win/audio_device_core_win.cc
+++ modules/audio_device/win/audio_device_core_win.cc
@@ -44,7 +44,6 @@
 #include "rtc_base/checks.h"
 #include "rtc_base/logging.h"
 #include "rtc_base/platform_thread.h"
-#include "rtc_base/ref_counted_object.h"
 #include "rtc_base/string_utils.h"
 #include "rtc_base/thread_annotations.h"
 #include "system_wrappers/include/sleep.h"

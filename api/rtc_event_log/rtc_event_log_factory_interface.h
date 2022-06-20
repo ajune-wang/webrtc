6fb7c90b23b9f6e3b3ee3fe7fe8ceb633bedbf00
Harald Alvestrand <hta@webrtc.org>
2022-06-20 11:04:46 +0000

    Change RTCEventLogFactory to have a const Create function
    Illustrative of proposed naming rule change.
    
    Bug: none
    Change-Id: Ibec148fada6303e2ebdc5e6405fd527065f69d41

--- api/rtc_event_log/rtc_event_log_factory_interface.h
+++ api/rtc_event_log/rtc_event_log_factory_interface.h
@@ -24,6 +24,9 @@ class RtcEventLogFactoryInterface {
  public:
   virtual ~RtcEventLogFactoryInterface() = default;
 
+  virtual std::unique_ptr<RtcEventLog> Create(
+      RtcEventLog::EncodingType encoding_type) const = 0;
+  // TODO(xxx): Deprecate and remove this name.
   virtual std::unique_ptr<RtcEventLog> CreateRtcEventLog(
       RtcEventLog::EncodingType encoding_type) = 0;
 };

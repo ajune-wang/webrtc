381a63df731f5c3444a71ec78d3ebddefeaa70e1
Ali Tofigh <alito@webrtc.org>
2022-06-30 15:25:05 +0200

    adopt absl::string_view in logging/
    Bug: webrtc:13579
    Change-Id: Ibc5fa7842d52321d61cc4cdd4770635af737ddff

--- logging/rtc_event_log/rtc_event_log_impl.cc
+++ logging/rtc_event_log/rtc_event_log_impl.cc
@@ -16,6 +16,7 @@
 #include <utility>
 #include <vector>
 
+#include "absl/strings/string_view.h"
 #include "absl/types/optional.h"
 #include "api/task_queue/queued_task.h"
 #include "api/task_queue/task_queue_base.h"
@@ -230,8 +231,8 @@ void RtcEventLogImpl::LogEventsFromMemoryToOutput() {
 }
 
 void RtcEventLogImpl::WriteConfigsAndHistoryToOutput(
-    const std::string& encoded_configs,
-    const std::string& encoded_history) {
+    absl::string_view encoded_configs,
+    absl::string_view encoded_history) {
   // This function is used to merge the strings instead of calling the output
   // object twice with small strings. The function also avoids copying any
   // strings in the typical case where there are no config events.
@@ -240,7 +241,7 @@ void RtcEventLogImpl::WriteConfigsAndHistoryToOutput(
   } else if (encoded_history.empty()) {
     WriteToOutput(encoded_configs);  // Very unusual case.
   } else {
-    WriteToOutput(encoded_configs + encoded_history);
+    WriteToOutput(std::string(encoded_configs) + std::string(encoded_history));
   }
 }
 
@@ -257,9 +258,9 @@ void RtcEventLogImpl::StopLoggingInternal() {
   StopOutput();
 }
 
-void RtcEventLogImpl::WriteToOutput(const std::string& output_string) {
+void RtcEventLogImpl::WriteToOutput(absl::string_view output_string) {
   RTC_DCHECK(event_output_ && event_output_->IsActive());
-  if (!event_output_->Write(output_string)) {
+  if (!event_output_->Write(std::string(output_string))) {
     RTC_LOG(LS_ERROR) << "Failed to write RTC event to output.";
     // The first failure closes the output.
     RTC_DCHECK(!event_output_->IsActive());

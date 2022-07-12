ee0bef0b31b2370144ffb64060a9f7c1d206505a
Philipp Hancke <philipp.hancke@googlemail.com>
2022-07-12 15:37:16 +0200

    WIP: add codec mimetype to insertable streams
    while knowing the payload type is great (and required for some use-cases), knowing the codec type is required to decide on an adaptable encryption scheme such as not encrypting the SPS/PPS for H264.
    This information is available in the localDescription, however may not be readily accessible in workers.
    
    More work is required for incoming audio here:
    https://source.chromium.org/chromium/chromium/src/+/main:third_party/webrtc/audio/channel_receive.cc;l=711;drc=d5c694fc357d3b1cb40b3d40bb0c1e1ca15d952f;bpv=1;bpt=1
    the codec is not known yet. The ACM receiver asks NetEq here:
      https://source.chromium.org/chromium/chromium/src/+/main:third_party/webrtc/modules/audio_coding/acm2/acm_receiver.cc;l=108;drc=d5c694fc357d3b1cb40b3d40bb0c1e1ca15d952f
    Asking the ACM for GetReceiveCodec() does not work for the first packet and the receive channel does not store the receive codecs itself.
    
    Chromium side: https://chromium-review.googlesource.com/c/chromium/src/+/3749630
    
    BUG=None
    
    Change-Id: I3b618e1c18a8a310b3e6ea56de36a505f7fd9c1f

--- modules/audio_coding/test/opus_test.cc
+++ modules/audio_coding/test/opus_test.cc
@@ -337,7 +337,8 @@ void OpusTest::Run(TestPackStereo* channel,
 
         // Send data to the channel. "channel" will handle the loss simulation.
         channel->SendData(AudioFrameType::kAudioFrameSpeech, payload_type_,
-                          rtp_timestamp_, bitstream, bitstream_len_byte, 0);
+                          AudioEncoder::CodecType::kOpus, rtp_timestamp_,
+                          bitstream, bitstream_len_byte, 0);
         if (first_packet) {
           first_packet = false;
           start_time_stamp = rtp_timestamp_;

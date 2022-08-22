c3ebd630ad67716bc7c801f54427d1fd1fbb8094
Tommi <tommi@webrtc.org>
2022-08-22 16:40:48 +0200

    Clean up decoders when stopping video receive stream
    Bug: none
    Change-Id: I41f4feea5584e5baaeed2143432136f8b9761321

--- modules/video_coding/video_receiver2.cc
+++ modules/video_coding/video_receiver2.cc
@@ -102,4 +102,9 @@ void VideoReceiver2::RegisterReceiveCodec(
   codec_database_.RegisterReceiveCodec(payload_type, settings);
 }
 
+void VideoReceiver2::DeregisterReceiveCodec(uint8_t payload_type) {
+  RTC_DCHECK_RUN_ON(&construction_sequence_checker_);
+  codec_database_.DeregisterReceiveCodec(payload_type);
+}
+
 }  // namespace webrtc

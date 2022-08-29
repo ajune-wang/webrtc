98de68c296d588bfa0193f991237805747583edf
Alessio Bazzica <alessiob@webrtc.org>
2022-08-29 15:20:47 +0200

    APM: add `AudioProcessingImpl::stream_analog_level_`
    The `recommended_stream_analog_level()` getter is used to retrieve both
    the stream and the recommended level. This design is error-prone since
    the caller must know what is returned based on the point in the code
    (namely, before or after the AGC has changed the last stream level into
    a recommended level).
    
    This CL is a first step to make clarity on which analog level is handled
    in different parts of APM. Next in the pipeline: make
    `recommended_stream_analog_level()` a trivial getter that always returns
    the recommended level.
    
    Bug: webrtc:7494, b/241923537
    Change-Id: I3cb4a731fd9f3dc19bf6ac679b7ed8c969ea283b

--- modules/audio_processing/audio_processing_impl.h
+++ modules/audio_processing/audio_processing_impl.h
@@ -464,12 +464,12 @@ class AudioProcessingImpl : public AudioProcessing {
     StreamConfig capture_processing_format;
     int split_rate;
     bool echo_path_gain_change;
-    int prev_analog_mic_level;
     float prev_pre_adjustment_gain;
     int playout_volume;
     int prev_playout_volume;
     AudioProcessingStats stats;
-    int cached_stream_analog_level_ = 0;
+    int applied_input_volume;
+    bool applied_input_volume_changed;
   } capture_ RTC_GUARDED_BY(mutex_capture_);
 
   struct ApmCaptureNonLockedState {
@@ -530,7 +530,7 @@ class AudioProcessingImpl : public AudioProcessing {
   RmsLevel capture_output_rms_ RTC_GUARDED_BY(mutex_capture_);
   int capture_rms_interval_counter_ RTC_GUARDED_BY(mutex_capture_) = 0;
 
-  AnalogGainStatsReporter analog_gain_stats_reporter_
+  AnalogGainStatsReporter input_volume_stats_reporter_
       RTC_GUARDED_BY(mutex_capture_);
 
   // Lock protection not needed.

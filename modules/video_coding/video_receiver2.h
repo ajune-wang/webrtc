472fc346c4d6a3b6945a65af57bd54ebb1230272
Tommi <tommi@webrtc.org>
2022-09-28 11:44:46 +0200

    Fork and rename VCMDecoderDatabase.
    This is to keep the deprecated VideoReceiver separate before updating
    DecoderDatabase to have ownership of the registered decoders.
    
    Bug: none
    Change-Id: I5ee755921454b0831b3af6d0161f5b48c7c60540

--- modules/video_coding/video_receiver2.h
+++ modules/video_coding/video_receiver2.h
@@ -21,6 +21,7 @@
 #include "modules/video_coding/encoded_frame.h"
 #include "modules/video_coding/generic_decoder.h"
 #include "modules/video_coding/timing/timing.h"
+#include "rtc_base/system/no_unique_address.h"
 #include "system_wrappers/include/clock.h"
 
 namespace webrtc {
@@ -51,8 +52,8 @@ class VideoReceiver2 {
   int32_t Decode(const VCMEncodedFrame* frame);
 
  private:
-  SequenceChecker construction_sequence_checker_;
-  SequenceChecker decoder_sequence_checker_;
+  RTC_NO_UNIQUE_ADDRESS SequenceChecker construction_sequence_checker_;
+  RTC_NO_UNIQUE_ADDRESS SequenceChecker decoder_sequence_checker_;
   Clock* const clock_;
   VCMDecodedFrameCallback decoded_frame_callback_;
   // Holds/owns the decoder instances that are registered via

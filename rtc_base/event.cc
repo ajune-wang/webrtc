867493efcd01842f669f606867040ca4fac1b2c4
Byoungchan Lee <daniel.l@hpcnt.com>
2022-09-30 17:56:15 +0900

    Use monotonic clock for rtc::Event::Wait on MacOS and iOS
    The minimum supported MacOS and iOS versions are 10.12 and 12.0
    respectively, so we can use clock_gettime.
    
    Bug: None
    Change-Id: Ie488129370ae19746cd3f8ccccc098f5461c7f35

--- rtc_base/event.cc
+++ rtc_base/event.cc
@@ -66,9 +66,10 @@ bool Event::Wait(TimeDelta give_up_after, TimeDelta /*warn_after*/) {
 #elif defined(WEBRTC_POSIX)
 
 // On MacOS, clock_gettime is available from version 10.12, and on
-// iOS, from version 10.0. So we can't use it yet.
+// iOS, from version 10.0, but not pthread_condattr_setclock.
 #if defined(WEBRTC_MAC) || defined(WEBRTC_IOS)
-#define USE_CLOCK_GETTIME 0
+#define USE_CLOCK_GETTIME 1
+#define USE_PTHREAD_CONDATTR_SETCLOCK 0
 #define USE_PTHREAD_COND_TIMEDWAIT_MONOTONIC_NP 0
 // On Android, pthread_condattr_setclock is available from version 21. By
 // default, we target a new enough version for 64-bit platforms but not for
@@ -76,9 +77,11 @@ bool Event::Wait(TimeDelta give_up_after, TimeDelta /*warn_after*/) {
 // pthread_cond_timedwait_monotonic_np.
 #elif defined(WEBRTC_ANDROID) && (__ANDROID_API__ < 21)
 #define USE_CLOCK_GETTIME 1
+#define USE_PTHREAD_CONDATTR_SETCLOCK 0
 #define USE_PTHREAD_COND_TIMEDWAIT_MONOTONIC_NP 1
 #else
 #define USE_CLOCK_GETTIME 1
+#define USE_PTHREAD_CONDATTR_SETCLOCK 1
 #define USE_PTHREAD_COND_TIMEDWAIT_MONOTONIC_NP 0
 #endif
 
@@ -87,7 +90,8 @@ Event::Event(bool manual_reset, bool initially_signaled)
   RTC_CHECK(pthread_mutex_init(&event_mutex_, nullptr) == 0);
   pthread_condattr_t cond_attr;
   RTC_CHECK(pthread_condattr_init(&cond_attr) == 0);
-#if USE_CLOCK_GETTIME && !USE_PTHREAD_COND_TIMEDWAIT_MONOTONIC_NP
+#if USE_CLOCK_GETTIME && USE_PTHREAD_CONDATTR_SETCLOCK && \
+    !USE_PTHREAD_COND_TIMEDWAIT_MONOTONIC_NP
   RTC_CHECK(pthread_condattr_setclock(&cond_attr, CLOCK_MONOTONIC) == 0);
 #endif
   RTC_CHECK(pthread_cond_init(&event_cond_, &cond_attr) == 0);

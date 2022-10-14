f29843f330339d4fd49e616bd0bdb1548905f7ad
Tomas Lundqvist <tomasl@google.com>
2022-10-14 13:16:42 +0000

    Expose jitter in time in addition to in samples.
    RFC 3550 specifies samples to be the unit while https://w3c.github.io/webrtc-stats/#receivedrtpstats-dict* specifies time. This avoids the need to convert to time in code that reads the jitter value from RtpReceiveStats.
    
    Bug: None
    Change-Id: I972996971c58b686babd621ff4e0f5790fdf2cb1

--- modules/rtp_rtcp/include/rtp_rtcp_defines.h
+++ modules/rtp_rtcp/include/rtp_rtcp_defines.h
@@ -25,6 +25,7 @@
 #include "api/audio_codecs/audio_format.h"
 #include "api/rtp_headers.h"
 #include "api/transport/network_types.h"
+#include "api/units/time_delta.h"
 #include "modules/rtp_rtcp/source/rtcp_packet/remote_estimate.h"
 #include "system_wrappers/include/clock.h"
 
@@ -442,7 +443,10 @@ struct RtpReceiveStats {
   // RTCReceivedRtpStreamStats dictionary, see
   // https://w3c.github.io/webrtc-stats/#receivedrtpstats-dict*
   int32_t packets_lost = 0;
+  // Jitter in samples.
   uint32_t jitter = 0;
+  // Jitter in time.
+  webrtc::TimeDelta jitter_duration = webrtc::TimeDelta::Zero();
 
   // Timestamp and counters exposed in RTCInboundRtpStreamStats, see
   // https://w3c.github.io/webrtc-stats/#inboundrtpstats-dict*

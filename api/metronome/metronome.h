4d87b5eb5b08b19639e1d1d77ee8fddbc1665f95
Markus Handell <handellm@webrtc.org>
2022-11-04 12:40:18 +0100

    WIP: Simplify metronome
    Bug: chromium:1381982
    Change-Id: I70da96725f4995ade112b574696b0b30593108e8

--- api/metronome/metronome.h
+++ api/metronome/metronome.h
@@ -17,13 +17,10 @@
 
 namespace webrtc {
 
-// The Metronome posts OnTick() on task queues provided by its listeners' task
-// queue periodically. The metronome can be used as an alternative to using
-// PostDelayedTask on a thread or task queue for coalescing work and reducing
-// the number of idle-wakeups.
-//
-// Listeners can be added and removed from any sequence, but it is illegal to
-// remove a listener from an OnTick invocation.
+// The Metronome posts OnTick() calls requested with RequestCallOnNextTick.
+// The API must be used from the worker thread, and scheduled callbacks will be
+// executed on the worker thread. There are no features for cancellation. When
+// that's needed, use e.g. ScopedTaskSafety from the client.
 //
 // The metronome concept is still under experimentation, and may not be availble
 // in all platforms or applications. See https://crbug.com/1253787 for more
@@ -32,29 +29,10 @@ namespace webrtc {
 // Metronome implementations must be thread-safe.
 class RTC_EXPORT Metronome {
  public:
-  class RTC_EXPORT TickListener {
-   public:
-    virtual ~TickListener() = default;
-
-    // OnTick is run on the task queue provided by OnTickTaskQueue each time the
-    // metronome ticks.
-    virtual void OnTick() = 0;
-
-    // The task queue that OnTick will run on. Must not be null.
-    virtual TaskQueueBase* OnTickTaskQueue() = 0;
-  };
-
   virtual ~Metronome() = default;
 
-  // Adds a tick listener to the metronome. Once this method has returned
-  // OnTick will be invoked on each metronome tick. A listener may
-  // only be added to the metronome once.
-  virtual void AddListener(TickListener* listener) = 0;
-
-  // Removes the tick listener from the metronome. Once this method has returned
-  // OnTick will never be called again. This method must not be called from
-  // within OnTick.
-  virtual void RemoveListener(TickListener* listener) = 0;
+  // Requests a call to `callback` on the next tick.
+  virtual void RequestCallOnNextTick(absl::AnyInvocable<void() &&> callback) {}
 
   // Returns the current tick period of the metronome.
   virtual TimeDelta TickPeriod() const = 0;

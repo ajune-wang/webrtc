522f8019f9492f9b6e9f0691b496fe5bf3f11ebf
Henrik Boström <hbos@webrtc.org>
2022-11-28 10:40:34 +0100

    Add more information to RTPVideoHeader::GetAsMetadata().
    Update GetAsMetadata() to include more of the RTPVideoHeader metadata.
    The intent is to be able to both get and set all of these from
    JavaScript behind a flag.
    
    Planned follow-up CLs:
    1. Also get codecs-specifics, starting with VP8.
    2. Test refactoring/rename: Move tests to RTPVideoHeaderTest.
    3. Add RTPVideoHeader::SetFromMetadata() covering everything gettable.
    4. Chrome plumbing.
    
    Bug: webrtc:14709
    Change-Id: I78679b9ff4ca749d50f309a1713e71ceabb826dd

--- api/video/video_frame_metadata.h
+++ api/video/video_frame_metadata.h
@@ -17,6 +17,7 @@
 #include "absl/types/optional.h"
 #include "api/array_view.h"
 #include "api/transport/rtp/dependency_descriptor.h"
+#include "api/video/video_frame_type.h"
 #include "rtc_base/system/rtc_export.h"
 
 namespace webrtc {
@@ -28,6 +29,9 @@ class RTC_EXPORT VideoFrameMetadata {
   VideoFrameMetadata();
   VideoFrameMetadata(const VideoFrameMetadata&) = default;
   VideoFrameMetadata& operator=(const VideoFrameMetadata&) = default;
+
+  VideoFrameType GetFrameType() const;
+  void SetFrameType(VideoFrameType frame_type);
 
   uint16_t GetWidth() const;
   void SetWidth(uint16_t width);
@@ -52,14 +56,30 @@ class RTC_EXPORT VideoFrameMetadata {
   void SetDecodeTargetIndications(
       rtc::ArrayView<const DecodeTargetIndication> decode_target_indications);
 
+  bool GetIsFirstPacketInFrame() const;
+  void SetIsFirstPacketInFrame(bool is_first_packet_in_frame);
+
+  bool GetIsLastPacketInFrame() const;
+  void SetIsLastPacketInFrame(bool is_last_packet_in_frame);
+
+  bool GetIsLastFrameInPicture() const;
+  void SetIsLastFrameInPicture(bool is_last_frame_in_picture);
+
  private:
+  VideoFrameType frame_type_ = VideoFrameType::kEmptyFrame;
   int16_t width_ = 0;
   int16_t height_ = 0;
+
+  // Corresponding to GenericDescriptorInfo.
   absl::optional<int64_t> frame_id_;
   int spatial_index_ = 0;
   int temporal_index_ = 0;
   absl::InlinedVector<int64_t, 5> frame_dependencies_;
   absl::InlinedVector<DecodeTargetIndication, 10> decode_target_indications_;
+
+  bool is_first_packet_in_frame_ = false;
+  bool is_last_packet_in_frame_ = false;
+  bool is_last_frame_in_picture_ = true;
 };
 }  // namespace webrtc
 

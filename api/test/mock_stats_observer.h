4d04a6d835d6ebcc7e782938c9066b3f48af974c
Jack Smith <jackdsmith@google.com>
2022-12-14 01:12:24 +0000

    Adds mocks and modifies existing mocks for webrtc api
    These mocks will reduce the need for external clients to define their own
    mocks for webrtc classes.
    
    Change-Id: I6a5f18fcfaa405e8a570b0a55ca5e1a610b724c8

---
+++ api/test/mock_stats_observer.h
@@ -0,0 +1,30 @@
+/*
+ *  Copyright 2016 The WebRTC project authors. All Rights Reserved.
+ *
+ *  Use of this source code is governed by a BSD-style license
+ *  that can be found in the LICENSE file in the root of the source
+ *  tree. An additional intellectual property rights grant can be found
+ *  in the file PATENTS.  All contributing project authors may
+ *  be found in the AUTHORS file in the root of the source tree.
+ */
+
+#ifndef API_TEST_MOCK_STATSOVSERVER_H_
+#define API_TEST_MOCK_STATSOVSERVER_H_
+
+#include "api/legacy_stats_types.h"
+#include "api/peer_connection_interface.h"
+#include "test/gmock.h"
+
+namespace webrtc {
+
+class MockStatsObserver : public MOCK_CLASS(webrtc::StatsObserver) {
+ public:
+  MOCK_METHOD(void,
+              OnComplete,
+              (const webrtc::StatsReports& legacy_stats_reports),
+              (override));
+};
+
+}  // namespace webrtc
+
+#endif  // API_TEST_MOCK_STATSOVSERVER_H_

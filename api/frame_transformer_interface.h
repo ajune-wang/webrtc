1a9ffe5b1201e9919de27dc9f7cd5be0a43fadd3
Henrik Boström <hbos@webrtc.org>
2023-01-13 17:38:25 +0100

    Surface the SetMetadata() method so that Chromium can use it.
    Unblocks
    https://chromium-review.googlesource.com/c/chromium/src/+/4164979.
    
    Bug: webrtc:14709
    Change-Id: I6089df9c03e9aa33feeb0830dd240dd456cb565e

--- api/frame_transformer_interface.h
+++ api/frame_transformer_interface.h
@@ -62,6 +62,7 @@ class TransformableVideoFrameInterface : public TransformableFrameInterface {
   virtual std::vector<uint8_t> GetAdditionalData() const = 0;
 
   virtual const VideoFrameMetadata& GetMetadata() const = 0;
+  virtual void SetMetadata(const VideoFrameMetadata&) = 0;
 };
 
 // Extends the TransformableFrameInterface to expose audio-specific information.

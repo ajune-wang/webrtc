1f780e031568da83ed47a43d4b2f7b1860277d2d
Sun Shin <sushin@nvidia.com>
2023-01-18 23:13:59 -0800

    Add decoder fallback stats to RTC stats
    Implement the metric for decoderFallback in getStats. This field
    will reflect if the current WebRTC session is fallbacked to the software
    decoder when the session has started with Hardware Decoder.
    
    Bug: webrtc:14831
    Change-Id: I2010d95edfc39983bbde38d56925b89dadee3d28

--- api/stats/rtcstats_objects.h
+++ api/stats/rtcstats_objects.h
@@ -512,6 +512,7 @@ class RTC_EXPORT RTCInboundRTPStreamStats final
   RTCStatsMember<std::string> goog_timing_frame_info;
   RTCRestrictedStatsMember<bool, StatExposureCriteria::kHardwareCapability>
       power_efficient_decoder;
+  RTCStatsMember<bool> decoder_fallback;
   // Non-standard audio metrics.
   RTCNonStandardStatsMember<uint64_t> jitter_buffer_flushes;
   RTCNonStandardStatsMember<uint64_t> delayed_packet_outage_samples;

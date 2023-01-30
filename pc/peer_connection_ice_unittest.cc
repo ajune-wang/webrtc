2c37d76ad4fa16d00f4df968ae29bc3715f33e44
Philipp Hancke <phancke@microsoft.com>
2023-01-30 09:58:19 +0100

    Let port allocator create ice tie breaker
    BUG=webrtc:14626
    
    Change-Id: I3a9a0980238d6108b1b154f45de2975b08793b1c

--- pc/peer_connection_ice_unittest.cc
+++ pc/peer_connection_ice_unittest.cc
@@ -86,7 +86,6 @@ using ::testing::Values;
 
 constexpr int kIceCandidatesTimeout = 10000;
 constexpr int64_t kWaitTimeout = 10000;
-constexpr uint64_t kTiebreakerDefault = 44444;
 
 class PeerConnectionWrapperForIceTest : public PeerConnectionWrapper {
  public:
@@ -1429,7 +1428,6 @@ class PeerConnectionIceConfigTest : public ::testing::Test {
                                        packet_socket_factory_.get(),
                                        &field_trials_));
     port_allocator_ = port_allocator.get();
-    port_allocator_->SetIceTiebreaker(kTiebreakerDefault);
     PeerConnectionDependencies pc_dependencies(&observer_);
     pc_dependencies.allocator = std::move(port_allocator);
     auto result = pc_factory_->CreatePeerConnectionOrError(

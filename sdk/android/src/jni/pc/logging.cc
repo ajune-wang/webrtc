4c01ded9d30e01fb9242aa369da40818625d8787
landrey <landrey@google.com>
2023-02-15 12:26:17 +0000

    Migrate WebRTC to the new JNI style
    See bug for more details
    
    Bug: webrtc:14839
    Change-Id: I5eb560dc22650fda90682f43f46ad2552fa2766e

--- sdk/android/src/jni/pc/logging.cc
+++ sdk/android/src/jni/pc/logging.cc
@@ -8,9 +8,11 @@
  *  be found in the AUTHORS file in the root of the source tree.
  */
 
+#include "rtc_base/logging.h"
+
 #include <memory>
 
-#include "rtc_base/logging.h"
+#include "rtc_base/generated_base_java_jni/Logging_jni.h"
 #include "sdk/android/native_api/jni/java_types.h"
 #include "sdk/android/src/jni/jni_helpers.h"
 
@@ -18,9 +20,8 @@ namespace webrtc {
 namespace jni {
 
 JNI_FUNCTION_DECLARATION(void,
-                         Logging_nativeEnableLogToDebugOutput,
+                         Logging_EnableLogToDebugOutput,
                          JNIEnv* jni,
-                         jclass,
                          jint nativeSeverity) {
   if (nativeSeverity >= rtc::LS_VERBOSE && nativeSeverity <= rtc::LS_NONE) {
     rtc::LogMessage::LogToDebug(
@@ -28,24 +29,17 @@ JNI_FUNCTION_DECLARATION(void,
   }
 }
 
-JNI_FUNCTION_DECLARATION(void,
-                         Logging_nativeEnableLogThreads,
-                         JNIEnv* jni,
-                         jclass) {
+JNI_FUNCTION_DECLARATION(void, Logging_EnableLogThreads, JNIEnv* jni) {
   rtc::LogMessage::LogThreads(true);
 }
 
-JNI_FUNCTION_DECLARATION(void,
-                         Logging_nativeEnableLogTimeStamps,
-                         JNIEnv* jni,
-                         jclass) {
+JNI_FUNCTION_DECLARATION(void, Logging_EnableLogTimeStamps, JNIEnv* jni) {
   rtc::LogMessage::LogTimestamps(true);
 }
 
 JNI_FUNCTION_DECLARATION(void,
-                         Logging_nativeLog,
+                         Logging_Log,
                          JNIEnv* jni,
-                         jclass,
                          jint j_severity,
                          jstring j_tag,
                          jstring j_message) {

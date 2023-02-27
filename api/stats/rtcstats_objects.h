b2668ca5cad6ae82c970c9824ba1542d54485f75
Philipp Hancke <phancke@microsoft.com>
2023-02-27 18:54:15 +0100

    stats: Implement receive RTX stats
    * retransmittedBytesReceived
    * retransmittedPacketsReceived
    added to the specification in
      https://github.com/w3c/webrtc-stats/pull/735
    
    BUG=webrtc:15096
    
    Change-Id: I6770e5d8d09ac1c2693c918fd943b0ab257ec7ba

--- api/stats/rtcstats_objects.h
+++ api/stats/rtcstats_objects.h
@@ -427,6 +427,10 @@ class RTC_EXPORT RTCInboundRTPStreamStats final
   RTCStatsMember<uint64_t> fec_packets_discarded;
   RTCStatsMember<uint64_t> bytes_received;
   RTCStatsMember<uint64_t> header_bytes_received;
+  // Inbound RTX stats. Only defined when RTX is used and it is possible
+  // to distinguish retransmissions.
+  RTCStatsMember<uint64_t> retransmitted_packets_received;
+  RTCStatsMember<uint64_t> retransmitted_bytes_received;
   RTCStatsMember<double> last_packet_received_timestamp;
   RTCStatsMember<double> jitter_buffer_delay;
   RTCStatsMember<double> jitter_buffer_target_delay;

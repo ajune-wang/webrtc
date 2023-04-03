17b77e05e5a487ef348e64f65438c10aa063c4c5
Henrik Boström <hbos@webrtc.org>
2023-04-03 15:28:00 +0200

    Deflake PeerConnectionSimulcastMediaFlowTests due to unstopped sources.
    Only in testing environments are the task queues shut down while sources
    still have media flowing. It's still not clear why heap-use-after-free
    happens, since it should be enough to close the PC, but it is clear that
    the crash is happening due to frames flowing while the test is shutting
    down, which is not something happening outside of testing.
    
    In an attempt to deflake, this CL makes sure to manually stop the
    test-only sources before closing the peer connection.
    
    Bug: webrtc:15018
    Change-Id: I48ee131a8994c9c4caee1bb4875580d255b97da1

--- pc/peer_connection_simulcast_unittest.cc
+++ pc/peer_connection_simulcast_unittest.cc
@@ -898,6 +898,7 @@ class PeerConnectionSimulcastWithMediaFlowTests
             /*audio=*/false, cricket::AudioOptions(), /*video=*/true,
             {.width = 1280, .height = 720});
     rtc::scoped_refptr<VideoTrackInterface> track = stream->GetVideoTracks()[0];
+    track->set_enabled(false);
 
     RTCErrorOr<rtc::scoped_refptr<RtpTransceiverInterface>>
         transceiver_or_error = local->pc()->AddTransceiver(
@@ -1172,12 +1173,6 @@ TEST_F(PeerConnectionSimulcastWithMediaFlowTests,
               StrCaseEq("video/VP8"));
   EXPECT_THAT(*outbound_rtps[0]->scalability_mode, StrEq("L1T1"));
 }
-
-// TODO(https://crbug.com/webrtc/15018): Investigate heap-use-after free during
-// shutdown of the test that is flakily happening on bots. It's not only
-// happening on ASAN, but it is rare enough on non-ASAN that we don't have to
-// disable everywhere.
-#if !defined(ADDRESS_SANITIZER)
 
 TEST_F(PeerConnectionSimulcastWithMediaFlowTests,
        SendingThreeEncodings_VP8_Simulcast) {
@@ -1756,7 +1751,5 @@ TEST_F(PeerConnectionSimulcastWithMediaFlowTests,
   EXPECT_THAT(*outbound_rtps[1]->scalability_mode, StrEq("L1T3"));
   EXPECT_THAT(*outbound_rtps[2]->scalability_mode, StrEq("L1T3"));
 }
-
-#endif  // !defined(ADDRESS_SANITIZER)
 
 }  // namespace webrtc

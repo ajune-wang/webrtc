dc776914d7b37553f23641f8f75e55804bdde442
Tommi <tommi@webrtc.org>
2023-04-05 15:18:09 +0200

    Reland "[DataChannel] Send and receive packets on the network thread."
    This reverts commit 7f16fcda0fd5bb625584b71311dd37b54c096136.
    
    Reason for revert: Re-landing the original change after running
    a few tests.
    
    Original change's description:
    > Revert "[DataChannel] Send and receive packets on the network thread."
    >
    > This reverts commit fe53fec24e02d2d644220f913c3f9ae596bbb2d9.
    >
    > Reason for revert: Speculative revert, may be breaking downstream project
    >
    > Original change's description:
    > > [DataChannel] Send and receive packets on the network thread.
    > >
    > > This updates sctp channels, including work that happens between the
    > > data channel controller and the transport, to run on the network
    > > thread. Previously all network traffic related to data channels was
    > > routed through the signaling thread before going to either the network
    > > thread or the caller's thread (e.g. js thread in chrome). Now the
    > > calls can go straight from the network thread to the JS thread with
    > > enabling a special flag on the observer (see below) and similarly
    > > calls to send data, involve 2 threads instead of 3.
    > >
    > > * Custom data channel observer adapter implementation that
    > >   maintains compatibility with existing observer implementations in
    > >   that notifications are delivered on the signaling thread.
    > >   The adapter can be explicitly disabled for implementations that
    > >   want to optimize the callback path and promise to not block the
    > >   network thread.
    > > * Remove the signaling thread copy of data channels in the controller.
    > > * Remove several PostTask operations that were needed to keep things
    > >   in sync (but the need has gone away).
    > > * Update tests for the controller to consistently call
    > >   TeardownDataChannelTransport_n to match with production.
    > > * Update stats collectors (current and legacy) to fetch the data
    > >   channel stats on the network thread where they're maintained.
    > > * Remove the AsyncChannelCloseTeardown test since the async teardown
    > >   step has gone away.
    > > * Remove `sid_s` in the channel code since we only need the network
    > >   state now.
    > > * For the custom observer support (with and without data adapter) and
    > >   maintain compatibility with existing implementations, added a new
    > >   proxy macro that allows an implementation to selectively provide
    > >   its own implementation without being proxied. This is used for
    > >   registering/unregistering a data channel observer.
    > > * Update the data channel proxy to map most methods to the network
    > >   thread, avoiding the interim jump to the signaling thread.
    > > * Update a plethora of thread checkers from signaling to network.
    > >
    > > Bug: webrtc:11547
    > > Change-Id: Ib4cff1482e31c46008e187189a79e967389bc518
    > > Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/299142
    > > Commit-Queue: Tomas Gunnarsson <tommi@webrtc.org>
    > > Reviewed-by: Henrik Boström <hbos@webrtc.org>
    > > Cr-Commit-Position: refs/heads/main@{#39760}
    >
    > Bug: webrtc:11547
    > Change-Id: Id0d65594bf727ccea5c49093c942b09714d101ad
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/300341
    > Auto-Submit: Andrey Logvin <landrey@webrtc.org>
    > Owners-Override: Andrey Logvin <landrey@webrtc.org>
    > Bot-Commit: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    > Commit-Queue: Mirko Bonadei <mbonadei@webrtc.org>
    > Reviewed-by: Mirko Bonadei <mbonadei@webrtc.org>
    > Cr-Commit-Position: refs/heads/main@{#39764}
    
    Bug: webrtc:11547
    Change-Id: I47dfa7e7168be0cd2faab4f8f3ebf110c3728af5

--- pc/data_channel_unittest.cc
+++ pc/data_channel_unittest.cc
@@ -90,11 +90,17 @@ class SctpDataChannelTest : public ::testing::Test {
 
   void SetChannelReady() {
     controller_->set_transport_available(true);
-    inner_channel_->OnTransportChannelCreated();
-    if (!inner_channel_->sid_s().HasValue()) {
-      SetChannelSid(inner_channel_, StreamId(0));
-    }
+    StreamId sid(0);
+    network_thread_.BlockingCall([&]() {
+      RTC_DCHECK_RUN_ON(&network_thread_);
+      if (!inner_channel_->sid_n().HasValue()) {
+        inner_channel_->SetSctpSid_n(sid);
+        controller_->AddSctpDataStream(sid);
+      }
+      inner_channel_->OnTransportChannelCreated();
+    });
     controller_->set_ready_to_send(true);
+    run_loop_.Flush();
   }
 
   // TODO(bugs.webrtc.org/11547): This mirrors what the DataChannelController
@@ -105,9 +111,10 @@ class SctpDataChannelTest : public ::testing::Test {
   void SetChannelSid(const rtc::scoped_refptr<SctpDataChannel>& channel,
                      StreamId sid) {
     RTC_DCHECK(sid.HasValue());
-    network_thread_.BlockingCall(
-        [&]() { controller_->AddSctpDataStream(sid); });
-    channel->SetSctpSid_s(sid);
+    network_thread_.BlockingCall([&]() {
+      channel->SetSctpSid_n(sid);
+      controller_->AddSctpDataStream(sid);
+    });
   }
 
   void AddObserver() {
@@ -143,11 +150,13 @@ TEST_F(SctpDataChannelTest, VerifyConfigurationGetters) {
 
   // Check the non-const part of the configuration.
   EXPECT_EQ(channel_->id(), init_.id);
-  EXPECT_EQ(inner_channel_->sid_s(), StreamId());
+  network_thread_.BlockingCall(
+      [&]() { EXPECT_EQ(inner_channel_->sid_n(), StreamId()); });
 
   SetChannelReady();
   EXPECT_EQ(channel_->id(), 0);
-  EXPECT_EQ(inner_channel_->sid_s(), StreamId(0));
+  network_thread_.BlockingCall(
+      [&]() { EXPECT_EQ(inner_channel_->sid_n(), StreamId(0)); });
 }
 
 // Verifies that the data channel is connected to the transport after creation.
@@ -155,13 +164,15 @@ TEST_F(SctpDataChannelTest, ConnectedToTransportOnCreated) {
   controller_->set_transport_available(true);
   rtc::scoped_refptr<SctpDataChannel> dc =
       controller_->CreateDataChannel("test1", init_);
-
   EXPECT_TRUE(controller_->IsConnected(dc.get()));
+
   // The sid is not set yet, so it should not have added the streams.
-  EXPECT_FALSE(controller_->IsStreamAdded(dc->sid_s()));
+  StreamId sid = network_thread_.BlockingCall([&]() { return dc->sid_n(); });
+  EXPECT_FALSE(controller_->IsStreamAdded(sid));
 
   SetChannelSid(dc, StreamId(0));
-  EXPECT_TRUE(controller_->IsStreamAdded(dc->sid_s()));
+  sid = network_thread_.BlockingCall([&]() { return dc->sid_n(); });
+  EXPECT_TRUE(controller_->IsStreamAdded(sid));
 }
 
 // Tests the state of the data channel.
@@ -180,7 +191,7 @@ TEST_F(SctpDataChannelTest, StateTransition) {
   channel_->Close();
   // The (simulated) transport close notifications runs on the network thread
   // and posts a completion notification to the signaling (current) thread.
-  // Allow that ooperation to complete before checking the state.
+  // Allow that operation to complete before checking the state.
   run_loop_.Flush();
   EXPECT_EQ(DataChannelInterface::kClosed, channel_->state());
   EXPECT_EQ(observer_->on_state_change_count(), 3u);
@@ -198,6 +209,7 @@ TEST_F(SctpDataChannelTest, BufferedAmountWhenBlocked) {
   EXPECT_TRUE(channel_->Send(buffer));
   size_t successful_send_count = 1;
 
+  run_loop_.Flush();
   EXPECT_EQ(0U, channel_->buffered_amount());
   EXPECT_EQ(successful_send_count,
             observer_->on_buffered_amount_change_count());
@@ -214,6 +226,7 @@ TEST_F(SctpDataChannelTest, BufferedAmountWhenBlocked) {
             observer_->on_buffered_amount_change_count());
 
   controller_->set_send_blocked(false);
+  run_loop_.Flush();
   successful_send_count += number_of_packets;
   EXPECT_EQ(0U, channel_->buffered_amount());
   EXPECT_EQ(successful_send_count,
@@ -334,10 +347,9 @@ TEST_F(SctpDataChannelTest, LateCreatedChannelTransitionToOpen) {
   SetChannelReady();
   InternalDataChannelInit init;
   init.id = 1;
-  rtc::scoped_refptr<SctpDataChannel> dc =
-      controller_->CreateDataChannel("test1", init);
-  EXPECT_EQ(DataChannelInterface::kConnecting, dc->state());
-  EXPECT_TRUE_WAIT(DataChannelInterface::kOpen == dc->state(), 1000);
+  auto dc = webrtc::SctpDataChannel::CreateProxy(
+      controller_->CreateDataChannel("test1", init));
+  EXPECT_EQ(DataChannelInterface::kOpen, dc->state());
 }
 
 // Tests that an unordered DataChannel sends data as ordered until the OPEN_ACK
@@ -349,21 +361,23 @@ TEST_F(SctpDataChannelTest, SendUnorderedAfterReceivesOpenAck) {
   init.ordered = false;
   rtc::scoped_refptr<SctpDataChannel> dc =
       controller_->CreateDataChannel("test1", init);
+  auto proxy = webrtc::SctpDataChannel::CreateProxy(dc);
 
-  EXPECT_EQ_WAIT(DataChannelInterface::kOpen, dc->state(), 1000);
+  EXPECT_EQ_WAIT(DataChannelInterface::kOpen, proxy->state(), 1000);
 
   // Sends a message and verifies it's ordered.
   DataBuffer buffer("some data");
-  ASSERT_TRUE(dc->Send(buffer));
+  ASSERT_TRUE(proxy->Send(buffer));
   EXPECT_TRUE(controller_->last_send_data_params().ordered);
 
   // Emulates receiving an OPEN_ACK message.
   rtc::CopyOnWriteBuffer payload;
   WriteDataChannelOpenAckMessage(&payload);
-  dc->OnDataReceived(DataMessageType::kControl, payload);
+  network_thread_.BlockingCall(
+      [&] { dc->OnDataReceived(DataMessageType::kControl, payload); });
 
   // Sends another message and verifies it's unordered.
-  ASSERT_TRUE(dc->Send(buffer));
+  ASSERT_TRUE(proxy->Send(buffer));
   EXPECT_FALSE(controller_->last_send_data_params().ordered);
 }
 
@@ -376,15 +390,17 @@ TEST_F(SctpDataChannelTest, SendUnorderedAfterReceiveData) {
   init.ordered = false;
   rtc::scoped_refptr<SctpDataChannel> dc =
       controller_->CreateDataChannel("test1", init);
+  auto proxy = webrtc::SctpDataChannel::CreateProxy(dc);
 
-  EXPECT_EQ_WAIT(DataChannelInterface::kOpen, dc->state(), 1000);
+  EXPECT_EQ_WAIT(DataChannelInterface::kOpen, proxy->state(), 1000);
 
   // Emulates receiving a DATA message.
   DataBuffer buffer("data");
-  dc->OnDataReceived(DataMessageType::kText, buffer.data);
+  network_thread_.BlockingCall(
+      [&] { dc->OnDataReceived(DataMessageType::kText, buffer.data); });
 
   // Sends a message and verifies it's unordered.
-  ASSERT_TRUE(dc->Send(buffer));
+  ASSERT_TRUE(proxy->Send(buffer));
   EXPECT_FALSE(controller_->last_send_data_params().ordered);
 }
 
@@ -437,7 +453,10 @@ TEST_F(SctpDataChannelTest, ReceiveDataWithValidId) {
   AddObserver();
 
   DataBuffer buffer("abcd");
-  inner_channel_->OnDataReceived(DataMessageType::kText, buffer.data);
+  network_thread_.BlockingCall([&] {
+    inner_channel_->OnDataReceived(DataMessageType::kText, buffer.data);
+  });
+  run_loop_.Flush();
   EXPECT_EQ(1U, observer_->messages_received());
 }
 
@@ -452,8 +471,9 @@ TEST_F(SctpDataChannelTest, NoMsgSentIfNegotiatedAndNotFromOpenMsg) {
   SetChannelReady();
   rtc::scoped_refptr<SctpDataChannel> dc =
       controller_->CreateDataChannel("test1", config);
+  auto proxy = webrtc::SctpDataChannel::CreateProxy(dc);
 
-  EXPECT_EQ_WAIT(DataChannelInterface::kOpen, dc->state(), 1000);
+  EXPECT_EQ_WAIT(DataChannelInterface::kOpen, proxy->state(), 1000);
   EXPECT_EQ(0, controller_->last_sid());
 }
 
@@ -477,9 +497,10 @@ TEST_F(SctpDataChannelTest, VerifyMessagesAndBytesReceived) {
   EXPECT_EQ(0U, channel_->bytes_received());
 
   // Receive three buffers while data channel isn't open.
-  inner_channel_->OnDataReceived(DataMessageType::kText, buffers[0].data);
-  inner_channel_->OnDataReceived(DataMessageType::kText, buffers[1].data);
-  inner_channel_->OnDataReceived(DataMessageType::kText, buffers[2].data);
+  network_thread_.BlockingCall([&] {
+    for (int i : {0, 1, 2})
+      inner_channel_->OnDataReceived(DataMessageType::kText, buffers[i].data);
+  });
   EXPECT_EQ(0U, observer_->messages_received());
   EXPECT_EQ(0U, channel_->messages_received());
   EXPECT_EQ(0U, channel_->bytes_received());
@@ -493,9 +514,11 @@ TEST_F(SctpDataChannelTest, VerifyMessagesAndBytesReceived) {
   EXPECT_EQ(bytes_received, channel_->bytes_received());
 
   // Receive three buffers while open.
-  inner_channel_->OnDataReceived(DataMessageType::kText, buffers[3].data);
-  inner_channel_->OnDataReceived(DataMessageType::kText, buffers[4].data);
-  inner_channel_->OnDataReceived(DataMessageType::kText, buffers[5].data);
+  network_thread_.BlockingCall([&] {
+    for (int i : {3, 4, 5})
+      inner_channel_->OnDataReceived(DataMessageType::kText, buffers[i].data);
+  });
+  run_loop_.Flush();
   bytes_received += buffers[3].size() + buffers[4].size() + buffers[5].size();
   EXPECT_EQ(6U, observer_->messages_received());
   EXPECT_EQ(6U, channel_->messages_received());
@@ -513,8 +536,9 @@ TEST_F(SctpDataChannelTest, OpenAckSentIfCreatedFromOpenMessage) {
   SetChannelReady();
   rtc::scoped_refptr<SctpDataChannel> dc =
       controller_->CreateDataChannel("test1", config);
+  auto proxy = webrtc::SctpDataChannel::CreateProxy(dc);
 
-  EXPECT_EQ_WAIT(DataChannelInterface::kOpen, dc->state(), 1000);
+  EXPECT_EQ_WAIT(DataChannelInterface::kOpen, proxy->state(), 1000);
 
   EXPECT_EQ(config.id, controller_->last_sid());
   EXPECT_EQ(DataMessageType::kControl,
@@ -551,9 +575,8 @@ TEST_F(SctpDataChannelTest, OpenWhenSendBufferFull) {
     EXPECT_TRUE(channel_->Send(packet));
   }
 
-  // The sending buffer shoul be full, send returns false.
+  // The sending buffer should be full, `Send()` returns false.
   EXPECT_FALSE(channel_->Send(packet));
-
   EXPECT_TRUE(DataChannelInterface::kOpen == channel_->state());
 }
 
@@ -577,10 +600,12 @@ TEST_F(SctpDataChannelTest, ClosedWhenReceivedBufferFull) {
   rtc::CopyOnWriteBuffer buffer(1024);
   memset(buffer.MutableData(), 0, buffer.size());
 
-  // Receiving data without having an observer will overflow the buffer.
-  for (size_t i = 0; i < 16 * 1024 + 1; ++i) {
-    inner_channel_->OnDataReceived(DataMessageType::kText, buffer);
-  }
+  network_thread_.BlockingCall([&] {
+    // Receiving data without having an observer will overflow the buffer.
+    for (size_t i = 0; i < 16 * 1024 + 1; ++i) {
+      inner_channel_->OnDataReceived(DataMessageType::kText, buffer);
+    }
+  });
   EXPECT_EQ(DataChannelInterface::kClosed, channel_->state());
   EXPECT_FALSE(channel_->error().ok());
   EXPECT_EQ(RTCErrorType::RESOURCE_EXHAUSTED, channel_->error().type());
@@ -601,7 +626,8 @@ TEST_F(SctpDataChannelTest, SendEmptyData) {
 // Tests that a channel can be closed without being opened or assigned an sid.
 TEST_F(SctpDataChannelTest, NeverOpened) {
   controller_->set_transport_available(true);
-  inner_channel_->OnTransportChannelCreated();
+  network_thread_.BlockingCall(
+      [&] { inner_channel_->OnTransportChannelCreated(); });
   channel_->Close();
 }
 
@@ -631,7 +657,8 @@ TEST_F(SctpDataChannelTest, TransportDestroyedWhileDataBuffered) {
   // transition to the "closed" state.
   RTCError error(RTCErrorType::OPERATION_ERROR_WITH_DATA, "");
   error.set_error_detail(RTCErrorDetailType::SCTP_FAILURE);
-  inner_channel_->OnTransportChannelClosed(error);
+  network_thread_.BlockingCall(
+      [&] { inner_channel_->OnTransportChannelClosed(error); });
   controller_.reset(nullptr);
   EXPECT_EQ_WAIT(DataChannelInterface::kClosed, channel_->state(),
                  kDefaultTimeout);
@@ -651,7 +678,8 @@ TEST_F(SctpDataChannelTest, TransportGotErrorCode) {
   error.set_error_detail(RTCErrorDetailType::SCTP_FAILURE);
   error.set_sctp_cause_code(
       static_cast<uint16_t>(cricket::SctpErrorCauseCode::kProtocolViolation));
-  inner_channel_->OnTransportChannelClosed(error);
+  network_thread_.BlockingCall(
+      [&] { inner_channel_->OnTransportChannelClosed(error); });
   controller_.reset(nullptr);
   EXPECT_EQ_WAIT(DataChannelInterface::kClosed, channel_->state(),
                  kDefaultTimeout);

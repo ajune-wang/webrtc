68606a5212a2c57d46d6234f8d2235eaa077216b
Artem Titov <titovartem@webrtc.org>
2023-04-20 15:27:39 +0200

    Extract platform specific code out from AudioDeviceModuleImpl.
    It is required to make AudioDeviceModuleImpl compilable with chromium
    to unblock https://webrtc-review.googlesource.com/c/src/+/301704.
    The idea is chromium has targets that requires TestADM and migration
    of TestADM on top of AudioDeviceModuleImpl will do unavailable in
    chromium. To prevent it this CL moves platform specifics logic out
    of AudioDeviceModuleImpl, making it a thin implementation that can be
    included into compilation with chromium.
    
    It is needed for this target:
    //rtc_tools:video_replay -> ///test:test_common
    
    Bug: b/272350185, webrtc:15104
    Change-Id: I7407181fd644632aa7d90498dbc61897528d70f6

---
+++ modules/audio_device/dummy/dummy_audio_device_factory.h
@@ -0,0 +1,39 @@
+/*
+ *  Copyright (c) 2023 The WebRTC project authors. All Rights Reserved.
+ *
+ *  Use of this source code is governed by a BSD-style license
+ *  that can be found in the LICENSE file in the root of the source
+ *  tree. An additional intellectual property rights grant can be found
+ *  in the file PATENTS.  All contributing project authors may
+ *  be found in the AUTHORS file in the root of the source tree.
+ */
+
+#ifndef MODULES_AUDIO_DEVICE_DUMMY_DUMMY_AUDIO_DEVICE_FACTORY_H_
+#define MODULES_AUDIO_DEVICE_DUMMY_DUMMY_AUDIO_DEVICE_FACTORY_H_
+
+#include <memory>
+
+#include "modules/audio_device/audio_device_factory.h"
+#include "modules/audio_device/audio_device_generic.h"
+#include "modules/audio_device/dummy/audio_device_dummy.h"
+#include "modules/audio_device/include/audio_device.h"
+
+namespace webrtc {
+
+// Defined and will be used only on Android.
+class AudioManager;
+
+class DummyAudioDeviceFactory : public AudioDeviceFactory {
+ public:
+  ~AudioDeviceFactoryImpl() override = default;
+
+  std::unique_ptr<AudioDeviceGeneric> CreateAudioDevice(
+      AudioDeviceModule::AudioLayer audio_layer,
+      AudioManager* android_audio_manager) override {
+    return std::make_unique<AudioDeviceDummy>();
+  }
+};
+
+}  // namespace webrtc
+
+#endif  // MODULES_AUDIO_DEVICE_DUMMY_DUMMY_AUDIO_DEVICE_FACTORY_H_

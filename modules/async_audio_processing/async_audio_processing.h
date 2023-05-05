d8fc5795e8199aea16c5adb74fd2c33cbb90ada9
Peter Hanspers <peterhanspers@webrtc.org>
2023-05-05 17:08:57 +0200

    Updating AsyncAudioProcessing API, part 1.
    Add an API to pass AudioFrameProcessor as a unique_ptr.
    
    Bug: webrtc:15111
    Change-Id: I4cefa35399c05c6e81c496e0b0387b95809bd8f8

--- modules/async_audio_processing/async_audio_processing.h
+++ modules/async_audio_processing/async_audio_processing.h
@@ -38,12 +38,15 @@ class AsyncAudioProcessing final {
     ~Factory();
     Factory(AudioFrameProcessor& frame_processor,
             TaskQueueFactory& task_queue_factory);
+    Factory(std::unique_ptr<AudioFrameProcessor> frame_processor,
+            TaskQueueFactory& task_queue_factory);
 
     std::unique_ptr<AsyncAudioProcessing> CreateAsyncAudioProcessing(
         AudioFrameProcessor::OnAudioFrameCallback on_frame_processed_callback);
 
    private:
     AudioFrameProcessor& frame_processor_;
+    std::unique_ptr<AudioFrameProcessor> owned_frame_processor_;
     TaskQueueFactory& task_queue_factory_;
   };
 
@@ -57,8 +60,21 @@ class AsyncAudioProcessing final {
   // into `on_frame_processed_callback`, which is posted back onto
   // `task_queue_`. `task_queue_` is created using the provided
   // `task_queue_factory`.
+  // TODO(bugs.webrtc.org/15111):
+  //   Remove this method in favour of the method taking the
+  //   unique_ptr<AudioFrameProcessor> in the follow-up.
   AsyncAudioProcessing(
       AudioFrameProcessor& frame_processor,
+      TaskQueueFactory& task_queue_factory,
+      AudioFrameProcessor::OnAudioFrameCallback on_frame_processed_callback);
+
+  // Creates AsyncAudioProcessing which will pass audio frames to
+  // `frame_processor` on `task_queue_` and reply with processed frames passed
+  // into `on_frame_processed_callback`, which is posted back onto
+  // `task_queue_`. `task_queue_` is created using the provided
+  // `task_queue_factory`.
+  AsyncAudioProcessing(
+      std::unique_ptr<AudioFrameProcessor> frame_processor,
       TaskQueueFactory& task_queue_factory,
       AudioFrameProcessor::OnAudioFrameCallback on_frame_processed_callback);
 
@@ -67,7 +83,15 @@ class AsyncAudioProcessing final {
 
  private:
   AudioFrameProcessor::OnAudioFrameCallback on_frame_processed_callback_;
+  // TODO(bugs.webrtc.org/15111):
+  //   Remove `frame_processor_` in favour of `owned_frame_processor_` in the
+  //   follow-up.
+  //   While transitioning this API from AudioFrameProcessor& to
+  //   std::unique_ptr<AudioFrameProcessor>, we have both `frame_processor_` and
+  //   `owned_frame_processor_`. Only one of them is used, depending on which
+  //   constructor is called.
   AudioFrameProcessor& frame_processor_;
+  std::unique_ptr<AudioFrameProcessor> owned_frame_processor_;
   rtc::TaskQueue task_queue_;
 };
 

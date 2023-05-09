fce8b4d64a0740954fa5b3e7083cce0f83e18384
Danil Chapovalov <danilchap@webrtc.org>
2023-05-09 18:45:51 +0200

    Use absl random library instead of webrtc own implementation
    Bug: None
    Change-Id: Ifad146e3c74bb28b567ef2bc1daff161794866d6

--- rtc_base/random.h
+++ rtc_base/random.h
@@ -14,8 +14,9 @@
 #include <stdint.h>
 
 #include <limits>
+#include <type_traits>
 
-#include "rtc_base/checks.h"
+#include "absl/random/random.h"
 
 namespace webrtc {
 
@@ -32,64 +33,59 @@ class Random {
   // If we go for a static seed generator in Random, we can use something from
   // webrtc/rtc_base and make sure that it works the same way across platforms.
   // See also discussion here: https://codereview.webrtc.org/1623543002/
-  explicit Random(uint64_t seed);
+  explicit Random(uint64_t /*seed*/) {}
 
   Random() = delete;
   Random(const Random&) = delete;
   Random& operator=(const Random&) = delete;
 
-  // Return pseudo-random integer of the specified type.
-  // We need to limit the size to 32 bits to keep the output close to uniform.
   template <typename T>
-  T Rand() {
-    static_assert(std::numeric_limits<T>::is_integer &&
-                      std::numeric_limits<T>::radix == 2 &&
-                      std::numeric_limits<T>::digits <= 32,
-                  "Rand is only supported for built-in integer types that are "
-                  "32 bits or smaller.");
-    return static_cast<T>(NextOutput());
+  typename std::enable_if_t<std::is_same_v<T, bool>, T> Rand() {
+    return absl::Bernoulli(bitgen_, 0.5);
+  }
+
+  template <typename T>
+  typename std::enable_if_t<std::is_integral_v<T> && !std::is_same_v<T, bool>,
+                            T>
+  Rand() {
+    return absl::Uniform<T>(absl::IntervalClosed, bitgen_,
+                            std::numeric_limits<T>::min(),
+                            std::numeric_limits<T>::max());
+  }
+
+  template <typename T>
+  typename std::enable_if_t<std::is_floating_point_v<T>, T> Rand() {
+    return absl::Uniform<T>(absl::IntervalClosed, bitgen_, 0.0, 1.0);
   }
 
   // Uniformly distributed pseudo-random number in the interval [0, t].
-  uint32_t Rand(uint32_t t);
-
-  // Uniformly distributed pseudo-random number in the interval [low, high].
-  uint32_t Rand(uint32_t low, uint32_t high);
-
-  // Uniformly distributed pseudo-random number in the interval [low, high].
-  int32_t Rand(int32_t low, int32_t high);
-
-  // Normal Distribution.
-  double Gaussian(double mean, double standard_deviation);
-
-  // Exponential Distribution.
-  double Exponential(double lambda);
-
- private:
-  // Outputs a nonzero 64-bit random number using Xorshift algorithm.
-  // https://en.wikipedia.org/wiki/Xorshift
-  uint64_t NextOutput() {
-    state_ ^= state_ >> 12;
-    state_ ^= state_ << 25;
-    state_ ^= state_ >> 27;
-    RTC_DCHECK(state_ != 0x0ULL);
-    return state_ * 2685821657736338717ull;
+  uint32_t Rand(uint32_t t) {
+    return absl::Uniform(absl::IntervalClosed, bitgen_, uint32_t{0}, t);
   }
 
-  uint64_t state_;
+  // Uniformly distributed pseudo-random number in the interval [low, high].
+  uint32_t Rand(uint32_t low, uint32_t high) {
+    return absl::Uniform(absl::IntervalClosed, bitgen_, low, high);
+  }
+
+  // Uniformly distributed pseudo-random number in the interval [low, high].
+  int32_t Rand(int32_t low, int32_t high) {
+    return absl::Uniform(absl::IntervalClosed, bitgen_, low, high);
+  }
+
+  // Normal Distribution.
+  double Gaussian(double mean, double standard_deviation) {
+    return absl::Gaussian(bitgen_, mean, standard_deviation);
+  }
+
+  // Exponential Distribution.
+  double Exponential(double lambda) {
+    return absl::Exponential(bitgen_, lambda);
+  }
+
+ private:
+  absl::BitGen bitgen_;
 };
-
-// Return pseudo-random number in the interval [0.0, 1.0).
-template <>
-float Random::Rand<float>();
-
-// Return pseudo-random number in the interval [0.0, 1.0).
-template <>
-double Random::Rand<double>();
-
-// Return pseudo-random boolean value.
-template <>
-bool Random::Rand<bool>();
 
 }  // namespace webrtc
 

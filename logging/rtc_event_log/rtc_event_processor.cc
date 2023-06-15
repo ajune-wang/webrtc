0fef15ae7791f8bb473d0a1d333f992e9dc53726
Björn Terelius <terelius@webrtc.org>
2023-06-15 18:03:24 +0200

    Allow custom tie breakers in RtcEventProcessor
    Bug:b/282153758
    Change-Id: I914e4500ca63e1a8754766d1833a7b32f6a38176

--- logging/rtc_event_log/rtc_event_processor.cc
+++ logging/rtc_event_log/rtc_event_processor.cc
@@ -12,30 +12,58 @@
 namespace webrtc {
 
 RtcEventProcessor::RtcEventProcessor() = default;
-
 RtcEventProcessor::~RtcEventProcessor() = default;
+
 void RtcEventProcessor::ProcessEventsInOrder() {
   // `event_lists_` is a min-heap of lists ordered by the timestamp of the
   // first element in the list. We therefore process the first element of the
   // first list, then reinsert the remainder of that list into the heap
   // if the list still contains unprocessed elements.
+  Comparator cmp;
+  std::make_heap(event_lists_.begin(), event_lists_.end(), cmp);
+
   while (!event_lists_.empty()) {
+    cmp.AdvanceTransportSeqNum(event_lists_.front());
     event_lists_.front()->ProcessNext();
-    std::pop_heap(event_lists_.begin(), event_lists_.end(), Cmp);
+    std::pop_heap(event_lists_.begin(), event_lists_.end(), cmp);
     if (event_lists_.back()->IsEmpty()) {
       event_lists_.pop_back();
     } else {
-      std::push_heap(event_lists_.begin(), event_lists_.end(), Cmp);
+      std::push_heap(event_lists_.begin(), event_lists_.end(), cmp);
     }
   }
 }
 
-bool RtcEventProcessor::Cmp(const RtcEventProcessor::ListPtrType& a,
-                            const RtcEventProcessor::ListPtrType& b) {
+bool RtcEventProcessor::Comparator::operator()(
+    const RtcEventProcessor::ListPtrType& a,
+    const RtcEventProcessor::ListPtrType& b) {
   int64_t time_diff = a->GetNextTime() - b->GetNextTime();
-  if (time_diff == 0)
-    return a->GetTieBreaker() > b->GetTieBreaker();
-  return time_diff > 0;
+  if (time_diff != 0)
+    return time_diff > 0;
+
+  if (a->GetTypeOrder() != b->GetTypeOrder())
+    return a->GetTypeOrder() > b->GetTypeOrder();
+
+  absl::optional<uint16_t> wrapped_seq_num_a = a->GetTransportSeqNum();
+  absl::optional<uint16_t> wrapped_seq_num_b = b->GetTransportSeqNum();
+  if (wrapped_seq_num_a && wrapped_seq_num_b) {
+    int64_t sequence_number_a = unwrapper_.PeekUnwrap(*wrapped_seq_num_a);
+    int64_t sequence_number_b = unwrapper_.PeekUnwrap(*wrapped_seq_num_b);
+    if (sequence_number_a != sequence_number_b)
+      return sequence_number_a > sequence_number_b;
+  } else if (wrapped_seq_num_a.has_value() != wrapped_seq_num_b.has_value()) {
+    return wrapped_seq_num_a.has_value();
+  }
+
+  return a->GetInsertionOrder() > b->GetInsertionOrder();
+}
+
+void RtcEventProcessor::Comparator::AdvanceTransportSeqNum(
+    const RtcEventProcessor::ListPtrType& a) {
+  absl::optional<uint16_t> wrapped_seq_num = a->GetTransportSeqNum();
+  if (wrapped_seq_num) {
+    unwrapper_.Unwrap(*wrapped_seq_num);
+  }
 }
 
 }  // namespace webrtc
--- logging/rtc_event_log/rtc_event_processor.h
+++ logging/rtc_event_log/rtc_event_processor.h
@@ -19,7 +19,10 @@
 #include <vector>
 
 #include "api/function_view.h"
+#include "logging/rtc_event_log/events/logged_rtp_rtcp.h"
+#include "logging/rtc_event_log/rtc_event_log_parser.h"
 #include "rtc_base/checks.h"
+#include "rtc_base/numerics/sequence_number_unwrapper.h"
 
 namespace webrtc {
 
@@ -30,7 +33,7 @@ namespace webrtc {
 
 namespace event_processor_impl {
 // Interface to allow "merging" lists of different types. ProcessNext()
-// processes the next unprocesses element in the list. IsEmpty() checks if all
+// processes the next unprocessed element in the list. IsEmpty() checks if all
 // elements have been processed. GetNextTime returns the timestamp of the next
 // unprocessed element.
 class ProcessableEventListInterface {
@@ -39,7 +42,9 @@ class ProcessableEventListInterface {
   virtual void ProcessNext() = 0;
   virtual bool IsEmpty() const = 0;
   virtual int64_t GetNextTime() const = 0;
-  virtual int GetTieBreaker() const = 0;
+  virtual int GetTypeOrder() const = 0;
+  virtual absl::optional<uint16_t> GetTransportSeqNum() const = 0;
+  virtual int GetInsertionOrder() const = 0;
 };
 
 // ProcessableEventList encapsulates a list of events and a function that will
@@ -50,8 +55,16 @@ class ProcessableEventList : public ProcessableEventListInterface {
   ProcessableEventList(Iterator begin,
                        Iterator end,
                        std::function<void(const T&)> f,
-                       int tie_breaker)
-      : begin_(begin), end_(end), f_(f), tie_breaker_(tie_breaker) {}
+                       int type_order,
+                       std::function<absl::optional<uint16_t>(const T&)>
+                           transport_seq_num_accessor,
+                       int insertion_order)
+      : begin_(begin),
+        end_(end),
+        f_(f),
+        type_order_(type_order),
+        transport_seq_num_accessor_(transport_seq_num_accessor),
+        insertion_order_(insertion_order) {}
 
   void ProcessNext() override {
     RTC_DCHECK(!IsEmpty());
@@ -65,15 +78,439 @@ class ProcessableEventList : public ProcessableEventListInterface {
     RTC_DCHECK(!IsEmpty());
     return begin_->log_time_us();
   }
-  int GetTieBreaker() const override { return tie_breaker_; }
+
+  int GetTypeOrder() const override { return type_order_; }
+
+  absl::optional<uint16_t> GetTransportSeqNum() const override {
+    RTC_DCHECK(!IsEmpty());
+    return transport_seq_num_accessor_(*begin_);
+  }
+
+  int GetInsertionOrder() const override { return insertion_order_; }
 
  private:
   Iterator begin_;
   Iterator end_;
   std::function<void(const T&)> f_;
-  int tie_breaker_;
+  int type_order_;
+  std::function<absl::optional<uint16_t>(const T&)> transport_seq_num_accessor_;
+  int insertion_order_;
 };
+
 }  // namespace event_processor_impl
+
+// The RTC event log only uses millisecond precision timestamps
+// and doesn't preserve order between events in different batches.
+// This is heuristic
+enum class TypeOrder {
+  Start,
+  // Connectivity and stream configurations before incoming packets
+  StreamConfig,
+  IceCondidateConfig,
+  IceCandidateEvent,
+  DtlsTransportState,
+  DtlsWritable,
+  RouteChange,
+  // Incoming packets
+  RtpIn,
+  RtcpIn,
+  GenericPacketIn,
+  GenericAckIn,
+  // BWE depends on incoming feedback (send side estimation)
+  // or incoming media packets (receive side estimation).
+  // Delay-based BWE depends on probe results.
+  // Loss-based BWE depends on delay-based BWE.
+  // Loss-based BWE may trigger new probes.
+  BweRemoteEstimate,
+  BweProbeFailure,
+  BweProbeSuccess,
+  BweDelayBased,
+  BweLossBased,
+  BweProbeCreated,
+  // General processing events. No obvious order.
+  AudioNetworkAdaptation,
+  NetEqSetMinDelay,
+  AudioPlayout,
+  FrameDecoded,
+  // Outgoing packets and feedback depends on BWE and might depend on
+  // processing.
+  RtpOut,
+  RtcpOut,
+  GenericPacketOut,
+  // Alr is updated after a packet is sent.
+  AlrState,
+  Stop,
+};
+
+template <typename T>
+class TieBreaker {
+  static_assert(sizeof(T) != sizeof(T),
+                "Specialize TieBreaker to define an order for the event type.");
+};
+
+template <>
+class TieBreaker<LoggedStartEvent> {
+ public:
+  static constexpr int type_order = static_cast<int>(TypeOrder::Start);
+  static absl::optional<uint16_t> transport_seq_num_accessor(
+      const LoggedStartEvent&) {
+    return absl::optional<uint16_t>();
+  }
+};
+
+template <>
+class TieBreaker<LoggedStopEvent> {
+ public:
+  static constexpr int type_order = static_cast<int>(TypeOrder::Start);
+  static absl::optional<uint16_t> transport_seq_num_accessor(
+      const LoggedStopEvent&) {
+    return absl::optional<uint16_t>();
+  }
+};
+
+template <>
+class TieBreaker<LoggedAudioRecvConfig> {
+ public:
+  static constexpr int type_order = static_cast<int>(TypeOrder::StreamConfig);
+  static absl::optional<uint16_t> transport_seq_num_accessor(
+      const LoggedAudioRecvConfig&) {
+    return absl::optional<uint16_t>();
+  }
+};
+
+template <>
+class TieBreaker<LoggedAudioSendConfig> {
+ public:
+  static constexpr int type_order = static_cast<int>(TypeOrder::StreamConfig);
+  static absl::optional<uint16_t> transport_seq_num_accessor(
+      const LoggedAudioSendConfig&) {
+    return absl::optional<uint16_t>();
+  }
+};
+
+template <>
+class TieBreaker<LoggedVideoRecvConfig> {
+ public:
+  static constexpr int type_order = static_cast<int>(TypeOrder::StreamConfig);
+  static absl::optional<uint16_t> transport_seq_num_accessor(
+      const LoggedVideoRecvConfig&) {
+    return absl::optional<uint16_t>();
+  }
+};
+
+template <>
+class TieBreaker<LoggedVideoSendConfig> {
+ public:
+  static constexpr int type_order = static_cast<int>(TypeOrder::StreamConfig);
+  static absl::optional<uint16_t> transport_seq_num_accessor(
+      const LoggedVideoSendConfig&) {
+    return absl::optional<uint16_t>();
+  }
+};
+
+template <>
+class TieBreaker<LoggedIceCandidatePairConfig> {
+ public:
+  static constexpr int type_order =
+      static_cast<int>(TypeOrder::IceCondidateConfig);
+  static absl::optional<uint16_t> transport_seq_num_accessor(
+      const LoggedIceCandidatePairConfig&) {
+    return absl::optional<uint16_t>();
+  }
+};
+
+template <>
+class TieBreaker<LoggedIceCandidatePairEvent> {
+ public:
+  static constexpr int type_order =
+      static_cast<int>(TypeOrder::IceCandidateEvent);
+  static absl::optional<uint16_t> transport_seq_num_accessor(
+      const LoggedIceCandidatePairEvent&) {
+    return absl::optional<uint16_t>();
+  }
+};
+
+template <>
+class TieBreaker<LoggedDtlsTransportState> {
+ public:
+  static constexpr int type_order =
+      static_cast<int>(TypeOrder::DtlsTransportState);
+  static absl::optional<uint16_t> transport_seq_num_accessor(
+      const LoggedDtlsTransportState&) {
+    return absl::optional<uint16_t>();
+  }
+};
+
+template <>
+class TieBreaker<LoggedDtlsWritableState> {
+ public:
+  static constexpr int type_order = static_cast<int>(TypeOrder::DtlsWritable);
+  static absl::optional<uint16_t> transport_seq_num_accessor(
+      const LoggedDtlsWritableState&) {
+    return absl::optional<uint16_t>();
+  }
+};
+
+template <>
+class TieBreaker<LoggedRouteChangeEvent> {
+ public:
+  static constexpr int type_order = static_cast<int>(TypeOrder::RouteChange);
+  static absl::optional<uint16_t> transport_seq_num_accessor(
+      const LoggedRouteChangeEvent&) {
+    return absl::optional<uint16_t>();
+  }
+};
+
+template <>
+class TieBreaker<LoggedRemoteEstimateEvent> {
+ public:
+  static constexpr int type_order =
+      static_cast<int>(TypeOrder::BweRemoteEstimate);
+  static absl::optional<uint16_t> transport_seq_num_accessor(
+      const LoggedRemoteEstimateEvent&) {
+    return absl::optional<uint16_t>();
+  }
+};
+
+template <>
+class TieBreaker<LoggedBweProbeFailureEvent> {
+ public:
+  static constexpr int type_order =
+      static_cast<int>(TypeOrder::BweProbeFailure);
+  static absl::optional<uint16_t> transport_seq_num_accessor(
+      const LoggedBweProbeFailureEvent&) {
+    return absl::optional<uint16_t>();
+  }
+};
+
+template <>
+class TieBreaker<LoggedBweProbeSuccessEvent> {
+ public:
+  static constexpr int type_order =
+      static_cast<int>(TypeOrder::BweProbeSuccess);
+  static absl::optional<uint16_t> transport_seq_num_accessor(
+      const LoggedBweProbeSuccessEvent&) {
+    return absl::optional<uint16_t>();
+  }
+};
+
+template <>
+class TieBreaker<LoggedBweDelayBasedUpdate> {
+ public:
+  static constexpr int type_order = static_cast<int>(TypeOrder::BweDelayBased);
+  static absl::optional<uint16_t> transport_seq_num_accessor(
+      const LoggedBweDelayBasedUpdate&) {
+    return absl::optional<uint16_t>();
+  }
+};
+
+template <>
+class TieBreaker<LoggedBweLossBasedUpdate> {
+ public:
+  static constexpr int type_order = static_cast<int>(TypeOrder::BweLossBased);
+  static absl::optional<uint16_t> transport_seq_num_accessor(
+      const LoggedBweLossBasedUpdate&) {
+    return absl::optional<uint16_t>();
+  }
+};
+
+template <>
+class TieBreaker<LoggedBweProbeClusterCreatedEvent> {
+ public:
+  static constexpr int type_order =
+      static_cast<int>(TypeOrder::BweProbeCreated);
+  static absl::optional<uint16_t> transport_seq_num_accessor(
+      const LoggedBweProbeClusterCreatedEvent&) {
+    return absl::optional<uint16_t>();
+  }
+};
+
+template <>
+class TieBreaker<LoggedAudioNetworkAdaptationEvent> {
+ public:
+  static constexpr int type_order =
+      static_cast<int>(TypeOrder::AudioNetworkAdaptation);
+  static absl::optional<uint16_t> transport_seq_num_accessor(
+      const LoggedAudioNetworkAdaptationEvent&) {
+    return absl::optional<uint16_t>();
+  }
+};
+
+template <>
+class TieBreaker<LoggedNetEqSetMinimumDelayEvent> {
+ public:
+  static constexpr int type_order =
+      static_cast<int>(TypeOrder::NetEqSetMinDelay);
+  static absl::optional<uint16_t> transport_seq_num_accessor(
+      const LoggedNetEqSetMinimumDelayEvent&) {
+    return absl::optional<uint16_t>();
+  }
+};
+
+template <>
+class TieBreaker<LoggedAudioPlayoutEvent> {
+ public:
+  static constexpr int type_order = static_cast<int>(TypeOrder::AudioPlayout);
+  static absl::optional<uint16_t> transport_seq_num_accessor(
+      const LoggedAudioPlayoutEvent&) {
+    return absl::optional<uint16_t>();
+  }
+};
+
+template <>
+class TieBreaker<LoggedFrameDecoded> {
+ public:
+  static constexpr int type_order = static_cast<int>(TypeOrder::FrameDecoded);
+  static absl::optional<uint16_t> transport_seq_num_accessor(
+      const LoggedFrameDecoded&) {
+    return absl::optional<uint16_t>();
+  }
+};
+
+template <>
+class TieBreaker<LoggedGenericPacketReceived> {
+ public:
+  static constexpr int type_order =
+      static_cast<int>(TypeOrder::GenericPacketIn);
+  static absl::optional<uint16_t> transport_seq_num_accessor(
+      const LoggedGenericPacketReceived&) {
+    return absl::optional<uint16_t>();
+  }
+};
+
+template <>
+class TieBreaker<LoggedGenericAckReceived> {
+ public:
+  static constexpr int type_order = static_cast<int>(TypeOrder::GenericAckIn);
+  static absl::optional<uint16_t> transport_seq_num_accessor(
+      const LoggedGenericAckReceived&) {
+    return absl::optional<uint16_t>();
+  }
+};
+
+template <>
+class TieBreaker<LoggedGenericPacketSent> {
+ public:
+  static constexpr int type_order =
+      static_cast<int>(TypeOrder::GenericPacketOut);
+  static absl::optional<uint16_t> transport_seq_num_accessor(
+      const LoggedGenericPacketSent&) {
+    return absl::optional<uint16_t>();
+  }
+};
+
+template <>
+class TieBreaker<LoggedRtpPacket> {
+ public:
+  static constexpr int type_order(PacketDirection direction) {
+    return static_cast<int>(direction == PacketDirection::kIncomingPacket
+                                ? TypeOrder::RtpIn
+                                : TypeOrder::RtpOut);
+  }
+  static absl::optional<uint16_t> transport_seq_num_accessor(
+      const LoggedRtpPacket& p) {
+    return p.header.extension.hasTransportSequenceNumber
+               ? p.header.extension.transportSequenceNumber
+               : absl::optional<uint16_t>();
+  }
+};
+
+template <>
+class TieBreaker<LoggedPacketInfo> {
+ public:
+  static constexpr int type_order(PacketDirection direction) {
+    return static_cast<int>(direction == PacketDirection::kIncomingPacket
+                                ? TypeOrder::RtpIn
+                                : TypeOrder::RtpOut);
+  }
+  static absl::optional<uint16_t> transport_seq_num_accessor(
+      const LoggedPacketInfo& p) {
+    return p.has_transport_seq_no ? p.transport_seq_no
+                                  : absl::optional<uint16_t>();
+  }
+};
+
+template <>
+class TieBreaker<LoggedRtpPacketIncoming> {
+ public:
+  static constexpr int type_order = static_cast<int>(TypeOrder::RtpIn);
+  static absl::optional<uint16_t> transport_seq_num_accessor(
+      const LoggedRtpPacketIncoming& p) {
+    return p.rtp.header.extension.hasTransportSequenceNumber
+               ? p.rtp.header.extension.transportSequenceNumber
+               : absl::optional<uint16_t>();
+  }
+};
+
+template <>
+class TieBreaker<LoggedRtpPacketOutgoing> {
+ public:
+  static constexpr int type_order = static_cast<int>(TypeOrder::RtpOut);
+  static absl::optional<uint16_t> transport_seq_num_accessor(
+      const LoggedRtpPacketOutgoing& p) {
+    return p.rtp.header.extension.hasTransportSequenceNumber
+               ? p.rtp.header.extension.transportSequenceNumber
+               : absl::optional<uint16_t>();
+  }
+};
+
+template <>
+class TieBreaker<LoggedRtcpPacketIncoming> {
+ public:
+  static constexpr int type_order = static_cast<int>(TypeOrder::RtcpIn);
+  static absl::optional<uint16_t> transport_seq_num_accessor(
+      const LoggedRtcpPacketIncoming&) {
+    return absl::optional<uint16_t>();
+  }
+};
+
+template <>
+class TieBreaker<LoggedRtcpPacketOutgoing> {
+ public:
+  static constexpr int type_order = static_cast<int>(TypeOrder::RtcpOut);
+  static absl::optional<uint16_t> transport_seq_num_accessor(
+      const LoggedRtcpPacketOutgoing&) {
+    return absl::optional<uint16_t>();
+  }
+};
+
+template <>
+class TieBreaker<LoggedRtcpPacketTransportFeedback> {
+ public:
+  static constexpr int type_order(PacketDirection direction) {
+    return static_cast<int>(direction == PacketDirection::kIncomingPacket
+                                ? TypeOrder::RtcpIn
+                                : TypeOrder::RtcpOut);
+  }
+  static absl::optional<uint16_t> transport_seq_num_accessor(
+      const LoggedRtcpPacketTransportFeedback&) {
+    return absl::optional<uint16_t>();
+  }
+};
+
+template <>
+class TieBreaker<LoggedRtcpPacketReceiverReport> {
+ public:
+  static constexpr int type_order(PacketDirection direction) {
+    return static_cast<int>(direction == PacketDirection::kIncomingPacket
+                                ? TypeOrder::RtcpIn
+                                : TypeOrder::RtcpOut);
+  }
+  static absl::optional<uint16_t> transport_seq_num_accessor(
+      const LoggedRtcpPacketReceiverReport&) {
+    return absl::optional<uint16_t>();
+  }
+};
+
+template <>
+class TieBreaker<LoggedAlrStateEvent> {
+ public:
+  static constexpr int type_order = static_cast<int>(TypeOrder::AlrState);
+  static absl::optional<uint16_t> transport_seq_num_accessor(
+      const LoggedAlrStateEvent&) {
+    return absl::optional<uint16_t>();
+  }
+};
 
 // Helper class used to "merge" two or more lists of ordered RtcEventLog events
 // so that they can be treated as a single ordered list. Since the individual
@@ -105,14 +542,41 @@ class RtcEventProcessor {
   void AddEvents(
       const Iterable& iterable,
       std::function<void(const typename Iterable::value_type&)> handler) {
+    using ValueType =
+        typename std::remove_const<typename Iterable::value_type>::type;
+    AddEvents(iterable, handler, TieBreaker<ValueType>::type_order,
+              TieBreaker<ValueType>::transport_seq_num_accessor,
+              num_insertions_);
+  }
+
+  template <typename Iterable>
+  void AddEvents(
+      const Iterable& iterable,
+      std::function<void(const typename Iterable::value_type&)> handler,
+      PacketDirection direction) {
+    using ValueType =
+        typename std::remove_const<typename Iterable::value_type>::type;
+    AddEvents(iterable, handler, TieBreaker<ValueType>::type_order(direction),
+              TieBreaker<ValueType>::transport_seq_num_accessor,
+              num_insertions_);
+  }
+
+  template <typename Iterable>
+  void AddEvents(
+      const Iterable& iterable,
+      std::function<void(const typename Iterable::value_type&)> handler,
+      int type_order,
+      std::function<absl::optional<uint16_t>(
+          const typename Iterable::value_type&)> transport_seq_num_accessor,
+      int insertion_order) {
     if (iterable.begin() == iterable.end())
       return;
+    num_insertions_++;
     event_lists_.push_back(
         std::make_unique<event_processor_impl::ProcessableEventList<
             typename Iterable::const_iterator, typename Iterable::value_type>>(
-            iterable.begin(), iterable.end(), handler,
-            insertion_order_index_++));
-    std::push_heap(event_lists_.begin(), event_lists_.end(), Cmp);
+            iterable.begin(), iterable.end(), handler, type_order,
+            transport_seq_num_accessor, insertion_order));
   }
 
   void ProcessEventsInOrder();
@@ -120,10 +584,19 @@ class RtcEventProcessor {
  private:
   using ListPtrType =
       std::unique_ptr<event_processor_impl::ProcessableEventListInterface>;
-  int insertion_order_index_ = 0;
-  std::vector<ListPtrType> event_lists_;
+
   // Comparison function to make `event_lists_` into a min heap.
-  static bool Cmp(const ListPtrType& a, const ListPtrType& b);
+  class Comparator {
+   public:
+    bool operator()(const ListPtrType& a, const ListPtrType& b);
+    void AdvanceTransportSeqNum(const ListPtrType& a);
+
+   private:
+    RtpSequenceNumberUnwrapper unwrapper_;
+  };
+
+  std::vector<ListPtrType> event_lists_;
+  int num_insertions_ = 0;
 };
 
 }  // namespace webrtc

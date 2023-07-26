dc2ba6276e1b880592c0970cb36fd80197ff32e0
Mirko Bonadei <mbonadei@webrtc.org>
2023-07-26 12:00:57 +0200

    Add possibility to set RTC_OBJC_TYPE_PREFIX from GN.
    Bug: b/292472934
    Change-Id: If63020e679c8670b4c797217eb38fc8c2954d422

--- sdk/objc/base/RTCMacros.h
+++ sdk/objc/base/RTCMacros.h
@@ -38,7 +38,9 @@
 //
 // This macro must only be defined here and not on via compiler flag to
 // ensure it has a unique value.
+#ifndef RTC_OBJC_TYPE_PREFIX
 #define RTC_OBJC_TYPE_PREFIX
+#endif
 
 // RCT_OBJC_TYPE
 //

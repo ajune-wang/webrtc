6bda550af1d13676af0d5c8f26d47ea2be25b8ac
Harald Alvestrand <hta@webrtc.org>
2023-08-22 11:39:34 +0000

    Add an async DNS resolver suitable for defaulting.
    This should replace the wrapping async DNS resolver used
    for default resolution.
    
    Bug: webrtc:12598
    Change-Id: Ic65ecd17da7a5695d0003178aeb30824a707ec78

---
+++ rtc_base/async_dns_resolver_unittest.cc
@@ -0,0 +1,40 @@
+/*
+ *  Copyright 2023 The WebRTC Project Authors. All rights reserved.
+ *
+ *  Use of this source code is governed by a BSD-style license
+ *  that can be found in the LICENSE file in the root of the source
+ *  tree. An additional intellectual property rights grant can be found
+ *  in the file PATENTS.  All contributing project authors may
+ *  be found in the AUTHORS file in the root of the source tree.
+ */
+
+#include "rtc_base/async_dns_resolver.h"
+
+#include "rtc_base/gunit.h"
+#include "test/gtest.h"
+
+namespace webrtc {
+namespace {
+const int kDefaultTimeout = 1000;
+const int kPortNumber = 3027;
+
+TEST(AsyncDnsResolver, ConstructorWorks) {
+  AsyncDnsResolver resolver;
+}
+
+TEST(AsyncDnsResolver, ResolvingLocalhostWorks) {
+  rtc::ThreadManager::Instance()->WrapCurrentThread();
+  AsyncDnsResolver resolver;
+  rtc::SocketAddress address("localhost",
+                             kPortNumber);  // Port number does not matter
+  rtc::SocketAddress resolved_address;
+  bool done = false;
+  resolver.Start(address, [&done] { done = true; });
+  ASSERT_TRUE_WAIT(done, kDefaultTimeout);
+  EXPECT_EQ(resolver.result().GetError(), 0);
+  EXPECT_TRUE(resolver.result().GetResolvedAddress(AF_INET, &resolved_address));
+  EXPECT_EQ(resolved_address, rtc::SocketAddress("127.0.0.1", kPortNumber));
+}
+
+}  // namespace
+}  // namespace webrtc

ad0a71406c0e8c764de0e55e6d973bd6e77bef08
Jeremy Leconte <jleconte@google.com>
2023-09-14 14:17:49 +0200

    Disbale XCTests relying on OCMVerify.
    Change-Id: I09d045ee6f6f55bb9d96b9e33fb064297ca20e60
    Bug: b/298960678

--- sdk/objc/unittests/RTCAudioSessionTest.mm
+++ sdk/objc/unittests/RTCAudioSessionTest.mm
@@ -9,8 +9,17 @@
  */
 
 #import <Foundation/Foundation.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
 #import <OCMock/OCMock.h>
+#ifdef __cplusplus
+}
+#endif
+
 #import <XCTest/XCTest.h>
+#include <climits>
 
 #include <vector>
 
@@ -212,19 +221,8 @@
   EXPECT_EQ(0, audioSession.activationCount);
 }
 
-// Hack - fixes OCMVerify link error
-// Link error is: Undefined symbols for architecture i386:
-// "OCMMakeLocation(objc_object*, char const*, int)", referenced from:
-// -[RTCAudioSessionTest testConfigureWebRTCSession] in RTCAudioSessionTest.o
-// ld: symbol(s) not found for architecture i386
-// REASON: https://github.com/erikdoe/ocmock/issues/238
-OCMLocation *OCMMakeLocation(id testCase, const char *fileCString, int line){
-  return [OCMLocation locationWithTestCase:testCase
-                                      file:[NSString stringWithUTF8String:fileCString]
-                                      line:line];
-}
-
-- (void)testConfigureWebRTCSession {
+// TODO(b/298960678): Test currently crashes.
+- (void)DISABLED_testConfigureWebRTCSession {
   NSError *error = nil;
 
   void (^setActiveBlock)(NSInvocation *invocation) = ^(NSInvocation *invocation) {

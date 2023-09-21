1d3272668d3fe3500627dd0c1183d562be5fcb7c
Diep Bui <diepbp@webrtc.org>
2023-09-21 08:50:02 +0000

    Use loss based bwe v2 in the start phase.
    TESTED=manual before:screen/ANtkMApoYczA2V5; after:screen/9kBoSvYKzKZR4sK
    
    Bug: webrtc:12707
    Change-Id: Ic156e363625c4b7476011059f3cd95641972091c

--- modules/congestion_controller/goog_cc/loss_based_bwe_v2.cc
+++ modules/congestion_controller/goog_cc/loss_based_bwe_v2.cc
@@ -136,6 +136,10 @@ bool LossBasedBweV2::IsReady() const {
          num_observations_ > 0;
 }
 
+bool LossBasedBweV2::UseInStartPhase() const {
+  return IsEnabled() && config_->use_in_start_phase;
+}
+
 LossBasedBweV2::Result LossBasedBweV2::GetLossBasedResult() const {
   Result result;
   result.state = current_state_;
@@ -418,6 +422,7 @@ absl::optional<LossBasedBweV2::Config> LossBasedBweV2::CreateConfig(
                                                   TimeDelta::Seconds(10));
   FieldTrialParameter<bool> not_use_acked_rate_in_alr("NotUseAckedRateInAlr",
                                                       false);
+  FieldTrialParameter<bool> use_in_start_phase("UseInStartPhase", false);
   if (key_value_config) {
     ParseFieldTrial({&enabled,
                      &bandwidth_rampup_upper_bound_factor,
@@ -455,7 +460,12 @@ absl::optional<LossBasedBweV2::Config> LossBasedBweV2::CreateConfig(
                      &high_loss_rate_threshold,
                      &bandwidth_cap_at_high_loss_rate,
                      &slope_of_bwe_high_loss_func,
+<<<<<<< PATCH SET (b6cf30 Use loss based bwe v2 in the start phase.)
+                     &not_use_acked_rate_in_alr,
+                     &use_in_start_phase},
+=======
                      &not_use_acked_rate_in_alr},
+>>>>>>> BASE      (7ee64b Remove the upper link capacity usage in the loss based bwe.)
                     key_value_config->Lookup("WebRTC-Bwe-LossBasedBweV2"));
   }
 
@@ -518,6 +528,7 @@ absl::optional<LossBasedBweV2::Config> LossBasedBweV2::CreateConfig(
   config->probe_integration_enabled = probe_integration_enabled.Get();
   config->probe_expiration = probe_expiration.Get();
   config->not_use_acked_rate_in_alr = not_use_acked_rate_in_alr.Get();
+  config->use_in_start_phase = use_in_start_phase.Get();
 
   return config;
 }

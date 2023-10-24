c8986531521ac9c07e263a72dbaab12af246b551
Diep Bui <diepbp@webrtc.org>
2023-10-24 08:22:20 +0000

    Add 1s as padding duration limit in loss based BWE.
    If we have been sending padding for 1s and estimate still is unchanged, then stop padding by transitioning to decrease state.
    
    Bug: webrtc:12707
    Change-Id: I0dca2e5cd98263fc7fae9882c23c21634413c7a0

--- modules/congestion_controller/goog_cc/loss_based_bwe_v2.h
+++ modules/congestion_controller/goog_cc/loss_based_bwe_v2.h
@@ -118,8 +118,8 @@ class LossBasedBweV2 {
     bool use_in_start_phase = false;
     int min_num_observations = 0;
     double lower_bound_by_acked_rate_factor = 0.0;
-    bool use_padding_for_increase = false;
     double hold_duration_factor = 0.0;
+    TimeDelta padding_duration = TimeDelta::Zero();
   };
 
   struct Derivatives {
@@ -141,6 +141,11 @@ class LossBasedBweV2 {
     int num_packets = 0;
     int num_lost_packets = 0;
     DataSize size = DataSize::Zero();
+  };
+
+  struct PaddingInfo {
+    DataRate padding_rate = DataRate::MinusInfinity();
+    Timestamp padding_timestamp = Timestamp::MinusInfinity();
   };
 
   static absl::optional<Config> CreateConfig(
@@ -173,6 +178,7 @@ class LossBasedBweV2 {
   bool IsEstimateIncreasingWhenLossLimited(DataRate old_estimate,
                                            DataRate new_estimate);
   bool IsInLossLimitedState() const;
+  bool CanKeepIncreasingState(DataRate estimate) const;
 
   absl::optional<DataRate> acknowledged_bitrate_;
   absl::optional<Config> config_;
@@ -194,6 +200,7 @@ class LossBasedBweV2 {
   LossBasedBweV2::Result loss_based_result_ = LossBasedBweV2::Result();
   Timestamp last_hold_timestamp_ = Timestamp::MinusInfinity();
   TimeDelta hold_duration_ = TimeDelta::Zero();
+  PaddingInfo last_padding_info_ = PaddingInfo();
 };
 
 }  // namespace webrtc

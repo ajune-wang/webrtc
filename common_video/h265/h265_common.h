ceb438f257455afe3d159bb8c701e7b3ce5849fa
qwu16 <qiujiao.wu@intel.com>
2023-11-01 15:22:57 +0800

    Add RTP depacketizer for H265
    Add rtp packetizer for H265
    
    Bug: webrtc:13485
    Change-Id: I876b3d7027ef89a5a9806a1dc3e6521c7bc080a3

--- common_video/h265/h265_common.h
+++ common_video/h265/h265_common.h
@@ -55,8 +55,12 @@ enum NaluType : uint8_t {
   kAud = 35,
   kPrefixSei = 39,
   kSuffixSei = 40,
-  kAP = 48,
-  kFU = 49
+  // Aggregation packets, defined in 4.4.2 of RTP HEVC spec RFC 7798
+  kAp = 48,
+  // Fragmentation units, defined in 4.4.3 of RTP HEVC spec RFC 7798
+  kFu = 49,
+  // PACI packets, defined in 4.4.4 of RTP HEVC spec RFC 7798
+  kPaci = 50
 };
 
 // Slice type definition. See table 7-7 of the H265 spec

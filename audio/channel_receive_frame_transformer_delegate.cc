263fd5de51b95d617b63945b47f60dcafee93a97
Tony Herre <herre@google.com>
2023-11-27 10:26:47 +0100

    Remove DCHECK on setting audio rcvr encoded transform twice
    Failing a DCHECK on a ChannelReceiver having its encoded transform set
    more than once contradicts the comment above - this can happen when
    reconfiguring a channel, eg as in the web platform test
    webrtc/recvonly-transceiver-can-become-sendrecv.https.html.
    
    It was added after the original code by a different author, and indeed
    the video side doesn't have such a check.
    
    Bug: chromium:1502781
    Change-Id: Id36e52601da34ebc194ff058e4672046379f576e

--- audio/channel_receive_frame_transformer_delegate.cc
+++ audio/channel_receive_frame_transformer_delegate.cc
@@ -145,4 +145,11 @@ void ChannelReceiveFrameTransformerDelegate::ReceiveFrame(
   // originally from this receiver.
   receive_frame_callback_(frame->GetData(), header);
 }
+
+rtc::scoped_refptr<FrameTransformerInterface>
+ChannelReceiveFrameTransformerDelegate::FrameTransformer() {
+  RTC_DCHECK_RUN_ON(&sequence_checker_);
+  return frame_transformer_;
+}
+
 }  // namespace webrtc

2f8690545c430b5f919d5a451b583d38eff4389c
Philipp Hancke <phancke@microsoft.com>
2023-12-12 13:15:03 +0100

    sdp: make msid support parsing more robust
    by also taking into account any a=msid: line in addition to
    msid-semantic. Also document issues with msid-semantic generation and unify support determination by removing the msid_supported flag.
    
    BUG=webrtc:10421
    
    Change-Id: Icea554ebd1998f2b526846457029eff6854a772a

--- pc/peer_connection_interface_unittest.cc
+++ pc/peer_connection_interface_unittest.cc
@@ -2815,7 +2815,7 @@ TEST_F(PeerConnectionInterfaceTestPlanB,
 // This tests that a default MediaStream is not created if a remote session
 // description is updated to not have any MediaStreams.
 // Don't run under Unified Plan since this behavior is Plan B specific.
-TEST_F(PeerConnectionInterfaceTestPlanB, VerifyDefaultStreamIsNotCreated) {
+TEST_F(PeerConnectionInterfaceTestPlanB, VerifyDefaultStreamIsNotRecreated) {
   RTCConfiguration config;
   CreatePeerConnection(config);
   CreateAndSetRemoteOffer(GetSdpStringWithStream1());
@@ -2823,7 +2823,7 @@ TEST_F(PeerConnectionInterfaceTestPlanB, VerifyDefaultStreamIsNotCreated) {
   EXPECT_TRUE(
       CompareStreamCollections(observer_.remote_streams(), reference.get()));
 
-  CreateAndSetRemoteOffer(kSdpStringWithoutStreams);
+  CreateAndSetRemoteOffer(kSdpStringWithMsidWithoutStreams);
   EXPECT_EQ(0u, observer_.remote_streams()->count());
 }
 

2fd8b4d9db8c9c5f8603a284b975256075f259bf
Henrik Boström <hbos@webrtc.org>
2024-01-08 15:16:01 +0100

    [Stats] Introduce Attribute, implementing RTCStatsMemberInterface.
    The member interface abstraction is needed because of iterating the
    RTCStats::Members() without knowing name or type at run-time. But most
    use cases of getStats (including Chrome's exposure to JS) does not make
    use of this abstraction, referring to the RTCStatsMember<T>s variables
    directly. They're used exactly like optional variables!
    
    So the plan is to replace RTCStatsMember<T> with absl::optional<T>. We
    do this in two steps to avoid breaking RTCStats::Members().
    
    Step 1 (this CL):
      Wrap all RTCStatsMember<T>s inside Attribute when Members() is called.
      The Attribute is just a thin wrapper pointing to the stat.
    
    Step 2 (next CL):
      Replace RTCStatsMember<T> with absl::optional<T>, updating Attribute
      to internally handle ptr to optional. Delete RTCStatsMember<T>.
    
    During this transitions, the existing uses of Members() will neither
    know or care that the members are being converted into optionals.
    
    In a future CL (step 3) we will replace Members() with an Attributes()
    and change the return value to `std::vector<Attribute>` and delete the
    abstraction "member interface" abstraction layer.
    
    Bug: webrtc:15164
    Change-Id: I3fdd5b24214bb5cc340a54a0171df73b516e1803

---
+++ stats/attribute.cc
@@ -0,0 +1,76 @@
+/*
+ *  Copyright 2024 The WebRTC Project Authors. All rights reserved.
+ *
+ *  Use of this source code is governed by a BSD-style license
+ *  that can be found in the LICENSE file in the root of the source
+ *  tree. An additional intellectual property rights grant can be found
+ *  in the file PATENTS.  All contributing project authors may
+ *  be found in the AUTHORS file in the root of the source tree.
+ */
+
+#include "stats/attribute.h"
+
+#include "absl/types/variant.h"
+#include "rtc_base/checks.h"
+
+namespace webrtc {
+
+namespace {
+
+struct VisitIsSequence {
+  // Any type of vector is a sequence.
+  template <typename T>
+  bool operator()(const RTCStatsMember<std::vector<T>>* attribute) {
+    return true;
+  }
+  // Any other type is not.
+  template <typename T>
+  bool operator()(const RTCStatsMember<T>* attribute) {
+    return false;
+  }
+};
+
+struct VisitIsEqual {
+  template <typename T>
+  bool operator()(const RTCStatsMember<T>* attribute) {
+    return attribute->IsEqual(other);
+  }
+
+  const RTCStatsMemberInterface& other;
+};
+
+}  // namespace
+
+RTCStatsMemberInterface::Type Attribute::type() const {
+  return absl::visit([](const auto* attr) { return attr->type(); }, attribute_);
+}
+
+bool Attribute::is_sequence() const {
+  return absl::visit(VisitIsSequence(), attribute_);
+}
+
+bool Attribute::is_string() const {
+  return absl::holds_alternative<const RTCStatsMember<std::string>*>(
+      attribute_);
+}
+
+bool Attribute::is_defined() const {
+  return absl::visit([](const auto* attr) { return attr->is_defined(); },
+                     attribute_);
+}
+
+std::string Attribute::ValueToString() const {
+  return absl::visit([](const auto* attr) { return attr->ValueToString(); },
+                     attribute_);
+}
+
+std::string Attribute::ValueToJson() const {
+  return absl::visit([](const auto* attr) { return attr->ValueToJson(); },
+                     attribute_);
+}
+
+bool Attribute::IsEqual(const RTCStatsMemberInterface& other) const {
+  return absl::visit(VisitIsEqual{.other = other}, attribute_);
+}
+
+}  // namespace webrtc

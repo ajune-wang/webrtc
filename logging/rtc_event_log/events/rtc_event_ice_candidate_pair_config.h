636c9f71d84a95b2d6e875f1bd7802aab53313fe
Tommi <tommi@webrtc.org>
2024-01-29 10:28:50 +0100

    Rename kLocal to kHost and kStun to kSrflx
    Bug: none
    Change-Id: I92845014024e5780365057e81b613b0882724d1d

--- logging/rtc_event_log/events/rtc_event_ice_candidate_pair_config.h
+++ logging/rtc_event_log/events/rtc_event_ice_candidate_pair_config.h
@@ -32,11 +32,10 @@ enum class IceCandidatePairConfigType {
   kNumValues,
 };
 
-// TODO(tommi): Change the names of candidate types to "host", "srflx" for
-// spec-compliance.
+// TODO(tommi): Move this definition to candidate.h.
 enum class IceCandidateType {
-  kLocal,
-  kStun,
+  kHost,
+  kSrflx,
   kPrflx,
   kRelay,
 };

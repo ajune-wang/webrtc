0bda7815760d6434966efbe29c5861c134d739fc
Philipp Hancke <phancke@microsoft.com>
2024-02-09 17:10:46 +0100

    Use codec-specific parameter comparison
    BUG=tbd
    
    Change-Id: I49844af348d3d306f2ef5f572cd6c05ce5a3724f

--- media/base/codec.cc
+++ media/base/codec.cc
@@ -246,7 +246,9 @@ bool Codec::MatchesRtpCodec(const webrtc::RtpCodec& codec_capability) const {
          (codec_parameters.name == cricket::kRtxCodecName ||
           (codec_parameters.num_channels == codec_capability.num_channels &&
            codec_parameters.clock_rate == codec_capability.clock_rate &&
-           codec_parameters.parameters == codec_capability.parameters));
+           IsSameCodecSpecific(
+               codec_parameters.name, codec_parameters.parameters,
+               codec_capability.name, codec_capability.parameters)));
 }
 
 bool Codec::GetParam(const std::string& name, std::string* out) const {

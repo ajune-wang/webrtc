a7559ccdce2201288d8551ce357afac5e5a8bfd7
Philipp Hancke <phancke@microsoft.com>
2024-02-09 19:26:57 +0100

    p2p: separate ICE tie breaker and foundation seed
    BUG=webrtc:14626
    
    Change-Id: I189a708192c9cef0b50c3fcbe798b30376d3b547

--- p2p/client/basic_port_allocator.cc
+++ p2p/client/basic_port_allocator.cc
@@ -1498,7 +1498,8 @@ void AllocationSequence::CreateUDPPorts() {
   }
 
   if (port) {
-    port->SetIceTiebreaker(session_->ice_tiebreaker());
+    port->SetIceTiebreaker(session_->allocator()->ice_tiebreaker());
+    port->SetFoundationSeed(session_->allocator()->foundation_seed());
     // If shared socket is enabled, STUN candidate will be allocated by the
     // UDPPort.
     if (IsFlagSet(PORTALLOCATOR_ENABLE_SHARED_SOCKET)) {
@@ -1534,7 +1535,8 @@ void AllocationSequence::CreateTCPPorts() {
       session_->allocator()->allow_tcp_listen(),
       session_->allocator()->field_trials());
   if (port) {
-    port->SetIceTiebreaker(session_->ice_tiebreaker());
+    port->SetIceTiebreaker(session_->allocator()->ice_tiebreaker());
+    port->SetFoundationSeed(session_->allocator()->foundation_seed());
     session_->AddAllocatedPort(port.release(), this);
     // Since TCPPort is not created using shared socket, `port` will not be
     // added to the dequeue.
@@ -1564,7 +1566,8 @@ void AllocationSequence::CreateStunPorts() {
       session_->allocator()->stun_candidate_keepalive_interval(),
       session_->allocator()->field_trials());
   if (port) {
-    port->SetIceTiebreaker(session_->ice_tiebreaker());
+    port->SetIceTiebreaker(session_->allocator()->ice_tiebreaker());
+    port->SetFoundationSeed(session_->allocator()->foundation_seed());
     session_->AddAllocatedPort(port.release(), this);
     // Since StunPort is not created using shared socket, `port` will not be
     // added to the dequeue.
@@ -1667,7 +1670,8 @@ void AllocationSequence::CreateTurnPort(const RelayServerConfig& config,
       }
     }
     RTC_DCHECK(port != NULL);
-    port->SetIceTiebreaker(session_->ice_tiebreaker());
+    port->SetIceTiebreaker(session_->allocator()->ice_tiebreaker());
+    port->SetFoundationSeed(session_->allocator()->foundation_seed());
     session_->AddAllocatedPort(port.release(), this);
   }
 }

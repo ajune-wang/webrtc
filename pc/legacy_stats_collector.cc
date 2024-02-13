c0bc5801c4cade6043818488f874f91620df7909
Tommi <tommi@webrtc.org>
2024-02-13 11:44:58 +0100

    Change type() getter from std::string to absl::string_view
    Add a `type_name()` getter that'll take over call sites where the
    display string is needed. Currently the same thing as `type()`.
    
    Bug: none
    Change-Id: I35c50d026e4578a25d51765d59c6f2e01b850c94

--- pc/legacy_stats_collector.cc
+++ pc/legacy_stats_collector.cc
@@ -811,11 +811,11 @@ StatsReport* LegacyStatsCollector::AddConnectionInfoReport(
   report->AddString(StatsReport::kStatsValueNameLocalAddress,
                     info.local_candidate.address().ToString());
   report->AddString(StatsReport::kStatsValueNameLocalCandidateType,
-                    info.local_candidate.type());
+                    info.local_candidate.type().data());
   report->AddString(StatsReport::kStatsValueNameRemoteAddress,
                     info.remote_candidate.address().ToString());
   report->AddString(StatsReport::kStatsValueNameRemoteCandidateType,
-                    info.remote_candidate.type());
+                    info.remote_candidate.type().data());
   report->AddString(StatsReport::kStatsValueNameTransportType,
                     info.local_candidate.protocol());
   report->AddString(StatsReport::kStatsValueNameLocalCandidateRelayProtocol,

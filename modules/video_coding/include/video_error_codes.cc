7f7c56fa0187f2cc557d82356523f7be5cc4de0a
henrika <henrika@webrtc.org>
2024-02-13 14:43:12 +0100

    Extends WebRTC logs for software encoder fallback
    This CL extends logging related to HW->SW fallbacks on the encoder
    side in WebRTC. The goal is to make it easier to track down the
    different steps taken when setting up the video encoder and why/when
    HW encoding fails.
    
    Current logs are added on several lines which makes regexp searching
    difficult. This CL adds all related information on one line instead.
    
    Two new search tags are also added VSE (VideoStreamEncoder) and VESFW
    (VideoEncoderSoftwareFallbackWrapper). The idea is to allow searching
    for the tags to see correlated logs.
    
    It has been verified that these added logs also show up in WebRTC
    logs in Meet.
    
    Logs from the GPU process are not included due to the sandboxed
    nature which makes it much more complex to add to the native
    WebRTC log. I think that these simple logs will provide value as is.
    
    Example: https://gist.github.com/henrik-and/697671ad663fb7cf8236e6c409cd44db
    
    Bug: b/322132132
    Change-Id: Iec58c9741a9dd6bab3236a88e9a6e45440f5d980

---
+++ modules/video_coding/include/video_error_codes.cc
@@ -0,0 +1,43 @@
+/*
+ *  Copyright (c) 2024 The WebRTC project authors. All Rights Reserved.
+ *
+ *  Use of this source code is governed by a BSD-style license
+ *  that can be found in the LICENSE file in the root of the source
+ *  tree. An additional intellectual property rights grant can be found
+ *  in the file PATENTS.  All contributing project authors may
+ *  be found in the AUTHORS file in the root of the source tree.
+ */
+
+#include "third_party/webrtc/modules/video_coding/include/video_error_codes.h"
+
+namespace webrtc {
+const char* WebRtcVideoCodecErrorToString(int32_t error_code) {
+  switch (error_code) {
+    case WEBRTC_VIDEO_CODEC_TARGET_BITRATE_OVERSHOOT:
+      return "CODEC_TARGET_BITRATE_OVERSHOOT";
+    case WEBRTC_VIDEO_CODEC_OK_REQUEST_KEYFRAME:
+      return "CODEC_OK_REQUEST_KEYFRAME";
+    case WEBRTC_VIDEO_CODEC_NO_OUTPUT:
+      return "CODEC_NO_OUTPUT";
+    case WEBRTC_VIDEO_CODEC_ERROR:
+      return "CODEC_ERROR";
+    case WEBRTC_VIDEO_CODEC_MEMORY:
+      return "CODEC_MEMORY";
+    case WEBRTC_VIDEO_CODEC_ERR_PARAMETER:
+      return "CODEC_ERR_PARAMETER";
+    case WEBRTC_VIDEO_CODEC_TIMEOUT:
+      return "CODEC_TIMEOUT";
+    case WEBRTC_VIDEO_CODEC_UNINITIALIZED:
+      return "CODEC_UNINITIALIZED";
+    case WEBRTC_VIDEO_CODEC_FALLBACK_SOFTWARE:
+      return "CODEC_FALLBACK_SOFTWARE";
+    case WEBRTC_VIDEO_CODEC_ERR_SIMULCAST_PARAMETERS_NOT_SUPPORTED:
+      return "CODEC_ERR_SIMULCAST_PARAMETERS_NOT_SUPPORTED";
+    case WEBRTC_VIDEO_CODEC_ENCODER_FAILURE:
+      return "CODEC_ENCODER_FAILURE";
+    default:
+      return "UNKNOWN";
+  }
+}
+
+}  // namespace webrtc

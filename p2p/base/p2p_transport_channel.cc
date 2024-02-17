ffa49813ada4f89691a196c0b4b7c077f1ec1c0d
Tommi <tommi@webrtc.org>
2024-02-17 19:40:01 +0100

    Deprecate old constructor and set_type()
    Bug: webrtc:15846
    Change-Id: If24ed674e175642efa49da37fd2bc847dd14f613

--- p2p/base/p2p_transport_channel.cc
+++ p2p/base/p2p_transport_channel.cc
@@ -1084,8 +1084,8 @@ void P2PTransportChannel::OnUnknownAddress(PortInterface* port,
     // candidate.
     remote_candidate = Candidate(
         component(), ProtoToString(proto), address, remote_candidate_priority,
-        remote_username, remote_password, PRFLX_PORT_TYPE, remote_generation,
-        "", network_id, network_cost);
+        remote_username, remote_password, IceCandidateType::kPrflx,
+        remote_generation, "", network_id, network_cost);
     if (proto == PROTO_TCP) {
       remote_candidate.set_tcptype(TCPTYPE_ACTIVE_STR);
     }

89cb8474b132ac6c6995052d51fc75e7bb29aca0
Per K <perkj@webrtc.org>
2024-02-28 16:50:49 +0100

    Propagate ECN information on posix sockets to rtc::ReceivedPacket
    Two new socket options are introduced OPT_SEND_ECN used for setting ECN bits. OPT_RECV_ECN used for reading the ECN bits.
    
    If ECN bits are set on received IP packets,  ECT(1) and CE is propagated via rtc::ReceivedPacket.
    
    Bug: webrtc:15368
    Change-Id: I3ac335007e2f7d30564569bbc80ce47fa541bef1

---
+++ rtc_base/network/ecn_marking.h
@@ -0,0 +1,26 @@
+/*
+ *  Copyright 2024 The WebRTC Project Authors. All rights reserved.
+ *
+ *  Use of this source code is governed by a BSD-style license
+ *  that can be found in the LICENSE file in the root of the source
+ *  tree. An additional intellectual property rights grant can be found
+ *  in the file PATENTS.  All contributing project authors may
+ *  be found in the AUTHORS file in the root of the source tree.
+ */
+
+#ifndef RTC_BASE_NETWORK_ECN_MARKING_H_
+#define RTC_BASE_NETWORK_ECN_MARKING_H_
+
+namespace rtc {
+
+// TODO(https://bugs.webrtc.org/15368): L4S support is slowly being developed.
+// Help is appreciated.
+
+// L4S Explicit Congestion Notification (ECN) .
+// https://www.rfc-editor.org/rfc/rfc9331.html ECT stands for ECN-Capable
+// Transport and CE stands for Congestion Experienced.
+enum class EcnMarking { kNotECT = 0, kECT1 = 1, kCE = 3 };
+
+}  // namespace rtc
+
+#endif  // RTC_BASE_NETWORK_ECN_MARKING_H_

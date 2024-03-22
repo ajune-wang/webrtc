46b147141a3568ef6e8434195e2a961fb2c3d196
Danil Chapovalov <danilchap@webrtc.org>
2024-03-22 15:19:55 +0100

    Update InternalEncoderFactory to implement non-deprecated variant of CreateVideoEncoder
    Bug: webrtc:15860
    Change-Id: I7511ac501bdcb6319546265c6212a639576859d7

--- media/engine/internal_encoder_factory.h
+++ media/engine/internal_encoder_factory.h
@@ -25,8 +25,8 @@ class RTC_EXPORT InternalEncoderFactory : public VideoEncoderFactory {
   CodecSupport QueryCodecSupport(
       const SdpVideoFormat& format,
       absl::optional<std::string> scalability_mode) const override;
-  std::unique_ptr<VideoEncoder> CreateVideoEncoder(
-      const SdpVideoFormat& format) override;
+  std::unique_ptr<VideoEncoder> Create(const Environment& env,
+                                       const SdpVideoFormat& format) override;
 };
 
 }  // namespace webrtc

5c5bf8c13692ed35fa018d041bc530768c2a4f6f
Tommi <tommi@webrtc.org>
2024-04-21 18:38:38 +0200

    Start using ArrayView in AudioFrame, update PushResampler
    Start introducing ArrayView to AudioFrame and code that flows down
    from there.  In this first step:
    * Add `data_view()` that returns a read-only ArrayView for
    * Add `mutable_data()` that requires two arguments for properly setting
      the samples per channel and number of channels that's required for
      accurately reserving the returned mutable ArrayView.
      A notable behavior change is that if the requested number of channels
      is larger than supported or the calculated buffer size is too large,
      the function will trigger a check.
    * Add `sample_count()` which represents the number of valid samples
      contained by the AudioFrame. This will be useful for future work
      and tackles what is a bit of a legacy issue for AudioFrame.
      AudioFrame has grown from starting out as a struct to now being a
      sort of hybrid with some public member variables, some private.
      Some and methods expect that public variables are sometimes
      ('might be') set before calling them and sometimes expect that
      they're adjusted _after_ a method call but before use of other
      operations (e.g. as is the case for the pre-existing `mutable_data()`
      method. To fix that, we'll over time need to migrate away from the
      current `data()` and `mutable_data()` methods, over to safer variants.
    * Add TODOs for following work.
    
    Bug: chromium:335805780
    Change-Id: I2937de800422589ebe6a3840b3caadf3d9ff8b00

--- api/audio/audio_frame.h
+++ api/audio/audio_frame.h
@@ -14,6 +14,7 @@
 #include <stddef.h>
 #include <stdint.h>
 
+#include "api/array_view.h"
 #include "api/audio/channel_layout.h"
 #include "api/rtp_packet_infos.h"
 
@@ -68,6 +69,7 @@ class AudioFrame {
   // ResetWithoutMuting() to skip this wasteful zeroing.
   void ResetWithoutMuting();
 
+  // TODO: b/335805780 - Accept ArrayView.
   void UpdateFrame(uint32_t timestamp,
                    const int16_t* data,
                    size_t samples_per_channel,
@@ -90,10 +92,19 @@ class AudioFrame {
   int64_t ElapsedProfileTimeMs() const;
 
   // data() returns a zeroed static buffer if the frame is muted.
-  // mutable_frame() always returns a non-static buffer; the first call to
-  // mutable_frame() zeros the non-static buffer and marks the frame unmuted.
+  // TODO: b/335805780 - Return ArrayView.
   const int16_t* data() const;
+  rtc::ArrayView<const int16_t> data_view() const;
+  // mutable_frame() always returns a non-static buffer; the first call to
+  // mutable_frame() zeros the buffer and marks the frame as unmuted.
+  // TODO: b/335805780 - Return ArrayView based on the current values for
+  // samples per channel and num channels.
   int16_t* mutable_data();
+  // Prepares the internal buffer for writing. If the state is currently muted,
+  // then the internal buffer will be zeroed out before returning a writable
+  // buffer.
+  rtc::ArrayView<int16_t> mutable_data(size_t samples_per_channel,
+                                       size_t num_channels);
 
   // Prefer to mute frames using AudioFrameOperations::Mute.
   void Mute();
@@ -105,6 +116,12 @@ class AudioFrame {
   size_t num_channels() const { return num_channels_; }
   ChannelLayout channel_layout() const { return channel_layout_; }
   int sample_rate_hz() const { return sample_rate_hz_; }
+  // Total number of valid 16 bit samples held in the internal buffer.
+  // Note that for legacy reasons, samples_per_channel_ may hold a non 0 value
+  // even after UpdateFrame has been called with a nullptr.
+  size_t sample_count() const {
+    return muted_ ? 0u : samples_per_channel_ * num_channels_;
+  }
 
   void set_absolute_capture_timestamp_ms(
       int64_t absolute_capture_time_stamp_ms) {

c9388e8a793e06733903b9818598976a18d0c30b
Raman Budny <budnyjj@gmail.com>
2024-04-22 09:21:52 +0000

    Revert "Allow streams to be sent without |source_| being initially set"
    This reverts commit af74dff19ee8fffd4b4dd1c807518eebb401db39 causing
    audio being captured and sent without AudioTrack created on Android.
    We'd have to use it as a temporary workaround till the proper solution
    will be provided by the WebRTC team.
    
    
    Bug: webrtc:15918
    Change-Id: I8cdc939c2730ef8990bae05054e78e0051259d18

--- media/engine/webrtc_voice_engine.cc
+++ media/engine/webrtc_voice_engine.cc
@@ -1094,10 +1094,9 @@ class WebRtcVoiceSendChannel::WebRtcAudioSendStream : public AudioSource::Sink {
     RTC_DCHECK_RUN_ON(&worker_thread_checker_);
     RTC_DCHECK(stream_);
     RTC_DCHECK_EQ(1UL, rtp_parameters_.encodings.size());
-    // Stream can be started without |source_| being set.
-    if (send_ && rtp_parameters_.encodings[0].active) {
+    if (send_ && source_ != nullptr && rtp_parameters_.encodings[0].active) {
       stream_->Start();
-    } else {
+    } else {  // !send || source_ = nullptr
       stream_->Stop();
     }
   }

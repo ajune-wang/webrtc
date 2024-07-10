329ddf879b25cb726c755e3c60a74a78c76d5669
Abby Yeh <abbyyeh@webrtc.org>
2024-07-10 16:33:05 +0200

    Add listener to detect mute speech event, and callback function to handle the event
    Bug: 343347289
    Change-Id: I56b1433b0dd8220f95d7d72fb04b4f92fe4a905e

--- sdk/objc/native/api/audio_device_module.h
+++ sdk/objc/native/api/audio_device_module.h
@@ -25,6 +25,11 @@ namespace webrtc {
 rtc::scoped_refptr<AudioDeviceModule> CreateAudioDeviceModule(
     bool bypass_voice_processing = false);
 
+// If `muted_speech_event_handler` is exist, audio unit will catch speech activity while muted.
+rtc::scoped_refptr<AudioDeviceModule> CreateMutedDetectAudioDeviceModule(
+    bool bypass_voice_processing = false,
+    AudioDeviceModule::MutedSpeechEventHandler muted_speech_event_handler = nullptr);
+
 }  // namespace webrtc
 
 #endif  // SDK_OBJC_NATIVE_API_AUDIO_DEVICE_MODULE_H_

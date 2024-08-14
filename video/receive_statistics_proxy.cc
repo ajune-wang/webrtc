4d72c483e9cddb0b60123a9f25af89a1b1dc7c14
Emil Vardar <vardar@google.com>
2024-08-14 13:44:58 +0000

    Aggregate and log corruption score.
    Bug: webrtc:358039777
    Change-Id: I4dade8e6daecf41e5b1f156416935c320f513d0b

--- video/receive_statistics_proxy.cc
+++ video/receive_statistics_proxy.cc
@@ -311,6 +311,14 @@ void ReceiveStatisticsProxy::UpdateHistograms(
                  << " " << *height << '\n';
     }
 
+    absl::optional<double> corruption_score = stats.corruption_score.GetMean();
+    if (corruption_score) {
+      RTC_HISTOGRAM_COUNTS(uma_prefix + ".CorruptionScore", *corruption_score,
+                           /*min=*/0, /*max=*/1, /*bucket_count=*/10);
+      log_stream << uma_prefix << ".CorruptionScore" << " " << *corruption_score
+                 << '\n';
+    }
+
     if (content_type != VideoContentType::UNSPECIFIED) {
       // Don't report these 3 metrics unsliced, as more precise variants
       // are reported separately in this method.
@@ -823,6 +831,26 @@ void ReceiveStatisticsProxy::OnRttUpdate(int64_t avg_rtt_ms) {
   avg_rtt_ms_ = avg_rtt_ms;
 }
 
+void ReceiveStatisticsProxy::OnCorruptionScore(double corruption_score,
+                                               VideoContentType content_type) {
+  RTC_DCHECK_RUN_ON(&main_thread_);
+
+  if (!stats_.corruption_score_sum.has_value()) {
+    RTC_DCHECK(!stats_.corruption_score_squared_sum.has_value());
+    RTC_DCHECK_EQ(stats_.frames_corruption_score_calculated, 0);
+    stats_.corruption_score_sum = 0;
+    stats_.corruption_score_squared_sum = 0;
+  }
+  *stats_.corruption_score_sum += corruption_score;
+  *stats_.corruption_score_squared_sum +=
+      (corruption_score) * (corruption_score);
+  ++stats_.frames_corruption_score_calculated;
+
+  ContentSpecificStats* content_specific_stats =
+      &content_specific_stats_[content_type];
+  content_specific_stats->corruption_score.AddSample(corruption_score);
+}
+
 void ReceiveStatisticsProxy::DecoderThreadStarting() {
   RTC_DCHECK_RUN_ON(&main_thread_);
 }
@@ -849,6 +877,7 @@ void ReceiveStatisticsProxy::ContentSpecificStats::Add(
   frame_counts.key_frames += other.frame_counts.key_frames;
   frame_counts.delta_frames += other.frame_counts.delta_frames;
   interframe_delay_percentiles.Add(other.interframe_delay_percentiles);
+  corruption_score.MergeStatistics(other.corruption_score);
 }
 
 }  // namespace internal

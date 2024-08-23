b5b6dc909d51a3391d118245dfd9abee8387f8a0
Benjamin Williams <bwwilliams@google.com>
2024-08-23 12:56:06 -0400

    Refactor WebRTC self assignments in if clauses
    This change refactors existing self-assignments within if clauses across
    the WebRTC codebase.
    
    *Why:*
    
    - Bug Prevention: Assignments within conditionals are frequently
      unintended errors, often mistaken for equality checks.
    
    - Clearer Code: Separating assignments from conditionals improves code
      readability and reduces the risk of misinterpretation.
    
    Change-Id: I199dc26a35ceca109a2ac569b446811314dfdf0b

--- examples/objc/AppRTCMobile/ARDCaptureController.m
+++ examples/objc/AppRTCMobile/ARDCaptureController.m
@@ -24,7 +24,9 @@ const Float64 kFramerateLimit = 30.0;
 
 - (instancetype)initWithCapturer:(RTC_OBJC_TYPE(RTCCameraVideoCapturer) *)capturer
                         settings:(ARDSettingsModel *)settings {
-  if (self = [super init]) {
+  self = [super init];
+
+  if (self) {
     _capturer = capturer;
     _settings = settings;
     _usingFrontCamera = YES;

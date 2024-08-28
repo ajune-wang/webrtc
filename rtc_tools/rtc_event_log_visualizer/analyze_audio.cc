ff1e66fa5532b326c5e0fcb62235fb40c2de7125
Danil Chapovalov <danilchap@webrtc.org>
2024-08-28 16:20:38 +0200

    Delete deprecated AudioDecoderFactory::MakeAudioDecoder
    Bug: webrtc:356878416
    Change-Id: I672796e5ec749c3ae0141802922951d4fc562d78

--- rtc_tools/rtc_event_log_visualizer/analyze_audio.cc
+++ rtc_tools/rtc_event_log_visualizer/analyze_audio.cc
@@ -218,7 +218,8 @@ class ReplacementAudioDecoderFactory : public AudioDecoderFactory {
     return true;
   }
 
-  std::unique_ptr<AudioDecoder> MakeAudioDecoder(
+  std::unique_ptr<AudioDecoder> Create(
+      const Environment& env,
       const SdpAudioFormat& format,
       absl::optional<AudioCodecPairId> codec_pair_id) override {
     auto replacement_file = std::make_unique<test::ResampleInputAudioFile>(

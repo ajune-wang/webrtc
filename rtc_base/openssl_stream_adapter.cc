b1f5d8a80d119378617aba59336a454523c4f6fc
Philipp Hancke <phancke@meta.com>
2024-08-31 13:34:42 -0700

    ssl: use  DTLSv1_set_initial_timeout_duration for OpenSSL too
    since it is available there as well (uhm ... is it?)
    
    and remove SSL_set_read_ahead for OpenSSL since it has no impact for DTLS:
      https://docs.openssl.org/1.1.1/man3/SSL_CTX_set_read_ahead/
    
    BUG=webrtc:353750117
    
    Change-Id: I09d135aa69471af9d2feaf82e35625da3ae2e247

--- rtc_base/openssl_stream_adapter.cc
+++ rtc_base/openssl_stream_adapter.cc
@@ -875,13 +875,7 @@ int OpenSSLStreamAdapter::BeginSSL() {
 
   SSL_set_bio(ssl_, bio, bio);  // the SSL object owns the bio now.
   if (ssl_mode_ == SSL_MODE_DTLS) {
-#ifdef OPENSSL_IS_BORINGSSL
     DTLSv1_set_initial_timeout_duration(ssl_, dtls_handshake_timeout_ms_);
-#else
-    // Enable read-ahead for DTLS so whole packets are read from internal BIO
-    // before parsing. This is done internally by BoringSSL for DTLS.
-    SSL_set_read_ahead(ssl_, 1);
-#endif
   }
 
   SSL_set_mode(ssl_, SSL_MODE_ENABLE_PARTIAL_WRITE |

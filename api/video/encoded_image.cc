fdfae3ff41f736a7ea37e99f02fd05e76737653e
Sergio Garcia Murillo <sergio.garcia.murillo@gmail.com>
2024-10-01 18:05:06 +0200

    Use rtc::Buffer for memory storage of EncodedImageBuffer
    The goal is to be able to write the rtc::Buffer by another utility 
    (like rtc::ByteBufferWriter) and pass it into EncodedImageBuffer
    without memcpy.
    
    Bug: webrtc:42223344
    Change-Id: Ieda55e77a36636e8cdff6ad6b7d078de0aeafec0

--- api/video/encoded_image.cc
+++ api/video/encoded_image.cc
@@ -23,18 +23,13 @@
 
 namespace webrtc {
 
-EncodedImageBuffer::EncodedImageBuffer(size_t size) : size_(size) {
-  buffer_ = static_cast<uint8_t*>(malloc(size));
-}
+EncodedImageBuffer::EncodedImageBuffer(size_t size) : buffer_(size) {}
 
 EncodedImageBuffer::EncodedImageBuffer(const uint8_t* data, size_t size)
-    : EncodedImageBuffer(size) {
-  std::copy_n(data, size, buffer_);
-}
+    : buffer_(data, size) {}
 
-EncodedImageBuffer::~EncodedImageBuffer() {
-  free(buffer_);
-}
+EncodedImageBuffer::EncodedImageBuffer(rtc::Buffer&& buffer)
+    : buffer_(std::move(buffer)) {}
 
 // static
 rtc::scoped_refptr<EncodedImageBuffer> EncodedImageBuffer::Create(size_t size) {
@@ -46,25 +41,24 @@ rtc::scoped_refptr<EncodedImageBuffer> EncodedImageBuffer::Create(
     size_t size) {
   return rtc::make_ref_counted<EncodedImageBuffer>(data, size);
 }
+// static
+rtc::scoped_refptr<EncodedImageBuffer> EncodedImageBuffer::Create(
+    rtc::Buffer&& buffer) {
+  return rtc::make_ref_counted<EncodedImageBuffer>(std::move(buffer));
+}
 
 const uint8_t* EncodedImageBuffer::data() const {
-  return buffer_;
+  return buffer_.data();
 }
 uint8_t* EncodedImageBuffer::data() {
-  return buffer_;
+  return buffer_.data();
 }
 size_t EncodedImageBuffer::size() const {
-  return size_;
+  return buffer_.size();
 }
 
 void EncodedImageBuffer::Realloc(size_t size) {
-  // Calling realloc with size == 0 is equivalent to free, and returns nullptr.
-  // Which is confusing on systems where malloc(0) doesn't return a nullptr.
-  // More specifically, it breaks expectations of
-  // VCMSessionInfo::UpdateDataPointers.
-  RTC_DCHECK(size > 0);
-  buffer_ = static_cast<uint8_t*>(realloc(buffer_, size));
-  size_ = size;
+  buffer_.SetSize(size);
 }
 
 EncodedImage::EncodedImage() = default;

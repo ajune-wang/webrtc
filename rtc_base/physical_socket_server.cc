55769a47168fcb311613181d3d847182bf6bd88e
Dor Hen <dorhen@meta.com>
2024-10-04 17:35:32 -0700

    Comment unused variables in implemented functions 3\n
    Bug: webrtc:370878648
    Change-Id: I40251cc529cc20fbf2b034fa25798965b91dbd88

--- rtc_base/physical_socket_server.cc
+++ rtc_base/physical_socket_server.cc
@@ -89,7 +89,7 @@ int64_t GetSocketRecvTimestamp(int socket) {
 
 #else
 
-int64_t GetSocketRecvTimestamp(int socket) {
+int64_t GetSocketRecvTimestamp(int /* socket */) {
   return -1;
 }
 #endif
@@ -1165,7 +1165,7 @@ class Signaler : public Dispatcher {
 
   uint32_t GetRequestedEvents() override { return DE_READ; }
 
-  void OnEvent(uint32_t ff, int err) override {
+  void OnEvent(uint32_t /* ff */, int /* err */) override {
     // It is not possible to perfectly emulate an auto-resetting event with
     // pipes.  This simulates it by resetting before the event is handled.
 
@@ -1347,7 +1347,7 @@ void PhysicalSocketServer::Remove(Dispatcher* pdispatcher) {
 #endif  // WEBRTC_USE_EPOLL
 }
 
-void PhysicalSocketServer::Update(Dispatcher* pdispatcher) {
+void PhysicalSocketServer::Update([[maybe_unused]] Dispatcher* pdispatcher) {
 #if defined(WEBRTC_USE_EPOLL)
   if (epoll_fd_ == INVALID_SOCKET) {
     return;

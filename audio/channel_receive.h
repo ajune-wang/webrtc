e01e75bee645840bb1ab461da6b1576dfbf66e92
Olov Brändström <brandstrom@google.com>
2024-10-08 23:13:12 +0200

    Add an environment clock timestamp to SenderReportStats.
    Add an environment clock timestamp to SenderReportStats and make it visible in rtc_stats_collector.cc. This make it possible to use the pc->GetConfiguration().stats_timestamp_with_environment_clock() flag to decide which timestamp to use when creating a RTCRemoteOutboundRtpStreamStats object.
    
    This CL is the third (and possible the last) of a series of CLs that aim to replace the UTC timestamps in RTCStats objects to Environment clock timestamps. The other CLs where https://webrtc-review.googlesource.com/c/src/+/363946 and https://webrtc-review.googlesource.com/c/src/+/364782.
    
    When Chromium and Google internal uses of RTCStats are updated to set the stats_timestamp_with_environment_clock configuration, the flag can be deleted.
    
    Bug:chromium:369369568
    Change-Id: Ic0b07d7b012505267bd6516f19a9ba90df4cafab

--- audio/channel_receive.h
+++ audio/channel_receive.h
@@ -67,6 +67,9 @@ struct CallReceiveStatistics {
   // Note that the timestamps below correspond to the time elapsed since the
   // Unix epoch.
   // https://w3c.github.io/webrtc-stats/#remoteoutboundrtpstats-dict*
+  std::optional<int64_t> last_sender_report_timestamp_ms;
+  // TODO(webrtc:370535296): When (webrtc:370535296) is fixed, we don't need the
+  // utc timestamp.
   std::optional<int64_t> last_sender_report_utc_timestamp_ms;
   std::optional<int64_t> last_sender_report_remote_utc_timestamp_ms;
   uint64_t sender_reports_packets_sent = 0;

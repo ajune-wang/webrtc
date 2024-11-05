54708e58de94fdb8beb749e8ad9d1724ce8e4c1d
Qiu Jianlin <jianlin.qiu@intel.com>
2024-11-05 15:05:38 +0800

    Include H.265 support in RTP video frame assembler.
    This addes support of H.265 into the RTP video frame assembler, which is
    now a public interface.
    
    Bug:chromium:41480904
    Change-Id: I74fd761949d0b095ba4526d2fa887e963f48abcb

--- api/video/rtp_video_frame_assembler.cc
+++ api/video/rtp_video_frame_assembler.cc
@@ -43,6 +43,10 @@
 #include "rtc_base/logging.h"
 #include "rtc_base/numerics/sequence_number_unwrapper.h"
 
+#ifdef RTC_ENABLE_H265
+#include "modules/rtp_rtcp/source/video_rtp_depacketizer_h265.h"
+#endif
+
 namespace webrtc {
 namespace {
 std::unique_ptr<VideoRtpDepacketizer> CreateDepacketizer(
@@ -61,9 +65,7 @@ std::unique_ptr<VideoRtpDepacketizer> CreateDepacketizer(
     case RtpVideoFrameAssembler::kGeneric:
       return std::make_unique<VideoRtpDepacketizerGeneric>();
     case RtpVideoFrameAssembler::kH265:
-      // TODO(bugs.webrtc.org/13485): Implement VideoRtpDepacketizerH265
-      RTC_DCHECK_NOTREACHED();
-      return nullptr;
+      return std::make_unique<VideoRtpDepacketizerH265>();
   }
   RTC_DCHECK_NOTREACHED();
   return nullptr;

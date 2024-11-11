6f0a7921146642e95fba776896f7f840e2aa3b4b
Qiu Jianlin <jianlin.qiu@intel.com>
2024-11-11 17:04:12 +0800

    Set default scalability mode for H.265 to L1T1.
    H.265 does not have software fallback, and it may have issue supporting
    more than 1 temporal layers on some devices. Set default to L1T1 when
    scalability is not configured, or if a scalability mode is reported as
    not supported by encoder.
    
    Bug:chromium:41480904
    Change-Id: I53895c45ec821d65774ffe2db5f418184e3fb02a

--- modules/video_coding/svc/scalability_mode_util.h
+++ modules/video_coding/svc/scalability_mode_util.h
@@ -26,6 +26,9 @@ enum class ScalabilityModeResolutionRatio {
 
 static constexpr char kDefaultScalabilityModeStr[] = "L1T2";
 
+// For codec without software fallback, prefer L1T1 as default.
+static constexpr char kDefaultNoScalabilityModeStr[] = "L1T1";
+
 RTC_EXPORT std::optional<ScalabilityMode> MakeScalabilityMode(
     int num_spatial_layers,
     int num_temporal_layers,

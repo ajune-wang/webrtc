fce8fb4430b2c4d502985f0a030bb41789259679
Qiu Jianlin <jianlin.qiu@intel.com>
2024-11-13 04:43:07 +0800

    Switch to use SvcRateAllocator for H.265 single-cast.
    New codecs should switch to SvcRateAllocator for single-cast stream.
    The SimulcastRateAllocator was designed for VP8 so avoid using it
    whenever possible.
    
    Bug:chromium:41480904
    Change-Id: Ie70d5be555b2691691655095dac47f0c85f51bb6

--- modules/video_coding/video_codec_initializer.cc
+++ modules/video_coding/video_codec_initializer.cc
@@ -31,6 +31,10 @@
 #include "rtc_base/logging.h"
 #include "rtc_base/numerics/safe_conversions.h"
 
+#ifdef RTC_ENABLE_H265
+#include "modules/video_coding/codecs/h265/h265_svc_config.h"
+#endif
+
 namespace webrtc {
 namespace {
 
@@ -359,6 +363,10 @@ VideoCodec VideoCodecInitializer::SetupCodec(
           break;
         }
       }
+      // Configure target bitrate for temporal scalability.
+      SetH265SvcConfig(video_codec,
+                       /*num_temporal_layers=*/
+                       streams.back().num_temporal_layers.value_or(1));
       break;
     default:
       // TODO(pbos): Support encoder_settings codec-agnostically.

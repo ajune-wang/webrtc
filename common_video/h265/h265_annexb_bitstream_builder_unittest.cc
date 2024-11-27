7ea383743051b81b8a2eff63061a22342965bc5a
Qiu Jianlin <jianlin.qiu@intel.com>
2024-11-27 22:01:37 +0800

    Add H.265 bitstream builder and fake frame builder.
    Adds the bitstream builder and fake frame builder, which produces
    bitstream that could pass checks by all parsers used in WebRTC modules,
    and the H.265 host bitstream parser before H.265 decode accelerators.
    Follow up CL will create the fake H.265 encoder and decoder using these
    builders, for the purpose of end-to-end integration tests, and
    frame-dropping tests.
    
    Bug:chromium:41480904
    Change-Id: Icd0a8719550c003a688f14a031b8301f72998bbe

---
+++ common_video/h265/h265_annexb_bitstream_builder_unittest.cc
@@ -0,0 +1,97 @@
+/*
+ *  Copyright (c) 2024 The WebRTC project authors. All Rights Reserved.
+ *
+ *  Use of this source code is governed by a BSD-style license
+ *  that can be found in the LICENSE file in the root of the source
+ *  tree. An additional intellectual property rights grant can be found
+ *  in the file PATENTS.  All contributing project authors may
+ *  be found in the AUTHORS file in the root of the source tree.
+ */
+
+#include "common_video/h265/h265_annexb_bitstream_builder.h"
+
+#include <stdint.h>
+
+#include "api/array_view.h"
+#include "common_video/h265/h265_common.h"
+#include "test/gtest.h"
+
+namespace webrtc {
+
+namespace {
+
+constexpr uint64_t kTestPattern = 0xfedcba0987654321;
+
+uint64_t GetDataFromBuffer(rtc::ArrayView<const uint8_t> buffer,
+                           uint64_t num_bits) {
+  uint64_t got = 0;
+  size_t index = 0;
+  while (num_bits > 8) {
+    got |= (buffer[index] & 0xff);
+    num_bits -= 8;
+    got <<= (num_bits > 8 ? 8 : num_bits);
+    index++;
+  }
+  if (num_bits > 0) {
+    uint64_t temp = (buffer[index] & 0xff);
+    temp >>= (8 - num_bits);
+    got |= temp;
+  }
+  return got;
+}
+
+uint64_t AlignUpToBytes(uint64_t num_bits) {
+  return (num_bits + 7) / 8;
+}
+
+}  // namespace
+
+class H265AnnexBBitstreamBuilderAppendBitsTest
+    : public ::testing::TestWithParam<uint64_t> {};
+
+TEST_P(H265AnnexBBitstreamBuilderAppendBitsTest, AppendAndVerifyBits) {
+  H265AnnexBBitstreamBuilder b;
+  uint64_t num_bits = GetParam();
+  ASSERT_LE(num_bits, 64u);
+  uint64_t num_bytes = AlignUpToBytes(num_bits);
+
+  b.AppendBits(num_bits, kTestPattern);
+  b.FlushReg();
+
+  EXPECT_EQ(b.BytesInBuffer(), num_bytes);
+
+  rtc::ArrayView<const uint8_t> ptr = b.data();
+  uint64_t got = GetDataFromBuffer(ptr, num_bits);
+  uint64_t expected = kTestPattern;
+
+  if (num_bits < 64) {
+    expected &= ((1ull << num_bits) - 1);
+  }
+
+  EXPECT_EQ(got, expected) << std::hex << "0x" << got << " vs 0x" << expected;
+}
+
+TEST_F(H265AnnexBBitstreamBuilderAppendBitsTest, VerifyFlushAndBitsInBuffer) {
+  H265AnnexBBitstreamBuilder b;
+  uint64_t num_bits = 20;
+  uint64_t num_bytes = AlignUpToBytes(num_bits);
+
+  b.AppendBits(num_bits, kTestPattern);
+  b.Flush();
+
+  EXPECT_EQ(b.BytesInBuffer(), num_bytes);
+  EXPECT_EQ(b.BitsInBuffer(), num_bits);
+
+  rtc::ArrayView<const uint8_t> ptr = b.data();
+  uint64_t got = GetDataFromBuffer(ptr, num_bits);
+  uint64_t expected = kTestPattern;
+  expected &= ((1ull << num_bits) - 1);
+
+  EXPECT_EQ(got, expected) << std::hex << "0x" << got << " vs 0x" << expected;
+}
+
+INSTANTIATE_TEST_SUITE_P(AppendNumBits,
+                         H265AnnexBBitstreamBuilderAppendBitsTest,
+                         ::testing::Range(static_cast<uint64_t>(1),
+                                          static_cast<uint64_t>(65)));
+}  // namespace webrtc
---
+++ common_video/h265/h265_frame_builder_unittest.cc
@@ -0,0 +1,111 @@
+/*
+ *  Copyright (c) 2024 The WebRTC project authors. All Rights Reserved.
+ *
+ *  Use of this source code is governed by a BSD-style license
+ *  that can be found in the LICENSE file in the root of the source
+ *  tree. An additional intellectual property rights grant can be found
+ *  in the file PATENTS.  All contributing project authors may
+ *  be found in the AUTHORS file in the root of the source tree.
+ */
+
+#include "common_video/h265/h265_frame_builder.h"
+
+#include <iostream>
+
+#include "common_video/h265/h265_annexb_bitstream_builder.h"
+#include "common_video/h265/h265_bitstream_parser.h"
+#include "common_video/h265/h265_common.h"
+#include "common_video/h265/h265_sps_parser.h"
+#include "test/gtest.h"
+
+namespace webrtc {
+
+class H265FrameBuilderTest : public ::testing::Test {
+ public:
+  H265FrameBuilderTest() {}
+  ~H265FrameBuilderTest() override {}
+};
+
+TEST_F(H265FrameBuilderTest, TestBuildKeyFrameSingleTemporalLayer) {
+  H265AnnexBBitstreamBuilder builder(
+      /*insert_emulation_prevention_bytes=*/true);
+
+  BuildKeyFrame(builder, 1920, 1080, /*num_temporal_layers=*/1,
+                /*qp=*/30, /*frame_size_bytes=*/2000);
+  H265BitstreamParser h265_parser;
+  h265_parser.ParseBitstream(builder.data());
+
+  std::optional<int> qp = h265_parser.GetLastSliceQp();
+  ASSERT_TRUE(qp.has_value());
+  EXPECT_EQ(30, *qp);
+
+  const H265SpsParser::SpsState* sps = h265_parser.GetSPS(0);
+  ASSERT_EQ(sps->width, 1920u);
+  ASSERT_EQ(sps->height, 1080u);
+  ASSERT_EQ(sps->sps_max_sub_layers_minus1, 0u);
+
+  BuildDeltaFrame(builder, /*temporal_layer_id=*/1, /*qp=*/27,
+                  /*frame_size_bytes=*/2000, /*wrapped_on_16_poc_lsb=*/1);
+  h265_parser.ParseBitstream(builder.data());
+  qp = h265_parser.GetLastSliceQp();
+  ASSERT_TRUE(qp.has_value());
+  EXPECT_EQ(27, *qp);
+}
+
+TEST_F(H265FrameBuilderTest, TestBuildFramesTwoTemporalLayers) {
+  H265AnnexBBitstreamBuilder builder(
+      /*insert_emulation_prevention_bytes=*/true);
+
+  BuildKeyFrame(builder, 1280, 720, /*num_temporal_layers=*/2, /*qp=*/30,
+                /*frame_size_bytes=*/2000);
+  H265BitstreamParser h265_parser;
+  h265_parser.ParseBitstream(builder.data());
+
+  std::optional<int> qp = h265_parser.GetLastSliceQp();
+  ASSERT_TRUE(qp.has_value());
+  EXPECT_EQ(30, *qp);
+
+  const H265SpsParser::SpsState* sps = h265_parser.GetSPS(0);
+  ASSERT_EQ(sps->width, 1280u);
+  ASSERT_EQ(sps->height, 720u);
+  ASSERT_EQ(sps->sps_max_sub_layers_minus1, 1u);
+
+  constexpr uint8_t kDeltaFrames = 32;
+  for (int i = 1; i <= kDeltaFrames; ++i) {
+    BuildDeltaFrame(builder, (i % 2), 27 + 3 * (i % 2), 2000, (i + 1) % 16);
+    h265_parser.ParseBitstream(builder.data());
+    qp = h265_parser.GetLastSliceQp();
+    ASSERT_TRUE(qp.has_value());
+    EXPECT_EQ(27 + 3 * (i % 2), *qp);
+  }
+}
+
+TEST_F(H265FrameBuilderTest, TestBuildFramesThreeTemporalLayers) {
+  H265AnnexBBitstreamBuilder builder(
+      /*insert_emulation_prevention_bytes=*/true);
+
+  BuildKeyFrame(builder, 1280, 720, /*num_temporal_layers=*/3, /*qp=*/30,
+                /*frame_size_bytes=*/2000);
+  H265BitstreamParser h265_parser;
+  h265_parser.ParseBitstream(builder.data());
+
+  std::optional<int> qp = h265_parser.GetLastSliceQp();
+  ASSERT_TRUE(qp.has_value());
+  EXPECT_EQ(30, *qp);
+
+  const H265SpsParser::SpsState* sps = h265_parser.GetSPS(0);
+  ASSERT_EQ(sps->width, 1280u);
+  ASSERT_EQ(sps->height, 720u);
+  ASSERT_EQ(sps->sps_max_sub_layers_minus1, 2u);
+
+  constexpr uint8_t kDeltaFrames = 32;
+  for (int i = 1; i <= kDeltaFrames; ++i) {
+    BuildDeltaFrame(builder, (i % 3), 33 + 3 * (i % 3), 2000, (i + 1) % 16);
+    h265_parser.ParseBitstream(builder.data());
+    qp = h265_parser.GetLastSliceQp();
+    ASSERT_TRUE(qp.has_value());
+    EXPECT_EQ(33 + 3 * (i % 3), *qp);
+  }
+}
+
+}  // namespace webrtc

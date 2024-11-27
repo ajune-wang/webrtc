31c0c0985a4237bbddd3bba92ce618ed89c3ec8b
Qiu Jianlin <jianlin.qiu@intel.com>
2024-11-27 21:42:40 +0800

    Add H.265 bitstream builder and fake frame builder.
    Adds the bitstream builder and fake frame builder, which produces
    bitstream that could pass checks by all parsers used in WebRTC modules,
    and the H.265 host bitstream parser before H.265 decode accelerators.
    Follow up CL will create the fake H.265 encoder and decoder using these
    builders, for the purpose of end-to-end integration tests, and
    frame-dropping tests.
    
    Bug:chromium:41480904
    Change-Id: Icd0a8719550c003a688f14a031b8301f72998bbe

---
+++ common_video/h265/h265_frame_builder.h
@@ -0,0 +1,41 @@
+/*
+ *  Copyright (c) 2024 The WebRTC project authors. All Rights Reserved.
+ *
+ *  Use of this source code is governed by a BSD-style license
+ *  that can be found in the LICENSE file in the root of the source
+ *  tree. An additional intellectual property rights grant can be found
+ *  in the file PATENTS.  All contributing project authors may
+ *  be found in the AUTHORS file in the root of the source tree.
+ */
+
+#ifndef COMMON_VIDEO_H265_H265_FRAME_BUILDER_H_
+#define COMMON_VIDEO_H265_H265_FRAME_BUILDER_H_
+
+#include <stdint.h>
+
+#include "rtc_base/system/rtc_export.h"
+
+namespace webrtc {
+
+class H265AnnexBBitstreamBuilder;
+
+// Builds a H.265 key frame with a single NALU containing a VPS, SPS and PPS.
+// Most of the params are fixed while the width, height, number of temporal
+// layers and QP can be set. The log2_max_pic_order_cnt_lsb_minus4 is set to 0.
+RTC_EXPORT void BuildKeyFrame(H265AnnexBBitstreamBuilder& builder,
+                              size_t width,
+                              size_t height,
+                              uint8_t num_temporal_layers,
+                              int qp,
+                              size_t frame_size_bytes);
+
+// Builds a H.265 delta frame with a single NALU containing a slice.
+RTC_EXPORT void BuildDeltaFrame(H265AnnexBBitstreamBuilder& builder,
+                                uint8_t temporal_layer_id,
+                                int qp,
+                                size_t frame_size,
+                                uint8_t wrapped_on_16_poc_lsb);
+
+}  // namespace webrtc
+
+#endif  // COMMON_VIDEO_H265_H265_FRAME_BUILDER_H_
